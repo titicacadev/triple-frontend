6e823ef8dc7ecb35aebefb4771967f00ba54bb20
Inbeom Hwang <hwanginbeom@gmail.com>
2018-08-03 19:15:10 +0900

    Add an example app

---
+++ packages/triple-design-system/examples/triple-article/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // include: (p) => { console.log(p); return true },
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, '../../src')
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/env",
+              "@babel/react"
+            ],
+            plugins: [
+              ["styled-components", { "ssr": true, "displayName": true, "preprocess": false }]
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
