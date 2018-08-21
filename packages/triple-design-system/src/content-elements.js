530b83447c879ac9660803428284507591eea507
Inbeom Hwang <hwanginbeom@gmail.com>
2018-08-21 15:46:10 +0900

    Use CDN: https://assets.triple.guide

--- packages/triple-design-system/src/content-elements.js
+++ packages/triple-design-system/src/content-elements.js
@@ -239,7 +239,7 @@ const PoiListScrapButton = styled.div`
   right: 0;
   width: 34px;
   height: 34px;
-  background-image: url(http://triple-web-assets-dev.s3-website-ap-northeast-1.amazonaws.com/images/${
+  background-image: url(https://assets.triple.guide/images/${
     ({ pressed }) => pressed ? 'btn-content-scrap-list-on@2x.png' : 'btn-content-scrap-list-off@2x.png'
   });
   background-size: 34px 34px;
@@ -362,7 +362,7 @@ const PoiCarouselScrapButton = styled.div`
   right: 3px;
   width: 36px;
   height: 36px;
-  background-image: url(http://triple-web-assets-dev.s3-website-ap-northeast-1.amazonaws.com/images/${
+  background-image: url(https://assets.triple.guide/images/${
     ({ pressed }) => pressed ? 'btn-content-scrap-overlay-on@2x.png' : 'btn-content-scrap-overlay-off@2x.png'
   });
   background-size: 36px 36px;
