4dcc73a936e3d8aa7524f37a1f597d4c23164e18
Kim Kyudeok <polysiya@gmail.com>
2018-09-05 10:56:28 +0900

    Merge pull request #51 from titicacadev/attraction-example
    Add attraction example

--- packages/triple-design-system/examples/triple-article/src/App.js
+++ packages/triple-design-system/examples/triple-article/src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import { Tab } from 'semantic-ui-react'
 import { Article } from '@titicaca/triple-design-system/src'
-import sample from './sample.json'
+
+import post from './post.json'
+import attraction from './attraction.json'
+
+const PANES = [
+  { menuItem: 'Post', render: () => <Post /> },
+  { menuItem: 'Attraction', render: () => <Attraction /> },
+]
+
+function Post() {
+  return <Article>{post}</Article>
+}
+
+function Attraction() {
+  const { featuredContent } = attraction
+
+  return <Article>{featuredContent}</Article>
+}
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Article>
-          {sample}
-        </Article>
+        <Tab panes={PANES} />
       </div>
     )
   }
