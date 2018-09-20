2af182cd3db2de4548fc8b9b06aaa693914efcce
Inbeom Hwang <hwanginbeom@gmail.com>
2018-09-20 16:31:00 +0900

    Associate full URLs with icon names

--- packages/triple-design-system/src/elements/icon.js
+++ packages/triple-design-system/src/elements/icon.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
-const NAMES = {
-  save: 'ico-save@2x.png',
-  web: 'ico-end-web@2x.png',
-  call: 'ico-end-call@2x.png',
-  map: 'ico-end-poi@2x.png',
+const URL_BY_NAMES = {
+  save: 'https://assets.triple.guide/images/ico-save@2x.png',
+  web: 'https://assets.triple.guide/images/ico-end-web@2x.png',
+  call: 'https://assets.triple.guide/images/ico-end-call@2x.png',
+  map: 'https://assets.triple.guide/images/ico-end-poi@2x.png',
 }
 
 const SIZES = {
@@ -17,7 +17,7 @@ const Icon = styled.div`
   width: ${({ size }) => SIZES[size || 'small']};
   height: ${({ size }) => SIZES[size || 'small']};
   background-image: ${({ src, name }) =>
-    `url(${src ? src : `https://assets.triple.guide/images/${NAMES[name]}`})`};
+    `url(${src ? src : URL_BY_NAMES[name]})`};
   background-size: ${({ size }) =>
     `${SIZES[size || 'small']} ${SIZES[size || 'small']}`};
   background-repeat: no-repeat;
