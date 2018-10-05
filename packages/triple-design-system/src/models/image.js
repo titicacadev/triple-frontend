3d37d2a88f66972c99c12e5e0339930244f3927e
Inbeom Hwang <hwanginbeom@gmail.com>
2018-10-05 16:42:25 +0900

    Allow fixation of images' frame/size

--- packages/triple-design-system/src/models/image.js
+++ packages/triple-design-system/src/models/image.js
@@ -2,16 +2,26 @@ import React from 'react'
 import Pager from '../elements/pager'
 import Image from '../elements/image'
 
-export const ImagePager = ({ margin, images, onImageClick, ImageSource }) => (
+export const ImagePager = ({
+  margin,
+  size: globalSize,
+  frame: globalFrame,
+  images,
+  onImageClick,
+  ImageSource,
+}) => (
   <Pager margin={margin}>
     {images.map((image, i) => {
-      const { frame, sizes, sourceUrl } = image
+      const { frame: imageFrame, size: imageSize, sizes, sourceUrl } = image
+      const size = globalSize || imageSize
+      const frame = size ? undefined : globalFrame || imageFrame
 
       return (
         <Image
           key={i}
           src={sizes.large.url}
           sourceUrl={sourceUrl}
+          size={size}
           frame={frame}
           ImageSource={ImageSource}
           onClick={onImageClick && ((e) => onImageClick(e, image))}
