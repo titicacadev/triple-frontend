61715627de618a2deaddade53cad69550c7c4759
Kim Kyudeok <polysiya@gmail.com>
2018-10-17 18:24:52 +0900

    Merge pull request #153 from titicacadev/footer
    Fix Footer texts and styles

--- packages/triple-design-system/src/elements/footer.js
+++ packages/triple-design-system/src/elements/footer.js
@@ -22,6 +22,7 @@ const LinksContainer = styled.div`
     font-style: normal;
     line-height: 2;
     color: #3a3a3a;
+    text-decoration: none;
   }
 
   margin: 10px 0;
@@ -33,12 +34,15 @@ const Footer = () => (
       minWidth={375}
       maxWidth={600}
       centered
-      padding={{ top: 50, left: 30, right: 30, bottom: 30 }}
+      padding={{ top: 50, left: 30, right: 30, bottom: 50 }}
     >
-      주식회사 트리플 ｜ 대표 김연정, 최휘영 사업자 등록번호 581-87-00266
+      주식회사 트리플 ｜ 대표 김연정, 최휘영
       <br />
-      통신판매업 신고번호 2017-성남분당-0275 경기도 성남시 분당구 판교역로
-      14번길 16, 3층
+      사업자 등록번호 581-87-00266
+      <br />
+      통신판매업 신고번호 2017-성남분당-0275
+      <br />
+      경기도 성남시 분당구 판교역로 14번길 16, 3층
       <br />
       호텔예약문의 02-2222-6625 ｜ 투어/티켓문의 1588-2539
       <br />
@@ -54,7 +58,7 @@ const Footer = () => (
       </LinksContainer>
       (주)트리플은 통신판매중개자로서 통신판매의 당사자가 아니며
       <br />
-      상품 거래정보 및 거래등에 대해 책임을 지지 않습니다.
+      상품 거래정보 및 거래 등에 대해 책임을 지지 않습니다.
     </Container>
   </FooterFrame>
 )
