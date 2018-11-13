59b568a5059fd87c2142bbd6135a22634a100d10
Kim Kyudeok <polysiya@gmail.com>
2018-11-13 14:23:09 +0900

    Merge pull request #205 from titicacadev/action-sheet-button
    Add ActionSheet.Button

--- packages/triple-design-system/src/elements/action-sheet.js
+++ packages/triple-design-system/src/elements/action-sheet.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Children } from 'react'
 import styled from 'styled-components'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
@@ -57,28 +57,75 @@ const Sheet = styled.div`
   border-radius: 12px 12px 0 0;
   background-color: #fff;
   box-sizing: border-box;
-  padding: 30px 30px 22px 30px;
+  padding: 30px 25px 13px 25px;
   margin: 0;
   font-family: sans-serif;
 `
 
 const Title = styled.div`
   height: 16px;
-  line-height: 16px;
   font-size: 13px;
   font-weight: bold;
   color: rgba(73, 73, 73, 0.7);
-  margin: 0 0 10px 0;
+  margin: 0 0 10px 2px;
 `
 
-const ActionItem = styled.a`
-  display: block;
+const ActionItemContainer = styled.div`
+  width: 100%;
+
+  &:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+`
+
+const ItemText = styled.div`
+  display: inline-block;
+  width: ${({ width }) => width || '100%'};
   height: 54px;
   line-height: 54px;
   font-size: 16px;
+  font-weight: 500;
   color: #3a3a3a;
-  margin: 0;
+  font-family: sans-serif;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
 `
+
+const ItemButton = styled.a`
+  float: right;
+  height: 30px;
+  line-height: 30px;
+  margin-top: 12px;
+  padding: 0 17px;
+  text-align: center;
+  border-radius: 15px;
+  background-color: #fafafa;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  font-family: sans-serif;
+  color: #3a3a3a;
+`
+
+function ActionItem({ textWidth, children }) {
+  return (
+    <ActionItemContainer>
+      {Children.toArray(children).map(
+        (child, i) =>
+          typeof child === 'string' ? (
+            <ItemText key={i} width={textWidth}>
+              {child}
+            </ItemText>
+          ) : (
+            child
+          ),
+      )}
+    </ActionItemContainer>
+  )
+}
 
 export default function ActionSheet({ open, onClose, children }) {
   return (
@@ -104,3 +151,4 @@ function silenceEvent(e) {
 
 ActionSheet.Title = Title
 ActionSheet.Item = ActionItem
+ActionSheet.Button = ItemButton
