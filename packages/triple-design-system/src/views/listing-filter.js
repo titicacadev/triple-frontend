c4f707ed15c9e114103bd7c478d19899fab533e2
Inbeom Hwang <hwanginbeom@gmail.com>
2019-01-23 22:49:01 +0900

    Implement ListingFilter component

---
+++ packages/triple-design-system/src/views/listing-filter.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FilterEntryBase = styled.div`
+  display: inline-block;
+  font-family: sans-serif;
+  font-size: 13px;
+  border: 1px solid
+    ${({ active }) => (active ? '#368fff' : 'rgba(58, 58, 58, 0.3)')};
+  color: ${({ active }) => (active ? '#368fff' : 'rgba(58, 58, 58, 0.3)')};
+  background-repeat: no-repeat;
+  border-radius: 2px;
+  box-sizing: border-box;
+  margin-right: 6px;
+`
+
+const ACTIVE_EXPANDER_ICON_URL =
+  'https://assets.triple-dev.titicaca-corp.com/images/ico-category-select-on.svg'
+const INACTIVE_EXPANDER_ICON_URL =
+  'https://assets.triple-dev.titicaca-corp.com/images/ico-category-select.svg'
+
+const ExpandingFilterEntry = styled(FilterEntryBase)`
+  padding: 8px 24px 8px 11px;
+  background-image: ${({ active }) =>
+    active
+      ? `url(${ACTIVE_EXPANDER_ICON_URL})`
+      : `url(${INACTIVE_EXPANDER_ICON_URL})`};
+  background-size: 10px 24px;
+  background-position: top 4px right 10px;
+`
+
+const RegularFilterEntry = styled(FilterEntryBase)`
+  padding: 8px 9px 8px 34px;
+  background-size: 24px 24px;
+  background-position: 5px 8px;
+`
+
+export const ListingFilter = styled.div`
+  white-space: nowrap;
+  overflow-x: scroll;
+  padding: 0 20px;
+
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+function FilterEntry({
+  active,
+  expanding,
+  activeIconImage,
+  inactiveIconImage,
+  ...props
+}) {
+  if (expanding) {
+    return <ExpandingFilterEntry active={active} {...props} />
+  }
+
+  return (
+    <RegularFilterEntry
+      active={active}
+      iconImage={active ? activeIconImage : inactiveIconImage}
+      {...props}
+    />
+  )
+}
+
+ListingFilter.FilterEntry = FilterEntry
