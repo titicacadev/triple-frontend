2cab95567f2060f9a4fc6a13cbfdc7a71788778c
Inbeom Hwang <hwanginbeom@gmail.com>
2019-01-24 13:39:30 +0900

    Fix image integration test page

--- packages/triple-design-system/test/src/elements/image/App.js
+++ packages/triple-design-system/test/src/elements/image/App.js
@@ -4,8 +4,14 @@ import { Image } from '@titicaca/triple-design-system'
 export default class App extends PureComponent {
   render() {
     return (
-      <div className="fixed-dimension">
-        <Image width="200" height="100" />
+      <div>
+        <div className="fixed-dimension">
+          <Image width="200" height="100" />
+        </div>
+
+        <div className="placeholder">
+          <Image placeholder width="200" height="100" />
+        </div>
       </div>
     )
   }
