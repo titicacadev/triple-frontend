c871f6bb82c1bab983e57b6d2db178aeed4cad3a
Inbeom Hwang <hwanginbeom@gmail.com>
2019-01-25 10:49:30 +0900

    Fix utilities.js

---
+++ packages/triple-design-system/src/utilities.js
@@ -0,0 +1,23 @@
+export function deriveCurrentStateAndCount({
+  initialState,
+  initialCount,
+  currentState,
+}) {
+  if (typeof initialState !== 'boolean' || typeof currentState !== 'boolean') {
+    /* At least one of the status are unknown: Reduces to a bitwise OR operation */
+    return {
+      state: !!initialState || !!currentState,
+      count: Number(initialCount || 0),
+    }
+  }
+
+  return {
+    state: currentState,
+    count:
+      initialState === currentState
+        ? initialCount
+        : currentState
+          ? Number(initialCount || 0) + 1
+          : Number(initialCount || 0) - 1,
+  }
+}
