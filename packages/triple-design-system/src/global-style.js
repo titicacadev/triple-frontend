daec09c233726aac450dd9043f7a236e2f7c4bf3
Kyudeok <polysiya@gmail.com>
2019-01-30 15:18:49 +0900

    Add width: 100% to global style

--- packages/triple-design-system/src/global-style.js
+++ packages/triple-design-system/src/global-style.js
@@ -3,6 +3,7 @@ import { injectGlobal } from 'styled-components'
 injectGlobal`
   .scroll-disabled {
     overflow: hidden !important;
+    width: 100%; 
     height: 100%; 
     position: fixed;
   }
