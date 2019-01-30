a6d5ecd4b62d5496f83b85befcd5fa7ce42f99c7
Kyudeok <polysiya@gmail.com>
2019-01-30 16:15:06 +0900

    Remove unneeded global style

--- packages/triple-design-system/src/global-style.js
+++ packages/triple-design-system/src/global-style.js
@@ -3,8 +3,5 @@ import { injectGlobal } from 'styled-components'
 injectGlobal`
   .scroll-disabled {
     overflow: hidden !important;
-    width: 100%; 
-    height: 100%; 
-    position: fixed;
   }
 `
