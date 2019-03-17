cde102d0a9190a663bfef869b4bc85b14abcd593
Inbeom Hwang <hwanginbeom@gmail.com>
2019-03-17 10:39:09 +0900

    Add PublicHeader stories

---
+++ packages/triple-design-system/docs/stories/public-header.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import { PublicHeader } from '@titicaca/triple-design-system'
+
+storiesOf('PublicHeader', module).add('일반', () => <PublicHeader />)
