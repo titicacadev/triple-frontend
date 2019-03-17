278e5edda26880747d9b8d35089c2ba8e91fcffc
Inbeom Hwang <hwanginbeom@gmail.com>
2019-03-17 10:38:48 +0900

    Add footer stories

---
+++ packages/triple-design-system/docs/stories/footer.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import { Footer } from '@titicaca/triple-design-system'
+
+storiesOf('Footer', module).add('일반', () => <Footer />)
