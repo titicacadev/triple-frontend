fcd2dc9676ee156b266a5deba69b8ac6e550865f
Inbeom Hwang <hwanginbeom@gmail.com>
2019-03-17 10:38:41 +0900

    Add DayPicker stories

---
+++ packages/triple-design-system/docs/stories/day-picker.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import DayPicker from '@titicaca/triple-design-system/elements/day-picker'
+
+storiesOf('DayPicker', module)
+  .add('일반', () => <DayPicker />)
+  .add('선택됨', () => (
+    <DayPicker from="2019-03-15" to="2019-06-15" date="2019-03-20" />
+  ))
+  .add('선택불가', () => (
+    <DayPicker
+      from="2019-03-15"
+      to="2019-06-15"
+      blockedDates={['2019-03-20']}
+    />
+  ))
