c9ed9aaa0e4a0615833488304950bd84a10cac10
Kyudeok <polysiya@gmail.com>
2019-03-18 18:23:06 +0900

    Add Confirm to storyboard

--- packages/triple-design-system/docs/stories/modal.stories.js
+++ packages/triple-design-system/docs/stories/modal.stories.js
@@ -2,22 +2,24 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 
-import { Text, Modal } from '@titicaca/triple-design-system'
+import { Text, Modal, Confirm } from '@titicaca/triple-design-system'
 
-storiesOf('Modal', module).add('일반', () => (
-  <Modal open>
-    <Text
-      center
-      size="large"
-      lineHeight={1.38}
-      color="gray"
-      padding={{ top: 40, bottom: 40, left: 30, right: 30 }}
-    >
-      안녕
-    </Text>
-    <Modal.Actions>
-      <Modal.Action color="blue">확인</Modal.Action>
-      <Modal.Action>취소</Modal.Action>
-    </Modal.Actions>
-  </Modal>
-))
+storiesOf('Modal', module)
+  .add('일반', () => (
+    <Modal open>
+      <Text
+        center
+        size="large"
+        lineHeight={1.38}
+        color="gray"
+        padding={{ top: 40, bottom: 40, left: 30, right: 30 }}
+      >
+        안녕
+      </Text>
+    </Modal>
+  ))
+  .add('컨펌', () => (
+    <Confirm open>
+      삭제하겠습니까? 삭제하면 적립된 리뷰 포인트도 함께 사라집니다.
+    </Confirm>
+  ))
