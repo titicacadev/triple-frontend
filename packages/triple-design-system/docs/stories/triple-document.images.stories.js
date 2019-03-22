2a927fff1d83d198994f637e560af5b9f90a9e8b
Unknown <torres@triple-corp.com>
2019-03-22 18:56:05 +0900

    Image 받아오는 과정 간소화

--- packages/triple-design-system/docs/stories/triple-document.images.stories.js
+++ packages/triple-design-system/docs/stories/triple-document.images.stories.js
@@ -10,33 +10,20 @@ const { images: Images } = ELEMENTS
 storiesOf('TripleDocument.이미지', module)
   .addDecorator(withKnobs)
   .add('1개', () => {
-    const {
-      id,
-      frame,
-      sizes,
-      title,
-      width,
-      height,
-      description,
-      sourceUrl,
-    } = IMAGES[0]
+    const [{ ...image }] = IMAGES
     return (
       <Images
         value={{
           images: [
             {
-              id,
               frame: select(
                 '크기',
                 ['mini', 'small', 'medium', 'large', 'big', 'huge'],
-                frame,
+                'small',
               ),
-              sizes,
-              title: text('캡션', title),
-              width,
-              height,
-              description,
-              sourceUrl: text('출처', sourceUrl),
+              title: text('캡션', 'TripleDocument 샘플 1'),
+              sourceUrl: text('출처', 'https://triple.guide'),
+              ...image,
             },
           ],
         }}
@@ -46,63 +33,24 @@ storiesOf('TripleDocument.이미지', module)
   .add('2개', () => (
     <Images
       value={{
-        images: IMAGES.map(
-          ({ id, frame, sizes, width, height, description, sourceUrl }) => ({
-            id,
-            frame,
-            sizes,
-            title: '',
-            width,
-            height,
-            description,
-            sourceUrl,
-          }),
-        ),
+        images: IMAGES.map((value) => ({
+          ...value,
+          title: '',
+        })),
       }}
     />
   ))
   .add('2개, 캡션', () => (
     <Images
       value={{
-        images: IMAGES.map(
-          ({
-            id,
-            frame,
-            sizes,
-            title,
-            width,
-            height,
-            description,
-            sourceUrl,
-          }) => ({
-            id,
-            frame,
-            sizes,
-            title,
-            width,
-            height,
-            description,
-            sourceUrl,
-          }),
-        ),
+        images: IMAGES.map((value) => value),
       }}
     />
   ))
   .add('2개, 블록', () => (
     <Images
       value={{
-        images: IMAGES.map(
-          ({ id, frame, sizes, width, height, description, sourceUrl }) => ({
-            id,
-            frame,
-            sizes,
-            title: '',
-            width,
-            height,
-            description,
-            sourceUrl,
-          }),
-        ),
+        images: IMAGES.map((value) => ({ ...value, title: '' })),
         display: 'block',
       }}
     />
@@ -110,27 +58,7 @@ storiesOf('TripleDocument.이미지', module)
   .add('2개, 블록, 캡션', () => (
     <Images
       value={{
-        images: IMAGES.map(
-          ({
-            id,
-            frame,
-            sizes,
-            title,
-            width,
-            height,
-            description,
-            sourceUrl,
-          }) => ({
-            id,
-            frame: frame,
-            sizes,
-            title,
-            width,
-            height,
-            description,
-            sourceUrl,
-          }),
-        ),
+        images: IMAGES.map((value) => value),
         display: 'block',
       }}
     />
