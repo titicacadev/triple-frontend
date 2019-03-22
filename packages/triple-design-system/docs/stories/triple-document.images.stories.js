4030f6589e8a3bd8366394d149765a919c3b728c
Unknown <torres@triple-corp.com>
2019-03-22 16:58:28 +0900

    Images Stories 구조 변경

--- packages/triple-design-system/docs/stories/triple-document.images.stories.js
+++ packages/triple-design-system/docs/stories/triple-document.images.stories.js
@@ -9,42 +9,129 @@ const { images: Images } = ELEMENTS
 
 storiesOf('TripleDocument.이미지', module)
   .addDecorator(withKnobs)
-  .add('1개', () => (
+  .add('1개', () => {
+    const {
+      id,
+      frame,
+      sizes,
+      title,
+      width,
+      height,
+      description,
+      sourceUrl,
+    } = IMAGES[0]
+    return (
+      <Images
+        value={{
+          images: [
+            {
+              id,
+              frame: select(
+                '크기',
+                ['mini', 'small', 'medium', 'large', 'big', 'huge'],
+                frame,
+              ),
+              sizes,
+              title: text('캡션', title),
+              width,
+              height,
+              description,
+              sourceUrl: text('출처', sourceUrl),
+            },
+          ],
+        }}
+      />
+    )
+  })
+  .add('2개', () => (
     <Images
       value={{
-        images: [
-          {
-            id: '07f5ed9c-1102-4ec0-b07c-7b1b098311b2',
-            frame: select(
-              '크기',
-              ['mini', 'small', 'medium', 'large', 'big', 'huge'],
-              'small',
-            ),
-            sizes: {
-              full: {
-                url:
-                  'https://res.cloudinary.com/triple-entry/image/upload/w_2048,h_2048,c_limit,f_auto/07f5ed9c-1102-4ec0-b07c-7b1b098311b2.jpg',
-              },
-              large: {
-                url:
-                  'https://res.cloudinary.com/triple-entry/image/upload/w_1024,h_1024,c_limit,f_auto/07f5ed9c-1102-4ec0-b07c-7b1b098311b2.jpg',
-              },
-              small_square: {
-                url:
-                  'https://res.cloudinary.com/triple-entry/image/upload/w_256,h_256,c_fill,f_auto/07f5ed9c-1102-4ec0-b07c-7b1b098311b2.jpg',
-              },
-            },
-            title: text('캡션', 'TripleDocument 샘플'),
-            width: 1024,
-            height: 1544,
-            description: '',
-            sourceUrl: text('출처', 'https://triple.guide'),
-          },
-        ],
+        images: IMAGES.map(
+          ({ id, frame, sizes, width, height, description, sourceUrl }) => ({
+            id,
+            frame: frame,
+            sizes,
+            title: '',
+            width,
+            height,
+            description,
+            sourceUrl,
+          }),
+        ),
       }}
     />
   ))
-  .add('2개', () => <Images value={IMAGES['two-normal']} />)
-  .add('2개, 캡션', () => <Images value={IMAGES['two-normal-caption']} />)
-  .add('2개, 블록', () => <Images value={IMAGES['two-block']} />)
-  .add('2개, 블록, 캡션', () => <Images value={IMAGES['two-block-caption']} />)
+  .add('2개, 캡션', () => (
+    <Images
+      value={{
+        images: IMAGES.map(
+          ({
+            id,
+            frame,
+            sizes,
+            title,
+            width,
+            height,
+            description,
+            sourceUrl,
+          }) => ({
+            id,
+            frame: frame,
+            sizes,
+            title,
+            width,
+            height,
+            description,
+            sourceUrl,
+          }),
+        ),
+      }}
+    />
+  ))
+  .add('2개, 블록', () => (
+    <Images
+      value={{
+        images: IMAGES.map(
+          ({ id, frame, sizes, width, height, description, sourceUrl }) => ({
+            id,
+            frame: frame,
+            sizes,
+            title: '',
+            width,
+            height,
+            description,
+            sourceUrl,
+          }),
+        ),
+        display: 'block',
+      }}
+    />
+  ))
+  .add('2개, 블록, 캡션', () => (
+    <Images
+      value={{
+        images: IMAGES.map(
+          ({
+            id,
+            frame,
+            sizes,
+            title,
+            width,
+            height,
+            description,
+            sourceUrl,
+          }) => ({
+            id,
+            frame: frame,
+            sizes,
+            title,
+            width,
+            height,
+            description,
+            sourceUrl,
+          }),
+        ),
+        display: 'block',
+      }}
+    />
+  ))
