2d73dd2a061328820eebd12b76fa7f9c13daf7fc
고석진 <appear.ko@gmail.com>
2019-03-24 17:35:05 +0900

    table component 추가

--- packages/triple-design-system/src/elements/table.js
+++ packages/triple-design-system/src/elements/table.js
@@ -1,18 +1,144 @@
-import React, { PureComponent } from 'react'
+import React, { Children } from 'react'
 import styled, { css } from 'styled-components'
+import Text from './text'
 
-export default class Table extends PureComponent {
-  render() {
-    const {
-      props: { row, coll },
-    } = this
+const COLORS_SET = {
+  header: '234, 234, 234',
+  body: '245, 245, 245',
+}
 
-    if (row) {
-      return <div> Hello </div>
-    }
+const Container = styled.div`
+  overflow: hidden;
+  box-sizing: border-box;
 
-    if (coll) {
-      return <div> Hello </div>
-    }
-  }
+  ${({ borderRadius }) =>
+    borderRadius &&
+    css`
+      border-radius: ${borderRadius}px;
+    `};
+
+  ${({ borderLine }) =>
+    borderLine &&
+    css`
+      & > div:not(:last-child) {
+        border-bottom: 1px solid rgb(${COLORS_SET.header});
+      }
+    `};
+`
+
+const Row = styled.div`
+  width: 100%;
+  display: table;
+
+  ${({ borderRadius }) =>
+    borderRadius &&
+    css`
+      border-radius: ${borderRadius}px;
+      overflow: hidden;
+    `};
+
+  ${({ verticalGap, children }) =>
+    verticalGap
+      ? css`
+          &:not(:last-child) {
+            margin-bottom: ${verticalGap}px;
+          }
+        `
+      : css`
+          & > div {
+            width: ${100 / Children.count(children)}%;
+          }
+        `};
+`
+
+const Column = styled.div`
+  width: ${({ width }) => width || '100'}%;
+  display: table-cell;
+  vertical-align: middle;
+  text-align: ${({ textAlign }) => textAlign || 'center'};
+
+  ${({ color }) =>
+    color &&
+    css`
+      background-color: rgb(${COLORS_SET[color || 'body']});
+    `};
+
+  ${({ padding }) =>
+    padding &&
+    css`
+      padding-top: ${padding.top || 0}px;
+      padding-bottom: ${padding.bottom || 0}px;
+      padding-left: ${padding.left || 0}px;
+      padding-right: ${padding.right || 0}px;
+    `};
+`
+
+function HorizontalTable({ head, body }) {
+  return (
+    <Container borderLine borderRadius={6}>
+      <Row>
+        {head.map((text, idx) => (
+          <Column key={idx} color="header" padding={{ top: 12, bottom: 12 }}>
+            <Text bold size="small">
+              {text}
+            </Text>
+          </Column>
+        ))}
+      </Row>
+
+      {body.map((texts, idx) => (
+        <Row key={idx}>
+          {texts.map((text, idx) => (
+            <Column key={idx} color="body" padding={{ top: 12, bottom: 12 }}>
+              <Text size="small">{text}</Text>
+            </Column>
+          ))}
+        </Row>
+      ))}
+    </Container>
+  )
+}
+
+function VerticalTable({ head, body }) {
+  return (
+    <Container>
+      {head.map((text, idx) => (
+        <Row key={idx} verticalGap={10} borderRadius={6}>
+          <Column
+            width="20"
+            color="header"
+            padding={{
+              top: 13,
+              bottom: 13,
+              left: 15,
+              right: 15,
+            }}
+            textAlign="left"
+          >
+            <Text size="small" bold>
+              {text}
+            </Text>
+          </Column>
+          <Column
+            width="80"
+            color="body"
+            padding={{
+              top: 13,
+              bottom: 13,
+              left: 15,
+              right: 15,
+            }}
+            textAlign="left"
+          >
+            <Text size="small">{body[idx]}</Text>
+          </Column>
+        </Row>
+      ))}
+    </Container>
+  )
+}
+
+export default function Table({ type, ...props }) {
+  const Container = type === 'vertical' ? VerticalTable : HorizontalTable
+  return <Container {...props} />
 }
--- packages/triple-design-system/test/src/elements/table/App.js
+++ packages/triple-design-system/test/src/elements/table/App.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react'
 import { Table } from '@titicaca/triple-design-system'
 
-const sample = {
+const row_sample = {
+  type: 'horizontal',
   head: ['목적지', '요금/소요시간', '운행간격'],
   body: [
     ['난바 OCAT', '1,050엔/45분', '30분'],
@@ -12,8 +13,29 @@ const sample = {
   ],
 }
 
+const col_sample = {
+  type: 'vertical',
+  head: ['루트', '요금', '소요\n시간', '운행\n간격'],
+  body: [
+    ['간사이 공항 → JR 교토역'],
+    ['하루카 편도 3,370엔 이코카 & 하루카 편도 3,600엔'],
+    ['1시간 15분'],
+    ['시간당 급행 2대 / 일반 3대'],
+  ],
+}
+
 export default class App extends PureComponent {
   render() {
-    return <div>Hello</div>
+    return (
+      <div>
+        <div>
+          <Table {...row_sample} />
+        </div>
+        <br />
+        <div>
+          <Table {...col_sample} />
+        </div>
+      </div>
+    )
   }
 }
