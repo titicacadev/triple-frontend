86e4336da3be6b5448b279bc3a30756d7a083a2c
Unknown <torres@triple-corp.com>
2019-03-29 17:08:23 +0900

    Modify Compact Icon Butto Sample

--- packages/triple-design-system/docs/stories/button.stories.js
+++ packages/triple-design-system/docs/stories/button.stories.js
@@ -45,7 +45,10 @@ storiesOf('Button', module)
       onClick={action('clicked')}
       color={select('버튼 색', ['gray', 'blue'])}
     >
-      <Button.Icon src="https://assets.triple-dev.titicaca-corp.com/images/save@4x.png" />
+      <Button.Icon
+        src="https://assets.triple-dev.titicaca-corp.com/images/save@4x.png"
+        size={select('아이콘 크기', ['mobile'], 'mobile')}
+      />
       {text('버튼 레이블', '저장하기')}
     </Button>
   ))
