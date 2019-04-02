52a74f0e84fb571caf439f031c2f46a71c3612fe
Inbeom Hwang <hwanginbeom@gmail.com>
2019-04-02 17:41:06 +0900

    Add Tabs component

---
+++ packages/triple-design-system/src/elements/tabs.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TabsContainer = styled.div`
+  background-color: #efefef;
+  border-radius: 4px;
+  padding: 2px;
+  white-space: nowrap;
+`
+
+const Tab = styled.div`
+  font-family: sans-serif;
+  display: inline-block;
+  box-sizing: border-box;
+  width: ${({ widthPercent }) => widthPercent}%;
+  height: 36px;
+  line-height: 36px;
+  text-align: center;
+  color: ${({ active }) => (active ? '#3a3a3a' : 'rgba(46, 46, 46, 0.3)')};
+  background-color: ${({ active }) => (active ? '#ffffff' : 'transparent')};
+  border-radius: 2px;
+  font-size: 14px;
+  font-weight: bold;
+`
+
+export default function Tabs({ value: currentValue, options, onChange }) {
+  return (
+    <TabsContainer>
+      {options.map(({ label, value }, i) => (
+        <Tab
+          active={currentValue === value}
+          widthPercent={options.length > 0 ? 100 / options.length : 100}
+          key={i}
+          onClick={(e) => onChange(e, value)}
+        >
+          {label}
+        </Tab>
+      ))}
+    </TabsContainer>
+  )
+}
