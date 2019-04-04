4c3e01c9cd84e57e08189d870f366e12debeaf97
고석진 <appear.ko@gmail.com>
2019-04-04 10:55:53 +0900

    table stories 추가

---
+++ packages/triple-design-system/docs/stories/table.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { Table } from '@titicaca/triple-design-system'
+import SAMPLE from './table.sample.json'
+
+storiesOf('Table', module)
+  .add('가로 테이블', () => <Table {...SAMPLE[0]} />)
+  .add('세로 테이블', () => <Table {...SAMPLE[1]} />)
