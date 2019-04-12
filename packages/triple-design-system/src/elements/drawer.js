4bda7f097259736c9e444901286e2bb0a58d9cb4
Inbeom Hwang <hwanginbeom@gmail.com>
2019-04-12 10:49:26 +0900

    Add Drawer element

---
+++ packages/triple-design-system/src/elements/drawer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+const DrawerContainer = styled.div`
+  z-index: 1;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  overflow: hidden;
+  background: #fff;
+  transform: translateY(100%);
+  transition: all 300ms ease-in-out;
+
+  ${({ active }) =>
+    active &&
+    css`
+      transform: translateY(0%);
+      transition: all 300ms ease-in-out;
+    `};
+`
+
+export default function Drawer({ active, children }) {
+  return <DrawerContainer active={active}>{children}</DrawerContainer>
+}
