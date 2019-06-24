5de972d8ef48f40624d6f5ef2334a4248d90d180
Kyudeok <polysiya@gmail.com>
2019-06-24 18:27:36 +0900

    Convert author.js to author.tsx

--- packages/triple-design-system/src/views/author.tsx
+++ packages/triple-design-system/src/views/author.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import * as React from 'react'
 
 import Container from '../elements/container'
 import Text from '../elements/text'
 import Image from '../elements/image'
 
-export function Author({ source: { name, bio, image }, bioOverride, onClick }) {
+export function Author({
+  source: { name, bio, image },
+  bioOverride,
+  onClick,
+}: {
+  source: { name: string; bio?: string; image?: any }
+  bioOverride?: string
+  onClick?: (e?: React.SyntheticEvent) => any
+}) {
   return (
     <Container centered textAlign="center" onClick={onClick}>
       {image && <Image size="medium" circular src={image.sizes.large.url} />}
