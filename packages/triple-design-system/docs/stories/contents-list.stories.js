8e965a3b06d18952d8f81293fc7fff215e0e229b
Unknown <torres@triple-corp.com>
2019-07-18 18:50:12 +0900

    Storybook에도 변경사항 반영

--- packages/triple-design-system/docs/stories/contents-list.stories.js
+++ packages/triple-design-system/docs/stories/contents-list.stories.js
@@ -13,30 +13,30 @@ storiesOf('RelatedContent', module).add('관련 컨텐츠', () => (
       다른 컨텐츠 더보기
     </Text>
     <RelatedContentsList margin={{ top: 30, right: -15 }}>
-      <RelatedContent img="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
+      <RelatedContent backgroundImageUrl="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
         한줄테스트
       </RelatedContent>
-      <RelatedContent img="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
+      <RelatedContent backgroundImageUrl="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
         두줄테스트
         <br />
         두줄테스트
       </RelatedContent>
-      <RelatedContent img="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
+      <RelatedContent backgroundImageUrl="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
         윗줄넘어갔어요오오오오윗줄넘어갔어요오오오오윗줄넘어갔어요오오오오윗줄넘어갔어요오오오오
         <br />
         윗줄테스트
       </RelatedContent>
-      <RelatedContent img="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
+      <RelatedContent backgroundImageUrl="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
         아랫줄테스트
         <br />
         아랫줄넘어갔어요오오오오아랫줄넘어갔어요오오오오아랫줄넘어갔어요오오오오아랫줄넘어갔어요오오오오
       </RelatedContent>
-      <RelatedContent img="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
+      <RelatedContent backgroundImageUrl="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
         윗줄넘어갔어요오오오오윗줄넘어갔어요오오오오윗줄넘어갔어요오오오오윗줄넘어갔어요오오오오
         <br />
         아랫줄넘어갔어요오오오오아랫줄넘어갔어요오오오오아랫줄넘어갔어요오오오오아랫줄넘어갔어요오오오오
       </RelatedContent>
-      <RelatedContent img="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
+      <RelatedContent backgroundImageUrl="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/4984852d-61df-4fab-95f9-da24d257a829.jpeg">
         한줄인데넘어갔어요오오오오오오오한줄인데넘어갔어요오오오오오오오한줄인데넘어갔어요오오오오오오오
       </RelatedContent>
     </RelatedContentsList>
