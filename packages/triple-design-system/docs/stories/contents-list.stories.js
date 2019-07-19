8529308b302ae520badfd180b546d313b7d9a0bf
Unknown <torres@triple-corp.com>
2019-07-19 13:58:25 +0900

    Storybook 이름 수정

--- packages/triple-design-system/docs/stories/contents-list.stories.js
+++ packages/triple-design-system/docs/stories/contents-list.stories.js
@@ -4,7 +4,7 @@ import { Container, Text } from '@titicaca/triple-design-system'
 import { contents } from './contents-list.sample.json'
 import RecommendedContentsList from '../../src/views/recommended-contents-list'
 
-storiesOf('RelatedContent', module).add('관련 컨텐츠', () => (
+storiesOf('RecommendedContentsList', module).add('추천 컨텐츠', () => (
   <Container margin={{ top: 50, left: 30, right: 30 }}>
     <Text size="big" bold>
       다른 컨텐츠 더보기
