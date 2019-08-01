ae05bb0e65bd26a97fba8b6cd89f48619be46164
Unknown <torres@triple-corp.com>
2019-08-01 18:55:52 +0900

    Add Recommended Content with fixed ratio

--- packages/triple-design-system/src/views/recommended-contents.tsx
+++ packages/triple-design-system/src/views/recommended-contents.tsx
@@ -21,13 +21,11 @@ const RecommendedContent = styled.li<{
 }>`
   display: inline-block;
   vertical-align: top;
-  width: 120px;
+  width: calc(50% - 45px);
   height: 130px;
   padding: 20px 15px 0 15px;
   margin: 0 15px 15px 0;
   border-radius: 6px;
-
-  cursor: pointer;
 
   ${({ backgroundImageUrl }) =>
     css`
@@ -38,23 +36,64 @@ const RecommendedContent = styled.li<{
   background-size: cover;
   background-position: center;
 
-  @media (min-width: 760px) {
-    width: calc(50% - 45px);
-    height: 130px;
+  cursor: pointer;
 
-    &:before {
-      content: '';
-      width: 20px;
-      height: 20px;
-      background-size: 20px 20px;
-      background-image: url('https://assets.triple.guide/images/ico-arrow@4x.png');
-      float: right;
-      margin-right: -20px;
-      position: relative;
-      right: 20px;
-      top: 45px;
-    }
+  @media(max-width: 759px) {
+    display: none;
   }
+
+  &:before {
+    content: '';
+    width: 20px;
+    height: 20px;
+    background-size: 20px 20px;
+    background-image: url('https://assets.triple.guide/images/ico-arrow@4x.png');
+    float: right;
+    margin-right: -20px;
+    position: relative;
+    right: 20px;
+    top: 45px;
+  }
+`
+
+const RecommendedContentWithFixedRatio = styled.li`
+  display: inline-block;
+  vertical-align: top;
+  width: calc(50% - 15px);
+  height: 0;
+  padding-top: calc(50% - 15px);
+  margin: 0 15px 15px 0;
+  border-radius: 6px;
+  position: relative;
+
+  cursor: pointer;
+
+  overflow: hidden;
+
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  @media(min-width: 760px) {
+    display: none;
+  }
+
+  & > * {
+    position: absolute;
+    top: 0;
+  }
+`
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+
+const ImageColorOverlay = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
 `
 
 export function RecommendedContents({
@@ -73,7 +112,20 @@ export function RecommendedContents({
   ) => any
 }) {
   return (
+    
     <RecommendedContentsContainer margin={margin}>
+      {contents.map((content, index) => (
+        <RecommendedContentWithFixedRatio
+          key={index}
+          onClick={onContentClick && ((e) => onContentClick(e, content))}
+        >
+          <Image src={content.backgroundImageUrl} />
+          <ImageColorOverlay />
+          <Text lineHeight="20px" color="white" bold maxLines={2} padding={{top: 20, left: 15, right: 15}}>
+            {content.title}
+          </Text>
+        </RecommendedContentWithFixedRatio>
+      ))}
       {contents.map((content, index) => (
         <RecommendedContent
           key={index}
