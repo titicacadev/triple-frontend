225b9e0a666bd3cbd05a8e529bcb1f0f4526242b
Inbeom Hwang <hwanginbeom@gmail.com>
2019-08-09 21:40:03 +0200

    Apply prettier

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,28 @@
-{
-  "presets": [
+module.exports = {
+  presets: [
     [
-      "@babel/env",
+      '@babel/env',
       {
-        "targets": {
-          "ie": "11",
-          "ios": "10"
-        }
-      }
+        targets: {
+          ie: '11',
+          ios: '10',
+        },
+      },
     ],
-    "@babel/react",
-    "@babel/typescript"
+    '@babel/react',
+    '@babel/typescript',
   ],
-  "plugins": [
+  plugins: [
     [
-      "styled-components",
+      'styled-components',
       {
-        "ssr": true,
-        "displayName": true,
-        "preprocess": false
-      }
+        ssr: true,
+        displayName: true,
+        preprocess: false,
+      },
     ],
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-transform-runtime"
-  ]
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime',
+  ],
 }
