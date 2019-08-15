177a1fed3afa873e5da45f40ba19f8c3163a88db
Inbeom Hwang <hwanginbeom@gmail.com>
2019-08-15 21:23:30 +0200

    Separate triple-product package

--- packages/triple-product/src/index.tsx
+++ packages/triple-product/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import { deriveCurrentStateAndCount } from '../utilities'
-import { ExtendedResourceListElement } from '../elements/resource-list-element'
+import { ExtendedResourceListElement } from '@titicaca/triple-design-system'
+import { deriveCurrentStateAndCount } from '@titicaca/triple-view-utilities'
 
 export class ProductListElement extends React.PureComponent<{
   product?: { id; title; image; subtitle; basePrice; salePrice }
