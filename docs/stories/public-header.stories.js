35acabe5d0e1567d8aba39a81e0049ac6c8b7b8a
Inbeom Hwang <hwanginbeom@gmail.com>
2019-09-05 10:37:40 +0900

    Fix docs

--- docs/stories/public-header.stories.js
+++ docs/stories/public-header.stories.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 
-import { PublicHeader } from '@titicaca/triple-design-system'
+import PublicHeader from '@titicaca/public-header'
 
 storiesOf('PublicHeader', module).add('일반', () => <PublicHeader />)
