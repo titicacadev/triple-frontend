824b8d24c84796d3d05444ce195e02a57319f4dc
appear <appear.ko@gmail.com>
2019-09-05 14:23:19 +0900

    pricing stories 변경

--- docs/stories/pricing.stories.js
+++ docs/stories/pricing.stories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean } from '@storybook/addon-knobs'
 
-import { Pricing } from '@titicaca/triple-design-system'
+import { Pricing, Text } from '@titicaca/triple-design-system'
 
 storiesOf('Pricing', module)
   .addDecorator(withKnobs)
@@ -17,6 +17,21 @@ storiesOf('Pricing', module)
       basePrice={30000}
       salePrice={25000}
       label="트리플 클럽가"
+      buttonText="객실예약"
+      description="10,000원 쿠폰 할인 가능"
+    />
+  ))
+  .add('Fixed (Custom Label) ', () => (
+    <Pricing
+      fixed
+      active={boolean('열림', true)}
+      basePrice={30000}
+      salePrice={25000}
+      label={
+        <Text size="mini" alpha={0.8}>
+          트리플 클럽가
+        </Text>
+      }
       buttonText="객실예약"
     />
   ))
