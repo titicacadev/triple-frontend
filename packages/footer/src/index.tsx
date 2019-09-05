274e7e7ce5a753db027d39dfd68ba8970b0ad7ad
Inbeom Hwang <hwanginbeom@gmail.com>
2019-09-05 09:47:02 +0900

    Use good old function instead of arrow function

--- packages/footer/src/index.tsx
+++ packages/footer/src/index.tsx
@@ -27,52 +27,52 @@ const LinksContainer = styled.div`
   margin: 10px 0;
 `
 
-const Footer = () => (
-  <FooterFrame>
-    <Container
-      whiteSpace="nowrap"
-      minWidth={320}
-      maxWidth={600}
-      centered
-      padding={{ top: 50, left: 30, right: 30, bottom: 50 }}
-    >
-      주식회사 트리플 ｜ 대표 김연정, 최휘영
-      <br />
-      사업자 등록번호 581-87-00266
-      <br />
-      통신판매업 신고번호 2017-성남분당-0275
-      <br />
-      경기도 성남시 분당구 판교역로 14번길 16, 3층
-      <br />
-      호텔 예약 및 투어/티켓 문의 1588-2539
-      <br />
-      help@triple-corp.com
-      <LinksContainer>
-        <a href="/pages/tos.html" target="_blank" rel="noopener noreferrer">
-          서비스 이용약관
-        </a>
-        {' | '}
-        <a
-          href="/pages/privacy-policy.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          개인정보 처리방침
-        </a>
-        {' | '}
-        <a
-          href="https://triple-corp.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          회사 소개
-        </a>
-      </LinksContainer>
-      (주)트리플은 통신판매중개자로서 통신판매의 당사자가 아니며
-      <br />
-      상품 거래정보 및 거래 등에 대해 책임을 지지 않습니다.
-    </Container>
-  </FooterFrame>
-)
-
-export default Footer
+export default function Footer() {
+  return (
+    <FooterFrame>
+      <Container
+        whiteSpace="nowrap"
+        minWidth={320}
+        maxWidth={600}
+        centered
+        padding={{ top: 50, left: 30, right: 30, bottom: 50 }}
+      >
+        주식회사 트리플 ｜ 대표 김연정, 최휘영
+        <br />
+        사업자 등록번호 581-87-00266
+        <br />
+        통신판매업 신고번호 2017-성남분당-0275
+        <br />
+        경기도 성남시 분당구 판교역로 14번길 16, 3층
+        <br />
+        호텔 예약 및 투어/티켓 문의 1588-2539
+        <br />
+        help@triple-corp.com
+        <LinksContainer>
+          <a href="/pages/tos.html" target="_blank" rel="noopener noreferrer">
+            서비스 이용약관
+          </a>
+          {' | '}
+          <a
+            href="/pages/privacy-policy.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            개인정보 처리방침
+          </a>
+          {' | '}
+          <a
+            href="https://triple-corp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            회사 소개
+          </a>
+        </LinksContainer>
+        (주)트리플은 통신판매중개자로서 통신판매의 당사자가 아니며
+        <br />
+        상품 거래정보 및 거래 등에 대해 책임을 지지 않습니다.
+      </Container>
+    </FooterFrame>
+  )
+}
