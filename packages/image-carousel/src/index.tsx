88f9fbd3f8a714832cda9d37b91583a05e72e83d
BoxeR <boxer@triple-corp.com>
2019-09-08 15:45:28 +0900

    [#21] :recycle: pageLabelRenderer 를 사용한 페이징 visibility 처리
    - pageLabelRenderer 를 사용자가 직접 props 로 추가
    - index 와 totalCount 를 넘겨받은 함수에서 페이징 렌더링 여부를 판별함
    - PageLabel 컴포넌트를 기본 제공하여 사용자가 이를 쓰거나, 직접 만들도록 지원함

--- packages/image-carousel/src/index.tsx
+++ packages/image-carousel/src/index.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { FlickingOptions, FlickingEvent } from '@egjs/flicking'
 import Carousel, { CarouselProps } from './carousel'
-import {
-  Image,
-  MarginPadding,
-  GlobalSizes,
-} from '@titicaca/triple-design-system'
+import { Image, GlobalSizes } from '@titicaca/triple-design-system'
 
 const PageLabelText = styled.div`
   font-size: 12px;
@@ -21,90 +16,104 @@ const PageLabelContainer = styled.div`
   background-color: rgba(0, 0, 0, 0.2);
 `
 
-interface ImagePagerProps extends Partial<CarouselProps> {
+export interface RendererProps {
+  index: number
+  totalCount: number
+}
+
+interface ImageCarouselProps extends Partial<CarouselProps> {
   size: GlobalSizes
   frame: GlobalSizes
   images: any[]
-  totalPageCount: number
+  ImageSource: string
   onImageClick: (e?: React.SyntheticEvent, image?: any) => void
-  ImageSource: any
-  lastPageOverlayContent: React.ReactNode
-  showMoreComponent: (index: number) => React.ReactNode | React.ReactNode
+  showMoreRenderer: (props: RendererProps) => React.ReactNode | React.ReactNode
+  pageLabelRenderer: (props: RendererProps) => React.ReactNode
 }
 
-export function ImageCarousel({
-  margin,
-  borderRadius,
-  size: globalSize,
-  frame: globalFrame,
-  images,
-  defaultIndex = 0,
-  totalPageCount,
-  onImageClick,
-  onMoveStart,
-  onMove,
-  onMoveEnd,
-  ImageSource,
-  lastPageOverlayContent,
-  showMoreComponent,
-}: Partial<ImagePagerProps>) {
-  return (
-    <Carousel
-      margin={margin}
-      borderRadius={borderRadius}
-      defaultIndex={defaultIndex}
-      onMoveStart={onMoveStart}
-      onMove={onMove}
-      onMoveEnd={onMoveEnd}
-      totalCount={totalPageCount || images.length}
-      pageLabelComponent={({ currentIndex }: any) => (
-        <PageLabel
-          current={currentIndex}
-          total={totalPageCount || images.length}
-        />
-      )}
-      showMoreComponentHandler={(function() {
-        if (typeof showMoreComponent !== 'function') {
-          return function(index?: number) {
-            return showMoreComponent
-          }
-        } else {
-          return showMoreComponent
-        }
-      })()}
-    >
-      {images.map((image, i) => {
-        const { frame: imageFrame, size: imageSize, sizes, sourceUrl } = image
-        const size = globalSize || imageSize
-        const frame = size ? undefined : globalFrame || imageFrame
+export class ImageCarousel extends React.PureComponent<
+  Partial<ImageCarouselProps>
+> {
+  static defaultProps = {
+    pageLabelRenderer: (props) => PageLabel(props),
+  }
 
-        return (
-          <Image
-            key={i}
-            src={sizes.large.url}
-            sourceUrl={sourceUrl}
-            size={size}
-            frame={frame}
-            ImageSource={ImageSource}
-            borderRadius={0}
-            onClick={onImageClick && ((e) => onImageClick(e, image))}
-            overlay={
-              typeof showMoreComponent === 'function'
-                ? showMoreComponent(i)
-                : showMoreComponent
-            }
-            overlayType="dark"
-          />
-        )
-      })}
-    </Carousel>
-  )
+  get carouselProps() {
+    const {
+      margin,
+      borderRadius,
+      defaultIndex,
+      images,
+      onMoveStart,
+      onMove,
+      onMoveEnd,
+      pageLabelRenderer,
+    } = this.props
+
+    return {
+      margin,
+      borderRadius,
+      defaultIndex,
+      onMoveStart,
+      onMove,
+      onMoveEnd,
+      pageLabelRenderer: ({ index }) =>
+        pageLabelRenderer({
+          index,
+          totalCount: images.length,
+        }),
+    }
+  }
+
+  render() {
+    const {
+      size: globalSize,
+      frame: globalFrame,
+      images,
+      onImageClick,
+      ImageSource,
+      showMoreRenderer,
+    } = this.props
+
+    const { carouselProps } = this
+
+    return (
+      <Carousel {...carouselProps}>
+        {images.map((image, i) => {
+          const { frame: imageFrame, size: imageSize, sizes, sourceUrl } = image
+          const size = globalSize || imageSize
+          const frame = size ? undefined : globalFrame || imageFrame
+
+          return (
+            <Image
+              key={i}
+              src={sizes.large.url}
+              sourceUrl={sourceUrl}
+              size={size}
+              frame={frame}
+              ImageSource={ImageSource}
+              borderRadius={0}
+              onClick={onImageClick && ((e) => onImageClick(e, image))}
+              overlay={
+                showMoreRenderer &&
+                showMoreRenderer({
+                  index: i,
+                  totalCount: images.length,
+                })
+              }
+              overlayType="dark"
+            />
+          )
+        })}
+      </Carousel>
+    )
+  }
 }
 
-function PageLabel({ current, total }: { current: number; total: number }) {
+export function PageLabel({ index, totalCount }: RendererProps) {
   return (
     <PageLabelContainer>
-      <PageLabelText>{`${current + 1} / ${total}`}</PageLabelText>
+      <PageLabelText>{`${index + 1} / ${totalCount}`}</PageLabelText>
     </PageLabelContainer>
   )
 }
