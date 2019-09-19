8b4de3f15ba416ac59a9dba78344c1d697e7bec6
Inbeom Hwang <hwanginbeom@gmail.com>
2019-09-19 18:21:46 +0900

    Add interaction testing document entries for action sheet

--- docs/stories/action-sheet.stories.js
+++ docs/stories/action-sheet.stories.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text, boolean, select } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 
 import ActionSheet from '@titicaca/action-sheet'
 
 storiesOf('ActionSheet', module)
+  .add('인터랙션 테스트: 초기상태 닫힘', () => (
+    <>
+      <a onClick={action('clicked')}>Click me</a>
+      <ActionSheet
+        open={boolean('열림', false)}
+        title={text('제목', '샘플 액션 시트')}
+        onClose={action('onClose')}
+      >
+        <ActionSheet.Item>메뉴 1</ActionSheet.Item>
+        <ActionSheet.Item>메뉴 2</ActionSheet.Item>
+      </ActionSheet>
+    </>
+  ))
+  .add('인터랙션 테스트: 초기상태 열림', () => (
+    <>
+      <a onClick={action('clicked')}>Click me</a>
+      <ActionSheet
+        open={boolean('열림', true)}
+        title={text('제목', '샘플 액션 시트')}
+        onClose={action('onClose')}
+      >
+        <ActionSheet.Item>메뉴 1</ActionSheet.Item>
+        <ActionSheet.Item>메뉴 2</ActionSheet.Item>
+      </ActionSheet>
+    </>
+  ))
   .add('텍스트 메뉴', () => (
     <ActionSheet
       open={boolean('열림', true)}
