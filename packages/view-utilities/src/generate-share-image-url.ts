e7d47118fc8f7dfe0653b2c21546eb01ab078d3b
appear <appear.ko@gmail.com>
2019-09-22 20:03:54 +0900

    no-use-before-define 린트 수정

--- packages/view-utilities/src/generate-share-image-url.ts
+++ packages/view-utilities/src/generate-share-image-url.ts
@@ -1,3 +1,9 @@
+const transformations: { [key: string]: string } = {
+  full: 'c_limit,h_2048,w_2048',
+  large: 'c_limit,h_1024,w_1024',
+  small: 'c_fill,h_256,w_256',
+}
+
 export function generateShareImageUrl({
   mediaBaseUrl,
   cloudinaryId,
@@ -9,10 +15,4 @@ export function generateShareImageUrl({
 }): string {
   const transformation = transformations[version] || transformations.large
   return `${mediaBaseUrl}/${transformation}/${cloudinaryId}.jpeg`
-}
-
-const transformations = {
-  full: 'c_limit,h_2048,w_2048',
-  large: 'c_limit,h_1024,w_1024',
-  small: 'c_fill,h_256,w_256',
 }
