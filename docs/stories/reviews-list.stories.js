6b11bb406b60af9cd94ffbf4fa88c6c012c93452
yceffort <yceffort@gmail.com>
2019-09-23 09:54:39 +0900

    💫 apply api request call in review.stories

--- docs/stories/reviews-list.stories.js
+++ docs/stories/reviews-list.stories.js
@@ -1,11 +1,36 @@
-import React from 'react'
-
+import React, { PureComponent } from 'react'
 import ReviewsList from '@titicaca/review'
-
 import { storiesOf } from '@storybook/react'
+// import Reviews from './reviews-list.sample.json'
+import fetch from 'isomorphic-fetch'
 
-import Reviews from './reviews-list.sample.json'
+class ReviewListStoriesComponent extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews: undefined,
+    }
+  }
+
+  async fetchSampleReviews() {
+    const response = await fetch(
+      // dev 유니버셜 스튜디오 재팬
+      '/api/pois/37cd28e1-ef29-4ac5-8821-3bbdc8d52908/review',
+    )
+    const { data: reviews } = await response.json()
+    this.setState({ reviews })
+  }
+
+  componentDidMount() {
+    this.fetchSampleReviews()
+  }
+
+  render() {
+    const { reviews } = this.state
+    return reviews ? <ReviewsList reviews={reviews} /> : null
+  }
+}
 
 storiesOf('ReviewsList', module).add('일반', () => (
-  <ReviewsList reviews={Reviews} />
+  <ReviewListStoriesComponent />
 ))
