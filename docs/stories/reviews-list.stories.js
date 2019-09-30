ccb1de392484e06078cf32a9d1a0e255b8fb6e37
Inbeom Hwang <hwanginbeom@gmail.com>
2019-09-30 19:27:07 +0900

    Refine ReviewsList docs by employing API invocation

--- docs/stories/reviews-list.stories.js
+++ docs/stories/reviews-list.stories.js
@@ -4,8 +4,10 @@ import ReviewsList from '@titicaca/review'
 
 import { storiesOf } from '@storybook/react'
 
-import Reviews from './reviews-list.sample.json'
-
 storiesOf('ReviewsList', module).add('일반', () => (
-  <ReviewsList reviews={Reviews} />
+  <ReviewsList
+    appNativeActions={{}}
+    resourceType="poi"
+    resourceId="a86a3f55-9f89-4540-a124-f8c4db07ab34"
+  />
 ))
