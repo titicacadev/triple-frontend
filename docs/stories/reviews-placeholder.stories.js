2b2919bb4fa6bb760d4e8d5b7422ba5e05bba1fd
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-01 14:51:15 +0900

    Allow selecting resourceType for reviews placeholder

--- docs/stories/reviews-placeholder.stories.js
+++ docs/stories/reviews-placeholder.stories.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 import ReviewsPlaceholder from '@titicaca/review/lib/review-placeholder-with-rating'
 import { storiesOf } from '@storybook/react'
+import { select } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 
 storiesOf('ReviewsList', module).add('플레이스홀더', () => (
   <ReviewsPlaceholder
-    resourceType="article"
+    resourceType={select(
+      'Resource Type',
+      ['poi', 'tna', 'article', 'hotel', 'default'],
+      'tna',
+    )}
     appUrlScheme="triple"
     onClick={action('clicked')}
   />
