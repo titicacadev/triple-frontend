4432d35b7d6e7a5cdf9ee0d4f9b1f539f06db44a
Unknown <torres@triple-corp.com>
2019-10-02 19:09:46 +0900

    ShareOptions 패키지를 만듭니다

--- docs/stories/share-options.stories.js
+++ docs/stories/share-options.stories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import { ShareOptions } from '@titicaca/core-elements'
+import ShareOptions from '@titicaca/share-options'
 
 storiesOf('ShareOptions', module).add('일반', () => (
   <ShareOptions title="친구들과 여행 정보를 공유하세요" />
---
+++ packages/share-options/README.md
@@ -0,0 +1,11 @@
+# `share-options`
+
+> TODO: description
+
+## Usage
+
+```
+const shareOptions = require('share-options');
+
+// TODO: DEMONSTRATE API
+```
