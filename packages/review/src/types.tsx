770e17681bb51f34d11432cf20f8e40e5028a2d2
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-02 14:40:17 +0900

    Refer types from types.tsx

---
+++ packages/review/src/types.tsx
@@ -0,0 +1,21 @@
+export interface ReviewProps {
+  resourceId: string
+  resourceType: string
+  regionId: string
+  reviewsCount: number
+  shortened?: boolean
+  reviewed?: boolean
+  isPublic?: boolean
+  onFullListButtonClick?: any
+  appUrlScheme: string
+  appNativeActions: AppNativeActionProps
+}
+
+export interface AppNativeActionProps {
+  subscribeLikedChangeEvent?: Function
+  subscribeReviewUpdateEvent?: Function
+  notifyReviewLiked?: Function
+  notifyReviewUnliked?: Function
+  showToast?: Function
+  notifyReviewDeleted?: Function
+}
