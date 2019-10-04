1c8f3b151f4b45600c2b4e85de60b2787aae6d56
Unknown <torres@triple-corp.com>
2019-10-04 18:13:27 +0900

    Method를 열거형으로 export합니다

--- packages/content-sharing/src/index.tsx
+++ packages/content-sharing/src/index.tsx
@@ -9,26 +9,32 @@ const ShareIcon = styled.img`
   height: 46px;
 `
 
+export enum Method {
+  Kakao = 'kakao',
+  Clipboard = 'clipboard',
+  Other = 'other',
+}
+
 export default function ContentSharing({
   onShareClick,
   label,
 }: {
-  onShareClick: ({ method: string }) => any
+  onShareClick: ({ methodType }: { methodType: keyof typeof Method }) => any
   label: string
 }) {
   return (
     <Container textAlign="center" margin={{ top: 50, bottom: 50 }}>
       <ShareIcon
         src="http://assets.triple.guide/images/btn-end-invite-kakao@3x.png"
-        onClick={() => onShareClick({ method: 'kakao' })}
+        onClick={() => onShareClick({ methodType: 'Kakao' })}
       />
       <ShareIcon
         src="http://assets.triple.guide/images/btn-end-invite-copy@3x.png"
-        onClick={() => onShareClick({ method: 'clipboard' })}
+        onClick={() => onShareClick({ methodType: 'Clipboard' })}
       />
       <ShareIcon
         src="http://assets.triple.guide/images/btn-end-invite-more@3x.png"
-        onClick={() => onShareClick({ method: 'other' })}
+        onClick={() => onShareClick({ methodType: 'Other' })}
       />
       <Text margin={{ top: 19 }} center alpha={1}>
         {label}
