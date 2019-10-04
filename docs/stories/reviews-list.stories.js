2ea4b380ca3ab9f52b6d0f9f2d1bb39342c5cd1d
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-04 12:59:59 +0900

    Add HistoryProvider to ReviewsList stories

--- docs/stories/reviews-list.stories.js
+++ docs/stories/reviews-list.stories.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { text, select } from '@storybook/addon-knobs'
 
 import ReviewsList from '@titicaca/review'
+import { HistoryProvider } from '@titicaca/react-contexts'
 
 import { storiesOf } from '@storybook/react'
 
 storiesOf('ReviewsList', module).add('일반', () => (
-  <ReviewsList
-    appNativeActions={{}}
-    resourceId={text('Resource ID', 'f939b4cb-ea3b-34b6-b430-eb5d28fbf467')}
-    resourceType={select('Resource Type', ['poi', 'tna', 'article'], 'tna')}
-  />
+  <HistoryProvider>
+    <ReviewsList
+      appNativeActions={{}}
+      resourceId={text('Resource ID', 'f939b4cb-ea3b-34b6-b430-eb5d28fbf467')}
+      resourceType={select('Resource Type', ['poi', 'tna', 'article'], 'tna')}
+    />
+  </HistoryProvider>
 ))
