b56357d84bd7e7c5e9eb4bc9090aca355201acee
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-04 12:59:39 +0900

    Separate action sheet packages

---
+++ packages/review/src/my-review-action-sheet.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import ActionSheet from '@titicaca/action-sheet'
+import { Confirm } from '@titicaca/modals'
+import {
+  useHistoryContext,
+  useMyReviewsContext,
+} from '@titicaca/react-contexts'
+import { deleteReview as deleteReviewApi } from './review-api-clients'
+
+export const HASH_MY_REVIEW_ACTION_SHEET =
+  'common.reviews-list.my-review-action-sheet'
+
+const HASH_DELETION_MODAL = 'common.reviews-list.deletion-modal'
+
+export default function MyReviewActionSheet({
+  myReview,
+  appUrlScheme,
+  regionId,
+  resourceType,
+  resourceId,
+  notifyReviewDeleted,
+}) {
+  const { uriHash, push, back } = useHistoryContext()
+  const {
+    actions: { deleteMyReview },
+  } = useMyReviewsContext()
+
+  const handleEditMenuClick = () => {
+    window.location.href = `${appUrlScheme}:////reviews/edit?region_id=${regionId}&resource_type=${resourceType}&resource_id=${resourceId}`
+  }
+
+  const handleDeleteMenuClick = () => {
+    push(HASH_DELETION_MODAL)
+
+    return true
+  }
+
+  const deleteReview = async () => {
+    const response = await deleteReviewApi({ id: myReview.id })
+
+    if (response.ok) {
+      notifyReviewDeleted(resourceId, myReview.id)
+
+      deleteMyReview({ id: myReview.id })
+    }
+  }
+
+  return (
+    <>
+      <ActionSheet
+        open={uriHash === HASH_MY_REVIEW_ACTION_SHEET}
+        onClose={back}
+      >
+        <ActionSheet.Item icon="review" onClick={handleEditMenuClick}>
+          수정하기
+        </ActionSheet.Item>
+        <ActionSheet.Item icon="delete" onClick={handleDeleteMenuClick}>
+          삭제하기
+        </ActionSheet.Item>
+      </ActionSheet>
+
+      <Confirm
+        open={uriHash === HASH_DELETION_MODAL}
+        onClose={back}
+        onConfirm={deleteReview}
+      >
+        삭제하겠습니까? 삭제하면 적립된 리뷰 포인트도 함께 사라집니다.
+      </Confirm>
+    </>
+  )
+}
