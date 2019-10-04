b0b647ce71f1b21b1dc1579ca9c6c70c3f9c9252
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-04 17:22:59 +0900

    Convert ReviewContainer to function component

--- packages/review/src/review-container.tsx
+++ packages/review/src/review-container.tsx
@@ -1,7 +1,8 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Section, Container, Text, Button, HR1 } from '@titicaca/core-elements'
 import { formatNumber } from '@titicaca/view-utilities'
+import { useUserAgentContext } from '@titicaca/react-contexts'
 import ReviewsPlaceholder from './review-placeholder-with-rating'
 import { writeReview } from './review-api-clients'
 import ReviewsList from './review-list'
@@ -17,14 +18,24 @@ const WriteIcon = styled.img`
   float: right;
 `
 
-export class ReviewContainer extends React.PureComponent<ReviewProps> {
-  state = { sortingOption: DEFAULT_SORTING_OPTION }
+export default function ReviewContainer({
+  reviewsCount,
+  resourceType,
+  regionId,
+  appUrlScheme,
+  resourceId,
+  appNativeActions: { notifyReviewDeleted, showToast },
+  shortened,
+  onFullListButtonClick,
+}: ReviewProps) {
+  const [sortingOption, setSortingOption] = useState(DEFAULT_SORTING_OPTION)
+  const { isPublic } = useUserAgentContext()
 
-  handleWriteButtonClick = (e: React.SyntheticEvent, rating: number = 0) => {
+  const handleWriteButtonClick = (
+    e: React.SyntheticEvent,
+    rating: number = 0,
+  ) => {
     e.stopPropagation()
-    const {
-      props: { isPublic, appUrlScheme, regionId, resourceType, resourceId },
-    } = this
 
     if (!isPublic) {
       writeReview({
@@ -37,95 +48,74 @@ export class ReviewContainer extends React.PureComponent<ReviewProps> {
     }
   }
 
-  handleSortingOptionSelect = (_, sortingOption) =>
-    this.setState({ sortingOption })
+  const handleSortingOptionSelect = (_, sortingOption) =>
+    setSortingOption(sortingOption)
 
-  render() {
-    const {
-      state: { sortingOption },
-      props: {
-        reviewsCount,
-        isPublic,
-        resourceType,
-        regionId,
-        appUrlScheme,
-        resourceId,
-        appNativeActions: { notifyReviewDeleted, showToast },
-        shortened,
-        onFullListButtonClick,
-      },
-    } = this
-
-    if (isPublic && !reviewsCount) {
-      return null
-    }
-
-    return (
-      <Section anchor={REVIEWS_SECTION_ID}>
-        <Container>
-          {shortened && (
-            <WriteIcon
-              src="https://assets.triple.guide/images/btn-com-write@2x.png"
-              onClick={this.handleWriteButtonClick}
-            />
-          )}
-
-          <Text bold size="huge" color="gray" alpha={1} inline>
-            리뷰
-          </Text>
-
-          {(reviewsCount || 0) > 0 ? (
-            <Text bold size="huge" color="blue" alpha={1} inline>
-              {` ${formatNumber(reviewsCount)}`}
-            </Text>
-          ) : null}
-        </Container>
-        {shortened && (
-          <>
-            <ReviewsPlaceholder
-              resourceType={resourceType}
-              appUrlScheme={appUrlScheme}
-              onClick={this.handleWriteButtonClick}
-            />
-            {(reviewsCount || 0) > 0 && <HR1 />}
-          </>
+  return (
+    <Section anchor={REVIEWS_SECTION_ID}>
+      <Container>
+        {isPublic ? null : (
+          <WriteIcon
+            src="https://assets.triple.guide/images/btn-com-write@2x.png"
+            onClick={handleWriteButtonClick}
+          />
         )}
+        <Text bold size="huge" color="gray" alpha={1} inline>
+          리뷰
+        </Text>
 
-        {(reviewsCount || 0) > 1 ? (
-          <>
-            <Container margin={{ top: 23 }} clearing>
-              <SortingOptions
-                selected={sortingOption}
-                onSelect={this.handleSortingOptionSelect}
-              />
-            </Container>
-          </>
+        {(reviewsCount || 0) > 0 ? (
+          <Text bold size="huge" color="blue" alpha={1} inline>
+            {` ${formatNumber(reviewsCount)}`}
+          </Text>
         ) : null}
+      </Container>
 
-        <ReviewsList
-          resourceType={resourceType}
-          regionId={regionId}
-          appUrlScheme={appUrlScheme}
-          margin={{ top: (reviewsCount || 0) > 1 ? 18 : 30 }}
-          resourceId={resourceId}
-          notifyReviewDeleted={notifyReviewDeleted}
-          showToast={showToast}
-        />
+      {shortened && (
+        <>
+          <ReviewsPlaceholder
+            resourceType={resourceType}
+            appUrlScheme={appUrlScheme}
+            onClick={handleWriteButtonClick}
+          />
+          {(reviewsCount || 0) > 0 && <HR1 />}
+        </>
+      )}
 
-        {reviewsCount > 3 && shortened ? (
-          <Container margin={{ top: 50 }}>
-            <Button
-              basic
-              fluid
-              compact
-              size="small"
-              onClick={() => onFullListButtonClick()}
-            >
-              {reviewsCount - 3}개 리뷰 더보기
-            </Button>
+      {(reviewsCount || 0) > 1 ? (
+        <>
+          <Container margin={{ top: 23 }} clearing>
+            <SortingOptions
+              selected={sortingOption}
+              onSelect={handleSortingOptionSelect}
+            />
           </Container>
-        ) : null}
-      </Section>
-    )
-  }
+        </>
+      ) : null}
+
+      <ReviewsList
+        resourceType={resourceType}
+        regionId={regionId}
+        appUrlScheme={appUrlScheme}
+        margin={{ top: (reviewsCount || 0) > 1 ? 18 : 30 }}
+        resourceId={resourceId}
+        notifyReviewDeleted={notifyReviewDeleted}
+        showToast={showToast}
+      />
+
+      {reviewsCount > 3 && shortened ? (
+        <Container margin={{ top: 50 }}>
+          <Button
+            basic
+            fluid
+            compact
+            size="small"
+            onClick={() => onFullListButtonClick()}
+          >
+            {reviewsCount - 3}개 리뷰 더보기
+          </Button>
+        </Container>
+      ) : null}
+    </Section>
+  )
 }
