c7d3c3b801b9dda34e6e4eeb21bd37047749ed46
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-04 18:11:32 +0900

    Add action interceptor for showing full list

--- docs/stories/reviews-list.stories.js
+++ docs/stories/reviews-list.stories.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { number, text, select, boolean } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 
 import Reviews from '@titicaca/review'
 import {
@@ -30,6 +31,7 @@ storiesOf('Reviews', module).add('일반', () => (
         appNativeActions={{}}
         resourceId={text('Resource ID', 'f939b4cb-ea3b-34b6-b430-eb5d28fbf467')}
         resourceType={select('Resource Type', ['poi', 'tna', 'article'], 'tna')}
+        onFullListButtonClick={action('Show full list')}
       />
     </HistoryProvider>
   </UserAgentProvider>
