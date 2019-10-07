7cd67f0aa6aa3c49227fe0bf9a00dbff5720b2eb
Unknown <torres@triple-corp.com>
2019-10-07 10:44:27 +0900

    Method를 사용합니다

--- packages/content-sharing/src/index.tsx
+++ packages/content-sharing/src/index.tsx
@@ -19,22 +19,22 @@ export default function ContentSharing({
   onShareClick,
   label,
 }: {
-  onShareClick: ({ methodType }: { methodType: keyof typeof Method }) => any
+  onShareClick: ({ methodType }: { methodType: Method }) => any
   label: string
 }) {
   return (
     <Container textAlign="center" margin={{ top: 50, bottom: 50 }}>
       <ShareIcon
         src="http://assets.triple.guide/images/btn-end-invite-kakao@3x.png"
-        onClick={() => onShareClick({ methodType: 'Kakao' })}
+        onClick={() => onShareClick({ methodType: Method.Kakao })}
       />
       <ShareIcon
         src="http://assets.triple.guide/images/btn-end-invite-copy@3x.png"
-        onClick={() => onShareClick({ methodType: 'Clipboard' })}
+        onClick={() => onShareClick({ methodType: Method.Clipboard })}
       />
       <ShareIcon
         src="http://assets.triple.guide/images/btn-end-invite-more@3x.png"
-        onClick={() => onShareClick({ methodType: 'Other' })}
+        onClick={() => onShareClick({ methodType: Method.Other })}
       />
       <Text margin={{ top: 19 }} center alpha={1}>
         {label}
