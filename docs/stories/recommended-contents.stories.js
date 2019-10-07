9ba5e808d7351f47a8b3f1979b9d6e5bbff9810c
Unknown <torres@triple-corp.com>
2019-10-07 17:20:20 +0900

    docs를 수정합니다

--- docs/stories/recommended-contents.stories.js
+++ docs/stories/recommended-contents.stories.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import { Container, Text } from '@titicaca/core-elements'
 import RecommendedContents from '@titicaca/recommended-contents'
 
@@ -10,6 +11,10 @@ storiesOf('RecommendedContents', module).add('추천 컨텐츠', () => (
     <Text size="big" bold>
       다른 컨텐츠 더보기
     </Text>
-    <RecommendedContents contents={contents} margin={{ top: 30 }} />
+    <RecommendedContents
+      contents={contents}
+      margin={{ top: 30 }}
+      onContentIntersecting={action('onContentIntersecting')}
+    />
   </Container>
 ))
