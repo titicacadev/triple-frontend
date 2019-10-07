2780726045c37ace5ed8ee61370ad3f590109dba
Unknown <torres@triple-corp.com>
2019-10-07 10:29:43 +0900

    액션 이름을 동일하게 맞춥니다

--- docs/stories/content-sharing.stories.js
+++ docs/stories/content-sharing.stories.js
@@ -7,6 +7,6 @@ import ContentSharing from '@titicaca/content-sharing'
 storiesOf('ContentSharing', module).add('일반', () => (
   <ContentSharing
     label="친구들과 여행 정보를 공유하세요"
-    onShareClick={action('shareClicked')}
+    onShareClick={action('onShareClick')}
   />
 ))
