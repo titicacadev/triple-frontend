606a1832c1fa1887e3eb308013b54fad057f76fe
yceffort <yceffort@gmail.com>
2019-10-07 13:58:41 +0900

    🗑remove unuse parameter

--- docs/stories/navbar.stories.js
+++ docs/stories/navbar.stories.js
@@ -120,7 +120,6 @@ storiesOf('Navbar', module)
   ))
   .add('검색', () => (
     <SearchNavbar
-      showDeleteButton={false}
       inputPlaceholder={'“호텔예약” 도시이름으로 검색'}
       onBackClick={action('onBackClick')}
       onDeleteClick={action('onDeleteClick')}
