5ef6f0ab23885727dc25935192e9c686a4dc0daa
Unknown <torres@triple-corp.com>
2019-10-08 16:38:09 +0900

    onContentclick의 props를 required하도록 변경

--- packages/recommended-contents/src/index.tsx
+++ packages/recommended-contents/src/index.tsx
@@ -116,7 +116,7 @@ export default function RecommendedContents<T extends ContentElementProps>({
 }: {
   contents: T[]
   margin?: MarginPadding
-  onContentClick?: (e?: React.SyntheticEvent, content?: T) => any
+  onContentClick?: (e: React.SyntheticEvent, content: T) => any
   onContentIntersect?: (content?: T) => any
 }) {
   const contents = contentsData.map(({ title, ...content }) => ({
