1679064c3fddc4005f139b8e3a29dcdae08531ba
Unknown <torres@triple-corp.com>
2019-10-11 11:52:42 +0900

    docs를 변경합니다

--- docs/stories/author.stories.js
+++ docs/stories/author.stories.js
@@ -1,37 +1,46 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 
 import Author from '@titicaca/author'
+import { HR2 } from '@titicaca/core-elements'
+
+import Sample from './author.sample.json'
 
 storiesOf('Author', module).add('일반', () => (
-  <Author
-    source={{
-      name: text('이름', '에디터가있을때'),
-      bio: text(
-        '소개',
-        '여행이 좋아 디지털 노마드로 전세계를 떠돌며\n일을하고 있습니다. budim@gmail.com',
-      ),
-      image: {
-        id: '568dea0a-c04a-403a-84c8-ae5171878c6a',
-        sizes: {
-          full: {
-            url:
-              'https://res.cloudinary.com/triple-dev/image/upload/w_2048,h_2048,c_limit,f_auto/568dea0a-c04a-403a-84c8-ae5171878c6a.jpg',
+  <>
+    <HR2 margin={{ top: 30 }} />
+    <Author
+      source={{
+        name: text('이름', '에디터가있을때'),
+        bio: text(
+          '소개',
+          '여행이 좋아 디지털 노마드로 전세계를 떠돌며\n일을하고 있습니다. budim@gmail.com',
+        ),
+        image: {
+          id: '568dea0a-c04a-403a-84c8-ae5171878c6a',
+          sizes: {
+            full: {
+              url:
+                'https://res.cloudinary.com/triple-dev/image/upload/w_2048,h_2048,c_limit,f_auto/568dea0a-c04a-403a-84c8-ae5171878c6a.jpg',
+            },
+            large: {
+              url:
+                'https://res.cloudinary.com/triple-dev/image/upload/w_1024,h_1024,c_limit,f_auto/568dea0a-c04a-403a-84c8-ae5171878c6a.jpg',
+            },
+            small_square: {
+              url:
+                'https://res.cloudinary.com/triple-dev/image/upload/w_256,h_256,c_fill,f_auto/568dea0a-c04a-403a-84c8-ae5171878c6a.jpg',
+            },
           },
-          large: {
-            url:
-              'https://res.cloudinary.com/triple-dev/image/upload/w_1024,h_1024,c_limit,f_auto/568dea0a-c04a-403a-84c8-ae5171878c6a.jpg',
-          },
-          small_square: {
-            url:
-              'https://res.cloudinary.com/triple-dev/image/upload/w_256,h_256,c_fill,f_auto/568dea0a-c04a-403a-84c8-ae5171878c6a.jpg',
-          },
+          width: 640,
+          height: 427,
         },
-        width: 640,
-        height: 427,
-      },
-    }}
-    bioOverride={text('소개 덮어쓰기', '')}
-  />
+      }}
+      bioOverride={text('소개 덮어쓰기', '')}
+      authorContents={Sample.authorContents}
+      onAuthorContentClick={action('onAuthorContentClick')}
+    />
+  </>
 ))
