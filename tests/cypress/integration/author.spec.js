221e74a1583c574e346e7869caa412f1e6103685
Unknown <torres@triple-corp.com>
2019-10-14 13:53:46 +0900

    Modify tests

--- tests/cypress/integration/author.spec.js
+++ tests/cypress/integration/author.spec.js
@@ -16,12 +16,22 @@ describe('Author', () => {
     cy.get('@author')
       .find('div')
       .first()
+      .as('info')
+
+    cy.get('@info')
+      .find('div')
+      .first()
       .as('name')
+
+    cy.get('@info')
+      .find('div')
+      .last()
+      .as('bio')
 
     cy.get('@author')
       .find('div')
       .last()
-      .as('bio')
+      .as('intro')
   })
 
   it('should have correct image style', () => {
@@ -42,16 +52,12 @@ describe('Author', () => {
     cy.get('@name')
       .should('have.css', 'color')
       .and('eq', 'rgb(58, 58, 58)')
-
-    cy.get('@name')
-      .should('have.css', 'margin-top')
-      .and('eq', '15px')
   })
 
   it('should have correct bio style', () => {
     cy.get('@bio')
       .should('have.css', 'font-size')
-      .and('eq', '14px')
+      .and('eq', '13px')
 
     cy.get('@bio')
       .should('have.css', 'font-weight')
@@ -59,14 +65,16 @@ describe('Author', () => {
 
     cy.get('@bio')
       .should('have.css', 'color')
+      .and('eq', 'rgba(58, 58, 58, 0.3)')
+  })
+
+  it('should have correct intro style', () => {
+    cy.get('@intro')
+      .should('have.css', 'font-size')
+      .and('eq', '14px')
+
+    cy.get('@intro')
+      .should('have.css', 'color')
       .and('eq', 'rgba(58, 58, 58, 0.5)')
-
-    cy.get('@bio')
-      .should('have.css', 'text-align')
-      .and('eq', 'center')
-
-    cy.get('@bio')
-      .should('have.css', 'margin-top')
-      .and('eq', '5px')
   })
 })
