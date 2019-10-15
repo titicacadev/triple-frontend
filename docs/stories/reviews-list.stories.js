af1e8d8e8b5b24fa063133e450ed695411858600
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-15 19:34:32 +0900

    Attach ReviewLikesProvider to ReviewsList stories

--- docs/stories/reviews-list.stories.js
+++ docs/stories/reviews-list.stories.js
@@ -7,40 +7,52 @@ import {
   UserAgentProvider,
   generateUserAgentValues,
   HistoryProvider,
+  ReviewLikesProvider,
 } from '@titicaca/react-contexts'
 
 import { storiesOf } from '@storybook/react'
 
 storiesOf('Reviews', module).add('일반', () => (
-  <UserAgentProvider
-    value={generateUserAgentValues(
-      select(
-        'User-Agent',
-        [
-          'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/3.0.0',
+  <ReviewLikesProvider>
+    <UserAgentProvider
+      value={generateUserAgentValues(
+        select(
+          'User-Agent',
+          [
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/3.0.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+          ],
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-        ],
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
-      ),
-    )}
-  >
-    <HistoryProvider>
-      <Reviews
-        shortened={boolean('Shortened', false)}
-        reviewsCount={number('Review count', 120)}
-        appNativeActions={{
-          subscribeLikedChangeEvent: action('subscribeLikedChangeEvent'),
-          subscribeReviewUpdateEvent: action('subscribeReviewUpdateEvent'),
-          unsubscribeReviewUpdateEvent: action('unsubscribeReviewUpdateEvent'),
-          notifyReviewDeleted: action('notifyReviewDeleted'),
-          notifyReviewLiked: action('notifyReviewLiked'),
-          notifyReviewUnliked: action('notifyReviewUnliked'),
-          showToast: action('showToast'),
-        }}
-        resourceId={text('Resource ID', 'f939b4cb-ea3b-34b6-b430-eb5d28fbf467')}
-        resourceType={select('Resource Type', ['poi', 'tna', 'article'], 'tna')}
-        onFullListButtonClick={action('onFullListButtonClick')}
-      />
-    </HistoryProvider>
-  </UserAgentProvider>
+        ),
+      )}
+    >
+      <HistoryProvider>
+        <Reviews
+          shortened={boolean('Shortened', false)}
+          reviewsCount={number('Review count', 120)}
+          appNativeActions={{
+            subscribeLikedChangeEvent: action('subscribeLikedChangeEvent'),
+            subscribeReviewUpdateEvent: action('subscribeReviewUpdateEvent'),
+            unsubscribeReviewUpdateEvent: action(
+              'unsubscribeReviewUpdateEvent',
+            ),
+            notifyReviewDeleted: action('notifyReviewDeleted'),
+            notifyReviewLiked: action('notifyReviewLiked'),
+            notifyReviewUnliked: action('notifyReviewUnliked'),
+            showToast: action('showToast'),
+          }}
+          resourceId={text(
+            'Resource ID',
+            'f939b4cb-ea3b-34b6-b430-eb5d28fbf467',
+          )}
+          resourceType={select(
+            'Resource Type',
+            ['poi', 'tna', 'article'],
+            'tna',
+          )}
+          onFullListButtonClick={action('onFullListButtonClick')}
+        />
+      </HistoryProvider>
+    </UserAgentProvider>
+  </ReviewLikesProvider>
 ))
