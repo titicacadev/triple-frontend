9985e1d7e9951594d661a8cc7f00f5702aa09f59
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-16 10:17:25 +0900

    Recompose exports of review package

--- packages/review/src/index.tsx
+++ packages/review/src/index.tsx
@@ -1,17 +1,13 @@
-import * as React from 'react'
+import React from 'react'
 import ReviewContainer from './review-container'
 import { TransitionModals } from './transition-modals'
 import { ReviewProps } from './types'
 
+export * from './review-likes-context'
 export * from './review-placeholder-with-rating'
-
 export * from './review-api-clients'
 
-export default function Reviews({
-  resourceId,
-  regionId,
-  ...props
-}: ReviewProps) {
+export function Reviews({ resourceId, regionId, ...props }: ReviewProps) {
   return (
     <>
       <ReviewContainer regionId={regionId} resourceId={resourceId} {...props} />
