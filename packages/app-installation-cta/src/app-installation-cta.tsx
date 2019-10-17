6e0b333ef6f14c0da1f9cfc55702b9b9a3c06b9b
박기완 <zprime0920@gmail.com>
2019-10-17 17:22:05 +0900

    Feature: 이미지, 텍스트 배너 통합 컴포넌트 구현
    컴포넌트가 마운트 되면 이미지 배너를 표시하고 이미지 배너를 dismiss하면 텍스트 배너를 표시합니다.

---
+++ packages/app-installation-cta/src/app-installation-cta.tsx
@@ -0,0 +1,70 @@
+import React, { FC, useState } from 'react'
+import styled from 'styled-components'
+
+import ImageBanner from './image-banner'
+import TextBanner from './text-banner'
+
+interface AppInstallationCTAProps {
+  imgUrl: string
+  installUrl: string
+  message: string
+}
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.07);
+  background-color: rgba(58, 58, 58, 0.5);
+  z-index: 10;
+`
+
+const BottomFixedContainer = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: 11;
+
+  > * {
+    margin: 0 auto;
+  }
+`
+
+const TopFixedContainer = styled.div<{ isVisible: boolean }>`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  z-index: 11;
+  transition: all ease 0.1s;
+  ${({ isVisible }) => (isVisible ? 'opacity: 1' : 'opacity: 0')};
+`
+
+const AppInstallationCTA: FC<AppInstallationCTAProps> = ({
+  imgUrl,
+  installUrl,
+  message,
+}) => {
+  const [isImageBannerOpen, setIsImageBannerOpen] = useState(true)
+
+  if (isImageBannerOpen) {
+    return (
+      <Overlay>
+        <BottomFixedContainer>
+          <ImageBanner
+            imgUrl={imgUrl}
+            installUrl={installUrl}
+            onDismiss={() => setIsImageBannerOpen(false)}
+          />
+        </BottomFixedContainer>
+      </Overlay>
+    )
+  }
+
+  return <TextBanner message={message} installUrl={installUrl} />
+}
+
+export default AppInstallationCTA
