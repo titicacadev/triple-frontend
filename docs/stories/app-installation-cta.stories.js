5beb5416a6f0e62dbfd39f3090448f569297619d
박기완 <zprime0920@gmail.com>
2019-10-17 15:35:46 +0900

    Feature: TextBanner UI 구현

--- docs/stories/app-installation-cta.stories.js
+++ docs/stories/app-installation-cta.stories.js
@@ -3,12 +3,18 @@ import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { text } from '@storybook/addon-knobs'
 
-import { ImageBanner } from '@titicaca/app-installation-cta'
+import { ImageBanner, TextBanner } from '@titicaca/app-installation-cta'
 
-storiesOf('AppInstallationCTA', module).add('이미지 배너', () => (
-  <ImageBanner
-    imgUrl={text('이미지 URL', '')}
-    installUrl={text('설치 URL', 'https://triple-dev.titicaca-corp.com')}
-    onDismiss={action('banner dismissed')}
-  />
-))
+storiesOf('AppInstallationCTA', module)
+  .add('이미지 배너', () => (
+    <ImageBanner
+      imgUrl={text('이미지 URL', '')}
+      installUrl={text('설치 URL', 'https://triple-dev.titicaca-corp.com')}
+      onDismiss={action('banner dismissed')}
+    />
+  ))
+  .add('텍스트 배너', () => (
+    <TextBanner
+      message={text('표시할 메시지', '앱 다운로드시 가이드북 무료')}
+    />
+  ))
