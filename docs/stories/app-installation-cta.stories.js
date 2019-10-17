e2fb6d687c181707a4334969be2e94e34bc43747
박기완 <zprime0920@gmail.com>
2019-10-17 14:25:05 +0900

    Feature: 앱설치 유도 배너 UI 구현
    네이버 검색광고로 유입된 사용자에게 표시할 배너 UI를 구현했습니다.

--- docs/stories/app-installation-cta.stories.js
+++ docs/stories/app-installation-cta.stories.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { text } from '@storybook/addon-knobs'
 
-import AppInstallationCTA from '@titicaca/app-installation-cta'
+import { Banner } from '@titicaca/app-installation-cta'
 
-storiesOf('AppInstallationCTA', module).add('기본', () => (
-  <AppInstallationCTA />
+storiesOf('AppInstallationCTA', module).add('배너', () => (
+  <Banner
+    imgUrl={text('이미지 URL', '')}
+    installUrl={text('설치 URL', 'https://triple-dev.titicaca-corp.com')}
+    onDismiss={action('banner dismissed')}
+  />
 ))
---
+++ packages/app-installation-cta/src/banner.tsx
@@ -0,0 +1,87 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+
+interface BannerProps {
+  imgUrl: string
+  installUrl: string
+  onDismiss: () => void
+}
+
+const CONTENT_MIN_WIDTH = 320
+const CONTNET_MAX_WIDTH = 760
+
+const BannerWrapper = styled.div`
+  box-sizing: border-box;
+  max-width: ${CONTNET_MAX_WIDTH}px;
+  min-width: ${CONTENT_MIN_WIDTH}px;
+  height: 230px;
+  padding: 0 20px 20px 20px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.07);
+  background-color: #0179ff;
+`
+const ImageWrapper = styled.div`
+  margin: 0 auto;
+  height: 130px;
+`
+
+const BannerImage = styled.img`
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: block;
+  width: 320px;
+  height: 130px;
+`
+
+const InstallLink = styled.a`
+  display: block;
+  box-sizing: border-box;
+  margin-top: 5px;
+  margin-bottom: 16px;
+  width: 100%;
+  height: 44px;
+  line-height: 23px;
+  border-radius: 25px;
+  padding: 10px 0 11px 0;
+  background-color: white;
+  color: black;
+  text-align: center;
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
+  text-decoration: none;
+`
+
+const DismissButton = styled.button`
+  display: block;
+  margin: 0 auto;
+  border: 0;
+  background-color: transparent;
+  opacity: 0.6;
+  font-size: 12px;
+  font-weight: 500;
+  text-align: center;
+  color: white;
+  text-decoration: underline;
+  outline: none;
+  cursor: pointer;
+`
+
+const Banner: FC<BannerProps> = ({ imgUrl, installUrl, onDismiss }) => {
+  return (
+    <BannerWrapper>
+      <ImageWrapper>
+        <BannerImage src={imgUrl} />
+      </ImageWrapper>
+
+      <InstallLink href={installUrl}>👋🏻 손쉽게 앱 설치하기</InstallLink>
+
+      <DismissButton onClick={onDismiss}>
+        아깝지만 나중에 받을게요
+      </DismissButton>
+    </BannerWrapper>
+  )
+}
+
+export default Banner
