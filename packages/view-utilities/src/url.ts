1ec64a63210bbc3b627d703419f83133ba0c78b5
Kyudeok <polysiya@gmail.com>
2019-10-22 10:18:13 +0900

    Add generateUrl function to view-utilities

--- packages/view-utilities/src/url.ts
+++ packages/view-utilities/src/url.ts
@@ -1,4 +1,13 @@
-export function parseUrl(rawHref?: string) {
+interface UrlElements {
+  href?: string
+  scheme?: string
+  host?: string
+  path?: string
+  query?: string
+  hash?: string
+}
+
+export function parseUrl(rawHref?: string): UrlElements {
   if (!rawHref) {
     return {}
   }
@@ -9,4 +18,21 @@ export function parseUrl(rawHref?: string) {
     ) || []
 
   return { href, scheme, host, path, query, hash }
+}
+
+export function generateUrl(elements: UrlElements, baseUrl?: string) {
+  const { scheme, host, path, query, hash } = {
+    ...(baseUrl && parseUrl(baseUrl)),
+    ...elements,
+  }
+
+  return [
+    scheme && `${scheme}://`,
+    host,
+    path,
+    query && `?${query}`,
+    hash && `#${hash}`,
+  ]
+    .filter((v) => v)
+    .join('')
 }
