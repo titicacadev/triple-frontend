8a95826186c12c6c6f6bdaaf00ce406832e1a2b7
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-24 23:40:32 +0900

    Separate actions and body from modals component

---
+++ packages/modals/src/body.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Container, Text } from '@titicaca/core-elements'
+
+export default function ModalBody({
+  title,
+  description,
+}: {
+  title?: string
+  description: string
+}) {
+  return (
+    <Container padding={{ top: 40, bottom: 40, left: 30, right: 30 }}>
+      {title ? (
+        <Text bold center size="big" color="gray" margin={{ bottom: 10 }}>
+          {title}
+        </Text>
+      ) : null}
+      <Text center size="large" lineHeight={1.38} color="gray">
+        {description}
+      </Text>
+    </Container>
+  )
+}
