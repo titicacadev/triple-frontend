7c38286a518b4eacc8ad84669bccfa33197cbe24
Inbeom Hwang <hwanginbeom@gmail.com>
2019-10-25 14:53:25 +0900

    Simplify index package of review package

--- packages/review/src/index.tsx
+++ packages/review/src/index.tsx
@@ -1,22 +1,5 @@
-import React from 'react'
-import ReviewContainer from './review-container'
-import { TransitionModals } from './transition-modals'
-import { ReviewProps } from './types'
+export { default as Reviews } from './review-container'
 
 export * from './review-likes-context'
 export * from './review-placeholder-with-rating'
 export * from './review-api-clients'
-
-export function Reviews({
-  resourceId,
-  regionId,
-  deepLink,
-  ...props
-}: ReviewProps) {
-  return (
-    <>
-      <ReviewContainer regionId={regionId} resourceId={resourceId} {...props} />
-      <TransitionModals deepLink={deepLink} />
-    </>
-  )
-}
