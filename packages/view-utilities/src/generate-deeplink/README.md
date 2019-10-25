57eaa2ac1ac3cb829ad45ea0274a83d1a0a78bd7
박기완 <zprime0920@gmail.com>
2019-10-25 15:31:27 +0900

    Feature: deeplink 생성기 모듈 구현

---
+++ packages/view-utilities/src/generate-deeplink/README.md
@@ -0,0 +1,22 @@
+# generate-deeplink
+
+## 사용 방법
+
+```:javascript
+const generateDeepLink = makeDeepLinkGenerator({
+  oneLinkParams: {
+    subdomain: '',
+    id: '',
+    pid: '',
+  },
+  appScheme: '',
+  webURLBase: '',
+})
+
+// 컴포넌트에서
+generateDeepLink({
+  campaign: '하단 배너',
+  ...injectContentSource(source),
+  ...injectUTMContext(utmContext),
+})
+```
---
+++ packages/view-utilities/src/generate-deeplink/param-injectors.ts
@@ -0,0 +1,42 @@
+interface ContentSource {
+  regionId: string
+  type: 'article' | 'restaurant' | 'hotel' | 'attraction'
+  id: string
+}
+
+interface UTMContext {
+  source: string
+  medium: string
+  campaign: string
+  term?: string
+  content?: string
+}
+
+/**
+ * 콘텐츠 데이터를 generator parameter 값으로 변환합니다.
+ *
+ * @param source regionId, type, id를 가진 콘텐츠 데이터
+ */
+export function injectContentSource({ regionId, type, id }: ContentSource) {
+  return { path: `/regions/${regionId}/${type}s/${id}` }
+}
+
+/**
+ * utm 콘텍스트를 generator parameter 값으로 변환합니다.
+ * @param utmContext
+ */
+export function injectUTMContext({
+  source,
+  medium,
+  campaign,
+  term,
+}: Partial<UTMContext> = {}) {
+  const adProvider = source && medium ? `${source}_${medium}` : undefined
+
+  return {
+    campaign,
+    adSet: adProvider,
+    ad: term,
+    channel: adProvider,
+  }
+}
