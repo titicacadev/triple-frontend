acbaba789fc3ebce77856cab9e82c8c1243f82c0
yceffort <yceffort@gmail.com>
2019-10-30 15:50:31 +0900

    export RegionListElement

--- packages/triple-document/src/index.tsx
+++ packages/triple-document/src/index.tsx
@@ -2,5 +2,6 @@ import { TripleDocument } from './triple-document'
 export { ELEMENTS, TextElement } from './triple-document'
 export * from './text'
 export * from './tna'
+export * from './region'
 
 export default TripleDocument
