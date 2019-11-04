56312be6ba3f557ce7d577cea377551f0dc80f7f
appear <appear.ko@gmail.com>
2019-11-04 17:01:40 +0900

    기존 tab 을 확장하여 line tab 을 추가합니다

--- docs/stories/tabs.stories.js
+++ docs/stories/tabs.stories.js
@@ -6,13 +6,30 @@ import { select } from '@storybook/addon-knobs'
 
 import { Tabs } from '@titicaca/core-elements'
 
-storiesOf('Tabs', module).add('이근처장소', () => (
-  <Tabs
-    value={select('현재 탭', ['attractions', 'restaurants'], 'attractions')}
-    options={[
-      { label: '관광지', value: 'attractions' },
-      { label: '맛집', value: 'restaurants' },
-    ]}
-    onChange={action('change')}
-  />
-))
+storiesOf('Tabs', module)
+  .add('이근처장소', () => (
+    <Tabs
+      value={select('현재 탭', ['attractions', 'restaurants'], 'attractions')}
+      options={[
+        { label: '관광지', value: 'attractions' },
+        { label: '맛집', value: 'restaurants' },
+      ]}
+      onChange={action('change')}
+    />
+  ))
+  .add('라인 탭', () => {
+    const options = [
+      { label: '투어티켓', value: '투어티켓' },
+      { label: '호텔', value: '호텔' },
+    ]
+
+    const values = options.map(({ value }) => value)
+
+    return (
+      <Tabs
+        line
+        options={options}
+        value={select('버튼 크기', values, values[0])}
+      />
+    )
+  })
--- packages/core-elements/src/elements/tabs.tsx
+++ packages/core-elements/src/elements/tabs.tsx
@@ -1,49 +1,121 @@
 import * as React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import Container from './container'
+import { GetGlobalColor } from '../commons'
 
-const TabsContainer = styled.div`
-  background-color: #efefef;
-  border-radius: 4px;
-  padding: 2px;
+interface TabProps {
+  value?: any
+  options?: [{ label: string; value: any }]
+  onChange?: (e?: React.SyntheticEvent, value?: any) => any
+  line?: boolean
+}
+
+const TabContainer = styled.div<{ line?: boolean }>`
   white-space: nowrap;
+  display: table;
+  width: 100%;
+  table-layout: fixed;
+
+  ${({ line }) =>
+    line
+      ? css`
+          border-bottom: 1px solid rgba(${GetGlobalColor('gray')}, 0.05);
+        `
+      : css`
+          background-color: #efefef;
+          border-radius: 4px;
+          padding: 2px;
+        `}
 `
 
-const Tab = styled.div<{ widthPercent?: number; active?: boolean }>`
-  display: inline-block;
+const TabLabel = styled.div<{ line?: boolean; active?: boolean }>`
+  display: table-cell;
   box-sizing: border-box;
-  width: ${({ widthPercent }) => widthPercent}%;
-  height: 36px;
-  line-height: 36px;
   text-align: center;
-  color: ${({ active }) => (active ? '#3a3a3a' : 'rgba(46, 46, 46, 0.3)')};
-  background-color: ${({ active }) => (active ? '#ffffff' : 'transparent')};
-  border-radius: 2px;
-  font-size: 14px;
-  font-weight: bold;
+  padding: 11px 0;
   cursor: pointer;
+
+  ${({ line, active }) =>
+    line
+      ? css`
+          font-size: 15px;
+          color: ${active
+            ? `rgba(${GetGlobalColor('gray')}, 1)`
+            : `rgba(${GetGlobalColor('gray')}, 0.3)`};
+        `
+      : css`
+          color: ${active ? '#3a3a3a' : 'rgba(46, 46, 46, 0.3)'};
+          background-color: ${active ? '#ffffff' : 'transparent'};
+          border-radius: 2px;
+          font-size: 14px;
+          font-weight: bold;
+        `}
 `
+
+const Line = styled.div<{ size: number; position: number }>`
+  position: absolute;
+  bottom: 0;
+  width: ${({ size }) => size}%;
+  left: ${({ position }) => position}%;
+  height: 2px;
+  background: rgba(${GetGlobalColor('blue')}, 1);
+  transition: all 0.2s;
+`
+
+function BasicTab({ options, value: currentValue, onChange }: TabProps) {
+  return (
+    <TabContainer>
+      {options.map(({ label, value }, i) => (
+        <TabLabel
+          active={currentValue === value}
+          key={i}
+          onClick={(e) => onChange(e, value)}
+        >
+          {label}
+        </TabLabel>
+      ))}
+    </TabContainer>
+  )
+}
+
+function LineTab({ options, value: currentValue, onChange, line }: TabProps) {
+  const size = 100 / options.length
+  const activeIdx = options.findIndex(({ value }) => value === currentValue)
+  const linePosition = activeIdx > -1 ? activeIdx * size : -100
+
+  return (
+    <Container position="relative">
+      <TabContainer line={line}>
+        {options.map(({ label, value }, idx) => (
+          <TabLabel
+            key={idx}
+            active={value === currentValue}
+            onClick={(e) => onChange(e, value)}
+            line={line}
+          >
+            {label}
+          </TabLabel>
+        ))}
+      </TabContainer>
+      <Line size={size} position={linePosition} />
+    </Container>
+  )
+}
 
 export default function Tabs({
   value: currentValue,
   options,
   onChange,
-}: {
-  value?: any
-  options?: [{ label: string; value: any }]
-  onChange?: (e?: React.SyntheticEvent, value?: any) => any
-}) {
+  line,
+}: TabProps) {
+  const Component = line ? LineTab : BasicTab
+
   return (
-    <TabsContainer>
-      {options.map(({ label, value }, i) => (
-        <Tab
-          active={currentValue === value}
-          widthPercent={options.length > 0 ? 100 / options.length : 100}
-          key={i}
-          onClick={(e) => onChange(e, value)}
-        >
-          {label}
-        </Tab>
-      ))}
-    </TabsContainer>
+    <Component
+      line={line}
+      onChange={onChange}
+      options={options}
+      value={currentValue}
+    />
   )
 }
