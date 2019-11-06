b695e944120e62d2e11c9fb2d9ab9d66e3d274ad
박기완 <zprime0920@gmail.com>
2019-11-06 11:38:30 +0900

    Feature: AD Banner 스토리에서 API 요청 테스트 할 수 있게 변경
    AdBannersView 컴포넌트 대신 AdBanners 컴포넌트를 임포트하도록 수정했다.
    API 요청에 필요한 의존성을 추가했다.

--- docs/stories/ad-banners.stories.js
+++ docs/stories/ad-banners.stories.js
@@ -1,35 +1,52 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { number } from '@storybook/addon-knobs'
+import { number, text, select, boolean } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 
-import AdBanners from '@titicaca/ad-banners/lib/ad-banners-view'
+import {
+  HistoryProvider,
+  EventTrackingProvider,
+  DeviceProvider,
+} from '@titicaca/react-contexts'
+import AdBanners from '@titicaca/ad-banners'
 
-const banners = [
-  {
-    id: '1',
-    desc: '설명',
-    image:
-      'https://media.triple.guide/triple-dev/a707bf40-bab7-4c96-bcd0-e1a73ab57028.jpg',
-    target: '',
-  },
-  {
-    id: '2',
-    desc: '설명',
-    image:
-      'https://media.triple.guide/triple-dev/45eb9535-85a3-49b9-b76c-cd627fc13f0e.jpg',
-    target: '',
-  },
-]
+const CONTENT_TYPE_SET = {
+  article: 'article',
+  attraction: 'attraction',
+  hotel: 'hotel',
+  restaurant: 'restaurant',
+}
 
 storiesOf('AdBanners', module).add('광고 배너 목록', () => (
-  <AdBanners
-    banners={banners}
-    padding={{
-      left: number('섹션 왼쪽 패딩'),
-      right: number('섹션 오른쪽 패딩'),
+  <DeviceProvider
+    value={{
+      inRegion: boolean('inRegion', true),
+      latitude: number('위도', 13.471778),
+      longitude: number('경도', 144.812304),
     }}
-    onClickBanner={action('onClick banner')}
-    onIntersectingBanner={action('onIntersecting banner')}
-  />
+  >
+    <EventTrackingProvider
+      pageLabel="광고 배너 테스트"
+      trackEvent={action('이벤트 트래킹')}
+      trackScreen={action('스크린 트래킹')}
+      viewItem={action('viewItem')}
+    >
+      <HistoryProvider>
+        <AdBanners
+          contentType={select(
+            '콘텐츠 타입',
+            CONTENT_TYPE_SET,
+            CONTENT_TYPE_SET['attraction'],
+          )}
+          contentId={text('콘텐츠 ID', '81977f84-ddd0-4112-8057-6cc9dab9aa70')}
+          regionId={text('리전 ID', 'ea2b52ff-9cdb-4028-9442-7a8defd13af9')}
+          padding={{
+            left: number('섹션 왼쪽 패딩'),
+            right: number('섹션 오른쪽 패딩'),
+          }}
+          eventAttributes={{ title: '콘텐츠 제목' }}
+        />
+      </HistoryProvider>
+    </EventTrackingProvider>
+  </DeviceProvider>
 ))
