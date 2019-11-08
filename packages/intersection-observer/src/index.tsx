672a144025d7fe749939785bdad5872fbb526eff
Unknown <torres@triple-corp.com>
2019-11-08 16:13:41 +0900

    Dynamic import with ssr

--- packages/intersection-observer/src/index.tsx
+++ packages/intersection-observer/src/index.tsx
@@ -1,33 +1,28 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 
-const OriginalObserver = dynamic(
-  async () => {
-    try {
-      if (
-        typeof window !== 'undefined' &&
-        !(
-          'IntersectionObserver' in window &&
-          'IntersectionObserverEntry' in window &&
-          'intersectionRatio' in
-            (window as any).IntersectionObserverEntry.prototype
-        )
-      ) {
-        await import('intersection-observer')
-      }
-
-      return import(
-        /* eslint-disable-next-line comma-dangle */
-        '@researchgate/react-intersection-observer'
+const OriginalObserver = dynamic(async () => {
+  try {
+    if (
+      typeof window !== 'undefined' &&
+      !(
+        'IntersectionObserver' in window &&
+        'IntersectionObserverEntry' in window &&
+        'intersectionRatio' in
+          (window as any).IntersectionObserverEntry.prototype
       )
-    } catch (e) {
-      return Promise.resolve(({ children }) => children || null)
+    ) {
+      await import('intersection-observer')
     }
-  },
-  {
-    ssr: false,
-  },
-)
+
+    return import(
+      /* eslint-disable-next-line comma-dangle */
+      '@researchgate/react-intersection-observer'
+    )
+  } catch (e) {
+    return Promise.resolve(({ children }) => children || null)
+  }
+})
 
 export default function IntersectionObserver(props) {
   return <OriginalObserver {...props} />
