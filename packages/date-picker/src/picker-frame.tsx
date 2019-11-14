bfcd68587197a02969700cbacdeae8d4f5f3e9a4
steve <steve@triple-corp.com>
2019-11-14 10:56:15 +0900

    modify css

--- packages/date-picker/src/picker-frame.tsx
+++ packages/date-picker/src/picker-frame.tsx
@@ -111,12 +111,11 @@ const PickerFrame = styled.div`
   .DayPicker-Day--saturday.DayPicker-Day--today {
     color: rgb(${GetGlobalColor('red')});
   }
-  .DayPicker-Day--holiday {
+  .DayPicker-Day--publicHolidays {
     color: rgb(${GetGlobalColor('red')});
     &.DayPicker-Day--disabled {
       color: rgba(${GetGlobalColor('gray')}, 0.5);
     }
   }
 `
-
 export default PickerFrame
