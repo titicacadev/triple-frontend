b716fd0727702f27af74ce4c696eee17b568447c
Luffy <luffy@triple-corp.com>
2019-11-19 16:43:02 +0900

    스토리 추가

---
+++ docs/stories/floating-install-button.stories.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { text, boolean } from '@storybook/addon-knobs'
+
+import FloatingInstallButton from '@titicaca/floating-install-button'
+
+storiesOf('FloatingInstallButton', module).add('플로팅 버튼', () => (
+  <FloatingInstallButton
+    appInstallLink={'https://triple.onelink.me/aZP6/21d43a81'}
+    isPublic={boolean('앱 밖', true)}
+    trackEvent={() => {}}
+    description={text('설명', '설명 텍스트가 들어갑니다.')}
+    trackEventParams={{
+      onShow: { ga: ['플로팅_설치하기_노출'] },
+      onSelect: { ga: ['플로팅_설치하기_선택'] },
+      onClose: { ga: ['플로팅_설치하기_닫기'] },
+    }}
+  />
+))
