b7531a61119ff239bc5c13513462089d01a5b93c
Luffy <luffy@triple-corp.com>
2019-11-20 19:03:23 +0900

    placeholder 개선 (링크, 설명 추가)

--- packages/review/src/review-placeholder-with-rating.tsx
+++ packages/review/src/review-placeholder-with-rating.tsx
@@ -3,20 +3,41 @@ import styled from 'styled-components'
 import { Container, Rating, Text } from '@titicaca/core-elements'
 
 const CommentByTypes = new Map()
-CommentByTypes.set('tna', '이 투어·티켓 어떠셨나요?')
-CommentByTypes.set('poi', '이곳의 첫 번째 리뷰를 올려주세요.')
-CommentByTypes.set(
-  'article',
-  '이 가이드 어떠셨나요?\n여행에서 알면 좋을 팁을 나눠주세요.',
-)
-CommentByTypes.set('hotel', '이곳의 첫 번째 리뷰를 올려주세요.')
-CommentByTypes.set('default', '이곳의 첫 번째 리뷰를 올려주세요.')
+CommentByTypes.set('tna', {
+  title: '이 투어·티켓 어떠셨나요?',
+  description: '리뷰를 남겨주시면, 여행자클럽 포인트를 드려요!',
+})
+CommentByTypes.set('poi', {
+  title: '이곳의 첫 번째 리뷰를 올려주세요.',
+  description: '리뷰를 남겨주시면, 여행자클럽 포인트를 드려요!',
+})
+CommentByTypes.set('article', {
+  title: '이 가이드 어떠셨나요?',
+  description: '여행 팁을 공유해주시면, 여행자클럽 포인트를 드려요!',
+})
+CommentByTypes.set('hotel', {
+  title: '이 호텔 어떠셨나요?',
+  description: '리뷰를 남겨주시면, 여행자클럽 포인트를 드려요!',
+})
+CommentByTypes.set('default', {
+  title: '이곳의 첫 번째 리뷰를 올려주세요.',
+  description: '리뷰를 남겨주시면, 여행자클럽 포인트를 드려요!',
+})
 
 const PlaceholderContainer = styled(Container)`
   width: 100%;
   text-align: center;
 `
 
+const MileageButton = styled.button.attrs({ type: 'button' })`
+  display: block;
+  padding-top: 19px;
+  font-size: 13px;
+  color: #2987f0;
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0 auto;
+`
 const GuideImage = styled.img`
   content: url('https://assets.triple.guide/images/img-card-guide-review@3x.png');
   display: block;
@@ -24,7 +45,11 @@ const GuideImage = styled.img`
   height: 50px;
   margin: auto;
 `
-export default function ReviewsPlaceholder({ resourceType, onClick }) {
+export default function ReviewsPlaceholder({
+  resourceType,
+  appUrlScheme,
+  onClick,
+}) {
   return (
     <PlaceholderContainer margin={{ top: 20 }} onClick={onClick}>
       {resourceType === 'article' ? (
@@ -39,8 +64,19 @@ export default function ReviewsPlaceholder({ resourceType, onClick }) {
         alpha={1}
         lineHeight={1.5}
       >
-        {CommentByTypes.get(resourceType || 'default')}
+        {CommentByTypes.get(resourceType || 'default').title}
       </Text>
+      <Text color="gray" alpha={0.5} size="tiny" margin={{ top: 3 }}>
+        {CommentByTypes.get(resourceType || 'default').description}
+      </Text>
+      <MileageButton
+        onClick={(e) => {
+          window.location.href = `${appUrlScheme}:///my/mileage`
+          e.preventDefault()
+        }}
+      >
+        여행자 클럽 혜택보기
+      </MileageButton>
     </PlaceholderContainer>
   )
 }
