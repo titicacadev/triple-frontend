43bce436d545095d8aa9e4bc5c644fb723f604dd
appear <appear.ko@gmail.com>
2019-12-04 10:44:47 +0900

    storybook 버젼을 v5.2 로 올립니다

--- docs/.storybook/config.js
+++ docs/.storybook/config.js
@@ -13,9 +13,4 @@ addParameters({
 })
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/)
-function loadStories() {
-  req.keys().forEach((filename) => req(filename))
-}
-
-configure(loadStories, module)
+configure(require.context('../stories', true, /\.stories\.js$/), module)
