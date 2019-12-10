38e715c3f06ca201b03677b687a96f024f9a8cd5
박기완 <zprime0920@gmail.com>
2019-12-10 14:16:45 +0900

    Refactor: ActionSheet 컴포넌트 삭제
    액션 시트 컴포넌트는 다른 파일에서 테스트 가능합니다.

--- docs/stories/popup.stories.js
+++ docs/stories/popup.stories.js
@@ -1,31 +1,20 @@
 import React from 'react'
-
+import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
 import { boolean, select } from '@storybook/addon-knobs'
-
-import ActionSheet from '@titicaca/action-sheet'
 import Popup from '@titicaca/popup'
-
-import styled from 'styled-components'
 
 const EmptyScroll = styled.div`
   height: 200vh;
 `
 
 storiesOf('Popup', module).add('일반', () => (
-  <>
-    <Popup
-      title="테스트"
-      borderless
-      open={boolean('팝업 열기', false)}
-      icon={select('네비바 아이콘', ['close', 'back'], 'close')}
-    >
-      <EmptyScroll>Scroll........</EmptyScroll>
-    </Popup>
-
-    <ActionSheet open={boolean('액션시트 열림', false)} title="메뉴 액션시트">
-      <ActionSheet.Item>메뉴 1</ActionSheet.Item>
-      <ActionSheet.Item>메뉴 2</ActionSheet.Item>
-    </ActionSheet>
-  </>
+  <Popup
+    title="테스트"
+    borderless
+    open={boolean('팝업 열기', false)}
+    icon={select('네비바 아이콘', ['close', 'back'], 'close')}
+  >
+    <EmptyScroll>Scroll........</EmptyScroll>
+  </Popup>
 ))
