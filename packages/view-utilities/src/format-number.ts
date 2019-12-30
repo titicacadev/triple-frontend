64ac2097e6343cac9f003012bfd7950376209330
박기완 <zprime0920@gmail.com>
2019-12-30 11:23:01 +0900

    [#352] Fix: 누락된 return 추가

--- packages/view-utilities/src/format-number.ts
+++ packages/view-utilities/src/format-number.ts
@@ -2,7 +2,7 @@ export function formatNumber(
   number: number | string | null | undefined,
 ): string {
   if (typeof number === 'number' || typeof number === 'string') {
-    number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
   }
 
   return '0'
