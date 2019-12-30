78bb9221aa91df685e444ee20a8ddf7844591416
박기완 <zprime0920@gmail.com>
2019-12-30 15:48:15 +0900

    [#352] Feature: DatePicker storybook 개선 및 정리
    - knob을 사용해서 스토리 통합
    - 적절한 prop 값이 넘어갈 수 있도록 정리

--- docs/stories/date-picker.stories.js
+++ docs/stories/date-picker.stories.js
@@ -1,62 +1,185 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
-
+import {
+  date,
+  array,
+  text,
+  number,
+  button,
+  boolean,
+} from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 import { DayPicker, RangePicker } from '@titicaca/date-picker'
 
-const today = new Date()
-const endDate = new Date()
-const afterDate = new Date()
-const specificDate = new Date()
-const nowYearMonth = `${new Date().getFullYear()}-${new Date().getMonth() + 1}`
-const publicHolidays = [
-  new Date(`${nowYearMonth}-01`),
-  new Date(`${nowYearMonth}-25`),
-  new Date(`${nowYearMonth}-26`),
-  new Date(`${nowYearMonth}-27`),
-]
+/**
+ * 유효한 날짜인지 확인하는 함수
+ */
+function checkValidDate(date) {
+  return new Date(date).toString() !== 'Invalid Date'
+}
 
-endDate.setDate(today.getDate() + 5)
-afterDate.setDate(today.getDate() + 20)
-specificDate.setDate(today.getDate() + 3)
+/**
+ * ISO8601 날짜 포맷으로 되었는지 확인하는 함수
+ */
+function checkValidISODateFormat(date) {
+  return /^\d{4}-\d{2}-\d{2}$/.test(date)
+}
+
+/**
+ * 한자리수 숫자를 0으로 채워 두자리로 만들어주는 함수
+ */
+function formatPaddedNumber(num) {
+  if (num < 10) {
+    return `0${num}`
+  }
+  return num.toString()
+}
+
+/**
+ * YYYY-MM-DD 형식으로 포맷팅하는 함수
+ */
+function formatDate(date) {
+  return `${date.getFullYear()}-${formatPaddedNumber(
+    date.getMonth() + 1,
+  )}-${formatPaddedNumber(date.getDate())}`
+}
+
+/**
+ * prop에 undefined를 넣을 수 있게 해주는 react hook
+ */
+function useOptionalKnob({
+  name,
+  knob,
+  initialVisibility,
+  initialValue,
+  extraKnobParams,
+}) {
+  return boolean(`${name} 활성화`, initialVisibility || false)
+    ? knob(name, initialValue, ...(extraKnobParams || {}))
+    : undefined
+}
+
+const today = new Date()
+
+const initialAfterBlock = new Date(
+  today.getFullYear(),
+  today.getMonth(),
+  today.getDate() + 20,
+)
+
+const initialDisabledDates = [new Date(), new Date()]
+  .map((date, index) => date.setDate(date.getDate() + index + 1))
+  .map((offset) => new Date(offset))
+  .map(formatDate)
+
+const initialHolidays = [
+  new Date().setDate(1),
+  new Date().setDate(12),
+  new Date().setDate(25),
+]
+  .map((offset) => new Date(offset))
+  .map((date) => formatDate(date))
 
 storiesOf('DatePicker', module)
-  .add('DayPicker - 일반', () => <DayPicker />)
-  .add('DayPicker - 선택됨', () => <DayPicker day={today.toString()} />)
-  .add('DayPicker - 선택불가', () => (
-    <DayPicker
-      day={today.toString()}
-      beforeBlock={today}
-      afterBlock={afterDate}
-      disabledDays={[specificDate.toString()]}
-      height="300px"
-    />
-  ))
-  .add('DayPicker - 휴일', () => (
-    <DayPicker
-      publicHolidays={publicHolidays}
-      day={today.toString()}
-      beforeBlock={today}
-      afterBlock={afterDate}
-      disabledDays={[specificDate.toString()]}
-      height="300px"
-    />
-  ))
-  .add('RangePicker - 일반', () => <RangePicker />)
-  .add('RangePicker - 선택됨', () => (
-    <RangePicker startDate={today.toString()} endDate={endDate.toString()} />
-  ))
-  .add('RangePicker - 선택불가', () => (
-    <RangePicker
-      beforeBlock={today}
-      afterBlock={afterDate}
-      disabledDays={[specificDate.toString()]}
-    />
-  ))
-  .add('RangePicker - 휴일', () => (
-    <RangePicker
-      publicHolidays={publicHolidays}
-      beforeBlock={today}
-      afterBlock={afterDate}
-      disabledDays={[specificDate.toString()]}
-    />
-  ))
+  .add('DayPicker', () => {
+    const [selectedDate, setSelectedDate] = useState(null)
+    const beforeBlock = useOptionalKnob({
+      name: 'beforeBlock',
+      knob: date,
+      initialValue: today,
+    })
+    const afterBlock = useOptionalKnob({
+      name: 'afterBlock',
+      knob: date,
+      initialValue: initialAfterBlock,
+    })
+    const disabledDays = useOptionalKnob({
+      name: 'disabledDays',
+      knob: array,
+      initialValue: initialDisabledDates,
+      extraKnobParams: ['\n'],
+    })
+
+    button('날짜 선택 리셋', () => {
+      setSelectedDate(null)
+    })
+
+    return (
+      <DayPicker
+        day={selectedDate}
+        onDateChange={(date) => {
+          action('날짜 선택')(date)
+          setSelectedDate(date)
+        }}
+        beforeBlock={beforeBlock ? new Date(beforeBlock) : undefined}
+        afterBlock={afterBlock ? new Date(afterBlock) : undefined}
+        disabledDays={
+          disabledDays
+            ? disabledDays
+                .filter(checkValidISODateFormat)
+                .filter(checkValidDate)
+            : undefined
+        }
+        height={text('높이', '300px')}
+        publicHolidays={array('공휴일', initialHolidays, '\n')
+          .filter(checkValidDate)
+          .map((date) => new Date(date))}
+        numberOfMonths={number('표시할 개월 수', 3)}
+      />
+    )
+  })
+  .add('RangePicker', () => {
+    const [{ startDate, endDate }, setDateRange] = useState({
+      startDate: null,
+      endDate: null,
+    })
+
+    const beforeBlock = useOptionalKnob({
+      name: 'beforeBlock',
+      knob: date,
+      initialValue: today,
+    })
+    const afterBlock = useOptionalKnob({
+      name: 'afterBlock',
+      knob: date,
+      initialValue: initialAfterBlock,
+    })
+    const disabledDays = useOptionalKnob({
+      name: 'disabledDays',
+      knob: array,
+      initialValue: initialDisabledDates,
+      extraKnobParams: ['\n'],
+    })
+
+    button('날짜 선택 리셋', () => {
+      setDateRange({
+        startDate: null,
+        endDate: null,
+      })
+    })
+
+    return (
+      <RangePicker
+        startDate={startDate}
+        endDate={endDate}
+        onDatesChange={({ startDate, endDate, nights }) => {
+          action('날짜 선택')({ startDate, endDate, nights })
+          setDateRange({ startDate, endDate })
+        }}
+        beforeBlock={beforeBlock ? new Date(beforeBlock) : undefined}
+        afterBlock={afterBlock ? new Date(afterBlock) : undefined}
+        disabledDays={
+          disabledDays
+            ? disabledDays
+                .filter(checkValidISODateFormat)
+                .filter(checkValidDate)
+            : undefined
+        }
+        publicHolidays={array('공휴일', initialHolidays, '\n')
+          .filter(checkValidDate)
+          .map((date) => new Date(date))}
+        numberOfMonths={number('표시할 개월 수', 3)}
+        height={text('높이', '300px')}
+      />
+    )
+  })
