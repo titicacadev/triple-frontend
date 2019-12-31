9b36bee96cbd07f0128a62643686346ad6bd396b
박기완 <zprime0920@gmail.com>
2019-12-31 15:14:31 +0900

    Fix: 소수부에는 쉼표 찍지 않도록 처리

--- packages/view-utilities/src/format-number.ts
+++ packages/view-utilities/src/format-number.ts
@@ -2,7 +2,16 @@ export function formatNumber(
   number: number | string | null | undefined,
 ): string {
   if (typeof number === 'number' || typeof number === 'string') {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    return number
+      .toString()
+      .split('.')
+      .reduce(
+        (result, current, index) =>
+          index === 0
+            ? current.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : `${result}.${current}`,
+        '',
+      )
   }
 
   return ''
