f35e9dae2f3ea994648b2b02e05ee5a3288b87a4
박기완 <zprime0920@gmail.com>
2020-01-02 10:15:23 +0900

    Refactor: 포매팅 로직 간소화

--- packages/view-utilities/src/format-number.ts
+++ packages/view-utilities/src/format-number.ts
@@ -2,16 +2,10 @@ export function formatNumber(
   number: number | string | null | undefined,
 ): string {
   if (typeof number === 'number' || typeof number === 'string') {
-    return number
-      .toString()
-      .split('.')
-      .reduce(
-        (result, current, index) =>
-          index === 0
-            ? current.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-            : `${result}.${current}`,
-        '',
-      )
+    const [integer, ...fractions] = number.toString().split('.')
+    return [integer.replace(/\B(?=(\d{3})+(?!\d))/g, ','), ...fractions].join(
+      '.',
+    )
   }
 
   return ''
