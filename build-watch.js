6900aded4dd93eadab7813f2816739e6b5931fe0
박기완 <zprime0920@gmail.com>
2020-01-07 16:31:45 +0900

    [#27] Feature: 변화가 일어난 패키지만 빌드하는 스크립트 작성
    dev 스크립트 실행시 해당 스크립트를 실행합니다.

---
+++ build-watch.js
@@ -0,0 +1,60 @@
+const chokidar = require('chokidar')
+const concurrently = require('concurrently')
+
+const BUILD_RESOURCES =
+  'BABEL_ENV=build babel --root-mode upward src --out-dir lib --source-maps --extensions .ts,.tsx,.js --no-comments'
+const BUILD_DECLARATIONS = 'tsc'
+
+const watcher = chokidar.watch('packages/', {
+  ignored: /(node_modules|lib|(^|[/\\])\..)/,
+  persistent: true,
+})
+
+let isReady = false
+
+watcher
+  .on('ready', () => {
+    isReady = true
+  })
+  .on('all', (event, path) => {
+    if (!isReady || !path) {
+      return
+    }
+
+    const [, packageName] = path.match(/packages\/([-a-z]+)\//)
+
+    if (typeof packageName !== 'string') {
+      return
+    }
+
+    console.log(`@titicaca/${packageName} is changed`)
+
+    concurrently(
+      [
+        {
+          command: `lerna exec --scope=@titicaca/${packageName} '${BUILD_RESOURCES}'`,
+          name: 'resources',
+        },
+        {
+          command: `lerna exec --scope=@titicaca/${packageName} ${BUILD_DECLARATIONS}`,
+          name: 'declarations',
+        },
+      ],
+      {
+        prefix: 'name',
+        killOthers: ['failure'],
+      },
+    )
+      .then(() => {
+        console.log(`Built @titicaca/${packageName}`)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  })
+
+process.on('SIGINT', () => {
+  watcher.close().then(() => {
+    console.log('Bye.')
+  })
+})
