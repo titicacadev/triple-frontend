7f8bf810c8ffe9c91f4b63b5d8324b52367d813d
Rhio Kim <rhio.kim@gmail.com>
2020-01-09 15:22:09 +0900

    feat: Add story for numeric-spinner

--- docs/stories/numeric-spinner.stories.js
+++ docs/stories/numeric-spinner.stories.js
@@ -1,15 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
 
 import { NumericSpinner } from '@titicaca/core-elements'
 
-storiesOf('NumericSpinner', module).add('숙박 인원', () => (
-  <NumericSpinner
-    size="big"
-    label="성인"
-    min={1}
-    max={8}
-    value={2}
-    padding={{ top: 15, right: 125, bottom: 15, left: 16 }}
-  />
-))
+storiesOf('NumericSpinner', module)
+  .add('숙박 인원', () => (
+    <NumericSpinner
+      size="big"
+      label="성인"
+      min={1}
+      max={8}
+      value={2}
+      padding={{ top: 15, right: 125, bottom: 15, left: 16 }}
+    />
+  ))
+  .add('스타일 변경', () => {
+    const NewStyledNumericSpinner = styled(NumericSpinner)`
+      > div:first-child {
+        color: red;
+      }
+    `
+    return (
+      <NewStyledNumericSpinner
+        size="big"
+        label="성인"
+        min={1}
+        max={8}
+        value={2}
+        padding={{ top: 15, right: 125, bottom: 15, left: 16 }}
+      ></NewStyledNumericSpinner>
+    )
+  })
