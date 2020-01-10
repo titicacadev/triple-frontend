ea7434f50f49ffbe474e975cca076b65069b1370
steve <steve@triple-corp.com>
2020-01-10 13:25:17 +0900

    [#395] popup docs에 기능을 추가합니다.

--- docs/stories/popup.stories.js
+++ docs/stories/popup.stories.js
@@ -15,7 +15,8 @@ storiesOf('Popup', module)
       title={text('팝업 제목', '테스트')}
       noNavbar={boolean('Navbar 숨김', false)}
       borderless={boolean('네비바 경계선 없음', true)}
-      open={boolean('팝업 열기', false)}
+      open={boolean('팝업 열기', true)}
+      onClose={boolean('팝업 닫기', false)}
       icon={select('네비바 아이콘', ['close', 'back'], 'close')}
     >
       <EmptyScroll>Scroll........</EmptyScroll>
