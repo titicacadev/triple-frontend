46b5bb097957b44e48a654a2aeb7c3954f758751
appear <appear.ko@gmail.com>
2020-01-14 11:42:17 +0900

    pricing rich 에서도 description 을 사용할 수 있도록 추가

--- docs/stories/pricing.stories.js
+++ docs/stories/pricing.stories.js
@@ -4,20 +4,45 @@ import { withKnobs, boolean, text, number } from '@storybook/addon-knobs'
 import Pricing from '@titicaca/pricing'
 import { Container, Text } from '@titicaca/core-elements'
 
+function PricingDescription() {
+  return (
+    <Container>
+      <Text size="tiny" inlineBlock margin={{ right: 3 }}>
+        쿠폰 적용시
+      </Text>
+      <Text size="tiny" inlineBlock>
+        270,500원
+      </Text>
+    </Container>
+  )
+}
+
 storiesOf('Pricing', module)
   .addDecorator(withKnobs)
   .add('일반', () => <Pricing basePrice={30000} salePrice={25000} />)
-  .add('Rich', () => (
-    <Pricing
-      basePrice={number('basePrice', 30000)}
-      pricingNote={text('문구', '1박, 세금포함')}
-      salePrice={25000}
-      label="트리플가"
-      rich
-    />
-  ))
+  .add('Rich', () => {
+    const useStringDescription = boolean('문자 타입의 설명')
+
+    return (
+      <Pricing
+        basePrice={number('basePrice', 30000)}
+        pricingNote={text('문구', '1박, 세금포함')}
+        salePrice={25000}
+        label="트리플가"
+        rich
+        description={
+          useStringDescription ? (
+            text('설명', '쿠폰적용시 10,000원')
+          ) : (
+            <PricingDescription />
+          )
+        }
+      />
+    )
+  })
   .add('Fixed', () => {
     const useStringDescription = boolean('문자 타입의 설명')
+    const useStringLabel = boolean('문자 타입의 라벨')
 
     return (
       <Pricing
@@ -25,20 +50,19 @@ storiesOf('Pricing', module)
         active={boolean('열림', true)}
         basePrice={30000}
         salePrice={25000}
-        label={text('라벨', '1박 세금포함')}
+        label={
+          useStringLabel ? (
+            text('라벨', '1박 세금포함')
+          ) : (
+            <Text size="mini">1박 세금포함</Text>
+          )
+        }
         buttonText="객실예약"
         description={
           useStringDescription ? (
             text('설명', '쿠폰적용시 10,000원')
           ) : (
-            <Container>
-              <Text size="tiny" inlineBlock margin={{ right: 3 }}>
-                쿠폰 적용시
-              </Text>
-              <Text size="tiny" inlineBlock>
-                270,500원
-              </Text>
-            </Container>
+            <PricingDescription />
           )
         }
       />
