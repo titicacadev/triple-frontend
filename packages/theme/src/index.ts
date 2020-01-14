b1a18abd78f8a9d3ab6ea9a096bb5cd90dd5a6a7
appear <appear.ko@gmail.com>
2020-01-14 14:09:49 +0900

    interface 를 export 합니다

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,3 +1,4 @@
 export { default as ThemeProvider } from './theme-provider'
 export { default as dark } from './themes/dark'
 export { default as light } from './themes/light'
+export * from './interfaces'
