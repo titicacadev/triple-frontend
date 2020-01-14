881c41363b434174d9f9820d98ea6d80098d2230
woody <giwan.dev@gmail.com>
2020-01-14 13:46:12 +0900

    Feature: IntersectionObserver에 safe prop 추가
    Experimental
    ssr: false 처리한 컴포넌트를 렌더링하는 prop을 추가합니다.

--- packages/intersection-observer/src/index.tsx
+++ packages/intersection-observer/src/index.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 import { Props } from '@researchgate/react-intersection-observer'
 
-const OriginalObserver = dynamic(async () => {
+interface IntersectionObserverProps extends Props {
+  safe?: boolean
+}
+
+async function importReactIntersectionObserver() {
   try {
     if (
       typeof window !== 'undefined' &&
@@ -24,8 +28,17 @@ const OriginalObserver = dynamic(async () => {
       Props
     >)
   }
+}
+
+const Observer = dynamic(importReactIntersectionObserver)
+
+const SafeObserver = dynamic(importReactIntersectionObserver, {
+  ssr: false,
 })
 
-export default function IntersectionObserver(props: Props) {
-  return <OriginalObserver {...props} />
+export default function IntersectionObserver({
+  safe,
+  ...props
+}: IntersectionObserverProps) {
+  return safe ? <SafeObserver {...props} /> : <Observer {...props} />
 }
