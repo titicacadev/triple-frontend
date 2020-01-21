e3e6384eaa3450cc4266a464d7aa8f21301e49d8
Rhio Kim <rhio.kim@gmail.com>
2020-01-21 11:04:07 +0900

    refactor: mediaQuery 관련 로직 제거

--- packages/core-elements/src/mixins/text-style.ts
+++ packages/core-elements/src/mixins/text-style.ts
@@ -1,26 +1,6 @@
 import { css } from 'styled-components'
 
 /**
- * TODO:
- * - [ ] TF/Theme 가 설정되면 breakpoints, device 와 같은 const 는 theme 로 이동
- */
-
-/**
- * chrome dev tool 기준
- */
-const breakpoints = {
-  mobileM: '375px',
-  mobileL: '425px',
-  tablet: '768px',
-  laptop: '1024px',
-  laptopL: '1440px',
-}
-
-const device = {
-  laptop: `(min-width: ${breakpoints.laptop})`,
-}
-
-/**
  * Create text style css
  * @param fontSize font-size
  * @param lineHeight line-height
@@ -37,14 +17,6 @@ export const textStyle = (
     letter-spacing: ${letterSpacing}px;
   `
 
-export const mediaQuery = (style: any, fontSize: number) => css`
-  ${style}
-
-  @media ${device.laptop} {
-    font-size: ${fontSize}px;
-  }
-`
-
 export const TextStyleMap = {
   /* 가계부 금액 */
   L6: textStyle(36, 47, -0.3),
@@ -53,21 +25,21 @@ export const TextStyleMap = {
   /* 쿠폰 금액 */
   M6: textStyle(28, 35, -0.3),
   /* 타이틀 */
-  M4: mediaQuery(textStyle(24, 33, -0.3), 32),
+  M4: textStyle(24, 33, -0.3),
   /* 금액 가격 등 숫자 */
   M2: textStyle(22, 31, -0.2),
   /* 서비스메인 카드 타이틀 */
-  M1: mediaQuery(textStyle(21, 29, -0.2), 28),
+  M1: textStyle(21, 29, -0.2),
   /* 목록, 타이틀 */
   M: textStyle(20, 24, -0.2),
   /* 서비스메인 타이틀 */
-  S9: mediaQuery(textStyle(19, 23, -0.2), 22),
+  S9: textStyle(19, 23, -0.2),
   /* 서비스 메인 타이틀 */
   S8: textStyle(18, 21, -0.2),
   /* 상단 네비게이션바 타이틀 */
   S7: textStyle(17, 20, -0.1),
   /* 본문, 도시메인 카드 타이틀, POI 타이틀, 최근검색어 */
-  S6: mediaQuery(textStyle(16, 19, -0.1), 18),
+  S6: textStyle(16, 19, -0.1),
   /* 본문 */
   S5: textStyle(15, 19, -0.1),
   /* 본문, 날짜 */
@@ -75,7 +47,7 @@ export const TextStyleMap = {
   /* 도시메인카드 본문, 서브 설명 */
   S3: textStyle(13, 23, 0),
   /* 부가설명, 일정판 요일 */
-  S2: mediaQuery(textStyle(12, 22, 0), 14),
+  S2: textStyle(12, 22, 0),
 }
 
 export function getTextStyle(type: KeyOfTextStyleMap) {
