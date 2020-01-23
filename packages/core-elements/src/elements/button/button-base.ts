58db870c3db065ae6b1649ccef4c19e884fc1320
woody <woody@triple-corp.com>
2020-01-23 14:45:06 +0900

    [#400] Refactor: Button 컴포넌트 파일 정리
    ButtonContainer에서 ButtonBase:first-child 스타일을 지정하고 있어
    ButtonGroup에서 다시 ButtonBase의 스타일을 지정하려고 하니
    specificity 순서가 잘못되었다는 린트 에러가 났습니다.
    && 등의 방법으로 우회가능하지만 이 기회에 파일 자체를 나누는 게 좋겠다고 판단헸습니다.

---
+++ packages/core-elements/src/elements/button/button-base.ts
@@ -0,0 +1,67 @@
+import styled, { css } from 'styled-components'
+import * as CSS from 'csstype'
+
+import { GlobalSizes, MarginPadding, GetGlobalColor } from '../../commons'
+import { marginMixin } from '../../mixins'
+
+export interface ButtonBaseProp {
+  size?: GlobalSizes
+  bold?: boolean
+  textColor?: string
+  textAlpha?: number
+  floated?: CSS.FloatProperty
+  fluid?: boolean
+  margin?: MarginPadding
+  disabled?: boolean
+}
+
+const SIZES: Partial<Record<GlobalSizes, ReturnType<typeof css>>> = {
+  tiny: css`
+    font-size: 13px;
+    line-height: 16px;
+  `,
+  small: css`
+    font-size: 14px;
+    line-height: 17px;
+  `,
+  large: css`
+    font-size: 16px;
+  `,
+}
+
+const ButtonBase = styled.a<ButtonBaseProp>`
+  display: inline-block;
+  ${({ size }) => (size ? SIZES[size] : '')}
+  font-weight: ${({ bold }) => (bold ? 'bold' : 500)};
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  box-sizing: border-box;
+  cursor: pointer;
+
+  &:active {
+    border-style: solid;
+  }
+
+  color: ${({ textColor = 'gray', textAlpha = 1 }) =>
+    `rgba(${GetGlobalColor(textColor)}, ${textAlpha})`};
+
+  float: ${({ floated }) => floated || 'none'};
+
+  ${({ fluid }) =>
+    fluid &&
+    css`
+      width: 100%;
+      display: block;
+    `};
+
+  ${marginMixin}
+
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      opacity: 0.3;
+    `};
+`
+
+export default ButtonBase
---
+++ packages/core-elements/src/elements/button/button-group.ts
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+import Container from '../container'
+import ButtonBase from './button-base'
+
+const ButtonGroup = styled(Container)<{
+  horizontalGap?: number
+  children?: React.ReactNode
+}>`
+  width: 100%;
+
+  ${ButtonBase} {
+    ${({ horizontalGap = 0, children }) => {
+      const childrenCount = React.Children.count(children)
+
+      return horizontalGap > 0
+        ? css`
+            width: ${childrenCount > 0
+              ? `calc((100% - ${(childrenCount - 1) *
+                  horizontalGap}px) / ${childrenCount})`
+              : '100%'};
+          `
+        : css`
+            width: ${100 / childrenCount}%;
+          `
+    }};
+
+    &:not(:first-child) {
+      ${({ horizontalGap }) => css`
+        margin-left: ${horizontalGap || 0}px;
+      `}
+    }
+  }
+`
+
+export default ButtonGroup
