02687a49a31f360d4027c295f0d79ee56f51bcfa
appear <appear.ko@gmail.com>
2020-01-29 11:41:58 +0900

    as ColorString 를 제거합니다

--- packages/color-palette/src/get-color.ts
+++ packages/color-palette/src/get-color.ts
@@ -3,5 +3,5 @@ import { ColorSet } from './colors'
 type ColorString = keyof typeof ColorSet
 
 export function getColor(color: ColorString): string {
-  return ColorSet[color as ColorString]
+  return ColorSet[color]
 }
