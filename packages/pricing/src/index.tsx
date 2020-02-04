a6d83f06413c254bdaa38db03cbd0b299fbd0774
woody <woody@triple-corp.com>
2020-02-04 11:01:48 +0900

    Refactor: FixedPricing 컴포넌트 분리

--- packages/pricing/src/index.tsx
+++ packages/pricing/src/index.tsx
@@ -4,13 +4,11 @@ import { formatNumber } from '@titicaca/view-utilities'
 import {
   Container,
   Text,
-  Drawer,
   GlobalSizes,
-  GlobalColors,
   MarginPadding,
-  Tooltip,
-  Button,
+  GlobalColors,
 } from '@titicaca/core-elements'
+import FixedPricing, { FixedPricingProps } from './fixed-pricing'
 
 export type BasePrice = number | null
 
@@ -25,18 +23,6 @@ interface RichPricingProps {
   label?: React.ReactNode
   pricingNote?: string
   description?: React.ReactNode
-}
-
-interface FixedPricingProps {
-  active?: boolean
-  label?: React.ReactNode
-  description?: React.ReactNode
-  buttonText?: string
-  buttonDisabled?: boolean
-  salePrice: number
-  tooltipLabel?: string
-  onClick?: (e?: React.SyntheticEvent) => any
-  onTooltipClick?: (e?: React.SyntheticEvent) => any
 }
 
 type PricingProps =
@@ -205,101 +191,6 @@ const RegularPricing = ({ basePrice, salePrice }: RegularPricingProps) => {
         {formatNumber(salePrice)}원
       </Price>
     </PricingContainer>
-  )
-}
-
-const FloatedFrame = styled(Container)`
-  border-top: 1px solid #efefef;
-  background: #fff;
-  @supports (padding: max(0px)) and (padding: env(safe-area-inset-bottom)) {
-    padding-bottom: max(14px, env(safe-area-inset-bottom, 14px));
-  }
-`
-
-const FloatedPricingContainer = styled(Container)`
-  width: 50%;
-`
-
-const PurchaseButton = styled(Button)`
-  position: absolute;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-  width: 41%;
-  height: 47px;
-`
-
-function FixedPricing({
-  active,
-  label,
-  buttonText,
-  buttonDisabled,
-  description,
-  salePrice,
-  tooltipLabel,
-  onClick,
-  onTooltipClick,
-}: FixedPricingProps) {
-  const pricingLabel = label ? (
-    typeof label === 'string' ? (
-      <Text color="gray" alpha={0.5} size="mini">
-        {label}
-      </Text>
-    ) : (
-      label
-    )
-  ) : null
-
-  const pricingDescription = description ? (
-    typeof description === 'string' ? (
-      <Text size="mini" alpha={0.5} margin={{ top: 1 }}>
-        {description}
-      </Text>
-    ) : (
-      description
-    )
-  ) : null
-
-  return (
-    <Drawer active={active} overflow="visible">
-      <FloatedFrame
-        padding={{
-          top: 14,
-          right: 20,
-          bottom: 14,
-          left: 20,
-        }}
-      >
-        {active && tooltipLabel && (
-          <Tooltip
-            borderRadius="30"
-            positioning={{ top: -20 }}
-            label={tooltipLabel}
-            onClick={onTooltipClick}
-          />
-        )}
-
-        <Container position="relative" clearing>
-          <FloatedPricingContainer floated="left">
-            {pricingLabel}
-            <Text size="huge" bold>
-              {formatNumber(salePrice)}원
-            </Text>
-            {pricingDescription}
-          </FloatedPricingContainer>
-
-          <PurchaseButton
-            borderRadius={4}
-            size="small"
-            color="blue"
-            disabled={buttonDisabled}
-            onClick={onClick}
-          >
-            {buttonText}
-          </PurchaseButton>
-        </Container>
-      </FloatedFrame>
-    </Drawer>
   )
 }
 
