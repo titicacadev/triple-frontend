8f6d67c04401fe04e8b1a07467838ce71808ac56
woody <woody@triple-corp.com>
2020-02-07 11:12:26 +0900

    [#459] Feature: SingleSlider 구현

--- docs/stories/slider.stories.js
+++ docs/stories/slider.stories.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 
-import { RangeSlider } from '@titicaca/slider'
+import { SingleSlider, RangeSlider } from '@titicaca/slider'
 import { Text } from '@titicaca/core-elements'
 
-function Label({ fromValue, toValue }) {
+function SingleLabel({ value }) {
+  return <Text size="large">컴포넌트 내부: {value}</Text>
+}
+
+function RangeLabel({ fromValue, toValue }) {
   return (
     <Text size="large">
       컴포넌트 내부: {fromValue} ~ {toValue}
@@ -14,31 +18,49 @@ function Label({ fromValue, toValue }) {
 
 storiesOf('Slider', module)
   .add('기본', () => {
-    const [values, setValues] = useState([])
+    const [value, setValue] = useState(500000)
+
+    return (
+      <div style={{ height: '4000px', padding: '500px 20px 0 20px' }}>
+        컴포넌트 외부: {value}
+        <SingleSlider
+          initialValue={value}
+          min={0}
+          max={500000}
+          onChange={setValue}
+          labelComponent={SingleLabel}
+        />
+      </div>
+    )
+  })
+  .add('범위', () => {
+    const [values, setValues] = useState([0, 500000])
 
     return (
       <div style={{ height: '4000px', padding: '500px 20px 0 20px' }}>
         컴포넌트 외부: {values.join(', ')}
         <RangeSlider
+          initialValues={values}
           min={0}
           max={500000}
           onChange={setValues}
-          labelComponent={Label}
+          labelComponent={RangeLabel}
         />
       </div>
     )
   })
   .add('Non linear', () => {
-    const [values, setValues] = useState([])
+    const [values, setValues] = useState([0, 500000])
 
     return (
       <div style={{ height: '4000px', padding: '500px 20px 0 20px' }}>
         컴포넌트 외부: {values.join(', ')}
         <RangeSlider
+          initialValues={values}
           min={0}
           max={500000}
           onChange={setValues}
-          labelComponent={Label}
+          labelComponent={RangeLabel}
           nonLinear
         />
       </div>
