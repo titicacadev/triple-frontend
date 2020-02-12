fab9dd83bdb5c9d52af967f40c497393aca6f8cd
appear <appear.ko@gmail.com>
2020-02-12 17:15:20 +0900

    alert, confirm children 타입을 변경합니다

--- packages/modals/src/body.tsx
+++ packages/modals/src/body.tsx
@@ -6,7 +6,7 @@ export default function ModalBody({
   description,
 }: {
   title?: string
-  description?: string
+  description?: React.ReactNode
 }) {
   return (
     <Container padding={{ top: 40, bottom: 40, left: 30, right: 30 }}>
