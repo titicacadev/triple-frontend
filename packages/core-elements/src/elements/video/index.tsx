8b874c8843a25ef63901d253182f66539f68e1d6
Inbeom Hwang <hwanginbeom@gmail.com>
2020-02-22 02:50:49 +0900

    Implement video controls

--- packages/core-elements/src/elements/video/index.tsx
+++ packages/core-elements/src/elements/video/index.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react'
+import React, { useRef, useState, useCallback, useEffect } from 'react'
 import styled from 'styled-components'
 
 import { GlobalSizes, MEDIA_FRAME_OPTIONS } from '../../commons'
 import Sources from './sources'
+import Controls from './controls'
 
 const VideoContainer = styled.div<
   { frame: GlobalSizes; fallbackImageUrl: string } & React.HTMLAttributes<
@@ -27,6 +28,19 @@ const VideoFrame = styled.video`
   object-fit: cover;
 `
 
+const Pending = styled.div`
+  position: absolute;
+  border: none;
+  background: none;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: yellow;
+`
+
 export default function Video({
   src,
   srcType = 'video/mp4',
@@ -44,19 +58,41 @@ export default function Video({
   frame: GlobalSizes
   autoPlay?: boolean
 }) {
+  const videoRef = useRef<HTMLVideoElement>(null)
+  const [pending, setPending] = useState(true)
+
+  const handlePending = useCallback(() => setPending(true), [setPending])
+  const handleReady = useCallback(() => setPending(false), [setPending])
+
+  useEffect(() => {
+    const currentRef = videoRef.current
+
+    if (currentRef) {
+      currentRef.addEventListener('canplaythrough', handleReady)
+      currentRef.addEventListener('waiting', handlePending)
+    }
+
+    return () => {
+      if (currentRef) {
+        currentRef.removeEventListener('canplaythrough', handleReady)
+        currentRef.removeEventListener('waiting', handlePending)
+      }
+    }
+  }, [videoRef, handleReady, handlePending])
+
   return (
     <VideoContainer frame={frame} fallbackImageUrl={fallbackImageUrl}>
-      <VideoFrame autoPlay={autoPlay} loop muted>
-        {cloudinaryBucket && cloudinaryId ? (
-          <Sources
-            cloudinaryBucket={cloudinaryBucket}
-            cloudinaryId={cloudinaryId}
-            frame={frame}
-          />
-        ) : (
-          <source src={src} type={srcType} />
-        )}
+      <VideoFrame autoPlay={autoPlay} loop muted ref={videoRef}>
+        <Sources
+          src={src}
+          srcType={srcType}
+          cloudinaryBucket={cloudinaryBucket}
+          cloudinaryId={cloudinaryId}
+          frame={frame}
+        />
       </VideoFrame>
+      {pending && <Pending />}
+      {videoRef && <Controls videoRef={videoRef} />}
     </VideoContainer>
   )
 }
---
+++ packages/core-elements/src/elements/video/seeker.tsx
@@ -0,0 +1,106 @@
+import React, { useState, useCallback, forwardRef } from 'react'
+import styled from 'styled-components'
+
+import { debounce } from '@titicaca/view-utilities'
+
+import { GetGlobalColor } from '../../commons'
+
+const SeekerBase = styled.input<{ handleVisible: boolean }>`
+  appearance: none;
+  background: transparent;
+  border-color: transparent;
+  color: transparent;
+  position: absolute;
+  left: 45px;
+  right: 45px;
+  width: calc(100% - 90px);
+  bottom: 10px;
+  margin: 0;
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-webkit-slider-thumb {
+    appearance: none;
+    width: 13px;
+    height: 13px;
+    border-radius: 10px;
+    background-color: rgba(${GetGlobalColor('blue')}, 1);
+    cursor: pointer;
+    opacity: ${({ handleVisible }) => (handleVisible ? '1' : '0')};
+    transition: opacity 0.3s;
+  }
+
+  &::-moz-range-thumb {
+    width: 13px;
+    height: 13px;
+    border-radius: 10px;
+    background-color: rgba(${GetGlobalColor('blue')}, 1);
+    cursor: pointer;
+    opacity: ${({ handleVisible }) => (handleVisible ? '1' : '0')};
+    transition: opacity 0.3s;
+  }
+
+  &::-ms-thumb {
+    width: 13px;
+    height: 13px;
+    border-radius: 10px;
+    background-color: rgba(${GetGlobalColor('blue')}, 1);
+    cursor: pointer;
+    opacity: ${({ handleVisible }) => (handleVisible ? '1' : '0')};
+    transition: opacity 0.3s;
+  }
+
+  &:hover {
+    &::-webkit-slider-thumb {
+      opacity: 1;
+    }
+  }
+`
+
+export default forwardRef(function Seeker(
+  {
+    duration,
+    visible,
+    onClick,
+    onChange,
+  }: {
+    duration: number
+    visible: boolean
+    onClick: (e: React.SyntheticEvent) => void
+    onChange: (e: React.SyntheticEvent) => void
+  },
+  ref: React.Ref<HTMLInputElement>,
+) {
+  const [handleVisible, setHandleVisible] = useState(false)
+
+  const handleHandleFadeOut = useCallback(
+    debounce(() => setHandleVisible(false), 500),
+    [setHandleVisible],
+  )
+
+  const handleChange = useCallback(
+    (e: React.SyntheticEvent) => {
+      if (visible) {
+        setHandleVisible(true)
+        onChange(e)
+        handleHandleFadeOut()
+      }
+    },
+    [visible, onChange, setHandleVisible, handleHandleFadeOut],
+  )
+
+  return (
+    <SeekerBase
+      handleVisible={handleVisible}
+      ref={ref}
+      type="range"
+      max={duration}
+      min={0}
+      step={0.01}
+      onClick={visible ? onClick : undefined}
+      onChange={handleChange}
+    />
+  )
+})
