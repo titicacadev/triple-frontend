9065232e861b21c8659c1732084e4d110d6cc47b
appear <appear.ko@gmail.com>
2020-02-27 12:08:44 +0900

    9:5 frame size 를 추가합니다

--- packages/core-elements/src/commons.ts
+++ packages/core-elements/src/commons.ts
@@ -29,30 +29,41 @@ export type GlobalColors = 'blue' | 'gray' | 'white' | 'red'
 export function GetGlobalColor(colorString: GlobalColors | string) {
   return GlobalColorSet[colorString as GlobalColors] || colorString // HACK: GlobalColors가 아닌 경우 colorString을 그대로 반환하게 되므로 에러 없음
 }
-export type Ratio = '4:1' | '5:3' | '11:7' | '4:3' | '1:1' | '10:11' | '5:8'
+export type Ratio =
+  | '4:1'
+  | '5:3'
+  | '11:7'
+  | '4:3'
+  | '1:1'
+  | '10:11'
+  | '5:8'
+  | '9:5'
 
 export type FrameRatioAndSizes =
   | Exclude<GlobalSizes, 'tiny' | 'massive'>
   | Ratio
 
-const ratio = ['25%', '60%', '63.64%', '75%', '100%', '110%', '160%']
+const ratio = {
+  '4:1': '25%',
+  '9:5': '55.56%',
+  '5:3': '60%',
+  '11:7': '63.64%',
+  '4:3': '75%',
+  '1:1': '100%',
+  '10:11': '110%',
+  '5:8': '160%',
+}
 
 export const MEDIA_FRAME_OPTIONS: {
   [key in FrameRatioAndSizes]: string
 } = {
-  mini: ratio[0],
-  small: ratio[1],
-  medium: ratio[3],
-  large: ratio[4],
-  big: ratio[5],
-  huge: ratio[6],
-  '4:1': ratio[0],
-  '5:3': ratio[1],
-  '11:7': ratio[2],
-  '4:3': ratio[3],
-  '1:1': ratio[4],
-  '10:11': ratio[5],
-  '5:8': ratio[6],
+  mini: ratio['4:1'],
+  small: ratio['5:3'],
+  medium: ratio['4:3'],
+  large: ratio['1:1'],
+  big: ratio['10:11'],
+  huge: ratio['5:8'],
+  ...ratio,
 }
 
 export type CarouselSizes = Exclude<
