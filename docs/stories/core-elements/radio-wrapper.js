e996a0f519a93989833653553c4ee2112d001b4f
woody <woody@triple-corp.com>
2020-02-28 14:33:24 +0900

    Feature: Radio 컴포넌트 새로운 UI와 구조 구현

---
+++ docs/stories/core-elements/radio-wrapper.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+import { Radio } from '@titicaca/core-elements'
+
+export default function RadioWrapper() {
+  const [value, setValue] = useState(undefined)
+
+  return (
+    <Radio
+      value={value}
+      name="지역_선택"
+      options={[
+        {
+          text: '전 지역',
+          value: null,
+        },
+        {
+          text: '신주쿠',
+          value: '2',
+        },
+        {
+          text: '우에노',
+          value: '3',
+        },
+        {
+          text: '긴자',
+          value: '4',
+        },
+      ]}
+      onChange={(e, value) => setValue(value)}
+    />
+  )
+}
--- packages/core-elements/src/elements/radio.tsx
+++ packages/core-elements/src/elements/radio.tsx
@@ -3,84 +3,69 @@ import * as React from 'react'
 import Text from './text'
 import styled, { css } from 'styled-components'
 import { withField } from '../utils/form-field'
-import { GetGlobalColor } from '../commons'
 
-interface Option {
+interface Option<V> {
   text: string
-  value: string
+  value: V
 }
 
-const RadioFrame = styled.div.attrs<{ name?: string }>({})`
+const RadioFrame = styled.div`
   position: relative;
-  padding: 11px 35px 11px 12px;
-  border: 1px solid rgba(${GetGlobalColor('gray')}, 0.1);
-  border-radius: 2px;
-  margin-bottom: 10px;
-  box-sizing: border-box;
+  margin-bottom: 20px;
+  line-height: 26px;
 
   &:last-child {
     margin-bottom: 0;
   }
 `
 
-const RadioContainer = styled.div.attrs({})<{ name?: string }>`
-  display: table;
-  width: 100%;
-`
-
-const Cell = styled.div<{ width: number }>`
-  width: ${({ width }) => width || 100}%;
-  display: table-cell;
-  vertical-align: middle;
-  word-break: break-all;
-`
-
-const Icon = styled.span<{ selected?: boolean }>`
-  opacity: 0.5;
-  display: inline-block;
-  width: 22px;
-  height: 22px;
-  margin-top: 2px;
-  background-size: 22px 22px;
+const RadioInput = styled.input.attrs({ type: 'radio' })<{
+  selected?: boolean
+}>`
+  float: right;
+  margin: 0;
+  width: 26px;
+  height: 26px;
+  border: 0;
+  padding: 0;
+  background-size: 26px 26px;
   background-repeat: no-repeat;
-  background-position: -1px -1px;
-  background-image: url('https://assets.triple.guide/images/radio-off@2x.png');
-  vertical-align: bottom;
+  transition: all 0.3s ease;
+  appearance: none;
+  outline: none;
 
   ${({ selected }) =>
-    selected &&
-    css`
-      opacity: 1;
-      background-image: url('https://assets.triple.guide/images/radio-on@2x.png');
-      transition: all 0.3s ease;
-    `};
+    selected
+      ? css`
+          opacity: 1;
+          background-image: url('https://assets.triple-dev.titicaca-corp.com/images/btn-filter-radio-check.svg');
+        `
+      : css`
+          opacity: 0.5;
+          background-image: url('https://assets.triple-dev.titicaca-corp.com/images/btn-filter-radio.svg');
+        `};
 `
 
-interface RadioProps {
+interface RadioProps<V> {
   name?: string
-  value?: string
-  onChange?: (e: React.SyntheticEvent, arg1: string) => any
-  options: Option[]
+  value?: V
+  onChange?: (e: React.SyntheticEvent, value: V) => void
+  options: Option<V>[]
 }
 
-function Radio({ name, value, onChange, options }: RadioProps) {
+function Radio<V>({ name, value, onChange, options }: RadioProps<V>) {
   return (
     <>
       {options.map(({ text, value: optionValue }, idx) => (
-        <RadioFrame key={idx}>
-          <RadioContainer
-            name={name}
-            onClick={(e) => onChange && onChange(e, optionValue)}
-          >
-            <Cell width={10}>
-              <Icon selected={value === optionValue} />
-            </Cell>
-            <Cell width={90}>
-              <Text size="small" lineHeight={1.45}>
-                {text}
-              </Text>
-            </Cell>
-          </RadioContainer>
+        <RadioFrame
+          key={idx}
+          onClick={(e) => onChange && onChange(e, optionValue)}
+        >
+          <Text inlineBlock size="large">
+            {text}
+          </Text>
+
+          <RadioInput name={name} selected={value === optionValue} />
         </RadioFrame>
       ))}
     </>
