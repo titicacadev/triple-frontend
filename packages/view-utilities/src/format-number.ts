dc0bc6a36f8af4bb385ac123a292dc43f5e6b65b
yceffort <yceffort@gmail.com>
2020-03-02 14:31:42 +0900

    [#372] 🔨use regex in formatnumber

--- packages/view-utilities/src/format-number.ts
+++ packages/view-utilities/src/format-number.ts
@@ -2,10 +2,7 @@ export function formatNumber(
   number: number | string | null | undefined,
 ): string {
   if (typeof number === 'number' || typeof number === 'string') {
-    const [integer, ...fractions] = number.toString().split('.')
-    return [integer.replace(/\B(?=(\d{3})+(?!\d))/g, ','), ...fractions].join(
-      '.',
-    )
+    return number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')
   }
 
   return ''
