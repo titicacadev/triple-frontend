565550adb2c40de99796a1fe80d10493958a2a91
appear <appear.ko@gmail.com>
2020-03-02 09:37:27 +0900

    style-box mdx 문서 추가

--- docs/stories/style-box/components/container.ts
+++
@@ -1,8 +0,0 @@
-import { margin, padding, ellipsis } from '@titicaca/style-box'
-import styled from 'styled-components'
-
-export default styled.div`
-  ${margin()}
-  ${padding()}
-  ${ellipsis()}
-`
