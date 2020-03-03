5bed8ebfdfdb34e489e10a54b23eab868761e465
luffy <luffy@triple-corp.com>
2020-03-03 13:48:14 +0900

    remove click event handlers

--- packages/review/src/reviews-list.tsx
+++ packages/review/src/reviews-list.tsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react'
-import qs from 'qs'
-import moment from 'moment'
 import { List, MarginPadding } from '@titicaca/core-elements'
 import {
   useHistoryContext,
   useUserAgentContext,
   useEventTrackingContext,
 } from '@titicaca/react-contexts'
-import { useTransitionModal, TransitionType } from '@titicaca/modals'
 
 import ReviewElement, { ReviewElementProps } from './review-element'
 import ReviewTimestamp from './review-timestamp'
@@ -17,34 +14,29 @@ import OthersReviewActionSheet, {
 } from './others-review-action-sheet'
 import { likeReview, unlikeReview } from './review-api-clients'
 import { useReviewLikesContext } from './review-likes-context'
-import {
-  ResourceType,
-  AppNativeActionProps,
-  ReviewData,
-  ImageEntity,
-} from './types'
+import { AppNativeActionProps, ReviewData } from './types'
 
 export default function ReviewsList({
   myReview,
   reviews,
+  regionId,
   fetchNext,
   appUrlScheme,
   margin,
-  resourceType,
   resourceId,
-  regionId,
   maxLength,
+  reviewRateDescriptions,
   showToast,
 }: {
   myReview?: ReviewData
   reviews: ReviewData[]
   fetchNext?: () => void
+  regionId: string
   appUrlScheme: string
   margin: MarginPadding
-  resourceType: ResourceType
   resourceId: string
-  regionId: string
   maxLength?: number
+  reviewRateDescriptions?: string[]
   showToast: AppNativeActionProps['showToast']
 }) {
   const [selectedReview, setSelectedReview] = useState<ReviewData | undefined>(
@@ -53,8 +45,7 @@ export default function ReviewsList({
   const { isPublic } = useUserAgentContext()
   const { trackEvent } = useEventTrackingContext()
   const { updateLikedStatus } = useReviewLikesContext()
-  const { navigate, push } = useHistoryContext()
-  const { show } = useTransitionModal()
+  const { push } = useHistoryContext()
 
   const handleUserClick: ReviewElementProps['onUserClick'] = (
     e,
@@ -93,21 +84,6 @@ export default function ReviewsList({
     }
   }
 
-  const handleLikesCountClick: ReviewElementProps['onLikesCountClick'] = (
-    e,
-    { id },
-  ) => {
-    if (isPublic) {
-      return
-    }
-
-    navigate(
-      `${appUrlScheme}:///inlink?path=${encodeURIComponent(
-        `/reviews/thanks?_triple_no_navbar&region_id=${regionId}&resource_id=${resourceId}&resource_type=${resourceType}&review_id=${id}`,
-      )}`,
-    )
-  }
-
   const handleMenuClick: ReviewElementProps['onMenuClick'] = (e, review) => {
     if (!isPublic) {
       if (myReview && review.id === myReview.id) {
@@ -117,40 +93,6 @@ export default function ReviewsList({
         push(HASH_REVIEW_ACTION_SHEET)
       }
     }
-  }
-
-  const handleImageClick: ReviewElementProps['onImageClick'] = (
-    e,
-    { user: { name }, comment, media, createdAt },
-    image,
-  ) => {
-    if (isPublic) {
-      return show(TransitionType.ReviewThumbnail)
-    }
-
-    const convertImage = (convertingImage: ImageEntity) => ({
-      id: convertingImage.id,
-      title: '',
-      description: (comment || '').replace(/\n\s*\n/g, '\n'),
-      width: convertingImage.width,
-      height: convertingImage.height,
-      sourceUrl: `${name} / ${moment(createdAt).format('YYYY.M.D')}`,
-      sizes: {
-        full: convertingImage.sizes.full,
-        large: convertingImage.sizes.large,
-        /* eslint-disable-next-line @typescript-eslint/camelcase */
-        small_square: convertingImage.sizes.smallSquare,
-      },
-    })
-
-    if (!media) {
-      return
-    }
-
-    window.location.href = `${appUrlScheme}:///images?${qs.stringify({
-      images: JSON.stringify(media.map(convertImage)),
-      index: media.findIndex(({ id }) => id === image.id),
-    })}`
   }
 
   const handleShow = fetchNext
@@ -170,12 +112,13 @@ export default function ReviewsList({
             isMyReview={!!(myReview && myReview.id === review.id)}
             key={review.id}
             index={i}
+            regionId={regionId}
+            appUrlScheme={appUrlScheme}
             review={review}
+            reviewRateDescriptions={reviewRateDescriptions}
             onUserClick={handleUserClick}
             onLikeButtonClick={handleLikeButtonClick}
-            onLikesCountClick={handleLikesCountClick}
             onMenuClick={handleMenuClick}
-            onImageClick={handleImageClick}
             likeVisible={!isPublic}
             menuVisible={!isPublic}
             resourceId={resourceId}
