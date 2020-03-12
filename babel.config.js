5744803bcb0ec066d9bbf642ee1cf62026bdd772
woody <woody@triple-corp.com>
2020-03-12 18:30:57 +0900

    Feature: TS3.7 피쳐의 babel plugin 설치
    optional chaining과 nullish coalescing의 babel plugin을 설치합니다.

--- babel.config.js
+++ babel.config.js
@@ -24,5 +24,7 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
   ],
 }
