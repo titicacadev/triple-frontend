789653eeb840daee3d1987f62fc1278334b52ace
appear <appear.ko@gmail.com>
2020-03-13 16:59:12 +0900

    feat: pricing 에서 사용자 메세지를 받을 수 있도록 추가합니다

--- packages/style-box/src/molecular/border-radius.ts
+++ packages/style-box/src/molecular/border-radius.ts
@@ -1,7 +1,6 @@
 import { css } from 'styled-components'
 
 import { BorderRadius } from '../types'
-import { overflow } from '../atom/overflow'
 
 export const borderRadius = (defaultValue?: BorderRadius) => css<{
   borderRadius?: BorderRadius
