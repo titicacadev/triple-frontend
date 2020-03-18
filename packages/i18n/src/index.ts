6f410754c90b132c03a7deaba14e959cb0f17a8a
Inbeom Hwang <hwanginbeom@gmail.com>
2020-03-18 22:44:46 +0900

    Add useI18n hook

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,3 +1,4 @@
 export { default as i18n } from './i18next'
-export { withTranslation, useTranslation } from 'react-i18next'
+export { withTranslation } from 'react-i18next'
 export { default as I18nProvider } from './provider'
+export { default as useI18n } from './use-i18n'
