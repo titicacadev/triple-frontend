806e61bdaabc81271006e034b38479dd5ebfd489
Inbeom Hwang <hwanginbeom@gmail.com>
2020-03-18 21:43:12 +0900

    Add @titicaca/i18n package

---
+++ packages/i18n/src/backend.ts
@@ -0,0 +1,22 @@
+import fetch from 'isomorphic-fetch'
+
+export default class I18nextFetchBackend {
+  type: 'backend' = 'backend'
+
+  read(language: string, namespace: string, callback: Function) {
+    return this.loadUrl(
+      `https://assets.triple-dev.titicaca-corp.com/locales/${language}/${namespace}.json`,
+      callback,
+    )
+  }
+
+  async loadUrl(url: string, callback: Function) {
+    const response = await fetch(url)
+
+    if (response.ok) {
+      callback(null, await response.json())
+    } else {
+      callback(new Error(`Failed to fetch ${url}`), false)
+    }
+  }
+}
---
+++ packages/i18n/src/index.ts
@@ -0,0 +1,3 @@
+export { default as i18n } from './i18next'
+export { withTranslation, useTranslation } from 'react-i18next'
+export { default as I18nProvider } from './provider'
