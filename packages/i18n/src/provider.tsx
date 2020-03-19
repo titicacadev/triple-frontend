cd3e219462c39394b06282c0bb72668ea06d371d
Inbeom Hwang <hwanginbeom@gmail.com>
2020-03-19 19:20:48 +0900

    Refine type annotation

--- packages/i18n/src/provider.tsx
+++ packages/i18n/src/provider.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import { I18nextProvider } from 'react-i18next'
+import { I18nextProvider, I18nextProviderProps } from 'react-i18next'
 
 import i18n from './i18next'
 
 export default function I18nProvider(
-  props: { language?: string } & Omit<
-    Parameters<typeof I18nextProvider>,
-    'i18n'
-  >,
+  props: { language?: string } & Omit<I18nextProviderProps, 'i18n'>,
 ) {
   const i18nInstance = isServer() ? i18n.cloneInstance() : i18n
 
   i18nInstance.changeLanguage(props.language || 'ko')
 
-  return <I18nextProvider {...props} i18n={i18nInstance} defaultNS="common" />
+  return (
+    <I18nextProvider
+      {...props}
+      i18n={i18nInstance}
+      defaultNS={props.defaultNS || 'common'}
+    />
+  )
 }
 
 function isServer() {
