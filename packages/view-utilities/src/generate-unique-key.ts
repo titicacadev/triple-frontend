9a5da88e0ea3069271128d6c367357b1380c5389
appear <appear.ko@gmail.com>
2020-03-19 12:00:16 +0900

    generateUniqueKey 만드는방법 변경

--- packages/view-utilities/src/generate-unique-key.ts
+++ packages/view-utilities/src/generate-unique-key.ts
@@ -1,2 +1,9 @@
-export const generateUniqueKey = (prefix: string) =>
-  `${prefix}_${new Date().getTime()}`
+const guid = () =>
+  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    .replace(/[xy]/g, (c) => {
+      const r = (Math.random() * 16) | 0
+      return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
+    })
+    .toUpperCase()
+
+export const generateUniqueKey = (prefix: string) => `${prefix}_${guid()}`
