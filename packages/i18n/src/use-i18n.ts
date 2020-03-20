5479f68e53b93de50823997ea8e6c755d8a96d79
Inbeom Hwang <hwanginbeom@gmail.com>
2020-03-20 10:16:19 +0900

    Expose i18n object to useI18n

--- packages/i18n/src/use-i18n.ts
+++ packages/i18n/src/use-i18n.ts
@@ -1,15 +1,17 @@
 import { useTranslation } from 'react-i18next'
 
 export default function useI18n(namespaces = 'common') {
-  const { t, ready } = useTranslation(namespaces, { useSuspense: false })
+  const { t, ready, i18n } = useTranslation(namespaces, { useSuspense: false })
 
   if (ready) {
     return {
       t: (key: string, fallback: string) => t([key, fallback]),
+      i18n,
     }
   } else {
     return {
       t: (key: string, fallback: string) => fallback,
+      i18n,
     }
   }
 }
