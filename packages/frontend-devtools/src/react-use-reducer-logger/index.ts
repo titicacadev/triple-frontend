a29272124ea2a935bd08fbc92c97718fe5dd465c
woody <woody@triple-corp.com>
2020-03-23 12:03:49 +0900

    Feature: reducer에 직접 작용하는 middleware로 변경
    본질적으로 reducer에 side effect이 달리는 것 같아서 이렇게 하기가 꺼려졌었습니다.
    그런데 redux랑 다른 모양이니 어쩔 수 없는 것 같아요...

--- packages/frontend-devtools/src/react-use-reducer-logger/index.ts
+++ packages/frontend-devtools/src/react-use-reducer-logger/index.ts
@@ -1,57 +1,25 @@
-import React, { Reducer, ReducerState, Dispatch, ReducerAction } from 'react'
+import React, { Reducer, ReducerState } from 'react'
 
 import diff from './json-diff'
 import printDiff from './print-diff'
 
-const dispatchMaps = new Map()
-
-class LoggerDispatcher<R extends Reducer<any, any>> {
-  reducer: R
-
-  state: ReducerState<R>
-
-  originalDispatch: Dispatch<ReducerAction<R>>
-
-  constructor(
-    reducer: R,
-    store: [ReducerState<R>, Dispatch<ReducerAction<R>>],
-  ) {
-    this.reducer = reducer
-    this.state = store[0]
-    this.originalDispatch = store[1]
-
-    this.dispatch = this.dispatch.bind(this)
-  }
-
-  dispatch(action: ReducerAction<R>) {
+function loggerMiddleware<R extends Reducer<any, any>>(next: R) {
+  return ((state, action) => {
     console.group(action.type, action.payload)
-    printDiff(diff(this.state, this.reducer(this.state, action)))
+    const nextState = next(state, action)
+    printDiff(diff(state, nextState))
     console.groupEnd()
 
-    this.originalDispatch(action)
-  }
+    return nextState
+  }) as R
 }
 
-function injectDevtool(original: typeof React.useReducer) {
+function injectLogger(original: typeof React.useReducer) {
   return <R extends Reducer<any, any>, I>(
     reducer: R,
     initializerArg: I & ReducerState<R>,
     initializer: (arg: I & ReducerState<R>) => ReducerState<R>,
-  ) => {
-    const store = original(reducer, initializerArg, initializer)
-
-    let dispatcher = dispatchMaps.get(store[1])
-
-    if (!dispatcher) {
-      dispatcher = new LoggerDispatcher(reducer, store)
-
-      dispatchMaps.set(store[1], dispatcher)
-    }
-
-    dispatcher.state = store[0]
-
-    return [dispatcher.state, dispatcher.dispatch]
-  }
+  ) => original(loggerMiddleware(reducer), initializerArg, initializer)
 }
 
-;(React.useReducer as any) = injectDevtool(React.useReducer)
+;(React.useReducer as any) = injectLogger(React.useReducer)
