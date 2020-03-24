4dcf601cfca2f66fff03f73e4d304340c32ae021
Inbeom Hwang <hwanginbeom@gmail.com>
2020-03-24 16:12:27 +0900

    Add LocationProperties component package
    This is related to #595

---
+++ packages/location-properties/src/location-properties.tsx
@@ -0,0 +1,128 @@
+import React, { useMemo } from 'react'
+import { Segment, List } from '@titicaca/core-elements'
+import ActionSheet from '@titicaca/action-sheet'
+import { useI18n } from '@titicaca/i18n'
+import { useHistoryContext } from '@titicaca/react-contexts'
+
+import PropertyItem, {
+  ACTION_SHEET_PREFIX,
+  PropertyItemProps,
+} from './property-item'
+
+interface TranslatedProperty {
+  primary?: string
+  ko?: string
+  en?: string
+  local?: string
+}
+
+interface ExtraProperty {
+  description: string
+  value: string
+}
+
+export default function LocationProperties({
+  addresses,
+  onAddressesClick,
+  phoneNumber,
+  onPhoneNumberClick,
+  officialSiteUrl,
+  onOfficialSiteUrlClick,
+  extraProperties,
+  onExtraPropertyClick,
+  ...props
+}: {
+  addresses?: TranslatedProperty
+  onAddressesClick?: () => void
+  phoneNumber?: string
+  onPhoneNumberClick?: () => void
+  officialSiteUrl?: string
+  onOfficialSiteUrlClick?: () => void
+  extraProperties?: ExtraProperty[]
+  onExtraPropertyClick?: (extraProperty: ExtraProperty) => void
+} & Parameters<typeof Segment>['0']) {
+  const { t } = useI18n()
+  const { uriHash, back } = useHistoryContext()
+
+  const properties: Map<
+    string,
+    Omit<PropertyItemProps, 'identifier'>
+  > = useMemo(() => {
+    const allValues = new Map<string, Omit<PropertyItemProps, 'identifier'>>()
+
+    allValues.set('addresses', {
+      title: t('common:address', '주소'),
+      value:
+        addresses?.primary ||
+        addresses?.ko ||
+        addresses?.en ||
+        addresses?.local,
+      onClick: onAddressesClick,
+      eventActionFragment: '기본정보_주소',
+    })
+
+    allValues.set('phoneNumber', {
+      title: t('common:contact', '전화'),
+      value: phoneNumber,
+      onClick: onPhoneNumberClick,
+      eventActionFragment: '기본정보_전화번호',
+    })
+
+    allValues.set('officialSiteUrl', {
+      title: t('common:homepage', '홈페이지'),
+      value: officialSiteUrl,
+      singleLine: true,
+      onClick: onOfficialSiteUrlClick,
+      eventActionFragment: '기본정보_홈페이지',
+    })
+
+    if (extraProperties) {
+      extraProperties.forEach(({ description, value, ...rest }, i) => {
+        allValues.set(`extraProperties.${i}`, {
+          title: description,
+          value,
+          onClick: onExtraPropertyClick
+            ? () => onExtraPropertyClick({ description, value, ...rest })
+            : undefined,
+        })
+      })
+    }
+
+    return allValues
+  }, [
+    t,
+    addresses,
+    phoneNumber,
+    officialSiteUrl,
+    extraProperties,
+    onAddressesClick,
+    onPhoneNumberClick,
+    onOfficialSiteUrlClick,
+    onExtraPropertyClick,
+  ])
+
+  const isActionSheetOpen = (uriHash || '').startsWith(ACTION_SHEET_PREFIX)
+
+  return (
+    <>
+      <Segment {...props}>
+        <List verticalGap={15}>
+          {[...properties.entries()].map(([key, props]) => (
+            <PropertyItem key={key} identifier={key} {...props} />
+          ))}
+        </List>
+      </Segment>
+      <ActionSheet
+        title={t('common:copyActionSheetTitle', '복사하기')}
+        open={isActionSheetOpen}
+        onClose={back}
+      >
+        <ActionSheet.Item buttonLabel={t('common:copy', '복사')}>
+          {isActionSheetOpen &&
+            properties.get(uriHash.replace(`${ACTION_SHEET_PREFIX}.`, ''))
+              ?.value}
+        </ActionSheet.Item>
+      </ActionSheet>
+    </>
+  )
+}
