afe931b79ce7e3a65822f88df8ca858735cd730d
Inbeom Hwang <hwanginbeom@gmail.com>
2020-03-24 18:32:36 +0900

    Consider all properties to be optional

--- docs/stories/location-properties/location-properties.stories.tsx
+++ docs/stories/location-properties/location-properties.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { boolean } from '@storybook/addon-knobs'
 import { HistoryProvider } from '@titicaca/react-contexts'
 import LocationProperties from '@titicaca/location-properties'
 
@@ -9,23 +10,37 @@ storiesOf('Location-Properties | LocationProperties', module).add(
   () => (
     <HistoryProvider>
       <LocationProperties
-        addresses={{
-          primary: null,
-          ko: null,
-          en: '1-1 Maihama, Urayasu, Chiba Prefecture 279-0031',
-          local: '〒279-0031 東京都千葉県浦安市舞浜11',
-        }}
+        addresses={
+          boolean('주소 있음', true)
+            ? {
+                primary: null,
+                ko: null,
+                en: '1-1 Maihama, Urayasu, Chiba Prefecture 279-0031',
+                local: '〒279-0031 東京都千葉県浦安市舞浜11',
+              }
+            : undefined
+        }
         onAddressesClick={action('onAddressesClick')}
-        phoneNumber="+81453305211"
+        phoneNumber={
+          boolean('전화번호 있음', true) ? '+81453305211' : undefined
+        }
         onPhoneNumberClick={action('onPhoneNumberClick')}
-        officialSiteUrl="http://www.tokyodisneyresort.jp/tdl/index.html"
+        officialSiteUrl={
+          boolean('홈페이지 있음', true)
+            ? 'http://www.tokyodisneyresort.jp/tdl/index.html'
+            : undefined
+        }
         onOfficialSiteUrlClick={action('onOfficialSiteUrlClick')}
-        extraProperties={[
-          {
-            description: '내비게이션용 맵코드',
-            value: '349 569 814*88',
-          },
-        ]}
+        extraProperties={
+          boolean('추가속성 있음', true)
+            ? [
+                {
+                  description: '내비게이션용 맵코드',
+                  value: '349 569 814*88',
+                },
+              ]
+            : undefined
+        }
         onExtraPropertyClick={action('onExtraPropertyClick')}
       />
     </HistoryProvider>
