47ff23eef69e181df6d0f98b3e2f74f40adc1986
Inbeom Hwang <hwanginbeom@gmail.com>
2020-03-25 17:16:41 +0900

    Separate PoiEntry sub-component

--- packages/nearby-pois/src/nearby-pois.tsx
+++ packages/nearby-pois/src/nearby-pois.tsx
@@ -1,13 +1,8 @@
 import React, { useReducer, useCallback, useEffect } from 'react'
 import { useI18n } from '@titicaca/i18n'
 import { Section, Button, List, Tabs } from '@titicaca/core-elements'
-import { PoiListElement } from '@titicaca/poi-list-elements'
 import { H1, Paragraph } from '@titicaca/triple-document'
-import {
-  useEventTrackingContext,
-  useHistoryContext,
-} from '@titicaca/react-contexts'
-import IntersectionObserver from '@titicaca/intersection-observer'
+import { useEventTrackingContext } from '@titicaca/react-contexts'
 
 import { PoiType } from './types'
 import nearbyPoisReducer, {
@@ -16,6 +11,7 @@ import nearbyPoisReducer, {
   appendPois,
 } from './reducer'
 import { fetchPois } from './service'
+import PoiEntry from './poi-entry'
 
 const INITIAL_STATE: NearbyPoisState = {
   attraction: {
@@ -71,8 +67,7 @@ export default function NearbyPois({
     ...(initialTab && { currentTab: initialTab }),
   })
   const { pois, hasMore, fetching } = state[currentTab]
-  const { trackEvent, trackSimpleEvent } = useEventTrackingContext()
-  const { navigate } = useHistoryContext()
+  const { trackSimpleEvent } = useEventTrackingContext()
 
   const { t } = useI18n()
 
@@ -136,6 +131,22 @@ export default function NearbyPois({
     )
   }, [poiId, regionId, lat, lon, currentTab, pois, dispatch, trackSimpleEvent])
 
+  const handleTabChange = useCallback(
+    (e?: React.SyntheticEvent, newTab?: string) => {
+      if (newTab) {
+        trackSimpleEvent({
+          action: '근처추천장소_탭선택',
+          label: EVENT_LABELS[newTab as PoiType],
+          /* eslint-disable-next-line @typescript-eslint/camelcase */
+          tab_name: EVENT_LABELS[newTab as PoiType],
+        })
+
+        dispatch(setCurrentTab({ type: newTab as PoiType }))
+      }
+    },
+    [trackSimpleEvent, dispatch],
+  )
+
   return (
     <Section anchor="nearby-pois" /* minHeight={404} */ {...props}>
       <H1 margin={{ bottom: 20 }}>
@@ -149,18 +160,7 @@ export default function NearbyPois({
           { label: t('common:attraction', '관광'), value: 'attraction' },
           { label: t('common:restaurant', '맛집'), value: 'restaurant' },
         ]}
-        onChange={(e?: React.SyntheticEvent, newTab?: PoiType) => {
-          if (newTab) {
-            trackSimpleEvent({
-              action: '근처추천장소_탭선택',
-              label: EVENT_LABELS[currentTab],
-              /* eslint-disable-next-line @typescript-eslint/camelcase */
-              tab_name: EVENT_LABELS[currentTab],
-            })
-
-            dispatch(setCurrentTab({ type: newTab as PoiType }))
-          }
-        }}
+        onChange={handleTabChange}
       />
 
       {pois.length === 0 && hasMore === false ? (
@@ -170,57 +170,14 @@ export default function NearbyPois({
       ) : (
         <List divided margin={{ top: 10 }}>
           {pois.map((poi, i) => (
-            <IntersectionObserver
-              safe
+            <PoiEntry
               key={poi.id}
-              onChange={({ isIntersecting }: { isIntersecting: boolean }) => {
-                if (isIntersecting) {
-                  trackEvent({
-                    fa: {
-                      action: '근처추천장소_POI노출',
-                      label: `${EVENT_LABELS[currentTab]}_${i}_${poi.id}`,
-                      /* eslint-disable-next-line @typescript-eslint/camelcase */
-                      item_id: poi.id,
-                    },
-                  })
-                }
-              }}
-            >
-              <div>
-                <PoiListElement
-                  poi={poi}
-                  onScrapedChange={(
-                    e?: React.SyntheticEvent,
-                    params?: {
-                      id: string
-                      type: string
-                      scraped: boolean
-                    },
-                  ) => {
-                    if (params) {
-                      onScrapedChange(
-                        params as {
-                          id: string
-                          type: string
-                          scraped: boolean
-                        },
-                      )
-                    }
-                  }}
-                  resourceScraps={scraps}
-                  onClick={() => {
-                    trackSimpleEvent({
-                      action: '근처추천장소_POI선택',
-                      label: `${EVENT_LABELS[currentTab]}_${i + 1}_${poi.id}`,
-                    })
-
-                    navigate(
-                      `/regions/${poi.source.regionId}/${poi.type}s/${poi.id}`,
-                    )
-                  }}
-                />
-              </div>
-            </IntersectionObserver>
+              index={i}
+              poi={poi}
+              scraps={scraps}
+              onScrapedChange={onScrapedChange}
+              eventLabel={EVENT_LABELS[currentTab]}
+            />
           ))}
 
           {hasMore && (
