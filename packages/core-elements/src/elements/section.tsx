3d45def122cda03565ccbdaf31520c5580e2f268
Inbeom Hwang <hwanginbeom@gmail.com>
2020-03-25 13:16:55 +0900

    Simplify Section props typing using Container props

--- packages/core-elements/src/elements/section.tsx
+++ packages/core-elements/src/elements/section.tsx
@@ -1,30 +1,21 @@
 import * as React from 'react'
-import * as CSS from 'csstype'
 
 import Container from './container'
 import { HR2 } from './hr'
-import { MarginPadding } from '../commons'
 
 export default function Section({
+  divider,
+  anchor,
+  children,
   minWidth = 320,
   maxWidth = 760,
   padding = { left: 30, right: 30 },
-  margin,
-  divider,
-  anchor,
-  userSelect,
-  children,
   ...props
 }: {
-  minWidth?: number
-  maxWidth?: number
-  padding?: MarginPadding
-  margin?: MarginPadding
   divider?: string
   anchor?: string
-  userSelect?: CSS.UserSelectProperty
   children?: React.ReactNode
-}) {
+} & Parameters<typeof Container>['0']) {
   if (React.Children.toArray(children).length > 0) {
     return (
       <>
@@ -38,8 +29,6 @@ export default function Section({
           minWidth={minWidth}
           maxWidth={maxWidth}
           padding={padding}
-          margin={margin}
-          userSelect={userSelect}
           {...props}
         >
           {children}
