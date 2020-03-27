e24cc9db1561a604b4ffef61e70e41936a999d97
woody <woody@triple-corp.com>
2020-03-27 17:42:37 +0900

    [#362] Refactor: 파일 분리

--- packages/type-definitions/src/index.ts
+++ packages/type-definitions/src/index.ts
@@ -1,13 +1,1 @@
-export interface TranslatedProperty {
-  primary?: string | null
-  /** @deprecated */
-  ko?: string | null
-  en: string | null
-  local: string | null
-}
-// TODO: content 마이그레이션이 완료되면 사용할 수 있음
-// export interface TranslatedProperty {
-//   primary: string | null
-//   en: string | null
-//   local: string | null
-// }
+export * from './translated-property'
