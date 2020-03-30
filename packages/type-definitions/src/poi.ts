32816a564cb90be3e3e71f9a7827e504543d0589
woody <woody@triple-corp.com>
2020-03-30 14:37:57 +0900

    [#362] Feature: POI, ImageMeta, PointGeoJSON 타입 정의  추가

---
+++ packages/type-definitions/src/poi.ts
@@ -0,0 +1,83 @@
+import { TranslatedProperty } from './translated-property'
+import { ImageMeta } from './image'
+
+export interface PointGeoJSON {
+  type: 'Point'
+  coordinates: number[] // [number, number]
+}
+
+interface POISourceBase {
+  id: string
+  areas?: { name: string }[]
+  categories?: { name: string }[]
+  comment?: string
+  grade: number
+  hasTnaProducts?: boolean
+  image: ImageMeta
+  location: number[] // [number, number]
+  names: TranslatedProperty
+  pointGeolocation: PointGeoJSON
+  reviewsRating?: number
+  reviewsCount?: number
+  scrapsCount?: number
+}
+
+interface POIBase {
+  id: string
+  nameOverride?: string
+  reviewed: boolean
+  scraped: boolean
+  distance?: number
+}
+
+export interface Attraction extends POIBase {
+  type: 'attraction'
+  source: POISourceBase & {
+    type: 'attraction'
+    regionId: string
+  }
+}
+
+export interface Restaurant extends POIBase {
+  type: 'restaurant'
+  source: POISourceBase & {
+    type: 'restaurant'
+    regionId: string
+  }
+}
+
+export interface Hotel extends POIBase {
+  type: 'hotel'
+  source: POISourceBase & {
+    type: 'hotel'
+    regionId?: string
+    starRating: number
+    tags: { name: string }[]
+    pricing: {
+      promoText: string
+      nightlyPrice: number
+      clubPromotionTarget: boolean
+      nightlyPriceHotelPromotionApplied: number
+      clubPromotionRate: number
+      clubMemberOnly: boolean
+      nightlyBasePrice: number
+      clubPromotionType: 'STATIC'
+    }
+  }
+  prices?: {
+    nightlyBasePrice?: number
+    nightlyPrice?: number
+    promoText: string
+    nightlyPriceHotelPromotionApplied: number
+    clubPromotionRate: number
+    clubPromotionType: 'STATIC'
+    clubMemberOnly: boolean
+    clubPromotionTarget: boolean
+  }
+  priceInfo: {
+    nightlyBasePrice?: number
+    nightlyPrice?: number
+  }
+}
+
+export type POI = Attraction | Restaurant | Hotel
