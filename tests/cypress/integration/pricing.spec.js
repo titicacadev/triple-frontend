e5cadf236b8ba85a9cf50cda118d2673ab48ca0b
appear <appear.ko@gmail.com>
2020-04-15 09:53:17 +0900

    [pricing] test: 변경된 스펙에 맞게 테스트 코드를 변경합니다

--- tests/cypress/integration/pricing.spec.js
+++ tests/cypress/integration/pricing.spec.js
@@ -6,7 +6,7 @@ describe('Pricing', () => {
   context('when using listing layout', () => {
     it('DescountRate Text have pink color', () => {
       cy.get('.rich-pressed > div')
-        .find('span')
+        .find('div')
         .contains('16%')
         .should('have.css', 'color')
         .and('eq', 'rgb(253, 46, 105)')
@@ -14,7 +14,7 @@ describe('Pricing', () => {
 
     it('Price Text have font-size 20px', () => {
       cy.get('.rich-pressed > div')
-        .find('span')
+        .find('div')
         .contains('25,000원')
         .should('have.css', 'font-size')
         .and('eq', '20px')
@@ -30,18 +30,18 @@ describe('Pricing', () => {
 
     it('Price Text have font-size 18px', () => {
       cy.get('.regular-pressed > div')
-        .find('span')
+        .find('div')
         .contains('25,000원')
         .should('have.css', 'font-size')
         .and('eq', '18px')
     })
 
-    it('BasePrice Text have absolute', () => {
+    it('BasePrice Text have display inline', () => {
       cy.get('.regular-pressed > div')
-        .find('span')
+        .find('div')
         .contains('30,000')
         .should('have.css', 'display')
-        .and('eq', 'inline-block')
+        .and('eq', 'inline')
     })
   })
 
