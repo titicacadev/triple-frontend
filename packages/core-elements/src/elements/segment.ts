9e0f0202b1e8ee3d244b29c8a6b17ea9d374ff52
Rhio Kim <rhio.kim@gmail.com>
2020-04-21 15:33:28 +0900

    feat: Implement basic Card component

--- packages/core-elements/src/elements/segment.ts
+++ packages/core-elements/src/elements/segment.ts
@@ -1,6 +1,6 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-import { MarginPadding } from '../commons'
+import { MarginPadding, BaseSizes } from '../commons'
 import { marginMixin, paddingMixin } from '../mixins'
 
 // eslint-disable-next-line no-unexpected-multiline
@@ -21,6 +21,52 @@ export const Segment = styled.div<{
     display: block;
     clear: both;
   }
+`
+
+export type BoxProps = {
+  width: number
+  height: number
+  radius: number
+  shadow: KeyOfShadowMap
+  margin: MarginPadding
+  padding: MarginPadding
+}
+
+const ShadowMap: { [key in BaseSizes]: string } = {
+  small: '0 0 10px 0 rgba(0, 0, 0, 0.07)',
+  medium: '0 0 20px 0 rgba(0, 0, 0, 0.07)',
+  large: '0 0 30px 0 rgba(0, 0, 0, 0.1)',
+}
+
+const shadow = ({ shadow: key }: any) => css`
+  box-shadow: ${ShadowMap[key as KeyOfShadowMap]};
+`
+const radius = ({ radius = 0 }: any) => css`
+  border-radius: ${radius}px;
+`
+
+export type KeyOfShadowMap = keyof typeof ShadowMap
+export type ShadowMapType = { [key in KeyOfShadowMap]: string }
+
+/**
+ * Card Component
+ *
+ * Props
+ *  - width: number
+ *  - height: number
+ *  - radius: number
+ *  - shadow: ShadowType
+ *  - margin: MarginPadding
+ *  - padding: MarginPadding
+ */
+export const Card = styled.div<Partial<BoxProps>>`
+  ${marginMixin}
+  ${paddingMixin}
+
+  ${radius}
+  ${shadow}
+
+  /* box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.07); */
 `
 
 export default Segment
