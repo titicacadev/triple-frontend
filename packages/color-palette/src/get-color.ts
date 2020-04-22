da028ac4d0eaf550ff03b2aec9deea7d76b2cb50
BoxeR <boxer@triple-corp.com>
2020-04-22 12:06:24 +0900

    :recycle: getColor 의 두번쨰 인자로 cssValue 를 만들어 줄 수 있는 옵션 제공

--- packages/color-palette/src/get-color.ts
+++ packages/color-palette/src/get-color.ts
@@ -2,6 +2,8 @@ import { ColorSet } from './colors'
 
 type ColorString = keyof typeof ColorSet
 
-export function getColor(color: ColorString): string {
-  return ColorSet[color]
+export function getColor(color: ColorString, cssValue?: boolean): string {
+  const colorCodes = ColorSet[color]
+
+  return cssValue ? `rgba(${colorCodes})` : colorCodes
 }
