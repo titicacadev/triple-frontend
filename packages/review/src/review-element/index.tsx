96d613c378d9208567b335714ec706aa4bda2f0b
luffy <luffy@triple-corp.com>
2020-04-22 17:24:18 +0900

    디자인 QA 반영

--- packages/review/src/review-element/index.tsx
+++ packages/review/src/review-element/index.tsx
@@ -97,6 +97,7 @@ const LikeButton = styled.div<{ liked?: boolean }>`
   margin-top: 5px;
   margin-right: 8px;
   padding: 2px 20px;
+  font-weight: bold;
   ${({ liked }) => css`
   color: ${getColor(liked ? 'blue' : 'gray')};
   background-image: url('https://assets.triple.guide/images/btn-lounge-thanks-${
--- packages/review/src/reviews-list.tsx
+++ packages/review/src/reviews-list.tsx
@@ -144,7 +144,7 @@ export default function ReviewsList({
 
   return (
     <>
-      <List margin={margin} divided verticalGap={20}>
+      <List margin={margin} divided verticalGap={56}>
         {displayedReviews.map((review, i) => (
           <ReviewElement
             isMyReview={!!(myReview && myReview.id === review.id)}
