a566bdf8d4f23cb6c75b5c44176ee467da8fa752
appear <appear.ko@gmail.com>
2020-04-22 17:18:33 +0900

    [form] feature: input story 를 추가합니다

---
+++ packages/form/src/input/index.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react'
+import InputMask, { MaskOptions } from 'react-input-mask'
+import styled, { css } from 'styled-components'
+import { getColor } from '@titicaca/color-palette'
+
+import withField from '../with-field'
+
+const BaseInput = styled(InputMask)<{ focused?: string; error?: string }>`
+  appearance: none;
+  margin: 0;
+  outline: none;
+  padding: 14px 16px;
+  font-size: 16px;
+  font-weight: 500;
+  border: 1px solid rgba(${getColor('gray100')});
+  border-radius: 2px;
+  box-sizing: border-box;
+  width: 100%;
+
+  ::placeholder {
+    color: rgba(${getColor('gray300')});
+  }
+
+  ${({ focused }) =>
+    focused &&
+    css`
+      border-color: rgb(${getColor('blue')});
+    `};
+
+  ${({ error }) =>
+    error &&
+    css`
+      border-color: rgb(${getColor('red')});
+    `};
+`
+
+type HTMLInputElementProps = React.InputHTMLAttributes<HTMLInputElement> &
+  MaskOptions
+
+interface InputProps extends Omit<HTMLInputElementProps, 'onChange'> {
+  id?: string
+  error?: string
+  focused?: string
+  onChange?: (e: React.SyntheticEvent, value: string) => any
+  onBlur?: (e: React.FocusEvent<any>) => any
+}
+
+function Input({ onChange, ...props }: InputProps) {
+  return (
+    <BaseInput
+      {...props}
+      onChange={(e) => onChange && onChange(e, e.target.value)}
+    />
+  )
+}
+
+export default withField(Input)
