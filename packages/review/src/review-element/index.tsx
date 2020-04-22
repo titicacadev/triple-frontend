a00b0e1fb2418e3f46ed1147b23d80475e90d925
luffy <luffy@triple-corp.com>
2020-04-22 18:15:53 +0900

    padding 위치 변경

--- packages/review/src/review-element/index.tsx
+++ packages/review/src/review-element/index.tsx
@@ -156,7 +156,7 @@ export default function ReviewElement({
         isIntersecting && onShow && onShow(index)
       }
     >
-      <List.Item padding={{ top: 20 }}>
+      <List.Item>
         <User user={user} onClick={(e) => onUserClick(e, review)}>
           {!blindedAt && !!rating ? <Score score={rating} /> : null}
         </User>
