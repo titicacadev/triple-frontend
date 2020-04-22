2fcb013d18e0534fa54e15b1a7b947af7f727c38
BoxeR <boxer@triple-corp.com>
2020-04-22 21:23:23 +0900

    :bug: color code 추출 정규식 오류 수정

--- packages/color-palette/src/get-color.ts
+++ packages/color-palette/src/get-color.ts
@@ -5,5 +5,5 @@ type ColorString = keyof typeof ColorSet
 export function getColor(color: ColorString): string {
   const colorCodes = (ColorSet[color] as unknown) as string
 
-  return colorCodes.replace(/^rgba\(((,?\s*\d+){4}).+$/, '$1')
+  return colorCodes.replace(/^rgba\(((,?\s*\d+){3}(,?\s*\d.\d){1}).+$/, '$1')
 }
