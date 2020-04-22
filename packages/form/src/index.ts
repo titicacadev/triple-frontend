723c2e6e57cced19852e95fcfbffaf0f19a4367e
appear <appear.ko@gmail.com>
2020-04-22 20:16:19 +0900

    [form] feature: textarea story 를 추가합니다

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Input } from './input'
 export { default as Select } from './select'
 export { default as ActionSheetSelector } from './action-sheet-selector'
+export { default as Textarea } from './textarea'
