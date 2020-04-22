6f077f74668ad661d5c7c007b0e8f1df5b581c05
luffy <luffy@triple-corp.com>
2020-04-22 17:24:18 +0900

    디자인 QA 반영

--- packages/review/src/review-element/index.tsx
+++ packages/review/src/review-element/index.tsx
@@ -97,6 +97,7 @@ const LikeButton = styled.div<{ liked?: boolean }>`
   margin-top: 5px;
   margin-right: 8px;
   padding: 2px 20px;
+  font-weight: bold;
   ${({ liked }) => css`
   color: rgba(${getColor(liked ? 'blue' : 'gray400')});
   background-image: url('https://assets.triple.guide/images/btn-lounge-thanks-${
