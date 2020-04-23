a8982d28e487975c05441743010dfd652368b29f
Inbeom Hwang <hwanginbeom@gmail.com>
2020-04-23 18:51:31 +0900

    Export all image-related components

--- packages/core-elements/src/elements/image/index.tsx
+++ packages/core-elements/src/elements/image/index.tsx
@@ -1,7 +1,5 @@
-import CompoundImage from './compound-image'
+export { default as Image } from './compound-image'
 export { default as FixedRatioImage } from './fixed-ratio-image'
 export { default as FixedDimensionsImage } from './fixed-dimensions-image'
 export { default as CircularImage } from './circular-image'
 export * from './types'
-
-export default CompoundImage
