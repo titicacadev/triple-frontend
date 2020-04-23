c2462820fad1d0cb58bf93e6df8742884cdab87f
luffy <luffy@triple-corp.com>
2020-04-23 11:08:27 +0900

    margin 재조정

--- packages/review/src/review-element/index.tsx
+++ packages/review/src/review-element/index.tsx
@@ -156,7 +156,7 @@ export default function ReviewElement({
         isIntersecting && onShow && onShow(index)
       }
     >
-      <List.Item style={{ paddingTop: 30 }}>
+      <List.Item style={{ paddingTop: 6 }}>
         <User user={user} onClick={(e) => onUserClick(e, review)}>
           {!blindedAt && !!rating ? <Score score={rating} /> : null}
         </User>
