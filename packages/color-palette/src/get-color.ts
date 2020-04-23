88434d4303a0ae009d39dd117bf260991b93604d
BoxeR <boxer@triple-corp.com>
2020-04-23 10:13:51 +0900

    [#685] :ok_hand: ColorSet 을 enum 에서 일반 객체로 변경합니다.

--- packages/color-palette/src/get-color.ts
+++ packages/color-palette/src/get-color.ts
@@ -3,7 +3,8 @@ import { ColorSet } from './colors'
 type ColorString = keyof typeof ColorSet
 
 export function getColor(color: ColorString): string {
-  const colorCodes = (ColorSet[color] as unknown) as string
-
-  return colorCodes.replace(/^rgba\(((,?\s*\d+){3}(,?\s*\d.?\d?){1}).+$/, '$1')
+  return ColorSet[color].replace(
+    /^rgba\(((,?\s*\d+){3}(,?\s*\d.?\d?){1}).+$/,
+    '$1',
+  )
 }
