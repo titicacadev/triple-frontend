8437c93344b83d45703c5b1f1fe85f1a0d132af3
appear <appear.ko@gmail.com>
2020-04-27 10:36:16 +0900

    [react-hooks] feature: use-scroll-lock 을 추가합니다

--- packages/core-elements/src/elements/label.tsx
+++ packages/core-elements/src/elements/label.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import styled, { css } from 'styled-components'
-import { ColorSet, Color } from '@titicaca/color-palette'
+import { ColorSet } from '@titicaca/color-palette'
 import CSS from 'csstype'
 
 import Container from './container'
