579fe9f80a77326af97060087530c0507bf816f2
woody <woody@triple-corp.com>
2020-04-27 15:50:04 +0900

    [#701] Feature: docs에서 long click 테스트 해볼 수 있도록 처리
    TODO: unit 테스트로 만드는 게 더 좋을 것 같다.

--- docs/stories/poi-detail/detail-header.stories.tsx
+++ docs/stories/poi-detail/detail-header.stories.tsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { number } from '@storybook/addon-knobs'
+import { number, boolean } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { DetailHeader } from '@titicaca/poi-detail'
-import { HistoryProvider } from '@titicaca/react-contexts'
+import { HistoryProvider, UserAgentProvider } from '@titicaca/react-contexts'
 
 storiesOf('poi-detail | DetailHeader', module).add('일반', () => (
   <HistoryProvider
     appUrlScheme="dev-soto"
     webUrlBase="https://triple-dev.titicaca-corp.com"
   >
-    <DetailHeader
-      names={{
-        primary: '도쿄 디즈니 랜드',
-        ko: '도쿄 디즈니 랜드',
-        en: 'Tokyo Disney land',
-        local: '東京ディズニーランド',
+    <UserAgentProvider
+      value={{
+        isPublic: boolean('isPublic', true),
+        isMobile: true,
+        os: {},
+        app: null,
       }}
-      scrapsCount={number('저장수', 682)}
-      reviewsCount={number('리뷰수', 13859)}
-      reviewsRating={number('리뷰평점', 4.45)}
-      onReviewsRatingClick={action('onReviewsRatingClick')}
-    />
+    >
+      <DetailHeader
+        names={{
+          primary: '도쿄 디즈니 랜드',
+          ko: '도쿄 디즈니 랜드',
+          en: 'Tokyo Disney land',
+          local: '東京ディズニーランド',
+        }}
+        scrapsCount={number('저장수', 682)}
+        reviewsCount={number('리뷰수', 13859)}
+        reviewsRating={number('리뷰평점', 4.45)}
+        onReviewsRatingClick={action('onReviewsRatingClick')}
+        onCopy={action('onCopy')}
+      />
+    </UserAgentProvider>
   </HistoryProvider>
 ))
