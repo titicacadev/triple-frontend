351f281a9cbb73fbf2aa4fe12d0c06258d5b055b
woody <woody@triple-corp.com>
2020-04-28 09:30:56 +0900

    Refactor: poi없는 경우 early return 처리

--- packages/poi-list-elements/src/carousel-element.tsx
+++ packages/poi-list-elements/src/carousel-element.tsx
@@ -36,51 +36,51 @@ export default function PoiCarouselElement<
   titleTopSpacing?: number
   imageFrame?: FrameRatioAndSizes
 }) {
-  if (poi) {
-    const {
-      id,
-      type,
-      nameOverride,
-      scraped: initialScraped,
-      source: { image, names },
-    } = poi
-
-    const { state: scraped } = deriveCurrentStateAndCount({
-      initialState: initialScraped,
-      initialCount: 0,
-      currentState: (resourceScraps || {})[id],
-    })
-
-    const name = nameOverride || names.ko || names.en || names.local
-
-    return (
-      <Carousel.Item size={carouselSize || 'small'} onClick={onClick}>
-        <Image
-          frame={imageFrame || 'large'}
-          asPlaceholder={!image}
-          src={image ? image.sizes.large.url : POI_IMAGE_PLACEHOLDERS[type]}
-          alt={name || ''}
-        />
-        <Text bold ellipsis alpha={1} margin={{ top: titleTopSpacing }}>
-          {name}
-        </Text>
-        <Text size="tiny" alpha={0.7} margin={{ top: 2 }}>
-          {description || TYPE_NAMES[type]}
-        </Text>
-
-        {actionButtonElement ||
-          (onScrapedChange && resourceScraps ? (
-            <ScrapButton
-              scraped={scraped}
-              resource={poi}
-              onScrapedChange={onScrapedChange}
-            />
-          ) : null)}
-
-        {additionalInfo}
-      </Carousel.Item>
-    )
+  if (!poi) {
+    return null
   }
 
-  return null
+  const {
+    id,
+    type,
+    nameOverride,
+    scraped: initialScraped,
+    source: { image, names },
+  } = poi
+
+  const { state: scraped } = deriveCurrentStateAndCount({
+    initialState: initialScraped,
+    initialCount: 0,
+    currentState: (resourceScraps || {})[id],
+  })
+
+  const name = nameOverride || names.ko || names.en || names.local
+
+  return (
+    <Carousel.Item size={carouselSize || 'small'} onClick={onClick}>
+      <Image
+        frame={imageFrame || 'large'}
+        asPlaceholder={!image}
+        src={image ? image.sizes.large.url : POI_IMAGE_PLACEHOLDERS[type]}
+        alt={name || ''}
+      />
+      <Text bold ellipsis alpha={1} margin={{ top: titleTopSpacing }}>
+        {name}
+      </Text>
+      <Text size="tiny" alpha={0.7} margin={{ top: 2 }}>
+        {description || TYPE_NAMES[type]}
+      </Text>
+
+      {actionButtonElement ||
+        (onScrapedChange && resourceScraps ? (
+          <ScrapButton
+            scraped={scraped}
+            resource={poi}
+            onScrapedChange={onScrapedChange}
+          />
+        ) : null)}
+
+      {additionalInfo}
+    </Carousel.Item>
+  )
 }
