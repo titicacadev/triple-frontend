a2a1f7a44c022f9be86125d0b2b51a095292d9f3
BoxeR <boxer@triple-corp.com>
2020-04-29 12:39:24 +0900

    :recycle: getColor 에서 color 가 ColorSet 이 아닐경우의 폴백을 제공합니다.

--- packages/color-palette/src/get-color.ts
+++ packages/color-palette/src/get-color.ts
@@ -2,8 +2,8 @@ import * as ColorSet from './colors'
 
 export type Color = keyof typeof ColorSet
 
-export function getColor(color: Color): string {
-  return ColorSet[color].replace(
+export function getColor(color: Color | string): string {
+  return (ColorSet[color as Color] ?? color).replace(
     /^rgba\(((,?\s*\d+){3}(,?\s*\d.?\d?){1}).+$/,
     '$1',
   )
