1b1cab69391e34d6413bea1dda91c28234cf7b32
Inbeom Hwang <hwanginbeom@gmail.com>
2020-04-29 18:55:08 +0900

    Add onArticlesClick mock handler

--- docs/stories/poi-detail/recommended-articles.stories.tsx
+++ docs/stories/poi-detail/recommended-articles.stories.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 import { RecommendedArticles } from '@titicaca/poi-detail'
 
 storiesOf('poi-detail | RecommendedArticles', module).add('일반', () => (
   <RecommendedArticles
     regionId={text('Region ID', '23c5965b-01ad-486b-a694-a2ced15f245c')}
+    onArticleClick={action('onArticleClick')}
   />
 ))
