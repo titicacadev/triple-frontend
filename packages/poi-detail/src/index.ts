1e36afa4a720aed1ea5fa8f132450e2687a1a1d8
Inbeom Hwang <hwanginbeom@gmail.com>
2020-04-29 18:52:47 +0900

    Add RecommendedArticles component

--- packages/poi-detail/src/index.ts
+++ packages/poi-detail/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Actions } from './actions'
 export { default as DetailHeader } from './detail-header'
 export { default as ImageCarousel } from './image-carousel'
+export { default as RecommendedArticles } from './recommended-articles'
---
+++ packages/poi-detail/src/recommended-articles/more-button.tsx
@@ -0,0 +1,30 @@
+import styled, { css } from 'styled-components'
+import * as CSS from 'csstype'
+import { marginMixin, MarginPadding } from '@titicaca/core-elements'
+
+export default styled.button<
+  {
+    margin?: MarginPadding
+    floated?: CSS.FloatProperty
+    center?: boolean
+  } & Omit<React.HTMLAttributes<HTMLDivElement>, 'margin'>
+>`
+  display: block;
+  cursor: pointer;
+  background: none;
+  padding: 0;
+  border: none;
+  float: ${({ floated }) => floated || 'none'};
+  color: rgba(54, 143, 255, 1);
+  text-decoration: underline;
+  font-size: 15px;
+  font-weight: 500;
+  ${({ center }) =>
+    center &&
+    css`
+      width: 100%;
+      text-align: center;
+    `};
+
+  ${marginMixin}
+`
