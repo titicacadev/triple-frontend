e5d09f302985ee22009f85fe72c9953cdc1e6d83
appear <appear.ko@gmail.com>
2020-04-29 17:27:19 +0900

    fix: getColor 의 alpha 소수점을 2 자리까지 허용합니다

--- packages/color-palette/src/get-color.ts
+++ packages/color-palette/src/get-color.ts
@@ -4,7 +4,7 @@ export type Color = keyof typeof ColorSet
 
 export function getColor(color: Color | string): string {
   return (ColorSet[color as Color] ?? color).replace(
-    /^rgba\(((,?\s*\d+){3}(,?\s*\d.?\d?){1}).+$/,
+    /^rgba\(((,?\s*\d+){3}(,?\s*\d.?\d?){2}).+$/,
     '$1',
   )
 }
