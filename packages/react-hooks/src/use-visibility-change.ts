8df0fcad34635a804a4e3f617aae38b50de2d5e2
appear <appear.ko@gmail.com>
2020-04-29 13:58:17 +0900

    [react-hooks] feature: useVisibilityChange hook 을 추가합니다

---
+++ packages/react-hooks/src/use-visibility-change.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react'
+import { useUserAgentContext } from '@titicaca/react-contexts'
+
+function useVisibilityChange(
+  onChange: (visible: boolean) => void,
+  deps: unknown[] = [],
+) {
+  const { isPublic } = useUserAgentContext()
+
+  useEffect(() => {
+    function handleChange() {
+      onChange(!document.hidden)
+    }
+
+    if (!isPublic) {
+      document.addEventListener('visibilitychange', handleChange)
+    }
+
+    return () => {
+      if (!isPublic) {
+        document.removeEventListener('visibilitychange', handleChange)
+      }
+    }
+  }, deps) // eslint-disable-line react-hooks/exhaustive-deps
+}
+
+export default useVisibilityChange
