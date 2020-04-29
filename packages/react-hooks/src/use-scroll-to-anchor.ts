0598b36c1cf9cac80bd4e364071c1b8b4c489550
appear <appear.ko@gmail.com>
2020-04-29 16:11:06 +0900

    [#716] feature: useScrollToAnchor 에서 alias 를 지원하도록 변경, hook story 추가

--- packages/react-hooks/src/use-scroll-to-anchor.ts
+++ packages/react-hooks/src/use-scroll-to-anchor.ts
@@ -1,21 +1,27 @@
 import { useEffect } from 'react'
 import scrollToElement from 'scroll-to-element'
 
-export function useScrollToAnchor(
-  hash?: string,
-  option?: {
-    offset: number
-    delayTime: number
-  },
-) {
+export function useScrollToAnchor(option?: {
+  offset?: number
+  delayTime?: number
+  alias?: {
+    [key: string]: string
+  }
+}) {
   useEffect(() => {
-    const anchor = hash || window.location.hash
+    const { offset = -52, delayTime = 1500, alias } = option || {} // HACK: 헤더 높이
 
-    if (anchor) {
-      const { offset = -52, delayTime = 1500 } = option || {} // HACK: 헤더 높이
+    const replacedHash = window.location.hash
+      ? window.location.hash.replace(/^#/, '')
+      : ''
 
+    const canonicalHash = (alias || {})[replacedHash] || replacedHash
+
+    const _el = document.getElementById(canonicalHash)
+
+    if (_el) {
       setTimeout(() => {
-        scrollToElement(anchor, { offset })
+        scrollToElement(_el, { offset })
       }, delayTime)
     }
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
