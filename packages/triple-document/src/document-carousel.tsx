950d41f1205bbdb4c6da4e1844fb158be9732fca
woody <woody@triple-corp.com>
2020-05-14 13:15:25 +0900

    Refactor: DocumentCarousel 파일 분리
    Embedded에서 사용하는 컴포넌트이다.
    embedded 컴포넌트를 별도 파일로 분리하기 위한 준비작업

---
+++ packages/triple-document/src/document-carousel.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MarginPadding, Carousel } from '@titicaca/core-elements'
+
+export default function DocumentCarousel({
+  margin,
+  children,
+}: React.PropsWithChildren<{ margin?: MarginPadding }>) {
+  return (
+    <Carousel margin={margin} containerPadding={{ left: 30, right: 30 }}>
+      {children}
+    </Carousel>
+  )
+}
