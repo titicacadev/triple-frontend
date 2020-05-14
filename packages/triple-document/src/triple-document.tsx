3f595d3734a039bd3db812354cbf27ba90ed91e7
woody <woody@triple-corp.com>
2020-05-14 13:36:26 +0900

    Refactor: Links 컴포넌트 분리
    Embedded 컴포넌트 분리를 위한 준비

--- packages/triple-document/src/triple-document.tsx
+++ packages/triple-document/src/triple-document.tsx
@@ -5,7 +5,6 @@ import {
   ImageCarouselElementContainer,
   ImageBlockElementContainer,
   ImageCaption,
-  SimpleLink,
   HR1,
   HR2,
   HR3,
@@ -13,16 +12,12 @@ import {
   HR5,
   HR6,
   Segment,
-  Button,
   Text,
   Table,
   Carousel,
   Container,
-  ResourceListItem,
-  SquareImage,
   ContainerProps,
   TableProps,
-  ButtonProps,
   ImageSourceType,
 } from '@titicaca/core-elements'
 import {
@@ -32,7 +27,6 @@ import {
 } from '@titicaca/poi-list-elements'
 import TripleMedia, { MediaMeta } from '@titicaca/triple-media'
 import { ListingPOI } from '@titicaca/type-definitions'
-import { getColor } from '@titicaca/color-palette'
 
 import { H1, H2, H3, H4, H2Props, H3Props, H4Props, H1Props } from './text'
 import { RegionListElement } from './region'
@@ -51,6 +45,7 @@ import Heading from './heading-hoc'
 import generateClickHandler from './generate-click-handler'
 import TextElement from './text-element'
 import ResourceList from './resource-list'
+import Links from './links'
 
 type ExtendedPOIListElementData = ListingPOI & {
   source: ListingPOI['source'] & {
@@ -395,169 +390,6 @@ export function Pois<T extends ExtendedPOIListElementData>({
             poi,
           })}
         />
-      ))}
-    </Container>
-  )
-}
-
-const LinksContainer = styled.div<{ compact?: boolean }>`
-  margin: ${({ compact }) => (compact ? '0' : '0 30px')};
-  margin-top: ${({ compact }) => (compact ? '10px' : '20px')};
-  margin-bottom: ${({ compact }) => (compact ? '-10px' : '-20px')};
-
-  a {
-    display: inline-block;
-    margin-bottom: ${({ compact }) => (compact ? '10px' : '20px')};
-    margin-right: ${({ compact }) => (compact ? '10px' : '20px')};
-  }
-`
-
-const ListLinkContainer = styled.div`
-  display: inline;
-  margin-left: 8px;
-  line-height: 1.63;
-`
-
-const ListLink = styled.a`
-  text-decoration: underline;
-  color: rgba(${getColor('gray900')});
-  font-weight: 500;
-`
-
-const ButtonContainer = styled.div<{ compact?: boolean }>`
-  margin: ${({ compact }) => (compact ? '12px 0 4px 0' : '50px 30px 0 30px')};
-  text-align: center;
-
-  a {
-    margin-top: 5px;
-  }
-
-  a:not(:first-child) {
-    margin-left: 5px;
-  }
-
-  @media (max-width: 360px) {
-    a:not(:first-child) {
-      margin-left: 0;
-    }
-  }
-`
-
-const BlockContainer = styled.div<{ compact?: boolean }>`
-  margin: ${({ compact }) => (compact ? '7px 0 4px 0' : '30px 30px 0 30px')};
-  text-align: center;
-`
-
-const LINK_CONTAINERS = {
-  button: ButtonContainer,
-  block: BlockContainer,
-  list: ListLinkContainer,
-  default: LinksContainer,
-  image: ResourceList,
-}
-
-function ButtonLink({
-  children,
-  ...props
-}: React.PropsWithChildren<ButtonProps>) {
-  return (
-    <Button bold color="blue" {...props}>
-      {children}
-    </Button>
-  )
-}
-
-function BlockLink({
-  children,
-  ...props
-}: React.PropsWithChildren<ButtonProps>) {
-  return (
-    <Button basic fluid compact color="gray" {...props}>
-      {children}
-    </Button>
-  )
-}
-
-const ImageLinkItem = styled.a`
-  text-decoration: none;
-`
-
-const IMAGE_PLACEHOLDER =
-  'https://assets.triple.guide/images/ico-blank-see@2x.png'
-
-function ImageLink({
-  href,
-  label,
-  description,
-  image,
-  onClick,
-}: {
-  href: string
-  label?: string
-  description?: string
-  image?: MediaMeta
-  onClick?: React.MouseEventHandler
-}) {
-  return (
-    <ResourceListItem onClick={onClick}>
-      <ImageLinkItem href={href}>
-        <SquareImage
-          floated="left"
-          size="small"
-          src={(image && image.sizes.small_square.url) || IMAGE_PLACEHOLDER}
-          alt={label}
-        />
-        <Text bold ellipsis alpha={1} margin={{ left: 50 }}>
-          {label}
-        </Text>
-        <Text size="tiny" alpha={0.7} margin={{ top: 4, left: 50 }}>
-          {description}
-        </Text>
-      </ImageLinkItem>
-    </ResourceListItem>
-  )
-}
-
-const LINK_ELEMENTS = {
-  button: ButtonLink,
-  block: BlockLink,
-  list: ListLink,
-  default: SimpleLink,
-  image: ImageLink,
-}
-
-function Links({
-  value: { display, links },
-  onLinkClick,
-  ...props
-}: {
-  value: {
-    display:
-      | (keyof typeof LINK_CONTAINERS & keyof typeof LINK_ELEMENTS)
-      | string
-    links: Link[]
-  }
-  onLinkClick?: LinkEventHandler
-  compact?: boolean
-}) {
-  const Container =
-    LINK_CONTAINERS[display as keyof typeof LINK_CONTAINERS] || LinksContainer
-  const Element =
-    LINK_ELEMENTS[display as keyof typeof LINK_ELEMENTS] || SimpleLink
-
-  return (
-    <Container {...props}>
-      {links.map((link, i) => (
-        <Element
-          key={i}
-          onClick={
-            onLinkClick && ((e: React.SyntheticEvent) => onLinkClick(e, link))
-          }
-          {...link}
-          href={link.href || '#'}
-        >
-          {link.label}
-        </Element>
       ))}
     </Container>
   )
