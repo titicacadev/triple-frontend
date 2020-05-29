d2b2ba0462f9192fe9907cab9345553e601c291f
appear <appear.ko@gmail.com>
2020-05-29 17:48:34 +0900

    [slider] docs: storybook 추가

--- docs/stories/slider/slider.stories.js
+++ docs/stories/slider/slider.stories.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
-import { number } from '@storybook/addon-knobs'
+import { number, select, boolean } from '@storybook/addon-knobs'
 import { SingleSlider, RangeSlider } from '@titicaca/slider'
 import { Text, Container } from '@titicaca/core-elements'
 
@@ -37,7 +37,7 @@ storiesOf('slider | Slider', module)
     )
   })
   .add('단일 (tootip)', () => {
-    const [value, setValue] = useState(500000)
+    const [value, setValue] = useState(0)
 
     return (
       <Container padding={{ top: 50, bottom: 50, left: 20, right: 20 }}>
@@ -45,12 +45,12 @@ storiesOf('slider | Slider', module)
           initialValue={value}
           min={0}
           max={10}
-          railHeight={8}
-          displayPercent
-          ActivateHandlerShadow
-          color="blue"
-          handlerSize={40}
-          handlerBorderWeight={4}
+          railHeight={number('레일 높이', 8)}
+          displayPercent={boolean('퍼센트 노출', true)}
+          ActivateHandlerShadow={boolean('핸들러 그림자', true)}
+          color={select('핸들러 색상', ['white', 'blue'], 'blue')}
+          handlerSize={number('핸들러 사이즈', 40)}
+          handlerBorderWeight={number('핸들러 테두리 굵기', 4)}
           onChange={setValue}
           toolTipLabel={['별로였어요', '괜찮았어요', '아주 좋았어요']}
           debounceTime={number('debounceTime')}
