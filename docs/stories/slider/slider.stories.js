18bea6282bc2f34c935cfd9c35849e331a0a7d88
appear <appear.ko@gmail.com>
2020-06-02 11:11:27 +0900

    [slider] docs: story 를 최신화합니다

--- docs/stories/slider/slider.stories.js
+++ docs/stories/slider/slider.stories.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
+import styled from 'styled-components'
 import { number, select, boolean } from '@storybook/addon-knobs'
 import { SingleSlider, RangeSlider } from '@titicaca/slider'
-import { Text, Container } from '@titicaca/core-elements'
+import { Text } from '@titicaca/core-elements'
+
+const Wrapper = styled.div`
+  padding: 50px 20px;
+`
+
+export default {
+  title: 'Slider',
+}
 
 function SingleLabel({ value }) {
   return <Text size="large">컴포넌트 내부: {value}</Text>
@@ -17,98 +24,76 @@ function RangeLabel({ fromValue, toValue }) {
   )
 }
 
-storiesOf('slider | Slider', module)
-  .add('기본', () => <SingleSlider onChange={action('onChange')} />)
-  .add('단일', () => {
-    const [value, setValue] = useState(500000)
+export function BasicSingleSlider() {
+  const [value, setValue] = useState(500000)
 
-    return (
-      <div>
-        컴포넌트 외부: {value}
-        <SingleSlider
-          initialValue={value}
-          min={0}
-          max={500000}
-          onChange={setValue}
-          labelComponent={SingleLabel}
-          debounceTime={number('debounceTime')}
-        />
-      </div>
-    )
-  })
-  .add('단일 (tootip)', () => {
-    const [value, setValue] = useState(0)
+  return (
+    <Wrapper>
+      <Text>컴포넌트 외부: {value}</Text>
+      <SingleSlider
+        initialValue={value}
+        min={0}
+        max={500000}
+        onChange={setValue}
+        labelComponent={SingleLabel}
+        debounceTime={number('debounceTime')}
+        nonLinear={boolean('nonLinear', false)}
+      />
+    </Wrapper>
+  )
+}
 
-    return (
-      <Container padding={{ top: 50, bottom: 50, left: 20, right: 20 }}>
-        <SingleSlider
-          initialValue={value}
-          min={0}
-          max={10}
-          railHeight={number('레일 높이', 8)}
-          displayPercent={boolean('퍼센트 노출', true)}
-          ActivateHandlerShadow={boolean('핸들러 그림자', true)}
-          color={select('핸들러 색상', ['white', 'blue'], 'blue')}
-          handlerSize={number('핸들러 사이즈', 40)}
-          handlerBorderWeight={number('핸들러 테두리 굵기', 4)}
-          displayMark={boolean('인디케이터 노출', true)}
-          onChange={setValue}
-          toolTipLabel={['별로였어요', '괜찮았어요', '아주 좋았어요']}
-          debounceTime={number('debounceTime')}
-        />
-      </Container>
-    )
-  })
-  .add('단일 non linear', () => {
-    const [value, setValue] = useState(500000)
+BasicSingleSlider.story = {
+  name: 'Single Slider',
+}
 
-    return (
-      <div>
-        컴포넌트 외부: {value}
-        <SingleSlider
-          initialValue={value}
-          min={0}
-          max={500000}
-          onChange={setValue}
-          labelComponent={SingleLabel}
-          nonLinear
-          debounceTime={number('debounceTime')}
-        />
-      </div>
-    )
-  })
-  .add('범위', () => {
-    const [values, setValues] = useState([0, 500000])
+export function SingleSliderWithToolTip() {
+  const [value, setValue] = useState(0)
 
-    return (
-      <div>
-        컴포넌트 외부: {values.join(', ')}
-        <RangeSlider
-          initialValues={values}
-          min={0}
-          max={500000}
-          onChange={setValues}
-          labelComponent={RangeLabel}
-          debounceTime={number('debounceTime')}
-        />
-      </div>
-    )
-  })
-  .add('범위 non linear', () => {
-    const [values, setValues] = useState([0, 500000])
+  return (
+    <Wrapper>
+      <SingleSlider
+        initialValue={value}
+        min={0}
+        max={10}
+        railHeight={number('레일 높이', 8)}
+        displayPercent={boolean('퍼센트 노출', true)}
+        ActivateHandlerShadow={boolean('핸들러 그림자', true)}
+        color={select('핸들러 색상', ['white', 'blue'], 'blue')}
+        handlerSize={number('핸들러 사이즈', 40)}
+        handlerBorderWeight={number('핸들러 테두리 굵기', 4)}
+        displayMark={boolean('인디케이터 노출', true)}
+        onChange={setValue}
+        toolTipLabel={['별로였어요', '괜찮았어요', '아주 좋았어요']}
+        debounceTime={number('debounceTime')}
+      />
+    </Wrapper>
+  )
+}
 
-    return (
-      <div>
-        컴포넌트 외부: {values.join(', ')}
-        <RangeSlider
-          initialValues={values}
-          min={0}
-          max={500000}
-          onChange={setValues}
-          labelComponent={RangeLabel}
-          nonLinear
-          debounceTime={number('debounceTime')}
-        />
-      </div>
-    )
-  })
+SingleSliderWithToolTip.story = {
+  name: 'Single Slider (Tooltip)',
+}
+
+export function BasicRangeSlider() {
+  const [values, setValues] = useState([0, 500000])
+
+  return (
+    <Wrapper>
+      <Text>컴포넌트 외부: {values.join(', ')}</Text>
+      <RangeSlider
+        initialValues={values}
+        min={0}
+        max={500000}
+        onChange={setValues}
+        labelComponent={RangeLabel}
+        nonLinear={boolean('nonLinear', false)}
+        debounceTime={number('debounceTime')}
+      />
+    </Wrapper>
+  )
+}
+
+BasicRangeSlider.story = {
+  name: 'Range Slider',
+}
