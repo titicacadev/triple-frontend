af66826825d8608807c1b788b6b7ccf84415528a
woody <woody@triple-corp.com>
2020-06-11 18:14:37 +0900

    [#625] Refactor: PickerFrame 내부 클래스 계층에 맞게 정리

--- packages/date-picker/src/picker-frame.ts
+++ packages/date-picker/src/picker-frame.ts
@@ -11,112 +11,129 @@ const PickerFrame = styled.div`
     font-weight: bold;
     font-size: 14px;
     background: #fafafa;
-  }
-  .DayPicker-Caption {
-    position: absolute;
-    top: 25px;
-    left: 20px;
-    color: #222;
-  }
-  .DayPicker-Caption > div {
-    font-size: 14px;
-    font-weight: 600;
-  }
-  .DayPicker-Weekdays {
-    display: table-row-group;
-  }
-  .DayPicker-WeekdaysRow {
-    display: table-row;
-  }
-  .DayPicker-Weekday {
-    display: table-cell;
-    color: #8b9898;
-    text-align: center;
-  }
-  .DayPicker-Month {
-    position: relative;
-    display: table;
-    text-align: center;
-    width: 100%;
-    border-spacing: 0 25px;
-    padding: 32px 0 30px 0;
-    user-select: none;
-    box-sizing: border-box;
-  }
-  .DayPicker-Body {
-    display: table-row-group;
-  }
-  .DayPicker-Week {
-    display: table-row;
-  }
-  .DayPicker-Day,
-  .DayPicker-WeekNumber {
-    position: relative;
-    display: table-cell;
-    padding: 9px 0;
-    width: 2%;
-    vertical-align: middle;
-    outline: none;
-  }
 
-  .DayPicker-Weekday abbr {
-    text-decoration: none;
-    color: rgba(${getColor('gray500')});
-    font-size: 12px;
-  }
-
-  .DayPicker-Day--today {
-    color: rgba(${getColor('blue')});
-
-    &:before {
-      top: 30px;
-      left: 0;
-      content: '오늘';
-      position: absolute;
-      display: inline-block;
-      font-size: 11px;
+    .DayPicker-Month {
+      position: relative;
+      display: table;
+      text-align: center;
       width: 100%;
-      color: rgba(${getColor('blue')});
-    }
+      border-spacing: 0 25px;
+      padding: 32px 0 30px 0;
+      user-select: none;
+      box-sizing: border-box;
 
-    &.DayPicker-Day--sunday,
-    &.DayPicker-Day--saturday,
-    &.DayPicker-Day--publicHolidays {
-      &:before {
-        color: rgba(${getColor('red')});
+      .DayPicker-Caption {
+        position: absolute;
+        top: 25px;
+        left: 20px;
+        color: #222;
+
+        > div {
+          font-size: 14px;
+          font-weight: 600;
+        }
+      }
+
+      .DayPicker-Weekdays {
+        display: table-row-group;
+
+        .DayPicker-WeekdaysRow {
+          display: table-row;
+
+          .DayPicker-Weekday {
+            display: table-cell;
+            color: #8b9898;
+            text-align: center;
+
+            &:first-child {
+              padding-left: 10px;
+            }
+            &:last-child {
+              padding-right: 10px;
+            }
+
+            abbr {
+              text-decoration: none;
+              color: rgba(${getColor('gray500')});
+              font-size: 12px;
+            }
+          }
+        }
+      }
+
+      .DayPicker-Body {
+        display: table-row-group;
+
+        .DayPicker-Week {
+          display: table-row;
+
+          .DayPicker-Day {
+            position: relative;
+            display: table-cell;
+            padding: 9px 0;
+            width: 2%;
+            vertical-align: middle;
+            outline: none;
+
+            &--today {
+              color: rgba(${getColor('blue')});
+
+              &:before {
+                top: 30px;
+                left: 0;
+                content: '오늘';
+                position: absolute;
+                display: inline-block;
+                font-size: 11px;
+                width: 100%;
+                color: rgba(${getColor('blue')});
+              }
+
+              &.DayPicker-Day--sunday,
+              &.DayPicker-Day--saturday,
+              &.DayPicker-Day--publicHolidays {
+                &:before {
+                  color: rgba(${getColor('red')});
+                }
+              }
+            }
+
+            &--sunday,
+            &--saturday,
+            &--publicHolidays {
+              color: rgba(${getColor('red')});
+            }
+
+            &--disabled {
+              color: rgba(${getColor('gray500')});
+            }
+
+            &--outside:after,
+            &--outside:before {
+              content: '' !important;
+              background: none !important;
+            }
+
+            &--sunday {
+              padding-left: 10px !important;
+            }
+            &--saturday {
+              padding-right: 10px !important;
+            }
+          }
+        }
       }
     }
-  }
 
-  .DayPicker-Day--sunday,
-  .DayPicker-Day--saturday,
-  .DayPicker-Day--publicHolidays {
-    color: rgba(${getColor('red')});
-  }
-
-  .DayPicker-Day--disabled {
-    color: rgba(${getColor('gray500')});
-  }
-
-  .DayPicker-Day--selected:before {
-    content: '';
-  }
-  .DayPicker-Day--outside:after,
-  .DayPicker-Day--outside:before {
-    content: '' !important;
-    background: none !important;
-  }
-  .DayPicker-Weekday:first-child {
-    padding-left: 10px;
-  }
-  .DayPicker-Weekday:last-child {
-    padding-right: 10px;
-  }
-  .DayPicker-Day--sunday {
-    padding-left: 10px !important;
-  }
-  .DayPicker-Day--saturday {
-    padding-right: 10px !important;
+    /* unknown class */
+    .DayPicker-WeekNumber {
+      position: relative;
+      display: table-cell;
+      padding: 9px 0;
+      width: 2%;
+      vertical-align: middle;
+      outline: none;
+    }
   }
 `
 export default PickerFrame
