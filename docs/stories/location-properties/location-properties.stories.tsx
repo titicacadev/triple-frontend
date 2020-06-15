3b2177f944dbe08e5e911a4f3b8b7faa84359a1b
appear <appear.ko@gmail.com>
2020-06-15 10:30:37 +0900

    docs: Location-Properties story 를 수정합니다

--- docs/stories/location-properties/location-properties.stories.tsx
+++ docs/stories/location-properties/location-properties.stories.tsx
@@ -1,61 +1,71 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { boolean } from '@storybook/addon-knobs'
 import { HistoryProvider, UserAgentProvider } from '@titicaca/react-contexts'
 import LocationProperties from '@titicaca/location-properties'
+import { StoryFn } from '@storybook/addons'
 
-storiesOf('Location-Properties | LocationProperties', module).add(
-  '기본',
-  () => (
-    <HistoryProvider
-      appUrlScheme="dev-soto"
-      webUrlBase="https://triple-dev.titicaca-corp.com"
-    >
-      <UserAgentProvider
-        value={{
-          isPublic: boolean('isPublic', true),
-          isMobile: true,
-          os: {},
-          app: null,
-        }}
+export default {
+  title: 'Location-Properties | LocationProperties',
+}
+
+export function BaseLocationProperties() {
+  return (
+    <LocationProperties
+      addresses={
+        boolean('주소 있음', true)
+          ? {
+              primary: null,
+              ko: null,
+              en: '1-1 Maihama, Urayasu, Chiba Prefecture 279-0031',
+              local: '〒279-0031 東京都千葉県浦安市舞浜11',
+            }
+          : undefined
+      }
+      onAddressesClick={action('onAddressesClick')}
+      phoneNumber={boolean('전화번호 있음', true) ? '+81453305211' : undefined}
+      onPhoneNumberClick={action('onPhoneNumberClick')}
+      officialSiteUrl={
+        boolean('홈페이지 있음', true)
+          ? 'http://www.tokyodisneyresort.jp/tdl/index.html'
+          : undefined
+      }
+      onOfficialSiteUrlClick={action('onOfficialSiteUrlClick')}
+      extraProperties={
+        boolean('추가속성 있음', true)
+          ? [
+              {
+                description: '내비게이션용 맵코드',
+                value: '349 569 814*88',
+              },
+            ]
+          : undefined
+      }
+      onExtraPropertyClick={action('onExtraPropertyClick')}
+      onCopy={action('onCopy')}
+    />
+  )
+}
+
+BaseLocationProperties.story = {
+  name: '기본 LocationProperties',
+  decorators: [
+    (storyFn: StoryFn<JSX.Element>) => (
+      <HistoryProvider
+        appUrlScheme="dev-soto"
+        webUrlBase="https://triple-dev.titicaca-corp.com"
       >
-        <LocationProperties
-          addresses={
-            boolean('주소 있음', true)
-              ? {
-                  primary: null,
-                  ko: null,
-                  en: '1-1 Maihama, Urayasu, Chiba Prefecture 279-0031',
-                  local: '〒279-0031 東京都千葉県浦安市舞浜11',
-                }
-              : undefined
-          }
-          onAddressesClick={action('onAddressesClick')}
-          phoneNumber={
-            boolean('전화번호 있음', true) ? '+81453305211' : undefined
-          }
-          onPhoneNumberClick={action('onPhoneNumberClick')}
-          officialSiteUrl={
-            boolean('홈페이지 있음', true)
-              ? 'http://www.tokyodisneyresort.jp/tdl/index.html'
-              : undefined
-          }
-          onOfficialSiteUrlClick={action('onOfficialSiteUrlClick')}
-          extraProperties={
-            boolean('추가속성 있음', true)
-              ? [
-                  {
-                    description: '내비게이션용 맵코드',
-                    value: '349 569 814*88',
-                  },
-                ]
-              : undefined
-          }
-          onExtraPropertyClick={action('onExtraPropertyClick')}
-          onCopy={action('onCopy')}
-        />
-      </UserAgentProvider>
-    </HistoryProvider>
-  ),
-)
+        <UserAgentProvider
+          value={{
+            isPublic: boolean('isPublic', true),
+            isMobile: true,
+            os: {},
+            app: null,
+          }}
+        >
+          <div>{storyFn()}</div>
+        </UserAgentProvider>
+      </HistoryProvider>
+    ),
+  ],
+}
