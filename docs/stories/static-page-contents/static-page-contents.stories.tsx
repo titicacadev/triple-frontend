e835e927cc88b96293fc9e04a6b34110e0eab8e1
Rhio Kim <rhio.kim@gmail.com>
2020-06-24 10:57:33 +0900

    fix(static-page-content): storiesOf 🙅🏻‍♂️ Fn 🙆🏻‍♂️

--- docs/stories/static-page-contents/static-page-contents.stories.tsx
+++ docs/stories/static-page-contents/static-page-contents.stories.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import styled from 'styled-components'
 import { select, text, boolean } from '@storybook/addon-knobs'
 import Popup from '@titicaca/popup'
 import { StaticPageContents } from '@titicaca/static-page-contents'
 
 export default {
-  title: 'Static Page Contents | FlexBox',
-  components: StaticPageContents,
+  title: 'Static Page Contents',
 }
 
 const CustomStyledStaticContents = styled(StaticPageContents)`
@@ -15,35 +13,50 @@ const CustomStyledStaticContents = styled(StaticPageContents)`
     color: red;
   }
 `
-storiesOf('Static Page Contents | Basic', module)
-  .add('Basic', () => {
-    return <StaticPageContents src="static-mock.html">ttt</StaticPageContents>
-  })
-  .add('with Popup', () => {
-    return (
-      <Popup
-        title={text('팝업 제목', '테스트')}
-        noNavbar={boolean('Navbar 숨김', false)}
-        borderless={boolean('네비바 경계선 없음', true)}
-        open={boolean('팝업 열기', true)}
-        icon={select('네비바 아이콘', ['close', 'back'], 'close')}
-      >
-        <StaticPageContents src="static-mock.html" />
-      </Popup>
-    )
-  })
-  .add('with Fallback', () => {
-    return (
-      <StaticPageContents
-        src="can-not-load-static-file.html"
-        onFallback={() => <span>custom fallback contents</span>}
-      />
-    )
-  })
-  .add('with Custom Style', () => {
-    return (
-      <CustomStyledStaticContents src="static-mock.html">
-        ttt
-      </CustomStyledStaticContents>
-    )
-  })
+
+export function Basic() {
+  return <StaticPageContents src="static-mock.html" />
+}
+
+Basic.story = {
+  name: '기본 사용',
+}
+
+export function withPopup() {
+  return (
+    <Popup
+      title={text('팝업 제목', '테스트')}
+      noNavbar={boolean('Navbar 숨김', false)}
+      borderless={boolean('네비바 경계선 없음', true)}
+      open={boolean('팝업 열기', true)}
+      icon={select('네비바 아이콘', ['close', 'back'], 'close')}
+    >
+      <StaticPageContents src="static-mock.html" />
+    </Popup>
+  )
+}
+
+withPopup.story = {
+  name: 'with Popup Component',
+}
+
+export function withFallback() {
+  return (
+    <StaticPageContents
+      src="can-not-load-static-file.html"
+      onFallback={() => <span>custom fallback contents</span>}
+    />
+  )
+}
+
+withFallback.story = {
+  name: 'with Fallback',
+}
+
+export function withCustomStyle() {
+  return <CustomStyledStaticContents src="static-mock.html" />
+}
+
+withCustomStyle.story = {
+  name: 'with Styled Component',
+}
