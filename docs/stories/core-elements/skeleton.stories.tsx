68dc1469610526b6bdc1fe30ace2352b0000a2bb
appear <appear.ko@gmail.com>
2020-06-29 09:49:13 +0900

    [core-elements] feature: skeleton ui 를 추가합니다

---
+++ docs/stories/core-elements/skeleton.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Skeleton, Container } from '@titicaca/core-elements'
+
+export default {
+  title: 'core-elements | skeleton',
+}
+
+export function BaseActionSheet() {
+  return (
+    <Container width="400px" margin={{top: 20, left: 20}}>
+      <Skeleton height="150px" margin={{bottom: 15}} borderRadius={4}/>
+      <Skeleton height="15px" margin={{bottom: 10}} />
+      <Skeleton width="80%" height="15px" />
+    </Container>
+  )
+}
+
+BaseActionSheet.story = {
+  name: '기본 스켈레톤',
+}
