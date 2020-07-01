f79336574712594f833d9f9a7c418aa9251c06c3
woody <woody@triple-corp.com>
2020-07-01 18:01:16 +0900

    Feature: test 패키지 및 관련 코드 제거

--- tests/src/text/App.js
+++
@@ -1,8 +0,0 @@
-import React, { PureComponent } from 'react'
-import { Text } from '@titicaca/core-elements'
-
-export default class App extends PureComponent {
-  render() {
-    return <Text>Default Text Component</Text>
-  }
-}
