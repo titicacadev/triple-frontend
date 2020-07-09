51e9d90a0ebc932b591862045a44cbde289be0a2
woody <woody@triple-corp.com>
2020-07-09 14:43:39 +0900

    Fix: lint 에러 수정

--- docs/stories/core-elements/skeleton.stories.tsx
+++ docs/stories/core-elements/skeleton.stories.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Skeleton, SkeletonText, SkeletonCircle, SkeletonButton, Container } from '@titicaca/core-elements'
+import {
+  Skeleton,
+  SkeletonText,
+  SkeletonCircle,
+  SkeletonButton,
+  Container,
+} from '@titicaca/core-elements'
 
 export default {
   title: 'core-elements | Skeleton',
@@ -7,17 +13,17 @@ export default {
 
 export function BaseSkeleton() {
   return (
-    <Container width="400px" margin={{top: 20, left: 20}}>
-      <Container margin={{bottom: 50}}>
-        <Skeleton height="150px" margin={{bottom: 15}} borderRadius={4}/>
-        <SkeletonText margin={{bottom: 10}} />
-        <SkeletonText width="80%" margin={{bottom: 10}} />
-        <SkeletonButton/>
+    <Container width="400px" margin={{ top: 20, left: 20 }}>
+      <Container margin={{ bottom: 50 }}>
+        <Skeleton height="150px" margin={{ bottom: 15 }} borderRadius={4} />
+        <SkeletonText margin={{ bottom: 10 }} />
+        <SkeletonText width="80%" margin={{ bottom: 10 }} />
+        <SkeletonButton />
       </Container>
       <Container>
-        <SkeletonCircle margin={{bottom: 15}} />
-        <SkeletonText margin={{bottom: 10}} />
-        <SkeletonText width="80%" margin={{bottom: 10}} />
+        <SkeletonCircle margin={{ bottom: 15 }} />
+        <SkeletonText margin={{ bottom: 10 }} />
+        <SkeletonText width="80%" margin={{ bottom: 10 }} />
       </Container>
     </Container>
   )
