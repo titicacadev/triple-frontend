3268d5d7e36204ce991a9e05b5cddb15ca5460a7
luffy <luffy@triple-corp.com>
2020-07-09 09:02:51 +0900

    multiline, direction, outline props 추가

--- packages/core-elements/src/elements/radio.tsx
+++ packages/core-elements/src/elements/radio.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import styled, { css } from 'styled-components'
+import { getColor } from '@titicaca/color-palette'
 
 import Text from './text'
 import { withField } from '../utils/form-field'
 
 type RadioValue = string | null
+type RadioDirection = 'left' | 'right'
 
 interface Option {
   text: string
@@ -13,15 +15,40 @@ interface Option {
 
 const RADIO_INPUT_SIZE = 26
 
-const RadioFrame = styled.div`
+const RadioFrame = styled.div<{
+  direction?: RadioDirection
+  outline?: boolean
+}>`
   position: relative;
   margin-bottom: 20px;
   line-height: ${RADIO_INPUT_SIZE}px;
-  padding-right: ${RADIO_INPUT_SIZE + 3}px;
-
   &:last-child {
     margin-bottom: 0;
   }
+
+  ${({ direction, outline }) =>
+    direction === 'left'
+      ? outline
+        ? css`
+            padding: 20px ${RADIO_INPUT_SIZE + 20}px 20px 20px;
+          `
+        : css`
+            padding-right: ${RADIO_INPUT_SIZE + 3}px;
+          `
+      : outline
+      ? css`
+          padding: 20px 20px 20px ${RADIO_INPUT_SIZE + 20}px;
+        `
+      : css`
+          padding-left: ${RADIO_INPUT_SIZE + 20}px;
+        `};
+
+  ${({ outline }) =>
+    outline &&
+    css`
+      border-radius: 2px;
+      border: solid 1px rgba(${getColor('gray100')});
+    `}
 `
 
 const RadioText = styled(Text)`
@@ -31,10 +58,11 @@ const RadioText = styled(Text)`
 
 const RadioInput = styled.input.attrs({ type: 'radio' })<{
   selected?: boolean
+  direction?: RadioDirection
+  outline?: boolean
 }>`
   position: absolute;
-  top: 0;
-  right: 0;
+  top: 50%;
   margin: 0;
   width: ${RADIO_INPUT_SIZE}px;
   height: ${RADIO_INPUT_SIZE}px;
@@ -45,6 +73,16 @@ const RadioInput = styled.input.attrs({ type: 'radio' })<{
   transition: all 0.3s ease;
   appearance: none;
   outline: none;
+  transform: translateY(-50%);
+
+  ${({ direction, outline }) =>
+    direction === 'left'
+      ? css`
+          right: ${outline ? '20px' : '0'};
+        `
+      : css`
+          left: ${outline ? '20px' : '0'};
+        `};
 
   ${({ selected }) =>
     selected
@@ -62,22 +100,40 @@ interface RadioProps {
   name?: string
   value?: RadioValue
   onChange?: (e: React.SyntheticEvent, value: RadioValue) => void
+  direction?: RadioDirection
+  multiline?: boolean
+  outline?: boolean
   options: Option[]
 }
 
-function Radio({ name, value, onChange, options }: RadioProps) {
+function Radio({
+  name,
+  value,
+  onChange,
+  direction = 'left',
+  multiline = false,
+  outline = false,
+  options,
+}: RadioProps) {
   return (
     <>
       {options.map(({ text, value: optionValue }, idx) => (
         <RadioFrame
           key={idx}
+          direction={direction}
+          outline={outline}
           onClick={(e) => onChange && onChange(e, optionValue)}
         >
-          <RadioText inlineBlock size="large" ellipsis>
+          <RadioText inlineBlock size="large" ellipsis={!multiline}>
             {text}
           </RadioText>
 
-          <RadioInput name={name} selected={optionValue === value} />
+          <RadioInput
+            name={name}
+            direction={direction}
+            outline={outline}
+            selected={optionValue === value}
+          />
         </RadioFrame>
       ))}
     </>
