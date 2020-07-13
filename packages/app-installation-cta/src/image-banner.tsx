4f7b3622d41fff56423e99e4ef1d021751fcacb3
BoxeR <boxer@triple-corp.com>
2020-07-13 18:14:17 +0900

    [#846] :chart_with_upwards_trend: cta 트래킹을 위한 props 를 추가합니다.

--- packages/app-installation-cta/src/image-banner.tsx
+++ packages/app-installation-cta/src/image-banner.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 
 import {
   ImageBannerWrapper,
@@ -7,8 +7,9 @@ import {
   InstallLink,
   DismissButton,
 } from './elements'
+import { EventTrackingProps } from './interfaces'
 
-interface ImageBannerProps {
+interface ImageBannerProps extends EventTrackingProps {
   imgUrl?: string
   installUrl: string
   onDismiss: () => void
@@ -18,10 +19,32 @@ export default function ImageBanner({
   imgUrl,
   installUrl,
   onDismiss,
+  trackEvent,
+  trackEventParams,
 }: ImageBannerProps) {
   const imgSrc =
     (imgUrl ?? '').trim() ||
     'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+
+  const sendTrackEventRequest = useCallback(
+    (param) => {
+      trackEvent && param && trackEvent(param)
+    },
+    [trackEvent],
+  )
+
+  useEffect(() => {
+    sendTrackEventRequest(trackEventParams && trackEventParams.onShow)
+  }, [sendTrackEventRequest, trackEventParams])
+
+  const handleClick = useCallback(() => {
+    sendTrackEventRequest(trackEventParams && trackEventParams.onSelect)
+  }, [sendTrackEventRequest, trackEventParams])
+
+  const handleDismiss = useCallback(() => {
+    onDismiss()
+    sendTrackEventRequest(trackEventParams && trackEventParams.onDismiss)
+  }, [onDismiss, sendTrackEventRequest, trackEventParams])
 
   return (
     <ImageBannerWrapper>
@@ -29,11 +52,11 @@ export default function ImageBanner({
         <BannerImage src={imgSrc} />
       </ImageWrapper>
 
-      <InstallLink href={installUrl}>
+      <InstallLink href={installUrl} onClick={handleClick}>
         👀&nbsp;&nbsp;편하게 앱에서 보기
       </InstallLink>
 
-      <DismissButton onClick={onDismiss}>
+      <DismissButton onClick={handleDismiss}>
         아깝지만 나중에 받을게요
       </DismissButton>
     </ImageBannerWrapper>
