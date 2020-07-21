5be2db1f2c1850928d2c73dd5ff2f67a2f3ace2b
appear <appear.ko@gmail.com>
2020-07-21 13:11:46 +0900

    [type-definitions] feature: 새롭게 정의된 priceInfos 타입을 추가합니다

--- packages/type-definitions/src/listing-poi.ts
+++ packages/type-definitions/src/listing-poi.ts
@@ -48,41 +48,19 @@ export interface ListingHotel extends ListingPOIBase {
     regionId?: string
     starRating: number
     tags: { name: string }[]
-
-    /** @deprecated priceInfo를 사용해주세요 */
-    pricing?: {
-      promoText: string
-      nightlyPrice: number
-      clubPromotionTarget: boolean
-      nightlyPriceHotelPromotionApplied: number
-      clubPromotionRate: number
-      clubMemberOnly: boolean
-      nightlyBasePrice: number
-      clubPromotionType: 'STATIC'
-    }
   }
 
-  /** @deprecated priceInfo를 사용해주세요 */
-  prices?: {
-    nightlyBasePrice?: number
-    nightlyPrice?: number
-    promoText: string
-    nightlyPriceHotelPromotionApplied: number
-    clubPromotionRate: number
-    clubPromotionType: 'STATIC'
-    clubMemberOnly: boolean
-    clubPromotionTarget: boolean
-  }
-
-  priceInfo?: {
-    nightlyBasePrice: number
-    nightlyPrice: number
-    price: number
-    discountRate: number
+  priceInfos?: {
     badge: string
-    excludedVat: number
-    originalPrice: number
-  }
+    nightlyPrice: number
+    originalPrice?: number
+    promotionText?: string
+    issuableCouponAppliedNightlyPrice?: number
+    site: string
+    siteImageId: string
+    supplier?: string
+    triplePriceCodes: string[]
+  }[]
 }
 
 export type ListingPOI = ListingAttraction | ListingRestaurant | ListingHotel
