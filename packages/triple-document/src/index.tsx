f29b9d25fb6c83ddf97601d93513bfbf381a5267
steve <steve@triple-corp.com>
2020-07-23 12:44:22 +0900

    [#891] feat: TripleElementData를 export합니다.

--- packages/triple-document/src/index.tsx
+++ packages/triple-document/src/index.tsx
@@ -1,5 +1,6 @@
 import { TripleDocument } from './triple-document'
 export { ELEMENTS } from './triple-document'
+export { TripleElementData } from './types'
 export * from './text'
 export * from './tna'
 export * from './region'
