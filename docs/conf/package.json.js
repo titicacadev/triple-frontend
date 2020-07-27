c21e406b6d10204669c72bbecf4b4faacb8bebc3
BoxeR <boxer@triple-corp.com>
2020-07-27 15:41:26 +0900

    [#552] :sparkles: release 후 버전태깅을 다른 GITHUB_TOKEN 으로 합니다.
    - release-canary 와 v1.30.0 식의 태그는 release-docs 가 반응할 수 있도록 합니다.

--- docs/conf/package.json.js
+++ docs/conf/package.json.js
@@ -2,16 +2,26 @@ const fsPromises = require('fs').promises
 
 const packageJSON = require('../package.json')
 
+const [TAG_NAME = 'latest'] = process.argv.slice(2)
+
 async function main() {
+  const tfVersion = /v[12].[0-9]+.[0-9]+/g.test(TAG_NAME)
+    ? TAG_NAME.substr(1)
+    : TAG_NAME === 'release-canary'
+    ? 'canary'
+    : 'latest'
+
   for (const key in packageJSON.dependencies) {
     if (key.indexOf('@titicaca/') === 0) {
-      packageJSON.dependencies[key] = 'latest'
+      packageJSON.dependencies[key] = tfVersion
     }
   }
 
   await fsPromises.writeFile(
     'package.json',
-    JSON.stringify(packageJSON, null, '  '),
+    `${JSON.stringify(packageJSON, null, '  ')}
+
+    `,
     { encoding: 'utf8' },
   )
 }
