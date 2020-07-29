b9cd21eed8cd7fd5e39a54648b6e1dec5c889db5
BoxeR <boxer@triple-corp.com>
2020-07-29 15:20:45 +0900

    [#894] :ok_hand: lerna list --json 의 패키지 리스트를 사용합니다.

--- docs/conf/package.json.js
+++ docs/conf/package.json.js
@@ -1,19 +1,38 @@
+const util = require('util')
 const fsPromises = require('fs').promises
+const exec = util.promisify(require('child_process').exec)
 
 const packageJSON = require('../package.json')
 
 const [TAG_NAME = 'latest'] = process.argv.slice(2)
 
 async function main() {
+  const { stdout } = await exec('lerna list --json')
+  let packageList
+
+  try {
+    packageList = JSON.parse(stdout)
+  } catch (ignored) {
+    packageList = null
+  }
+
   const tfVersion = /v[12].[0-9]+.[0-9]+/g.test(TAG_NAME)
     ? TAG_NAME.substr(1)
     : TAG_NAME === 'release-canary'
     ? 'canary'
     : 'latest'
 
-  for (const key in packageJSON.dependencies) {
-    if (key.indexOf('@titicaca/') === 0) {
-      packageJSON.dependencies[key] = tfVersion
+  if (packageList) {
+    packageList.forEach(({ name }) => {
+      if (packageJSON.dependencies[name]) {
+        packageJSON.dependencies[name] = tfVersion
+      }
+    })
+  } else {
+    for (const key in packageJSON.dependencies) {
+      if (key.indexOf('@titicaca/') === 0) {
+        packageJSON.dependencies[key] = tfVersion
+      }
     }
   }
 
