4184bfbb4f5c6b3df2bb2e5c472a4619c2cae03e
Inbeom Hwang <hwanginbeom@gmail.com>
2020-08-06 18:36:21 +0900

    Fix type errors

--- packages/standard-action-handler/src/show-toast.ts
+++ packages/standard-action-handler/src/show-toast.ts
@@ -3,7 +3,7 @@ import { UrlElements } from '@titicaca/view-utilities'
 import { showToast as nativeShowToast } from '@titicaca/triple-web-to-native-interfaces'
 
 export default async function showToast({ path, query }: UrlElements) {
-  if (path === '/web-action/show-toast') {
+  if (path === '/web-action/show-toast' && query) {
     const { text } = qs.parse(query, { ignoreQueryPrefix: true })
 
     nativeShowToast(text as string)
