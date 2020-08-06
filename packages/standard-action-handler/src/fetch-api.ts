bab9e1a13f3b2df9c032e3255df75b1c09d9c58f
Inbeom Hwang <hwanginbeom@gmail.com>
2020-08-06 18:45:32 +0900

    Parse query after asserting its presence

--- packages/standard-action-handler/src/fetch-api.ts
+++ packages/standard-action-handler/src/fetch-api.ts
@@ -3,11 +3,11 @@ import fetch from 'isomorphic-fetch'
 import { UrlElements } from '@titicaca/view-utilities'
 
 export default async function fetchApi({ path, query }: UrlElements) {
-  const { path: apiPath, method, body } = qs.parse(query, {
-    ignoreQueryPrefix: true,
-  })
+  if (path === '/web-action/fetch-api' && query) {
+    const { path: apiPath, method, body } = qs.parse(query, {
+      ignoreQueryPrefix: true,
+    })
 
-  if (path === '/web-action/fetch-api') {
     await fetch(apiPath as string, {
       method: (method as string) || 'GET',
       ...(body ? { headers: { 'content-type': 'application/json' } } : {}),
