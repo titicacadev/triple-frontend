950a9fb4b743259173f1f001dec50b6e2017dabe
Inbeom Hwang <hwanginbeom@gmail.com>
2020-08-24 11:02:53 +0900

    Extract POI list element components

---
+++ packages/poi-list-elements/src/poi-list-element.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ListingPOI } from '@titicaca/type-definitions'
+
+import {
+  CompactPoiListElement,
+  CompactPoiListElementProps,
+} from './compact-poi-list-element'
+import {
+  ExtendedPoiListElement,
+  ExtendedPoiListElementProps,
+} from './extended-poi-list-element'
+
+export type PoiListElementProps<T extends ListingPOI> =
+  | ({ compact: true } & CompactPoiListElementProps<T>)
+  | ({ compact?: false } & ExtendedPoiListElementProps<T>)
+
+export function PoiListElement<T extends ListingPOI>({
+  compact,
+  ...props
+}: PoiListElementProps<T>) {
+  return compact ? (
+    <CompactPoiListElement {...props} />
+  ) : (
+    <ExtendedPoiListElement {...props} />
+  )
+}
