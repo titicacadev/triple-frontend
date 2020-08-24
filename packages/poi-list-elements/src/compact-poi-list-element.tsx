8817aaa1b52e07a3793abfc89341aef8f9d1c7d6
Inbeom Hwang <hwanginbeom@gmail.com>
2020-08-24 10:53:06 +0900

    Separate compact poi list element

---
+++ packages/poi-list-elements/src/compact-poi-list-element.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { Text, SquareImage, ResourceListItem } from '@titicaca/core-elements'
+import { ListingPOI } from '@titicaca/type-definitions'
+import { CompactScrapButton } from '@titicaca/scrap-button'
+
+import { POIListElementBaseProps, ActionButtonElement } from './types'
+import { TYPE_NAMES } from './constants'
+
+interface CompactPoiListElementBaseProps<T extends ListingPOI>
+  extends POIListElementBaseProps<T> {
+  actionButtonElement?: ActionButtonElement
+}
+
+export type CompactPoiListElementProps<
+  T extends ListingPOI
+> = CompactPoiListElementBaseProps<T> &
+  Partial<Pick<Parameters<typeof ResourceListItem>['0'], 'as'>>
+
+const POI_IMAGE_PLACEHOLDERS_SMALL: { [key in ListingPOI['type']]: string } = {
+  attraction: 'https://assets.triple.guide/images/ico-blank-see-small@2x.png',
+  restaurant: 'https://assets.triple.guide/images/ico-blank-eat-small@2x.png',
+  hotel: 'https://assets.triple.guide/images/ico-blank-hotel-small@2x.png',
+}
+
+export function CompactPoiListElement<T extends ListingPOI>({
+  actionButtonElement,
+  poi,
+  poi: {
+    type,
+    nameOverride,
+    source: { names, image },
+  },
+  onClick,
+}: CompactPoiListElementProps<T>) {
+  const [actionButtonWidth, setActionButtonWidth] = useState(0)
+  const actionButtonRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (actionButtonRef && actionButtonRef.current) {
+      setActionButtonWidth((actionButtonRef?.current as any)?.width)
+    }
+  }, [actionButtonRef])
+
+  const name = nameOverride || names.ko || names.en || names.local
+
+  return (
+    <ResourceListItem onClick={onClick}>
+      <SquareImage
+        floated="left"
+        size="small"
+        src={image ? image.sizes.large.url : POI_IMAGE_PLACEHOLDERS_SMALL[type]}
+        alt={name || ''}
+      />
+      <Text
+        bold
+        ellipsis
+        alpha={1}
+        margin={{ left: 50, right: actionButtonWidth }}
+      >
+        {name}
+      </Text>
+      <Text size="tiny" alpha={0.7} margin={{ top: 4, left: 50 }}>
+        {TYPE_NAMES[type]}
+      </Text>
+
+      {actionButtonElement ? (
+        <div ref={actionButtonRef}>{actionButtonElement}</div>
+      ) : (
+        <CompactScrapButton resource={poi} />
+      )}
+    </ResourceListItem>
+  )
+}
--- packages/poi-list-elements/src/index.tsx
+++ packages/poi-list-elements/src/index.tsx
@@ -1,28 +1,18 @@
-import * as React from 'react'
-import { Text, SquareImage, ResourceListItem } from '@titicaca/core-elements'
+import React from 'react'
 import ExtendedResourceListElement, {
   ResourceListElementProps,
 } from '@titicaca/resource-list-element'
-import { CompactScrapButton } from '@titicaca/scrap-button'
 import { ListingPOI, ListingHotel } from '@titicaca/type-definitions'
 
-import { TYPE_NAMES, POI_IMAGE_PLACEHOLDERS } from './constants'
-import { POIListElementBaseProps, ActionButtonElement } from './types'
+import { POI_IMAGE_PLACEHOLDERS } from './constants'
+import { POIListElementBaseProps } from './types'
+import {
+  CompactPoiListElement,
+  CompactPoiListElementProps,
+} from './compact-poi-list-element'
 
 export { default as POICardElement } from './poi-card-element'
 export { default as PoiCarouselElement } from './carousel-element'
-
-type PoiTypes = ListingPOI['type']
-
-interface CompactPoiListElementBaseProps<T extends ListingPOI>
-  extends POIListElementBaseProps<T> {
-  actionButtonElement?: ActionButtonElement
-}
-
-type CompactPoiListElementProps<
-  T extends ListingPOI
-> = CompactPoiListElementBaseProps<T> &
-  Partial<Pick<Parameters<typeof ResourceListItem>['0'], 'as'>>
 
 interface ExtendedPoiListElementBaseProps<T extends ListingPOI>
   extends POIListElementBaseProps<T> {
@@ -43,12 +33,6 @@ export type PoiListElementProps<T extends ListingPOI> =
   | ({ compact: true } & CompactPoiListElementProps<T>)
   | ({ compact?: false } & ExtendedPoiListElementProps<T>)
 
-const POI_IMAGE_PLACEHOLDERS_SMALL: { [key in PoiTypes]: string } = {
-  attraction: 'https://assets.triple.guide/images/ico-blank-see-small@2x.png',
-  restaurant: 'https://assets.triple.guide/images/ico-blank-eat-small@2x.png',
-  hotel: 'https://assets.triple.guide/images/ico-blank-hotel-small@2x.png',
-}
-
 export function PoiListElement<T extends ListingPOI>({
   compact,
   ...props
@@ -58,73 +42,6 @@ export function PoiListElement<T extends ListingPOI>({
   ) : (
     <ExtendedPoiListElement {...props} />
   )
-}
-
-class CompactPoiListElement<T extends ListingPOI> extends React.PureComponent<
-  CompactPoiListElementProps<T>,
-  { actionButtonWidth: number }
-> {
-  state = { actionButtonWidth: 34 }
-
-  setActionButtonRef = (ref: HTMLDivElement) => {
-    if (ref && ref.children[0]) {
-      const {
-        state: { actionButtonWidth },
-      } = this
-      const newWidth = ref.children[0].clientWidth
-
-      if (newWidth !== actionButtonWidth) {
-        this.setState({ actionButtonWidth: newWidth })
-      }
-    }
-  }
-
-  render() {
-    const {
-      props: {
-        actionButtonElement,
-        poi: {
-          type,
-          nameOverride,
-          source: { names, image },
-        },
-        onClick,
-      },
-      state: { actionButtonWidth },
-    } = this
-
-    const name = nameOverride || names.ko || names.en || names.local
-
-    return (
-      <ResourceListItem onClick={onClick}>
-        <SquareImage
-          floated="left"
-          size="small"
-          src={
-            image ? image.sizes.large.url : POI_IMAGE_PLACEHOLDERS_SMALL[type]
-          }
-          alt={name || ''}
-        />
-        <Text
-          bold
-          ellipsis
-          alpha={1}
-          margin={{ left: 50, right: actionButtonWidth }}
-        >
-          {name}
-        </Text>
-        <Text size="tiny" alpha={0.7} margin={{ top: 4, left: 50 }}>
-          {TYPE_NAMES[type]}
-        </Text>
-
-        {actionButtonElement ? (
-          <div ref={this.setActionButtonRef}>{actionButtonElement}</div>
-        ) : (
-          <CompactScrapButton resource={this.props.poi} />
-        )}
-      </ResourceListItem>
-    )
-  }
 }
 
 class ExtendedPoiListElement<T extends ListingPOI> extends React.PureComponent<
