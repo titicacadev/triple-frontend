c6a06fd2b4183cdad2ea9e1862c23c9febbc8002
woody <woody@triple-corp.com>
2020-08-27 09:21:41 +0900

    [#933] Feature: context에서 사용하지 않는 값 제거
    borderRadius와 from은 사용하지 않으므로 제거합니다.

--- packages/action-sheet/src/index.tsx
+++ packages/action-sheet/src/index.tsx
@@ -4,13 +4,7 @@ import ActionItemUI from './components/action-item'
 import ActionSheetUI from './components/action-sheet'
 import { ActionSheetContextValue } from './types'
 
-const DEFAULT_FROM = 'bottom'
-const DEFAULT_BORDER_RADIUS = 12
-
-const { Provider, Consumer } = React.createContext<ActionSheetContextValue>({
-  from: DEFAULT_FROM,
-  borderRadius: DEFAULT_BORDER_RADIUS,
-})
+const { Provider, Consumer } = React.createContext<ActionSheetContextValue>({})
 
 function ActionItem(
   props: Omit<Parameters<typeof ActionItemUI>[0], 'onClose'>,
@@ -24,19 +18,11 @@ function ActionItem(
 
 export default function ActionSheet({
   onClose,
-  from = DEFAULT_FROM,
-  borderRadius = DEFAULT_BORDER_RADIUS,
   ...restProps
-}: Partial<ActionSheetContextValue> &
-  Omit<Parameters<typeof ActionSheetUI>[0], 'from' | 'borderRadius'>) {
+}: Parameters<typeof ActionSheetUI>[0]) {
   return (
-    <Provider value={{ onClose, from, borderRadius }}>
-      <ActionSheetUI
-        {...restProps}
-        onClose={onClose}
-        from={from}
-        borderRadius={borderRadius}
-      />
+    <Provider value={{ onClose }}>
+      <ActionSheetUI {...restProps} onClose={onClose} />
     </Provider>
   )
 }
--- packages/action-sheet/src/types.ts
+++ packages/action-sheet/src/types.ts
@@ -1,7 +1,5 @@
 import { SyntheticEvent } from 'react'
 
 export interface ActionSheetContextValue {
-  borderRadius: number
-  from: 'bottom' | 'top'
   onClose?: (e?: SyntheticEvent) => any
 }
