3519368415bcf93c453d70a014752c3699baeb1a
Unknown <torres@triple-corp.com>
2020-09-01 13:37:39 +0900

    [#921] og prefix 제거

--- packages/meta-tags/src/facebook-open-graph.tsx
+++ packages/meta-tags/src/facebook-open-graph.tsx
@@ -1,48 +1,48 @@
 import React, { Fragment } from 'react'
 
 export function FacebookOpenGraph({
-  ogTitle = '실시간 여행 가이드 - 트리플',
-  ogDescription,
-  ogUrl = 'https://triple.guide/',
-  ogType = 'website',
-  ogLocale = 'ko_KR',
-  ogImage,
+  title = '실시간 여행 가이드 - 트리플',
+  description,
+  url = 'https://triple.guide/',
+  type = 'website',
+  locale = 'ko_KR',
+  image,
   fbAppId = '136540730081853',
 }: {
-  ogTitle?: string
-  ogDescription?: string
-  ogUrl?: string
-  ogType?: string
-  ogLocale?: string
-  ogImage?: { url: string; width: number; height: number }
+  title?: string
+  description?: string
+  url?: string
+  type?: string
+  locale?: string
+  image?: { url: string; width: number; height: number }
   fbAppId?: string
 }) {
   return (
     <Fragment>
-      <meta property="og:title" content={ogTitle} />
-      <meta property="og:url" content={ogUrl} />
-      <meta property="og:type" content={ogType} />
-      <meta property="og:locale" content={ogLocale} />
+      <meta property="og:title" content={title} />
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content={type} />
+      <meta property="og:locale" content={locale} />
       <meta
         property="og:image"
         content={
-          ogImage?.url ||
+          image?.url ||
           'https://assets.triple.guide/images/default-cover-image.jpg'
         }
       />
-      {!ogImage || ogImage.width || ogImage.height ? (
+      {!image || image.width || image.height ? (
         <>
           <meta
             property="og:image:width"
-            content={encodeURIComponent(ogImage?.width || 1052)}
+            content={encodeURIComponent(image?.width || 1052)}
           />
           <meta
             property="og:image:height"
-            content={encodeURIComponent(ogImage?.height || 1052)}
+            content={encodeURIComponent(image?.height || 1052)}
           />
         </>
       ) : null}
-      <meta property="og:description" content={ogDescription || ''} />
+      <meta property="og:description" content={description || ''} />
       <meta property="fb:app_id" content={fbAppId} />
     </Fragment>
   )
