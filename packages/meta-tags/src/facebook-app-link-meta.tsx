9644dc1cd76a1e0c37bfd9ffdeab86087b213002
Unknown <torres@triple-corp.com>
2020-09-02 14:07:08 +0900

    [#921] 리뷰반영

--- packages/meta-tags/src/facebook-app-link-meta.tsx
+++ packages/meta-tags/src/facebook-app-link-meta.tsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react'
 
-export function FacebookAppLink({
+export function FacebookAppLinkMeta({
   appName = '트리플',
   iosAppStoreId = '1225499481',
   appUrl = '/',
   appPackageName = 'com.titicacacorp.triple',
-  appUrlScheme = 'triple://',
+  appUrlScheme = 'triple',
 }: {
   appName?: string
   iosAppStoreId?: string
@@ -17,9 +17,9 @@ export function FacebookAppLink({
     <Fragment>
       <meta property="al:ios:app_name" content={appName} />
       <meta property="al:android:app_name" content={appName} />
-      <meta property="al:ios:url" content={`${appUrlScheme}${appUrl}`} />
+      <meta property="al:ios:url" content={`${appUrlScheme}://${appUrl}`} />
       <meta property="al:ios:app_store_id" content={iosAppStoreId} />
-      <meta property="al:android:url" content={`${appUrlScheme}${appUrl}`} />
+      <meta property="al:android:url" content={`${appUrlScheme}://${appUrl}`} />
       <meta property="al:android:package" content={appPackageName} />
     </Fragment>
   )
