9d19be80c309d0e5290e620a3e84b949db6cf0ca
Unknown <torres@triple-corp.com>
2020-09-02 14:07:08 +0900

    [#921] 리뷰반영

--- packages/meta-tags/src/apple-smart-banner-meta.tsx
+++ packages/meta-tags/src/apple-smart-banner-meta.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export function AppleSmartBannerMeta({
   appId = '1225499481',
-  appUrlScheme = 'com.titicacacorp.triple://',
+  appUrlScheme = 'com.titicacacorp.triple',
   appPath = '/',
 }: {
   appId?: string
@@ -12,7 +12,7 @@ export function AppleSmartBannerMeta({
   return (
     <meta
       name="apple-itunes-app"
-      content={`app-id=${appId}, app-argument=${appUrlScheme}${appPath}`}
+      content={`app-id=${appId}, app-argument=${appUrlScheme}://${appPath}`}
     />
   )
 }
