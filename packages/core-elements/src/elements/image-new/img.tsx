83b4f41f92b5406bf3986e9a8b9dc2a8d86895cf
woody <woody@triple-corp.com>
2020-09-09 17:25:34 +0900

    [#574] Feature: image Img 컴포넌트 구현
    img 태그에 대응되는 컴포넌트입니다.

---
+++ packages/core-elements/src/elements/image-new/img.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { useImageState } from './context'
+
+const Img = styled.img<{
+  borderRadius?: number
+  dimmed?: boolean
+  fitHeight?: boolean
+}>`
+  ${({ fitHeight }) =>
+    fitHeight &&
+    `
+      position: absolute;
+      top: 0;
+      height: 100%;
+    `}
+  width: 100%;
+  border-radius: ${({ borderRadius }) =>
+    borderRadius === 0 ? 0 : borderRadius || 6}px;
+  object-fit: cover;
+  opacity: ${({ dimmed }) => (dimmed ? 80 : 100)}%;
+`
+
+export default function ImageImg(
+  props: Omit<Parameters<typeof Img>[0], 'borderRadius'>,
+) {
+  const { borderRadius } = useImageState()
+
+  return <Img {...props} borderRadius={borderRadius} />
+}
