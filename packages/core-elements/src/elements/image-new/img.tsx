6a9da239387ecbe0fe2424a69399596913998669
woody <woody@triple-corp.com>
2020-09-09 18:04:50 +0900

    [#574] Feature: FixedRatioFrame 컴포넌트 구현
    fitHeight를 context로 제공합니다.
    
    Refactor: content를 absolute positioning하는 context 이름 변경

--- packages/core-elements/src/elements/image-new/img.tsx
+++ packages/core-elements/src/elements/image-new/img.tsx
@@ -2,30 +2,43 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { useImageState } from './context'
+import { useContentAbsolute } from './fixed-ratio-frame'
 
 const Img = styled.img<{
   borderRadius?: number
   dimmed?: boolean
-  fitHeight?: boolean
+  absolute: boolean
 }>`
-  ${({ fitHeight }) =>
-    fitHeight &&
-    `
-      position: absolute;
-      top: 0;
-      height: 100%;
-    `}
   width: 100%;
+  height: 100%;
   border-radius: ${({ borderRadius }) =>
     borderRadius === 0 ? 0 : borderRadius || 6}px;
   object-fit: cover;
   opacity: ${({ dimmed }) => (dimmed ? 80 : 100)}%;
+
+  ${({ absolute }) =>
+    absolute &&
+    `
+    position: absolute;
+    top: 0;
+  `}
 `
 
 export default function ImageImg(
-  props: Omit<Parameters<typeof Img>[0], 'borderRadius' | 'dimmed'>,
+  props: Omit<
+    Parameters<typeof Img>[0],
+    'borderRadius' | 'dimmed' | 'fitHeight'
+  >,
 ) {
   const { borderRadius, overlayMounted } = useImageState()
+  const absolute = useContentAbsolute()
 
-  return <Img {...props} borderRadius={borderRadius} dimmed={overlayMounted} />
+  return (
+    <Img
+      {...props}
+      borderRadius={borderRadius}
+      dimmed={overlayMounted}
+      absolute={absolute}
+    />
+  )
 }
