6e8206600758f508f6b3db649a75ecfeabb07138
woody <woody@triple-corp.com>
2020-09-09 18:05:56 +0900

    [#574] Feature: FixedDimensionsFrame 컴포넌트 구현
    Fix: floated 옵셔널 처리

---
+++ packages/core-elements/src/elements/image-new/fixed-dimensions-frame.tsx
@@ -0,0 +1,72 @@
+import React, { PropsWithChildren, MouseEventHandler } from 'react'
+import styled from 'styled-components'
+import * as CSS from 'csstype'
+
+import { GlobalSizes, MarginPadding } from '../../commons'
+import { marginMixin } from '../../mixins'
+import { useImageState } from './context'
+
+const IMAGE_HEIGHT_OPTIONS: Partial<Record<GlobalSizes, string>> = {
+  mini: '80px',
+  small: '110px',
+  medium: '200px',
+  large: '400px',
+}
+
+const FixedDimensionsFrameContainer = styled.div<{
+  size?: GlobalSizes
+  width?: number
+  height?: number
+  borderRadius?: number
+  floated?: CSS.Property.Float
+  margin?: MarginPadding
+}>`
+  background-color: #f5f5f5;
+  overflow: hidden;
+
+  font-size: 0;
+  position: relative;
+
+  width: ${({ width }) => (width && `${width}px`) || '100%'};
+  height: ${({ height, size }) =>
+    (height && `${height}px`) || (size ? IMAGE_HEIGHT_OPTIONS[size] : '')};
+
+  ${({ borderRadius }) => `border-radius: ${borderRadius ?? 6}px;`}
+
+  float: ${({ floated }) => floated || 'none'};
+
+  ${marginMixin}
+`
+
+export default function ImageFixedDimensionsFrame({
+  size,
+  width,
+  height,
+  floated,
+  margin,
+  onClick,
+  children,
+}: PropsWithChildren<{
+  size?: GlobalSizes
+  width?: number
+  height?: number
+  floated?: CSS.Property.Float
+  margin?: MarginPadding
+  onClick?: MouseEventHandler
+}>) {
+  const { borderRadius } = useImageState()
+
+  return (
+    <FixedDimensionsFrameContainer
+      size={size}
+      width={width}
+      height={height}
+      floated={floated}
+      margin={margin}
+      borderRadius={borderRadius}
+      onClick={onClick}
+    >
+      {children}
+    </FixedDimensionsFrameContainer>
+  )
+}
