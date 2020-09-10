82ac568ae65e8e95cac96c255d7f87eb828549ae
woody <woody@triple-corp.com>
2020-09-10 17:47:51 +0900

    [#870] Docs: GlobalStyle 스토리북 작성

---
+++ docs/stories/core-elements/global-style.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { GlobalStyle } from '@titicaca/core-elements'
+
+export default {
+  title: 'Core-Elements | GlobalStyle',
+}
+
+export function defaultExample() {
+  return (
+    <>
+      <GlobalStyle />
+
+      <a href="">anchor tag</a>
+
+      <button>button tag</button>
+    </>
+  )
+}
