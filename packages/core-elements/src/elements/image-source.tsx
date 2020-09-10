77c70a8058256af6612cbea87fc7c52b3134ade8
woody <woody@triple-corp.com>
2020-09-10 18:04:30 +0900

    Feature: 런타임에 sourceUrl에 falsy 값이 들어왔을 때 대응 추가

--- packages/core-elements/src/elements/image-source.tsx
+++ packages/core-elements/src/elements/image-source.tsx
@@ -5,5 +5,9 @@ import formatSourceURL from '../utils/format-source-url'
 export type ImageSourceType = typeof ImageSource
 
 export default function ImageSource({ sourceUrl }: { sourceUrl: string }) {
+  if (!sourceUrl) {
+    return null
+  }
+
   return <>{formatSourceURL(sourceUrl)}</>
 }
