b0d18eca07827e1c9849270a8dd353689be0faac
Inbeom Hwang <hwanginbeom@gmail.com>
2020-09-10 16:55:22 +0900

    Export LayeringMixinProps

--- packages/core-elements/src/mixins/layering.ts
+++ packages/core-elements/src/mixins/layering.ts
@@ -1,9 +1,11 @@
 import { css } from 'styled-components'
 
-export const layeringMixin = (defaultTier: number) => css<{
+export interface LayeringMixinProps {
   zTier?: number
   zIndex?: number
-}>`
+}
+
+export const layeringMixin = (defaultTier: number) => css<LayeringMixinProps>`
   z-index: ${({ zTier = defaultTier, zIndex = 0 }) =>
     (zTier || defaultTier) * 100 + zIndex};
 `
