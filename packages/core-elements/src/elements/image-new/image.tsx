4ec493f8d9fa4363413ee77ffca2c3b8c3cc1f7d
woody <woody@triple-corp.com>
2020-09-10 09:33:27 +0900

    [#574] Feature: Image 컴포넌트 추가

---
+++ packages/core-elements/src/elements/image-new/image.tsx
@@ -0,0 +1,36 @@
+import React, { PropsWithChildren } from 'react'
+
+import { ImageStateContextProvider } from './context'
+import ImageImg from './img'
+import ImageSourceUrl from './source-url'
+import ImageOverlay from './overlay'
+import ImageLinkIndicator from './link-indicator'
+import ImageFixedRatioFrame from './fixed-ratio-frame'
+import ImageFixedDimensionsFrame from './fixed-dimensions-frame'
+import ImageCircular from './circular'
+import ImagePlaceholder from './placeholder'
+
+export default function Image({
+  borderRadius,
+  children,
+}: PropsWithChildren<{
+  borderRadius?: number
+}>) {
+  return (
+    <ImageStateContextProvider borderRadius={borderRadius}>
+      {children}
+    </ImageStateContextProvider>
+  )
+}
+
+Image.FixedRatioFrame = ImageFixedRatioFrame
+Image.FixedDimensionsFrame = ImageFixedDimensionsFrame
+
+Image.Img = ImageImg
+Image.Placeholder = ImagePlaceholder
+
+Image.SourceUrl = ImageSourceUrl
+Image.Overlay = ImageOverlay
+Image.LinkIndicator = ImageLinkIndicator
+
+Image.Circular = ImageCircular
