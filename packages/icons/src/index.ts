29cfd41a1b3df11666ccd850424ba1ae0bf8d6f3
Rhio Kim <rhio.kim@gmail.com>
2020-09-16 15:33:05 +0900

    feat(icons): search-place icon 추가

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,3 +1,4 @@
 export { default as Back } from './back'
 export { default as Search } from './search'
 export { default as Close } from './close'
+export { default as SearchPlace } from './search-place'
