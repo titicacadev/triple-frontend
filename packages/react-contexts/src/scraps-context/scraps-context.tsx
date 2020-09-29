48bdf95f94c8bff6a2ae40fd1bc8545e93d637ef
Inbeom Hwang <hwanginbeom@gmail.com>
2020-09-29 10:05:21 +0900

    Fix deprecated comment

--- packages/react-contexts/src/scraps-context/scraps-context.tsx
+++ packages/react-contexts/src/scraps-context/scraps-context.tsx
@@ -23,14 +23,14 @@ interface ScrapsContext {
     scrapsCount?: number
   }) => { scraped: boolean; scrapsCount: number }
   scrape: (target: Target) => Promise<void>
-  /** DEPRECATED: Use scrape instead */
+  /** @deprecated Use `scrape()` instead */
   scrapeArticle: (id: string) => Promise<void>
-  /** DEPRECATED: Use scrape instead */
+  /** @deprecated Use `scrape()` instead */
   scrapePoi: (id: string) => Promise<void>
   unscrape: (target: Target) => Promise<void>
-  /** DEPRECATED: Use unscrape instead */
+  /** @deprecated Use `unscrape()` instead */
   unscrapeArticle: (id: string) => Promise<void>
-  /** DEPRECATED: Use unscrape instead */
+  /** @deprecated Use `unscrape()` instead */
   unscrapePoi: (id: string) => Promise<void>
 }
 
