c6e8f96a57ea9294cfecb9d15cbb826c69460fbd
woody <woody@triple-corp.com>
2020-10-07 16:16:39 +0900

    [#981] Feature: poi-detail/actions에서 ScrapButton과 context 연동

--- packages/poi-detail/src/actions.tsx
+++ packages/poi-detail/src/actions.tsx
@@ -1,33 +1,67 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import styled from 'styled-components'
 import { Section, HR1, Button, MarginPadding } from '@titicaca/core-elements'
 import { useI18n } from '@titicaca/i18n'
+import { useScrapsContext } from '@titicaca/react-contexts'
 
 const ActionButton = styled(Button)`
   padding-left: 0;
   padding-right: 0;
 `
 
+function ScrapButton({
+  resource: { id, type, scraped },
+}: {
+  resource: { id: string; type: string; scraped?: boolean }
+}) {
+  const { scrape, unscrape, deriveCurrentStateAndCount } = useScrapsContext()
+  const { t } = useI18n()
+
+  const { scraped: actualScraped } = deriveCurrentStateAndCount({
+    id,
+    scraped,
+  })
+
+  const handleClick = useCallback(
+    (e: React.SyntheticEvent) => {
+      e.preventDefault()
+      e.stopPropagation()
+
+      actualScraped ? unscrape({ id, type }) : scrape({ id, type })
+    },
+    [actualScraped, unscrape, id, type, scrape],
+  )
+
+  return (
+    <ActionButton
+      icon={scraped ? 'saveFilled' : 'saveEmpty'}
+      onClick={handleClick}
+    >
+      {scraped
+        ? t('common:unscrap', '저장취소')
+        : t('common:scrap', '저장하기')}
+    </ActionButton>
+  )
+}
+
 export default function Actions({
-  scraped,
   reviewed,
   onScheduleAdd,
-  onScrapedChange,
   onContentShare,
   onReviewEdit,
   noDivider,
+  scrapableResource,
   ...props
 }: {
   poiId: string
-  scraped: boolean
   reviewed: boolean
   onScheduleAdd?: () => void
-  onScrapedChange?: () => void
   onContentShare: () => void
   onReviewEdit: () => void
   margin?: MarginPadding
   padding?: MarginPadding
   noDivider?: boolean
+  scrapableResource?: { id: string; type: string; scraped?: boolean }
 }) {
   const { t } = useI18n()
 
@@ -36,21 +70,18 @@ export default function Actions({
       <Button.Group
         horizontalGap={22}
         buttonCount={
-          [onScheduleAdd, onScrapedChange, onContentShare, onReviewEdit].filter(
-            Boolean,
-          ).length
+          [
+            onScheduleAdd,
+            scrapableResource,
+            onContentShare,
+            onReviewEdit,
+          ].filter(Boolean).length
         }
       >
-        {onScrapedChange ? (
-          <ActionButton
-            icon={scraped ? 'saveFilled' : 'saveEmpty'}
-            onClick={onScrapedChange}
-          >
-            {scraped
-              ? t('common:unscrap', '저장취소')
-              : t('common:scrap', '저장하기')}
-          </ActionButton>
+        {scrapableResource ? (
+          <ScrapButton resource={scrapableResource} />
         ) : null}
+
         {onScheduleAdd ? (
           <ActionButton icon="schedule" onClick={onScheduleAdd}>
             {t('common:addSchedule', '일정추가')}
