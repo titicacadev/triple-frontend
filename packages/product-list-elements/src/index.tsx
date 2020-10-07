6314f8734f95b32b9d508bb7518b97a4965eee14
woody <woody@triple-corp.com>
2020-10-07 16:24:41 +0900

    [#981] Refactor: product-list-element  함수 컴포넌트로 변경

--- packages/product-list-elements/src/index.tsx
+++ packages/product-list-elements/src/index.tsx
@@ -14,54 +14,47 @@ interface Product {
   salePrice?: number
 }
 
-export class ProductListElement<T extends Product> extends React.PureComponent<
-  {
-    product: T
-    scraped?: boolean
-    scrapsCount?: number
-    resourceScraps?: { [key: string]: boolean }
-  } & Pick<
-    ResourceListElementProps<T>,
-    'reviewsCount' | 'reviewsRating' | 'onClick' | 'onScrapedChange' | 'as'
-  >
-> {
-  render() {
-    const {
-      props: {
-        product: { id, title, image, subtitle, basePrice, salePrice },
-        scraped: initialScraped,
-        scrapsCount: initialScrapsCount,
-        reviewsCount,
-        reviewsRating,
-        onClick,
-        onScrapedChange,
-        resourceScraps,
-        as,
-      },
-    } = this
+export function ProductListElement<T extends Product>({
+  product,
+  product: { id, title, image, subtitle, basePrice, salePrice },
+  scraped: initialScraped,
+  scrapsCount: initialScrapsCount,
+  reviewsCount,
+  reviewsRating,
+  onClick,
+  onScrapedChange,
+  resourceScraps,
+  as,
+}: {
+  product: T
+  scraped?: boolean
+  scrapsCount?: number
+  resourceScraps?: { [key: string]: boolean }
+} & Pick<
+  ResourceListElementProps<T>,
+  'reviewsCount' | 'reviewsRating' | 'onClick' | 'onScrapedChange' | 'as'
+>) {
+  const { state: scraped, count: scrapsCount } = deriveCurrentStateAndCount({
+    initialState: initialScraped,
+    initialCount: initialScrapsCount,
+    currentState: (resourceScraps || {})[id],
+  })
 
-    const { state: scraped, count: scrapsCount } = deriveCurrentStateAndCount({
-      initialState: initialScraped,
-      initialCount: initialScrapsCount,
-      currentState: (resourceScraps || {})[id],
-    })
-
-    return (
-      <ExtendedResourceListElement
-        resource={this.props.product}
-        image={image}
-        name={title}
-        comment={subtitle}
-        basePrice={basePrice}
-        salePrice={salePrice}
-        reviewsCount={reviewsCount}
-        reviewsRating={reviewsRating}
-        scraped={scraped}
-        scrapsCount={scrapsCount}
-        onScrapedChange={onScrapedChange}
-        onClick={onClick}
-        as={as}
-      />
-    )
-  }
+  return (
+    <ExtendedResourceListElement
+      resource={product}
+      image={image}
+      name={title}
+      comment={subtitle}
+      basePrice={basePrice}
+      salePrice={salePrice}
+      reviewsCount={reviewsCount}
+      reviewsRating={reviewsRating}
+      scraped={scraped}
+      scrapsCount={scrapsCount}
+      onScrapedChange={onScrapedChange}
+      onClick={onClick}
+      as={as}
+    />
+  )
 }
