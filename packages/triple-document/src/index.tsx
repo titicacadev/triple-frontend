a921b21880d71bc41c808fa58eb3d6f0e3002e8d
Inbeom Hwang <hwanginbeom@gmail.com>
2020-10-07 09:32:23 +0900

    Export text-related elements

--- packages/triple-document/src/index.tsx
+++ packages/triple-document/src/index.tsx
@@ -1,8 +1,6 @@
 import { TripleDocument } from './triple-document'
 export { ELEMENTS } from './triple-document'
 export { TripleElementData as TripleDocumentElementData } from './types'
-export * from './text'
-export * from './tna'
-export * from './region'
+export * from './elements/text'
 
 export default TripleDocument
