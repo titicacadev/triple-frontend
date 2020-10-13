a23b3105b3fe7da7eb08cd3f83f149114f14a3c7
Inbeom Hwang <hwanginbeom@gmail.com>
2020-10-13 23:25:02 +0900

    Add onClick handler to Text element for inline links

--- packages/triple-document/src/elements/text/plain.tsx
+++ packages/triple-document/src/elements/text/plain.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Text, Paragraph } from '@titicaca/core-elements'
 
 export default function TextElement({
   value: { text, rawHTML },
   compact,
+  onLinkClick,
   ...props
 }: any) {
+  const handleClick = useCallback(
+    (e: React.SyntheticEvent) => {
+      const target = e.target as HTMLElement
+
+      if (target.tagName === 'A') {
+        e.preventDefault()
+        e.stopPropagation()
+
+        onLinkClick(e, {
+          href: target?.getAttribute('href'),
+          label: (target as HTMLAnchorElement)?.text,
+        })
+      }
+    },
+    [onLinkClick],
+  )
+
   if (rawHTML) {
     return (
       <Text.Html
         margin={compact ? { top: 4 } : { top: 10, left: 30, right: 30 }}
         alpha={0.9}
         dangerouslySetInnerHTML={{ __html: rawHTML }}
+        onClick={handleClick}
         {...props}
       />
     )
