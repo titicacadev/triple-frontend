5d212c16e423860b97171e46c253a45d98d66160
woody <woody@triple-corp.com>
2020-10-16 16:07:55 +0900

    Refactor: tabs 스토리 함수 형태로 개선

--- docs/stories/core-elements/tabs.stories.js
+++ docs/stories/core-elements/tabs.stories.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { select } from '@storybook/addon-knobs'
 import { Tabs } from '@titicaca/core-elements'
 
-storiesOf('Core-Elements | Tabs', module)
-  .add('이근처장소', () => (
+export default {
+  title: 'Core-Elements | Tabs',
+}
+
+export function nearbyPlaces() {
+  return (
     <Tabs
       value={select('현재 탭', ['attractions', 'restaurants'], 'attractions')}
       options={[
@@ -14,43 +17,53 @@ storiesOf('Core-Elements | Tabs', module)
       ]}
       onChange={action('change')}
     />
-  ))
-  .add('라인 탭', () => {
-    const options = [
-      { label: '투어티켓', value: '투어티켓' },
-      { label: '호텔', value: '호텔' },
-    ]
+  )
+}
 
-    const options2 = [
-      { label: '투어티켓1', value: '투어티켓' },
-      { label: '호텔2', value: '호텔2' },
-      { label: '투어티켓3', value: '투어티켓3' },
-      { label: '호텔4', value: '호텔4' },
-      { label: '투어티켓5', value: '투어티켓5' },
-      { label: '호텔6', value: '호텔6' },
-      { label: '투어티켓7', value: '투어티켓7' },
-      { label: '호텔8', value: '호텔8' },
-    ]
+nearbyPlaces.story = {
+  name: '이 근처 장소',
+}
 
-    const values = options.map(({ value }) => value)
-    const values2 = options2.map(({ value }) => value)
+export function lineTab() {
+  const options = [
+    { label: '투어티켓', value: '투어티켓' },
+    { label: '호텔', value: '호텔' },
+  ]
 
-    return (
-      <>
-        <Tabs
-          type="pointing"
-          options={options}
-          onChange={action('change')}
-          value={select('버튼 크기', values, values[0])}
-        />
-        <br /> <br /> <br /> <br />
-        <Tabs
-          scroll
-          type="pointing"
-          options={options2}
-          onChange={action('change')}
-          value={select('버튼 크기2', values2, values2[0])}
-        />
-      </>
-    )
-  })
+  const options2 = [
+    { label: '투어티켓1', value: '투어티켓' },
+    { label: '호텔2', value: '호텔2' },
+    { label: '투어티켓3', value: '투어티켓3' },
+    { label: '호텔4', value: '호텔4' },
+    { label: '투어티켓5', value: '투어티켓5' },
+    { label: '호텔6', value: '호텔6' },
+    { label: '투어티켓7', value: '투어티켓7' },
+    { label: '호텔8', value: '호텔8' },
+  ]
+
+  const values = options.map(({ value }) => value)
+  const values2 = options2.map(({ value }) => value)
+
+  return (
+    <>
+      <Tabs
+        type="pointing"
+        options={options}
+        onChange={action('change')}
+        value={select('버튼 크기', values, values[0])}
+      />
+      <br /> <br /> <br /> <br />
+      <Tabs
+        scroll
+        type="pointing"
+        options={options2}
+        onChange={action('change')}
+        value={select('버튼 크기2', values2, values2[0])}
+      />
+    </>
+  )
+}
+
+lineTab.story = {
+  name: '라인 탭',
+}
