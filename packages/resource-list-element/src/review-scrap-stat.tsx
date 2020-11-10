9ad285d71d670f61d0cc00e3625a9f041ed8f7ef
woody <woody@triple-corp.com>
2020-11-10 14:34:45 +0900

    Feature: resource-list-element에서 스크랩 개수를 사용하는 부분 scrapContext 없을 때 표시 안 함

--- packages/resource-list-element/src/review-scrap-stat.tsx
+++ packages/resource-list-element/src/review-scrap-stat.tsx
@@ -1,15 +1,42 @@
 import React from 'react'
 import { Container, Rating } from '@titicaca/core-elements'
+import {
+  useScrapsContext,
+  withScrapsContextGuard,
+} from '@titicaca/react-contexts'
 import { formatNumber } from '@titicaca/view-utilities'
 
 import ResourceListElementStats from './stats'
+
+const GuardedScrapsCount = withScrapsContextGuard(function ScrapsCountStat({
+  id,
+  scraped,
+  scrapsCount: rawScrapsCount,
+}: {
+  id: string
+  scraped: boolean
+  scrapsCount: number | undefined
+}) {
+  const { deriveCurrentStateAndCount } = useScrapsContext()
+  const { scrapsCount } = deriveCurrentStateAndCount({
+    id,
+    scraped,
+    scrapsCount: rawScrapsCount,
+  })
+
+  return scrapsCount ? <span>저장 {formatNumber(scrapsCount)}</span> : null
+})
 
 export default function ReviewScrapStat({
   reviewsCount,
   scrapsCount,
   reviewsRating,
+  id,
+  scraped,
   ...containerProps
 }: Parameters<typeof Container>[0] & {
+  id: string
+  scraped: boolean
   reviewsRating: number | undefined
   reviewsCount: number | undefined
   scrapsCount: number | undefined
@@ -26,7 +53,11 @@ export default function ReviewScrapStat({
 
       <ResourceListElementStats inline size="tiny" alpha={0.4}>
         {reviewsCount ? <span>({formatNumber(reviewsCount)})</span> : null}
-        {scrapsCount ? <span>저장 {formatNumber(scrapsCount)}</span> : null}
+        <GuardedScrapsCount
+          id={id}
+          scraped={scraped}
+          scrapsCount={scrapsCount}
+        />
       </ResourceListElementStats>
     </Container>
   )
