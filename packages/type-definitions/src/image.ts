fef00a4d18d63ba08f3532492b0c17bc3e4f5613
Inbeom Hwang <hwanginbeom@gmail.com>
2020-11-11 12:39:13 +0900

    Broaden ImageMeta properties

--- packages/type-definitions/src/image.ts
+++ packages/type-definitions/src/image.ts
@@ -13,14 +13,56 @@ interface SnakeSmallSquare {
 
 type SmallSquare = CamelSmallSquare | SnakeSmallSquare
 
+export type BaseSizes = 'small' | 'medium' | 'large'
+
+export type GlobalSizes =
+  | 'mini'
+  | 'tiny'
+  | 'big'
+  | 'huge'
+  | 'massive'
+  | BaseSizes
+
+export type GlobalColors = 'blue' | 'gray' | 'white' | 'red'
+
+export type Ratio =
+  | '4:1'
+  | '5:3'
+  | '11:7'
+  | '4:3'
+  | '1:1'
+  | '10:11'
+  | '5:8'
+  | '9:5'
+
+export type FrameRatioAndSizes =
+  | Exclude<GlobalSizes, 'tiny' | 'massive'>
+  | 'original'
+  | Ratio
+
 export interface ImageMeta {
   id: string
-  title: string | null
-  description: string | null
-  sourceUrl: string
+  type?: string
+  title?: string | null
+  description?: string | null
+  sourceUrl?: string
+  width?: number
+  height?: number
+  cloudinaryId?: string
+  cloudinaryBucket?: string
   sizes: {
     full: ImageURL
     large: ImageURL
   } & SmallSquare
+  video?: {
+    full: { url: string }
+    large: { url: string }
+  } & SmallSquare
   attachmentId?: string
+  /* TODO: Remove FrameRatioAndSizes from core-elements */
+  frame?: FrameRatioAndSizes
+  link?: {
+    href: string
+    label?: string
+  }
 }
