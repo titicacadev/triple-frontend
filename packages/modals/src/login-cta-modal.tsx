2e88aaf102ee8c5045b4c5435a1c34ffa07d3ea4
Inbeom Hwang <hwanginbeom@gmail.com>
2020-11-12 10:19:04 +0900

    Render LoginCTAModal without path to /login on legacy Android devices

--- packages/modals/src/login-cta-modal.tsx
+++ packages/modals/src/login-cta-modal.tsx
@@ -1,4 +1,3 @@
-import { useHistoryFunctions, useURIHash } from '@titicaca/react-contexts'
 import React, {
   ComponentType,
   createContext,
@@ -6,10 +5,18 @@ import React, {
   useContext,
   useMemo,
 } from 'react'
+import {
+  useHistoryFunctions,
+  useURIHash,
+  useUserAgentContext,
+} from '@titicaca/react-contexts'
+import semver from 'semver'
 
-import { Confirm } from './modals'
+import { Confirm, Alert } from './modals'
 
 const LOGIN_CTA_MODAL_HASH = 'login-cta-modal'
+
+const WITH_LOGIN_PATH_APP_VERSION = '5.0.0'
 
 const LoginCTAFlagContext = createContext(false)
 
@@ -17,29 +24,44 @@ export function LoginCTAModalProvider({ children }: PropsWithChildren<{}>) {
   const uriHash = useURIHash()
   const { back, navigate } = useHistoryFunctions()
   const hasParentModal = useContext(LoginCTAFlagContext)
-
+  const { isPublic, os, app } = useUserAgentContext()
+  const appVersion = semver.coerce(app?.version)
   const open = uriHash === LOGIN_CTA_MODAL_HASH
 
   if (hasParentModal) {
     return <>{children}</>
   }
 
+  const isLegacyAndroidApp = Boolean(
+    !isPublic &&
+      os?.name === 'Android' &&
+      appVersion &&
+      semver.lt(appVersion, WITH_LOGIN_PATH_APP_VERSION),
+  )
+
   return (
     <LoginCTAFlagContext.Provider value={true}>
       {children}
 
-      <Confirm
-        open={open}
-        title="로그인이 필요합니다."
-        onClose={back}
-        onCancel={back}
-        onConfirm={() => {
-          navigate('/login')
-        }}
-      >
-        로그인하고 트리플을
-        <br />더 편하게 이용하세요🙂
-      </Confirm>
+      {isLegacyAndroidApp ? (
+        <Alert open={open} title="로그인이 필요합니다." onConfirm={back}>
+          로그인하고 트리플을
+          <br />더 편하게 이용하세요🙂
+        </Alert>
+      ) : (
+        <Confirm
+          open={open}
+          title="로그인이 필요합니다."
+          onClose={back}
+          onCancel={back}
+          onConfirm={() => {
+            navigate('/login')
+          }}
+        >
+          로그인하고 트리플을
+          <br />더 편하게 이용하세요🙂
+        </Confirm>
+      )}
     </LoginCTAFlagContext.Provider>
   )
 }
