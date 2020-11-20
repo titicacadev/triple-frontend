dc828151e7dad2507d006a947eae730f27db714b
woody <woody@triple-corp.com>
2020-11-20 14:05:34 +0900

    ✨ history, session context위에 env provider 추가

--- docs/decorators/index.tsx
+++ docs/decorators/index.tsx
@@ -3,30 +3,42 @@ import { StoryFn } from '@storybook/addons'
 import {
   HistoryProvider,
   SessionContextProvider,
+  EnvProvider,
 } from '@titicaca/react-contexts'
 import { boolean } from '@storybook/addon-knobs'
 
 export function historyProviderDecorator(storyFn: StoryFn<JSX.Element>) {
   return (
-    <HistoryProvider
+    <EnvProvider
       appUrlScheme="dev-soto"
       webUrlBase="https://triple-dev.titicaca-corp.com"
-      isPublic={false}
-      isAndroid={false}
-      transitionModalHash="transition.general"
+      authBasePath="MOCK_AUTH_BASE_PATH"
     >
-      {storyFn()}
-    </HistoryProvider>
+      <HistoryProvider
+        isPublic={false}
+        isAndroid={false}
+        transitionModalHash="transition.general"
+      >
+        {storyFn()}
+      </HistoryProvider>
+    </EnvProvider>
   )
 }
 
 export function sessionContextProviderDecorator(storyFn: StoryFn<JSX.Element>) {
   return (
-    <SessionContextProvider
-      sessionId={boolean('hasSessionId', false) ? 'MOCK_SESSION_ID' : undefined}
+    <EnvProvider
+      appUrlScheme="dev-soto"
+      webUrlBase="https://triple-dev.titicaca-corp.com"
       authBasePath="MOCK_AUTH_BASE_PATH"
     >
-      {storyFn()}
-    </SessionContextProvider>
+      <SessionContextProvider
+        sessionId={
+          boolean('hasSessionId', false) ? 'MOCK_SESSION_ID' : undefined
+        }
+      >
+        {storyFn()}
+      </SessionContextProvider>
+    </EnvProvider>
   )
 }
