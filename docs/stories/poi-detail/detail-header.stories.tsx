47b027cb1767df3d5e99b54129ec80059a693b05
woody <woody@triple-corp.com>
2020-11-20 13:59:12 +0900

    🎨 DetailHeader 컴포넌트 스토리 storybook 5 인터페이스로 업그레이드

--- docs/stories/poi-detail/detail-header.stories.tsx
+++ docs/stories/poi-detail/detail-header.stories.tsx
@@ -1,40 +1,51 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 import { number, boolean } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { DetailHeader } from '@titicaca/poi-detail'
-import { HistoryProvider, UserAgentProvider } from '@titicaca/react-contexts'
+import { UserAgentProvider } from '@titicaca/react-contexts'
+import { StoryFn } from '@storybook/addons'
 
-storiesOf('poi-detail | DetailHeader', module).add('일반', () => (
-  <HistoryProvider
-    appUrlScheme="dev-soto"
-    webUrlBase="https://triple-dev.titicaca-corp.com"
-  >
-    <UserAgentProvider
-      value={{
-        isPublic: boolean('isPublic', true),
-        isMobile: true,
-        os: {},
-        app: null,
-      }}
-    >
-      <DetailHeader
-        names={{
-          primary: '도쿄 디즈니 랜드',
-          ko: '도쿄 디즈니 랜드',
-          en: 'Tokyo Disney land',
-          local: '東京ディズニーランド',
+import { historyProviderDecorator } from '../../decorators'
+
+export default {
+  title: 'poi-detail | DetailHeader',
+  decorators: [
+    historyProviderDecorator,
+    (storyFn: StoryFn<JSX.Element>) => (
+      <UserAgentProvider
+        value={{
+          isPublic: boolean('isPublic', true),
+          isMobile: true,
+          os: {},
+          app: null,
         }}
-        areas={[
-          { id: 1, name: '도쿄' },
-          { id: 2, name: '오사카' },
-        ]}
-        scrapsCount={number('저장수', 682)}
-        reviewsCount={number('리뷰수', 13859)}
-        reviewsRating={number('리뷰평점', 4.45)}
-        onReviewsRatingClick={action('onReviewsRatingClick')}
-        onCopy={action('onCopy')}
-      />
-    </UserAgentProvider>
-  </HistoryProvider>
-))
+      >
+        {storyFn()}
+      </UserAgentProvider>
+    ),
+  ],
+}
+
+export function baseExample() {
+  return (
+    <DetailHeader
+      names={{
+        primary: '도쿄 디즈니 랜드',
+        ko: '도쿄 디즈니 랜드',
+        en: 'Tokyo Disney land',
+        local: '東京ディズニーランド',
+      }}
+      areas={[
+        { id: 1, name: '도쿄' },
+        { id: 2, name: '오사카' },
+      ]}
+      scrapsCount={number('저장수', 682)}
+      reviewsCount={number('리뷰수', 13859)}
+      reviewsRating={number('리뷰평점', 4.45)}
+      onReviewsRatingClick={action('onReviewsRatingClick')}
+      onCopy={action('onCopy')}
+    />
+  )
+}
+
+baseExample.story = { name: '기본' }
