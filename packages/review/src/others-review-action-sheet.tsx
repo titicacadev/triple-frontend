8f3cc3d29285ea2061f9cead395620b0452c5e17
woody <woody@triple-corp.com>
2020-11-20 16:14:44 +0900

    🐛 리뷰 액션시트 zTier 3으로 지정
    리뷰 액션시트가 호텔 상세 페이지의 drawer 버튼에 가려지는 문제가 있습니다.

--- packages/review/src/others-review-action-sheet.tsx
+++ packages/review/src/others-review-action-sheet.tsx
@@ -31,6 +31,7 @@ export default function OthersReviewActionSheet({
     <ActionSheet
       open={uriHash === HASH_REVIEW_ACTION_SHEET && !!selectedReview}
       onClose={back}
+      zTier={3}
     >
       <ActionSheet.Item icon="report" onClick={handleReportClick}>
         신고하기
