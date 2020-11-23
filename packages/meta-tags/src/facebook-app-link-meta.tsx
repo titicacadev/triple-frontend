cfaaccc2cb72ff72342bbfabafe880c71d2114b4
woody <woody@triple-corp.com>
2020-11-23 14:57:26 +0900

    🗑 FacebookAppLinkMeta에 env context 연동 및 prop deprecate
    appUrlScheme을 context에서 참조합니다.
    만약 이전처럼 prop을 이용하면 deprecate 알림 메시지를 표시합니다.

--- packages/meta-tags/src/facebook-app-link-meta.tsx
+++ packages/meta-tags/src/facebook-app-link-meta.tsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Head from 'next/head'
+import { useEnv } from '@titicaca/react-contexts'
 
 export function FacebookAppLinkMeta({
   appName = '트리플',
   iosAppStoreId = '1225499481',
   appPath = '/',
   appPackageName = 'com.titicacacorp.triple',
-  appUrlScheme = 'triple',
+  appUrlScheme: appUrlSchemeFromProps = 'triple',
 }: {
   appName?: string
   iosAppStoreId?: string
   appPath?: string
   appPackageName?: string
+  /**
+   * @deprecated EnvProvider가 있으면 이 prop을 넣어주지 않아도 됩니다.
+   */
   appUrlScheme?: string
 }) {
+  const { appUrlScheme: appUrlSchemeFromContext } = useEnv()
+
+  const appUrlScheme = useMemo(() => {
+    if (appUrlSchemeFromContext) {
+      return appUrlSchemeFromContext
+    }
+    if (typeof appUrlSchemeFromProps === 'string') {
+      // TODO: 개발용 logger 만들기
+      if (process.env.NODE_ENV === 'development') {
+        // eslint-disable-next-line no-console
+        console.warn(
+          'appUrlScheme prop은 deprecate되었습니다.\n다음 메이저 버전부터 env context를 사용해야 합니다.',
+        )
+      }
+
+      return appUrlSchemeFromProps
+    }
+
+    return 'triple'
+  }, [appUrlSchemeFromContext, appUrlSchemeFromProps])
+
   return (
     <Head>
       <meta
