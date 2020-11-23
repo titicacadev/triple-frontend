c1b78b6b2afd88c2b23c39364a19d0e54a3a3ef6
woody <woody@triple-corp.com>
2020-11-23 14:57:08 +0900

    🗑 AppleSmartBannerMeta에 env context 연동 및 prop deprecate
    appUrlScheme을 context에서 참조합니다.
    만약 이전처럼 prop을 이용하면 deprecate 알림 메시지를 표시합니다.

--- packages/meta-tags/src/apple-smart-banner-meta.tsx
+++ packages/meta-tags/src/apple-smart-banner-meta.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Head from 'next/head'
+import { useEnv } from '@titicaca/react-contexts'
 
 export function AppleSmartBannerMeta({
   appId = '1225499481',
-  appUrlScheme = 'triple',
+  appUrlScheme: appUrlSchemeFromProps,
   appPath = '/',
 }: {
   appId?: string
+  /**
+   * @deprecated EnvProvider가 있으면 이 prop을 넣어주지 않아도 됩니다.
+   */
   appUrlScheme?: string
   appPath?: string
 }) {
+  const { appUrlScheme: appUrlSchemeFromContext } = useEnv()
+
+  const appUrlScheme = useMemo(() => {
+    if (appUrlSchemeFromContext) {
+      return appUrlSchemeFromContext
+    }
+    if (typeof appUrlSchemeFromProps === 'string') {
+      // TODO: 개발용 logger 만들기
+      if (process.env.NODE_ENV === 'development') {
+        // eslint-disable-next-line no-console
+        console.warn(
+          'appUrlScheme prop은 deprecate되었습니다.\n다음 메이저 버전부터 env context를 사용해야 합니다.',
+        )
+      }
+
+      return appUrlSchemeFromProps
+    }
+
+    return 'triple'
+  }, [appUrlSchemeFromContext, appUrlSchemeFromProps])
+
   return (
     <Head>
       <meta
