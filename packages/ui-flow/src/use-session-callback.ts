8aaa74a81fbefa5ade957259e1eb3b70415a8c53
Inbeom Hwang <hwanginbeom@gmail.com>
2020-11-23 09:31:27 +0900

    Add specifics on returnValue

--- packages/ui-flow/src/use-session-callback.ts
+++ packages/ui-flow/src/use-session-callback.ts
@@ -6,11 +6,15 @@ import { useLoginCTAModal } from '@titicaca/modals'
  * sessionId가 있는 환경에서만 주어진 콜백을 실행하는 함수를 반환하는 훅
  * sessionId가 없으면 로그인 유도 모달을 띄웁니다.
  * @param fn
+ * @param returnValue sessionId가 없을 때 리턴할 값
  */
-export function useSessionCallback<T extends (...args: any[]) => any>(
+export function useSessionCallback<
+  T extends (...args: any[]) => any,
+  S = unknown
+>(
   fn: T,
-  returnValue?: any,
-): (...args: Parameters<T>) => ReturnType<T> | void {
+  returnValue?: S,
+): (...args: Parameters<T>) => ReturnType<T> | S | void {
   const { hasSessionId } = useSessionContext()
   const { show } = useLoginCTAModal()
 
