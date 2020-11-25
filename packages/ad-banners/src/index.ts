587167da85b82e08d75e573fd43cefbf190cea4f
Unknown <torres@triple-corp.com>
2020-11-25 13:37:49 +0900

    [#952] default export에 @depreacted 주석을 달아줍니다

--- packages/ad-banners/src/index.ts
+++ packages/ad-banners/src/index.ts
@@ -2,4 +2,8 @@ import ListTopBanners from './list-top-banners'
 export { ListDirection } from './typing'
 
 export { ListTopBanners }
+
+/**
+ * @deprecated named export를 사용해주세요.
+ */
 export default ListTopBanners
