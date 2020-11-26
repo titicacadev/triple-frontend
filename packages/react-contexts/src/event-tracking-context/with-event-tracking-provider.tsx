ebec8c0b194e1b77c8c971c9d6135449419195d4
Unknown <torres@triple-corp.com>
2020-11-26 11:45:53 +0900

    :sparkles: withEventTrackingProvider options추가

--- packages/react-contexts/src/event-tracking-context/with-event-tracking-provider.tsx
+++ packages/react-contexts/src/event-tracking-context/with-event-tracking-provider.tsx
@@ -5,10 +5,13 @@ import { EventTrackingProvider } from './event-tracking-context'
 export function withEventTrackingProvider<P>(
   pageLabel: string,
   Component: React.ComponentType<P>,
+  options?: {
+    onError?: (error: Error) => void
+  },
 ): React.ComponentType<P> {
   const Page: React.ComponentType<P> = function (props: P) {
     return (
-      <EventTrackingProvider pageLabel={pageLabel}>
+      <EventTrackingProvider pageLabel={pageLabel} {...(options || {})}>
         <Component {...props} />
       </EventTrackingProvider>
     )
