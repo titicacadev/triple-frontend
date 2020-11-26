d69687b4d43121bf570b34b855a7ee4aea29b18a
Unknown <torres@triple-corp.com>
2020-11-26 19:09:41 +0900

    [#952] regionId를 required props로 바꿉니다

--- packages/ad-banners/src/list-top-banners.tsx
+++ packages/ad-banners/src/list-top-banners.tsx
@@ -27,9 +27,8 @@ interface EventAttributes {
  */
 interface AdSystemBannerProps {
   contentType: ContentType
+  regionId: string
   contentId?: string
-  regionId?: string
-
   eventAttributes?: EventAttributes
 }
 
