e2b0625056297573aa349404c3f5e18da5c139be
woody <woody@triple-corp.com>
2020-11-26 11:04:00 +0900

    💬 deprecate 경고 console에 가이드 링크 추가

--- packages/react-contexts/src/session-context/session-context.tsx
+++ packages/react-contexts/src/session-context/session-context.tsx
@@ -76,7 +76,7 @@ export function SessionContextProvider({
       if (process.env.NODE_ENV === 'development') {
         // eslint-disable-next-line no-console
         console.warn(
-          'authBasePath prop은 deprecate되었습니다.\n다음 메이저 버전부터 env context를 사용해야 합니다.',
+          'authBasePath prop은 deprecate되었습니다.\n다음 메이저 버전부터 env context를 사용해야 합니다.\nhttps://github.com/titicacadev/triple-frontend/blob/ab1648a7cdb684ee2752eb5b80eed02940106964/packages/react-contexts/src/env-context/README.md#%EB%A7%88%EC%9D%B4%EA%B7%B8%EB%A0%88%EC%9D%B4%EC%85%98-%ED%95%98%EB%8A%94-%EB%B2%95',
         )
       }
 
