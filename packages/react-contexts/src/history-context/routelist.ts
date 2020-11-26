0b02f9a2b8b61f35b776872baf58ccd3bf896df0
Inbeom Hwang <hwanginbeom@gmail.com>
2020-11-26 13:32:29 +0900

    Add general URL canonization function

--- packages/react-contexts/src/history-context/routelist.ts
+++ packages/react-contexts/src/history-context/routelist.ts
@@ -1,5 +1,4 @@
-import { parse } from 'qs'
-import { parseUrl, generateUrl } from '@titicaca/view-utilities'
+import { parseUrl } from '@titicaca/view-utilities'
 
 const PUBLIC_ROUTELIST_REGEXES = [
   /^\/login$/,
@@ -19,31 +18,4 @@ export function checkIfRoutable({ href }: { href: string }) {
   }
 
   return true
-}
-
-export function generateTargetAddressOnPublic({
-  href,
-  webUrlBase,
-}: {
-  href: string
-  webUrlBase: string
-}): string {
-  const { host: webUrlBaseHost } = parseUrl(webUrlBase)
-  const { host, path, query, ...rest } = parseUrl(href)
-
-  if (host && webUrlBaseHost === host) {
-    return generateUrl({ path, query, ...rest, scheme: undefined })
-  } else if (host) {
-    return generateUrl({ host, path, query, ...rest })
-  } else if (path === '/inlink') {
-    const { path, _web_expand: expandable } = parse(query as string)
-
-    return expandable !== undefined ? (path as string) : href
-  } else if (path === '/outlink') {
-    const { url } = parse(query as string)
-
-    return generateTargetAddressOnPublic({ href: url as string, webUrlBase })
-  }
-
-  return href
 }
