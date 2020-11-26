c8e6c952e18ec15b0bd8b92c28136b72c01e2dfc
woody <woody@triple-corp.com>
2020-11-26 10:47:45 +0900

    💡 EnvProvider prop 설명 주석 추가
    컴포넌트를 사용할 때 prop 설명을 보고 도움을 받을 수 있습니다.

--- packages/react-contexts/src/env-context/index.tsx
+++ packages/react-contexts/src/env-context/index.tsx
@@ -6,11 +6,29 @@ import React, {
 } from 'react'
 
 interface EnvContextValue {
+  /**
+   * 앱을 여는데 필요한 scheme
+   */
   appUrlScheme: string
+  /**
+   * 서비스 path 앞에 붙여서 URL을 만드는 값
+   */
   webUrlBase: string
+  /**
+   * 로그인 페이지 path. SessionContext에서 로그인 페이지를 열 때 사용합니다.
+   */
   authBasePath: string
+  /**
+   * facebook에 등록된 App ID. Facebook Open Graph 관련 태그로 사용합니다.
+   */
   facebookAppId: string
+  /**
+   * 페이지의 기본 제목. title 태그의 기본값이 됩니다.
+   */
   defaultPageTitle: string
+  /**
+   * 페이지의 기본 설명. meta[name="description"] 태그의 기본 content가 됩니다.
+   */
   defaultPageDescription: string
 }
 
