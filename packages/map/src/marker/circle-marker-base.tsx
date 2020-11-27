cf951da7050af5ec3e40ea47c24aba00eba91431
Rhio Kim <rhio.kim@gmail.com>
2020-11-27 16:38:17 +0900

    feat(map): 맵 컴포넌트 추가

---
+++ packages/map/src/marker/circle-marker-base.tsx
@@ -0,0 +1,115 @@
+import styled, { css, keyframes } from 'styled-components'
+import { white } from '@titicaca/color-palette'
+
+export interface MarkerBaseProps {
+  /** 마커 사이즈, 단위 px */
+  width?: number
+  height?: number
+  /** 마커 선택 상태값 */
+  active?: boolean
+  /** 마커 기본 색상 */
+  color: string
+  /** 활성화 마커 백그라운드 이미지 */
+  src: string
+  /** 마커 zIndex */
+  zIndex?: number
+}
+
+/** 맵 상에 표시해야할 마커의 총 개수 */
+const MAX_ZINDEX_WEIGHT = 100
+
+const bounce = keyframes`
+  from {
+    transform: scale(0.01);
+  }
+  to {
+    transform: scale(1);
+  }
+`
+
+const bounceAnimationMixin = css<{
+  active: boolean
+  animationDuration?: number
+}>`
+  will-change: transform;
+  ${({ active, animationDuration = 400 }) => {
+    return active
+      ? css`
+          animation: ${bounce} ${animationDuration}ms
+            cubic-bezier(0.68, -0.55, 0.265, 1.55);
+        `
+      : ''
+  }};
+`
+
+function withActive({
+  color,
+  width,
+  height,
+  active,
+}: Required<MarkerBaseProps>) {
+  return active
+    ? css`
+        left: 6px;
+        top: 5px;
+        width: 30px;
+        height: 30px;
+        line-height: 30px;
+        background-color: transparent;
+      `
+    : css`
+        left: 0;
+        top: 0;
+        width: ${width}px;
+        height: ${height}px;
+        line-height: ${height}px;
+        background-color: ${color};
+        box-shadow: 0 2px 1px 0 rgba(0, 0, 0, 0.15);
+      `
+}
+
+export const Circle = styled.div`
+  position: absolute;
+  z-index: 1;
+  color: ${white};
+  font-size: 12px;
+  text-align: center;
+  border-radius: 50%;
+`
+
+export const CirclePin = styled.div<
+  Required<MarkerBaseProps> & { animationDuration?: number }
+>`
+  position: absolute;
+  transform-origin: 21px 50px;
+
+  ${({ active }) => (active ? 'pointer-events: none;' : '')}
+
+  /**
+   * FIXME: 마커, 핀 좌표 정확도 리펙토링 필요 */
+  ${({ src, width, height, active, zIndex }) => {
+    return active
+      ? css`
+          left: -21px;
+          top: -50px;
+          background: url(${src}) no-repeat 0 0;
+          background-size: 42px 50px;
+          width: 42px;
+          height: 50px;
+          z-index: ${MAX_ZINDEX_WEIGHT + zIndex};
+        `
+      : css`
+          left: -${width / 2}px;
+          top: -${height / 2}px;
+          width: ${width}px;
+          height: ${height}px;
+          z-index: ${zIndex};
+        `
+  }}
+
+  ${bounceAnimationMixin}
+
+  ${Circle} {
+    ${(props) => withActive(props)}
+  }
+`
---
+++ packages/map/src/marker/circle-marker.tsx
@@ -0,0 +1,54 @@
+import React, { PropsWithChildren, useCallback, useRef } from 'react'
+import { OverlayView, OverlayViewProps } from '@react-google-maps/api'
+
+import { Circle, CirclePin, MarkerBaseProps } from './circle-marker-base'
+
+export interface CircleMarkerProps
+  extends MarkerBaseProps,
+    Omit<OverlayViewProps, 'mapPaneName'> {
+  onClick?: (e: MouseEvent) => void
+}
+
+export function CircleMarker({
+  color,
+  src,
+  zIndex = 1,
+  active = false,
+  width = 28,
+  height = 28,
+  onLoad,
+  onClick,
+  children,
+  ...overlayViewProps
+}: PropsWithChildren<CircleMarkerProps>) {
+  const overlayViewRef = useRef<OverlayView>(null)
+
+  const handleClick = useCallback(
+    (e) => {
+      onClick && onClick(e)
+    },
+    [onClick],
+  )
+
+  return (
+    <OverlayView
+      {...overlayViewProps}
+      mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+      ref={overlayViewRef}
+      onLoad={onLoad}
+    >
+      {/* TODO: active/inactive 상태에 따른 컴포넌트를 갖도록 개선 */}
+      <CirclePin
+        zIndex={zIndex}
+        width={width}
+        height={height}
+        active={active}
+        color={color}
+        src={src}
+        onClick={handleClick}
+      >
+        <Circle>{children}</Circle>
+      </CirclePin>
+    </OverlayView>
+  )
+}
