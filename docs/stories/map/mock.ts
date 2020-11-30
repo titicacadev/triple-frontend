4822af5e9989c0d3169d0d5fc998936f19d3389e
Rhio Kim <rhio.kim@gmail.com>
2020-11-30 17:50:36 +0900

    docs(map): width/height 사이즈 설정 예시 추가 및 코드 개선

---
+++ docs/stories/map/mock.ts
@@ -0,0 +1,20 @@
+import { getGeometry } from '@titicaca/map'
+
+import HOTELS from '../__mocks__/map/hotel-recommandations.json'
+
+import { RecommendationHotelResourceType } from './types'
+
+export const coordinates: [
+  number,
+  number,
+][] = ((HOTELS as unknown) as RecommendationHotelResourceType[])
+  .map(({ hotel }) => hotel)
+  .map(
+    ({
+      source: {
+        pointGeolocation: { coordinates },
+      },
+    }) => coordinates as [number, number],
+  )
+
+export const { center, bounds } = getGeometry(coordinates)
