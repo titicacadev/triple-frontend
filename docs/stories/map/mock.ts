850c22589aa85fa2f1124f9c71bf75f9d4fc7ba0
Rhio Kim <rhio.kim@gmail.com>
2020-12-02 17:32:26 +0900

    fix(map): Polyline 컴포넌트 Knob 처리

--- docs/stories/map/mock.ts
+++ docs/stories/map/mock.ts
@@ -18,3 +18,17 @@ export const coordinates: [
   )
 
 export const mapOptions = getGeometry(coordinates)
+export const polylinePaths = [
+  { lat: 16.0563348, lng: 108.2025533 },
+  { lat: 16.0131183, lng: 108.2637083 },
+  { lat: 16.0349407, lng: 108.2294803 },
+  { lat: 16.0347492, lng: 108.2291364 },
+  { lat: 16.0616944, lng: 108.2469346 },
+  { lat: 16.0691917, lng: 108.2429779 },
+  { lat: 16.0694295, lng: 108.2422102 },
+  { lat: 16.0131183, lng: 108.2637083 },
+]
+
+export const polylineGeometry = getGeometry(
+  polylinePaths.map((path) => [path.lng, path.lat]),
+)
