dce308728e04112a47aca1cc1a83b1c1f63c0035
Rhio Kim <rhio.kim@gmail.com>
2020-12-02 17:19:43 +0900

    fix(map): 구글 키 하드코딩 및 불필요한 Container 제거

--- docs/stories/map/map.stories.tsx
+++ docs/stories/map/map.stories.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React from 'react'
 import { text, number } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { Container } from '@titicaca/core-elements'
@@ -7,40 +6,26 @@ import { MapProvider } from '@titicaca/map'
 
 import { mapOptions } from './mock'
 
-const MapContainer = styled(Container)`
-  background-color: #ffffff;
-  width: ${({ width }) => width || '100%'};
-  height: ${({ height }) => height || '200px'};
-`
+/**
+ * FIXME: security issue
+ */
+const GOOGLE_MAPS_API_KEY = 'AIzaSyA1Shc9dPadNkDCemFqXX8D7DJzT6OA4a8'
 
 export default {
   title: 'Map | Map',
 }
 
 export function Map() {
-  const [key, setKey] = useState<string>('')
-
   return (
-    <MapContainer>
-      <input
-        type="text"
-        defaultValue={key}
-        onBlur={(e) => {
-          setKey(e.currentTarget.value)
+    <Container width="100vw" height="100vh">
+      <MapProvider
+        options={mapOptions}
+        onLoad={action('맵 로드 완료 액션')}
+        googleMapLoadOptions={{
+          googleMapsApiKey: GOOGLE_MAPS_API_KEY,
         }}
-        size={50}
-        placeholder="google map api key"
-      />
-      {key ? (
-        <MapProvider
-          options={mapOptions}
-          onLoad={action('맵 로드 완료 액션')}
-          googleMapLoadOptions={{
-            googleMapsApiKey: key,
-          }}
-        ></MapProvider>
-      ) : null}
-    </MapContainer>
+      ></MapProvider>
+    </Container>
   )
 }
 
@@ -49,35 +34,19 @@ Map.story = {
 }
 
 export function MapWithProps() {
-  const [key, setKey] = useState<string>('')
-
   return (
-    <MapContainer>
-      <input
-        type="text"
-        defaultValue={key}
-        onBlur={(e) => {
-          setKey(e.currentTarget.value)
+    <Container
+      width={text('맵 컨테이너 가로 사이즈', '50%')}
+      height={number('맵 컨테이너 세로 사이즈', 200)}
+    >
+      <MapProvider
+        options={mapOptions}
+        onLoad={action('맵 로드 완료 액션')}
+        googleMapLoadOptions={{
+          googleMapsApiKey: GOOGLE_MAPS_API_KEY,
         }}
-        size={50}
-        placeholder="google map api key"
       />
-
-      <Container
-        width={text('맵 컨테이너 가로 사이즈', '50%')}
-        height={number('맵 컨테이너 세로 사이즈', 200)}
-      >
-        {key ? (
-          <MapProvider
-            options={mapOptions}
-            onLoad={action('맵 로드 완료 액션')}
-            googleMapLoadOptions={{
-              googleMapsApiKey: key,
-            }}
-          ></MapProvider>
-        ) : null}
-      </Container>
-    </MapContainer>
+    </Container>
   )
 }
 
