129dfbc06ba6f54bf2860a6fbdfe59ca35130fac
Unknown <torres@triple-corp.com>
2020-12-09 17:35:29 +0900

    [#955] RecommendedArticles Story 수정

--- docs/stories/poi-detail/recommended-articles.stories.tsx
+++ docs/stories/poi-detail/recommended-articles.stories.tsx
@@ -3,15 +3,18 @@ import { storiesOf } from '@storybook/react'
 import { text } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { RecommendedArticles } from '@titicaca/poi-detail'
+import { Container } from '@titicaca/core-elements'
 
 storiesOf('poi-detail | RecommendedArticles', module).add('일반', () => (
-  <RecommendedArticles
-    appInstallationCta={{
-      inventoryId: text('Inventory ID', 'app-install-cta-footer-hotel-v1'),
-      href: text('설치 URL', 'https://triple-dev.titicaca-corp.com'),
-      onClick: action('onCTAClick'),
-    }}
-    regionId={text('Region ID', '23c5965b-01ad-486b-a694-a2ced15f245c')}
-    onArticleClick={action('onArticleClick')}
-  />
+  <Container padding={{ left: 30, right: 30 }}>
+    <RecommendedArticles
+      appInstallationCta={{
+        inventoryId: text('Inventory ID', 'app-install-cta-footer-hotel-v1'),
+        href: text('설치 URL', 'https://triple-dev.titicaca-corp.com'),
+        onClick: action('onCTAClick'),
+      }}
+      regionId={text('Region ID', '23c5965b-01ad-486b-a694-a2ced15f245c')}
+      onArticleClick={action('onArticleClick')}
+    />
+  </Container>
 ))
