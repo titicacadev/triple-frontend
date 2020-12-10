a8a9b2f056656fd0d7e990b96bde5433396c4e68
woody <woody@triple-corp.com>
2020-12-10 17:03:41 +0900

    [#794] ✨ LocalLink 컴포넌트 구현
    같은 도메인의 페이지로 이동할 때 사용하는 링크 컴포넌트입니다.
    RouterGuardedLink 컴포넌트로 전달하는 target, relList, allowSource
    prop을 가지고 있습니다.
    replace prop을 이용해 next의 replace 라우터를 사용할 수 있습니다.
    target이 current일 때만 작동합니다.
    onClick 함수는 anchor 태그처럼 라우팅 이전에 작동합니다.
    
    target이 current이면 next js의 라우팅 함수를 이용합니다.
    target이 new이고 현재 환경이 웹이면 a[target="_blank"]의 기본 작동 방식을 따릅니다.
    target이 new이고 현재 환경이 앱이면 inlink를 이용해 라우팅합니다.
    target이 browser고 웹이면 a[target="_blank"]의 기본 작동 방식을 따릅니다.
    target이 browser고 앱이면 outlink를 이용해 라우팅합니다.

---
+++ packages/router/src/link/local-link.tsx
@@ -0,0 +1,77 @@
+import React, { MouseEventHandler, PropsWithChildren } from 'react'
+import Router from 'next/router'
+import { useEnv, useUserAgentContext } from '@titicaca/react-contexts'
+
+import { useAppBridge } from './use-app-bridge'
+import { LinkType } from './use-rel'
+import { ANCHOR_TARGET_MAP, TargetType } from './target'
+import { AllowSource, RouterGuardedLink } from './router-guarded-link'
+
+/**
+ * 같은 도메인의 페이지로 이동할 때 사용하는 링크 컴포넌트
+ */
+export function LocalLink({
+  href,
+  target,
+  relList,
+  allowSource,
+  replace,
+  onClick,
+  children,
+}: PropsWithChildren<{
+  href: string
+  target: TargetType
+  relList?: LinkType[]
+  allowSource?: AllowSource
+  replace?: boolean
+  onClick?: () => void
+}>) {
+  const { webUrlBase } = useEnv()
+  const { isPublic } = useUserAgentContext()
+  const { openInlink, openOutlink } = useAppBridge()
+
+  const fullHref = `${Router.basePath}${href}`
+
+  const handleClick: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    if (onClick) {
+      onClick()
+    }
+
+    switch (target) {
+      case 'current':
+        e.preventDefault()
+
+        Router[replace ? 'replace' : 'push'](href)
+        return
+
+      case 'new':
+        if (!isPublic) {
+          e.preventDefault()
+
+          openInlink(fullHref)
+        }
+        return
+
+      case 'browser':
+        if (!isPublic) {
+          e.preventDefault()
+
+          openOutlink(`${webUrlBase}${fullHref}`, {
+            target: 'browser',
+          })
+        }
+    }
+  }
+
+  return (
+    <RouterGuardedLink
+      href={fullHref}
+      relList={relList}
+      allowSource={allowSource}
+      onClick={handleClick}
+      target={ANCHOR_TARGET_MAP[target]}
+    >
+      {children}
+    </RouterGuardedLink>
+  )
+}
