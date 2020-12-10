0cc1198b842c728e579b9f69a53b177a3693f96d
woody <woody@triple-corp.com>
2020-12-10 15:51:28 +0900

    [#794] 🎨 쿠폰 모달에서 navigate 대신 ExternalLink 사용
    [#1121] 🔥 triple-document에서 불필요한 스타일 컴포넌트 제거
    
    [#794] 🐛 triple-document 링크 사용하는 부분에 allowSource 추가

--- packages/triple-document/src/elements/coupon/modals.tsx
+++ packages/triple-document/src/elements/coupon/modals.tsx
@@ -3,6 +3,7 @@ import { Text } from '@titicaca/core-elements'
 import { Modal, Alert } from '@titicaca/modals'
 import styled from 'styled-components'
 import { useURIHash, useHistoryFunctions } from '@titicaca/react-contexts'
+import { ExternalLink } from '@titicaca/router'
 
 type HashKeyValue = {
   [hash: string]: string
@@ -48,7 +49,7 @@ const CouponIcon = styled.img`
 
 export function CouponModal() {
   const uriHash = useURIHash()
-  const { back, navigate } = useHistoryFunctions()
+  const { back } = useHistoryFunctions()
 
   return (
     <Modal open={MODAL_HASHES.includes(uriHash)} onClose={back}>
@@ -87,18 +88,17 @@ export function CouponModal() {
         <Modal.Action color="gray" onClick={back}>
           취소
         </Modal.Action>
-        <Modal.Action
-          color="blue"
-          onClick={() => {
-            back()
-            navigate(
-              `/inlink?path=${encodeURIComponent(
-                '/benefit/coupons/my?_triple_no_navbar',
-              )}`,
-            )
-          }}
-        >
-          {CONFIRM_MESSAGE_TYPES[uriHash]}
+        <Modal.Action color="blue">
+          <ExternalLink
+            href="/benefit/coupons/my?_triple_no_navbar"
+            target="new"
+            allowSource="app-with-session"
+            onClick={() => {
+              back()
+            }}
+          >
+            <a>{CONFIRM_MESSAGE_TYPES[uriHash]}</a>
+          </ExternalLink>
         </Modal.Action>
       </Modal.Actions>
     </Modal>
