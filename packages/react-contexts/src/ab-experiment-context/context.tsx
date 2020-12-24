85e3d7f1cd1beff5c0bbbe906a40ed4398e02d98
woody <woody@triple-corp.com>
2020-12-24 10:53:29 +0900

    💚 잘못된 임포트 수정

--- packages/react-contexts/src/ab-experiment-context/context.tsx
+++ packages/react-contexts/src/ab-experiment-context/context.tsx
@@ -8,8 +8,8 @@ import React, {
   useRef,
   useState,
 } from 'react'
-import { useSessionContext } from 'src/session-context'
 
+import { useSessionContext } from '../session-context'
 import { useEventTrackingContext } from '../event-tracking-context'
 
 import { ABExperimentMeta, getABExperiment } from './service'
