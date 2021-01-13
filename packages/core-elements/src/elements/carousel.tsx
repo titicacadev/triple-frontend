bacfc3f2074c56a993992b7af3caa015223f35ea
Unknown <torres@triple-corp.com>
2021-01-13 13:55:35 +0900

    [#955] 기존 carousel deprecated 주석 추가

--- packages/core-elements/src/elements/carousel.tsx
+++ packages/core-elements/src/elements/carousel.tsx
@@ -1,21 +1,9 @@
 import styled, { css } from 'styled-components'
-import React, {
-  PropsWithChildren,
-  useRef,
-  useEffect,
-  useState,
-  useMemo,
-} from 'react'
-import uniqid from 'uniqid'
+import React, { PropsWithChildren } from 'react'
 import { StaticIntersectionObserver } from '@titicaca/intersection-observer'
-import { useUserAgentContext } from '@titicaca/react-contexts'
-import { ArrowButton } from '@titicaca/icons'
-import NativeFlicking, { FlickingOptions } from '@egjs/flicking'
 
 import { MarginPadding, CarouselSizes } from '../commons'
 import { marginMixin } from '../mixins'
-
-import Container from './container'
 
 const CAROUSEL_WIDTH_SIZES = {
   small: '140px',
@@ -41,18 +29,14 @@ const CarouselBase = styled.ul<CarouselBaseProps>`
   margin: 0;
   padding: 0;
   padding-bottom: 10px;
-
   ${marginMixin}
-
   white-space: nowrap;
   overflow-x: scroll;
   -webkit-overflow-scrolling: touch;
   overflow-y: hidden;
-
   ::-webkit-scrollbar {
     display: none;
   }
-
   ${({ containerPadding }) =>
     containerPadding &&
     css`
@@ -69,30 +53,10 @@ const Item = styled.li<{ size?: CarouselSizes }>`
   display: inline-block;
   position: relative;
   width: ${({ size }) => CAROUSEL_WIDTH_SIZES[size || 'small']};
+  margin-left: ${({ size }) => CAROUSEL_LEFT_SPACING_SIZES[size || 'small']};
   vertical-align: top;
   white-space: normal;
   cursor: pointer;
-  &:not(:first-child) {
-    margin-left: ${({ size }) => CAROUSEL_LEFT_SPACING_SIZES[size || 'small']};
-  }
-`
-
-const CarouselScrollButton = styled.div<{ direction: 'left' | 'right' }>`
-  position: absolute;
-  cursor: pointer;
-  border-radius: 50%;
-  width: 60px;
-  height: 60px;
-  top: calc(50% - 30px);
-  ${({ direction }) =>
-    direction === 'left'
-      ? css`
-          left: -30px;
-        `
-      : css`
-          right: -30px;
-        `};
-  z-index: 60;
 `
 
 function CarouselItem({
@@ -132,37 +96,6 @@ function CarouselItem({
     </Item>
   )
 }
-const FLICK_ATTRIBUTES: Partial<FlickingOptions> = {
-  deceleration: 0.0075,
-  horizontal: true,
-  circular: true,
-  infinite: false,
-  infiniteThreshold: 0,
-  lastIndex: Infinity,
-  threshold: 40,
-  duration: 100,
-  panelEffect: (x: number) => 1 - Math.pow(1 - x, 3),
-  defaultIndex: 0,
-  thresholdAngle: 45,
-  bounce: 10,
-  autoResize: false,
-  adaptive: false,
-  bound: false,
-  overflow: false,
-  hanger: '50%',
-  anchor: '50%',
-  gap: 10,
-  moveType: { type: 'snap', count: 1 },
-  collectStatistics: true,
-  zIndex: 50,
-  classPrefix: 'eg-flick',
-}
-
-const FlickingContainer = styled.div`
-  .eg-flick-panel {
-    margin-left: 0 !important;
-  }
-`
 
 function Carousel({
   margin,
@@ -170,43 +103,8 @@ function Carousel({
   children,
   className,
 }: PropsWithChildren<CarouselBaseProps>) {
-  const carouselRef = useRef() as React.RefObject<HTMLUListElement>
-  const [scrollable, setScrollable] = useState(false)
-  const [flicking, setFlicking] = useState<NativeFlicking>()
-  const { isMobile } = useUserAgentContext()
-  const uniqueId = useMemo(() => uniqid('egjs-flick-'), [])
-
-  useEffect(() => {
-    const carouselElement = carouselRef?.current
-
-    if (!carouselElement) {
-      return
-    }
-
-    if (carouselElement.scrollWidth > carouselElement.clientWidth) {
-      setScrollable(true)
-    }
-  }, [carouselRef])
-
-  useEffect(() => {
-    if (scrollable) {
-      setFlicking(new NativeFlicking(`.${uniqueId}`, FLICK_ATTRIBUTES))
-    }
-  }, [scrollable, uniqueId])
-
-  return !isMobile && scrollable ? (
-    <Container position="relative" margin={margin} padding={containerPadding}>
-      <FlickingContainer className={uniqueId}>{children}</FlickingContainer>
-      <CarouselScrollButton direction="left" onClick={() => flicking?.prev()}>
-        <ArrowButton direction="left" />
-      </CarouselScrollButton>
-      <CarouselScrollButton direction="right" onClick={() => flicking?.next()}>
-        <ArrowButton direction="right" />
-      </CarouselScrollButton>
-    </Container>
-  ) : (
+  return (
     <CarouselBase
-      ref={carouselRef}
       className={className}
       margin={margin}
       containerPadding={containerPadding}
@@ -217,5 +115,7 @@ function Carousel({
 }
 
 Carousel.Item = CarouselItem
-
+/**
+ * @deprecated @titicaca/carousel 패키지를 사용해주세요.
+ */
 export default Carousel
