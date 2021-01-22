a47c288a9da8f8efa31f4765aac0ce9dae432d91
Kyudeok <polysiya@gmail.com>
2021-01-22 15:51:01 +0900

    Use image-api case API for image of social reviews

--- packages/social-reviews/src/index.tsx
+++ packages/social-reviews/src/index.tsx
@@ -62,7 +62,10 @@ export default function SocialReviews({
               >
                 <Image borderRadius={4}>
                   <Image.FixedRatioFrame frame="big">
-                    <Image.Img src={imageUrl} alt={`${title} 썸네일`} />
+                    <Image.Img
+                      src={`/api/images/cast?url=${imageUrl}&transformation=c_fill,f_auto,h_256,w_256`}
+                      alt={`${title} 썸네일`}
+                    />
                   </Image.FixedRatioFrame>
                 </Image>
               </Container>
