edcc5161c918f3f50f4421ceabc8282ed73281bc
koo9ku@gmail.com <rndlstjd9!@>
2021-01-27 19:32:31 +0900

    fix: lint error

--- packages/core-elements/src/elements/image/optimized-img.tsx
+++ packages/core-elements/src/elements/image/optimized-img.tsx
@@ -5,7 +5,6 @@ import { generateImageUrl, Version, Quality } from '@titicaca/content-utilities'
 
 import { useImageState } from './context'
 import { useContentAbsolute } from './fixed-ratio-frame'
-import { Placeholder } from './placeholder'
 
 export interface OptimizedImgProps {
   placeholderSrc?: string
