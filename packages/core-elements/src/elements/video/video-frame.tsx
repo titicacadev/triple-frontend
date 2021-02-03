9375d0a601efa118a8107e62dc90e215394bbca5
dddeok <unknown>
2021-02-03 17:58:41 +0900

    fix. Video Frame 분리

---
+++ packages/core-elements/src/elements/video/video-frame.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import { MEDIA_FRAME_OPTIONS, FrameRatioAndSizes } from '../../commons'
+import { formatMarginPadding } from '../../mixins'
+
+const VideoContainer = styled.div<{
+  frame: FrameRatioAndSizes
+  fallbackImageUrl: string
+  borderRadius?: number
+}>`
+  width: 100%;
+  overflow: hidden;
+  height: 0;
+  position: relative;
+  background-image: url(${({ fallbackImageUrl }) => fallbackImageUrl});
+  background-size: cover;
+  border-radius: ${({ borderRadius }) =>
+    borderRadius === 0 ? 0 : borderRadius || 6}px;
+
+  ${({ frame }) =>
+    frame !== 'original' &&
+    formatMarginPadding(
+      { top: MEDIA_FRAME_OPTIONS[frame || 'small'] },
+      'padding',
+    )}
+`
+
+export default function VideoFrame({
+  frame,
+  fallbackImageUrl,
+  borderRadius,
+  children,
+}: React.PropsWithChildren<{
+  frame: FrameRatioAndSizes
+  fallbackImageUrl: string
+  borderRadius: number
+}>) {
+  return (
+    <VideoContainer
+      frame={frame}
+      fallbackImageUrl={fallbackImageUrl}
+      borderRadius={borderRadius}
+    >
+      {children}
+    </VideoContainer>
+  )
+}
