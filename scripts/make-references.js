57252b876b9702c54194ea34618d8c769faa2850
woody <woody@triple-corp.com>
2021-02-09 10:43:59 +0900

    🔨 tsconfig에 reference 추가하는 스크립트 작성

---
+++ scripts/make-references.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+const { error } = require('console')
+
+const prettier = require('prettier')
+
+const {
+  dependencies,
+  devDependencies,
+  peerDependencies,
+} = require(path.resolve(process.cwd(), './package.json'))
+const tsconfig = require(path.resolve(process.cwd(), './tsconfig.json'))
+
+const references = [
+  ...dependenciesToReferences(dependencies),
+  ...dependenciesToReferences(devDependencies),
+  ...dependenciesToReferences(peerDependencies),
+].sort()
+
+const newTsconfig = {
+  ...tsconfig,
+  ...(references.length > 0 ? { references } : {}),
+}
+
+fs.writeFile(
+  path.resolve(process.cwd(), './tsconfig.json'),
+  prettier.format(`${JSON.stringify(newTsconfig, null, '  ')}\n`, {
+    parser: 'json',
+  }),
+  { encoding: 'utf8' },
+  (err) => {
+    if (err) {
+      error(err)
+    }
+  },
+)
+
+function dependenciesToReferences(deps) {
+  if (!deps) {
+    return []
+  }
+  return Object.keys(deps)
+    .filter((packageName) => packageName.startsWith('@titicaca/'))
+    .map((packageName) => ({
+      path: `../${packageName.replace('@titicaca/', '')}`,
+    }))
+}
