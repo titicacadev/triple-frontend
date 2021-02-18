c0135a40988eb4d86b587d6c463eaf742da9e69e
dddeok <>
2021-02-18 16:47:24 +0900

    fix. useEffect내 반복 로직 수정

--- packages/core-elements/src/elements/video/index.ts
+++ packages/core-elements/src/elements/video/index.ts
@@ -1,6 +1,5 @@
 import VideoFrame from './video-frame'
 import Video from './video'
 import VideoElement from './video-element'
-import { VideoWrapper } from './context'
 
-export { Video, VideoWrapper, VideoFrame, VideoElement }
+export { Video, VideoFrame, VideoElement }
--- packages/core-elements/src/elements/video/video.tsx
+++ packages/core-elements/src/elements/video/video.tsx
@@ -1,35 +1,9 @@
 import React from 'react'
-import styled from 'styled-components'
 
 import { FrameRatioAndSizes } from '../../commons'
 
-import { useVideoRef } from './use-video-ref'
-import Controls from './controls'
 import VideoFrame from './video-frame'
 import VideoElement from './video-element'
-
-const Pending = styled.div`
-  position: absolute;
-  border: none;
-  background: none;
-  width: 60px;
-  height: 60px;
-  border-radius: 30px;
-  top: 50%;
-  left: 50%;
-  background-image: url(https://assets.triple.guide/images/img-video-loading@3x.png);
-  background-size: cover;
-  animation: rotation 2s infinite linear;
-
-  @keyframes rotation {
-    from {
-      transform: translate(-50%, -50%) rotate(0deg);
-    }
-    to {
-      transform: translate(-50%, -50%) rotate(359deg);
-    }
-  }
-`
 
 export default function Video({
   src,
@@ -55,8 +29,6 @@ export default function Video({
   showNativeControls?: boolean
   removeFrame?: boolean
 }) {
-  const { videoRef, pending } = useVideoRef()
-
   return (
     <VideoFrame
       removeFrame={removeFrame}
@@ -64,18 +36,14 @@ export default function Video({
       fallbackImageUrl={fallbackImageUrl}
     >
       <VideoElement
-        videoRef={videoRef}
         src={src}
         srcType={srcType}
         cloudinaryBucket={cloudinaryBucket}
         cloudinaryId={cloudinaryId}
         autoPlay={autoPlay}
         showNativeControls={showNativeControls}
+        hideControls={hideControls}
       />
-      {pending && <Pending />}
-      {videoRef && !hideControls && (
-        <Controls videoRef={videoRef} autoPlay={!!autoPlay} />
-      )}
     </VideoFrame>
   )
 }
