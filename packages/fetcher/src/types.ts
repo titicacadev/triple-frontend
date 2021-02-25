adcf8ee43695e6646ef242a55c6ac9e33f2f584d
Unknown <torres@triple-corp.com>
2021-02-25 14:46:53 +0900

    :ok_hand::skin-tone-2:  리뷰 반영

--- packages/fetcher/src/types.ts
+++ packages/fetcher/src/types.ts
@@ -10,7 +10,7 @@ export type RequestOptions = {
   /** header object */
   headers?: HeadersInit
   /** don't stringfy body */
-  noStringfyBody?: boolean
+  useBodyAsRaw?: boolean
   [key: string]: any
 }
 
