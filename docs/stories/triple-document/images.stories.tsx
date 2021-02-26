d17ebcbfed7880b19b1ecef2798cd5bf68e4d2f3
woody <woody@triple-corp.com>
2021-02-26 16:33:42 +0900

    🎨 triple-document/images 스토리 CSF로 변환

--- docs/stories/triple-document/images.stories.tsx
+++ docs/stories/triple-document/images.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
+import { Meta } from '@storybook/react'
 import { select, text } from '@storybook/addon-knobs'
 import { ELEMENTS } from '@titicaca/triple-document'
 
@@ -8,50 +8,60 @@ import IMAGES_FRAME from '../__mocks__/images-frame.sample.json'
 
 const { images: Images } = ELEMENTS
 
-storiesOf('triple-document / 이미지', module)
-  .add('1개', () => {
-    const [image] = IMAGES
-    return (
-      <Images
-        value={{
-          images: [
-            {
-              ...image,
-              frame: select(
-                '크기',
-                ['mini', 'small', 'medium', 'large', 'big', 'huge', 'original'],
-                'small',
-              ),
-              title: text('캡션', 'TripleDocument 샘플 1'),
-              sourceUrl: text('출처', 'https://triple.guide'),
-            },
-          ],
-        }}
-      />
-    )
-  })
-  .add('1개, 프레임', () => {
-    const [image] = IMAGES_FRAME
-    return (
-      <Images
-        value={{
-          images: [
-            {
-              ...image,
-              frame: select(
-                '크기',
-                ['mini', 'small', 'medium', 'large', 'big', 'huge', 'original'],
-                'small',
-              ),
-              title: text('캡션', 'TripleDocument 샘플 1'),
-              sourceUrl: text('출처', 'https://triple.guide'),
-            },
-          ],
-        }}
-      />
-    )
-  })
-  .add('2개', () => (
+export default {
+  title: 'triple-document / 이미지',
+  component: Images,
+} as Meta
+
+export function OneImage() {
+  const [image] = IMAGES
+
+  return (
+    <Images
+      value={{
+        images: [
+          {
+            ...image,
+            frame: select(
+              '크기',
+              ['mini', 'small', 'medium', 'large', 'big', 'huge', 'original'],
+              'small',
+            ),
+            title: text('캡션', 'TripleDocument 샘플 1'),
+            sourceUrl: text('출처', 'https://triple.guide'),
+          },
+        ],
+      }}
+    />
+  )
+}
+OneImage.storyName = '1개'
+
+export function OneImageWithFrame() {
+  const [image] = IMAGES_FRAME
+  return (
+    <Images
+      value={{
+        images: [
+          {
+            ...image,
+            frame: select(
+              '크기',
+              ['mini', 'small', 'medium', 'large', 'big', 'huge', 'original'],
+              'small',
+            ),
+            title: text('캡션', 'TripleDocument 샘플 1'),
+            sourceUrl: text('출처', 'https://triple.guide'),
+          },
+        ],
+      }}
+    />
+  )
+}
+OneImageWithFrame.storyName = '1개, 프레임'
+
+export function TwoImages() {
+  return (
     <Images
       value={{
         images: IMAGES.map((value) => ({
@@ -60,19 +70,29 @@ storiesOf('triple-document / 이미지', module)
         })),
       }}
     />
-  ))
-  .add('2개, 캡션', () => (
+  )
+}
+TwoImages.storyName = '2개'
+
+export function TwoImagesWithCaption() {
+  return (
     <Images
       value={{
         images: IMAGES,
       }}
     />
-  ))
-  .add('2개, 블록', () => (
+  )
+}
+TwoImagesWithCaption.storyName = '2개, 캡션'
+
+export function TwoBlockImages() {
+  return (
     <Images
       value={{
         images: IMAGES.map((value) => ({ ...value, title: '' })),
         display: 'block',
       }}
     />
-  ))
+  )
+}
+TwoBlockImages.storyName = '2개, 블록'
