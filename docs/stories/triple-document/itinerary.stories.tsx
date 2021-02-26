dfd53f051623bbf7cc3722d06e876101a22d18a6
woody <woody@triple-corp.com>
2021-02-26 15:43:39 +0900

    🎨 session context decorator 사용

--- docs/stories/triple-document/itinerary.stories.tsx
+++ docs/stories/triple-document/itinerary.stories.tsx
@@ -1,39 +1,15 @@
 import React from 'react'
 import { ELEMENTS } from '@titicaca/triple-document'
 import { action } from '@storybook/addon-actions'
-import { StoryFn } from '@storybook/addons'
-import { boolean } from '@storybook/addon-knobs'
-import { EnvProvider, SessionContextProvider } from '@titicaca/react-contexts'
 
 import mock from '../__mocks__/triple-document.itinerary.json'
+import { sessionContextProviderDecorator } from '../../decorators'
 
 const { itinerary: Itinerary } = ELEMENTS
 
-function envProviderDecorator(storyFn: StoryFn<JSX.Element>) {
-  return (
-    <EnvProvider
-      appUrlScheme="dev-soto"
-      webUrlBase="https://triple-dev.titicaca-corp.com"
-      authBasePath="MOCK_AUTH_BASE_PATH"
-      facebookAppId=""
-      defaultPageTitle=""
-      defaultPageDescription=""
-      googleMapsApiKey="AIzaSyBaOSog5Kc4PkNw1JiSIcvz8WHt1Y78lNU"
-    >
-      <SessionContextProvider
-        sessionId={
-          boolean('hasSessionId', false) ? 'MOCK_SESSION_ID' : undefined
-        }
-      >
-        {storyFn()}
-      </SessionContextProvider>
-    </EnvProvider>
-  )
-}
-
 export default {
   title: 'TripleDocument / 추천코스',
-  decorators: [envProviderDecorator],
+  decorators: [sessionContextProviderDecorator],
 }
 
 export function DocumentItinerary() {
