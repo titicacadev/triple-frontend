02da7c60e1642f074270ce35aa58118f7a696aaf
Unknown <torres@triple-corp.com>
2021-03-15 14:43:20 +0900

    fix: Images 컴포넌트 파일 분리

---
+++ packages/review/src/review-element/images/multiple-images.tsx
@@ -0,0 +1,89 @@
+import { Responsive } from '@titicaca/core-elements'
+import React from 'react'
+
+import { ImageEntity } from '../../types'
+
+import {
+  ImageElement,
+  SquareFrame,
+  ImagesContainer,
+  FlexItemContainer,
+} from './elements'
+
+const IMAGES_CONTAINER_HEIGHTS = [217, 292, 328]
+const IMAGE_HEIGHTS = [217, 143, 105]
+
+export default function MultipleImages({
+  images,
+  onImageClick,
+}: {
+  images: ImageEntity[]
+  onImageClick: (e: React.SyntheticEvent, index: number) => void
+}) {
+  return (
+    <>
+      <Responsive minWidth={500}>
+        <ImagesContainer
+          flexDirection="row"
+          height={IMAGES_CONTAINER_HEIGHTS[images.length - 2]}
+        >
+          <FlexItemContainer flexShrink={1}>
+            <ImageElement
+              src={images[0].sizes.large.url}
+              fullHeight
+              onClick={(e) => onImageClick(e, 0)}
+            />
+          </FlexItemContainer>
+
+          <FlexItemContainer flexShrink={images.length - 1}>
+            <ImagesContainer flexDirection="column">
+              {images.slice(1, images.length).map(({ id, sizes }, index) => (
+                <ImageElement
+                  key={`review.img.${id}.${index}`}
+                  src={sizes.large.url}
+                  height={IMAGE_HEIGHTS[images.length - 2]}
+                  onClick={(e) => onImageClick(e, index)}
+                />
+              ))}
+            </ImagesContainer>
+          </FlexItemContainer>
+        </ImagesContainer>
+      </Responsive>
+
+      <Responsive maxWidth={499}>
+        <ImagesContainer flexDirection="row">
+          <FlexItemContainer flexShrink={1}>
+            <SquareFrame>
+              <ImageElement
+                src={images[0].sizes.large.url}
+                fullHeight
+                absolute
+                onClick={(e) => onImageClick(e, 0)}
+              />
+            </SquareFrame>
+          </FlexItemContainer>
+
+          <FlexItemContainer flexShrink={images.length - 1}>
+            <ImagesContainer flexDirection="column">
+              {images.slice(1, images.length).map(({ id, sizes }, index) => (
+                <FlexItemContainer
+                  key={`review.img.${id}.${index}`}
+                  flexShrink={1}
+                >
+                  <SquareFrame>
+                    <ImageElement
+                      src={sizes.large.url}
+                      absolute
+                      fullHeight
+                      onClick={(e) => onImageClick(e, index)}
+                    />
+                  </SquareFrame>
+                </FlexItemContainer>
+              ))}
+            </ImagesContainer>
+          </FlexItemContainer>
+        </ImagesContainer>
+      </Responsive>
+    </>
+  )
+}
---
+++ packages/review/src/review-element/images/penta-images.tsx
@@ -0,0 +1,109 @@
+import { Responsive, Text } from '@titicaca/core-elements'
+import React from 'react'
+
+import { ImageEntity } from '../../types'
+
+import {
+  ImageElement,
+  SquareFrame,
+  Dimmer,
+  FlexItemContainer,
+  ImagesContainer,
+} from './elements'
+
+export default function PentaImages({
+  images,
+  onImageClick,
+}: {
+  images: ImageEntity[]
+  onImageClick: (e: React.SyntheticEvent, index: number) => void
+}) {
+  return (
+    <>
+      <Responsive maxWidth={499}>
+        <ImagesContainer flexDirection="column">
+          <ImagesContainer flexDirection="row">
+            {images.slice(0, 2).map(({ id, sizes }, index) => (
+              <FlexItemContainer
+                key={`review-img.${id}.${index}`}
+                flexShrink={1}
+              >
+                <SquareFrame>
+                  <ImageElement
+                    src={sizes.large.url}
+                    absolute
+                    fullHeight
+                    onClick={(e) => onImageClick(e, index)}
+                  />
+                </SquareFrame>
+              </FlexItemContainer>
+            ))}
+          </ImagesContainer>
+          <ImagesContainer flexDirection="row">
+            {images.slice(2, 5).map(({ id, sizes }, index) => (
+              <FlexItemContainer
+                key={`review-img.${id}.${index}`}
+                flexShrink={1}
+              >
+                <SquareFrame>
+                  {images.length > 5 && index === 2 ? (
+                    <Dimmer>
+                      <Text bold color="white900" textAlign="center">
+                        + {images.length - 5}
+                      </Text>
+                    </Dimmer>
+                  ) : null}
+                  <ImageElement
+                    src={sizes.large.url}
+                    absolute
+                    fullHeight
+                    onClick={(e) => onImageClick(e, index)}
+                  />
+                </SquareFrame>
+              </FlexItemContainer>
+            ))}
+          </ImagesContainer>
+        </ImagesContainer>
+      </Responsive>
+      <Responsive minWidth={500}>
+        <ImagesContainer flexDirection="column">
+          <ImagesContainer flexDirection="row" height={217}>
+            {images.slice(0, 2).map(({ id, sizes }, index) => (
+              <FlexItemContainer
+                key={`review-img.${id}.${index}`}
+                flexShrink={1}
+              >
+                <ImageElement
+                  src={sizes.large.url}
+                  fullHeight
+                  onClick={(e) => onImageClick(e, index)}
+                />
+              </FlexItemContainer>
+            ))}
+          </ImagesContainer>
+          <ImagesContainer flexDirection="row" height={143}>
+            {images.slice(2, 5).map(({ id, sizes }, index) => (
+              <FlexItemContainer
+                key={`review-img.${id}.${index}`}
+                flexShrink={1}
+              >
+                {images.length > 5 && index === 2 ? (
+                  <Dimmer>
+                    <Text bold color="white900" textAlign="center">
+                      + {images.length - 5}
+                    </Text>
+                  </Dimmer>
+                ) : null}
+                <ImageElement
+                  src={sizes.large.url}
+                  fullHeight
+                  onClick={(e) => onImageClick(e, index)}
+                />
+              </FlexItemContainer>
+            ))}
+          </ImagesContainer>
+        </ImagesContainer>
+      </Responsive>
+    </>
+  )
+}
