7532cfd954e6018aa72777c01cafcac7c2d01eca
dddeok <unknown>
2021-04-06 10:28:09 +0900

    borderRadius 관련 Mixin 생성

--- packages/core-elements/src/mixins/safari-bug.ts
+++
@@ -1,9 +0,0 @@
-import { css } from 'styled-components'
-/**
- * overflow: hidden과 border-radius 사용 시 사파리에서 발생하는 버그를 위한  CSS 추가
- * @param safariStackingContextWorkaround
- */
-
-export const safariStackingContextWorkaround = css`
-  mask-image: radial-gradient(white, black);
-`
