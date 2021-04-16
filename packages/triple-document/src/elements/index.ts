dda6b1a23dc0f0c5aac131dfd81b481a3701de6e
woody <woody@triple-corp.com>
2021-04-16 16:07:50 +0900

    ♻️ ELEMENTS 객체 위치 변경
    elements 모듈에서 각각의 컴포넌트를 노출할 필요 없고,
    이를 모두 묶은 ELEMENTS 객체만 노출하면 된다.

--- packages/triple-document/src/elements/index.ts
+++ packages/triple-document/src/elements/index.ts
@@ -1,14 +1,47 @@
-export { default as Coupon } from './coupon'
-export * from './text'
-export { default as Embedded } from './embedded'
-export { default as ExternalVideo } from './external-video'
-export { default as Images } from './images'
-export { default as Links } from './links'
-export { default as List } from './list'
-export { default as Note } from './note'
-export { default as Pois } from './pois'
-export { default as Regions } from './regions'
-export { default as Table } from './table'
-export { TnaProductsList } from './tna'
-export { default as Itinerary } from './itinerary'
-export { default as Anchor } from './anchor'
+import { HR1, HR2, HR3, HR4, HR5, HR6 } from '@titicaca/core-elements'
+
+import { ElementSet } from '../types'
+
+import Anchor from './anchor'
+import Coupon from './coupon'
+import Embedded from './embedded'
+import ExternalVideo from './external-video'
+import Images from './images'
+import ItineraryElement from './itinerary'
+import Links from './links'
+import List from './list'
+import Note from './note'
+import Pois from './pois'
+import Regions from './regions'
+import Table from './table'
+import { MH1, MH2, MH3, MH4, Text } from './text'
+import { TnaProductsList } from './tna'
+
+const ELEMENTS: ElementSet = {
+  heading1: MH1,
+  heading2: MH2,
+  heading3: MH3,
+  heading4: MH4,
+  text: Text,
+  images: Images,
+  hr1: HR1,
+  hr2: HR2,
+  hr3: HR3,
+  hr4: HR4,
+  hr5: HR5,
+  hr6: HR6,
+  pois: Pois,
+  links: Links,
+  embedded: Embedded,
+  note: Note,
+  list: List,
+  regions: Regions,
+  video: ExternalVideo,
+  tnaProducts: TnaProductsList,
+  table: Table,
+  coupon: Coupon,
+  itinerary: ItineraryElement,
+  anchor: Anchor,
+}
+
+export default ELEMENTS
