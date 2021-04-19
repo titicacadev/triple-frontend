28d1189dcc920bb6221d7a8b3ffbbe43824cd9e4
woody <woody@triple-corp.com>
2021-04-19 10:24:59 +0900

    ✨ triple-document가 prop consume 훅을 노출
    custom Element를 만들 때 prop으로 공급하는 값을 사용할 수 있도록 외부로 노출합니다.

--- packages/triple-document/src/index.ts
+++ packages/triple-document/src/index.ts
@@ -3,4 +3,13 @@ export { default as ELEMENTS } from './elements'
 export { TripleElementData as TripleDocumentElementData } from './types'
 export * from './elements/text'
 
+export { useDeepLink } from './prop-context/deep-link'
+export { useImageClickHandler } from './prop-context/image-click-handler'
+export { useImageSource } from './prop-context/image-source'
+export { useLinkClickHandler } from './prop-context/link-click-handler'
+export { useMediaConfig } from './prop-context/media-config'
+export { useResourceClickHandler } from './prop-context/resource-click-handler'
+export { useTNAProductClickHandler } from './prop-context/tna-product-click-handler'
+export { useTNAProductsFetcher } from './prop-context/tna-products-fetcher'
+
 export default TripleDocument
