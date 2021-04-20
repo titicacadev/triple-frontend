82f5316d9e1110b756a4fb79c47be2f884763b07
dddeok <unknown>
2021-04-20 17:35:44 +0900

    useEffect 로직 변경

--- packages/react-hooks/src/use-intersection-observer.ts
+++ packages/react-hooks/src/use-intersection-observer.ts
@@ -2,30 +2,29 @@ import { useState, useEffect, useCallback, useRef } from 'react'
 
 const useInterSection = ({ threshold }: { threshold?: number }) => {
   const ref = useRef<HTMLDivElement>(null)
-  const [isInterSection, setIsInterSection] = useState(false)
+  const [isIntersection, setIsIntersection] = useState(false)
 
   const handleScroll = useCallback(([entry]) => {
     if (entry.isIntersecting) {
-      setIsInterSection(true)
+      setIsIntersection(true)
     }
   }, [])
 
   useEffect(() => {
-    let observer: any
-    const { current } = ref
-    if (current) {
-      observer = new IntersectionObserver(handleScroll, {
-        threshold: threshold ?? 0,
-      })
-      observer.observe(current)
-
-      return () => observer && observer.disconnect()
+    if (!ref.current) {
+      return
     }
+
+    const observer = new IntersectionObserver(handleScroll, {
+      threshold: threshold ?? 0,
+    })
+
+    return () => observer && observer.disconnect()
   }, [ref, threshold, handleScroll])
 
   return {
     ref,
-    isInterSection,
+    isIntersection,
   }
 }
 
