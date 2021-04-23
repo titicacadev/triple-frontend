c8703ffc23ff79092ea28155e8548aa908e3b0b0
Rhio Kim <rhio.kim@gmail.com>
2021-04-23 09:48:32 +0900

    feat(core-element): Stack 컴포넌트 추가

--- packages/core-elements/src/elements/stack.ts
+++ packages/core-elements/src/elements/stack.ts
@@ -2,25 +2,37 @@ import styled, { css } from 'styled-components'
 
 /**
  * 나열형 컴포넌트의 상하, 좌우 여백을 리셋하기 위한 컴포넌트
+ *
+ * - 새로형
+ * <Stack>...</Stack>
+ * <Stack vertical>...</Stack>
+ *
+ * - 가로형
+ * <Stack horizontal>...</Stack>
  */
 export const Stack = styled.div<{ vertical?: boolean; horizontal?: boolean }>`
-  ${({ vertical }) =>
-    vertical &&
-    css`
-      > :first-child {
-        margin-top: 0;
-      }
-
-      > :last-child {
-        margin-bottom: 0;
-      }
-    `}
-
   ${({ horizontal }) =>
-    horizontal &&
-    css`
-      > :not(:first-child) {
-        margin-left: 0;
-      }
-    `}
+    !horizontal
+      ? css`
+          > :first-child {
+            margin-top: 0;
+            padding-top: 0;
+          }
+
+          > :last-child {
+            margin-bottom: 0;
+            padding-top: 0;
+          }
+        `
+      : css`
+          > :first-child {
+            margin-left: 0;
+            padding-left: 0;
+          }
+
+          > :last-child {
+            margin-right: 0;
+            padding-right: 0;
+          }
+        `}
 `
