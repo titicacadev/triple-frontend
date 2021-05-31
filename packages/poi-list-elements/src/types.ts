84b191d2a7c9d01152415b5318461b9c636726b4
Gi Sung Park <43316372+pgs787@users.noreply.github.com>
2021-05-31 13:29:23 +0900

    region 타입 이동

--- packages/poi-list-elements/src/types.ts
+++ packages/poi-list-elements/src/types.ts
@@ -1,6 +1,14 @@
+import { TranslatedProperty } from '@titicaca/type-definitions'
+
 export type ActionButtonElement = React.ReactNode
 
 export interface POIListElementBaseProps<T> {
-  poi: T
+  poi: T & {
+    region?: {
+      source: {
+        names: TranslatedProperty
+      }
+    }
+  }
   onClick?: React.MouseEventHandler<HTMLLIElement>
 }
