f31c686b22ab529cece13abae0f1e73c3ea99ab9
Unknown <torres@triple-corp.com>
2021-06-03 13:08:54 +0900

    feat: Public Footer 추가

---
+++ packages/footer/src/elements.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const FooterFrame = styled.footer`
+  color: rgba(46, 46, 46, 0.5);
+  font-size: 11px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.55;
+  background-color: #fafafa;
+`
