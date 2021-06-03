3f95bcafee2f88293e11e21358e0b39d7e35231d
Unknown <torres@triple-corp.com>
2021-06-03 13:23:06 +0900

    fix: Default 푸터를 대체합니다

--- packages/footer/src/index.ts
+++ packages/footer/src/index.ts
@@ -1,6 +1,5 @@
 import DefaultFooter from './default-footer'
 export { default as CSFooter } from './cs-footer'
 export { default as LogoFooter } from './logo-footer'
-export { default as PublicFooter } from './public-footer'
 
 export default DefaultFooter
