98130f5e21c28ee6f4ea549ede64e7455024a20f
Unknown <torres@triple-corp.com>
2021-06-28 12:08:31 +0900

    feat: useSessionCallback에 useAuthWeb 옵션 추가

--- packages/ui-flow/src/use-session-callback.ts
+++ packages/ui-flow/src/use-session-callback.ts
@@ -9,25 +9,47 @@ import { useLoginCTAModal } from '@titicaca/modals'
  * @param returnValue sessionId가 없을 때 리턴할 값
  * @param returnUrl 로그인 완료 후 복귀할 페이지 주소
  */
-export function useSessionCallback<
-  T extends (...args: any[]) => any,
-  S = unknown
->(
+export function useSessionCallback<T extends (...args: any[]) => any>(
   fn: T,
-  returnValue?: S,
-  returnUrl?: string,
-): (...args: Parameters<T>) => ReturnType<T> | S | void {
-  const { hasSessionId } = useSessionContext()
+  ...options:
+    | [boolean, string]
+    | [boolean]
+    | [undefined, string]
+    | [
+        {
+          returnUrl?: string
+          returnValue?: boolean
+          useAuthWeb?: boolean
+        },
+      ]
+    | []
+): (...args: Parameters<T>) => ReturnType<T> | boolean | void {
+  const { hasSessionId, login } = useSessionContext()
   const { show } = useLoginCTAModal()
 
   return useCallback(
     (...args) => {
       if (!hasSessionId) {
-        show(returnUrl)
-        return returnValue
+        if (typeof options[0] === 'object') {
+          const { returnUrl, returnValue, useAuthWeb } = options[0]
+
+          if (useAuthWeb) {
+            login({ returnUrl })
+          } else {
+            show(returnUrl)
+          }
+
+          return returnValue
+        } else {
+          const [returnValue, returnUrl] = options
+
+          show(returnUrl)
+
+          return returnValue
+        }
       }
       return fn(...args)
     },
-    [fn, hasSessionId, show, returnValue, returnUrl],
+    [fn, show, login, options, hasSessionId],
   )
 }
