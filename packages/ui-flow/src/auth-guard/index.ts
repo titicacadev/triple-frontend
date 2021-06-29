cbf48930ff6659db93f854a1ef9a679b47250194
woody <woody@triple-corp.com>
2021-06-29 15:43:14 +0900

    ✨ 앱에서 401에러가 발생하면 토큰 새로고침을 시도한 다음 로그인 뷰로 라우팅
    앱에서는 토큰이 만료되었을 때 401 에러가 발생할 수 있습니다.
    따라서 401 에러가 발생하면 먼저 토큰 새로고침을 시도하고,
    다시 401 에러가 발생했을 때만 로그인 페이지로 이동합니다.

--- packages/ui-flow/src/auth-guard/index.ts
+++ packages/ui-flow/src/auth-guard/index.ts
@@ -2,6 +2,7 @@ import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
 import { get } from '@titicaca/fetcher'
 import { parseApp } from '@titicaca/react-contexts'
 import qs from 'qs'
+import { generateUrl, parseUrl, strictQuery } from '@titicaca/view-utilities'
 
 interface UserResponse {
   uid: string
@@ -45,10 +46,6 @@ export function authGuard<Props>(
       ? options.resolveReturnUrl(ctx)
       : `${process.env.NEXT_PUBLIC_BASE_PATH || ''}${resolvedUrl}`
 
-    if (userAgentString && !!parseApp(userAgentString)) {
-      return gssp(ctx)
-    }
-
     const { result: user, status, error } = await get<UserResponse>(
       '/api/users/me',
       {
@@ -67,6 +64,45 @@ export function authGuard<Props>(
     }
 
     if (status === 401 || isNonMember) {
+      if (userAgentString && parseApp(userAgentString) && status === 401) {
+        const { query: currentQuery } = parseUrl(resolvedUrl)
+        const { refreshed, attemptedLogin } = strictQuery(
+          currentQuery ? qs.parse(currentQuery) : ({} as any),
+        )
+          .boolean('refreshed')
+          .boolean('attemptedLogin')
+          .use()
+
+        if (refreshed && !attemptedLogin) {
+          const loginReturnUrl = generateUrl(
+            { query: qs.stringify({ attemptedLogin: true }) },
+            returnUrl,
+          )
+
+          return {
+            redirect: {
+              destination: `${
+                process.env.NEXT_PUBLIC_APP_SCHEME
+              }:///login?returnUrl=${encodeURIComponent(loginReturnUrl)}`,
+              basePath: false,
+              permanent: false,
+            },
+          }
+        }
+
+        const destinationQuery = qs.stringify({
+          returnUrl: generateUrl({ query: 'refreshed=true' }, returnUrl),
+        })
+
+        return {
+          redirect: {
+            destination: `/landing/refresh?${destinationQuery}`,
+            basePath: false,
+            permanent: false,
+          },
+        }
+      }
+
       const query = qs.stringify({
         returnUrl,
         type: options?.authType,
