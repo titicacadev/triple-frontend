2247c3ddd5c431d1e59828d1b0b2b790edb2a2c9
eddie <unknown>
2021-07-05 14:24:59 +0900

    chore: restore legacy marker

--- packages/map/src/marker/index.ts
+++ packages/map/src/marker/index.ts
@@ -7,4 +7,6 @@ export * from './with-type-circle-marker'
 export const HotelCircleMarker = withTypeCircleMarker('hotel')
 export const AttractionCircleMarker = withTypeCircleMarker('attraction')
 export const RestaurantCircleMarker = withTypeCircleMarker('restaurant')
+export const AttractionCirlceMarker = withTypeCircleMarker('attraction') // TODO: delete
+export const RestaurantCirlceMarker = withTypeCircleMarker('restaurant') // TODO: delete
 export const TnaCircleMarker = withTypeCircleMarker('tna')
