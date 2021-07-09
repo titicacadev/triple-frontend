5db29f8695f3a93883663a34c1f401523903b46d
woody <woody@triple-corp.com>
2021-07-09 15:36:23 +0900

    ✨ 사용자 안내 문구 추가
    에러의 종류에 따라 사용자가 취할 수 있는 행동을 안내하는 문구를 작성합니다.

--- packages/web-storage/src/error.ts
+++ packages/web-storage/src/error.ts
@@ -3,6 +3,10 @@ import { CustomError } from 'ts-custom-error'
 type ErrorType = 'NotBrowser' | 'Unavailable' | 'QuotaExceeded'
 
 export class WebStorageError extends CustomError {
+  private type: ErrorType
+
+  private storageType: 'localStorage' | 'sessionStorage'
+
   constructor(type: 'localStorage' | 'sessionStorage', errorType: ErrorType) {
     const messageMap: { [key in ErrorType]: string } = {
       NotBrowser: '브라우저 환경일 때만 WebStorage API를 사용할 수 있습니다.',
@@ -11,5 +15,23 @@ export class WebStorageError extends CustomError {
     }
 
     super(messageMap[errorType])
+    this.type = errorType
+    this.storageType = type
+  }
+
+  get userGuideMessage() {
+    switch (this.type) {
+      case 'NotBrowser':
+        return '브라우저에서 사용해주세요.'
+      case 'QuotaExceeded':
+        if (this.storageType === 'sessionStorage') {
+          return '브라우저를 완전히 종료하고 다시 접속해 주세요.'
+        }
+        return '브라우저 캐시를 모두 비워주세요.'
+      case 'Unavailable':
+        return '브라우저의 쿠키 차단 설정을 해제해주세요.'
+      default:
+        return '알 수 없는 에러가 발생했습니다. 잠시 후 다시 시도해 주세요.'
+    }
   }
 }
