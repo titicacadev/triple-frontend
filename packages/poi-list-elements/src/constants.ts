2d41e1a7763181864446afe228a3f65b864206c1
Gi Sung Park <43316372+pgs787@users.noreply.github.com>
2021-07-16 18:29:13 +0900

    change poi blank image

--- packages/poi-list-elements/src/constants.ts
+++ packages/poi-list-elements/src/constants.ts
@@ -3,9 +3,9 @@ import { PoiListElementType } from './types'
 export const POI_IMAGE_PLACEHOLDERS: {
   [key in PoiListElementType['type']]: string
 } = {
-  attraction: 'https://assets.triple.guide/images/ico-blank-see@2x.png',
-  restaurant: 'https://assets.triple.guide/images/ico-blank-eat@2x.png',
-  hotel: 'https://assets.triple.guide/images/ico-blank-hotel@2x.png',
+  attraction: 'https://assets.triple.guide/images/ico_blank_see@2x.png',
+  restaurant: 'https://assets.triple.guide/images/ico_blank_eat@2x.png',
+  hotel: 'https://assets.triple.guide/images/ico_blank_hotel@2x.png',
 }
 
 export const TYPE_NAMES: { [key in PoiListElementType['type']]: string } = {
