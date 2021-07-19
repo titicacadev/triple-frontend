87a700b9b11acd92ddc66e61558e55b4733b05e0
finn <finn@triple-corp.com>
2021-07-19 17:58:06 +0900

    fix: fetcher params 주석 내용 보강

--- packages/fetcher/src/types.ts
+++ packages/fetcher/src/types.ts
@@ -3,10 +3,12 @@ import { IncomingMessage } from 'http'
 import { HttpError } from './error'
 
 export type RequestOptions = Omit<RequestInit, 'body'> & {
-  /** request object ssr only */
   /**
-   * @deprecated TF v3.x부터 req를 한번에 받지 않고 용도별로 세분화하여 받도록 합니다.
-   * cookie, absoluteUrl을 사용해주세요!
+   * @deprecated TF v3.x부터 req보다는 cookie, absoluteUrl의 사용을 권장합니다!
+   * req를 직접 참조할 시 생기는 이슈
+   * 1. "서버환경에서 쿠키가 필요없는 상황"에서 사용하기 애매 (우회를 해야하는 번거로움)
+   * 2. 쿠키가 누락되는 현상 발생
+   *
    * 참조: https://github.com/titicacadev/triple-frontend/issues/1334
    */
   req?: IncomingMessage
@@ -18,7 +20,15 @@ export type RequestOptions = Omit<RequestInit, 'body'> & {
    * RequestInit.body의 타입 오버라이드
    */
   body?: BodyInit | { [key: string]: any }
+  /**
+   * cookie를 인자로 받을 시
+   * cookie를 universal-cookie로 파싱하여 x-soto-session 값을 요청 헤더로 전달
+   */
   cookie?: string
+  /**
+   * absoluteUrl true일 때
+   * URL에 API base URL을 붙여 요청을 절대 경로로 보낼 수 있게 만듬
+   */
   absoluteUrl?: boolean
 }
 
