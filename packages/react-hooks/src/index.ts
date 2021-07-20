aa884c4794353159c8abbace3ccf6e6cfd7e3bf6
woody <woody@triple-corp.com>
2021-07-20 11:29:10 +0900

    🚚 useThrowError -> useErrorHandler

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -4,6 +4,6 @@ export * from './use-visibility-change'
 export * from './use-scroll-to-anchor'
 export * from './use-scroll-to-element'
 export * from './use-lottie'
-export * from './use-throw-error'
+export * from './use-error-handler'
 
 export { default as useDebouncedState } from './use-debounce'
