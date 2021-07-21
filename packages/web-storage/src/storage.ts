aa814a16bd5e922b298a2d3b2337f7826c98f2e3
woody <woody@triple-corp.com>
2021-07-21 15:42:52 +0900

    ♻️ cookie를 storage로 사용하는 코드를 별도 함수로 분리

--- packages/web-storage/src/storage.ts
+++ packages/web-storage/src/storage.ts
@@ -14,62 +14,66 @@ function addCookieKeyPrefix(key: string) {
   return `${COOKIE_KEY_PREFIX}/${key}`
 }
 
+function getCookieStorage({ storageType }: { storageType: WebStorageType }) {
+  if (!cookieAvailable()) {
+    throw new WebStorageError({ type: 'Unavailable', storageType })
+  }
+
+  const cookie = new Cookies()
+  const keys = Object.keys(cookie.getAll()).filter((key) =>
+    key.startsWith(COOKIE_KEY_PREFIX),
+  )
+
+  return {
+    get length() {
+      return keys.length
+    },
+
+    key(index: number): string | null {
+      return keys[index] ?? null
+    },
+
+    getItem(key: string): string | null {
+      return cookie.get(addCookieKeyPrefix(key), {
+        doNotParse: true,
+      })
+    },
+
+    setItem(key: string, value: string) {
+      try {
+        // TODO: 초기화되는 로직이 쿠키와 Storage가 약간 다르다.
+        return cookie.set(addCookieKeyPrefix(key), value)
+      } catch (error) {
+        if (checkQuotaExceededError(error)) {
+          throw new WebStorageError({
+            type: 'QuotaExceeded',
+            storageType,
+          })
+        }
+
+        throw error
+      }
+    },
+
+    removeItem(key: string) {
+      return cookie.remove(addCookieKeyPrefix(key))
+    },
+
+    clear() {
+      keys.forEach((key) => {
+        cookie.remove(key)
+      })
+    },
+  }
+}
+
 export function getWebStorage(type: WebStorageType = 'localStorage') {
   if (typeof window === 'undefined') {
     throw new WebStorageError({ type: 'NotBrowser', storageType: type })
   }
 
   if (!storageAvailable(type)) {
-    if (!cookieAvailable()) {
-      throw new WebStorageError({ type: 'Unavailable', storageType: type })
-    }
-
-    const cookie = new Cookies()
-    const keys = Object.keys(cookie.getAll()).filter((key) =>
-      key.startsWith(COOKIE_KEY_PREFIX),
-    )
-
-    return {
-      get length() {
-        return keys.length
-      },
-
-      key(index: number): string | null {
-        return keys[index] ?? null
-      },
-
-      getItem(key: string): string | null {
-        return cookie.get(addCookieKeyPrefix(key), {
-          doNotParse: true,
-        })
-      },
-
-      setItem(key: string, value: string) {
-        try {
-          // TODO: 초기화되는 로직이 쿠키와 Storage가 약간 다르다.
-          return cookie.set(addCookieKeyPrefix(key), value)
-        } catch (error) {
-          if (checkQuotaExceededError(error)) {
-            throw new WebStorageError({
-              type: 'QuotaExceeded',
-              storageType: type,
-            })
-          }
-
-          throw error
-        }
-      },
-
-      removeItem(key: string) {
-        return cookie.remove(addCookieKeyPrefix(key))
-      },
-
-      clear() {
-        keys.forEach((key) => {
-          cookie.remove(key)
-        })
-      },
-    }
+    return getCookieStorage({ storageType: type })
   }
 
   const storage = window[type]
