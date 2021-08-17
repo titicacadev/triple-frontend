a9b6279f808f59022056885c2fc71300b73bba29
woody <woody@triple-corp.com>
2021-08-17 15:26:08 +0900

    🎨 fetch의 파라미터를 생성하는 로직을 별도 함수로 분리
    테스트 코드 작성을 쉽게 할 수 있도록 분리합니다.

---
+++ packages/fetcher/src/make-request-params.ts
@@ -0,0 +1,53 @@
+import Cookies from 'universal-cookie'
+
+import { RequestOptions } from './types'
+
+export function makeRequestParams(
+  href: string,
+  {
+    req,
+    useBodyAsRaw,
+    retryable,
+    body,
+    headers: customHeaders,
+    cookie,
+    withApiUriBase,
+    ...rest
+  }: RequestOptions,
+): [string, RequestInit | undefined] {
+  if (req && !process.env.API_URI_BASE) {
+    throw new Error(
+      'Insufficient environment variables in `.env.*` files\n- API_URI_BASE',
+    )
+  }
+
+  const baseUrl: string =
+    req || withApiUriBase ? (process.env.API_URI_BASE as string) : ''
+
+  const reqUrl: string = baseUrl + href
+
+  const sessionId = req
+    ? new Cookies(req.headers.cookie).get('x-soto-session')
+    : undefined
+
+  const headers = {
+    ...customHeaders,
+    ...(body && !useBodyAsRaw && { 'Content-Type': 'application/json' }),
+    ...(sessionId && { 'X-Soto-Session': sessionId }),
+    ...(cookie && { cookie }),
+  }
+
+  return [
+    reqUrl,
+    {
+      credentials: 'same-origin',
+      headers,
+      body: body
+        ? useBodyAsRaw
+          ? (body as BodyInit)
+          : JSON.stringify(body)
+        : undefined,
+      ...rest,
+    },
+  ]
+}
