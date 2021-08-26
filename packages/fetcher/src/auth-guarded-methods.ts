1328ccf74cf4f2fca2bedbb690aaf79ec4650d34
woody <woody@triple-corp.com>
2021-08-26 15:27:28 +0900

    ✨ authGuardedFetchers에 fetcher 함수 추가
    일반적인 상황에 사용할 수 있는 fetcher 함수를 추가합니다.

--- packages/fetcher/src/auth-guarded-methods.ts
+++ packages/fetcher/src/auth-guarded-methods.ts
@@ -1,7 +1,11 @@
 import { authFetcherize } from './factories'
+import { fetcher } from './fetcher'
 import { del, get, post, put } from './methods'
 
 export const authGuardedFetchers = {
+  fetcher: authFetcherize((href, options) => fetcher(href, options || {}), {
+    refresh: () => post('/api/users/web-session/token'),
+  }),
   get: authFetcherize(get, {
     refresh: () => post('/api/users/web-session/token'),
   }),
