0b90a221b88b76012c2df25d8d23711679927e04
drakang4 <drakang4@gmail.com>
2021-09-01 14:19:43 +0900

    feat(react-contexts): scrap, unscrape api client에 tna type 추가

--- packages/react-contexts/src/scraps-context/api-client.ts
+++ packages/react-contexts/src/scraps-context/api-client.ts
@@ -2,15 +2,26 @@ import fetch from 'isomorphic-fetch'
 
 import { Target } from './types'
 
+function mapTypes(type: unknown) {
+  switch (type) {
+    case 'article':
+      return 'articles'
+    case 'tna':
+      return 'tna'
+    default:
+      return 'pois'
+  }
+}
+
 export function scrape({ id, type }: Target) {
-  return fetch(`/api/${type === 'article' ? 'articles' : 'pois'}/${id}/scrap`, {
+  return fetch(`/api/${mapTypes(type)}/${id}/scrap`, {
     method: 'POST',
     credentials: 'same-origin',
   })
 }
 
 export function unscrape({ id, type }: Target) {
-  return fetch(`/api/${type === 'article' ? 'articles' : 'pois'}/${id}/scrap`, {
+  return fetch(`/api/${mapTypes(type)}/${id}/scrap`, {
     method: 'DELETE',
     credentials: 'same-origin',
   })
