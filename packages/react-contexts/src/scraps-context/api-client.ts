f2045059943227ddc72f9f186b4065080748cd98
Heeryong Kang <drakang4@gmail.com>
2021-09-02 17:23:36 +0900

    feat(react-contexts): scrap, unscrape api endpoint 변경

--- packages/react-contexts/src/scraps-context/api-client.ts
+++ packages/react-contexts/src/scraps-context/api-client.ts
@@ -14,9 +14,9 @@ function mapTypes(type: unknown) {
 }
 
 export function scrape({ id, type }: Target) {
-  return post(`/api/${mapTypes(type)}/${id}/scrap`)
+  return post(`/api/scraps/${mapTypes(type)}/${id}/scrap`)
 }
 
 export function unscrape({ id, type }: Target) {
-  return del(`/api/${mapTypes(type)}/${id}/scrap`)
+  return del(`/api/scraps/${mapTypes(type)}/${id}/scrap`)
 }
