a2f03058928fe2a5cc2f69900395b6fd71f05bf2
finn <finn@triple-corp.com>
2021-09-07 13:27:35 +0900

    fix: containerId 조건 추가

--- packages/google-optimize/src/google-optimize-context.tsx
+++ packages/google-optimize/src/google-optimize-context.tsx
@@ -48,7 +48,7 @@ export function GoogleOptimizeExperimentProvider({
     })
   }, [experimentId])
 
-  if (!experimentId) {
+  if (!experimentId || !containerId) {
     return <>{children}</>
   }
 
