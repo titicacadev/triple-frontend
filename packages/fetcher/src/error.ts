77c45a17e58562b694289d3333a43dfe8f8a3d5d
woody <woody@triple-corp.com>
2021-09-24 15:30:49 +0900

    🔥 불필요한 주석 제거

--- packages/fetcher/src/error.ts
+++ packages/fetcher/src/error.ts
@@ -2,26 +2,12 @@ import { CustomError } from 'ts-custom-error'
 
 import { HttpErrorResponse } from './types'
 
-// 미리 정의된 공통 에러 포맷
 const DEFAULT_HTTP_ERROR_RESPONSE: HttpErrorResponse = {
   code: 'UNKNOWN',
   message: '알 수 없는 에러가 발생하였습니다.',
   name: 'UNKNOWN',
 }
 
-/**
- * 공통 에러 모듈
- *
- * 현재는 fetch 수행 이후 에러가 발생(`서버에서 에러를 응답을 내려주면` 이 아님)하면
- * Sentry 로깅을 하는 일만 수행함
- *
- * TODO:
- *
- * - [ ] 각 서비스별 error response 타입이 다를 수 있기 때문에 generic type 을 허용해야함
- * - [ ] fetch 가 정상 수행되고 서버에서 4xx, 5xx 의 응답을 반환하는 경우에도 공통 에러 객체를 반환하는 형태로 개선
- *
- * ref - https://github.com/adriengibrat/ts-custom-error
- */
 export class HttpError<E> extends CustomError {
   private readonly _statusCode: number
 
@@ -32,8 +18,6 @@ export class HttpError<E> extends CustomError {
     super(statusText)
     this._statusCode = status
     this._errorData = errorData
-
-    // Sentry.captureException(errorData)
   }
 
   is(code: number) {
