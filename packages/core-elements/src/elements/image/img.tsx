348c66b436a866df0d9394be424a9f015ce83b4c
Gi Sung Park <43316372+pgs787@users.noreply.github.com>
2021-09-27 12:31:54 +0900

    fix: adjust feedback

--- packages/core-elements/src/elements/image/img.tsx
+++ packages/core-elements/src/elements/image/img.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import * as CSS from 'csstype'
 
 import { useImageState } from './context'
 import { useContentAbsolute } from './fixed-ratio-frame'
@@ -8,6 +9,7 @@ const Img = styled.img<{
   borderRadius: number
   dimmed?: boolean
   absolute: boolean
+  cursor?: CSS.Property.Cursor
 }>`
   width: 100%;
   height: 100%;
