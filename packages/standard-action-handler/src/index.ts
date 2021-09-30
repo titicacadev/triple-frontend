54ab8c11cd70ac579a50ef7044686da8f5040804
finn <finn@triple-corp.com>
2021-09-30 12:01:16 +0900

    feat: 공유 액션 핸들러 생성

--- packages/standard-action-handler/src/index.ts
+++ packages/standard-action-handler/src/index.ts
@@ -3,11 +3,12 @@ import serial from './serial'
 import invokeCta from './invoke-cta'
 import showToast from './show-toast'
 import fetchApi from './fetch-api'
+import share from './share'
 import { ContextOptions } from './types'
 
 export function initialize(options: ContextOptions) {
   const handler = new Handler({
-    handlers: [serial, invokeCta, showToast, fetchApi],
+    handlers: [serial, invokeCta, showToast, fetchApi, share],
     options,
   })
 
