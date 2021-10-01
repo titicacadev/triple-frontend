5ea37e1616bc065b074745abcaae9cc780e643a7
Heeryong Kang <drakang4@gmail.com>
2021-10-01 14:47:20 +0900

    docs: public-header 스토리 변경

--- docs/stories/public-header/public-header.stories.tsx
+++ docs/stories/public-header/public-header.stories.tsx
@@ -1,25 +1,51 @@
 import React from 'react'
-import { text, boolean, select } from '@storybook/addon-knobs'
-import PublicHeader from '@titicaca/public-header'
+import PublicHeader, { PublicHeaderProps } from '@titicaca/public-header'
+import { Meta, Story } from '@storybook/react'
+import {
+  EnvProvider,
+  generateUserAgentValues,
+  UserAgentProvider,
+} from '@titicaca/react-contexts'
 
 export default {
   title: 'public-header / PublicHeader',
+  component: PublicHeader,
+  argTypes: {
+    category: {
+      control: { type: 'select' },
+      options: ['air', 'hotels', 'tna'],
+    },
+  },
+} as Meta
+
+const Template: Story<PublicHeaderProps> = (args) => {
+  return (
+    <EnvProvider
+      afOnelinkId=""
+      afOnelinkPid=""
+      afOnelinkSubdomain=""
+      appUrlScheme=""
+      defaultPageDescription=""
+      defaultPageTitle=""
+      facebookAppId=""
+      webUrlBase=""
+    >
+      <UserAgentProvider value={generateUserAgentValues(navigator.userAgent)}>
+        <PublicHeader {...args} />
+      </UserAgentProvider>
+    </EnvProvider>
+  )
 }
 
-export function BasePublicHeader() {
-  return (
-    <PublicHeader
-      fixed={boolean('fixed', false)}
-      deeplinkHref={text('deeplinkHref', 'https://triple.guide')}
-      category={select(
-        'category',
-        {
-          air: 'air',
-          hotels: 'hotels',
-          tna: 'tna',
-        },
-        'air',
-      )}
-    />
-  )
+export const Basic = Template.bind({})
+Basic.args = {
+  fixed: false,
+  deeplinkPath: 'https://triple.guide',
+  category: undefined,
+}
+
+export const DisableAutoHide = Template.bind({})
+DisableAutoHide.args = {
+  ...Basic.args,
+  disableAutohide: true,
 }
