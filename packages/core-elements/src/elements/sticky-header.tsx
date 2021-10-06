c4d85f9c27e44636bee5f2c51a513246ca7d4c5e
Heeryong Kang <drakang4@gmail.com>
2021-10-06 15:40:50 +0900

    feat(core-elements): sticky-header 추가

---
+++ packages/core-elements/src/elements/sticky-header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  position: sticky;
+  top: 0;
+  z-index: 1;
+`
+
+interface Props {
+  children?: React.ReactNode
+}
+
+function StickyHeader({ children }: Props) {
+  return <StyledHeader>{children}</StyledHeader>
+}
+
+export default StickyHeader
