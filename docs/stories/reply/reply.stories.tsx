be43cca308f07f3658b2792493e1f69b066978f0
Gi Sung Park <43316372+pgs787@users.noreply.github.com>
2021-10-08 15:48:03 +0900

    fix: comment

--- docs/stories/reply/reply.stories.tsx
+++ docs/stories/reply/reply.stories.tsx
@@ -1,25 +1,37 @@
 import React from 'react'
 import Reply from '@titicaca/reply'
+import {
+  TransitionModal,
+  TransitionType,
+  useTransitionModal,
+} from '@titicaca/modals'
 
 import {
   itinerary as ItineraryData,
   reactions as ReactionsData,
   reply as ReplyData,
 } from '../__mocks__/reply.sample.json'
+import { historyProviderDecorator } from '../../decorators'
 
 export default {
   title: 'Reply',
 }
 
 export function Replies() {
+  const { show } = useTransitionModal()
+
   return (
-    <Reply
-      itinerary={ItineraryData}
-      reactions={ReactionsData}
-      reply={ReplyData}
-      onClick={() => alert('테스트')}
-    />
+    <>
+      <Reply
+        itinerary={ItineraryData}
+        reactions={ReactionsData}
+        reply={ReplyData}
+        onClick={() => show(TransitionType.General)}
+      />
+      <TransitionModal deepLink="" />
+    </>
   )
 }
 
 Replies.storyName = '기본 댓글'
+Replies.decorators = [historyProviderDecorator]
