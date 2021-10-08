2a191c2f5d0c8cdcb2a297fb59dc0dba6461bf49
Gi Sung Park <43316372+pgs787@users.noreply.github.com>
2021-10-08 17:22:04 +0900

    change: reply storybook

--- docs/stories/reply/reply.stories.tsx
+++ docs/stories/reply/reply.stories.tsx
@@ -1,32 +1,26 @@
 import React from 'react'
 import Reply from '@titicaca/reply'
-import {
-  TransitionModal,
-  TransitionType,
-  useTransitionModal,
-} from '@titicaca/modals'
+import { TransitionModal } from '@titicaca/modals'
+import { text } from '@storybook/addon-knobs'
 
 import {
   itinerary as ItineraryData,
   reactions as ReactionsData,
   reply as ReplyData,
 } from '../__mocks__/reply.sample.json'
-import { historyProviderDecorator } from '../../decorators'
 
 export default {
   title: 'Reply',
 }
 
 export function Replies() {
-  const { show } = useTransitionModal()
-
   return (
     <>
       <Reply
         itinerary={ItineraryData}
         reactions={ReactionsData}
         reply={ReplyData}
-        onClick={() => show(TransitionType.General)}
+        onClick={() => alert(text('알림 내용 ', 'onClick 테스트'))}
       />
       <TransitionModal deepLink="" />
     </>
@@ -34,4 +28,3 @@ export function Replies() {
 }
 
 Replies.storyName = '기본 댓글'
-Replies.decorators = [historyProviderDecorator]
