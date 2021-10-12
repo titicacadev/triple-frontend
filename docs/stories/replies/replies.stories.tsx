1e56966e628e48e220161b28b8520dde0573861e
Gi Sung Park <43316372+pgs787@users.noreply.github.com>
2021-10-12 17:40:22 +0900

    change: customOnClick => onClick

--- docs/stories/replies/replies.stories.tsx
+++ docs/stories/replies/replies.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
+import Replies from '@titicaca/replies'
 
-import Replies from '../../../packages/replies'
 import { reply as repliesData } from '../__mocks__/replies.sample.json'
 
 export default {
@@ -12,7 +12,7 @@ export function RepliesStory() {
   return (
     <Replies
       replies={repliesData}
-      customOnClick={action('onClick 적용된 아이콘, 버튼 클릭')}
+      onClick={action('onClick 적용된 아이콘, 버튼 클릭')}
     />
   )
 }
