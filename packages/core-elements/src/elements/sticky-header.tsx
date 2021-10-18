f442440a73c132eb2c222962832d32b1d7bd220f
Heeryong Kang <drakang4@gmail.com>
2021-10-18 13:01:10 +0900

    fix(core-elements): sticky-header의 zTier, zIndex 기본 값 수정

--- packages/core-elements/src/elements/sticky-header.tsx
+++ packages/core-elements/src/elements/sticky-header.tsx
@@ -6,12 +6,12 @@ import { layeringMixin, LayeringMixinProps } from '../mixins'
 const StyledHeader = styled.header<LayeringMixinProps>`
   position: sticky;
   top: 0;
-  ${layeringMixin(10)}
+  ${layeringMixin(0)}
 `
 
 export type StickyHeaderProps = React.PropsWithChildren<LayeringMixinProps>
 
-function StickyHeader({ children, zIndex, zTier }: StickyHeaderProps) {
+function StickyHeader({ children, zIndex = 3, zTier }: StickyHeaderProps) {
   return (
     <StyledHeader zIndex={zIndex} zTier={zTier}>
       {children}
