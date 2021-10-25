876ce5dd3c8eb7810f2154df1c8e3f519cb85ad5
Heeryong Kang <drakang4@gmail.com>
2021-10-25 12:39:35 +0900

    feat(core-elements): polymorphic 유틸 타입 추가

---
+++ packages/core-elements/src/polymorphic.ts
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Merge<T, U> = Omit<T, keyof U> & U
+
+type PropsWithAsProp<P, C extends Polymorphic> = P & { as?: C }
+
+export type Polymorphic =
+  | keyof JSX.IntrinsicElements
+  | React.ComponentType
+  | undefined
+
+export type PolymorphicProps<P, C extends Polymorphic> = Merge<
+  C extends keyof JSX.IntrinsicElements
+    ? React.PropsWithoutRef<JSX.IntrinsicElements[C]>
+    : C extends React.ComponentType
+    ? React.ComponentPropsWithoutRef<C>
+    : {},
+  PropsWithAsProp<P, C>
+>
