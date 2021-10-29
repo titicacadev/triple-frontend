63b13b94449981dfdd19108949f01984619abac8
Heeryong Kang <drakang4@gmail.com>
2021-10-29 14:13:39 +0900

    chore(docs): storybook에 next.js router mock 설정

--- docs/.storybook/preview.js
+++ docs/.storybook/preview.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { addDecorator } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
 import { GlobalStyle } from '@titicaca/core-elements'
+import { RouterContext } from 'next/dist/next-server/lib/router-context' // next < 11.2
 
 addDecorator(withKnobs)
 addDecorator((stories) => (
@@ -10,3 +11,9 @@ addDecorator((stories) => (
     {stories()}
   </>
 ))
+
+export const parameters = {
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+}
