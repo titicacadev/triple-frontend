e2bac191f10d0d93b845130091300057a545b8c6
finn <finn@triple-corp.com>
2021-11-01 17:00:41 +0900

    fix: lineHeight 값 관리 수정

--- packages/replies/src/auto-resizing-textarea.tsx
+++ packages/replies/src/auto-resizing-textarea.tsx
@@ -1,17 +1,17 @@
 import React, { ChangeEvent, useState } from 'react'
 import styled from 'styled-components'
 
-const Textarea = styled.textarea`
+const Textarea = styled.textarea<{ lineHeight: number }>`
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial,
     sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
   resize: none;
   width: calc(100% - 46px);
   font-size: 15px;
-  line-height: 19px;
   outline: none;
   border: none;
   padding: 0;
   color: var(--color-gray);
+  line-height: ${({ lineHeight }) => lineHeight}px;
 
   ::placeholder {
     color: var(--color-gray300);
@@ -60,6 +60,7 @@ export default function AutoResizingTextarea({
       value={value}
       placeholder={placeholder || '이 일정에 궁금한 점은 댓글로 써주세요.'}
       onChange={handleChange}
+      lineHeight={TEXTAREA_LINE_HEIGHT}
     />
   )
 }
