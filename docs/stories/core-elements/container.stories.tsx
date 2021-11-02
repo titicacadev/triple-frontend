ba2346f54c00db9d878742d0d61c6b28f79d665c
Heeryong Kang <drakang4@gmail.com>
2021-11-02 12:40:40 +0900

    docs: container, sticky-header stories 추가

---
+++ docs/stories/core-elements/container.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from '@titicaca/core-elements'
+import { Meta, Story } from '@storybook/react'
+
+export default {
+  title: 'Core-Elements / Container',
+  component: Container,
+} as Meta
+
+const Template: Story = (args) => <Container {...args} />
+
+export const Basic = Template.bind({})
+Basic.args = { children: 'Basic Container' }
+
+export const CustomCSS = Template.bind({})
+CustomCSS.args = {
+  children: 'Custom CSS Container',
+  css: { padding: 50, backgroundColor: 'gray', color: 'white' },
+  borderRadius: 10,
+}
