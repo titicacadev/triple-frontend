002f9c464604209f9a27e9ce6e1095654fb7d5d3
Inbeom Hwang <hwanginbeom@gmail.com>
2021-11-05 16:56:01 +0900

    Fire visibilitychange event on public pages

--- packages/react-hooks/src/use-visibility-change.ts
+++ packages/react-hooks/src/use-visibility-change.ts
@@ -1,25 +1,18 @@
 import { useEffect } from 'react'
-import { useUserAgentContext } from '@titicaca/react-contexts'
 
 export function useVisibilityChange(
   onChange: (visible: boolean) => void,
   deps: unknown[] = [],
 ) {
-  const { isPublic } = useUserAgentContext()
-
   useEffect(() => {
     function handleChange() {
       onChange(!document.hidden)
     }
 
-    if (!isPublic) {
-      document.addEventListener('visibilitychange', handleChange)
-    }
+    document.addEventListener('visibilitychange', handleChange)
 
     return () => {
-      if (!isPublic) {
-        document.removeEventListener('visibilitychange', handleChange)
-      }
+      document.removeEventListener('visibilitychange', handleChange)
     }
   }, deps) // eslint-disable-line react-hooks/exhaustive-deps
 }
