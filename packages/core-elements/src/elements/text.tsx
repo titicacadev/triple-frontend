551a0d768de509d041b3914ee0f74f81b3bfa29a
Heeryong Kang <drakang4@gmail.com>
2021-11-06 20:46:12 +0900

    refactor(core-elements): text 컴포넌트 리팩토링
    refactor(core-elements): text 컴포넌트 리팩토링

--- packages/core-elements/src/elements/text.tsx
+++ packages/core-elements/src/elements/text.tsx
@@ -128,6 +128,9 @@ function TextTitle({ css, children, margin }: TextTitleProps) {
     </TextTitleBase>
   )
 }
+;(Text as CompoundedText).Html = TextHtml
+;(Text as CompoundedText).Title = TextTitle
+;(Text as CompoundedText).WithRef = Text
 
 type CompoundedText = typeof Text & {
   Html: typeof TextHtml
