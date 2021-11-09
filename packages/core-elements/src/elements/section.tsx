d77ea364ffb83d5492bb854c5fb167657735d18e
Heeryong Kang <drakang4@gmail.com>
2021-11-09 13:13:25 +0900

    refactor(core-elements): section 리팩토링

--- packages/core-elements/src/elements/section.tsx
+++ packages/core-elements/src/elements/section.tsx
@@ -1,43 +1,50 @@
-import * as React from 'react'
+import React from 'react'
+import { css } from 'styled-components'
 
-import Container from './container'
+import Container, { ContainerProps } from './container'
 import { HR2 } from './hr'
 
-export default function Section({
+export interface SectionProps extends ContainerProps {
+  divider?: string
+  anchor?: string
+}
+
+function Section({
+  css: _css,
+  children,
   divider,
   anchor,
-  children,
   minWidth = 320,
   maxWidth = 768,
   padding = { left: 30, right: 30 },
   ...props
-}: {
-  divider?: string
-  anchor?: string
-  children?: React.ReactNode
-} & Parameters<typeof Container>['0']) {
-  if (React.Children.toArray(children).length > 0) {
-    return (
-      <>
-        {divider === 'top' && <HR2 compact />}
-
-        <Container
-          id={anchor}
-          position="relative"
-          clearing
-          centered
-          minWidth={minWidth}
-          maxWidth={maxWidth}
-          padding={padding}
-          {...props}
-        >
-          {children}
-        </Container>
-
-        {divider === 'bottom' && <HR2 compact />}
-      </>
-    )
+}: SectionProps) {
+  if (!children) {
+    return null
   }
 
-  return null
+  return (
+    <>
+      {divider === 'top' && <HR2 compact />}
+      <Container
+        id={anchor}
+        css={css`
+          position: relative;
+          margin-left: auto;
+          margin-right: auto;
+          ${_css}
+        `}
+        clearing
+        minWidth={minWidth}
+        maxWidth={maxWidth}
+        padding={padding}
+        {...props}
+      >
+        {children}
+      </Container>
+      {divider === 'bottom' && <HR2 compact />}
+    </>
+  )
 }
+
+export default Section
