4cc295e5454eda746c062363bd2396ba1ca01c59
Heeryong Kang <drakang4@gmail.com>
2021-11-09 13:12:48 +0900

    refactor(core-elements): stack 리팩토링

---
+++ packages/core-elements/src/elements/stack.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Stack from './stack'
+
+import 'jest-styled-components'
+
+it('should override style with css prop', () => {
+  const tree = renderer
+    .create(<Stack position="absolute" css={{ position: 'fixed' }} />)
+    .toJSON()
+
+  expect(tree).toHaveStyleRule('position', 'fixed')
+})
