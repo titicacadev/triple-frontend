3f4eb6473663d137634d5192c8169c18ce1e6f82
Heeryong Kang <drakang4@gmail.com>
2021-11-10 14:00:30 +0900

    test(core-elements): section children, divider 테스트 추가

--- packages/core-elements/src/elements/section.test.tsx
+++ packages/core-elements/src/elements/section.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import renderer, { ReactTestRendererJSON } from 'react-test-renderer'
 
 import Section from './section'
 
 import 'jest-styled-components'
+
+it('should render null if children is empty', () => {
+  const tree = renderer.create(<Section />).toJSON()
+
+  expect(tree).toBeNull()
+})
+
+it('should show top divider', () => {
+  const tree = renderer
+    .create(<Section divider="top">Section</Section>)
+    .toJSON()
+
+  expect(tree).toHaveLength(2)
+  expect((tree as ReactTestRendererJSON[])[0].children).toBeNull()
+  expect((tree as ReactTestRendererJSON[])[1].children).toContain('Section')
+})
+
+it('should show bottom divider', () => {
+  const tree = renderer
+    .create(<Section divider="bottom">Section</Section>)
+    .toJSON()
+
+  expect(tree).toHaveLength(2)
+  expect((tree as ReactTestRendererJSON[])[0].children).toContain('Section')
+  expect((tree as ReactTestRendererJSON[])[1].children).toBeNull()
+})
 
 it('should override style with css prop', () => {
   const tree = renderer
