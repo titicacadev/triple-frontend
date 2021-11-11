60508bcfb5577b963bfe981746c9613701c8cd95
woody <woody@triple-corp.com>
2021-11-11 14:10:46 +0900

    ♻️ captureHttpError export 방식 변경

--- packages/fetcher/src/response-handler.ts
+++ packages/fetcher/src/response-handler.ts
@@ -1,10 +1,8 @@
 import { HttpResponse } from './types'
 
-function captureHttpError<Response extends HttpResponse<any, any>>(
+export function captureHttpError<Response extends HttpResponse<any, any>>(
   response: Response,
 ): void {
   const { error: { capture } = { capture: () => {} } } = response
   capture()
 }
-
-export { captureHttpError }
