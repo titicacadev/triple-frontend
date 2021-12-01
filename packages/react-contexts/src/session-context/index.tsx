3fb2baf95e0fec91bc2a3454e0ae3f0d350b7187
woody <woody@triple-corp.com>
2021-12-01 10:36:34 +0900

    ✨ request로 세션 존재 유무를 파악할 수 있는 함수 구현
    getServerSideProps에서 활용할 수 있습니다.

--- packages/react-contexts/src/session-context/index.tsx
+++ packages/react-contexts/src/session-context/index.tsx
@@ -1,3 +1,4 @@
 export { default as SessionContextProvider } from './provider'
 export { useSessionAvailability, useSessionControllers } from './hooks'
 export { useUser } from './user'
+export { default as getSessionAvailabilityFromRequest } from './session-availability'
