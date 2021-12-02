6775c84a8e95aa5d1876cce91b02de431a787c1a
Heeryong Kang <drakang4@gmail.com>
2021-12-02 18:51:44 +0900

    build(docs): webpack5, babel mode v7, story store v7 사용

--- docs/.storybook/main.js
+++ docs/.storybook/main.js
@@ -1,43 +1,18 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|mdx|tsx)'],
   addons: [
-    '@storybook/addon-actions/register',
-    '@storybook/addon-controls',
-    '@storybook/addon-links/register',
-    '@storybook/addon-viewport/register',
-    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
     'storybook-addon-next-router',
   ],
   typescript: {
     check: true,
   },
-  webpackFinal: async (config, { configType }) => {
-    config.mode = 'development'
-    config.module.rules.push({
-      test: /\.(js|ts|tsx)$/,
-      exclude: /node_modules/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [['react-app', { flow: false, typescript: true }]],
-            plugins: [
-              ['styled-components', { ssr: true, displayName: true }],
-              ['@babel/plugin-proposal-class-properties'],
-            ],
-          },
-        },
-      ],
-      resolve: {
-        extensions: ['.js', '.ts', '.tsx', '.json'],
-      },
-    })
-
-    config.watchOptions = {
-      ignored: /node_modules\/.+tsx?$/,
-      aggregateTimeout: 1000,
-    }
-
-    return config
+  core: {
+    builder: 'webpack5',
+  },
+  features: {
+    babelModeV7: true,
+    storyStoreV7: true,
   },
 }
