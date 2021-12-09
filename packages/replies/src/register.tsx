60541ee0c1dd827d4b599265972bd7b99ca10190
finn <finn@triple-corp.com>
2021-12-09 13:42:45 +0900

    feat: Register Component File

---
+++ packages/replies/src/register.tsx
@@ -0,0 +1,65 @@
+import React, { Ref, useState } from 'react'
+import styled from 'styled-components'
+import { Container, FlexBox, HR1 } from '@titicaca/core-elements'
+
+import AutoResizingTextarea from './auto-resizing-textarea'
+
+const RegisterButton = styled.button`
+  width: 26px;
+  padding: 0;
+  margin-left: 20px;
+  line-height: 1.2;
+  font-size: 15px;
+  font-weight: bold;
+  color: var(--color-blue);
+  background: inherit;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`
+
+export default function Register({
+  registerPlaceholder,
+  textareaRef,
+  onSubmit,
+}: {
+  registerPlaceholder?: string
+  textareaRef?: Ref<HTMLTextAreaElement>
+  onSubmit: (replyContent: string) => void
+}) {
+  const [replyContent, setReplyContent] = useState('')
+
+  return (
+    <Container cursor="pointer">
+      <HR1 margin={{ top: 0 }} />
+
+      <FlexBox
+        flex
+        alignItems="flex-end"
+        padding={{ top: 20, bottom: 20, left: 20, right: 20 }}
+      >
+        <AutoResizingTextarea
+          placeholder={
+            registerPlaceholder || '이 일정에 궁금한 점은 댓글로 써주세요.'
+          }
+          minRows={1}
+          maxRows={4}
+          value={replyContent}
+          onChange={setReplyContent}
+          ref={textareaRef}
+        />
+
+        <RegisterButton
+          onClick={() => {
+            onSubmit(replyContent)
+            setReplyContent('')
+          }}
+        >
+          등록
+        </RegisterButton>
+      </FlexBox>
+
+      <HR1 margin={{ top: 0 }} />
+    </Container>
+  )
+}
