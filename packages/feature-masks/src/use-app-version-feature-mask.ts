25952c7e1bf54757ed50c3d73cf921f05918c4a5
Inbeom Hwang <hwanginbeom@gmail.com>
2021-12-09 13:42:00 +0900

    Support osName

--- packages/feature-masks/src/use-app-version-feature-mask.ts
+++ packages/feature-masks/src/use-app-version-feature-mask.ts
@@ -14,19 +14,24 @@ export function useAppVersionFeatureMask({
   operator,
   version,
   availableOnPublic,
+  osName,
 }: {
   operator: Operator
   version: string
   availableOnPublic: boolean
+  osName?: ReturnType<typeof useUserAgentContext>['os']['name']
 }) {
-  const { isPublic, app } = useUserAgentContext()
+  const { isPublic, app, os } = useUserAgentContext()
 
   if (isPublic) {
     return availableOnPublic
   } else {
     const appVersion = semver.coerce(app?.version)
     const operation = VERSION_OPERATIONS[operator]
+    const hasMatchingOs = osName !== undefined ? os?.name === osName : true
 
-    return Boolean(appVersion && operation(appVersion, version))
+    return Boolean(
+      appVersion && operation(appVersion, version) && hasMatchingOs,
+    )
   }
 }
