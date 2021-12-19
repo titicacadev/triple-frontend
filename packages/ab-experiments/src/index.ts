508ce9e168ce8e94ebd3284e07caa8e6d4a82a18
finn <finn@triple-corp.com>
2021-12-19 18:33:24 +0900

    refactor: move ab-experiment-context in react-contexts to ab-experiments packages

--- packages/ab-experiments/src/index.ts
+++ packages/ab-experiments/src/index.ts
@@ -2,3 +2,13 @@ export {
   GoogleOptimizeExperimentProvider,
   useExperimentVariant,
 } from './google-optimize-context'
+
+export {
+  getABExperiment,
+  ABExperimentMeta,
+  ABExperimentProvider,
+  useABExperimentVariant,
+  useABExperimentConversionTracker,
+  useABExperimentImpressionTracker,
+  useABExperimentContext,
+} from './ab-experiment-context'
