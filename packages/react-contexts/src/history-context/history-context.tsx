479ac2517ee90fcff842798dc8f72320993f1398
finn <finn@triple-corp.com>
2021-12-19 17:50:36 +0900

    delete: HistoryProvider deprecated props (appUrlScheme, webUrlBase)

--- packages/react-contexts/src/history-context/history-context.tsx
+++ packages/react-contexts/src/history-context/history-context.tsx
@@ -97,14 +97,6 @@ export enum HashStrategy {
   PUSH,
 }
 export type HistoryProviderProps = PropsWithChildren<{
-  /**
-   * @deprecated EnvProvider가 있으면 이 prop을 넣어주지 않아도 됩니다.
-   */
-  appUrlScheme?: string
-  /**
-   * @deprecated EnvProvider가 있으면 이 prop을 넣어주지 않아도 됩니다.
-   */
-  webUrlBase?: string
   transitionModalHash?: string
   loginCTAModalHash?: string
   isAndroid?: boolean
@@ -113,8 +105,6 @@ export type HistoryProviderProps = PropsWithChildren<{
 }>
 
 export function HistoryProvider({
-  appUrlScheme: appUrlSchemeFromProps,
-  webUrlBase: webUrlBaseFromProps,
   transitionModalHash = '',
   loginCTAModalHash = '',
   isAndroid = false,
@@ -122,10 +112,7 @@ export function HistoryProvider({
   initialHashStrategy = HashStrategy.NONE,
   children,
 }: HistoryProviderProps) {
-  const {
-    appUrlScheme: appUrlSchemeFromContext,
-    webUrlBase: webUrlBaseFromContext,
-  } = useEnv()
+  const { appUrlScheme, webUrlBase } = useEnv()
   const sessionAvailable = useSessionAvailability()
 
   const [uriHash, setUriHash] = useState<URIHash>(() => {
@@ -140,43 +127,6 @@ export function HistoryProvider({
       ? [{ hash: getInitialHash(), useRouter: isAndroid }]
       : [],
   )
-
-  const appUrlScheme = useMemo(() => {
-    if (appUrlSchemeFromContext) {
-      return appUrlSchemeFromContext
-    }
-    if (typeof appUrlSchemeFromProps === 'string') {
-      // TODO: 개발용 logger 만들기
-      if (process.env.NODE_ENV === 'development') {
-        // eslint-disable-next-line no-console
-        console.warn(
-          'appUrlScheme prop은 deprecate되었습니다.\n다음 메이저 버전부터 env context를 사용해야 합니다.\nhttps://github.com/titicacadev/triple-frontend/blob/ab1648a7cdb684ee2752eb5b80eed02940106964/packages/react-contexts/src/env-context/README.md#%EB%A7%88%EC%9D%B4%EA%B7%B8%EB%A0%88%EC%9D%B4%EC%85%98-%ED%95%98%EB%8A%94-%EB%B2%95',
-        )
-      }
-
-      return appUrlSchemeFromProps
-    }
-
-    throw new Error('appUrlScheme을 구할 수 없습니다.')
-  }, [appUrlSchemeFromContext, appUrlSchemeFromProps])
-
-  const webUrlBase = useMemo(() => {
-    if (webUrlBaseFromContext) {
-      return webUrlBaseFromContext
-    }
-    if (typeof webUrlBaseFromProps === 'string') {
-      // TODO: 개발용 logger 만들기
-      if (process.env.NODE_ENV === 'development') {
-        // eslint-disable-next-line no-console
-        console.warn(
-          'webUrlBase prop은 deprecate되었습니다.\n다음 메이저 버전부터 env context를 사용해야 합니다.\nhttps://github.com/titicacadev/triple-frontend/blob/ab1648a7cdb684ee2752eb5b80eed02940106964/packages/react-contexts/src/env-context/README.md#%EB%A7%88%EC%9D%B4%EA%B7%B8%EB%A0%88%EC%9D%B4%EC%85%98-%ED%95%98%EB%8A%94-%EB%B2%95',
-        )
-      }
-
-      return webUrlBaseFromProps
-    }
-    throw new Error('webUrlBase를 구할 수 없습니다.')
-  }, [webUrlBaseFromContext, webUrlBaseFromProps])
 
   useEffect(() => {
     const onHashChange = (url: string) => {
