fa63b5d31029d86d2676b048f436c9ce43c76ade
finn <finn@triple-corp.com>
2021-12-19 17:35:08 +0900

    delete: CSFooter deprecated props (appUrlScheme)

--- packages/footer/src/cs-footer.tsx
+++ packages/footer/src/cs-footer.tsx
@@ -17,7 +17,6 @@ export default function CSFooter({
   csTime,
   csMessage,
   data,
-  appUrlScheme: appUrlSchemeFromProps,
   showCSButton = true,
   onFAQButtonClick = () => {},
   onCSButtonClick = () => {},
@@ -28,35 +27,12 @@ export default function CSFooter({
   csTime: string
   csMessage?: string
   data?: { [key: string]: string | number | boolean | undefined }
-  /**
-   * @deprecated EnvProvider가 있으면 이 prop을 넣어주지 않아도 됩니다.
-   */
-  appUrlScheme?: string
   showCSButton?: boolean
   onFAQButtonClick?: () => void
   onCSButtonClick?: () => void
 }) {
-  const { appUrlScheme: appUrlSchemeFromContext } = useEnv()
+  const { appUrlScheme } = useEnv()
   const { navigate } = useHistoryFunctions()
-
-  const appUrlScheme = React.useMemo(() => {
-    if (appUrlSchemeFromContext) {
-      return appUrlSchemeFromContext
-    }
-    if (typeof appUrlSchemeFromProps === 'string') {
-      // TODO: 개발용 logger 만들기
-      if (process.env.NODE_ENV === 'development') {
-        // eslint-disable-next-line no-console
-        console.warn(
-          'appUrlScheme prop은 deprecate되었습니다.\n다음 메이저 버전부터 env context를 사용해야 합니다.\nhttps://github.com/titicacadev/triple-frontend/blob/ab1648a7cdb684ee2752eb5b80eed02940106964/packages/react-contexts/src/env-context/README.md#%EB%A7%88%EC%9D%B4%EA%B7%B8%EB%A0%88%EC%9D%B4%EC%85%98-%ED%95%98%EB%8A%94-%EB%B2%95',
-        )
-      }
-
-      return appUrlSchemeFromProps
-    }
-
-    throw new Error('appUrlScheme을 구할 수 없습니다.')
-  }, [appUrlSchemeFromContext, appUrlSchemeFromProps])
 
   const movetoFAQ = () => {
     onFAQButtonClick()
