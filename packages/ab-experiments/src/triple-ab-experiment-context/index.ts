227a406eedd5f7c1f2fd16121f733254efcb3f7d
finn <finn@triple-corp.com>
2021-12-20 10:34:10 +0900

    fix: interface naming (ab-experiment => triple-ab-experiment)

---
+++ packages/ab-experiments/src/triple-ab-experiment-context/index.ts
@@ -0,0 +1,8 @@
+export { getTripleABExperiment, TripleABExperimentMeta } from './service'
+export {
+  TripleABExperimentProvider,
+  useTripleABExperimentVariant,
+  useTripleABExperimentConversionTracker,
+  useTripleABExperimentImpressionTracker,
+  useTripleABExperimentContext,
+} from './context'
