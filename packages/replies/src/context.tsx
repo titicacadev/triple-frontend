c4c58a180742b493870012529adf162113965c1d
finn <finn@triple-corp.com>
2021-12-20 14:31:57 +0900

    fix: typos

--- packages/replies/src/context.tsx
+++ packages/replies/src/context.tsx
@@ -125,7 +125,7 @@ export function useRepliesContext() {
   const context = useContext(RepliesContext)
 
   if (context === undefined) {
-    throw new Error('RepliesContext의 Provider가 없습ㄴ디ㅏ.')
+    throw new Error('RepliesContext의 Provider가 없습니다.')
   }
 
   return context
