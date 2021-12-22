b1a179e89051d07787e89ca934b719c7438c358e
woody <woody@triple-corp.com>
2021-12-22 10:28:04 +0900

    🔥 jest rootDir 옵션 제거

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,11 @@ const { compilerOptions } = require('./tsconfig.test')
 
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  rootDir: './packages',
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['lib', 'node_modules'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>',
+    prefix: '<rootDir>/packages',
   }),
   globals: {
     'ts-jest': {
