f11af435fe4da0cb469ed2e96571471bd3fcf466
woody <woody@triple-corp.com>
2021-12-22 09:23:55 +0900

    🔧 ts-jest 설정

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,19 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+
+const { compilerOptions } = require('./tsconfig.test')
+
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  rootDir: './packages',
+  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['lib', 'node_modules'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
+    },
+  },
 }
