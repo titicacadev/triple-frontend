6383799057e4d7425495706bd2f35e6fad60d982
woody <woody@triple-corp.com>
2021-12-23 16:39:23 +0900

    🚨 safe-parse-json.test.ts 파일 린트 오류 해결

--- packages/fetcher/src/safe-parse-json.test.ts
+++ packages/fetcher/src/safe-parse-json.test.ts
@@ -5,10 +5,11 @@ import safeParseJSON from './safe-parse-json'
 it('JSON 파싱 에러를 조용히 넘깁니다.', async () => {
   const response = new Response('', {
     headers: {
-      'Content-Type': 'application/json',
+      'content-type': 'application/json',
     },
   })
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const json = await safeParseJSON(response as any)
   expect(json).toBe(undefined)
 })
