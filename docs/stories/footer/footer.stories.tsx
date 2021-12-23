53348945bd89d94908f45d428f240599021940be
Gi Sung Park <lucian@triple-corp.com>
2021-12-23 21:23:14 +0900

    fix: 스토리북에서 CSFooter 제거

--- docs/stories/footer/footer.stories.tsx
+++ docs/stories/footer/footer.stories.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import DefaultFooter, { LogoFooter, CSFooter } from '@titicaca/footer'
-import { select, boolean } from '@storybook/addon-knobs'
-import { action } from '@storybook/addon-actions'
+import DefaultFooter, { LogoFooter } from '@titicaca/footer'
 
 import { sessionContextProviderDecorator } from '../../decorators'
 
@@ -15,23 +13,6 @@ export function BaseFooter() {
 }
 
 BaseFooter.storyName = '기본 푸터'
-
-export function BaseCSFooter() {
-  return (
-    <CSFooter
-      service={select('서비스', ['AIR', 'TNA', 'HOTEL'], 'AIR')}
-      csTime="오전 9시 - 오후 6시 (한국시간 기준, 연중무휴)"
-      csMessage={
-        '현지사용 긴급문의 카카오톡 @트리플서비스\n(오전 9시 - 오후 10시)'
-      }
-      showCSButton={boolean('1:1 문의 버튼 표시 여부', true)}
-      onFAQButtonClick={action('자주묻는 질문 버튼 클릭')}
-      onCSButtonClick={action('1:1 문의 버튼 클릭')}
-    />
-  )
-}
-
-BaseCSFooter.storyName = 'CS 푸터'
 
 export function BaseLogoFooter() {
   return <LogoFooter />
