94bffcf69374b27a2d023ff5a1ff11a6249c4c48
woody <woody@triple-corp.com>
2021-12-28 10:08:09 +0900

    🧪 add-web-url-base 테스트에 href가 "/"인 경우 추가
    href가 "/"이면 그냥 base URL을 반환해야합니다.

--- packages/router/src/common/add-web-url-base.test.ts
+++ packages/router/src/common/add-web-url-base.test.ts
@@ -18,3 +18,12 @@ test('useEnv에서 webUrlBase를 가져와서 주어진 href에 붙입니다.', 
 
   expect(addWebUrlBaseToHref(path)).toEqual(`${MOCK_URL_BASE}${path}`)
 })
+
+test('주어진 href가 /이면 그냥 baseURL을 반환합니다.', () => {
+  const { result } = renderHook(useWebUrlBaseAdder)
+
+  const addWebUrlBaseToHref = result.current
+  const path = '/'
+
+  expect(addWebUrlBaseToHref(path)).toEqual(MOCK_URL_BASE)
+})
