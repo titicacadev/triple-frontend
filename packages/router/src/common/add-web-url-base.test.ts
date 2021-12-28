398042ca2c7c8dae044b1e8198e33f0517f52cce
woody <woody@triple-corp.com>
2021-12-28 10:05:36 +0900

    ✅ add-web-url-base 테스트 코드 추가

---
+++ packages/router/src/common/add-web-url-base.test.ts
@@ -0,0 +1,20 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { useEnv } from '@titicaca/react-contexts'
+
+import { useWebUrlBaseAdder } from './add-web-url-base'
+
+const MOCK_URL_BASE = 'https://triple.guide'
+
+jest.mock('@titicaca/react-contexts')
+;((useEnv as unknown) as jest.MockedFunction<
+  () => Pick<ReturnType<typeof useEnv>, 'webUrlBase'>
+>).mockImplementation(() => ({ webUrlBase: MOCK_URL_BASE }))
+
+test('useEnv에서 webUrlBase를 가져와서 주어진 href에 붙입니다.', () => {
+  const { result } = renderHook(useWebUrlBaseAdder)
+
+  const addWebUrlBaseToHref = result.current
+  const path = '/base'
+
+  expect(addWebUrlBaseToHref(path)).toEqual(`${MOCK_URL_BASE}${path}`)
+})
