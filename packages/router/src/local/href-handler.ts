94dd39e12f1c955b5d30b59e9975521644f27e2d
woody <woody@triple-corp.com>
2022-01-03 11:42:23 +0900

    ♻️ href-handler 훅을 별도 모듈로 분리
    라우터 훅에서 사용할 수 있도록 별도 모듈로 분리합니다.

---
+++ packages/router/src/local/href-handler.ts
@@ -0,0 +1,103 @@
+import { useUserAgentContext } from '@titicaca/react-contexts'
+import { useRouter } from 'next/router'
+
+import { useWebUrlBaseAdder } from '../common/add-web-url-base'
+import { useAppBridge } from '../common/app-bridge'
+import {
+  AppSpecificLinkProps,
+  useTripleAppRoutingOptionsAdder,
+} from '../common/app-specific-link-options'
+import { HrefProps } from '../common/types'
+import { TargetProps } from '../common/target'
+
+import { useBasePathAdder } from './base-path'
+
+export interface NextjsRoutingOptions {
+  /**
+   * 현재 창을 history에 남기지 않고 이동합니다. target="current"일 때만 작동합니다.
+   */
+  replace?: boolean
+  /**
+   * 현재창에서 라우팅할 때 페이지 스크롤을 상단으로 올릴지 여부를 결정합니다.
+   * 기본 값 true
+   */
+  scroll?: boolean
+}
+
+export function useLocalHrefHandler() {
+  const router = useRouter()
+  const { isPublic } = useUserAgentContext()
+  const { openInlink, openOutlink } = useAppBridge()
+  const addTripleAppRoutingOptions = useTripleAppRoutingOptionsAdder()
+  const addWebUrlBase = useWebUrlBaseAdder()
+  const addBasePath = useBasePathAdder()
+
+  const handleNextjsRouting = async (
+    href: string,
+    { replace, scroll = true }: NextjsRoutingOptions,
+  ): Promise<void> => {
+    const success = await router[replace ? 'replace' : 'push'](
+      href,
+      undefined,
+      {
+        scroll,
+      },
+    )
+    if (success && scroll) {
+      window.scrollTo(0, 0)
+    }
+  }
+
+  const handleHrefLocally = async ({
+    href,
+    target,
+    lnbTarget,
+    noNavbar,
+    shouldPresent,
+    swipeToClose,
+    replace,
+    scroll,
+    isKeyPressing,
+    stopDefaultHandler,
+  }: HrefProps &
+    TargetProps &
+    NextjsRoutingOptions &
+    AppSpecificLinkProps & {
+      isKeyPressing: boolean
+      stopDefaultHandler: () => void
+    }) => {
+    if (target === 'current' && isKeyPressing === false) {
+      stopDefaultHandler()
+
+      await handleNextjsRouting(href, { replace, scroll })
+
+      return
+    }
+
+    const finalHref = addTripleAppRoutingOptions({
+      href: addBasePath(href),
+      lnbTarget,
+      noNavbar,
+      shouldPresent,
+      swipeToClose,
+    })
+
+    if (target === 'new' && isPublic === false) {
+      stopDefaultHandler()
+
+      openInlink(finalHref)
+
+      return
+    }
+
+    if (target === 'browser' && isPublic === false) {
+      stopDefaultHandler()
+
+      openOutlink(addWebUrlBase(finalHref), {
+        target: 'browser',
+      })
+    }
+  }
+
+  return handleHrefLocally
+}
