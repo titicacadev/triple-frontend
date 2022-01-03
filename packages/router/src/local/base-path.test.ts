514f38996ef7b0448b0d4202bc613edae54df396
woody <woody@triple-corp.com>
2022-01-03 15:51:26 +0900

    ✅ base-path 모듈 테스트 코드 추가

---
+++ packages/router/src/local/base-path.test.ts
@@ -0,0 +1,41 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { useRouter } from 'next/router'
+
+import { useBasePathAdder } from './base-path'
+
+jest.mock('next/router')
+
+test('주어진 href에 basePath를 더합니다.', () => {
+  const { basePath } = mockNextRouter()
+  const href =
+    '/5b700a4e-4b0f-4266-81db-eb42f834bdd9?regionId=71476976-cf9a-4ae8-a60f-76e6fb26900d'
+
+  const {
+    result: { current: addBasePath },
+  } = renderHook(useBasePathAdder)
+
+  expect(addBasePath(href)).toEqual(`${basePath}${href}`)
+})
+
+test('주어진 href가 "/"이면 그냥 basePath를 반환합니다.', () => {
+  const { basePath } = mockNextRouter()
+  const href = '/'
+
+  const {
+    result: { current: addBasePath },
+  } = renderHook(useBasePathAdder)
+
+  expect(addBasePath(href)).toEqual(basePath)
+})
+
+function mockNextRouter() {
+  const basePath = '/test-env'
+
+  ;(
+    useRouter as unknown as jest.MockedFunction<
+      () => Pick<ReturnType<typeof useRouter>, 'basePath'>
+    >
+  ).mockImplementation(() => ({ basePath }))
+
+  return { basePath }
+}
