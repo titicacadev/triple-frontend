05e821fc3375328a07fa2eaddefa85a91fbbc340
woody <woody@triple-corp.com>
2022-01-05 10:33:17 +0900

    💥 CSFooter 컴포넌트 및 관련 코드 제거

--- packages/footer/src/index.ts
+++ packages/footer/src/index.ts
@@ -1,6 +1,5 @@
 import DefaultFooter from './default-footer'
 
-export { default as CSFooter } from './cs-footer'
 export { default as LogoFooter } from './logo-footer'
 
 export default DefaultFooter
