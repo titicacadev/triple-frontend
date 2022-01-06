cb30766461f6c5c777a1dbfd766fa22e0a4c0e6a
Gi Sung Park <lucian@triple-corp.com>
2022-01-07 02:58:38 +0900

    fix: 테스트 코드 수정

--- packages/scroll-spy/src/index.test.tsx
+++ packages/scroll-spy/src/index.test.tsx
@@ -6,44 +6,41 @@ import renderer from 'react-test-renderer'
 
 jest.mock('@titicaca/intersection-observer')
 
-test('activeId가 일치합니다.', () => {
-  const IntersectionObserver = prepareTest()
+describe('ScrollSpy', () => {
+  beforeEach(() => {
+    ;(
+      IntersectionObserver as unknown as jest.MockedFunction<
+        typeof IntersectionObserver
+      >
+    ).mockImplementation(({ onChange, children }) => {
+      useEffect(() => {
+        onChange({} as IntersectionObserverEntry, jest.fn())
+      }, [onChange])
 
-  /* prepareTest의 IntersectionObserver에서 div를 래핑하기에 a태그를 사용 */
-  const tree = renderer.create(
-    <IntersectionObserver onChange={jest.fn()}>
-      <a id="target-element">테스트 태그</a>
-    </IntersectionObserver>,
-  ).root
-
-  expect(tree.findByProps({ id: 'target-element' })).toStrictEqual(
-    tree.findByType('a'),
-  )
-})
-
-test('intersectionObserver 동작을 체크합니다.', () => {
-  const IntersectionObserver = prepareTest()
-
-  const tree = renderer.create(
-    <IntersectionObserver onChange={jest.fn()}>
-      <div id="target-element" />
-    </IntersectionObserver>,
-  ).root
-
-  expect(tree.findByProps({ id: 'target-element' }))
-})
-
-function prepareTest() {
-  ;(
-    IntersectionObserver as unknown as jest.MockedFunction<
-      typeof IntersectionObserver
-    >
-  ).mockImplementation(({ onChange, children }) => {
-    useEffect(() => {
-      onChange({} as IntersectionObserverEntry, jest.fn())
-    }, [onChange])
-
-    return <div>{children}</div>
+      return <div>{children}</div>
+    })
   })
-  return IntersectionObserver
-}
+
+  test('activeId가 일치합니다.', () => {
+    /* prepareTest의 IntersectionObserver에서 div를 래핑하기에 a태그를 사용 */
+    const tree = renderer.create(
+      <IntersectionObserver onChange={jest.fn()}>
+        <a id="target-element">테스트 태그</a>
+      </IntersectionObserver>,
+    ).root
+
+    expect(tree.findByProps({ id: 'target-element' })).toStrictEqual(
+      tree.findByType('a'),
+    )
+  })
+
+  test('intersectionObserver 동작을 체크합니다.', () => {
+    const tree = renderer.create(
+      <IntersectionObserver onChange={jest.fn()}>
+        <div id="target-element" />
+      </IntersectionObserver>,
+    ).root
+
+    expect(tree.findByProps({ id: 'target-element' }))
+  })
+})
