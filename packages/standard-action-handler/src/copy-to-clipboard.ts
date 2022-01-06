ae938529a4bf0ecea297befdaed71ef25d1f55b4
finn <finn@triple-corp.com>
2022-01-06 15:53:30 +0900

    fix: 잘못된 path 변경

--- packages/standard-action-handler/src/copy-to-clipboard.ts
+++ packages/standard-action-handler/src/copy-to-clipboard.ts
@@ -8,7 +8,7 @@ import {
 const ALERT_MESSAGE = '클립보드에 복사되었습니다.'
 
 export default async function copyToClipboard({ path, query }: UrlElements) {
-  if (path === '/web-action/share' && query) {
+  if (path === '/web-action/copy-to-clipboard' && query) {
     const { text } = qs.parse(query || '')
 
     if (text) {
