8627d7b4dfa4931c609af594867d8358f8ef2f02
woody <woody@triple-corp.com>
2022-01-06 17:24:02 +0900

    📝 user-verification 스토리 수정

--- docs/stories/user-verification/verification-request.stories.tsx
+++ docs/stories/user-verification/verification-request.stories.tsx
@@ -1,9 +1,20 @@
 import { VerificationRequest } from '@titicaca/user-verification'
 import { Meta, StoryObj } from '@storybook/react'
 
+import {
+  envProviderDecorator,
+  sessionContextProviderDecorator,
+  userAgentProviderDecorator,
+} from '../../decorators'
+
 export default {
   title: 'user-verification / VerificationRequest',
   component: VerificationRequest,
+  decorators: [
+    userAgentProviderDecorator,
+    envProviderDecorator,
+    sessionContextProviderDecorator,
+  ],
 } as Meta
 
 export const ExampleVerificationRequest: StoryObj = {
