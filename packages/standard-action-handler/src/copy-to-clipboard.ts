a3806761fbb013b53698694301a90184eb7f11ca
finn <finn@triple-corp.com>
2022-01-07 18:21:30 +0900

    fix: factories 함수 사용 (createClipboardCopier / createShareUrl)

--- packages/standard-action-handler/src/copy-to-clipboard.ts
+++ packages/standard-action-handler/src/copy-to-clipboard.ts
@@ -1,51 +1,22 @@
 import qs from 'qs'
 import { UrlElements } from '@titicaca/view-utilities'
-import {
-  hasAccessibleTripleNativeClients,
-  showToast,
-} from '@titicaca/triple-web-to-native-interfaces'
 
-import { copyToClipboard } from './utils'
+import { createClipboardCopier } from './factories'
 
 const ALERT_MESSAGE = '클립보드에 복사되었습니다.'
 
-export default async function copyTextToClipboard({
-  path,
-  query,
-}: UrlElements) {
+export default async function copyToClipboard({ path, query }: UrlElements) {
   if (path === '/web-action/copy-to-clipboard' && query) {
     const { text } = qs.parse(query || '')
 
     if (text) {
-      const copyText = createCopyText()
+      const textClipboardCopier = createClipboardCopier()
 
-      copyText({ text: text as string, message: ALERT_MESSAGE })
+      textClipboardCopier({ text: text as string, message: ALERT_MESSAGE })
     }
 
     return true
   }
 
   return false
-}
-
-function createCopyText() {
-  if (!hasAccessibleTripleNativeClients()) {
-    return copyToClipboard
-  } else {
-    return copyTextNativeInterface
-  }
-}
-
-function copyTextNativeInterface({
-  text,
-  message,
-}: {
-  text: string
-  message: string
-}) {
-  window.location.href = `${
-    process.env.NEXT_PUBLIC_APP_URL_SCHEME
-  }:///action/copy_to_clipboard?text=${encodeURIComponent(text)}`
-
-  showToast(message)
 }
