82a041d47de82897b7c5e705b458cf7c027c9e85
Inbeom Hwang <hwanginbeom@gmail.com>
2022-01-10 10:42:34 +0900

    Add specs for useClientActions

---
+++ packages/react-client-interfaces/src/use-client-actions.spec.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import type { PropsWithChildren } from 'react'
+import { renderHook } from '@testing-library/react-hooks'
+
+import { ClientContextProvider } from './client-context'
+import { useClientActions } from './use-client-actions'
+
+it('should return the function as-is if version requirement is not listed', () => {
+  const wrapper = ({ children }: PropsWithChildren<Record<string, never>>) => (
+    <ClientContextProvider appName="Triple-iOS" appVersion="5.11.0">
+      {children}
+    </ClientContextProvider>
+  )
+
+  const { result } = renderHook(() => useClientActions(), { wrapper })
+
+  expect(result.current.showToast).not.toBe(null)
+})
+
+it('should not return function if it does not match version requirement', () => {
+  const wrapper = ({ children }: PropsWithChildren<Record<string, never>>) => (
+    <ClientContextProvider appName="Triple-iOS" appVersion="5.10.0">
+      {children}
+    </ClientContextProvider>
+  )
+
+  const { result } = renderHook(() => useClientActions(), { wrapper })
+
+  expect(result.current.subscribeTripUpdateEvent).toBeFalsy()
+})
+
+it('should return function if it matches version requirement', () => {
+  const wrapper = ({ children }: PropsWithChildren<Record<string, never>>) => (
+    <ClientContextProvider appName="Triple-iOS" appVersion="5.12.0">
+      {children}
+    </ClientContextProvider>
+  )
+
+  const { result } = renderHook(() => useClientActions(), { wrapper })
+
+  expect(result.current.subscribeTripUpdateEvent).not.toBeFalsy()
+})
