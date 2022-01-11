a637307314cdb96e37badae34ca455b783ac9835
woody <woody@triple-corp.com>
2022-01-11 10:15:06 +0900

    🎨 link 모듈을 common 모듈과 통합
    독자적인 인터페이스를 노출하는 모듈이 아니고,
    링크 컴포넌트에서 사용하는 코드이기 때문에 common으로 통합 관리합니다.

--- packages/router/src/common/types.ts
+++ packages/router/src/common/types.ts
@@ -1,3 +1,23 @@
+import { AppSpecificLinkProps } from './app-specific-link-options'
+import { AllowSourceProps } from './disabled-link-notifier'
+import { TargetProps } from './target'
+import { RelListProps } from './use-rel'
+
 export interface HrefProps {
   href: string
 }
+
+/**
+ * LocalLink, ExternalLink 공통으로 쓰는 props
+ */
+export type LinkCommonProps = {
+  /**
+   * anchor를 클릭했을 때 작동하는 핸들러입니다.
+   * `allowSource` 조건에 의해 사용할 수 없는 링크이면 클릭해도 작동하지 않습니다.
+   */
+  onClick?: () => void
+} & HrefProps &
+  TargetProps &
+  RelListProps &
+  AllowSourceProps &
+  AppSpecificLinkProps
