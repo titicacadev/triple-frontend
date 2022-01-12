3b6beeb72d08805860962afe8e2581eebca65f7d
Gi Sung Park <lucian@triple-corp.com>
2022-01-12 15:02:27 +0900

    fix: sticky map container

--- packages/map/src/sticky-map-container.tsx
+++ packages/map/src/sticky-map-container.tsx
@@ -5,7 +5,10 @@ import {
   layeringMixin,
 } from '@titicaca/core-elements'
 
+export const DEFAULT_MAP_HEIGHT = 180
+
 export const StickyMapContainer = styled(Container)<LayeringMixinProps>`
   ${layeringMixin(3)}
+  background-color: #ffffff;
   position: sticky;
 `
