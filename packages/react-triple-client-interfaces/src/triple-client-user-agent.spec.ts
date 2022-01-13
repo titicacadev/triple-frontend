359fa39dcb1380a2a846b849b5b6f4c9878297b7
Inbeom Hwang <hwanginbeom@gmail.com>
2022-01-13 19:40:26 +0900

    Rename all modules

--- packages/react-triple-client-interfaces/src/triple-client-user-agent.spec.ts
+++ packages/react-triple-client-interfaces/src/triple-client-user-agent.spec.ts
@@ -1,8 +1,8 @@
-import { parseNativeClientUserAgent } from './client-user-agent'
+import { parseTripleClientUserAgent } from './triple-client-user-agent'
 
 it('should parse Chrome on Windows as a non-app', () => {
   expect(
-    parseNativeClientUserAgent(
+    parseTripleClientUserAgent(
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
     ),
   ).toBeNull()
@@ -10,7 +10,7 @@ it('should parse Chrome on Windows as a non-app', () => {
 
 it('should parse Triple Android debug client as an app', () => {
   expect(
-    parseNativeClientUserAgent(
+    parseTripleClientUserAgent(
       'Mozilla/5.0 (Linux; Android 11; SM-G975N Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.86 Mobile Safari/537.36 Triple-Android/5.3.0-debug',
     ),
   ).toEqual({
@@ -21,7 +21,7 @@ it('should parse Triple Android debug client as an app', () => {
 
 it('should parse Triple Android client as an app', () => {
   expect(
-    parseNativeClientUserAgent(
+    parseTripleClientUserAgent(
       'Mozilla/5.0 (Linux; Android 10; SM-N960N Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.92 Mobile Safari/537.36 Triple-Android/5.10.0',
     ),
   ).toEqual({
@@ -32,7 +32,7 @@ it('should parse Triple Android client as an app', () => {
 
 it('should parse Triple iOS client as an app', () => {
   expect(
-    parseNativeClientUserAgent(
+    parseTripleClientUserAgent(
       'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/5.10.0',
     ),
   ).toEqual({
--- packages/react-triple-client-interfaces/src/use-triple-client-actions.spec.tsx
+++ packages/react-triple-client-interfaces/src/use-triple-client-actions.spec.tsx
@@ -2,51 +2,51 @@ import React from 'react'
 import type { PropsWithChildren } from 'react'
 import { renderHook } from '@testing-library/react-hooks'
 
-import { ClientContextProvider } from './client-context'
-import { useClientActions } from './use-client-actions'
+import { TripleClientMetadataProvider } from './triple-client-metadata-context'
+import { useTripleClientActions } from './use-triple-client-actions'
 
 it('should return the function as-is if version requirement is not listed', () => {
   const wrapper = ({ children }: PropsWithChildren<Record<string, never>>) => (
-    <ClientContextProvider appName="Triple-iOS" appVersion="5.11.0">
+    <TripleClientMetadataProvider appName="Triple-iOS" appVersion="5.11.0">
       {children}
-    </ClientContextProvider>
+    </TripleClientMetadataProvider>
   )
 
-  const { result } = renderHook(() => useClientActions(), { wrapper })
+  const { result } = renderHook(() => useTripleClientActions(), { wrapper })
 
   expect(result.current.showToast).not.toBe(null)
 })
 
 it('should not return the function if it does not match version requirement', () => {
   const wrapper = ({ children }: PropsWithChildren<Record<string, never>>) => (
-    <ClientContextProvider appName="Triple-iOS" appVersion="5.10.0">
+    <TripleClientMetadataProvider appName="Triple-iOS" appVersion="5.10.0">
       {children}
-    </ClientContextProvider>
+    </TripleClientMetadataProvider>
   )
 
-  const { result } = renderHook(() => useClientActions(), { wrapper })
+  const { result } = renderHook(() => useTripleClientActions(), { wrapper })
 
   expect(result.current.subscribeTripUpdateEvent).toBeFalsy()
 })
 
 it('should return the function if it matches version requirement', () => {
   const wrapper = ({ children }: PropsWithChildren<Record<string, never>>) => (
-    <ClientContextProvider appName="Triple-iOS" appVersion="5.12.0">
+    <TripleClientMetadataProvider appName="Triple-iOS" appVersion="5.12.0">
       {children}
-    </ClientContextProvider>
+    </TripleClientMetadataProvider>
   )
 
-  const { result } = renderHook(() => useClientActions(), { wrapper })
+  const { result } = renderHook(() => useTripleClientActions(), { wrapper })
 
   expect(result.current.subscribeTripUpdateEvent).not.toBeFalsy()
 })
 
 it('should not return the function if the page is not on triple client', () => {
   const wrapper = ({ children }: PropsWithChildren<Record<string, never>>) => (
-    <ClientContextProvider>{children}</ClientContextProvider>
+    <TripleClientMetadataProvider>{children}</TripleClientMetadataProvider>
   )
 
-  const { result } = renderHook(() => useClientActions(), { wrapper })
+  const { result } = renderHook(() => useTripleClientActions(), { wrapper })
 
   expect(result.current.showToast).toBeFalsy()
 })
