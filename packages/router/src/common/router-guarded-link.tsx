6e2d14101350b260d91c285f993af2a6759bc0d6
giwan <giwan.dev@gmail.com>
2022-01-24 15:57:34 +0900

    💥 RouterGuardedLink가 HTML 태그를 렌더링하도록 변경
    비활성화 링크일 땐 버튼을, 활성화 링크일 땐 앵커를 렌더링합니다.

--- packages/router/src/common/router-guarded-link.tsx
+++ packages/router/src/common/router-guarded-link.tsx
@@ -1,10 +1,4 @@
-import React, {
-  AnchorHTMLAttributes,
-  Children,
-  cloneElement,
-  MouseEventHandler,
-  PropsWithChildren,
-} from 'react'
+import React, { AnchorHTMLAttributes, PropsWithChildren } from 'react'
 
 import {
   AllowSourceProps,
@@ -17,11 +11,10 @@ import { RelListProps, useRel } from './use-rel'
  * 조건부 라우팅 검사 로직을 자식 a 엘리먼트에 주입하는 컴포넌트
  */
 export function RouterGuardedLink({
-  href,
   relList = [],
   allowSource = 'all',
-  onClick,
   children,
+  className,
   ...restProps
 }: PropsWithChildren<
   Partial<Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'rel'>> &
@@ -33,48 +26,17 @@ export function RouterGuardedLink({
 
   const disabledLinkNotifier = createDisabledLinkNotifier({ allowSource })
 
-  const isDisabledRoute = disabledLinkNotifier !== undefined
-
-  const disabledLinkClickHandler: MouseEventHandler<HTMLAnchorElement> = (
-    e,
-  ) => {
-    if (disabledLinkNotifier === undefined) {
-      return
-    }
-
-    e.preventDefault()
-
-    disabledLinkNotifier()
+  if (disabledLinkNotifier !== undefined) {
+    return (
+      <button className={className} onClick={() => disabledLinkNotifier()}>
+        {children}
+      </button>
+    )
   }
 
-  const anchorProps: Partial<AnchorHTMLAttributes<HTMLAnchorElement>> = {
-    ...restProps,
-    href: isDisabledRoute ? undefined : href,
-    rel,
-    onClick: isDisabledRoute ? disabledLinkClickHandler : onClick,
-  }
-
-  const child = Children.only(children)
-
-  if (!child || typeof child !== 'object' || !('type' in child)) {
-    return <>{children}</>
-  }
-
-  warnDuplicateAttributes(child.props, anchorProps)
-
-  return cloneElement(child, anchorProps)
-}
-
-function warnDuplicateAttributes(
-  originalProps: Record<string, unknown>,
-  overridingProps: Record<string, unknown>,
-) {
-  const originalPropKeys = Object.keys(originalProps)
-
-  originalPropKeys.forEach((key) => {
-    if (overridingProps[key]) {
-      // eslint-disable-next-line no-console
-      console.warn(`Link 컴포넌트의 자식 태그의 속성이 덮어쓰였습니다. ${key}`)
-    }
-  })
+  return (
+    <a className={className} rel={rel} {...restProps}>
+      {children}
+    </a>
+  )
 }
