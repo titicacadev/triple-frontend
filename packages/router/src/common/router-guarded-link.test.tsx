60daa01ad280c1c9f06cec198ad78752f585fc11
giwan <giwan.dev@gmail.com>
2022-01-24 15:58:28 +0900

    ✅ router-guarded-link 테스트 재작성
    HTML 태그를 렌더링하는 스펙에 맞게 테스트를 재작성합니다.

--- packages/router/src/common/router-guarded-link.test.tsx
+++ packages/router/src/common/router-guarded-link.test.tsx
@@ -3,50 +3,120 @@ import { fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 import { RouterGuardedLink } from './router-guarded-link'
+import { useDisabledLinkNotifierCreator } from './disabled-link-notifier'
 
-jest.mock('../common/disabled-link-notifier', () => ({
-  ...jest.requireActual('../common/disabled-link-notifier'),
-  useDisabledLinkNotifierCreator: () => jest.fn(),
-}))
+jest.mock('../common/disabled-link-notifier')
 
-test('자식 태그의 href, onClick 속성은 덮어쓰입니다.', () => {
-  // eslint-disable-next-line no-console
-  console.warn = jest.fn()
-  const clickHandler = jest.fn()
-  const { getByRole } = render(
-    <RouterGuardedLink href="https://triple.guide">
-      <a href="https://triple.guide/hotels" onClick={clickHandler}>
-        테스트링크
-      </a>
-    </RouterGuardedLink>,
-  )
-  expect(getByRole('link')).toHaveAttribute('href', 'https://triple.guide')
-  fireEvent.click(getByRole('link'))
-  expect(clickHandler).not.toBeCalled()
-})
-
-test('자식 태그가 덮어쓰일 속성을 가지고 있으면 경고를 보냅니다.', () => {
-  /* eslint-disable no-console */
-  console.warn = jest.fn()
-
-  render(
-    <RouterGuardedLink
-      href="https://triple.guide"
-      className="asdf"
-      onClick={jest.fn()}
+afterEach(() => {
+  ;(
+    useDisabledLinkNotifierCreator as jest.MockedFunction<
+      typeof useDisabledLinkNotifierCreator
     >
-      <a
-        href="https://triple.guide/hotels"
-        onClick={jest.fn()}
-        className="test-link"
-      >
-        테스트링크
-      </a>
-    </RouterGuardedLink>,
-  )
-
-  expect(console.warn).toBeCalledWith(expect.stringContaining('href'))
-  expect(console.warn).toBeCalledWith(expect.stringContaining('onClick'))
-  expect(console.warn).toBeCalledWith(expect.stringContaining('className'))
-  /* eslint-enable no-console */
+  ).mockClear()
 })
+
+describe('라우팅할 수 없는 환경일 때', () => {
+  let notifier: (() => void) | undefined
+
+  beforeEach(() => {
+    notifier = mockDisabledLinkNotifierCreatorHook(true)
+  })
+
+  test('버튼을 렌더링합니다.', () => {
+    const { getByRole } = render(
+      <RouterGuardedLink href="">테스트링크</RouterGuardedLink>,
+    )
+
+    expect(getByRole('button')).toBeTruthy()
+  })
+
+  test('클릭하면 disabled link notifier를 호출합니다.', () => {
+    const { getByRole } = render(
+      <RouterGuardedLink href="">테스트링크</RouterGuardedLink>,
+    )
+
+    const button = getByRole('button')
+
+    fireEvent.click(button)
+
+    expect(notifier).toBeCalled()
+  })
+
+  test('className prop을 전달합니다.', () => {
+    const className = 'TEST_CLASS_NAME'
+    const { getByRole } = render(
+      <RouterGuardedLink href="" className={className}>
+        테스트링크
+      </RouterGuardedLink>,
+    )
+
+    const button = getByRole('button')
+
+    expect(button).toHaveAttribute('class', className)
+  })
+})
+
+describe('라우팅할 수 있는 환경일 때', () => {
+  beforeEach(() => {
+    mockDisabledLinkNotifierCreatorHook(false)
+  })
+
+  test('앵커 태그를 렌더링합니다.', () => {
+    const { getByRole } = render(
+      <RouterGuardedLink href="/foo">테스트링크</RouterGuardedLink>,
+    )
+
+    const link = getByRole('link')
+
+    expect(link).toBeTruthy()
+  })
+
+  test('className prop을 클래스로 전달합니다.', () => {
+    const className = 'TEST_CLASS_NAME'
+    const { getByRole } = render(
+      <RouterGuardedLink href="/foo" className={className}>
+        테스트링크
+      </RouterGuardedLink>,
+    )
+
+    const link = getByRole('link')
+
+    expect(link).toHaveAttribute('class', className)
+  })
+
+  test('relList prop을 rel로 전달합니다.', () => {
+    const relList: Parameters<typeof RouterGuardedLink>[0]['relList'] = [
+      'external',
+    ]
+
+    const { getByRole } = render(
+      <RouterGuardedLink href="/foo" relList={relList}>
+        테스트링크
+      </RouterGuardedLink>,
+    )
+
+    const link = getByRole('link')
+
+    expect(link).toHaveAttribute('rel', expect.stringContaining('external'))
+  })
+})
+
+function mockDisabledLinkNotifierCreatorHook(disabled: boolean) {
+  if (disabled) {
+    const notifier = jest.fn()
+
+    ;(
+      useDisabledLinkNotifierCreator as jest.MockedFunction<
+        typeof useDisabledLinkNotifierCreator
+      >
+    ).mockReturnValue(() => notifier)
+
+    return notifier
+  }
+
+  ;(
+    useDisabledLinkNotifierCreator as jest.MockedFunction<
+      typeof useDisabledLinkNotifierCreator
+    >
+  ).mockReturnValue(() => undefined)
+}
