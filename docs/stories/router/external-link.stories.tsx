9fa2f58fac0de1f474b300dd155868a50a690e39
giwan <giwan.dev@gmail.com>
2022-01-26 10:07:37 +0900

    ✅ ExternalLink 스토리북 작성
    기본 형태와 비활성화형태의 스토리를 작성합니다.

---
+++ docs/stories/router/external-link.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta } from '@storybook/react'
+import { ExternalLink } from '@titicaca/router'
+import React from 'react'
+
+import {
+  envProviderDecorator,
+  globalStyleDecorator,
+  sessionContextProviderDecorator,
+} from '../../decorators'
+
+export default {
+  component: (args) => (
+    <ExternalLink href="/foo" target="current" {...args}>
+      테스트링크
+    </ExternalLink>
+  ),
+  decorators: [
+    globalStyleDecorator,
+    envProviderDecorator,
+    sessionContextProviderDecorator,
+  ],
+} as Meta
+
+export const Primary = {}
+export const Disabled = {
+  ...Primary,
+  args: { allowSource: 'none' },
+}
