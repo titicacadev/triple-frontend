1213d95468db90a8d5cb71d16f9867e0934f7917
giwan <giwan.dev@gmail.com>
2022-01-26 10:01:58 +0900

    ✅ LocalLink 스토리북 작성
    기본 상태와 비활성화 상태일 때 스토리를 작성합니다.

---
+++ docs/stories/router/local-link.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta } from '@storybook/react'
+import { LocalLink } from '@titicaca/router'
+import React from 'react'
+
+import {
+  envProviderDecorator,
+  globalStyleDecorator,
+  sessionContextProviderDecorator,
+} from '../../decorators'
+
+export default {
+  component: (args) => (
+    <LocalLink href="/foo" target="current" {...args}>
+      테스트링크
+    </LocalLink>
+  ),
+  decorators: [
+    globalStyleDecorator,
+    envProviderDecorator,
+    sessionContextProviderDecorator,
+  ],
+} as Meta
+
+export const Primary = {}
+export const Disabled = {
+  ...Primary,
+  args: { allowSource: 'none' },
+}
