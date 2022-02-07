a4411ca528ca88bcd7ee8b0a44a4e315b7a500ab
finn <finn@triple-corp.com>
2022-02-07 14:42:52 +0900

    feat: Text Component

---
+++ packages/email-triple-document/src/elements/text.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Box from '../common/box'
+import FluidTable from '../common/fluid-table'
+
+export interface TextDocument {
+  type: 'text'
+  value: {
+    rawHTML: string
+  }
+}
+
+const HtmlContainer = styled.div`
+  p {
+    margin: 24px 0 0 0;
+    font-size: 16px;
+    line-height: 1.63;
+    font-weight: 500;
+    color: var(--color-gray900);
+
+    &:first-of-type {
+      margin-top: 0;
+    }
+  }
+
+  strong {
+    font-size: 16px;
+    line-height: 1.63;
+    font-weight: bold;
+    color: var(--color-gray);
+  }
+
+  && {
+    a {
+      font-size: 16px;
+      line-height: 1.63;
+      font-weight: bold;
+      color: #2987f0;
+      text-decoration: underline;
+    }
+  }
+`
+
+export default function TextView({
+  value: { rawHTML },
+}: {
+  value: TextDocument['value']
+}) {
+  return (
+    <FluidTable>
+      <tr>
+        <Box padding={{ top: 10, left: 30, right: 30 }}>
+          <HtmlContainer dangerouslySetInnerHTML={{ __html: rawHTML }} />
+        </Box>
+      </tr>
+    </FluidTable>
+  )
+}
