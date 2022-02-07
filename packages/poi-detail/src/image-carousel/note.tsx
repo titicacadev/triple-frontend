c21fb6acf26134aab0a19488b349a08258bfe866
DeokHyeon,Son <crew0813@gmail.com>
2022-02-08 00:50:52 +0900

    fix: Note 컨테이너가 섹션을 넘어 서는 것을 수정하고, number = 0 값으로 인해 연산이 제대로 수행 되지 않는 것을 수정합니다

--- packages/poi-detail/src/image-carousel/note.tsx
+++ packages/poi-detail/src/image-carousel/note.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-import { Text } from '@titicaca/core-elements'
+import { Container, Text } from '@titicaca/core-elements'
 
 const NoteContainer = styled.div<{
   warning?: boolean
@@ -12,6 +12,7 @@ const NoteContainer = styled.div<{
   right: 0;
   height: 40px;
   background-color: ${({ warning }) => (warning ? '#ff3636' : '#2987f0')};
+  z-index: 2;
 
   & > div {
     margin: 0 13px;
@@ -19,12 +20,12 @@ const NoteContainer = styled.div<{
   }
 
   ${({ bottomBorderRadius }) =>
-    bottomBorderRadius &&
-    css`
-      border-bottom-left-radius: ${bottomBorderRadius}px;
-      border-bottom-right-radius: ${bottomBorderRadius}px;
-    `}
-  z-index: 2;
+    bottomBorderRadius
+      ? css`
+          border-bottom-left-radius: ${bottomBorderRadius}px;
+          border-bottom-right-radius: ${bottomBorderRadius}px;
+        `
+      : null}
 `
 
 export function PermanentlyClosedNote({
@@ -33,11 +34,13 @@ export function PermanentlyClosedNote({
   bottomBorderRadius?: number
 }) {
   return (
-    <NoteContainer warning bottomBorderRadius={bottomBorderRadius}>
-      <Text bold size="small" color="white">
-        더이상 운영하지 않습니다.
-      </Text>
-    </NoteContainer>
+    <Container position="relative">
+      <NoteContainer warning bottomBorderRadius={bottomBorderRadius}>
+        <Text bold size="small" color="white">
+          더이상 운영하지 않습니다.
+        </Text>
+      </NoteContainer>
+    </Container>
   )
 }
 
@@ -55,18 +58,20 @@ export function BusinessHoursNote({
   onClick: () => void
 }) {
   return (
-    <NoteContainer
-      onClick={onClick}
-      warning={!currentBusinessHours}
-      bottomBorderRadius={bottomBorderRadius}
-    >
-      <Text bold size="small" color="white">
-        {currentBusinessHours
-          ? `영업중 ${todayBusinessHours}`
-          : todayBusinessHours
-          ? `영업준비중 ${todayBusinessHours}`
-          : '휴무일'}
-      </Text>
-    </NoteContainer>
+    <Container position="relative">
+      <NoteContainer
+        onClick={onClick}
+        warning={!currentBusinessHours}
+        bottomBorderRadius={bottomBorderRadius}
+      >
+        <Text bold size="small" color="white">
+          {currentBusinessHours
+            ? `영업중 ${todayBusinessHours}`
+            : todayBusinessHours
+            ? `영업준비중 ${todayBusinessHours}`
+            : '휴무일'}
+        </Text>
+      </NoteContainer>
+    </Container>
   )
 }
