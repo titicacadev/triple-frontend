b7fe775b7c5f260a0a91629b1e76c26f65ea7aaa
finn <finn@triple-corp.com>
2022-02-07 16:49:58 +0900

    feat: export Email Elements

--- packages/email-triple-document/src/index.ts
+++ packages/email-triple-document/src/index.ts
@@ -1,5 +1,6 @@
 import { EmailTripleDocument } from './email-triple-document'
 
+export { default as ELEMENTS } from './elements'
 export { TripleDocumentElement as EmailTripleDocumentElement } from './elements'
 
 export default EmailTripleDocument
