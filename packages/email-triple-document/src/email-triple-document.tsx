b80c9da58838ba3d09991b66e1b5766673eec198
finn <finn@triple-corp.com>
2022-02-07 14:50:49 +0900

    feat: EmailTripleDocument Component

---
+++ packages/email-triple-document/src/email-triple-document.tsx
@@ -0,0 +1,35 @@
+import React, { ComponentType } from 'react'
+
+import ELEMENTS, { TripleDocumentElement, GetValue } from './elements'
+import FluidTable from './common/fluid-table'
+
+export function EmailTripleDocument({
+  children,
+}: {
+  children: TripleDocumentElement[]
+}) {
+  return (
+    <FluidTable>
+      <tbody>
+        {children.map(({ type, value }, index) => {
+          const Element = ELEMENTS[type] as ComponentType<{
+            value: GetValue<typeof type>
+          }>
+          // type 단언으로 data가 어떤 타입인지 컴파일 타임에선 알 수 없어서 생기는 불일치 해소
+
+          if (Element === undefined) {
+            return null
+          }
+
+          return (
+            <tr key={index}>
+              <td>
+                <Element value={value} />
+              </td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </FluidTable>
+  )
+}
