69ba686d10a826980f3954619179384b26bead9a
finn <finn@triple-corp.com>
2022-02-07 14:45:15 +0900

    feat: Note Component

---
+++ packages/email-triple-document/src/elements/note.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Box from '../common/box'
+import FluidTable from '../common/fluid-table'
+
+export interface NoteDocument {
+  type: 'note'
+  value: {
+    title: string
+    body: string
+  }
+}
+
+const SegmentStlyed = styled.div`
+  padding: 20px;
+  border-radius: 6px;
+  background-color: #fafafa;
+
+  &:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+`
+
+const NoteTextStyled = styled.div`
+  font-size: 14px;
+  line-height: 1.57;
+`
+
+const TitleStyled = styled(NoteTextStyled)`
+  font-weight: 700;
+  color: var(--color-gray);
+`
+
+const BodyStyled = styled(NoteTextStyled)`
+  color: var(--color-gray800);
+`
+
+export default function NoteView({
+  value: { title, body },
+}: {
+  value: NoteDocument['value']
+}) {
+  return (
+    <FluidTable>
+      <tr>
+        <Box padding={{ top: 30, bottom: 30, left: 30, right: 30 }}>
+          <SegmentStlyed>
+            <TitleStyled>{title}</TitleStyled>
+            <BodyStyled>{body}</BodyStyled>
+          </SegmentStlyed>
+        </Box>
+      </tr>
+    </FluidTable>
+  )
+}
