86cba744513ae6de68c5ada76d5654a58cfce285
finn <finn@triple-corp.com>
2022-02-07 14:50:49 +0900

    feat: EmailTripleDocument Component

---
+++ packages/email-triple-document/src/index.ts
@@ -0,0 +1,5 @@
+import { EmailTripleDocument } from './email-triple-document'
+
+export { TripleDocumentElement as EmailTripleDocumentElement } from './elements'
+
+export default EmailTripleDocument
