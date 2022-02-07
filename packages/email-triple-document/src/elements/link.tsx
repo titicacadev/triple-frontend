0fe8cf7d4af419daa1b558494b4b25fe9f30c6ed
finn <finn@triple-corp.com>
2022-02-07 14:44:01 +0900

    feat: Link Component

---
+++ packages/email-triple-document/src/elements/link.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Box from '../common/box'
+import FluidTable from '../common/fluid-table'
+
+export interface LinkDocument {
+  type: 'link'
+  value: {
+    id: string
+    label: string
+    href: string
+    display: 'button'
+  }
+}
+
+const LinkStyled = styled.a`
+  padding: 13px 25px 13px 25px;
+  display: inline-block;
+  font-size: 13px;
+  font-weight: bold;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  cursor: pointer;
+  border: 0;
+  color: var(--color-white);
+  background-color: var(--color-blue);
+  float: none;
+  border-radius: 21px;
+`
+const LinkContainer = styled.div`
+  text-align: center;
+`
+
+export default function LinkView({
+  value: { label, href: givenHref, id: linkId },
+}: {
+  value: LinkDocument['value']
+}) {
+  return (
+    <FluidTable>
+      <tr>
+        <Box padding={{ top: 55, left: 30, right: 30 }}>
+          {/* triple document의 경우 자체 margin 5 0 0 0, container margin 50 30 0 30 */}
+          <LinkContainer>
+            <LinkStyled href={givenHref} ses:tags={`link:${linkId}`}>
+              {label}
+            </LinkStyled>
+          </LinkContainer>
+        </Box>
+      </tr>
+    </FluidTable>
+  )
+}
