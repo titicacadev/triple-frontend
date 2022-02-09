9909252fbf89a19acb25c4e32afd705c66bd61c7
finn <finn@triple-corp.com>
2022-02-09 14:23:24 +0900

    feat: Link Element test code

--- packages/triple-email-document/src/elements.test.tsx
+++ packages/triple-email-document/src/elements.test.tsx
@@ -230,3 +230,36 @@ describe('Divider Elements', () => {
     expect(dividerImgSrc).toStrictEqual(expectedImgSrc)
   })
 })
+
+describe('Link Element', () => {
+  test('버튼형 링크 Element를 렌더링합니다.', () => {
+    const Link = ELEMENTS.link
+
+    const { getByRole } = render(
+      <Link
+        value={{
+          id: '',
+          label: '네버랜드 바로 가기',
+          href: 'Test Href',
+          display: 'button',
+        }}
+      />,
+    )
+
+    const anchorElement = getByRole('link')
+    const anchorLabel = anchorElement.textContent
+    const anchorHref = anchorElement.getAttribute('href')
+
+    const createdAnchor = {
+      label: anchorLabel,
+      href: anchorHref,
+    }
+
+    const expectedAnchor = {
+      label: '네버랜드 바로 가기',
+      href: 'Test Href',
+    }
+
+    expect(createdAnchor).toStrictEqual(expectedAnchor)
+  })
+})
