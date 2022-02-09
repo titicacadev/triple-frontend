3e9d521ab62106aee27b9933973e7af72be1142d
finn <finn@triple-corp.com>
2022-02-09 18:23:44 +0900

    feat: type이 정의되지 않은 Element 관련 테스트 코드 추가

--- packages/triple-email-document/src/triple-email-document.test.tsx
+++ packages/triple-email-document/src/triple-email-document.test.tsx
@@ -4,12 +4,40 @@ import { render } from '@testing-library/react'
 
 import TripleEmailDocument, { EmailTripleDocumentElement } from '.'
 
+type ExtendedEmailTripleDocumentElement =
+  | EmailTripleDocumentElement
+  | UndefinedDocument
+
+interface UndefinedDocument {
+  type: undefined
+  value: undefined
+}
+
 describe('Triple Email Document', () => {
-  test('Email Document Element가 렌더링되지 않습니다.', () => {
+  test('children이 빈 배열이면 Email Document Element가 렌더링되지 않습니다.', () => {
     const mockedChildren: EmailTripleDocumentElement[] = []
 
     const { queryByRole } = render(
       <TripleEmailDocument>{mockedChildren}</TripleEmailDocument>,
+    )
+
+    const TrElement = queryByRole('tr')
+
+    expect(TrElement).toEqual(null)
+  })
+
+  test('type이 정의되지 않은 Element는 렌더링되지 않습니다.', () => {
+    const mockedChildren: ExtendedEmailTripleDocumentElement[] = [
+      {
+        type: undefined,
+        value: undefined,
+      },
+    ]
+
+    const { queryByRole } = render(
+      <TripleEmailDocument>
+        {mockedChildren as EmailTripleDocumentElement[]}
+      </TripleEmailDocument>,
     )
 
     const TrElement = queryByRole('tr')
