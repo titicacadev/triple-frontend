e4c481ea8a9e962c6ec570e6818732a1fc8421df
finn <finn@triple-corp.com>
2022-02-09 14:22:03 +0900

    feat: Heading Element test code

---
+++ packages/triple-email-document/src/elements.test.tsx
@@ -0,0 +1,134 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+
+import { ELEMENTS } from '.'
+
+describe('Heading Elements', () => {
+  const mockedHeadingValue = {
+    text: 'This is heading',
+  }
+
+  test('헤드라인이 있는 제목 1 Element를 렌더링합니다.', () => {
+    const mockedHeading1Value = {
+      headline: 'This is headline',
+      ...mockedHeadingValue,
+    }
+
+    const Heading1WithHeadline = ELEMENTS.heading1
+
+    const { getByText } = render(
+      <Heading1WithHeadline value={mockedHeading1Value} />,
+    )
+
+    const HeadlineElement = getByText(/This is headline/i)
+    const HeadingElement = getByText(/This is heading/i)
+
+    const headlineFontSize = getComputedStyle(HeadlineElement).fontSize
+    const headingFontSize = getComputedStyle(HeadingElement).fontSize
+
+    const createdResult = {
+      headlineFontSize,
+      headingFontSize,
+      headline: HeadlineElement.textContent,
+      text: HeadingElement.textContent,
+    }
+
+    const expectedResult = {
+      headlineFontSize: '13px',
+      headingFontSize: '21px',
+      headline: 'This is headline',
+      text: 'This is heading',
+    }
+
+    expect(createdResult).toStrictEqual(expectedResult)
+  })
+
+  test('헤드라인이 없는 제목 1 Element를 렌더링합니다.', () => {
+    const Heading1Normal = ELEMENTS.heading1
+
+    const { getByText } = render(<Heading1Normal value={mockedHeadingValue} />)
+
+    const HeadingElement = getByText(/This is heading/i)
+
+    const headingFontSize = getComputedStyle(HeadingElement).fontSize
+
+    const createdResult = {
+      fontSize: headingFontSize,
+      text: HeadingElement.textContent,
+    }
+
+    const expectedResult = {
+      fontSize: '21px',
+      text: 'This is heading',
+    }
+
+    expect(createdResult).toStrictEqual(expectedResult)
+  })
+
+  test('글자크기 19px, 색상 gray 인 제목 2 Element를 렌더링합니다.', () => {
+    const Heading2 = ELEMENTS.heading2
+
+    const { getByText } = render(<Heading2 value={mockedHeadingValue} />)
+
+    const HeadingElement = getByText(/This is heading/i)
+    const headingStyle = getComputedStyle(HeadingElement)
+
+    const createdResult = {
+      fontSize: headingStyle.fontSize,
+      text: HeadingElement.textContent,
+    }
+
+    const expectedResult = {
+      fontSize: '19px',
+      text: 'This is heading',
+    }
+
+    expect(createdResult).toStrictEqual(expectedResult)
+  })
+
+  test('글자크기 16px, 색상은 gray 인 제목 3 Element를 렌더링합니다.', () => {
+    const Heading3 = ELEMENTS.heading3
+
+    const { getByText } = render(<Heading3 value={mockedHeadingValue} />)
+
+    const HeadingElement = getByText(/This is heading/i)
+    const headingStyle = getComputedStyle(HeadingElement)
+
+    const createdResult = {
+      fontSize: headingStyle.fontSize,
+      text: HeadingElement.textContent,
+    }
+
+    const expectedResult = {
+      fontSize: '16px',
+      text: 'This is heading',
+    }
+
+    expect(createdResult).toStrictEqual(expectedResult)
+  })
+
+  test('글자크기 16px, 색상은 #2987f0 인 제목 4 Element를 렌더링합니다.', () => {
+    const Heading4 = ELEMENTS.heading4
+
+    const { getByText } = render(<Heading4 value={mockedHeadingValue} />)
+
+    const HeadingElement = getByText(/This is heading/i)
+
+    const headingFontStyle = getComputedStyle(HeadingElement)
+
+    const createdResult = {
+      fontSize: headingFontStyle.fontSize,
+      text: HeadingElement.textContent,
+      color: headingFontStyle.color,
+    }
+
+    const expectedResult = {
+      fontSize: '16px',
+      text: 'This is heading',
+      color: 'rgb(41, 135, 240)', // #2987f0
+    }
+
+    expect(createdResult).toStrictEqual(expectedResult)
+  })
+})
