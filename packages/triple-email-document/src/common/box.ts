91d2353a0b86317b64b728333a4fe2c9f8a493e7
finn <finn@triple-corp.com>
2022-02-09 18:14:55 +0900

    fix: padding type

--- packages/triple-email-document/src/common/box.ts
+++ packages/triple-email-document/src/common/box.ts
@@ -1,20 +1,14 @@
 import styled from 'styled-components'
+import { MarginPadding } from '@titicaca/core-elements'
 
-interface Positioning {
-  top?: number
-  bottom?: number
-  left?: number
-  right?: number
-}
-
-const Box = styled.td<{ padding?: Positioning }>`
+const Box = styled.td<{ padding?: MarginPadding }>`
   padding: ${({ padding }) => {
     const { top, bottom, left, right } = padding || {}
     return [top, right, bottom, left].map(addUnit).join(' ')
   }};
 `
 
-function addUnit(value: number | undefined) {
+function addUnit(value: string | number | undefined) {
   return value !== undefined && value !== 0 ? `${value}px` : 0
 }
 
