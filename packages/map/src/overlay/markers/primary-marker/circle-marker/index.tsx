22c4839916bd3aeaa0f6c32907cb448f91d49fdf
Gi Sung Park <lucian@triple-corp.com>
2022-02-10 17:01:55 +0900

    fix: 최종 수정

--- packages/map/src/overlay/markers/primary-marker/circle-marker/index.tsx
+++ packages/map/src/overlay/markers/primary-marker/circle-marker/index.tsx
@@ -1,12 +1,40 @@
 import React, { MouseEvent, PropsWithChildren, useCallback } from 'react'
 import { OverlayViewProps, OverlayView } from '@react-google-maps/api'
 
-import { Circle, CirclePin, MarkerBaseProps } from './circle-marker-base'
+import {
+  Circle,
+  CirclePin,
+  CircleType,
+  MarkerBaseProps,
+} from './circle-marker-base'
 
 export interface CircleMarkerProps
   extends MarkerBaseProps,
     Omit<OverlayViewProps, 'mapPaneName'> {
   onClick?: (e: MouseEvent) => void
+}
+
+/** 마커 종류의 따른 css 값 */
+export const CIRCLE_MARKER: {
+  [key in CircleType]: {
+    color: string
+    imageUrl: string
+  }
+} = {
+  attraction: {
+    color: 'var(--color-purple)',
+    imageUrl:
+      'https://assets.triple.guide/images/img-map-pin-attraction-on@3x.png',
+  },
+  restaurant: {
+    color: 'var(--color-vermilion)',
+    imageUrl:
+      'https://assets.triple.guide/images/img-map-pin-restaurant-on@3x.png',
+  },
+  hotel: {
+    color: 'var(--color-purple)',
+    imageUrl: 'https://assets.triple.guide/images/img-map-pin-hotel-on@3x.png',
+  },
 }
 
 export function CircleMarker({
