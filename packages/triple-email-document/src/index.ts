3f1e1fa97d6b4a220e4a391e11d38190b812f812
finn <finn@triple-corp.com>
2022-02-11 13:57:08 +0900

    feat: export EmailFooter & EmailPreview component

--- packages/triple-email-document/src/index.ts
+++ packages/triple-email-document/src/index.ts
@@ -2,5 +2,6 @@ import { TripleEmailDocument } from './triple-email-document'
 
 export { default as ELEMENTS } from './elements'
 export { TripleEmailElementData as TripleEmailDocumentElement } from './elements'
+export { EmailFooter, EmailPreview } from './components'
 
 export default TripleEmailDocument
