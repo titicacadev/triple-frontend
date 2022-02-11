e3a8af4f40f32b84f262d7a9cdb01c48008a1637
finn <finn@triple-corp.com>
2022-02-11 14:20:06 +0900

    feat: EmailPreview test code

---
+++ packages/triple-email-document/src/components/preview.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+
+import EmailPreview from './preview'
+
+test('EmailPreview를 렌더링합니다.', () => {
+  const mockedPreviewValue = {
+    phrase: 'Cheking Email',
+  }
+
+  const { getByText } = render(<EmailPreview value={mockedPreviewValue} />)
+
+  const previewElement = getByText(mockedPreviewValue.phrase)
+
+  expect(previewElement).toBeInTheDocument()
+})
