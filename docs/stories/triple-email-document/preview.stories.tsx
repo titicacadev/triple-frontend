cb6b4a739fa86e9b4f2c987ba86f0391d498d353
finn <finn@triple-corp.com>
2022-02-11 17:26:03 +0900

    feat: EmailPreview storybook code

---
+++ docs/stories/triple-email-document/preview.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentMeta } from '@storybook/react'
+import { EmailPreview } from '@titicaca/triple-email-document'
+
+export default {
+  title: 'Document / triple-email-document / components',
+  component: EmailPreview,
+  argTypes: {
+    value: {
+      phrase: {
+        type: 'string',
+        require: true,
+      },
+    },
+  },
+} as ComponentMeta<typeof EmailPreview>
+
+export const DefaultPreview = {
+  storyName: '미리보기',
+  args: {
+    value: {
+      phrase: '지금 10만원 받고, 트리플로 새해여행 어때요?',
+    },
+  },
+}
+
+DefaultPreview.storyName = '미리보기'
