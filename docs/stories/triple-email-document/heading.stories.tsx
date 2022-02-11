d3dffbd440d8aa32742e8bfacdfd7aa5809232f2
finn <finn@triple-corp.com>
2022-02-11 17:30:31 +0900

    fix: heading title lower case

--- docs/stories/triple-email-document/heading.stories.tsx
+++ docs/stories/triple-email-document/heading.stories.tsx
@@ -10,7 +10,7 @@ const {
 } = ELEMENTS
 
 export default {
-  title: 'Document / triple-email-document / elements / Heading',
+  title: 'Document / triple-email-document / elements / heading',
 } as Meta
 
 const Heading1Template: ComponentStory<typeof Heading1> = (args) => (
