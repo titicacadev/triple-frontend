57a2a510f69d665d69c4a6c9520c6d1eca30a761
finn <finn@triple-corp.com>
2022-02-11 13:55:59 +0900

    feat: HandlebarsAnchor common component

---
+++ packages/triple-email-document/src/common/handlebars-anchor.tsx
@@ -0,0 +1,16 @@
+import React, { PropsWithChildren } from 'react'
+
+export default function HandlebarsAnchor({
+  linkId,
+  className,
+  children,
+}: PropsWithChildren<{
+  linkId: string
+  className?: string
+}>) {
+  return (
+    <a className={className} href={`{{${linkId}}}`} ses:tags={`link:${linkId}`}>
+      {children}
+    </a>
+  )
+}
--- packages/triple-email-document/src/common/index.ts
+++ packages/triple-email-document/src/common/index.ts
@@ -1,2 +1,3 @@
 export { default as Box } from './box'
 export { default as FluidTable } from './fluid-table'
+export { default as HandlebarsAnchor } from './handlebars-anchor'
