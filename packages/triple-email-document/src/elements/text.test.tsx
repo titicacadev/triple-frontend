f20641110016e9483d808e0fc7cfd4c70e2ae2fa
finn <finn@triple-corp.com>
2022-02-14 13:13:25 +0900

    feat: render text in Text Elements test code

--- packages/triple-email-document/src/elements/text.test.tsx
+++ packages/triple-email-document/src/elements/text.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
 
 import { ELEMENTS } from '../index'
 
 test('rawHTML을 렌더링합니다.', () => {
   const mockedTextValue = {
-    rawHTML: '<a href="/regions/:regionId">Inline link</a>',
+    rawHTML: '<a href="/regions/:regionId">rawHtml Inline link</a>',
   }
 
   const Text = ELEMENTS.text
@@ -15,4 +16,18 @@ test('rawHTML을 렌더링합니다.', () => {
   const anchorElement = getByText(/Inline link/i).outerHTML
 
   expect(anchorElement).toBe(mockedTextValue.rawHTML)
+})
+
+test('text를 렌더링합니다.', () => {
+  const mockedTextValue = {
+    text: 'Default Text',
+  }
+
+  const Text = ELEMENTS.text
+
+  const { getByText } = render(<Text value={mockedTextValue} />)
+
+  const textElement = getByText(/Default Text/i)
+
+  expect(textElement).toBeInTheDocument()
 })
