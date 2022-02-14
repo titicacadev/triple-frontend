b8bbb04016bcde3583884df32504e5f2fdc43866
finn <finn@triple-corp.com>
2022-02-14 11:59:41 +0900

    feat: export Images Types

--- packages/triple-email-document/src/index.ts
+++ packages/triple-email-document/src/index.ts
@@ -2,6 +2,7 @@ import { TripleEmailDocument } from './triple-email-document'
 
 export { default as ELEMENTS } from './elements'
 export { TripleEmailElementData as TripleEmailDocumentElement } from './elements'
+export { ExtendedImageMeta, ImageDocument } from './elements/images'
 export { EmailFooter, EmailPreview } from './components'
 
 export default TripleEmailDocument
