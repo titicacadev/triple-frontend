38d91713613c2cb0c7b126874af6b36677bcab61
finn <finn@triple-corp.com>
2022-02-14 12:28:19 +0900

    feat: export PrevieDocument Type

--- packages/triple-email-document/src/index.ts
+++ packages/triple-email-document/src/index.ts
@@ -3,6 +3,6 @@ import { TripleEmailDocument } from './triple-email-document'
 export { default as ELEMENTS } from './elements'
 export { TripleEmailElementData as TripleEmailDocumentElement } from './elements'
 export { ExtendedImageMeta, ImageDocument } from './elements/images'
-export { EmailFooter, EmailPreview } from './components'
+export { EmailFooter, EmailPreview, PreviewDocument } from './components'
 
 export default TripleEmailDocument
