1302b5b0e96c054e7431d4b28d6724b579f6d517
finn <finn@triple-corp.com>
2022-02-15 17:18:28 +0900

    feat: Register test code

---
+++ packages/replies/src/register.test.tsx
@@ -0,0 +1,59 @@
+import React, { PropsWithChildren } from 'react'
+import '@testing-library/jest-dom'
+import 'jest-styled-components'
+import { render } from '@testing-library/react'
+import { EnvProvider, SessionContextProvider } from '@titicaca/react-contexts'
+
+import { RepliesProvider } from './context'
+import Register from './register'
+
+describe('Reply 등록 버튼을 테스트합니다.', () => {
+  test('입력 창에 입력된 값이 없으면 버튼의 색상은 blue500 입니다.', () => {
+    const mockedOnReplyAdd = jest.fn()
+    const mockedOnReplyEdit = jest.fn()
+
+    const { getByRole } = render(
+      <Register
+        resourceId=""
+        resourceType="article"
+        onReplyAdd={mockedOnReplyAdd}
+        onReplyEdit={mockedOnReplyEdit}
+      />,
+      { wrapper: ReplyWithLoginWrapper },
+    )
+
+    const registerButtonElement = getByRole('button', { name: /등록/ })
+
+    expect(registerButtonElement).toHaveStyleRule(
+      'color',
+      'var(--color-blue500)',
+    )
+  })
+})
+
+function ReplyWithLoginWrapper({ children }: PropsWithChildren<unknown>) {
+  return (
+    <EnvProvider
+      appUrlScheme=""
+      webUrlBase=""
+      authBasePath="/"
+      facebookAppId=""
+      defaultPageTitle=""
+      defaultPageDescription=""
+      googleMapsApiKey=""
+      afOnelinkId=""
+      afOnelinkPid=""
+      afOnelinkSubdomain=""
+    >
+      <SessionContextProvider
+        type="browser"
+        props={{
+          initialUser: undefined,
+          initialSessionAvailability: true,
+        }}
+      >
+        <RepliesProvider>{children}</RepliesProvider>
+      </SessionContextProvider>
+    </EnvProvider>
+  )
+}
