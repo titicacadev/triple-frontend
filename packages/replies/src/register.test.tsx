c8187818a2389cc7aa726eceaf8460febae4b19b
finn <finn@triple-corp.com>
2022-02-16 13:51:19 +0900

    feat: 입력 값이 있을 때 등록 버튼 배경색상 관련 테스트 코드 추가

--- packages/replies/src/register.test.tsx
+++ packages/replies/src/register.test.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren } from 'react'
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { EnvProvider, SessionContextProvider } from '@titicaca/react-contexts'
 
 import { RepliesProvider } from './context'
@@ -28,6 +28,29 @@ describe('Reply 등록 버튼을 테스트합니다.', () => {
       'color',
       'var(--color-blue500)',
     )
+  })
+
+  test('입력 창에 입력된 값이 있으면 버튼의 색상은 blue 입니다.', async () => {
+    const mockedOnReplyAdd = jest.fn()
+    const mockedOnReplyEdit = jest.fn()
+
+    const { getByRole } = render(
+      <Register
+        resourceId=""
+        resourceType="article"
+        onReplyAdd={mockedOnReplyAdd}
+        onReplyEdit={mockedOnReplyEdit}
+      />,
+      { wrapper: ReplyWithLoginWrapper },
+    )
+
+    const textareaElement = getByRole('textbox')
+
+    fireEvent.change(textareaElement, { target: { value: 'Hi' } })
+
+    const registerButtonElement = getByRole('button', { name: /등록/ })
+
+    expect(registerButtonElement).toHaveStyleRule('color', 'var(--color-blue)')
   })
 })
 
