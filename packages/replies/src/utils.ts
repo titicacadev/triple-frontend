acb79120f10792e4e7dd335146f294a3dbfec8f5
finn <finn@triple-corp.com>
2022-02-21 01:54:51 +0900

    fix: 답글 생성 시간 기준으로 오름차순 정렬

--- packages/replies/src/utils.ts
+++ packages/replies/src/utils.ts
@@ -2,7 +2,12 @@ import { Reply } from './types'
 
 export function checkUniqueReply(reply: Reply[]): Reply[] {
   const result = [
-    ...new Map((reply || []).map((item) => [item.id, item])).values(),
+    ...new Map(
+      (reply || []).map((item) => [
+        item.id,
+        { ...item, children: checkUniqueReply(item.children) },
+      ]),
+    ).values(),
   ].sort(
     (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
   )
