ed2e849b3a2ddfd174ecb90232a4a99deefdd9cb
finn <finn@triple-corp.com>
2022-02-21 10:46:17 +0900

    fix: checkUniqueReply rollback

--- packages/replies/src/utils.ts
+++ packages/replies/src/utils.ts
@@ -2,12 +2,7 @@ import { Reply } from './types'
 
 export function checkUniqueReply(reply: Reply[]): Reply[] {
   const result = [
-    ...new Map(
-      (reply || []).map((item) => [
-        item.id,
-        { ...item, children: checkUniqueReply(item.children) },
-      ]),
-    ).values(),
+    ...new Map((reply || []).map((item) => [item.id, item])).values(),
   ].sort(
     (a, b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
   )
