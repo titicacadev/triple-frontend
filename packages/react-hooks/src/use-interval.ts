fd600af52203355ae108ca53937f126887b900d2
guswl98 <hjee0901@gmail.com>
2022-02-22 17:26:20 +0900

    useInterval 훅 추가

---
+++ packages/react-hooks/src/use-interval.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from 'react'
+
+/**
+ * @param {number|null} delay null인 경우 setInterval을 일시 중지한다.
+ */
+
+export function useInterval(callback?: Function, delay: number | null = null) {
+  const savedCallback = useRef<Function>()
+
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
+
+  useEffect(() => {
+    function tick() {
+      if (savedCallback.current) savedCallback.current()
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
+}
