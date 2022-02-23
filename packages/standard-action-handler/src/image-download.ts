57abb3193c6ac8e88f72644f892843747cf4981a
Gi Sung Park <lucian@triple-corp.com>
2022-02-23 11:09:12 +0900

    test: blob

--- packages/standard-action-handler/src/image-download.ts
+++ packages/standard-action-handler/src/image-download.ts
@@ -1,16 +1,41 @@
 import qs from 'qs'
 import { UrlElements } from '@titicaca/view-utilities'
+import fetch from 'isomorphic-fetch'
 
 export default async function imageDownload({ path, query }: UrlElements) {
   if (path === '/web-action/image-download' && query) {
     const { imageId } = qs.parse(query, { ignoreQueryPrefix: true })
-    const imagesApiHref = 'api/images/download/' + imageId
 
-    const downloadAnchor = document.createElement('a')
-    downloadAnchor.setAttribute('href', imagesApiHref)
-    downloadAnchor.setAttribute('download', '')
-    downloadAnchor.click()
+    const response = await fetch(`/api/images/media?ids=${imageId}`, {
+      method: 'GET',
+      headers: { 'content-type': 'application/json' },
+    })
 
+    const {
+      sizes: {
+        large: { imageUrl },
+      },
+    } = await response.json()
+
+    const image = await fetch(imageUrl)
+    const blobImage = image.blob()
+
+    const windowUrl = window.URL || window.webkitURL
+    const imageToDomString = windowUrl.createObjectURL(blobImage)
+
+    // ie에서는 Blob 저장이 정상적으로 이루어지지 않으므로 해당 케이스 추가
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(imageToDomString, 'image')
+    } else {
+      const downloadAnchor = document.createElement('a')
+      downloadAnchor.setAttribute('href', imageToDomString)
+      downloadAnchor.setAttribute('download', 'image')
+      downloadAnchor.click()
+      downloadAnchor.remove()
+
+      // URL 더이상 사용되지 않아 메모리 누수를 위해 제거
+      setTimeout(() => windowUrl.revokeObjectURL(imageToDomString), 60)
+    }
     return true
   }
 
