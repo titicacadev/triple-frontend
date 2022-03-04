4411fa5483dc5afd8bd05e0272fb112cd5175e2a
finn <finn@triple-corp.com>
2022-03-04 13:42:18 +0900

    fix: export file lint error

--- packages/intersection-observer/src/index.tsx
+++ packages/intersection-observer/src/index.tsx
@@ -1,5 +1,5 @@
 import IntersectionObserver from './lazy-loaded-intersection-observer'
 
 export { default as StaticIntersectionObserver } from './static-intersection-observer'
-export * from './use-intersection'
+export { default as useIntersection } from './use-intersection'
 export default IntersectionObserver
