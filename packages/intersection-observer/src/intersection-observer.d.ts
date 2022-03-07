659bef040447283ad71ac82acb615b7ed38f214a
Gi Sung Park <lucian@triple-corp.com>
2022-03-08 08:36:41 +0900

    fix: build error

--- packages/intersection-observer/src/intersection-observer.d.ts
+++ packages/intersection-observer/src/intersection-observer.d.ts
@@ -4,6 +4,7 @@ declare module 'intersection-observer' {
     IntersectionObserverEntry: unknown
     intersectionRatio: unknown
   }
-  const content: unknown
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any
   export default content
 }
