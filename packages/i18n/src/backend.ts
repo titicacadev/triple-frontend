524c43360752a5e0308005aee6dd2913e2ad562d
Inbeom Hwang <hwanginbeom@gmail.com>
2022-03-10 17:40:56 +0900

    Fix lint errors of i18n package

--- packages/i18n/src/backend.ts
+++ packages/i18n/src/backend.ts
@@ -1,18 +1,27 @@
+/* eslint-disable promise/prefer-await-to-callbacks */
+
 import fetch from 'isomorphic-fetch'
 
 export default class I18nextFetchBackend {
-  type: 'backend' = 'backend'
+  public type: 'backend' = 'backend'
 
-  static type: 'backend' = 'backend'
+  public static type: 'backend' = 'backend'
 
-  read(language: string, namespace: string, callback: Function) {
+  public read(
+    language: string,
+    namespace: string,
+    callback: (error: Error | null, data: unknown) => void,
+  ) {
     return this.loadUrl(
       `https://assets.triple.guide/locales/${language}/${namespace}.json`,
       callback,
     )
   }
 
-  async loadUrl(url: string, callback: Function) {
+  public async loadUrl(
+    url: string,
+    callback: (error: Error | null, data: unknown) => void,
+  ) {
     const response = await fetch(url)
 
     if (response.ok) {
