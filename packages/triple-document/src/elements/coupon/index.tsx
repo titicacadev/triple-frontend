14db0318dd757e71e0c6c44800b6699d6b2ca6c5
guswl98 <hjee0901@gmail.com>
2022-03-25 11:51:42 +0900

    enabledAt prop 추가

--- packages/triple-document/src/elements/coupon/index.tsx
+++ packages/triple-document/src/elements/coupon/index.tsx
@@ -60,6 +60,7 @@ export default function Coupon({
         <CouponGroupDownloadButton
           verificationType={verificationType}
           groupId={identifier}
+          enabledAt={enabledAt}
           onClick={handleCouponClick}
         />
       )}
