edd72b6bbab9d0b83a4c2be309aefc1eeacb3749
Inbeom Hwang <hwanginbeom@gmail.com>
2022-03-28 11:46:21 +0900

    Deprecate verification type

--- packages/user-verification/src/types.ts
+++ packages/user-verification/src/types.ts
@@ -1,4 +1,4 @@
-export type VerificationType =
-  | 'sms-verification'
-  | 'personal-id-verification-with-residence'
-  | 'personal-id-verification'
+/**
+ * @deprecated 다양한 외부 프로모션 타입에 대응하기 위해 string 타입을 사용합니다.
+ */
+export type VerificationType = string
