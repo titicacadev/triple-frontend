2b0bee7a038d075657be19dfa271ba0e18d98c72
finn <finn@triple-corp.com>
2022-04-15 02:31:36 +0900

    feat: 아이콘 추가

--- packages/poi-detail/src/detail-header/business-hours-note.tsx
+++ packages/poi-detail/src/detail-header/business-hours-note.tsx
@@ -1,5 +1,7 @@
 import { FlexBox, Text } from '@titicaca/core-elements'
 
+import { TimeIcon, RightArrowIcon } from './business-hours-icons'
+
 export default function BusinessHoursNote({
   currentBusinessHours,
   todayBusinessHours,
@@ -10,10 +12,10 @@ export default function BusinessHoursNote({
   onClick: () => void
 }) {
   return (
-    <FlexBox flex onClick={onClick} margin={{ top: 14 }}>
-      {/* TODO : Alert Icon */}
+    <FlexBox flex onClick={onClick} margin={{ top: 14 }} alignItems="center">
+      <TimeIcon />
 
-      <Text size={13} bold lineHeight="16px" color="red">
+      <Text size={13} bold lineHeight="16px" color="red" margin={{ left: 4 }}>
         {currentBusinessHours
           ? `영업중 ${todayBusinessHours || ''}`
           : todayBusinessHours
@@ -21,7 +23,7 @@ export default function BusinessHoursNote({
           : '휴무일'}
       </Text>
 
-      {/* TODO : Right Arrow Icon */}
+      <RightArrowIcon />
     </FlexBox>
   )
 }
