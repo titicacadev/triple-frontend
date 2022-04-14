fecdab4ff2a746cd3b453058a202ac3a2a9ac2b1
finn <finn@triple-corp.com>
2022-04-15 02:31:36 +0900

    feat: 아이콘 추가

---
+++ packages/poi-detail/src/detail-header/business-hours-icons.tsx
@@ -0,0 +1,50 @@
+export function TimeIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="1.68892"
+        y="1.68892"
+        width="12.6222"
+        height="12.6222"
+        rx="6.31111"
+        stroke="#FF213C"
+        strokeWidth="1.6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.6667 8.00004H8L8 5.33337"
+        stroke="#FF213C"
+        strokeWidth="1.6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export function RightArrowIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.44664 12L10.3999 8.02351L6.3999 4"
+        stroke="#FF213C"
+        strokeWidth="1.6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
