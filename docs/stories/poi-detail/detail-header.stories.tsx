4581c2b67744aedda58e56581df1d94af7da0aa0
finn <finn@triple-corp.com>
2022-04-15 01:31:30 +0900

    feat: 운영시간이 추가된 PoiDetail Header 스토리북 작성

--- docs/stories/poi-detail/detail-header.stories.tsx
+++ docs/stories/poi-detail/detail-header.stories.tsx
@@ -43,3 +43,26 @@ export const Basic: ComponentStoryObj<typeof DetailHeader> = {
     reviewsRating: 4.45,
   },
 }
+
+export const WithBusinessHoursNote: ComponentStoryObj<typeof DetailHeader> = {
+  storyName: '영업시간 추가',
+  args: {
+    names: {
+      primary: '도쿄 디즈니 랜드',
+      ko: '도쿄 디즈니 랜드',
+      en: 'Tokyo Disney land',
+      local: '東京ディズニーランド',
+    },
+    areas: [
+      { id: 1, name: '도쿄' },
+      { id: 2, name: '오사카' },
+    ],
+    scrapsCount: 682,
+    reviewsCount: 13859,
+    reviewsRating: 4.45,
+    currentBusinessHours: { dayOfWeek: 1, from: 660, to: 1080 },
+    // todayBusinessHours: '11:00 - 18:00',
+    onBusinessHoursClick: undefined,
+    permanentlyClosed: false,
+  },
+}
