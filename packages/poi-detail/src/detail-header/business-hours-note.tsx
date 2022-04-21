cabf14c1da110a24b013ad7796785d6f94c4c7cb
finn <finn@triple-corp.com>
2022-04-21 11:54:25 +0900

    fix: BusinessHoursNote 렌더링 조건 변경

--- packages/poi-detail/src/detail-header/business-hours-note.tsx
+++ packages/poi-detail/src/detail-header/business-hours-note.tsx
@@ -3,37 +3,23 @@ import { Container, FlexBox, Text } from '@titicaca/core-elements'
 import { TimeIcon, RightArrowIcon } from './business-hours-icons'
 
 export default function BusinessHoursNote({
-  currentBusinessHours,
   todayBusinessHours,
   onClick,
 }: {
-  currentBusinessHours?: null | { from: number; to: number; dayOfWeek: number }
   todayBusinessHours?: string
   onClick: () => void
 }) {
   return (
-    <>
-      {currentBusinessHours ? null : (
-        <Container margin={{ top: 5 }}>
-          <FlexBox flex onClick={onClick} alignItems="center">
-            <TimeIcon />
+    <Container margin={{ top: 5 }}>
+      <FlexBox flex onClick={onClick} alignItems="center">
+        <TimeIcon />
 
-            <Text
-              size={13}
-              bold
-              lineHeight="16px"
-              color="red"
-              margin={{ left: 4 }}
-            >
-              {todayBusinessHours
-                ? `영업준비중 ${todayBusinessHours}`
-                : '휴무일'}
-            </Text>
+        <Text size={13} bold lineHeight="16px" color="red" margin={{ left: 4 }}>
+          {todayBusinessHours ? `영업준비중 ${todayBusinessHours}` : '휴무일'}
+        </Text>
 
-            <RightArrowIcon />
-          </FlexBox>
-        </Container>
-      )}
-    </>
+        <RightArrowIcon />
+      </FlexBox>
+    </Container>
   )
 }
