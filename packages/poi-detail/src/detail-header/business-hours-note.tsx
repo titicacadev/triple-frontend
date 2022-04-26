b2fd76035982c14a71784c9afb101ed79d97b343
finn <finn@triple-corp.com>
2022-04-26 16:26:54 +0900

    fix: font-size (13 -> 15)

--- packages/poi-detail/src/detail-header/business-hours-note.tsx
+++ packages/poi-detail/src/detail-header/business-hours-note.tsx
@@ -14,7 +14,7 @@ export default function BusinessHoursNote({
       <FlexBox flex onClick={onClick} alignItems="center">
         <TimeIcon />
 
-        <Text size={13} bold lineHeight="16px" color="red" margin={{ left: 4 }}>
+        <Text size={15} bold lineHeight="16px" color="red" margin={{ left: 4 }}>
           {todayBusinessHours ? `영업준비중 ${todayBusinessHours}` : '휴무일'}
         </Text>
 
