81d69b18e75852c76238328697cd8afce10d6108
finn <finn@triple-corp.com>
2022-04-27 11:29:02 +0900

    fix: 말줄임표 추가

--- packages/poi-detail/src/detail-header/business-hours-note.tsx
+++ packages/poi-detail/src/detail-header/business-hours-note.tsx
@@ -10,11 +10,18 @@ export default function BusinessHoursNote({
   onClick: () => void
 }) {
   return (
-    <Container margin={{ top: 10 }} display="inline-block">
+    <Container margin={{ top: 10 }}>
       <FlexBox flex onClick={onClick} alignItems="center">
         <TimeIcon />
 
-        <Text size={15} bold lineHeight="16px" color="red" margin={{ left: 4 }}>
+        <Text
+          size={15}
+          bold
+          lineHeight="16px"
+          color="red"
+          margin={{ left: 4 }}
+          ellipsis
+        >
           {todayBusinessHours ? `영업준비중 ${todayBusinessHours}` : '휴무일'}
         </Text>
 
