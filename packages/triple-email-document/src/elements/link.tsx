4729a188694bc89eb97388c51faef51655829ab0
finn <finn@triple-corp.com>
2022-04-27 18:22:16 +0900

    feat: block 디자인의 링크 추가

--- packages/triple-email-document/src/elements/link.tsx
+++ packages/triple-email-document/src/elements/link.tsx
@@ -1,7 +1,8 @@
-import { HTMLAttributes } from 'react'
+import { HTMLAttributes, PropsWithChildren } from 'react'
+import { Container } from '@titicaca/core-elements'
 import styled from 'styled-components'
 
-import { FluidTable, Box } from '../common'
+import { FluidTable, Box as DefaultBox } from '../common'
 
 export interface LinkDocument {
   type: 'link'
@@ -9,11 +10,11 @@ export interface LinkDocument {
     id: string
     label: string
     href: string
-    display: 'button'
+    display: 'button' | 'block'
   }
 }
 
-const LinkStyled = styled.a`
+const ButtonLink = styled.a`
   padding: 13px 25px 13px 25px;
   display: inline-block;
   font-size: 13px;
@@ -28,8 +29,20 @@ const LinkStyled = styled.a`
   float: none;
   border-radius: 21px;
 `
-const LinkContainer = styled.div`
+
+const BlockLink = styled.a`
+  padding: 7px 12px 8px 12px;
+  display: block;
+  font-size: 14px;
+  font-weight: bold;
   text-align: center;
+  text-decoration: none;
+  outline: none;
+  line-height: 17px;
+  color: rgba(58, 58, 58, 1);
+  border: 1px solid rgba(58, 58, 58, 0.2);
+  border-radius: 4px;
+  background-color: rgba(255, 255, 255, 1);
 `
 
 type HtmlTagAttributes<T> = HTMLAttributes<T>
@@ -40,24 +53,54 @@ declare module 'react' {
     'ses:tags'?: string
   }
 }
+
+const LINK_BOXES = {
+  button: ButtonBox,
+  block: BlockBox,
+}
+
+const LINK_ELEMENTS = {
+  button: ButtonLink,
+  block: BlockLink,
+}
+
 export default function LinkView({
-  value: { label, href: givenHref, id: linkId },
+  value: { label, href: givenHref, id: linkId, display },
 }: {
   value: LinkDocument['value']
 }) {
+  const Box = LINK_BOXES[display] || ButtonBox
+  const Element = LINK_ELEMENTS[display] || ButtonLink
+
   return (
     <FluidTable>
       <tbody>
         <tr>
-          <Box padding={{ top: 55, left: 30, right: 30 }}>
-            <LinkContainer>
-              <LinkStyled href={givenHref} ses:tags={`link:${linkId}`}>
+          <Box>
+            <Container textAlign="center">
+              <Element href={givenHref} ses:tags={`link:${linkId}`}>
                 {label}
-              </LinkStyled>
-            </LinkContainer>
+              </Element>
+            </Container>
           </Box>
         </tr>
       </tbody>
     </FluidTable>
+  )
+}
+
+function ButtonBox({ children }: PropsWithChildren<unknown>) {
+  return (
+    <DefaultBox padding={{ top: 55, left: 30, right: 30 }}>
+      {children}
+    </DefaultBox>
+  )
+}
+
+function BlockBox({ children }: PropsWithChildren<unknown>) {
+  return (
+    <DefaultBox padding={{ top: 15, left: 30, right: 30 }}>
+      {children}
+    </DefaultBox>
   )
 }
