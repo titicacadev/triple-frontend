bd992710588044f2fec6b69659ebd496fde7781c
finn <finn@triple-corp.com>
2022-04-28 15:20:09 +0900

    fix: inline-block

--- packages/poi-detail/src/detail-header/business-hours-note.tsx
+++ packages/poi-detail/src/detail-header/business-hours-note.tsx
@@ -10,7 +10,7 @@ export default function BusinessHoursNote({
   onClick: () => void
 }) {
   return (
-    <Container margin={{ top: 10 }}>
+    <Container margin={{ top: 10 }} display="inline-block">
       <FlexBox flex onClick={onClick} alignItems="center">
         <TimeIcon />
 
