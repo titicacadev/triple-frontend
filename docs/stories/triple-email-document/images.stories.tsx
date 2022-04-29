cf5b6c522e01055be810177c4137b8af91778e1b
finn <finn@triple-corp.com>
2022-04-29 12:31:20 +0900

    fix: images element display로 구분지어 작업

--- docs/stories/triple-email-document/images.stories.tsx
+++ docs/stories/triple-email-document/images.stories.tsx
@@ -37,25 +37,6 @@ const TITIES: { [key: string]: string } = {
   3: '사이즈, 링크, 제목이 있는 데이터 구조',
 }
 
-const ImageTemplate: {
-  (args: { propList: ComponentProps<typeof Images>[] }): JSX.Element
-  storyName?: string
-  args?: {
-    propList: ComponentProps<typeof Images>[]
-  }
-} = (args) => (
-  <Container>
-    {args.propList.map((props, index) => (
-      <ImageContainer key={index}>
-        <Title>{TITIES[index]}</Title>
-        <Border>
-          <Images {...props} />
-        </Border>
-      </ImageContainer>
-    ))}
-  </Container>
-)
-
 const IMAGE = {
   id: 'IMAGE_ID',
   sizes: {
@@ -92,126 +73,119 @@ const IMAGE_WITH_TITLE_AND_LINK = {
   ...IMAGE_WITH_LINK,
 }
 
+const ImageTemplate: {
+  (args: { propList: ComponentProps<typeof Images>[] }): JSX.Element
+  storyName?: string
+  args?: {
+    propList: ComponentProps<typeof Images>[]
+  }
+} = (args) => {
+  return (
+    <Container>
+      {args.propList.map((props, index) => (
+        <ImageContainer key={index}>
+          <Title>{TITIES[index]}</Title>
+          <Border>
+            <Images {...props} />
+          </Border>
+        </ImageContainer>
+      ))}
+    </Container>
+  )
+}
+
 export const Default = ImageTemplate.bind({})
 Default.storyName = '간격 없는 이미지 1개'
 Default.args = {
-  propList: [
-    {
-      value: {
-        display: 'gapless-block',
-        images: [IMAGE],
-      },
-    },
-    {
-      value: {
-        display: 'gapless-block',
-        images: [IMAGE_WITH_TITLE],
-      },
-    },
-    {
-      value: {
-        display: 'gapless-block',
-        images: [IMAGE_WITH_LINK],
-      },
-    },
-    {
-      value: {
-        display: 'gapless-block',
-        images: [IMAGE_WITH_TITLE_AND_LINK],
-      },
-    },
-  ],
+  propList: generateSampleImages('gapless-block'),
 }
 
 export const TwoImages = ImageTemplate.bind({})
 TwoImages.storyName = '간격 없는 이미지 2개'
 TwoImages.args = {
-  propList: [
-    {
-      value: {
-        display: 'gapless-block',
-        images: [IMAGE, IMAGE],
-      },
-    },
-    {
-      value: {
-        display: 'gapless-block',
-        images: [IMAGE_WITH_TITLE, IMAGE_WITH_TITLE],
-      },
-    },
-    {
-      value: {
-        display: 'gapless-block',
-        images: [IMAGE_WITH_LINK, IMAGE_WITH_LINK],
-      },
-    },
-    {
-      value: {
-        display: 'gapless-block',
-        images: [IMAGE_WITH_TITLE_AND_LINK, IMAGE_WITH_TITLE_AND_LINK],
-      },
-    },
-  ],
+  propList: generateSampleImagesTwo('gapless-block'),
 }
 
 export const OneImageWithPadding = ImageTemplate.bind({})
 OneImageWithPadding.storyName = '간격 있는 이미지 1개'
 OneImageWithPadding.args = {
-  propList: [
-    {
-      value: {
-        display: 'default',
-        images: [IMAGE],
-      },
-    },
-    {
-      value: {
-        display: 'default',
-        images: [IMAGE_WITH_TITLE],
-      },
-    },
-    {
-      value: {
-        display: 'default',
-        images: [IMAGE_WITH_LINK],
-      },
-    },
-    {
-      value: {
-        display: 'default',
-        images: [IMAGE_WITH_TITLE_AND_LINK],
-      },
-    },
-  ],
+  propList: generateSampleImages('default'),
 }
 
 export const TwoImagesWithPadding = ImageTemplate.bind({})
 TwoImagesWithPadding.storyName = '간격 있는 이미지 2개'
 TwoImagesWithPadding.args = {
-  propList: [
+  propList: generateSampleImagesTwo('default'),
+}
+
+export const OneImageWithPaddingV2 = ImageTemplate.bind({})
+OneImageWithPaddingV2.storyName = '간격 있는 이미지 1개 V2'
+OneImageWithPaddingV2.args = {
+  propList: generateSampleImagesTwo('default-v2'),
+}
+
+export const TwoImagesWithPaddingV2 = ImageTemplate.bind({})
+TwoImagesWithPaddingV2.storyName = '간격 있는 이미지 2개 V2'
+TwoImagesWithPaddingV2.args = {
+  propList: generateSampleImagesTwo('default-v2'),
+}
+
+type ImageDisply = 'default' | 'gapless-block' | 'default-v2'
+
+function generateSampleImagesTwo(type: ImageDisply) {
+  return [
     {
       value: {
-        display: 'default',
+        display: type,
         images: [IMAGE, IMAGE],
       },
     },
     {
       value: {
-        display: 'default',
+        display: type,
         images: [IMAGE_WITH_TITLE, IMAGE_WITH_TITLE],
       },
     },
     {
       value: {
-        display: 'default',
+        display: type,
         images: [IMAGE_WITH_LINK, IMAGE_WITH_LINK],
       },
     },
     {
       value: {
-        display: 'default',
+        display: type,
         images: [IMAGE_WITH_TITLE_AND_LINK, IMAGE_WITH_TITLE_AND_LINK],
       },
     },
-  ],
+  ]
+}
+
+function generateSampleImages(type: ImageDisply) {
+  return [
+    {
+      value: {
+        display: type,
+        images: [IMAGE],
+      },
+    },
+    {
+      value: {
+        display: type,
+        images: [IMAGE_WITH_TITLE],
+      },
+    },
+    {
+      value: {
+        display: type,
+        images: [IMAGE_WITH_LINK],
+      },
+    },
+    {
+      value: {
+        display: type,
+        images: [IMAGE_WITH_TITLE_AND_LINK],
+      },
+    },
+  ]
 }
