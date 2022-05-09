ca5ec86fc446d304aa6dca64e04c8b07ca7b423b
finn <finn@triple-corp.com>
2022-05-09 22:48:23 +0900

    fix: scrollToElement logic

--- packages/scroll-to-element/src/scroll-to-element.ts
+++ packages/scroll-to-element/src/scroll-to-element.ts
@@ -2,22 +2,18 @@ import { scroll } from './scroll'
 import { calculateScrollOffset } from './calculates'
 import { ScrollOptions } from './types'
 
-export function scrollToElement(elementId: string, options: ScrollOptions) {
-  setTimeout(() => {
-    const element = document.getElementById(elementId)
+export function scrollToElement(element: Element, options: ScrollOptions) {
+  if (!element) {
+    return
+  }
 
-    if (!element) {
-      return
-    }
-
-    scroll({
-      x: 0,
-      y: calculateScrollOffset({
-        element,
-        offset: options.offset,
-        alignment: options.align,
-      }),
-      options,
-    })
-  }, 1500)
+  return scroll({
+    x: 0,
+    y: calculateScrollOffset({
+      element,
+      offset: options.offset,
+      alignment: options.align,
+    }),
+    options,
+  })
 }
