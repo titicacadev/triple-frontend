a9efd29a4c2f5f31606e6591f262d2c80c7af80d
Gi Sung Park <lucian@triple-corp.com>
2022-05-09 12:02:10 +0900

    fix: fetchApi

--- packages/standard-action-handler/src/fetch-api.ts
+++ packages/standard-action-handler/src/fetch-api.ts
@@ -1,14 +1,18 @@
 import qs from 'qs'
 import { UrlElements } from '@titicaca/view-utilities'
-import { get } from '@titicaca/fetcher'
 
 export default async function fetchApi({ path, query }: UrlElements) {
   if (path === '/web-action/fetch-api' && query) {
-    const { path: apiPath, body } = qs.parse(query, {
+    const {
+      path: apiPath,
+      method,
+      body,
+    } = qs.parse(query, {
       ignoreQueryPrefix: true,
     })
 
-    await get(apiPath as string, {
+    await fetch(apiPath as string, {
+      method: (method as string) || 'GET',
       ...(body ? { headers: { 'content-type': 'application/json' } } : {}),
       body: body as string,
     })
