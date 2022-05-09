4bd4514b0c5ff77ce80930f86889c493050c0aba
finn <finn@triple-corp.com>
2022-05-09 18:51:54 +0900

    fix: 비구조화 할당 문법 수정

--- packages/scroll-to-element/src/scroll-to-element.ts
+++ packages/scroll-to-element/src/scroll-to-element.ts
@@ -1,13 +1,7 @@
 import { scroll, calculateScrollOffset } from './scroll'
 import { ScrollOptions } from './types'
 
-export function scrollToElement({
-  elementId,
-  options,
-}: {
-  elementId: string
-  options: ScrollOptions
-}) {
+export function scrollToElement(elementId: string, options: ScrollOptions) {
   setTimeout(() => {
     const element = document.getElementById(elementId)
 
