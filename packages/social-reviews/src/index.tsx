371a165c7f5327bec0ec2ee981dc554690b95c8d
Kim Kyudeok <royd@triple-corp.com>
2022-05-09 18:15:09 +0900

    SocialReviews 컴포넌트에서 ExternalLinks 컴포넌트 사용

--- packages/social-reviews/src/index.tsx
+++ packages/social-reviews/src/index.tsx
@@ -1,20 +1,9 @@
-import styled from 'styled-components'
-import {
-  Section,
-  Container,
-  List,
-  Text,
-  Image,
-  H1,
-  H3,
-} from '@titicaca/core-elements'
+import { Section } from '@titicaca/core-elements'
 import { useEventTrackingContext } from '@titicaca/react-contexts'
 import { useI18n } from '@titicaca/i18n'
 import { useNavigate } from '@titicaca/router'
 
-const SocialReviewEntry = styled(List.Item)`
-  cursor: pointer;
-`
+import { ExternalLinks } from './external-links'
 
 export interface SocialReview {
   imageUrl?: string
@@ -42,58 +31,31 @@ export default function SocialReviews({
   }
 
   return (
-    <Section anchor="external-links" {...props}>
-      <H1>{t('common:externalLinks', '소셜 리뷰')}</H1>
-      <List divided clearing margin={{ top: 10 }}>
-        {socialReviews.map(
-          ({ imageUrl, publisher, title, url }: SocialReview, i) => (
-            <SocialReviewEntry
-              key={`external-link-${i}`}
-              minHeight={106}
-              onClick={() => {
-                trackSimpleEvent({ action: '소셜리뷰선택' })
-                navigate(url, { title })
-              }}
-            >
-              <Container
-                floated="right"
-                width={60}
-                margin={{ left: 20 }}
-                padding={{ top: 20, bottom: 20 }}
-              >
-                <Image borderRadius={4}>
-                  <Image.FixedRatioFrame frame="big">
-                    <Image.Img
-                      src={
-                        imageUrl &&
-                        `/api/images/cast?url=${encodeURIComponent(
-                          imageUrl,
-                        )}&transformation=${encodeURIComponent(
-                          'c_fill,f_auto,q_auto,h_256,w_256',
-                        )}${
-                          placeholderImageUrl
-                            ? `&placeholder=${encodeURIComponent(
-                                placeholderImageUrl,
-                              )}`
-                            : ''
-                        }`
-                      }
-                      alt={`${title} 썸네일`}
-                    />
-                  </Image.FixedRatioFrame>
-                </Image>
-              </Container>
-
-              <Container padding={{ top: 20 }}>
-                <H3 maxLines={2}>{title}</H3>
-                <Text size="small" alpha={0.5} margin={{ top: 8 }} ellipsis>
-                  {publisher}
-                </Text>
-              </Container>
-            </SocialReviewEntry>
-          ),
-        )}
-      </List>
-    </Section>
+    <ExternalLinks
+      title={t('common:externalLinks', '소셜 리뷰')}
+      externalLinks={socialReviews.map(
+        ({ imageUrl, publisher: summary, title, url }) => ({
+          url,
+          title,
+          summary,
+          imageUrl:
+            imageUrl &&
+            `/api/images/cast?url=${encodeURIComponent(
+              imageUrl,
+            )}&transformation=${encodeURIComponent(
+              'c_fill,f_auto,q_auto,h_256,w_256',
+            )}${
+              placeholderImageUrl
+                ? `&placeholder=${encodeURIComponent(placeholderImageUrl)}`
+                : ''
+            }`,
+        }),
+      )}
+      onItemClick={(_, { url, title }) => {
+        trackSimpleEvent({ action: '소셜리뷰선택' })
+        navigate(url, { title })
+      }}
+      {...props}
+    />
   )
 }
