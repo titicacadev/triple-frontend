70fdab83bd16a172e04656fa44012caaf0fc2469
finn <finn@triple-corp.com>
2022-05-10 11:39:39 +0900

    feat: scroll-to-element handler 추가

---
+++ packages/standard-action-handler/src/scroll-to-element.ts
@@ -0,0 +1,20 @@
+import qs from 'qs'
+import { UrlElements } from '@titicaca/view-utilities'
+import { scrollToElement as scrollTo } from '@titicaca/scroll-to-element'
+
+export default async function scrollToElement({ path, query }: UrlElements) {
+  if (path === '/web-action/scroll-to-element' && query) {
+    const { hash } = qs.parse(query) as {
+      hash?: string
+    }
+
+    if (hash) {
+      const element = document.getElementById(hash)
+      element && scrollTo(element, { offset: -52 })
+
+      return true
+    }
+  }
+
+  return false
+}
