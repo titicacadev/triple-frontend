ae936d92e1c2f4addf9868cf8670bd3bf95b8ebd
finn <finn@triple-corp.com>
2022-05-11 10:37:24 +0900

    delete: describe 블록 제거

--- packages/scroll-to-element/src/calculates.test.ts
+++ packages/scroll-to-element/src/calculates.test.ts
@@ -16,32 +16,30 @@ jest
   .spyOn(document.documentElement, 'clientHeight', 'get')
   .mockImplementation(() => 300)
 
-describe('calculates', () => {
-  test('현재 스크롤의 위치 정보를 반환합니다.', () => {
-    const { top, left } = initialScrollPosition()
+test('현재 스크롤의 위치 정보를 반환합니다.', () => {
+  const { top, left } = initialScrollPosition()
 
-    expect(top).toEqual(100)
-    expect(left).toEqual(10)
-  })
+  expect(top).toEqual(100)
+  expect(left).toEqual(10)
+})
 
-  test('스크롤 오프셋을 반환합니다.', () => {
-    const element = document.createElement('div')
+test('스크롤 오프셋을 반환합니다.', () => {
+  const element = document.createElement('div')
 
-    element.getBoundingClientRect = jest.fn(() => ({
-      x: 0,
-      y: 0,
-      width: 120,
-      height: 120,
-      top: 900,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      toJSON: () => {},
-    }))
+  element.getBoundingClientRect = jest.fn(() => ({
+    x: 0,
+    y: 0,
+    width: 120,
+    height: 120,
+    top: 900,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    toJSON: () => {},
+  }))
 
-    const scrollOffset = calculateScrollOffset({ element, offset: -52 })
+  const scrollOffset = calculateScrollOffset({ element, offset: -52 })
 
-    expect(scrollOffset).toEqual(200)
-  })
+  expect(scrollOffset).toEqual(200)
 })
