df6a5d83d7f044c6695f2ea16112fd11ba92e12c
finn <finn@triple-corp.com>
2022-05-16 15:06:15 +0900

    feat: fluid-table marginMixin props 확장

--- packages/triple-email-document/src/common/fluid-table.ts
+++ packages/triple-email-document/src/common/fluid-table.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components'
+import { marginMixin, MarginPadding } from '@titicaca/core-elements'
 
-const FluidTable = styled.table`
+const FluidTable = styled.table<{ margin?: MarginPadding }>`
   width: 100%;
   border-collapse: collapse;
+
+  ${marginMixin}
 `
 
 export default FluidTable
