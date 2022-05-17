c94cb25e0b2de34ad19f90cbaedb36da7d05ebb7
finn <finn@triple-corp.com>
2022-05-17 16:50:11 +0900

    delete: EmailFooter Component 제거

--- packages/triple-email-document/src/index.ts
+++ packages/triple-email-document/src/index.ts
@@ -3,7 +3,7 @@ import { TripleEmailDocument } from './triple-email-document'
 export { default as ELEMENTS } from './elements'
 export { TripleEmailElementData as TripleEmailDocumentElement } from './elements'
 export { ExtendedImageMeta, ImageDocument } from './elements/images'
-export { EmailFooter, EmailPreview, PreviewDocument } from './components'
+export { EmailPreview, PreviewDocument } from './components'
 export { default as FullEmailTemplate } from './full-email-template'
 
 export default TripleEmailDocument
