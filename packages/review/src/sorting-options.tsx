a97ee86739e06bf3f951c429a9d81697a05fd446
Gi Sung Park <lucian@triple-corp.com>
2022-06-15 10:42:29 +0900

    feat: 툴팁 및 최근 여행 체크박스 추가

--- packages/review/src/sorting-options.tsx
+++ packages/review/src/sorting-options.tsx
@@ -1,6 +1,6 @@
 import { SyntheticEvent } from 'react'
 import styled from 'styled-components'
-import { Label } from '@titicaca/core-elements'
+import { Label, FlexBox } from '@titicaca/core-elements'
 
 export interface SortingOptionsProps {
   onSelect: (e: SyntheticEvent, key: string) => void
@@ -15,9 +15,8 @@ const SORTING_OPTIONS = [
   { key: ORDER_BY_RECENCY, text: '최신순' },
 ]
 
-const OptionsContainer = styled.div`
-  float: right;
-  margin: 0;
+const OptionsContainer = styled(FlexBox)`
+  margin-top: 6px;
   padding: 0;
 
   div:not(:first-child) {
@@ -30,7 +29,7 @@ export default function SortingOptions({
   selected,
 }: SortingOptionsProps) {
   return (
-    <OptionsContainer>
+    <OptionsContainer flex alignItems="center">
       {SORTING_OPTIONS.map(({ key, text }) => (
         <Label key={key} radio selected={selected === key}>
           {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
