8b6f4ed560f556f8cc94b4ef0e591e3bdfcbc518
Gi Sung Park <lucian@triple-corp.com>
2022-06-15 10:51:46 +0900

    fix: 스타일시트 수정

--- packages/review/src/recent-checkbox.tsx
+++ packages/review/src/recent-checkbox.tsx
@@ -37,23 +37,17 @@ const CheckBox = styled.input`
 
 const TooltipInfo = styled(Text)`
   position: absolute;
-  min-width: 200px;
-  min-height: 60px;
-  border: 1px solid var(--color-brightGray);
   top: 101px;
   right: 0;
   bottom: 0;
   z-index: 1;
-  transform: translateY(calc(-100% - 10px));
-  font-weight: 500;
-  font-size: 12px;
-  line-height: 16px;
-  color: var(--color-gray800);
-  background: white;
-  box-sizing: border-box;
-  box-shadow: 0 0 15px rgba(0, 0, 0, 0.05);
+  background: var(--color-white);
+  min-width: 200px;
+  min-height: 60px;
+  border: 1px solid var(--color-brightGray);
   border-radius: 8px;
-  padding: 15px 37px 15px 15px;
+  transform: translateY(calc(-100% - 10px));
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.05);
 `
 
 const OpenIcon = styled.img`
@@ -93,7 +87,12 @@ function ToolTip() {
         onClick={() => setIsVisible(true)}
       />
       {isVisible ? (
-        <TooltipInfo>
+        <TooltipInfo
+          size={12}
+          lineHeight="16px"
+          color="gray800"
+          padding={{ top: 15, left: 15, bottom: 15, right: 37 }}
+        >
           최근 6개월 내에 방문한 여행의 리뷰만 모아 볼 수 있습니다.
           <CloseIcon
             width={10}
