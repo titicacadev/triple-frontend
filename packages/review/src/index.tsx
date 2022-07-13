1bf0938683ca4491bc0d650861caa1f72fd5cec0
Gi Sung Park <lucian@triple-corp.com>
2022-07-13 15:41:59 +0900

    delete: QueryProvider export 제거

--- packages/review/src/index.tsx
+++ packages/review/src/index.tsx
@@ -1,5 +1,4 @@
 export { default as Reviews } from './components/review-container'
-export { default as QueryProvider } from './components/query-provider'
 
 export * from './components/review-likes-context'
 export { default as ReviewsPlaceholder } from './components/review-placeholder-with-rating'
