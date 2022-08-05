d33dc237e4fb465d0652886103502250298b2e60
Heeryong Kang <drakang4@gmail.com>
2022-08-05 16:00:17 +0900

    feat(review): export ReviewElement

--- packages/review/src/index.tsx
+++ packages/review/src/index.tsx
@@ -1,4 +1,5 @@
 export { default as Reviews } from './components/review-container'
+export { default as ReviewElement } from './components/review-element'
 
 export * from './components/review-likes-context'
 export { default as ReviewsPlaceholder } from './components/review-placeholder-with-rating'
