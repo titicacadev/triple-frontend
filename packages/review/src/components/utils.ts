a4905d02855d90c3289f1711fccc64888c479ca8
jaehyeon48 <imjaehyeon48@gmail.com>
2022-08-16 14:01:51 +0900

    리뷰데이터를 번역된 리뷰데이터로 변환하는 함수 추가
    성공적으로 번역된 리뷰의 경우 리뷰객체의 comment 프로퍼티값이 번역된 리뷰로 변환되고,
    번역에 실패한 리뷰의 경우 변환되지 않고 기존의 리뷰객체가 그대로 유지됩니다.

--- packages/review/src/components/utils.ts
+++ packages/review/src/components/utils.ts
@@ -1,9 +1,23 @@
-import { TranslatedReviewResult } from './types'
+import { ReviewData, TranslatedReviewResult } from './types'
 
 export function makeSuccessfullyTranslatedReviews(
   translatedReviews: TranslatedReviewResult[],
 ) {
   return new Map<string, string>(
     translatedReviews.map(({ id, translated }) => [id, translated]),
+  )
+}
+
+export function convertReviewsToTranslatedReviews(
+  reviews: ReviewData[],
+  successfullyTranslatedReviews: Map<string, string>,
+) {
+  return reviews.map((review) =>
+    successfullyTranslatedReviews.has(review.id)
+      ? {
+          ...review,
+          comment: successfullyTranslatedReviews.get(review.id) as string,
+        }
+      : review,
   )
 }
