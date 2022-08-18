52dbfec4a92cc456ce84e6f9db91db93f0e86909
Heeryong Kang <drakang4@gmail.com>
2022-08-18 16:01:24 +0900

    feat(reivew): 첫번째 비디오는 자동 재생

--- packages/review/src/components/review-element/media/medium.tsx
+++ packages/review/src/components/review-element/media/medium.tsx
@@ -5,13 +5,14 @@ import Video from './video'
 
 interface Props {
   medium: ImageMeta
+  index: number
 }
 
-function Medium({ medium }: Props) {
+function Medium({ medium, index }: Props) {
   const isVideo = medium.type === 'video'
 
   if (isVideo) {
-    return <Video medium={medium} />
+    return <Video medium={medium} autoPlay={index === 0} />
   }
 
   return <Image medium={medium} />
