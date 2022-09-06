2854dd1615e095630745acad6d40285fee47b56e
Heeryong Kang <drakang4@gmail.com>
2022-09-06 11:47:33 +0900

    feat(review): isIntersecting일 때 비디오 자동 재생

--- packages/review/src/components/review-element/media/video.tsx
+++ packages/review/src/components/review-element/media/video.tsx
@@ -1,22 +1,50 @@
 import { ImageMeta } from '@titicaca/type-definitions'
 import { Container, Video as TfVideo } from '@titicaca/core-elements'
+import { useIntersection } from '@titicaca/intersection-observer'
+import { useEffect } from 'react'
+import { useDeviceContext } from '@titicaca/react-contexts'
 
 interface Props {
   medium: ImageMeta
-  autoPlay?: boolean
 }
 
-function Video({ medium, autoPlay }: Props) {
+function Video({ medium }: Props) {
+  const { ref, isIntersecting } = useIntersection<HTMLVideoElement>({
+    threshold: 0.5,
+  })
+
+  const {
+    deviceState: { autoplay, networkType },
+  } = useDeviceContext()
+
+  const videoAutoplay =
+    autoplay === 'always' ||
+    (autoplay === 'wifi_only' && networkType === 'wifi')
+
+  useEffect(() => {
+    if (!videoAutoplay) {
+      return
+    }
+
+    if (isIntersecting) {
+      ref.current?.play()
+    } else {
+      ref.current?.pause()
+    }
+  }, [isIntersecting, videoAutoplay])
+
   return (
     <Container borderRadius={6}>
       <TfVideo
         removeFrame
-        autoPlay={autoPlay}
         frame="medium"
         src={medium.video?.large.url}
         fallbackImageUrl={medium.sizes.large.url}
         cloudinaryBucket={medium.cloudinaryBucket}
         cloudinaryId={medium.cloudinaryId}
+        muted
+        initialControlHidden
+        ref={ref}
       />
     </Container>
   )
