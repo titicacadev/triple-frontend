571de2ddb8840635ae5c7a44fe9ea405f24fed15
Heeryong Kang <drakang4@gmail.com>
2022-09-21 14:02:46 +0900

    feat: image-carousel, review에서 core-elements video를 사용하지 않도록 변경

--- packages/review/src/components/review-element/media/video.tsx
+++ packages/review/src/components/review-element/media/video.tsx
@@ -1,12 +1,45 @@
 import { ImageMeta } from '@titicaca/type-definitions'
-import { Container, Video as TfVideo } from '@titicaca/core-elements'
+import { Container } from '@titicaca/core-elements'
 import { useIntersection } from '@titicaca/intersection-observer'
 import { useEffect } from 'react'
 import { useDeviceContext } from '@titicaca/react-contexts'
+import styled from 'styled-components'
 
 interface Props {
   medium: ImageMeta
 }
+
+const StyledVideo = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+
+const PLAY_BUTTON_IMAGE_URL =
+  'https://assets.triple.guide/images/btn-video-play@3x.png'
+
+const PlayPauseButtonBase = styled.button`
+  position: absolute;
+  border: none;
+  background: none;
+  width: 60px;
+  height: 60px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-image: url(${PLAY_BUTTON_IMAGE_URL});
+  background-size: cover;
+
+  &:focus {
+    outline: none;
+  }
+  transition: opacity 0.3s;
+`
 
 function Video({ medium }: Props) {
   const { ref, isIntersecting } = useIntersection<HTMLVideoElement>({
@@ -35,16 +68,16 @@ function Video({ medium }: Props) {
 
   return (
     <Container borderRadius={6}>
-      <TfVideo
-        removeFrame
-        frame="medium"
-        src={medium.video?.large.url}
-        fallbackImageUrl={medium.sizes.large.url}
-        muted
-        hideControls
-        initialControlsHidden={videoAutoplay}
+      <StyledVideo
         ref={ref}
+        src={medium.video?.large.url}
+        controls={false}
+        loop
+        muted
+        playsInline
+        poster={medium.sizes.large.url}
       />
+      {!videoAutoplay && <PlayPauseButtonBase disabled />}
     </Container>
   )
 }
