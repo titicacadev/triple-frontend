79e63ac8cc66c6d2006af8c37773d5b5df25e608
Heeryong Kang <drakang4@gmail.com>
2022-09-22 13:48:00 +0900

    fix: poster 복구

--- packages/review/src/components/review-element/media/video.tsx
+++ packages/review/src/components/review-element/media/video.tsx
@@ -75,6 +75,7 @@ function Video({ medium }: Props) {
         loop
         muted
         playsInline
+        poster={medium.sizes.large.url}
       />
       {!videoAutoplay && <PlayPauseButtonBase disabled />}
     </Container>
