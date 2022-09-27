8c9b8e2d2727aea24a1cea772362606e5245cc21
hoolie <hoolie@triple-corp.com>
2022-09-27 17:17:01 +0900

    Revert "fix: apply codemod"
    This reverts commit 56592afb16a8027c8216af15722bc61fe0a431b2.

--- packages/nearby-pois/src/nearby-pois.tsx
+++ packages/nearby-pois/src/nearby-pois.tsx
@@ -143,13 +143,7 @@ export default function NearbyPois({
   )
 
   return (
-    <Section
-      anchor="nearby-pois"
-      {...props}
-      css={{
-        minHeight: 404,
-      }}
-    >
+    <Section anchor="nearby-pois" minHeight={404} {...props}>
       <H1 margin={{ bottom: 20 }}>
         {t('common:nearbyPois', '근처의 추천 장소')}
       </H1>
