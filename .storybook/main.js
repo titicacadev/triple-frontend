494350d7692c119bab6504d920e0a8061d49e673
Heeryong Kang <drakang4@gmail.com>
2022-09-29 17:48:30 +0900

    chore: root에 storybook 추가

---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-addon-next-router',
+    'storybook-addon-swc',
+  ],
+  typescript: {
+    check: true,
+  },
+  framework: '@storybook/react',
+  core: {
+    builder: {
+      name: 'webpack5',
+      options: {
+        lazyCompilation: true,
+        fsCache: true,
+      },
+    },
+  },
+  features: {
+    postcss: false,
+    storyStoreV7: true,
+  },
+}
