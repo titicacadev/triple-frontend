226fa4c8f26dbf33041768f03871ed908e21fc17
Heeryong Kang <drakang4@gmail.com>
2022-09-30 14:15:18 +0900

    chore: storybook-next-router-addon 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,8 @@
+import { RouterContext } from 'next/dist/shared/lib/router-context' // next 12
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  nextRouter: {
+    Provider: RouterContext.Provider,
   },
 }
