14ea3e7dc056b2b5b0d7666c6665464a52a5ed2e
Heeryong Kang <drakang4@gmail.com>
2022-10-06 16:33:32 +0900

    refactor: iterable에 spread operator 대신 Array.from 사용

--- packages/review/src/components/review-container.tsx
+++ packages/review/src/components/review-container.tsx
@@ -165,7 +165,7 @@ function ReviewContainer({
     (review) =>
       setMyReviewStatus(([, ids]) => [
         review,
-        review ? new Set<string>([String(review.id), ...ids]) : ids,
+        review ? new Set<string>([String(review.id), ...Array.from(ids)]) : ids,
       ]),
     [setMyReviewStatus],
   )
