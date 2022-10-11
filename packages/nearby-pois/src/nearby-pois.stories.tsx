82df70d01dee12f251be43db30e739e629f02653
Heeryong Kang <drakang4@gmail.com>
2022-10-11 19:52:54 +0900

    chore(nearby-pois): 중복 스토리 제거

--- packages/nearby-pois/src/nearby-pois.stories.tsx
+++ packages/nearby-pois/src/nearby-pois.stories.tsx
@@ -1,11 +1,11 @@
-import { ComponentStoryObj, Meta } from '@storybook/react'
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
 
 import NearbyPois from './nearby-pois'
 
 export default {
   title: 'Nearby-Pois / NearbyPois',
   component: NearbyPois,
-} as Meta
+} as ComponentMeta<typeof NearbyPois>
 
 export const Basic: ComponentStoryObj<typeof NearbyPois> = {
   args: {
@@ -18,13 +18,14 @@ export const Basic: ComponentStoryObj<typeof NearbyPois> = {
   },
 }
 
-export const WithRecommended: ComponentStoryObj<typeof NearbyPois> = {
-  args: {
-    poiId: 'a86a3f55-9f89-4540-a124-f8c4db07ab34',
-    geolocation: {
-      type: 'Point',
-      coordinates: [125.50129726256557, 34.668727308992935],
-    },
-    regionId: '71476976-cf9a-4ae8-a60f-76e6fb26900d',
-  },
-}
+// Basic과 동일한데 히스토리를 모르겠음
+// export const WithRecommended: ComponentStoryObj<typeof NearbyPois> = {
+//   args: {
+//     poiId: 'a86a3f55-9f89-4540-a124-f8c4db07ab34',
+//     geolocation: {
+//       type: 'Point',
+//       coordinates: [125.50129726256557, 34.668727308992935],
+//     },
+//     regionId: '71476976-cf9a-4ae8-a60f-76e6fb26900d',
+//   },
+// }
