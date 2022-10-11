df5d7c93fa56087e6183aca8a1d62888bbfd3a5c
Heeryong Kang <drakang4@gmail.com>
2022-10-11 19:49:20 +0900

    docs(booking-completion): 스토리 종류 추가

--- packages/booking-completion/src/booking-completion.stories.tsx
+++ packages/booking-completion/src/booking-completion.stories.tsx
@@ -8,17 +8,29 @@ export default {
 } as Meta
 
 export const Basic: ComponentStoryObj<typeof BookingCompletion> = {
+  args: {},
+}
+
+export const Compact: ComponentStoryObj<typeof BookingCompletion> = {
   args: {
-    title: '발권이\n완료되었습니다.',
+    compact: true,
+  },
+}
+
+export const WithDescriptions: ComponentStoryObj<typeof BookingCompletion> = {
+  args: {
     descriptions: [
       '공급사 확인 후 예약이 확정됩니다.',
       '예약이 확정되면 이메일로 바우처가 발송됩니다.',
     ],
+  },
+}
+
+export const WithRegion: ComponentStoryObj<typeof BookingCompletion> = {
+  args: {
     region: {
       id: '',
       names: { ko: '바르셀로나', en: 'Barcelona', local: null },
     },
-    compact: false,
-    myBookingButtonTitle: '',
   },
 }
