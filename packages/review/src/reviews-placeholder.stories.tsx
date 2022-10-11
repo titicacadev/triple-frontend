799278205e95302b1d86984194fd9ed16b64fce6
Heeryong Kang <drakang4@gmail.com>
2022-10-11 19:56:00 +0900

    docs(review): 스토리 종류 보완

--- packages/review/src/reviews-placeholder.stories.tsx
+++ packages/review/src/reviews-placeholder.stories.tsx
@@ -1,15 +1,53 @@
-import { Meta, StoryObj } from '@storybook/react'
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
 
 import ReviewsPlaceholder from './components/review-placeholder-with-rating'
 
 export default {
   title: 'Review / Review Placeholder',
   component: ReviewsPlaceholder,
-} as Meta
+} as ComponentMeta<typeof ReviewsPlaceholder>
 
-export const Basic: StoryObj = {
-  name: '플레이스홀더',
+export const Basic: ComponentStoryObj<typeof ReviewsPlaceholder> = {
   args: {
     resourceType: 'tna',
+    recentTrip: false,
+    hasReviews: false,
+    isMorePage: false,
+  },
+}
+
+export const Article: ComponentStoryObj<typeof ReviewsPlaceholder> = {
+  args: {
+    resourceType: 'article',
+    recentTrip: false,
+    hasReviews: false,
+    isMorePage: false,
+  },
+}
+
+export const RecentTrip: ComponentStoryObj<typeof ReviewsPlaceholder> = {
+  args: {
+    resourceType: 'tna',
+    recentTrip: true,
+    hasReviews: false,
+    isMorePage: false,
+  },
+}
+
+export const HasReviews: ComponentStoryObj<typeof ReviewsPlaceholder> = {
+  args: {
+    resourceType: 'tna',
+    recentTrip: true,
+    hasReviews: true,
+    isMorePage: false,
+  },
+}
+
+export const IsMorePage: ComponentStoryObj<typeof ReviewsPlaceholder> = {
+  args: {
+    resourceType: 'tna',
+    recentTrip: true,
+    hasReviews: false,
+    isMorePage: true,
   },
 }
