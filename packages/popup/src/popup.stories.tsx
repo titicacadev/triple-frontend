1e0419b2254ea399c2353d6e08323d97e4b32d72
Heeryong Kang <drakang4@gmail.com>
2022-10-11 19:55:06 +0900

    docs(popup): 스토리 종류 보완

--- packages/popup/src/popup.stories.tsx
+++ packages/popup/src/popup.stories.tsx
@@ -1,3 +1,4 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
 import styled from 'styled-components'
 
 import Popup from '.'
@@ -5,69 +6,70 @@ import Popup from '.'
 export default {
   title: 'popup / Popup',
   component: Popup,
-}
+  parameters: {
+    docs: {
+      inlineStories: false,
+      iframeHeight: 500,
+    },
+  },
+} as ComponentMeta<typeof Popup>
 
 const EmptyScroll = styled.div`
   height: 200vh;
 `
 
-export const Basic = () => {
-  return (
-    <Popup
-      title="테스트"
-      noNavbar={false}
-      borderless
-      open={false}
-      icon="close"
-      onClose={() => {}}
-    >
-      <EmptyScroll>Scroll........</EmptyScroll>
-    </Popup>
-  )
+export const Basic: ComponentStoryObj<typeof Popup> = {
+  args: {
+    open: true,
+    title: '테스트',
+    children: <EmptyScroll>Scroll........</EmptyScroll>,
+  },
 }
-Basic.storyName = '일반'
 
-export const AfterActionSheet = () => {
-  return (
-    <>
-      <Popup title="팝업입니다" open onClose={() => {}}>
-        <EmptyScroll>Scroll........</EmptyScroll>
-      </Popup>
-
-      {/* <ActionSheet open={false} title="샘플 액션시트">
-        <ActionSheet.Item>메뉴 1</ActionSheet.Item>
-        <ActionSheet.Item>메뉴 2</ActionSheet.Item>
-      </ActionSheet> */}
-    </>
-  )
+export const Borderless: ComponentStoryObj<typeof Popup> = {
+  args: {
+    open: true,
+    borderless: true,
+    title: '테스트',
+    children: <EmptyScroll>Scroll........</EmptyScroll>,
+  },
 }
-AfterActionSheet.storyName = '팝업과 액션시트가 같은 계층에 있는 경우'
 
-export const WithInActionSheet = () => {
-  return (
-    <Popup title="팝업입니다" open onClose={() => {}}>
-      <EmptyScroll>Scroll........</EmptyScroll>
-      {/* <ActionSheet open={false} title="샘플 액션시트">
-        <ActionSheet.Item>메뉴 1</ActionSheet.Item>
-        <ActionSheet.Item>메뉴 2</ActionSheet.Item>
-      </ActionSheet> */}
-    </Popup>
-  )
+export const NoNavbar: ComponentStoryObj<typeof Popup> = {
+  args: {
+    open: true,
+    noNavbar: true,
+    title: '테스트',
+    children: <EmptyScroll>Scroll........</EmptyScroll>,
+  },
 }
-WithInActionSheet.storyName = '팝업 안에 액션시트가 있는 경우'
 
-export const Event = () => {
-  return (
-    <Popup
-      title="테스트"
-      noNavbar={false}
-      borderless
-      open={false}
-      icon="close"
-      onClose={() => {}}
-    >
-      다양한 이벤트가 있습니다.
-    </Popup>
-  )
-}
-Event.storyName = '팝업 이벤트'
+// TODO
+// export const AfterActionSheet = () => {
+//   return (
+//     <>
+//       <Popup title="팝업입니다" open onClose={() => {}}>
+//         <EmptyScroll>Scroll........</EmptyScroll>
+//       </Popup>
+
+//       <ActionSheet open={false} title="샘플 액션시트">
+//         <ActionSheet.Item>메뉴 1</ActionSheet.Item>
+//         <ActionSheet.Item>메뉴 2</ActionSheet.Item>
+//       </ActionSheet>
+//     </>
+//   )
+// }
+// AfterActionSheet.storyName = '팝업과 액션시트가 같은 계층에 있는 경우'
+
+// export const WithInActionSheet = () => {
+//   return (
+//     <Popup title="팝업입니다" open onClose={() => {}}>
+//       <EmptyScroll>Scroll........</EmptyScroll>
+//       <ActionSheet open={false} title="샘플 액션시트">
+//         <ActionSheet.Item>메뉴 1</ActionSheet.Item>
+//         <ActionSheet.Item>메뉴 2</ActionSheet.Item>
+//       </ActionSheet>
+//     </Popup>
+//   )
+// }
+// WithInActionSheet.storyName = '팝업 안에 액션시트가 있는 경우'
