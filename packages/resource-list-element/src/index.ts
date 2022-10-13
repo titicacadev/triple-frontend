a84905355f6a1e4ad1d8f772bd879382da4a464a
Heeryong Kang <drakang4@gmail.com>
2022-10-13 19:15:20 +0900

    chore(resource-list-element): stats 파일 이름 변경

--- packages/resource-list-element/src/index.ts
+++ packages/resource-list-element/src/index.ts
@@ -1,6 +1,6 @@
 export { default as ReviewScrapStat } from './review-scrap-stat'
 
-export { default as ResourceListElementStats } from './stats'
+export { default as ResourceListElementStats } from './resource-list-element-stats'
 
 export {
   default,
