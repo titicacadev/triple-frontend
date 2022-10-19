4775974e424dc7a4d6302fcea1b7f6a201426a77
Heeryong Kang <drakang4@gmail.com>
2022-10-19 14:16:03 +0900

    test: testing-library react-hooks -> react 변경

--- packages/ui-flow/src/use-app-callback.test.ts
+++ packages/ui-flow/src/use-app-callback.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react'
 import { useTripleClientMetadata } from '@titicaca/react-triple-client-interfaces'
 import { TransitionType, useTransitionModal } from '@titicaca/modals'
 
