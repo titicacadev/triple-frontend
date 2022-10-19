25fe91a5f687a179c563f5fa49c5b209b7c17924
Heeryong Kang <drakang4@gmail.com>
2022-10-19 15:56:39 +0900

    docs(core-elements): Accordion 주석 추가

--- packages/core-elements/src/elements/accordion/accordion.tsx
+++ packages/core-elements/src/elements/accordion/accordion.tsx
@@ -6,10 +6,19 @@ import { AccordionTitle } from './accordion-title'
 import { AccordionContext } from './context'
 
 export interface AccordionProps extends PropsWithChildren {
+  /**
+   * true일 때는 Content, false일 때는 Folded가 보입니다.
+   */
   active: boolean
+  /**
+   * active가 변경될 때 콜백
+   */
   onActiveChange?: () => void
 }
 
+/**
+ * 콘텐츠를 확장하고 축소할 수 있는 영역입니다.
+ */
 export const Accordion = ({
   children,
   active,
