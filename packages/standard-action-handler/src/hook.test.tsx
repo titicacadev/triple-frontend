d3f9a0515283cb1508b2b133a054536b8b1b652d
Heeryong Kang <drakang4@gmail.com>
2022-10-19 14:16:03 +0900

    test: testing-library react-hooks -> react 변경

--- packages/standard-action-handler/src/hook.test.tsx
+++ packages/standard-action-handler/src/hook.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react'
 
 import Handler from './handler'
 import { useStandardActionHandler } from './hook'
--- packages/ui-flow/src/use-session-callback.test.tsx
+++ packages/ui-flow/src/use-session-callback.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react'
 import { useLoginCtaModal } from '@titicaca/modals'
 import {
   useSessionAvailability,
