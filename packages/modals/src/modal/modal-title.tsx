5126b1c41b7a1ad2285d26d131eece6c65e76ebd
Heeryong Kang <drakang4@gmail.com>
2022-10-24 11:06:58 +0900

    feat(modals): Modal 컴포넌트를accessible 하도록 개선

---
+++ packages/modals/src/modal/modal-title.tsx
@@ -0,0 +1,21 @@
+import { PropsWithChildren } from 'react'
+import { Text } from '@titicaca/core-elements'
+
+import { useModal } from './modal-context'
+
+export const ModalTitle = ({ children, ...props }: PropsWithChildren) => {
+  const { titleId } = useModal()
+
+  return (
+    <Text
+      id={titleId}
+      bold
+      size="big"
+      color="gray"
+      css={{ marginBottom: 10 }}
+      {...props}
+    >
+      {children}
+    </Text>
+  )
+}
