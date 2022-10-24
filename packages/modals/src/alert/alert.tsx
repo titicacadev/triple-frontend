cc70fb023324bf4ce651e07d2f37f3eb51e2c4a6
Heeryong Kang <drakang4@gmail.com>
2022-10-24 11:10:01 +0900

    feat(modals): Alert 컴포넌트 추가

---
+++ packages/modals/src/alert/alert.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from 'react'
+
+import { Modal } from '../modal'
+
+export interface AlertProps {
+  children?: ReactNode
+  title?: string
+  open?: boolean
+  confirmText?: ReactNode
+  onClose?: () => void
+  onConfirm?: () => void
+}
+
+export const Alert = ({
+  children,
+  title,
+  open,
+  confirmText = '확인',
+  onClose,
+  onConfirm,
+}: AlertProps) => {
+  const handleConfirm = () => {
+    onConfirm?.()
+    onClose?.()
+  }
+
+  return (
+    <Modal open={open} onClose={onClose}>
+      <Modal.Body>
+        {title && <Modal.Title>{title}</Modal.Title>}
+        {children && <Modal.Description>{children}</Modal.Description>}
+      </Modal.Body>
+      <Modal.Actions>
+        <Modal.Action color="gray" onClick={handleConfirm}>
+          {confirmText}
+        </Modal.Action>
+      </Modal.Actions>
+    </Modal>
+  )
+}
