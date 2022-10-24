84bf27bbf47b68592e506e8272a182a9e7c3bcc2
Heeryong Kang <drakang4@gmail.com>
2022-10-24 11:06:58 +0900

    feat(modals): Modal 컴포넌트를accessible 하도록 개선

---
+++ packages/modals/src/modal/modal-action.tsx
@@ -0,0 +1,18 @@
+import { GlobalColors } from '@titicaca/core-elements'
+import styled from 'styled-components'
+
+const ACTION_COLORS: Partial<Record<GlobalColors, string>> = {
+  gray: 'rgba(58, 58, 58, 0.5)',
+  blue: '#368fff',
+}
+
+export const ModalAction = styled.a<{ color?: GlobalColors }>`
+  display: inline-block;
+  white-space: nowrap;
+  height: 50px;
+  line-height: 50px;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  color: ${({ color }) => ACTION_COLORS[color || 'gray']};
+`
