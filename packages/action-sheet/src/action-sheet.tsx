5b41257359f2d24f49d2491ea33135807f272bbf
Heeryong Kang <drakang4@gmail.com>
2022-10-24 19:49:02 +0900

    feat(action-sheet): ActionSheet 접근성 개선

---
+++ packages/action-sheet/src/action-sheet.tsx
@@ -0,0 +1,44 @@
+import { PropsWithChildren, ReactNode, useId } from 'react'
+
+import { ActionSheetBase } from './action-sheet-base'
+import { ActionSheetContext } from './action-sheet-context'
+import { ActionSheetItem } from './action-sheet-item'
+
+export interface ActionSheetProps extends PropsWithChildren {
+  open?: boolean
+  title?: ReactNode
+  borderRadius?: number
+  bottomSpaicng?: number
+  from?: 'top' | 'bottom'
+  maxContentHeight?: string | number
+  onClose?: () => void
+}
+
+export const ActionSheet = ({
+  children,
+  open = false,
+  title,
+  borderRadius = 12,
+  bottomSpaicng = 13,
+  from = 'bottom',
+  maxContentHeight = 'calc(100vh - 256px)',
+  onClose,
+}: ActionSheetProps) => {
+  const titleId = useId()
+
+  return (
+    <ActionSheetContext.Provider value={{ open, titleId, onClose }}>
+      <ActionSheetBase
+        borderRadius={borderRadius}
+        bottomSpacing={bottomSpaicng}
+        maxContentHeight={maxContentHeight}
+        from={from}
+        title={title}
+      >
+        {children}
+      </ActionSheetBase>
+    </ActionSheetContext.Provider>
+  )
+}
+
+ActionSheet.Item = ActionSheetItem
