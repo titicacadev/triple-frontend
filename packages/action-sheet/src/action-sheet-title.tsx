95e7a26a07659030022461f366c664ea25b6237c
Heeryong Kang <drakang4@gmail.com>
2022-10-24 19:49:02 +0900

    feat(action-sheet): ActionSheet 접근성 개선

---
+++ packages/action-sheet/src/action-sheet-title.tsx
@@ -0,0 +1,35 @@
+import { isValidElement, PropsWithChildren } from 'react'
+import { Container, Text } from '@titicaca/core-elements'
+
+import { useActionSheet } from './action-sheet-context'
+
+type ActionSheetTitleProps = PropsWithChildren
+
+export const ActionSheetTitle = ({ children }: ActionSheetTitleProps) => {
+  const { titleId } = useActionSheet()
+
+  if (
+    typeof children === 'string' ||
+    typeof children === 'number' ||
+    typeof children === 'boolean'
+  ) {
+    return (
+      <Container
+        css={{
+          height: '16px',
+          margin: '0 0 10px 27px',
+        }}
+      >
+        <Text id={titleId} size="tiny" bold color="gray700">
+          {children}
+        </Text>
+      </Container>
+    )
+  }
+
+  if (isValidElement(children)) {
+    return children
+  }
+
+  return null
+}
