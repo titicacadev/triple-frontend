d3fe3c4190c8b126e867b92466037690695ceb60
Heeryong Kang <drakang4@gmail.com>
2022-10-25 11:15:27 +0900

    test(action-sheet): 렌더 테스트 에러 수정

--- packages/action-sheet/src/action-sheet.test.tsx
+++ packages/action-sheet/src/action-sheet.test.tsx
@@ -6,7 +6,7 @@ import '@testing-library/jest-dom'
 
 describe('ActionSheet', () => {
   it('should render with no error.', () => {
-    render(<ActionSheet title="액션시트 제목" />)
+    render(<ActionSheet open title="액션시트 제목" />)
 
     expect(screen.getByRole('dialog')).toHaveTextContent('액션시트 제목')
   })
