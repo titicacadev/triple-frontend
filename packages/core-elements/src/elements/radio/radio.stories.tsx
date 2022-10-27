fa615612c834322ac8086804c012bda2b3338b80
Heeryong Kang <drakang4@gmail.com>
2022-10-27 11:13:36 +0900

    feat(core-elements): 접근성 개선된 radio, radio-group 컴포넌트 추가

---
+++ packages/core-elements/src/elements/radio/radio.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { useState } from 'react'
+
+import { Radio } from './radio'
+
+export default {
+  title: 'Core-Elements / Radio',
+  component: Radio,
+} as ComponentMeta<typeof Radio>
+
+export const Default: ComponentStoryObj<typeof Radio> = {
+  args: {
+    children: 'Radio',
+  },
+}
+
+export const Controlled = () => {
+  const [value, setValue] = useState<string>()
+
+  return (
+    <Radio
+      value="a"
+      checked={value === 'a'}
+      onChange={(value) => setValue(value)}
+    >
+      Option A
+    </Radio>
+  )
+}
