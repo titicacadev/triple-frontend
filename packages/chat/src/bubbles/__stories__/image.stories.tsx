8d5087e0b1fd58dd567f4ff315a2b2197db0ff6d
hoolie <hoolie@triple-corp.com>
2022-10-28 14:13:25 +0900

    fix: prettier error

--- packages/chat/src/bubbles/__stories__/image.stories.tsx
+++ packages/chat/src/bubbles/__stories__/image.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ComponentMeta, ComponentStory} from "@storybook/react";
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 import { ImageBubble } from '../image'
 
