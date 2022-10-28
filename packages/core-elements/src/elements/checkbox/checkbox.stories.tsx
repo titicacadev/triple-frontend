ea32b992eb75b6d1318ed2d9d2e2c923f9eb3572
Heeryong Kang <drakang4@gmail.com>
2022-10-28 12:45:55 +0900

    feat(core-elements): 접근성 지원되는 Checkbox 컴포넌트 추가

---
+++ packages/core-elements/src/elements/checkbox/checkbox.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { useState } from 'react'
+
+import { Checkbox } from './checkbox'
+
+export default {
+  component: Checkbox,
+} as ComponentMeta<typeof Checkbox>
+
+export const Default: ComponentStory<typeof Checkbox> = (args) => {
+  return <Checkbox {...args}>Option</Checkbox>
+}
+
+export const Controlled: ComponentStory<typeof Checkbox> = () => {
+  const [checked, setChecekd] = useState(false)
+
+  return (
+    <Checkbox
+      checked={checked}
+      onChange={(event) => setChecekd(event.target.checked)}
+    >
+      Option
+    </Checkbox>
+  )
+}
