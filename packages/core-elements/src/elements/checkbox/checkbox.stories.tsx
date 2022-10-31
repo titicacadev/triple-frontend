9c0a22d74c23fac87d77ed8a04984ff789bef7e7
Heeryong Kang <drakang4@gmail.com>
2022-10-31 14:50:32 +0900

    docs(core-elements): Checkbox, CheckboxGroup 스토리 title 변경

--- packages/core-elements/src/elements/checkbox/checkbox.stories.tsx
+++ packages/core-elements/src/elements/checkbox/checkbox.stories.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { Checkbox } from './checkbox'
 
 export default {
+  title: 'Core-Elements / Checkbox',
   component: Checkbox,
 } as ComponentMeta<typeof Checkbox>
 
