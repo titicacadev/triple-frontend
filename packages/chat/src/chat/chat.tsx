5207cae33d373ee1c630068025c27d8c8eb0eec9
hoolie <hoolie@triple-corp.com>
2022-11-01 12:10:31 +0900

    feat: add chat root component

--- packages/chat/src/chat/chat.tsx
+++ packages/chat/src/chat/chat.tsx
@@ -1,22 +1,78 @@
-import React, { useCallback, useEffect, useReducer, useRef } from 'react'
+import { IncomingMessage } from 'http'
 
-import { ChatReducer, initialChatState } from './reducer'
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+} from 'react'
+import { StaticIntersectionObserver as IntersectionObserver } from '@titicaca/intersection-observer'
 
-/*eslint-disable*/
-const Chat = () => {
+import {
+  HasUnreadOfRoomInterface,
+  MessageInterface,
+  MetaDataInterface,
+  UserInfoInterface,
+  UserType,
+} from '../types'
+import ChatBubble from '../chat-bubble'
+import { HiddenElement } from '../chat-bubble/elements'
+import { Polling } from '../utils'
+
+import { ChatActions, ChatReducer, initialChatState } from './reducer'
+import { ChatContext, ChatContextValue } from './chat-context'
+
+const FETCH_INTERVAL_SECS = 5
+const MINIMUM_INTERSECTING_TIME = 3000
+
+interface ChatProps extends ChatContextValue {
+  fetchPastMessages: (firstMessageId?: number) => Promise<MessageInterface[]>
+  displayTarget: UserType
+  userInfo: UserInfoInterface
+  postMessage?: () => Promise<boolean>
+  getMessages: (option: {
+    roomId: string
+    backward?: boolean
+    lastMessageId: number | string
+    req?: IncomingMessage
+  }) => Promise<MessageInterface[]>
+  getUnreadRoom?: (option: {
+    roomId: string
+    lastSeenMessageId: number
+  }) => Promise<HasUnreadOfRoomInterface>
+  roomId: string
+}
+
+const defaultOnImageBubbleClick = (imageInfos: MetaDataInterface[]) => {
+  window.open(imageInfos[0].originalUrl, '_blank')
+}
+
+const Chat = ({
+  fetchPastMessages,
+  displayTarget,
+  userInfo,
+  postMessage,
+  getMessages,
+  getUnreadRoom,
+  roomId,
+
+  textBubbleFontSize,
+  textBubbleMaxWidthOffset,
+  mediaUrlBase,
+  cloudinaryName,
+  onRichBubbleButtonBeforeRouting,
+  onImageBubbleClick = defaultOnImageBubbleClick,
+  onTextBubbleClick,
+}: ChatProps) => {
   const chatRoomRef = useRef<HTMLDivElement>(null)
 
   const [
-    {
-      messages,
-      hasPrevMessage,
-      scrollY,
-      otherUnreadInfo,
-      lastMessageId,
-      firstMessageId,
-    },
+    { messages, hasPrevMessage, scrollY, otherUnreadInfo, lastMessageId },
     dispatch,
   ] = useReducer(ChatReducer, initialChatState)
+
+  const fetchJob = useMemo(() => new Polling(FETCH_INTERVAL_SECS * 1000), [])
 
   const scrollDown = useCallback(() => {
     if (chatRoomRef.current && chatRoomRef.current.parentElement) {
@@ -35,7 +91,113 @@ const Chat = () => {
     }
   }, [scrollY])
 
-  return <div>TODO: chat list 불러오기</div>
+  useEffect(() => {
+    fetchJob.run(() => pollingFetchJob())
+    return () => {
+      fetchJob.stop()
+    }
+  }, [lastMessageId]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    dispatch({
+      action: ChatActions.INIT,
+      messages,
+      lastMessageId: lastMessageId as number,
+    })
+
+    window.setTimeout(() => {
+      scrollDown()
+    }, 0)
+  }, [messages, lastMessageId, scrollDown]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function fetchNewMessages(): Promise<MessageInterface[]> {
+    if (lastMessageId !== null && roomId) {
+      return getMessages({
+        roomId,
+        lastMessageId,
+      })
+    } else {
+      return []
+    }
+  }
+
+  async function pollingFetchJob() {
+    if (lastMessageId !== null && roomId && getUnreadRoom) {
+      const { hasUnread, others } = await getUnreadRoom({
+        roomId,
+        lastSeenMessageId: lastMessageId,
+      })
+
+      dispatch({
+        action: ChatActions.UPDATE,
+        otherUnreadInfo: others,
+      })
+
+      if (hasUnread) {
+        const newMessages = await fetchNewMessages()
+        dispatch({
+          action: ChatActions.NEW,
+          messages: newMessages,
+        })
+        scrollDown()
+      }
+    }
+  }
+
+  return (
+    <ChatContext.Provider
+      value={{
+        textBubbleFontSize,
+        textBubbleMaxWidthOffset,
+        mediaUrlBase,
+        cloudinaryName,
+        onRichBubbleButtonBeforeRouting,
+        onImageBubbleClick,
+        onTextBubbleClick,
+      }}
+    >
+      <IntersectionObserver
+        onChange={async ({ isIntersecting, time }) => {
+          if (
+            isIntersecting &&
+            time >= MINIMUM_INTERSECTING_TIME &&
+            hasPrevMessage
+          ) {
+            const pastMessages = await fetchPastMessages(messages[0].id)
+
+            const prevScrollY = chatRoomRef.current
+              ? chatRoomRef.current.getBoundingClientRect().height
+              : 0
+
+            await dispatch({
+              action: ChatActions.PAST,
+              messages: pastMessages,
+              scrollY: prevScrollY,
+            })
+          }
+        }}
+      >
+        <HiddenElement />
+      </IntersectionObserver>
+      <div ref={chatRoomRef}>
+        <ul>
+          {messages.map((message: MessageInterface, index) => (
+            <li key={index}>
+              {userInfo ? (
+                <ChatBubble
+                  displayTarget={displayTarget}
+                  message={message}
+                  userInfo={userInfo}
+                  postMessage={postMessage}
+                  otherReadInfo={otherUnreadInfo}
+                />
+              ) : null}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </ChatContext.Provider>
+  )
 }
 
 export default Chat
