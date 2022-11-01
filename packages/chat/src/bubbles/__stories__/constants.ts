227dc3b756418feb90140a96ba10fff1d977ed2f
hoolie <hoolie@triple-corp.com>
2022-11-01 12:11:03 +0900

    docs: fix chat story

--- packages/chat/src/bubbles/__stories__/constants.ts
+++ packages/chat/src/bubbles/__stories__/constants.ts
@@ -1,5 +1,7 @@
 import { MetaDataInterface } from '../../types'
 
+import { ChatContextValue } from '@titicaca/chat'
+
 export const SAMPLE_IMAGES: MetaDataInterface[] = [
   {
     cloudinaryId: '70c9db60-cd42-49c3-b6a8-274318695cc2',
@@ -22,7 +24,14 @@ export const SAMPLE_IMAGES: MetaDataInterface[] = [
   },
 ]
 
-export const MEDIA_ARGS = {
+const empty = () => {}
+
+export const MEDIA_ARGS: ChatContextValue = {
   cloudinaryName: 'triple-cms',
   mediaUrlBase: 'https://media.triple.guide',
+  textBubbleFontSize: 'medium',
+  textBubbleMaxWidthOffset: 100,
+  onRichBubbleButtonBeforeRouting: empty,
+  onImageBubbleClick: empty,
+  onTextBubbleClick: empty,
 }
