d7449ebdccbd80a1b0ebadc8c4c0cafacaadf7c0
Heeryong Kang <drakang4@gmail.com>
2022-11-04 13:00:44 +0900

    style: custom-property-pattern 수정

--- packages/core-elements/src/global-style.ts
+++ packages/core-elements/src/global-style.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+  /* stylelint-disable custom-property-pattern */
   :root {
     --color-gray: rgba(58, 58, 58, 1);
     --color-gray20: rgba(58, 58, 58, 0.02);
