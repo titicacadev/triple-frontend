ed71af0c951e99fb97de4ec0c7b97b004b42a03f
Heeryong Kang <drakang4@gmail.com>
2022-11-04 11:39:08 +0900

    style: function-whitespace-after 수정

--- packages/triple-email-document/src/common/box.ts
+++ packages/triple-email-document/src/common/box.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { MarginPadding } from '@titicaca/core-elements'
 
 const Box = styled.td<{ padding?: MarginPadding }>`
+  /* stylelint-disable function-whitespace-after */
   padding: ${({ padding }) => {
     const { top, bottom, left, right } = padding || {}
     return [top, right, bottom, left].map(addUnit).join(' ')
