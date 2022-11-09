994ccc54f1a84ac03236964423d1f1f2935ff468
Heeryong Kang <drakang4@gmail.com>
2022-11-09 11:09:38 +0900

    feat(core-elements): RadioBase 추출

--- packages/core-elements/src/elements/radio/radio.tsx
+++ packages/core-elements/src/elements/radio/radio.tsx
@@ -1,9 +1,9 @@
-import { blue, gray200 } from '@titicaca/color-palette'
 import { ChangeEventHandler, PropsWithChildren } from 'react'
 import styled from 'styled-components'
 
 import Text from '../text'
 
+import { RadioBase, RadioBaseProps } from './radio-base'
 import { useRadioGroup } from './radio-group-context'
 
 const RadioLabel = styled.label`
@@ -20,40 +20,7 @@ const RadioText = styled(Text)`
   flex: 1;
 `
 
-const RadioInput = styled.input`
-  appearance: none;
-  position: relative;
-  width: 26px;
-  height: 26px;
-  border: 1px solid ${gray200};
-  border-radius: 50%;
-
-  &::after {
-    content: '';
-    display: block;
-    position: absolute;
-    top: 7px;
-    left: 7px;
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    background-color: ${blue};
-    opacity: 0;
-    transition: opacity 0.3s ease;
-  }
-
-  &:checked::after {
-    opacity: 1;
-  }
-`
-
-export interface RadioProps extends PropsWithChildren {
-  name?: string
-  checked?: boolean
-  tabIndex?: number
-  value?: string
-  onChange?: (value: string) => void
-}
+export type RadioProps = RadioBaseProps & PropsWithChildren
 
 export const Radio = ({
   children,
@@ -65,15 +32,17 @@ export const Radio = ({
   const group = useRadioGroup()
 
   const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    const handler = onChange ?? group?.onChange
-    handler?.(event.target.value)
+    if (group) {
+      group.onChange?.(event.target.value)
+    } else {
+      onChange?.(event)
+    }
   }
 
   return (
     <RadioLabel>
       <RadioText size="large">{children}</RadioText>
-      <RadioInput
-        type="radio"
+      <RadioBase
         name={name ?? group?.name}
         checked={checked ?? group?.value === value}
         value={value}
