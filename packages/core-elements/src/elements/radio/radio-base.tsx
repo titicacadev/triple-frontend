50a5c7b13b19fd63b9bbfdfa6b0d05672d2cab49
Heeryong Kang <drakang4@gmail.com>
2022-11-09 11:09:38 +0900

    feat(core-elements): RadioBase 추출

---
+++ packages/core-elements/src/elements/radio/radio-base.tsx
@@ -0,0 +1,54 @@
+import { ChangeEventHandler } from 'react'
+import styled from 'styled-components'
+import { blue, gray200 } from '@titicaca/color-palette'
+
+const RadioInput = styled.input`
+  appearance: none;
+  position: relative;
+  width: 26px;
+  height: 26px;
+  border: 1px solid ${gray200};
+  border-radius: 50%;
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 7px;
+    left: 7px;
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: ${blue};
+    opacity: 0;
+    transition: opacity 0.3s ease;
+  }
+
+  &:checked::after {
+    opacity: 1;
+  }
+`
+
+export interface RadioBaseProps {
+  name?: string
+  checked?: boolean
+  value?: string
+  onChange?: ChangeEventHandler<HTMLInputElement>
+}
+
+export const RadioBase = ({
+  name,
+  checked,
+  value,
+  onChange,
+}: RadioBaseProps) => {
+  return (
+    <RadioInput
+      type="radio"
+      name={name}
+      checked={checked}
+      value={value}
+      onChange={onChange}
+    />
+  )
+}
