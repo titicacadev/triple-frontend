3943ce2c6cd22c787547ceac5c91437475eda94c
hoolie <hoolie@triple-corp.com>
2022-11-11 16:23:46 +0900

    fix: document not defined error

--- packages/core-elements/src/elements/portal/portal.tsx
+++ packages/core-elements/src/elements/portal/portal.tsx
@@ -2,13 +2,20 @@ import { PropsWithChildren, useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 
 export const Portal = ({ children }: PropsWithChildren) => {
-  const [container] = useState(() => {
-    const element = document.createElement('div')
-    element.classList.add('triple-portal')
-    return element
-  })
+  const [container, setContainer] = useState<HTMLDivElement | null>(null)
 
   useEffect(() => {
+    setContainer(() => {
+      const element = document.createElement('div')
+      element.classList.add('triple-portal')
+      return element
+    })
+  }, [])
+
+  useEffect(() => {
+    if (container === null) {
+      return
+    }
     document.body.appendChild(container)
 
     return () => {
@@ -16,5 +23,5 @@ export const Portal = ({ children }: PropsWithChildren) => {
     }
   }, [container])
 
-  return createPortal(children, container)
+  return container ? createPortal(children, container) : null
 }
