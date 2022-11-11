a4dced4b5b4510be2f55bb961dc6573a35c6870a
Heeryong Kang <drakang4@gmail.com>
2022-11-11 15:30:15 +0900

    feat(core-elements): CheckboxGroup 컴포넌트에 FormField 기능 추가

--- packages/core-elements/src/elements/checkbox/checkbox-group.tsx
+++ packages/core-elements/src/elements/checkbox/checkbox-group.tsx
@@ -1,12 +1,55 @@
 import { PropsWithChildren } from 'react'
 
-import { CheckboxGroupContext } from './checkbox-group-context'
+import {
+  FormField,
+  FormFieldError,
+  FormFieldHelp,
+  FormFieldLabel,
+  useFormField,
+} from '../form-field'
 
-export interface CheckboxGroupProps extends PropsWithChildren {
-  name?: string
-  defaultValue?: string[]
-  value?: string[]
-  onChange?: (value: string[]) => void
+import {
+  CheckboxGroupContext,
+  CheckboxGroupContextValue,
+} from './checkbox-group-context'
+
+interface CheckboxGroupBaseProps extends PropsWithChildren {
+  hasLabel: boolean
+  hasHelp: boolean
+  isError: boolean
+}
+
+const CheckboxGroupBase = ({
+  children,
+  hasLabel,
+  hasHelp,
+  isError,
+}: CheckboxGroupBaseProps) => {
+  const formField = useFormField()
+
+  return (
+    <div
+      role="group"
+      aria-labelledby={hasLabel ? formField?.labelId : undefined}
+      aria-describedby={
+        hasHelp
+          ? formField?.descriptionId
+          : isError
+          ? formField?.errorId
+          : undefined
+      }
+    >
+      {children}
+    </div>
+  )
+}
+export interface CheckboxGroupProps
+  extends PropsWithChildren,
+    CheckboxGroupContextValue {
+  required?: boolean
+  label?: string
+  error?: string
+  help?: string
 }
 
 export const CheckboxGroup = ({
@@ -14,13 +57,35 @@ export const CheckboxGroup = ({
   name,
   defaultValue = [],
   value = [],
+  required,
+  label,
+  error,
+  help,
   onChange,
 }: CheckboxGroupProps) => {
   return (
     <CheckboxGroupContext.Provider
       value={{ name, defaultValue, value, onChange }}
     >
-      <div role="group">{children}</div>
+      <FormField>
+        {label ? (
+          <FormFieldLabel isError={!!error} isRequired={required}>
+            {label}
+          </FormFieldLabel>
+        ) : null}
+        <CheckboxGroupBase
+          hasLabel={!!label}
+          hasHelp={!!label}
+          isError={!!error}
+        >
+          {children}
+        </CheckboxGroupBase>
+        {error ? (
+          <FormFieldError>{error}</FormFieldError>
+        ) : help ? (
+          <FormFieldHelp>{help}</FormFieldHelp>
+        ) : null}
+      </FormField>
     </CheckboxGroupContext.Provider>
   )
 }
