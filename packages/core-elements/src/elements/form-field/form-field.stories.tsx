fdeacb9a7c65205480702d50e41663e9aa70e65f
Heeryong Kang <drakang4@gmail.com>
2022-11-11 15:47:44 +0900

    docs(core-elements): FormField 스토리 추가

--- packages/core-elements/src/elements/form-field/form-field.stories.tsx
+++ packages/core-elements/src/elements/form-field/form-field.stories.tsx
@@ -1,6 +1,10 @@
 import { ComponentMeta } from '@storybook/react'
 
 import { FormField } from './form-field'
+import { FormFieldContext } from './form-field-context'
+import { FormFieldError } from './form-field-error'
+import { FormFieldHelp } from './form-field-help'
+import { FormFieldLabel } from './form-field-label'
 
 export default {
   title: 'core-elements / FormField',
@@ -8,5 +12,57 @@ export default {
 } as ComponentMeta<typeof FormField>
 
 export const Default = () => {
-  return <FormField />
+  return (
+    <FormField>
+      <FormFieldLabel>Label</FormFieldLabel>
+      <FormFieldContext.Consumer>
+        {(formField) => (
+          <input
+            id={formField?.inputId}
+            aria-labelledby={formField?.labelId}
+            aria-describedby={formField?.descriptionId}
+          />
+        )}
+      </FormFieldContext.Consumer>
+      <FormFieldHelp>Helper text.</FormFieldHelp>
+    </FormField>
+  )
+}
+
+export const Required = () => {
+  return (
+    <FormField>
+      <FormFieldLabel isRequired>Label</FormFieldLabel>
+      <FormFieldContext.Consumer>
+        {(formField) => (
+          <input
+            id={formField?.inputId}
+            aria-labelledby={formField?.labelId}
+            aria-describedby={formField?.descriptionId}
+            required
+          />
+        )}
+      </FormFieldContext.Consumer>
+      <FormFieldHelp>Helper text.</FormFieldHelp>
+    </FormField>
+  )
+}
+
+export const Error = () => {
+  return (
+    <FormField>
+      <FormFieldLabel isError>Label</FormFieldLabel>
+      <FormFieldContext.Consumer>
+        {(formField) => (
+          <input
+            id={formField?.inputId}
+            aria-labelledby={formField?.labelId}
+            aria-errormessage={formField?.errorId}
+            aria-invalid
+          />
+        )}
+      </FormFieldContext.Consumer>
+      <FormFieldError>Error message.</FormFieldError>
+    </FormField>
+  )
 }
