903dd4eceedcb26279f775ee0e50a7a7046e51cb
Heeryong Kang <drakang4@gmail.com>
2022-11-16 17:54:47 +0900

    feat(core-elements): Textarea 접근성 개선

--- packages/core-elements/src/elements/textarea/textarea.stories.tsx
+++ packages/core-elements/src/elements/textarea/textarea.stories.tsx
@@ -8,8 +8,24 @@ export default {
 } as ComponentMeta<typeof Textarea>
 
 export const Default: ComponentStoryObj<typeof Textarea> = {
-  error: false,
-  label: '요청사항',
-  placeholder: '요청사항을 입력해주세요',
-  help: '고객님의 요청사항은 해당 호텔에 전달됩니다만 호텔 사정에 따라 필요하신 내용이 이루어지지 않을 수 있으니 많은 양해 바랍니다.',
+  args: {
+    error: false,
+    label: '요청사항',
+    placeholder: '요청사항을 입력해주세요',
+    help: '고객님의 요청사항은 해당 호텔에 전달됩니다만 호텔 사정에 따라 필요하신 내용이 이루어지지 않을 수 있으니 많은 양해 바랍니다.',
+  },
+}
+
+export const Required: ComponentStoryObj<typeof Textarea> = {
+  args: {
+    ...Default.args,
+    required: true,
+  },
+}
+
+export const Error: ComponentStoryObj<typeof Textarea> = {
+  args: {
+    ...Default.args,
+    error: '요청사항은 필수 입력 사항입니다.',
+  },
 }
