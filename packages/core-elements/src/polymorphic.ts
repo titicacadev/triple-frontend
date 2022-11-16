700b30375c48dcdef80d4b8431c08bc9d36e335e
Heeryong Kang <drakang4@gmail.com>
2022-11-16 12:44:23 +0900

    feat(core-elements): exotic component 제거

--- packages/core-elements/src/polymorphic.ts
+++ packages/core-elements/src/polymorphic.ts
@@ -2,8 +2,6 @@ import {
   ComponentPropsWithoutRef,
   ComponentPropsWithRef,
   ElementType,
-  ExoticComponent,
-  ReactElement,
 } from 'react'
 
 type Merge<T, U> = Omit<T, keyof U> & U
@@ -24,20 +22,3 @@ export type PolymorphicPropsWithRef<
 > = PolymorphicProps<C, P> & {
   ref?: PolymorphicRef<C>
 }
-
-type PolymorphicExoticComponent<C extends ElementType, P = unknown> = Merge<
-  ExoticComponent<P & { [key: string]: unknown }>,
-  {
-    <InstanceC extends ElementType = C>(
-      props: PolymorphicPropsWithRef<InstanceC, P>,
-    ): ReactElement | null
-  }
->
-
-export type PolymorphicForwardRefExoticComponent<
-  C extends ElementType,
-  P = unknown,
-> = Merge<
-  React.ForwardRefExoticComponent<P & { [key: string]: unknown }>,
-  PolymorphicExoticComponent<C, P>
->
