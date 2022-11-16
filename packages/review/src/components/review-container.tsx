ac28c68da8c56e7eae0712fc74ad5cc6cbb88bda
Heeryong Kang <drakang4@gmail.com>
2022-11-16 12:51:51 +0900

    fix(review): implicit any 에러 수정

--- packages/review/src/components/review-container.tsx
+++ packages/review/src/components/review-container.tsx
@@ -4,6 +4,7 @@ import {
   useCallback,
   SyntheticEvent,
   useMemo,
+  MouseEvent,
 } from 'react'
 import { useTranslation, Trans } from '@titicaca/next-i18next'
 import styled from 'styled-components'
@@ -423,7 +424,8 @@ function ReviewContainer({
                 size="small"
                 onClick={
                   onFullListButtonClick
-                    ? (e) => onFullListButtonClick(e, sortingOption)
+                    ? (e: MouseEvent<HTMLButtonElement>) =>
+                        onFullListButtonClick(e, sortingOption)
                     : handleFullListButtonClick
                 }
               >
