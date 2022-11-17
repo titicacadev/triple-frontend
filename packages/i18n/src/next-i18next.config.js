e6efc61f28cc9c42ebb5c4f8f315a39be034d7e4
jaehyeon48 <imjaehyeon48@gmail.com>
2022-11-17 17:07:45 +0900

    [i18n] 서버/브라우저 환경에 따라  next-i18next.config에서 에셋 파일 가져오는 방식 분기

--- packages/i18n/src/next-i18next.config.js
+++ packages/i18n/src/next-i18next.config.js
@@ -1,15 +1,37 @@
-/* eslint-disable import/no-commonjs */
-/* eslint-disable sort-keys */
+/* eslint-disable import/no-absolute-path */
 const path = require('path')
 
 const LOCALE_PATH = './public/static/locales'
 
-const koLocal = require(path.resolve(LOCALE_PATH, 'ko', 'local.json'))
-const koCommon = require(path.resolve(LOCALE_PATH, 'ko', 'common-web.json'))
-const jaLocal = require(path.resolve(LOCALE_PATH, 'ja', 'local.json'))
-const jaCommon = require(path.resolve(LOCALE_PATH, 'ja', 'common-web.json'))
-const twLocal = require(path.resolve(LOCALE_PATH, 'zh-TW', 'local.json'))
-const twCommon = require(path.resolve(LOCALE_PATH, 'zh-TW', 'common-web.json'))
+const koLocal =
+  typeof window === 'undefined'
+    ? require(path.resolve(LOCALE_PATH, 'ko', 'local.json'))
+    : require('/public/static/locales/ko/local.json')
+
+const koCommon =
+  typeof window === 'undefined'
+    ? require(path.resolve(LOCALE_PATH, 'ko', 'common-web.json'))
+    : require('/public/static/locales/ko/common-web.json')
+
+const jaLocal =
+  typeof window === 'undefined'
+    ? require(path.resolve(LOCALE_PATH, 'ja', 'local.json'))
+    : require('/public/static/locales/ja/local.json')
+
+const jaCommon =
+  typeof window === 'undefined'
+    ? require(path.resolve(LOCALE_PATH, 'ja', 'common-web.json'))
+    : require('/public/static/locales/ja/common-web.json')
+
+const twLocal =
+  typeof window === 'undefined'
+    ? require(path.resolve(LOCALE_PATH, 'zh-TW', 'local.json'))
+    : require('/public/static/locales/zh-TW/local.json')
+
+const twCommon =
+  typeof window === 'undefined'
+    ? require(path.resolve(LOCALE_PATH, 'zh-TW', 'common-web.json'))
+    : require('/public/static/locales/zh-TW/common-web.json')
 
 const resources = {
   ja: {
@@ -31,7 +53,10 @@ module.exports = {
     defaultLocale: 'ko',
     locales: ['ko', 'ja', 'zh-TW'],
   },
-  localePath: path.resolve('./public/static/locales'),
+  localePath:
+    typeof window === 'undefined'
+      ? path.resolve(LOCALE_PATH)
+      : '/public/static/locales',
   defaultNS: 'local',
   serializeConfig: false,
   resources,
