39f4e3b90003507f464e038b70698cbb5ec5aaba
jaehyeon48 <imjaehyeon48@gmail.com>
2022-11-17 17:07:45 +0900

    [i18n] 서버/브라우저 환경에 따라  next-i18next.config에서 에셋 파일 가져오는 방식 분기

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,6 +1,3 @@
-import nextI18nextConfig from './next-i18next.config'
-
 export { default as useI18n } from './use-i18n'
 export { default as withI18n } from './with-i18n'
 export { default as I18nextTripleWebAssetsBackend } from './backend'
-export { nextI18nextConfig }
