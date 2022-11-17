1612ff77da118da5d77a1de2c8d8b88b9390e58d
Heeryong Kang <drakang4@gmail.com>
2022-11-17 15:17:59 +0900

    feat(core-elements): Button을 styled 컴포넌트를 바로 export 하도록 변경

--- packages/core-elements/src/elements/button/basic-button.tsx
+++ packages/core-elements/src/elements/button/basic-button.tsx
@@ -1,7 +1,7 @@
-import styled, { css } from 'styled-components'
+import { css } from 'styled-components'
 import { Color, getColor } from '@titicaca/color-palette'
 
-import { ButtonBase, ButtonBaseProps } from './button-base'
+import { buttonBaseMixin, ButtonBaseProps } from './button-base'
 
 const BASIC_INVERTED_COLORS: Partial<Record<Color, string>> = {
   blue: '#368fff',
@@ -14,18 +14,23 @@ export interface BasicButtonProps extends ButtonBaseProps {
   inverted?: boolean
 }
 
-export const BasicButton = styled(ButtonBase)<BasicButtonProps>`
+export const basicButtonMixin = ({
+  color = 'blue',
+  compact,
+  inverted,
+  ...props
+}: BasicButtonProps) => css`
+  ${buttonBaseMixin(props)}
   border: 1px solid rgba(${getColor('gray200')});
   border-radius: 4px;
   background-color: transparent;
   color: #3a3a3a;
-  padding: ${({ compact }) => (compact ? '7px 12px' : '14px 12px')};
+  padding: ${compact ? '7px 12px' : '14px 12px'};
 
-  ${({ inverted, color = 'blue' }) =>
-    inverted &&
-    css`
-      border: 1px solid ${BASIC_INVERTED_COLORS[color]};
-      background-color: ${BASIC_INVERTED_COLORS[color]};
-      color: white;
-    `}
+  ${inverted &&
+  css`
+    border: 1px solid ${BASIC_INVERTED_COLORS[color]};
+    background-color: ${BASIC_INVERTED_COLORS[color]};
+    color: white;
+  `}
 `
