5f0978d21bcdbe84cbebf3ffcc52501a40053fda
Heeryong Kang <drakang4@gmail.com>
2022-11-21 11:28:15 +0900

    docs(core-elements): Tabs 스토리 내용 보완

--- packages/core-elements/src/elements/tabs/tabs.stories.tsx
+++ packages/core-elements/src/elements/tabs/tabs.stories.tsx
@@ -1,72 +1,56 @@
-import { ComponentStoryObj, Meta } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { useState } from 'react'
 
-import { Tabs } from '.'
+import { Tab } from './tab'
+import { TabList } from './tab-list'
+import { TabPanel } from './tab-panel'
+import { Tabs } from './tabs'
 
 export default {
-  title: 'Core-Elements / Tabs',
+  title: 'core-elements / Tabs',
   component: Tabs,
-} as Meta
+} as ComponentMeta<typeof Tabs>
 
-export const Basic: ComponentStoryObj<typeof Tabs> = {
-  name: '이 근처 장소',
-  args: {
-    value: 'attractions',
-    options: [
-      { label: '관광지', value: 'attractions' },
-      { label: '맛집', value: 'restaurants' },
-    ],
-  },
+const Template: ComponentStory<typeof Tabs> = (args) => {
+  const [value, setValue] = useState('a')
+
+  return (
+    <Tabs {...args} value={value} onChange={setValue}>
+      <TabList>
+        <Tab value="a">A</Tab>
+        <Tab value="b">B</Tab>
+        <Tab value="c">C</Tab>
+      </TabList>
+      <TabPanel value="a">This is panel A</TabPanel>
+      <TabPanel value="b">This is panel B</TabPanel>
+      <TabPanel value="c">This is panel C</TabPanel>
+    </Tabs>
+  )
 }
 
-export const Line: ComponentStoryObj<typeof Tabs> = {
-  name: '라인 탭',
-  args: {
-    type: 'pointing',
-    options: [
-      { label: '투어티켓', value: '투어티켓' },
-      { label: '호텔', value: '호텔' },
-    ],
-    value: '투어티켓',
-    labelPadding: {
-      top: 11,
-      bottom: 11,
-    },
-  },
+export const Basic = Template.bind({})
+Basic.args = {
+  variant: 'basic',
 }
 
-export const LineWithScroll: ComponentStoryObj<typeof Tabs> = {
-  name: '라인 탭 스크롤',
-  args: {
-    scroll: true,
-    type: 'pointing',
-    options: [
-      { label: '투어티켓1', value: '투어티켓' },
-      { label: '호텔2', value: '호텔2' },
-      { label: '투어티켓3', value: '투어티켓3' },
-      { label: '호텔4', value: '호텔4' },
-      { label: '투어티켓5', value: '투어티켓5' },
-      { label: '호텔6', value: '호텔6' },
-      { label: '투어티켓7', value: '투어티켓7' },
-      { label: '호텔8', value: '호텔8' },
-    ],
-    value: '투어티켓',
-  },
+export const Pointing = Template.bind({})
+Pointing.args = {
+  variant: 'pointing',
 }
-export const RoundedTabWithScroll: ComponentStoryObj<typeof Tabs> = {
-  name: '둥근 탭 스크롤',
-  args: {
-    scroll: true,
-    type: 'rounded',
-    options: [
-      { label: '김포 - 제주', value: '김포 - 제주' },
-      { label: '김포 - 여수', value: '김포 - 여수' },
-      { label: '김포 - 동탄', value: '김포 - 동탄' },
-      { label: '김포 - 부평', value: '김포 - 부평' },
-      { label: '김포 - 부산', value: '김포 - 부산' },
-      { label: '김포 - 트리플', value: '김포 - 트리플' },
-      { label: '김포 - 백현동', value: '김포 - 백현동' },
-      { label: '김포 - 김파이브', value: '김포 - 김파이브' },
-    ],
-    value: '김포 - 제주',
-  },
+
+export const PointingScroll = Template.bind({})
+PointingScroll.args = {
+  ...Pointing.args,
+  scroll: true,
+}
+
+export const Rounded = Template.bind({})
+Rounded.args = {
+  variant: 'rounded',
+}
+
+export const RoundedScroll = Template.bind({})
+RoundedScroll.args = {
+  ...Rounded.args,
+  scroll: true,
 }
