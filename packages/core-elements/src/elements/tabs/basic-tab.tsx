20389aa008fe59380e94ca611d38e0a1ccf332d4
Heeryong Kang <drakang4@gmail.com>
2022-11-21 11:27:45 +0900

    feat(core-elements): Tabs 컴포넌트 접근성 개선

--- packages/core-elements/src/elements/tabs/basic-tab.tsx
+++ packages/core-elements/src/elements/tabs/basic-tab.tsx
@@ -1,42 +1,22 @@
-import { brightGray, gray, white } from '@titicaca/color-palette'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
+import { gray, white } from '@titicaca/color-palette'
 
-import TabContainer from './tab-container'
-import TabLabel from './tab-label'
-import { TabProps } from './types'
+import { TabBase, TabBaseProps } from './tab-base'
 
-const BasicContainer = styled(TabContainer)`
-  background-color: ${brightGray};
-  border-radius: 4px;
-  padding: 2px;
+const StyledTabBase = styled(TabBase)`
+  flex: 1;
+  color: rgba(46, 46, 46, 0.3);
+  border-radius: 2px;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 11px 0;
+
+  &[aria-selected='true'] {
+    color: ${gray};
+    background-color: ${white};
+  }
 `
 
-const BasicLabel = styled(TabLabel)`
-  ${({ active }) => css`
-    color: ${active ? gray : 'rgba(46, 46, 46, 0.3)'};
-    background-color: ${active ? white : 'transparent'};
-    border-radius: 2px;
-    font-size: 14px;
-    font-weight: bold;
-  `}
-`
-
-export default function BasicTab<Value>({
-  options,
-  value: currentValue,
-  onChange,
-}: TabProps<Value>) {
-  return (
-    <BasicContainer>
-      {options.map(({ label, value }, i) => (
-        <BasicLabel
-          active={currentValue === value}
-          key={i}
-          onClick={(e) => onChange(e, value)}
-        >
-          {label}
-        </BasicLabel>
-      ))}
-    </BasicContainer>
-  )
+export const BasicTab = ({ children, ...props }: TabBaseProps) => {
+  return <StyledTabBase {...props}>{children}</StyledTabBase>
 }
