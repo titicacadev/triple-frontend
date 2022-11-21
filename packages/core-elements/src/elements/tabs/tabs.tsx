decb379dbce927de9a94ed515332a2b1f38a5bf9
Heeryong Kang <drakang4@gmail.com>
2022-11-21 11:46:47 +0900

    docs(core-eleements): Tabs props 주석 추가

--- packages/core-elements/src/elements/tabs/tabs.tsx
+++ packages/core-elements/src/elements/tabs/tabs.tsx
@@ -7,9 +7,21 @@ import { TabsContext } from './tabs-context'
 import { TabVariant } from './types'
 
 export interface TabsProps extends PropsWithChildren {
+  /**
+   * 현재 탭을 가르키는 값
+   */
   value: string
+  /**
+   * 디자인 variant
+   */
   variant?: TabVariant
+  /**
+   * 스크롤 가능한 탭 사용 여부. `pointing` 또는 `rounded` variant 일 때만 사용 가능
+   */
   scroll?: boolean
+  /**
+   * 탭 변경 이벤트 핸들러
+   */
   onChange?: (value: string) => void
 }
 
