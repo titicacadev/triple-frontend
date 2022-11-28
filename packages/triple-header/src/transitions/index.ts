fe2bd2323e0e16f2af59c69aea97c19bb220e32b
finn <finn@triple-corp.com>
2022-11-28 14:12:22 +0900

    feat: Marquee Transition Component

--- packages/triple-header/src/transitions/index.ts
+++ packages/triple-header/src/transitions/index.ts
@@ -1,7 +1,9 @@
 import Slide from './slide'
 import Rolling from './rolling'
+import Marquee from './marquee'
 
 export const TRANSITIONS = {
   slide: Slide,
   rolling: Rolling,
+  marquee: Marquee,
 }
---
+++ packages/triple-header/src/transitions/marquee.tsx
@@ -0,0 +1,46 @@
+import { ReactNode, useEffect, useRef, useState } from 'react'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
+
+const marqueeVariants = {
+  animate: (offsetX: number) => ({
+    x: [0, -offsetX],
+    transition: {
+      x: {
+        repeat: Infinity,
+        repeatType: 'loop',
+        duration: 20,
+        ease: 'linear',
+      },
+    },
+  }),
+}
+
+const Track = styled(motion.div)`
+  display: -webkit-box;
+  height: 100%;
+`
+
+export default function Marquee({ children }: { children: ReactNode[] }) {
+  const [offsetX, setOffsetX] = useState(0)
+
+  const trackRef = useRef<HTMLDivElement>(null)
+  const frames = [...children, ...children]
+
+  useEffect(() => {
+    if (trackRef.current) {
+      setOffsetX(trackRef.current.clientWidth)
+    }
+  }, [trackRef])
+
+  return (
+    <Track
+      variants={marqueeVariants}
+      animate="animate"
+      ref={trackRef}
+      custom={offsetX * children.length}
+    >
+      {frames}
+    </Track>
+  )
+}
