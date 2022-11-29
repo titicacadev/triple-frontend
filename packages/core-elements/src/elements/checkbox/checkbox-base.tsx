18cb9e93c6a191ef59af4640bc67535de4e5f248
Heeryong Kang <drakang4@gmail.com>
2022-11-29 13:27:39 +0900

    feat(core-elemenets): input 컴포넌트들에 forwardRef 추가

--- packages/core-elements/src/elements/checkbox/checkbox-base.tsx
+++ packages/core-elements/src/elements/checkbox/checkbox-base.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react'
+import { forwardRef, InputHTMLAttributes } from 'react'
 import styled from 'styled-components'
 import { useVisuallyHidden } from '@react-aria/visually-hidden'
 
@@ -41,31 +41,35 @@ export interface CheckboxBaseProps
   variant?: CheckboxVariant
 }
 
-export const CheckboxBase = ({
-  variant = 'square',
-  ...props
-}: CheckboxBaseProps) => {
-  const { visuallyHiddenProps } = useVisuallyHidden()
+export const CheckboxBase = forwardRef<HTMLInputElement, CheckboxBaseProps>(
+  function CheckboxBase({ variant = 'square', ...props }, ref) {
+    const { visuallyHiddenProps } = useVisuallyHidden()
 
-  return (
-    <CheckboxBaseWrapper>
-      <CheckboxBaseInput type="checkbox" {...visuallyHiddenProps} {...props} />
-      <CheckboxBaseControl variant={variant}>
-        <CheckboxBaseSvg
-          width="14"
-          height="10"
-          viewBox="0 0 14 10"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          focusable={false}
-          aria-hidden
-        >
-          <path
-            d="M12.1893 0.251888C12.4644 -0.0577739 12.9385 -0.0857889 13.2481 0.189315C13.5296 0.43941 13.5784 0.853921 13.3782 1.15987L13.3107 1.24813L6.20347 9.24813C5.94894 9.53464 5.52525 9.57933 5.21887 9.36885L5.13077 9.29804L0.237987 4.72682C-0.0646829 4.44405 -0.0808087 3.96945 0.20197 3.66678C0.459041 3.39162 0.874642 3.35328 1.17548 3.56104L1.26202 3.63076L5.593 7.67601L12.1893 0.251888Z"
-            fill="white"
-          />
-        </CheckboxBaseSvg>
-      </CheckboxBaseControl>
-    </CheckboxBaseWrapper>
-  )
-}
+    return (
+      <CheckboxBaseWrapper>
+        <CheckboxBaseInput
+          ref={ref}
+          type="checkbox"
+          {...visuallyHiddenProps}
+          {...props}
+        />
+        <CheckboxBaseControl variant={variant}>
+          <CheckboxBaseSvg
+            width="14"
+            height="10"
+            viewBox="0 0 14 10"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            focusable={false}
+            aria-hidden
+          >
+            <path
+              d="M12.1893 0.251888C12.4644 -0.0577739 12.9385 -0.0857889 13.2481 0.189315C13.5296 0.43941 13.5784 0.853921 13.3782 1.15987L13.3107 1.24813L6.20347 9.24813C5.94894 9.53464 5.52525 9.57933 5.21887 9.36885L5.13077 9.29804L0.237987 4.72682C-0.0646829 4.44405 -0.0808087 3.96945 0.20197 3.66678C0.459041 3.39162 0.874642 3.35328 1.17548 3.56104L1.26202 3.63076L5.593 7.67601L12.1893 0.251888Z"
+              fill="white"
+            />
+          </CheckboxBaseSvg>
+        </CheckboxBaseControl>
+      </CheckboxBaseWrapper>
+    )
+  },
+)
