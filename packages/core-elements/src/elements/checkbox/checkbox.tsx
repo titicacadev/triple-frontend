bb953f41c262e232d3bc0dbe9c1f6b5582191df2
Heeryong Kang <drakang4@gmail.com>
2022-11-29 13:27:39 +0900

    feat(core-elemenets): input 컴포넌트들에 forwardRef 추가

--- packages/core-elements/src/elements/checkbox/checkbox.tsx
+++ packages/core-elements/src/elements/checkbox/checkbox.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, PropsWithChildren } from 'react'
+import { ChangeEventHandler, forwardRef, PropsWithChildren } from 'react'
 import styled from 'styled-components'
 
 import { Text } from '../text'
@@ -22,42 +22,40 @@ const CheckboxText = styled(Text)`
 
 export interface CheckboxProps extends CheckboxBaseProps, PropsWithChildren {}
 
-export const Checkbox = ({
-  children,
-  variant = 'square',
-  name,
-  checked,
-  value,
-  onChange,
-  ...props
-}: CheckboxProps) => {
-  const group = useCheckboxGroup()
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  function Checkbox(
+    { children, variant = 'square', name, checked, value, onChange, ...props },
+    ref,
+  ) {
+    const group = useCheckboxGroup()
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    if (group) {
-      const nextValue = event.target.checked
-        ? group.value.concat(event.target.value)
-        : group.value.filter((value) => event.target.value !== value)
-      group?.onChange?.(nextValue)
-    } else {
-      onChange?.(event)
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+      if (group) {
+        const nextValue = event.target.checked
+          ? group.value.concat(event.target.value)
+          : group.value.filter((value) => event.target.value !== value)
+        group?.onChange?.(nextValue)
+      } else {
+        onChange?.(event)
+      }
     }
-  }
 
-  return (
-    <CheckboxLabel>
-      <CheckboxText>{children}</CheckboxText>
-      <CheckboxBase
-        {...props}
-        variant={variant}
-        name={name ?? group?.name}
-        checked={
-          checked ??
-          (value ? group?.value?.includes(value.toString()) : undefined)
-        }
-        value={value}
-        onChange={handleChange}
-      />
-    </CheckboxLabel>
-  )
-}
+    return (
+      <CheckboxLabel>
+        <CheckboxText>{children}</CheckboxText>
+        <CheckboxBase
+          {...props}
+          ref={ref}
+          variant={variant}
+          name={name ?? group?.name}
+          checked={
+            checked ??
+            (value ? group?.value?.includes(value.toString()) : undefined)
+          }
+          value={value}
+          onChange={handleChange}
+        />
+      </CheckboxLabel>
+    )
+  },
+)
