9a88d5c529a629df47d80f34c36d634073684353
jaehyeon48 <imjaehyeon48@gmail.com>
2022-12-01 15:27:51 +0900

    i18n 에셋을 fetch 할 때 TF 패키지의 버전을 고려하도록 함

--- packages/i18n/src/fetch-locales.ts
+++ packages/i18n/src/fetch-locales.ts
@@ -7,14 +7,19 @@ import { program } from 'commander'
 
 const WEB_ASSETS_URL = 'https://assets.triple.guide'
 
-const languages = ['ko', 'ja', 'zh-TW']
-
 program
   .action(async () => {
+    const languages = ['ko', 'ja', 'zh-TW']
     const packageJson = readFileSync('package.json', { encoding: 'utf-8' })
-    console.log(packageJson)
+    const { version: packageVersion }: { version: string | undefined } =
+      JSON.parse(packageJson)
+
+    if (!packageVersion) {
+      throw new Error('package.json에 버전을 명시해 주세요.')
+    }
+
     for (const language of languages) {
-      const filePath = `public/static/locales/${language}`
+      const filePath = `public/static/locales/${packageVersion}/${language}`
 
       const response = await fetch(
         `${WEB_ASSETS_URL}/locales/${language}/common-web.json`,
