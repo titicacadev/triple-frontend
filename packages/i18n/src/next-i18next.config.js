c5b1635c3b2f098d958ad58865892c9faf221bfd
jaehyeon48 <imjaehyeon48@gmail.com>
2022-12-01 17:10:16 +0900

    next-i18next.cofig 에서 i18n 에셋 파일을 TF/i18n 패키지로부터 가져오는 동작 테스트

--- packages/i18n/src/next-i18next.config.js
+++ packages/i18n/src/next-i18next.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-absolute-path */
 const path = require('path')
 
+const { koCommonWeb, jaCommonWeb, zhTwCommonWeb } = require('@titicaca/i18n')
+
 const LOCALE_PATH = './public/static/locales'
 
 const koLocal =
@@ -8,43 +10,28 @@ const koLocal =
     ? require(path.resolve(LOCALE_PATH, 'ko', 'local.json'))
     : require('/public/static/locales/ko/local.json')
 
-const koCommon =
-  typeof window === 'undefined'
-    ? require(path.resolve(LOCALE_PATH, 'ko', 'common-web.json'))
-    : require('/public/static/locales/ko/common-web.json')
-
 const jaLocal =
   typeof window === 'undefined'
     ? require(path.resolve(LOCALE_PATH, 'ja', 'local.json'))
     : require('/public/static/locales/ja/local.json')
-
-const jaCommon =
-  typeof window === 'undefined'
-    ? require(path.resolve(LOCALE_PATH, 'ja', 'common-web.json'))
-    : require('/public/static/locales/ja/common-web.json')
 
 const twLocal =
   typeof window === 'undefined'
     ? require(path.resolve(LOCALE_PATH, 'zh-TW', 'local.json'))
     : require('/public/static/locales/zh-TW/local.json')
 
-const twCommon =
-  typeof window === 'undefined'
-    ? require(path.resolve(LOCALE_PATH, 'zh-TW', 'common-web.json'))
-    : require('/public/static/locales/zh-TW/common-web.json')
-
 const resources = {
-  ja: {
-    local: jaLocal,
-    'common-web': jaCommon,
-  },
   ko: {
     local: koLocal,
-    'common-web': koCommon,
+    'common-web': koCommonWeb,
+  },
+  ja: {
+    local: jaLocal,
+    'common-web': jaCommonWeb,
   },
   'zh-TW': {
     local: twLocal,
-    'common-web': twCommon,
+    'common-web': zhTwCommonWeb,
   },
 }
 
