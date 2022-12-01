225e9ef0a7737537a7858f71a4452e2088a0566a
jaehyeon48 <imjaehyeon48@gmail.com>
2022-12-01 18:24:54 +0900

    TF 버전에 따른 common-web.json 변경 테스트

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -1,5 +1,1 @@
 export { default as I18nextTripleWebAssetsBackend } from './backend'
-
-export { koCommonWeb } from './assets/ko/common-web'
-export { jaCommonWeb } from './assets/ja/common-web'
-export { zhTwCommonWeb } from './assets/zh-TW/common-web'
