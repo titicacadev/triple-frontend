5d0c51e7b68099c5e1deddf111277e36dd34c983
jaehyeon48 <imjaehyeon48@gmail.com>
2022-12-07 21:04:03 +0900

    I18nKeys -> I18nCommonWebKeys 이름 변경

--- packages/app-installation-cta/src/i18n-types.d.ts
+++ packages/app-installation-cta/src/i18n-types.d.ts
@@ -1,12 +1,12 @@
 import '@titicaca/next-i18next'
-import { I18nKeys } from '@titicaca/i18n'
+import { I18nCommonWebKeys } from '@titicaca/i18n'
 
 declare module 'i18next' {
   interface CustomTypeOptions {
     // eslint-disable-next-line @typescript-eslint/naming-convention
     defaultNS: 'common-web'
     resources: {
-      'common-web': I18nKeys
+      'common-web': I18nCommonWebKeys
     }
   }
 }
--- packages/poi-list-elements/src/get-type-names.ts
+++ packages/poi-list-elements/src/get-type-names.ts
@@ -1,9 +1,11 @@
 import { getTranslation, i18n } from '@titicaca/next-i18next'
-import { I18nKeys } from '@titicaca/i18n'
+import { I18nCommonWebKeys } from '@titicaca/i18n'
 
 import { PoiListElementType } from './types'
 
-const TYPE_NAMES: { [key in PoiListElementType['type']]: keyof I18nKeys } = {
+const TYPE_NAMES: {
+  [key in PoiListElementType['type']]: keyof I18nCommonWebKeys
+} = {
   attraction: 'gwangwangmyeongso',
   restaurant: 'eumsigjeom',
   hotel: 'hotel',
