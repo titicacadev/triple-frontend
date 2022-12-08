089d50d579c9bb830332e6600390f8405222f435
Heeryong Kang <drakang4@gmail.com>
2022-12-08 15:53:28 +0900

    fix(replies): hr margin 수정

--- packages/replies/src/list/not-exist-replies.tsx
+++ packages/replies/src/list/not-exist-replies.tsx
@@ -5,6 +5,7 @@ export default function NotExistReplies() {
     <>
       <HR1
         color="var(--color-gray50)"
+        compact
         css={{ marginTop: 20, marginLeft: 30, marginRight: 30 }}
       />
 
@@ -20,7 +21,7 @@ export default function NotExistReplies() {
         </Text>
       </Container>
 
-      <HR1 color="var(--color-gray50)" css={{ marginTop: 0 }} />
+      <HR1 color="var(--color-gray50)" compact />
     </>
   )
 }
