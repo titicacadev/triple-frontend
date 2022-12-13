a584b1d3becb016a7cd47ec58cd0cee632dce948
hoolie-triple <sohyun4283@naver.com>
2022-12-13 16:32:58 +0900

    docs(chat): 불필요한 스토리 삭제 및 스토리 설명 개선

--- packages/chat/src/chat/chat-container.tsx
+++ packages/chat/src/chat/chat-container.tsx
@@ -11,8 +11,17 @@ import {
 import { ChatContext, ChatContextValue } from './chat-context'
 
 export interface ChatContainerProps extends ChatContextValue {
+  /**
+   * Chat list와 보내기 Input 창을 감싸는 컨테이너로, 커스텀 스타일 등 적용 가능
+   */
   container: ElementType
+  /**
+   * input 창, 보내기 버튼 등을 포함하는 컴포넌트
+   */
   inputElement?: ElementType
+  /**
+   * 메시지를 전송하는 api를 래핑하는 함수
+   */
   postMessage?: (
     payload: TextPayload | ImagePayload,
   ) => Promise<{ success: boolean; newMessages: MessageInterface[] }>
@@ -22,7 +31,12 @@ const defaultOnImageBubbleClick = (imageInfos: MetaDataInterface[]) => {
   window.open(imageInfos[0].originalUrl, '_blank')
 }
 
-const ChatContainer = ({
+/**
+ * chat-room 페이지에 삽입되는 컴포넌트입니다.
+ *
+ * ChatContainer에는 공통 필수 prop, Chat 에는 로직, api 관련된 prop을 전달해 구성합니다.
+ */
+export const ChatContainer = ({
   container: Container,
   inputElement: Input,
   children,
@@ -57,5 +71,3 @@ const ChatContainer = ({
     </ChatContext.Provider>
   )
 }
-
-export default ChatContainer
