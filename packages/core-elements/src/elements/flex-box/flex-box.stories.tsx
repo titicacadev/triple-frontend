41a9f140cd5a41bf1100366571d135d7c5f3e4c3
hoolie-triple <sohyun4283@naver.com>
2022-12-14 10:13:28 +0900

    docs(core-elements): container, flexbox 스토리 정리

--- packages/core-elements/src/elements/flex-box/flex-box.stories.tsx
+++ packages/core-elements/src/elements/flex-box/flex-box.stories.tsx
@@ -1,22 +1,24 @@
 import styled from 'styled-components'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 
-import { Container } from '../container'
 import { FlexBox } from '../flex-box'
-import { Text } from '../text'
 
 export default {
   title: 'Core-Elements / FlexBox',
   components: FlexBox,
-}
-
-const Section = styled(Container)`
-  padding: 50px;
-`
-
-const Summary = styled(Text)`
-  font-size: 15px;
-  margin-bottom: 30px;
-`
+  subcomponents: {
+    /* eslint-disable @typescript-eslint/naming-convention */
+    'FlexBox.Item': FlexBox.Item,
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'Flex로 레이아웃 구성이 필요할 때 사용합니다.\n * FlexBox 는 Container 를 상속받아 구성되어있기 때문에 Container 의 Prop을 그대로 이용 할 수 있습니다.\n * flex children 요소가 사용 가능한 flex, flexGrow, flexShrink, flexBasis, alignSelf, order는 중첩된 구조의 flex 사용 시에만 사용 권장합니다.',
+      },
+    },
+  },
+} as ComponentMeta<typeof FlexBox>
 
 const Item = styled(FlexBox.Item)`
   border: 2px solid #e91e63;
@@ -24,198 +26,161 @@ const Item = styled(FlexBox.Item)`
   border-radius: 10px;
 `
 
-export const Flex = () => {
+export const Flex: ComponentStory<typeof FlexBox> = (args) => {
   return (
-    <Section>
-      <Summary>
-        flex 속성을 추가하면 display: flex 가 적용됩니다. <br />
-        FlexBox 는 Container 를 상속받아 구성되어있기 때문에 Container 의 Prop
-        을 그대로 이용 할 수 있습니다.
-        <br />
-        flex children 요소가 사용 가능한 flex, flexGrow, flexShrink, flexBasis,
-        alignSelf, order는 중첩된 구조의 flex 사용 시에만 사용 권장합니다.
-      </Summary>
-      <FlexBox flex>
+    <FlexBox {...args}>
+      <FlexBox.Item>
         <Item>Item1</Item>
         <Item>Item2</Item>
         <Item>Item3</Item>
-      </FlexBox>
-    </Section>
+      </FlexBox.Item>
+      <FlexBox.Item>
+        <Item>Item4</Item>
+        <Item>Item5</Item>
+        <Item>Item6</Item>
+      </FlexBox.Item>
+    </FlexBox>
+  )
+}
+Flex.storyName = 'Default'
+Flex.args = {
+  flex: true,
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  alignItems: 'center',
+  alignContent: 'baseline',
+  gap: 'normal',
+  columnGap: 'normal',
+  rowGap: 'normal',
+}
+
+export const FlexItem: ComponentStory<typeof FlexBox> = () => {
+  return (
+    <FlexBox flex>
+      <FlexBox.Item
+        order={3}
+        css={`
+          border: 2px solid #e91e63;
+          padding: 10px;
+          border-radius: 10px;
+        `}
+      >
+        Item1 (order=3)
+      </FlexBox.Item>
+      <FlexBox.Item>Item2</FlexBox.Item>
+      <FlexBox.Item>Item3</FlexBox.Item>
+    </FlexBox>
   )
 }
 
-export const FlexItem = () => {
+export const FlexGrow: ComponentStory<typeof FlexBox> = () => {
   return (
-    <Section>
-      <Summary>
-        FlexBox 내부에 Item이 있을 경우 FlexBox가 아닌 FlexBox.Item으로 사용할
-        수 있습니다.
-        <br />
-        flex children 요소가 사용 가능한 flex, flexGrow, flexShrink, flexBasis,
-        alignSelf, order가 사용 가능합니다.
-        <br />
-        이외의 스타일은 css prop으로 사용합니다.
-      </Summary>
-      <FlexBox flex>
-        <FlexBox.Item
-          order={3}
-          css={`
-            border: 2px solid #e91e63;
-            padding: 10px;
-            border-radius: 10px;
-          `}
-        >
-          Item1 (order=3)
-        </FlexBox.Item>
-        <FlexBox.Item>Item2</FlexBox.Item>
-        <FlexBox.Item>Item3</FlexBox.Item>
-      </FlexBox>
-    </Section>
+    <FlexBox flex>
+      <Item flexGrow={1}>Item1</Item>
+      <Item flexGrow={1}>Item2</Item>
+      <Item flexGrow={1}>Item3</Item>
+    </FlexBox>
   )
 }
 
-export const FlexGrow = () => {
+export const Order: ComponentStory<typeof FlexBox> = () => {
   return (
-    <Section>
-      <Summary>
-        flexGrow 는 컨테이너 요소 내부에서 할당 가능한 공간의 정도를 선언합니다.
-      </Summary>
-      <FlexBox flex>
-        <Item flexGrow={1}>Item1</Item>
-        <Item flexGrow={1}>Item2</Item>
-        <Item flexGrow={1}>Item3</Item>
-      </FlexBox>
-    </Section>
+    <FlexBox flex>
+      <Item order={3}>Item1</Item>
+      <Item order={2}>Item2</Item>
+      <Item order={1}>Item3</Item>
+    </FlexBox>
   )
 }
 
-export const Order = () => {
+export const FlexShrink: ComponentStory<typeof FlexBox> = () => {
   return (
-    <Section>
-      <Summary>order를 이용하여 컴포넌트 순서를 조절 할 수 있습니다.</Summary>
-      <FlexBox flex>
-        <Item order={3}>Item1</Item>
-        <Item order={2}>Item2</Item>
-        <Item order={1}>Item3</Item>
-      </FlexBox>
-    </Section>
+    <FlexBox
+      flex
+      css={{
+        width: 300,
+      }}
+    >
+      <Item flexBasis="500px" flexShrink={1}>
+        Item1
+      </Item>
+      <Item>Item2</Item>
+      <Item>Item3</Item>
+    </FlexBox>
   )
 }
 
-export const FlexShrink = () => {
+export const FlexDirection: ComponentStory<typeof FlexBox> = () => {
   return (
-    <Section>
-      <Summary>
-        컨테이너에 속한 아이템 크기가 컨테이너 보다 클 때 flexShrink 를 이용하면
-        값에 따라 컨테이너에 맞게 축소됩니다.
-      </Summary>
-      <FlexBox
-        flex
+    <FlexBox flex flexDirection="row">
+      <Item>Item1</Item>
+      <Item>Item2</Item>
+      <Item>Item3</Item>
+    </FlexBox>
+  )
+}
+
+export const FlexWrap: ComponentStory<typeof FlexBox> = () => {
+  return (
+    <FlexBox
+      flex
+      flexWrap="wrap"
+      css={{
+        width: 200,
+      }}
+    >
+      <Item
         css={{
-          width: 300,
+          width: 100,
         }}
       >
-        <Item flexBasis="500px" flexShrink={1}>
-          Item1
-        </Item>
-        <Item>Item2</Item>
-        <Item>Item3</Item>
-      </FlexBox>
-    </Section>
-  )
-}
-
-export const FlexDirection = () => {
-  return (
-    <Section>
-      <Summary>
-        아이템을 배치할 때 사용할 주축 및 방향(정방향, 역방향)을 지정합니다.
-      </Summary>
-      <FlexBox flex flexDirection="row">
-        <Item>Item1</Item>
-        <Item>Item2</Item>
-        <Item>Item3</Item>
-      </FlexBox>
-    </Section>
-  )
-}
-
-export const FlexWrap = () => {
-  return (
-    <Section>
-      <Summary>
-        컨테이너 내부의 아이템들을 강제로 한줄에 배치되게 할 것인지, 또는 가능한
-        영역 내에서 벗어나지 않고 여러행으로 나누어 표현 할 것인지 결정하는
-        속성입니다.
-      </Summary>
-      <FlexBox
-        flex
-        flexWrap="wrap"
+        Item1
+      </Item>
+      <Item
         css={{
-          width: 200,
+          width: 100,
         }}
       >
-        <Item
-          css={{
-            width: 100,
-          }}
-        >
-          Item1
-        </Item>
-        <Item
-          css={{
-            width: 100,
-          }}
-        >
-          Item2
-        </Item>
-        <Item
-          css={{
-            width: 100,
-          }}
-        >
-          Item3
-        </Item>
-      </FlexBox>
-    </Section>
+        Item2
+      </Item>
+      <Item
+        css={{
+          width: 100,
+        }}
+      >
+        Item3
+      </Item>
+    </FlexBox>
   )
 }
 
-export const JustifyContent = () => {
+export const JustifyContent: ComponentStory<typeof FlexBox> = () => {
   return (
-    <Section>
-      <Summary>JustifyContent 는 주축 정렬을 제어합니다.</Summary>
-      <FlexBox flex flexDirection="row" justifyContent="space-between">
-        <Item>Item1</Item>
-        <Item>Item2</Item>
-        <Item>Item3</Item>
-      </FlexBox>
-    </Section>
+    <FlexBox flex flexDirection="row" justifyContent="space-between">
+      <Item>Item1</Item>
+      <Item>Item2</Item>
+      <Item>Item3</Item>
+    </FlexBox>
   )
 }
 
-export const AlignItems = () => {
+export const AlignItems: ComponentStory<typeof FlexBox> = () => {
   return (
-    <Section>
-      <Summary>align-items 는 교차축 정렬을 제어합니다.</Summary>
-      <FlexBox flex flexDirection="column" alignItems="center">
-        <Item>Item1</Item>
-        <Item>Item2</Item>
-        <Item>Item3</Item>
-      </FlexBox>
-    </Section>
+    <FlexBox flex flexDirection="column" alignItems="center">
+      <Item>Item1</Item>
+      <Item>Item2</Item>
+      <Item>Item3</Item>
+    </FlexBox>
   )
 }
 
-export const Gap = () => {
+export const Gap: ComponentStory<typeof FlexBox> = () => {
   return (
-    <Section>
-      <Summary>Gap은 행과 열 사이의 간격(거터)을 설정합니다.</Summary>
-      <FlexBox flex gap="10px">
-        <Item>Item1</Item>
-        <Item>Item2</Item>
-        <Item>Item3</Item>
-      </FlexBox>
-    </Section>
+    <FlexBox flex gap="10px">
+      <Item>Item1</Item>
+      <Item>Item2</Item>
+      <Item>Item3</Item>
+    </FlexBox>
   )
 }
--- packages/core-elements/src/elements/flex-box/flex-box.tsx
+++ packages/core-elements/src/elements/flex-box/flex-box.tsx
@@ -7,23 +7,56 @@ import { Container, ContainerProps } from '../container'
 export interface FlexBoxProps
   extends Omit<FlexItemOwnProps, 'flex'>,
     HTMLAttributes<Element> {
+  /**
+   * boolean
+   */
   flex?: boolean
+  /**
+   * 아이템을 배치할 때 사용할 주축 및 방향(정방향, 역방향)을 지정합니다.
+   * */
   flexDirection?: Property.FlexDirection
+  /**
+  * 컨테이너 내부의 아이템들을 강제로 한줄에 배치되게 할 것인지, 또는 가능한
+        영역 내에서 벗어나지 않고 여러행으로 나누어 표현 할 것인지 결정하는
+        속성입니다.
+  * */
   flexWrap?: Property.FlexWrap
+  /**
+   * JustifyContent 는 주축 정렬을 제어합니다.
+   * */
   justifyContent?: Property.JustifyContent
+  /**
+   * align-items 는 교차축 정렬을 제어합니다.
+   * */
   alignItems?: Property.AlignItems
   alignContent?: Property.AlignContent
+  /**
+   * Gap은 행과 열 사이의 간격(거터)을 설정합니다.
+   * */
   gap?: Property.Gap
   columnGap?: Property.ColumnGap
   rowGap?: Property.RowGap
 }
 
 export interface FlexItemOwnProps extends ContainerProps {
+  /**
+   * boolean
+   */
   flex?: Property.Flex
+  /**
+   * flexGrow 는 컨테이너 요소 내부에서 할당 가능한 공간의 정도를 선언합니다.
+   */
   flexGrow?: Property.FlexGrow
+  /**
+  * 컨테이너에 속한 아이템 크기가 컨테이너 보다 클 때 flexShrink 를 이용하면
+        값에 따라 컨테이너에 맞게 축소됩니다.
+  * */
   flexShrink?: Property.FlexShrink
   flexBasis?: Property.FlexBasis
   alignSelf?: Property.AlignSelf
+  /**
+   * order를 이용하여 컴포넌트 순서를 조절 할 수 있습니다.
+   * */
   order?: Property.Order
 }
 
@@ -47,8 +80,6 @@ const StyledFlexBox = styled(Container)<FlexBoxProps>((props) => ({
   gap: props.gap,
   columnGap: props.columnGap,
   rowGap: props.rowGap,
-
-  // 중첩된 flex 사용할 경우에만 사용할 것
   flexGrow: props.flexGrow,
   flexShrink: props.flexShrink,
   flexBasis: props.flexBasis,
@@ -56,6 +87,14 @@ const StyledFlexBox = styled(Container)<FlexBoxProps>((props) => ({
   order: props.order,
 }))
 
+/**
+ * flex 속성을 추가하면 display: flex 가 적용됩니다.
+ * FlexBox 는 Container 를 상속받아 구성되어있기 때문에 Container 의 Prop
+ * 을 그대로 이용 할 수 있습니다.
+ *
+ * flex children 요소가 사용 가능한 flex, flexGrow, flexShrink, flexBasis,
+ * alignSelf, order는 중첩된 구조의 flex 사용 시에만 사용 권장합니다.
+ */
 export const FlexBox = (props: FlexBoxProps) => {
   return <StyledFlexBox {...props} />
 }
