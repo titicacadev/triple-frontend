20c4d866d5f0eb7d5f3bb6b70587ae200a28602f
finn <finn@triple-corp.com>
2022-12-21 11:35:24 +0900

    feat: Slide Transition Component 추가

---
+++ packages/triple-header/src/transitions/slide.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState, ReactNode } from 'react'
+import { AnimatePresence } from 'framer-motion'
+
+import { wrap } from '../utils'
+
+export default function Slide({ children }: { children: ReactNode[] }) {
+  const [[page, direction], setPage] = useState([0, 0])
+  const index = wrap(0, children.length, page)
+
+  useEffect(() => {
+    const timer = setInterval(() => setPage((prev) => [prev[0] + 1, 0]), 3000)
+
+    return () => clearInterval(timer)
+  }, [page])
+
+  return (
+    <AnimatePresence initial exitBeforeEnter custom={direction}>
+      {children[index]}
+    </AnimatePresence>
+  )
+}
