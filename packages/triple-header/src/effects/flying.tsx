c3c995ed12fb3e7133b8c3cf2bc44478f4d7ed87
finn <finn@triple-corp.com>
2022-12-21 11:21:56 +0900

    feat: Flying Effect 추가

---
+++ packages/triple-header/src/effects/flying.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react'
+import { motion } from 'framer-motion'
+
+export interface FlyingProps {
+  type: 'rotate'
+  options?: {
+    degree: number
+    infinity?: boolean
+    repeatType?: 'loop' | 'reverse' | 'mirror'
+  }
+  children: ReactNode
+}
+
+export default function Flying({
+  children,
+  options: initialOptions,
+}: FlyingProps) {
+  const options = initialOptions
+    ? {
+        degree: 0,
+        ...(initialOptions?.infinity && { repeat: initialOptions.infinity }),
+        ...(initialOptions?.repeatType && {
+          repeatType: initialOptions.repeatType,
+        }),
+      }
+    : {}
+
+  const transition = {
+    ease: 'linear',
+    duration: 3,
+    ...options,
+  }
+
+  return (
+    <motion.div
+      style={{ position: 'absolute', top: 0, width: '100%', height: '100%' }}
+      initial={{ x: '150%', rotate: 0 }}
+      animate={{ x: 0, rotate: initialOptions?.degree || 0 }}
+      transition={transition}
+    >
+      {children}
+    </motion.div>
+  )
+}
