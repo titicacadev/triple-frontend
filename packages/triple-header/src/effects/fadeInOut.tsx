4572fddde2680039b0998bac0fb065335471b724
finn <finn@triple-corp.com>
2022-12-31 21:52:27 +0900

    Add an effect called FadeInOut

---
+++ packages/triple-header/src/effects/fadeInOut.tsx
@@ -0,0 +1,56 @@
+import { ReactNode } from 'react'
+import { motion } from 'framer-motion'
+
+export interface FadeInOutProps {
+  type: 'fadeInOut'
+  options?: {
+    infinity?: boolean
+    repeatType?: 'loop' | 'reverse' | 'mirror'
+  }
+  children: ReactNode
+}
+
+const variants = {
+  enter: () => ({
+    opacity: 0,
+  }),
+  active: () => ({
+    opacity: 1,
+  }),
+  exit: () => ({
+    opacity: 0,
+  }),
+}
+
+export default function FadeInOut({
+  children,
+  options: initialOptions,
+}: FadeInOutProps) {
+  const options = initialOptions
+    ? {
+        ...(initialOptions.infinity && { repeat: Infinity }),
+        ...(initialOptions.repeatType && {
+          repeatType: initialOptions.repeatType,
+        }),
+      }
+    : {}
+
+  const transition = {
+    ease: 'linear',
+    duration: 3,
+    ...options,
+  }
+
+  return (
+    <motion.div
+      variants={variants}
+      style={{ position: 'absolute', top: 0, width: '100%', height: '100%' }}
+      initial="enter"
+      animate="active"
+      exit="exit"
+      transition={transition}
+    >
+      {children}
+    </motion.div>
+  )
+}
