3059fdd402251b3d968323054308e0ca2a803d00
finn <finn@triple-corp.com>
2022-12-31 22:31:36 +0900

    Add a transition called Marquee

--- packages/triple-header/src/transitions/index.ts
+++ packages/triple-header/src/transitions/index.ts
@@ -1,9 +1,11 @@
 import Slide from './slide'
 import Rolling from './rolling'
+import Marquee from './marquee'
 
 const TRANSITIONS = {
   slide: Slide,
   rolling: Rolling,
+  marquee: Marquee,
 }
 
 export default TRANSITIONS
