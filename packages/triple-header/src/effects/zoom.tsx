602ebf6a69b381e98039d2bf35056eb1f587a489
finn <finn@triple-corp.com>
2023-01-06 17:36:18 +0900

    Add Zoom effect

---
+++ packages/triple-header/src/effects/zoom.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+import { motion } from 'framer-motion'
+
+import { InitialEffectOptions } from './types'
+
+export interface ZoomProps {
+  type: 'zoom'
+  options?: InitialEffectOptions
+  children: ReactNode
+}
+
+export default function Zoom({ children, options: initialOptions }: ZoomProps) {
+  const options = initialOptions
+    ? {
+        ...(initialOptions.infinity && { repeat: Infinity }),
+        ...(initialOptions.repeatType && {
+          repeatType: initialOptions.repeatType,
+        }),
+      }
+    : {}
+
+  const transition = {
+    ease: 'linear',
+    duration: 1,
+    ...options,
+  }
+
+  return (
+    <motion.div
+      key={`zoom_${initialOptions?.infinity || ''}`}
+      style={{ position: 'absolute', top: 0, width: '100%', height: '100%' }}
+      animate={{ scale: 1.2 }}
+      transition={transition}
+    >
+      {children}
+    </motion.div>
+  )
+}
