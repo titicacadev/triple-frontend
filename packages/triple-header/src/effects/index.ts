90f7a38570151e9428428b9212e71227b13b552d
finn <finn@triple-corp.com>
2023-01-06 17:36:18 +0900

    Add Zoom effect

--- packages/triple-header/src/effects/index.ts
+++ packages/triple-header/src/effects/index.ts
@@ -1,13 +1,15 @@
 import None from './none'
+import Zoom, { ZoomProps } from './zoom'
 import FadeInOut, { FadeInOutProps } from './fadeInOut'
 import Rotate, { RotateProps } from './rotate'
 import Flying, { FlyingProps } from './flying'
 
 export const EFFECTES = {
   none: None,
+  zoom: Zoom,
   fadeInOut: FadeInOut,
   rotate: Rotate,
   flying: Flying,
 }
 
-export type EffectData = FadeInOutProps | RotateProps | FlyingProps
+export type EffectData = ZoomProps | FadeInOutProps | RotateProps | FlyingProps
