1a24486f5796fe70db23c2a340228fef8cbf13d5
finn <finn@triple-corp.com>
2023-01-06 17:40:42 +0900

    Delete FadeInOut effect

--- packages/triple-header/src/effects/index.ts
+++ packages/triple-header/src/effects/index.ts
@@ -1,15 +1,13 @@
 import None from './none'
 import Zoom, { ZoomProps } from './zoom'
-import FadeInOut, { FadeInOutProps } from './fadeInOut'
 import Rotate, { RotateProps } from './rotate'
 import Flying, { FlyingProps } from './flying'
 
 export const EFFECTES = {
   none: None,
   zoom: Zoom,
-  fadeInOut: FadeInOut,
   rotate: Rotate,
   flying: Flying,
 }
 
-export type EffectData = ZoomProps | FadeInOutProps | RotateProps | FlyingProps
+export type EffectData = ZoomProps | RotateProps | FlyingProps
