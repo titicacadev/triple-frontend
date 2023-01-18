49249e56c9cd08beaa940cc32e58c1aac2b3a511
YWTechIT <ywtechit@gmail.com>
2023-01-18 09:29:57 +0900

    Add loungeHome component at PublicHeader

---
+++ packages/public-header/src/extra-action-lounge-home-item.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const ExtraActionLoungeHomeItem = styled.a`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 42px;
+  height: 24px;
+  padding: 5px 9px;
+  border: 1px solid var(--color-mint);
+  border-radius: 100px;
+  margin-left: 6px;
+  font-size: 12px;
+  font-weight: bold;
+  text-decoration: none;
+  color: var(--color-mint);
+`
--- packages/public-header/src/public-header-deeplink.tsx
+++ packages/public-header/src/public-header-deeplink.tsx
@@ -2,14 +2,16 @@ import { useTranslation } from '@titicaca/next-i18next'
 import { useEventTrackerWithMetadata } from '@titicaca/react-contexts'
 
 import { ExtraActionItem } from './extra-action-item'
+import { ExtraActionLoungeHomeItem } from './extra-action-lounge-home-item'
 import { ExtraActionSeperator } from './extra-action-seperator'
 import { useDeeplinkHref } from './use-deeplink-href'
 
 interface Props {
   deeplinkPath: string
+  isLoungeHome?: boolean
 }
 
-export function PublicHeaderDeeplink({ deeplinkPath }: Props) {
+export function PublicHeaderDeeplink({ deeplinkPath, isLoungeHome }: Props) {
   const { t } = useTranslation('common-web')
 
   const trackEventWithMetadata = useEventTrackerWithMetadata()
@@ -17,20 +19,38 @@ export function PublicHeaderDeeplink({ deeplinkPath }: Props) {
 
   return (
     <>
-      <ExtraActionSeperator />
-      <ExtraActionItem
-        href={deeplinkHref}
-        onClick={() =>
-          trackEventWithMetadata({
-            ga: ['헤더_설치유도_선택', '앱에서 보기'],
-            pixel: {
-              action: '헤더_설치유도_선택',
-            },
-          })
-        }
-      >
-        {t(['aebeseo-bogi', '앱에서 보기'])}
-      </ExtraActionItem>
+      {isLoungeHome ? (
+        <ExtraActionLoungeHomeItem
+          href={deeplinkHref}
+          onClick={() =>
+            trackEventWithMetadata({
+              ga: ['헤더_설치유도_선택', '앱에서 보기'],
+              pixel: {
+                action: '헤더_설치유도_선택',
+              },
+            })
+          }
+        >
+          APP
+        </ExtraActionLoungeHomeItem>
+      ) : (
+        <>
+          <ExtraActionSeperator />
+          <ExtraActionItem
+            href={deeplinkHref}
+            onClick={() =>
+              trackEventWithMetadata({
+                ga: ['헤더_설치유도_선택', '앱에서 보기'],
+                pixel: {
+                  action: '헤더_설치유도_선택',
+                },
+              })
+            }
+          >
+            {t(['aebeseo-bogi', '앱에서 보기'])}
+          </ExtraActionItem>
+        </>
+      )}
     </>
   )
 }
