d44e73dbf4f1a3c3aadfc90985d239eef47490f3
finn <finn@triple-corp.com>
2023-01-25 15:15:20 +0900

    Fix None effect type

--- packages/triple-header/src/effects/none.tsx
+++ packages/triple-header/src/effects/none.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion'
 import { PropsWithChildren } from 'react'
 
-export default function None({ children }: PropsWithChildren<unknown>) {
+export default function None({ children }: PropsWithChildren) {
   return (
     <motion.div
       style={{ position: 'absolute', top: 0, width: '100%', height: '100%' }}
