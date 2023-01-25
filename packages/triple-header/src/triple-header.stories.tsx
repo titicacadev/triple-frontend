0027228173429d6177b74ebd2ddc96e94ab0a05c
finn <finn@triple-corp.com>
2023-01-25 15:18:15 +0900

    Fix TripleHeader storybook type

--- packages/triple-header/src/triple-header.stories.tsx
+++ packages/triple-header/src/triple-header.stories.tsx
@@ -1,11 +1,11 @@
-import { Meta } from '@storybook/react'
+import { ComponentMeta } from '@storybook/react'
 
 import TripleHeader from './triple-header'
 import SAMPLE from './mocks/triple-header.sample.json'
 
 export default {
   title: 'triple-header / TripleHeader',
-} as Meta
+} as ComponentMeta<typeof TripleHeader>
 
 export function Sample() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
