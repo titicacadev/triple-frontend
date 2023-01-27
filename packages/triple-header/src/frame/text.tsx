8e73ab2d4ec414cb0645926b4153758fc74acfc8
guswl98 <hjee0901@gmail.com>
2023-01-27 14:20:15 +0900

    frame에서 Link 클릭 기능

--- packages/triple-header/src/frame/text.tsx
+++ packages/triple-header/src/frame/text.tsx
@@ -1,25 +1,39 @@
 import { Text } from '@titicaca/core-elements'
 
 import { MotionContainer } from '../motion-container'
+import { Link, LinkEventHandler } from '../types'
 
+import { generateLinkClickHandler } from './common'
 import { EFFECTS, Effect } from './effects'
 
 export type TextFrame = { type: 'text' } & TextFrameProps
 interface TextFrameProps {
   value: {
-    text: string
+    text: {
+      content: string
+      link?: Link
+    }
   }
   width?: number
   height?: number
   effect?: Effect
+  onLinkClick?: LinkEventHandler
 }
 
-export function TextFrame({ value: { text }, effect }: TextFrameProps) {
+export function TextFrame({
+  value: { text },
+  effect,
+  onLinkClick,
+}: TextFrameProps) {
   const EffectElement = effect ? EFFECTS[effect.type] : MotionContainer
 
   return (
     <EffectElement options={effect?.options}>
-      <Text>{text}</Text>
+      <Text
+        onClick={(e) => generateLinkClickHandler(onLinkClick)(e, text.link)}
+      >
+        {text.content}
+      </Text>
     </EffectElement>
   )
 }
