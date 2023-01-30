41fb2e32aaa7b4e839272ce9a8c9476ef8b1936d
hoolie-triple <sohyun4283@naver.com>
2023-01-30 10:47:46 +0900

    docs(core-elements): rating story 경우의 수 추가

--- packages/core-elements/src/elements/rating/rating.stories.tsx
+++ packages/core-elements/src/elements/rating/rating.stories.tsx
@@ -1,16 +1,43 @@
-import { ComponentStoryObj, Meta } from '@storybook/react'
+import {
+  ComponentMeta,
+  ComponentStory,
+  ComponentStoryObj,
+} from '@storybook/react'
 
 import { Rating } from './rating'
 
 export default {
   title: 'core-elements / Rating',
   component: Rating,
-} as Meta
+} as ComponentMeta<typeof Rating>
 
 export const Basic: ComponentStoryObj<typeof Rating> = {
-  name: '리뷰점수',
   args: {
     size: 'tiny',
     score: 5,
   },
+}
+
+export const Score: ComponentStory<typeof Rating> = () => {
+  return (
+    <>
+      <Rating size="tiny" score={3} />
+      <br />
+      <Rating size="tiny" score={-6} />
+      <br />
+      <Rating size="tiny" score={11} />
+    </>
+  )
+}
+
+export const Size: ComponentStory<typeof Rating> = () => {
+  return (
+    <>
+      <Rating size="tiny" score={3} />
+      <br />
+      <Rating size="small" score={3} />
+      <br />
+      <Rating size="medium" score={3} />
+    </>
+  )
 }
