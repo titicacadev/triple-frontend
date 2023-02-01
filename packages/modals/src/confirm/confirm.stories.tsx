d8e14842bc780bfeedca00d71e5f06e493d3dd17
hoolie-triple <sohyun4283@naver.com>
2023-02-01 15:02:53 +0900

    docs(modals): Controlled 스토리 추가

--- packages/modals/src/confirm/confirm.stories.tsx
+++ packages/modals/src/confirm/confirm.stories.tsx
@@ -1,4 +1,9 @@
-import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import {
+  ComponentMeta,
+  ComponentStory,
+  ComponentStoryObj,
+} from '@storybook/react'
+import { useState } from 'react'
 
 import { Confirm } from './confirm'
 
@@ -22,4 +27,22 @@ export const Default: ComponentStoryObj<typeof Confirm> = {
     cancelText: '예약 취소',
     confirmText: '네, 진행합니다.',
   },
+}
+
+export const Controlled: ComponentStory<typeof Confirm> = () => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <>
+      <button onClick={() => setOpen(true)}>열기</button>
+      <Confirm
+        open={open}
+        title="요금 변동을 확인해주세요"
+        onClose={() => setOpen(false)}
+      >
+        유류할증료 및 기타세금에 변동이 있어 항공권의 총 요금이 변경되었습니다.
+        이 요금으로 결제를 진행하시겠습니까?
+      </Confirm>
+    </>
+  )
 }
