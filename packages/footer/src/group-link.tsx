af774274c53198f7ed4492e8ae95115106ea1530
YWTechIT <ywtechit@gmail.com>
2023-02-01 20:21:21 +0900

    Modularzation GroupLink component

---
+++ packages/footer/src/group-link.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import { Container } from '@titicaca/core-elements'
+
+export const LinksContainer = styled(Container)`
+  font-size: 11px;
+  font-weight: bold;
+  line-height: 20px;
+  color: var(--color-gray);
+
+  a {
+    color: var(--color-gray);
+    text-decoration: none;
+    margin: 6px;
+  }
+
+  a:first-child {
+    margin-left: 0;
+  }
+`
+export function GroupLink() {
+  return (
+    <LinksContainer>
+      <a href="/pages/tos.html" target="_blank" rel="noreferrer">
+        서비스 이용약관
+      </a>
+      |
+      <a href="/pages/privacy-policy.html" target="_blank" rel="noreferrer">
+        개인정보 처리방침
+      </a>
+      |
+      <a href="/cs-bridge/entry" target="_blank" rel="noreferrer">
+        고객센터
+      </a>
+    </LinksContainer>
+  )
+}
--- packages/footer/src/index.ts
+++ packages/footer/src/index.ts
@@ -1,5 +1,7 @@
 import DefaultFooter from './default-footer'
 
+export * from './group-link'
+
 export { default as LogoFooter } from './logo-footer'
 
 export default DefaultFooter
