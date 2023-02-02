2a872b36ca101a0a914db14a7f89f4e84ecefe49
YWTechIT <ywtechit@gmail.com>
2023-02-02 17:20:21 +0900

    Add AwardFooter component, stories

---
+++ packages/footer/src/award-footer.stories.tsx
@@ -0,0 +1,10 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+
+import { AwardFooter } from './award-footer'
+
+export default {
+  title: 'footer / AwardFooter',
+  component: AwardFooter,
+} as ComponentMeta<typeof AwardFooter>
+
+export const Basic: ComponentStoryObj<typeof AwardFooter> = {}
