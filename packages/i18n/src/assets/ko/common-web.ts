f4a71ec66a9c394e2b51fcff81ab11309545f8f5
jaehyeon48 <imjaehyeon48@gmail.com>
2023-02-03 12:07:15 +0900

    문구에서 '들' 제거

--- packages/i18n/src/assets/ko/common-web.ts
+++ packages/i18n/src/assets/ko/common-web.ts
@@ -190,6 +190,6 @@ export const koCommonWeb = {
   gwangwang: '관광',
   masjib: '맛집',
   'soljighan-ribyu-110mangaewa-coesin-yeohaeng-sosigdeulggaji-aebeseo-pyeonhage-hwaginhaseyo':
-    '솔직한 리뷰 110만개와 최신 여행 소식들까지 앱에서 편하게 확인하세요!',
+    '솔직한 리뷰 110만개와 최신 여행 소식까지 앱에서 편하게 확인하세요!',
   'yeogineun-teuripeul-aebi-pilyohaeyo': '여기는 트리플 앱이 필요해요',
 } as const
