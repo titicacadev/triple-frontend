05763856618b18de2a9c9e177060346d46c7909e
jaehyeon48 <imjaehyeon48@gmail.com>
2023-02-15 18:22:13 +0900

    scroll-spy 테스트 로직 수정

--- packages/scroll-spy/src/index.test.tsx
+++ packages/scroll-spy/src/index.test.tsx
@@ -1,29 +1,30 @@
 import { useEffect } from 'react'
-import IntersectionObserver from '@titicaca/intersection-observer'
 import { render } from '@testing-library/react'
 
 import '@testing-library/jest-dom'
 
 import { ScrollSpyContainer, ScrollSpyEntity } from './index'
 
-jest.mock('@titicaca/intersection-observer')
+jest.mock('@titicaca/intersection-observer', () => ({
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  StaticIntersectionObserver: jest
+    .fn()
+    .mockImplementation(({ onChange, children }) => {
+      useEffect(() => {
+        onChange(
+          { isIntersecting: true } as IntersectionObserverEntry,
+          jest.fn(),
+        )
+      }, [onChange])
+
+      return <div>{children}</div>
+    }),
+}))
 jest.mock('@titicaca/react-hooks', () => ({
   useScrollToElement: () => ({ isScrolling: () => {} }),
 }))
 
 test('ScrollSpyEntity로 감싼 영역이 화면에 들어오면 ScrollSpyContainer의 onChange로 전달한 함수를 호출합니다.', () => {
-  ;(
-    IntersectionObserver as unknown as jest.MockedFunction<
-      typeof IntersectionObserver
-    >
-  ).mockImplementation(({ onChange, children }) => {
-    useEffect(() => {
-      onChange({ isIntersecting: true } as IntersectionObserverEntry, jest.fn())
-    }, [onChange])
-
-    return <div>{children}</div>
-  })
-
   const targetId = 'target-id'
   const handleActiveIdChange = jest.fn()
 
