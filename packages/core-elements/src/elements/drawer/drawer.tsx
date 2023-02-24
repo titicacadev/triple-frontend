82a78ba234e8d7bf0b47dbdee9c1b2d57f7a4ee1
Heeryong Kang <drakang4@gmail.com>
2023-02-24 16:49:23 +0900

    fix(core-elements): Drawer는 기본적으로 unmountOnExit

--- packages/core-elements/src/elements/drawer/drawer.tsx
+++ packages/core-elements/src/elements/drawer/drawer.tsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
-import { CSSTransition } from 'react-transition-group'
-import { ReactNode, useRef } from 'react'
+import { Transition, TransitionStatus } from 'react-transition-group'
+import { CSSProperties, ReactNode, useRef } from 'react'
 import { Portal } from '@headlessui/react'
 
-import { LayeringMixinProps } from '../../mixins'
 import { Container } from '../container'
 
 const TRANSITION_DURATION = 300
@@ -13,102 +12,74 @@ interface DrawerContainerProps {
   duration: number
 }
 
-const inactiveDrawerStyle = `
-  transform: translateY(100%);
-`
-
-const activeDrawerStyle = `
-  transform: translateY(0%);
-`
-
-const drawerTransitionConfig = `
-  transition: transform ${TRANSITION_DURATION}ms ease-in-out;
-`
-
-const DrawerContainer = styled.div<DrawerContainerProps & LayeringMixinProps>`
+const DrawerContainer = styled.div<DrawerContainerProps>`
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
   overflow: ${({ overflow }) => overflow || 'hidden'};
   z-index: 9999;
-
-  &:not([class*='drawer-slide-']) {
-    ${inactiveDrawerStyle}
-
-    display: none;
-  }
-
-  &.drawer-slide-appear,
-  &.drawer-slide-enter {
-    ${inactiveDrawerStyle}
-  }
-
-  &.drawer-slide-appear-active,
-  &.drawer-slide-enter-active {
-    ${activeDrawerStyle}
-    ${drawerTransitionConfig}
-  }
-
-  &.drawer-slide-enter-done {
-    ${activeDrawerStyle}
-  }
-
-  &.drawer-slide-exit {
-    ${activeDrawerStyle}
-  }
-
-  &.drawer-slide-exit-active {
-    ${inactiveDrawerStyle}
-    ${drawerTransitionConfig}
-  }
-
-  &.drawer-slide-exit-done {
-    ${inactiveDrawerStyle}
-
-    display: none;
-  }
 `
+
+const transitionStyles: Record<TransitionStatus, CSSProperties> = {
+  entering: {
+    transform: 'translateY(0%)',
+  },
+  entered: {
+    transform: 'translateY(0%)',
+  },
+  exiting: {
+    transform: 'translateY(100%)',
+  },
+  exited: {
+    transform: 'translateY(100%)',
+  },
+  unmounted: {},
+}
 
 export function Drawer({
   active,
   overflow,
   children,
-  unmountOnExit,
 }: {
   active?: boolean
   overflow?: string
-  unmountOnExit?: boolean
   children?: ReactNode
 }) {
   const drawerContainerRef = useRef<HTMLDivElement>(null)
 
   return (
-    <CSSTransition
+    <Transition
       nodeRef={drawerContainerRef}
       in={active}
       appear
-      classNames="drawer-slide"
       timeout={TRANSITION_DURATION}
-      mountOnEnter={unmountOnExit}
-      unmountOnExit={unmountOnExit}
+      mountOnEnter
+      unmountOnExit
     >
-      <Portal>
-        <DrawerContainer
-          ref={drawerContainerRef}
-          duration={TRANSITION_DURATION}
-          overflow={overflow}
-        >
-          <Container
-            centered
-            css={{
-              maxWidth: 768,
+      {(transitionStatus) => (
+        <Portal>
+          <DrawerContainer
+            ref={drawerContainerRef}
+            duration={TRANSITION_DURATION}
+            overflow={overflow}
+            style={{
+              transition: `transform ${TRANSITION_DURATION}ms ease-in-out`,
+              transform: 'translateY(100%)',
+              ...transitionStyles[transitionStatus],
             }}
           >
-            {children}
-          </Container>
-        </DrawerContainer>
-      </Portal>
-    </CSSTransition>
+            <Container
+              centered
+              css={{
+                maxWidth: 768,
+              }}
+            >
+              {children}
+            </Container>
+          </DrawerContainer>
+        </Portal>
+      )}
+    </Transition>
   )
 }
