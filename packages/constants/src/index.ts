84f55064fd7175ef734f767e816fb2a7c8c6b278
YWTechIT <ywtechit@gmail.com>
2023-03-03 13:50:24 +0900

    feat: constant 값 추가

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -2,3 +2,6 @@ export * from './regex'
 
 /** API 호출 시 필요한 사용자 세션 HTTP 헤더 키  */
 export const SESSION_KEY = 'x-soto-session'
+export const FOO = 'foo'
+export const BAR = 'bar'
+export const BAZ = 'baz'
