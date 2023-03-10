c8df25e2847b7a1feaede0a2f91dca91980b7b7c
JeongHyeon <37819666+sohnjunior@users.noreply.github.com>
2023-03-10 17:49:14 +0900

    test(core-elements): list 유닛 테스트 추가

---
+++ packages/core-elements/src/elements/list/list.test.tsx
@@ -0,0 +1,46 @@
+import renderer from 'react-test-renderer'
+
+import { List } from './list'
+
+import 'jest-styled-components'
+
+it('should accept weight divided prop', () => {
+  const tree = renderer
+    .create(
+      <List divided dividerWeight={10}>
+        <List.Item />
+      </List>,
+    )
+    .toJSON()
+
+  expect(tree).toHaveStyleRule('border-bottom', 'solid 10px #efefef', {
+    modifier: '> li:not(:last-child)::after',
+  })
+  expect(tree).toHaveStyleRule('content', "''", {
+    modifier: '> li:not(:last-child)::after',
+  })
+})
+
+it('should accept marker prop', () => {
+  const tree = renderer
+    .create(
+      <List marker>
+        <List.Item />
+      </List>,
+    )
+    .toJSON()
+
+  expect(tree).toHaveStyleRule('content', "'·'", { modifier: '> li::before' })
+})
+
+it('should override style with css prop', () => {
+  const tree = renderer
+    .create(
+      <List css={{ color: 'red' }}>
+        <List.Item />
+      </List>,
+    )
+    .toJSON()
+
+  expect(tree).toHaveStyleRule('color', 'red')
+})
