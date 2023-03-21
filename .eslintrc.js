66d21a849f5cfac57c01025af260b6c28a216a89
Heeryong Kang <drakang4@gmail.com>
2023-03-21 12:19:51 +0900

    chore: eslint 캐시 가능하도록 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,9 @@
 /** @type import('eslint').Linter.Config */
 module.exports = {
+  root: true,
   extends: [
     '@titicaca/eslint-config-triple',
     '@titicaca/eslint-config-triple/frontend',
     '@titicaca/eslint-config-triple/prettier',
-  ],
-  parserOptions: {
-    project: './tsconfig.eslint.json',
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      settings: {
-        'import/resolver': {
-          typescript: {
-            project: 'packages/*/tsconfig.json',
-          },
-        },
-      },
-    },
   ],
 }
