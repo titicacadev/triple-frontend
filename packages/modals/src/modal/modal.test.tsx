04544e2e632dedc530d814a7b3e6344c4222ee0e
Heeryong Kang <drakang4@gmail.com>
2023-03-28 14:55:58 +0900

    test: action-sheet, modal, popup 접근성 테스트 추가

---
+++ packages/modals/src/modal/modal.test.tsx
@@ -0,0 +1,104 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import { Modal } from './modal'
+
+test('올바른 aria attributes를 가집니다.', () => {
+  const onClose = jest.fn()
+
+  render(
+    <Modal open onClose={onClose}>
+      <Modal.Body>
+        <Modal.Title>Title</Modal.Title>
+        <Modal.Description>Description</Modal.Description>
+      </Modal.Body>
+      <Modal.Actions>
+        <Modal.Action>Close</Modal.Action>
+      </Modal.Actions>
+    </Modal>,
+  )
+
+  const modal = screen.getByRole('dialog')
+
+  expect(modal).toHaveAttribute('role', 'dialog')
+  expect(modal).toHaveAttribute('aria-modal', 'true')
+  expect(modal).toHaveAttribute('aria-labelledby', screen.getByText('Title').id)
+  expect(modal).toHaveAttribute(
+    'aria-describedby',
+    screen.getByText('Description').id,
+  )
+})
+
+test('외부를 클릭하면 닫습니다.', async () => {
+  const user = userEvent.setup()
+
+  const onClose = jest.fn()
+
+  render(
+    <Modal open onClose={onClose}>
+      <Modal.Body>
+        <Modal.Title>Title</Modal.Title>
+        <Modal.Description>Description</Modal.Description>
+      </Modal.Body>
+      <Modal.Actions>
+        <Modal.Action>Close</Modal.Action>
+      </Modal.Actions>
+    </Modal>,
+  )
+
+  await user.click(document.body)
+
+  expect(onClose).toHaveBeenCalledTimes(1)
+})
+
+test('ESC 키를 누르면 닫습니다.', async () => {
+  const user = userEvent.setup()
+
+  const onClose = jest.fn()
+
+  render(
+    <Modal open onClose={onClose}>
+      <Modal.Body>
+        <Modal.Title>Title</Modal.Title>
+        <Modal.Description>Description</Modal.Description>
+      </Modal.Body>
+      <Modal.Actions>
+        <Modal.Action>Close</Modal.Action>
+      </Modal.Actions>
+    </Modal>,
+  )
+
+  await user.keyboard('{Escape}')
+
+  expect(onClose).toHaveBeenCalledTimes(1)
+})
+
+test('focus trap을 사용합니다.', async () => {
+  const user = userEvent.setup()
+
+  const onClose = jest.fn()
+
+  render(
+    <Modal open onClose={onClose}>
+      <Modal.Body>
+        <button>Button 1</button>
+        <button>Button 2</button>
+      </Modal.Body>
+      <Modal.Actions>
+        <Modal.Action>Close</Modal.Action>
+      </Modal.Actions>
+    </Modal>,
+  )
+
+  await user.tab()
+
+  expect(document.activeElement).toHaveTextContent('Button 1')
+
+  await user.tab()
+
+  expect(document.activeElement).toHaveTextContent('Button 2')
+
+  await user.tab()
+
+  expect(document.activeElement).toHaveTextContent('Button 1')
+})
