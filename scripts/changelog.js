e13b53fcd8aab09429e17d5b654ddc2996cf2891
jaehyeon48 <imjaehyeon48@gmail.com>
2023-04-17 14:50:13 +0900

    removeEmojis 함수 제거

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -121,20 +121,6 @@ Output markdown:
 `
 }
 
-function removeEmojis(str) {
-  const emojiRegex =
-    // eslint-disable-next-line no-misleading-character-class
-    /([\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}\u{1F191}-\u{1F251}\u{FE0E}\u{FE0F}]|[\u{1F3FB}-\u{1F3FF}][\u{1F9B0}-\u{1F9B3}])|([\u{200D}\u{FE0E}\u{FE0F}]|[\u{1F3FB}-\u{1F3FF}])/gu
-  const emojiPresentationRegex = /\p{Emoji_Presentation}/gu
-  const emojiModifierRegex = /\p{Emoji_Modifier_Base}\p{Emoji_Modifier}?/gu
-
-  return str
-    .replace(emojiRegex, '')
-    .replace(emojiPresentationRegex, '')
-    .replace(emojiModifierRegex, '')
-    .trim()
-}
-
 function groupPullRequestsByPackage(pullRequests) {
   const groupedPullRequests = pullRequests.reduce(
     (result, { packages, title, number, url }) => {
@@ -189,7 +175,7 @@ async function fetchPrsInMilestone() {
       title,
       number,
       url,
-      packages: labels.map(({ name }) => removeEmojis(name)),
+      packages: labels.map(({ name }) => name),
     }))
     .sort((a, b) => a.number - b.number)
 
