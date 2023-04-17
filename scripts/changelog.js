db8201d96a5310cb18440fd3df05de9beb713289
jaehyeon48 <imjaehyeon48@gmail.com>
2023-04-17 14:49:02 +0900

    removeEmojis 함수 로직 수정

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 
 const fetch = require('node-fetch')
+const emojiRegex = require('emoji-regex')
 
 function generateChatGptPrompt(inputData) {
   return `
@@ -122,16 +123,13 @@ Output markdown:
 }
 
 function removeEmojis(str) {
-  const emojiRegex =
-    // eslint-disable-next-line no-misleading-character-class
-    /([\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F900}-\u{1F9FF}\u{1F1E0}-\u{1F1FF}\u{1F191}-\u{1F251}\u{FE0E}\u{FE0F}]|[\u{1F3FB}-\u{1F3FF}][\u{1F9B0}-\u{1F9B3}])|([\u{200D}\u{FE0E}\u{FE0F}]|[\u{1F3FB}-\u{1F3FF}])/gu
-  const emojiPresentationRegex = /\p{Emoji_Presentation}/gu
-  const emojiModifierRegex = /\p{Emoji_Modifier_Base}\p{Emoji_Modifier}?/gu
+  const emojiShortCodeRegex = /:.*:/gu
+  const variationSelectorRegex = /[\uFE00-\uFE0F]/g
 
   return str
-    .replace(emojiRegex, '')
-    .replace(emojiPresentationRegex, '')
-    .replace(emojiModifierRegex, '')
+    .replace(emojiRegex(), '')
+    .replace(emojiShortCodeRegex, '')
+    .replace(variationSelectorRegex, '')
     .trim()
 }
 
