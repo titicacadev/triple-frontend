694b2000e1d6736603d765d47a32447143a7aa90
jaehyeon48 <imjaehyeon48@gmail.com>
2023-05-05 17:27:45 +0900

    emoji 제거하는 함수 사용하지 않도록 하고, npm i 대신 npm ci 사용

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -3,7 +3,6 @@
 const fs = require('fs')
 
 const fetch = require('node-fetch')
-const emojiRegex = require('emoji-regex')
 
 function generateChatGptPrompt(inputData) {
   return `
@@ -122,17 +121,6 @@ Output markdown:
 `
 }
 
-function removeEmojis(str) {
-  const emojiShortCodeRegex = /:.*:/gu
-  const variationSelectorRegex = /[\uFE00-\uFE0F]/g
-
-  return str
-    .replace(emojiRegex(), '')
-    .replace(emojiShortCodeRegex, '')
-    .replace(variationSelectorRegex, '')
-    .trim()
-}
-
 function groupPullRequestsByPackage(pullRequests) {
   const groupedPullRequests = pullRequests.reduce(
     (result, { packages, title, number, url }) => {
@@ -187,7 +175,7 @@ async function fetchPrsInMilestone() {
       title,
       number,
       url,
-      packages: labels.map(({ name }) => removeEmojis(name)),
+      packages: labels.map(({ name }) => name),
     }))
     .sort((a, b) => a.number - b.number)
 
