68fe32769cbee059a516f028cbeb4cc307bf1193
Hajung Song <dongoc21hj@gmail.com>
2023-05-23 10:40:49 +0900

    article script의 date 형식 validation 추가

--- packages/meta-tags/src/article-script.tsx
+++ packages/meta-tags/src/article-script.tsx
@@ -5,8 +5,8 @@ import { filterValidValue } from './utils'
 interface ArticleScriptProps {
   headline: string
   image?: string[]
-  datePublished?: Date
-  dateModified?: Date
+  datePublished?: string
+  dateModified?: string
   author?: ArticleAuthor[]
   publisher?: ArticlePublisher[]
 }
@@ -34,8 +34,8 @@ export function ArticleScript({
     '@type': 'Article',
     headline,
     image,
-    datePublished: datePublished ? datePublished.toISOString() : undefined,
-    dateModified: dateModified ? dateModified.toISOString() : undefined,
+    datePublished: toISOString(datePublished),
+    dateModified: toISOString(dateModified),
     publisher,
     author:
       author?.map((author) =>
@@ -54,4 +54,16 @@ export function ArticleScript({
       </script>
     </Head>
   )
+}
+
+function toISOString(dateString: string | undefined) {
+  if (!dateString) {
+    return
+  }
+
+  // safari에서 '-'가 들어간 날짜 string을 invalid date로 취급합니다.
+  const date = new Date(dateString.replace(/-/g, '/'))
+  const isValidDate = date instanceof Date && !isNaN(date.getTime())
+
+  return isValidDate ? date.toISOString() : undefined
 }
