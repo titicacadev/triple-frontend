1874fa245658e2894eb6a2cb3f3daa420f4fcb29
Heeryong Kang <drakang4@gmail.com>
2023-05-27 17:38:05 +0900

    chore: update graphql codegen

---
+++ packages/review/codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'https://triple-dev.titicaca-corp.com/api/graphql',
+  documents: 'src/data/graphql/*.graphql',
+  generates: {
+    'src/data/graphql/generated.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-generic-sdk',
+      ],
+    },
+  },
+}
+
+export default config
