b7e097121ac62c1264232a51bdaca8f55a29d9e8
choisohyun <sohyun4283@naver.com>
2023-06-05 15:35:04 +0900

    fix: pusher 변경 사항 적용

--- packages/chat/src/chat/chat.tsx
+++ packages/chat/src/chat/chat.tsx
@@ -1,12 +1,6 @@
 import { IncomingMessage } from 'http'
 
-import React, {
-  useCallback,
-  useEffect,
-  useMemo,
-  useReducer,
-  useRef,
-} from 'react'
+import React, { useCallback, useEffect, useMemo, useReducer } from 'react'
 import { StaticIntersectionObserver as IntersectionObserver } from '@titicaca/intersection-observer'
 import { useUserAgentContext } from '@titicaca/react-contexts'
 import { closeKeyboard } from '@titicaca/triple-web-to-native-interfaces'
@@ -24,15 +18,17 @@ import {
 } from '../types'
 import ChatBubble from '../chat-bubble'
 import { HiddenElement } from '../chat-bubble/elements'
-import { Polling } from '../utils'
 
 import { ChatActions, ChatReducer, initialChatState } from './reducer'
 import { useChat } from './chat-context'
+import { useChatPusher } from './use-chat-pusher'
+import { getChatListHeight, useScrollContext } from './scroll-context'
 
-const FETCH_INTERVAL_SECS = 5
 const MINIMUM_INTERSECTING_TIME = 3000
+export const CHAT_CONTAINER_ID = 'chat-inner-container'
 
 export interface ChatProps {
+  pusherKey: string
   displayTarget: UserType
   /**
    * me(sender), others(receiver)에 대한 기본 정보
@@ -64,6 +60,7 @@ export interface ChatProps {
  * ChatContainer로 감싸서 함께 사용해야 합니다.
  */
 export const Chat = ({
+  pusherKey,
   displayTarget,
   userInfo,
   room,
@@ -76,22 +73,22 @@ export const Chat = ({
   showFailToast,
   ...props
 }: ChatProps) => {
-  const chatRoomRef = useRef<HTMLDivElement>(null)
+  const { chatRoomRef, bottomRef, setScrollY, scrollToBottom } =
+    useScrollContext()
 
   const { setPostMessage } = useChat()
   const [
     {
       messages,
       hasPrevMessage,
-      scrollY,
       otherUnreadInfo,
       lastMessageId,
       firstMessageId,
     },
     dispatch,
   ] = useReducer(ChatReducer, initialChatState)
+  useChatPusher({ pusherKey, room, notifyNewMessage, dispatch })
 
-  const fetchJob = useMemo(() => new Polling(FETCH_INTERVAL_SECS * 1000), [])
   const { os } = useUserAgentContext()
   const isIos = useMemo(() => os.name === 'iOS', [os.name])
 
@@ -113,23 +110,6 @@ export const Chat = ({
 
     return hasUnread
   }, [lastMessageId, getUnreadRoom, room.id])
-
-  const scrollDown = useCallback(() => {
-    window.setTimeout(() => {
-      if (chatRoomRef.current) {
-        chatRoomRef.current.lastElementChild?.lastElementChild?.scrollIntoView()
-      }
-    }, 0)
-  }, [])
-
-  useEffect(() => {
-    if (scrollY && chatRoomRef.current) {
-      window.scrollTo(
-        0,
-        chatRoomRef.current.getBoundingClientRect().height - scrollY,
-      )
-    }
-  }, [scrollY])
 
   useEffect(() => {
     const chatListDiv = chatRoomRef.current
@@ -173,38 +153,10 @@ export const Chat = ({
       await updateUnread()
 
       window.setTimeout(() => {
-        scrollDown()
+        scrollToBottom()
       }, 0)
     })()
   }, [room, initMessages]) // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
-    fetchJob.add('refreshChatRoom', () => pollingFetchJob())
-    return () => fetchJob.remove('refreshChatRoom')
-  }, [lastMessageId]) // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
-    function pauseFetchJob() {
-      document.hidden ? fetchJob.pause() : fetchJob.resume(true)
-    }
-
-    document.addEventListener('visibilitychange', pauseFetchJob)
-
-    return () => {
-      document.removeEventListener('visibilitychange', pauseFetchJob)
-    }
-  }, [fetchJob])
-
-  async function fetchNewMessages(): Promise<MessageInterface[]> {
-    if (lastMessageId !== null && room.id) {
-      return getMessages({
-        roomId: room.id,
-        lastMessageId,
-      })
-    } else {
-      return []
-    }
-  }
 
   async function fetchPastMessages(): Promise<MessageInterface[]> {
     if (messages.length) {
@@ -218,41 +170,10 @@ export const Chat = ({
     }
   }
 
-  async function pollingFetchJob() {
-    if (lastMessageId === null || !room.id || !getUnreadRoom) {
-      return
-    }
-    const { hasUnread, others } = await getUnreadRoom({
-      roomId: room.id,
-      lastSeenMessageId: lastMessageId,
-    })
-
-    dispatch({
-      action: ChatActions.UPDATE,
-      otherUnreadInfo: others,
-    })
-
-    if (!hasUnread) {
-      return
-    }
-    const newMessages = await fetchNewMessages()
-    if (!newMessages?.length) {
-      return
-    }
-
-    dispatch({
-      action: ChatActions.NEW,
-      messages: newMessages,
-    })
-    notifyNewMessage?.({ ...newMessages[notifyNewMessage.length - 1] })
-    scrollDown()
-  }
-
   const postMessageAction = async (
     payload: TextPayload | ImagePayload,
     retry = false,
   ): Promise<boolean> => {
-    fetchJob.pause()
     const result = await postMessage?.(payload)
     const { success, newMessages } = result || {
       success: false,
@@ -283,9 +204,7 @@ export const Chat = ({
       showFailToast?.('메시지 발송에 실패했습니다.')
     }
 
-    scrollDown()
-
-    await fetchJob.resume()
+    scrollToBottom()
 
     return success
   }
@@ -294,22 +213,37 @@ export const Chat = ({
     postMessage && setPostMessage?.(() => postMessageAction)
   }, [postMessage, setPostMessage]) // eslint-disable-line react-hooks/exhaustive-deps
 
+  /** 채팅창 메시지(lastMessageId)가 업데이트 될 경우 채팅창 스크롤을 맨 아래로 이동 */
+  const chatRoomResizeObserver = useMemo(
+    () => new ResizeObserver(scrollToBottom),
+    [],
+  )
+
+  useEffect(() => {
+    if (!chatRoomRef?.current) {
+      return
+    }
+
+    chatRoomResizeObserver.observe(chatRoomRef.current)
+
+    return () => {
+      chatRoomResizeObserver.disconnect()
+    }
+  }, [lastMessageId, chatRoomResizeObserver])
+
   const onChangeScroll = async ({
     isIntersecting,
     time,
   }: IntersectionObserverEntry) => {
+    const prevScrollY = getChatListHeight()
     if (isIntersecting && time >= MINIMUM_INTERSECTING_TIME && hasPrevMessage) {
       const pastMessages = await fetchPastMessages()
-
-      const prevScrollY = chatRoomRef.current
-        ? chatRoomRef.current.getBoundingClientRect().height
-        : 0
 
       await dispatch({
         action: ChatActions.PAST,
         messages: pastMessages,
-        scrollY: prevScrollY,
       })
+      setScrollY(prevScrollY)
     }
   }
 
@@ -318,7 +252,7 @@ export const Chat = ({
       <IntersectionObserver onChange={onChangeScroll}>
         <HiddenElement />
       </IntersectionObserver>
-      <Container ref={chatRoomRef} {...props}>
+      <Container ref={chatRoomRef} id={CHAT_CONTAINER_ID} {...props}>
         <ul>
           {messages.map((message: MessageInterface, index) => (
             <li key={index}>
@@ -337,6 +271,7 @@ export const Chat = ({
           ))}
         </ul>
       </Container>
+      <HiddenElement ref={bottomRef} />
     </>
   )
 }
--- packages/chat/src/utils/index.ts
+++ packages/chat/src/utils/index.ts
@@ -1,2 +1,1 @@
 export * from './image'
-export * from './polling'
