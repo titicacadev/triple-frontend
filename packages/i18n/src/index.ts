1894d25cbaa8d696e8d7df5b8b64b5e2cbcd40d8
jaehyeon48 <imjaehyeon48@gmail.com>
2023-06-20 11:20:15 +0900

    i18n 타입, 상수 export

--- packages/i18n/src/index.ts
+++ packages/i18n/src/index.ts
@@ -3,3 +3,4 @@ import i18nConfig from './next-i18next.config'
 export { default as I18nextTripleWebAssetsBackend } from './backend'
 export { I18nCommonWebKeys } from './i18n-common-web-keys'
 export { i18nConfig }
+export * from './app-dir'
