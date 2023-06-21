e526c01f9b278225bf22e5f1016f01a783c830e7
jaehyeon48 <imjaehyeon48@gmail.com>
2023-06-21 14:54:38 +0900

    클라이언트 컴포넌트용 <Trans /> 컴포넌트 구현

--- packages/i18n/src/app-directory/client.tsx
+++ packages/i18n/src/app-directory/client.tsx
@@ -1,10 +1,26 @@
 'use client'
 
+import { ComponentProps } from 'react'
 import { useParams } from 'next/navigation'
+import { Trans as OriginalTrans } from 'react-i18next'
 
 import { initializeI18n, i18nInstance } from './server'
 import { FALLBACK_LANGUAGE, LANGUAGES } from './constants'
 import { Language } from './types'
+
+export function Trans(
+  props: ComponentProps<typeof OriginalTrans> & {
+    namespace: string
+  },
+) {
+  const { children, namespace, ...rest } = props
+
+  return (
+    <OriginalTrans i18n={i18nInstance} t={useTranslation(namespace)} {...rest}>
+      {children}
+    </OriginalTrans>
+  )
+}
 
 export function useCurrentLanguage() {
   const { lang } = useParams()
