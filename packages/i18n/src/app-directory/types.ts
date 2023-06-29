663a8d112d299881d9251105cd57b5782d52855a
jaehyeon48 <imjaehyeon48@gmail.com>
2023-06-29 11:37:03 +0900

    LangParams 인터페이스 제거

--- packages/i18n/src/app-directory/types.ts
+++ packages/i18n/src/app-directory/types.ts
@@ -1,7 +1,2 @@
 export type Language = 'ko' | 'en' | 'ja' | 'zh' | 'zh-TW'
 export type Namespace = 'local' | 'common-web' | 'common-admin'
-export interface LangParams {
-  params: {
-    lang: Language
-  }
-}
