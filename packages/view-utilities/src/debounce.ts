b39f6c65ed3835f63db0e7d17c5bfab475a573fa
dia-triple <dia99@interpark.com>
2023-06-30 12:36:44 +0900

    feat: debounce에 leading, trailing 옵션 추가

--- packages/view-utilities/src/debounce.ts
+++ packages/view-utilities/src/debounce.ts
@@ -1,12 +1,30 @@
+interface Option {
+  leading?: boolean
+  trailing?: boolean
+}
+
 export function debounce<Params extends unknown[]>(
   func: (...args: Params) => unknown,
   timeout: number,
+  option?: Option,
 ): (...args: Params) => void {
   let timer: ReturnType<typeof setTimeout>
+  let leadingCall = option?.leading ?? false
+  const trailing = option?.trailing ?? true
+
   return (...args: Params) => {
+    if (!timer && leadingCall) {
+      func(...args)
+    }
+
     clearTimeout(timer)
     timer = setTimeout(() => {
-      func(...args)
+      if (trailing || !leadingCall) {
+        func(...args)
+      }
+      if (leadingCall) {
+        leadingCall = false
+      }
     }, timeout)
   }
 }
