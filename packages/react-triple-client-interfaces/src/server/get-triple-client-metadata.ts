ef4c56be5ef0cc0d640fe8e1e45213d8a99dbefe
Hajung Song <dongoc21hj@gmail.com>
2023-07-03 15:39:54 +0900

    get-triple-client-metadata 함수를 server 폴더로 이동

--- packages/react-triple-client-interfaces/src/server/get-triple-client-metadata.ts
+++ packages/react-triple-client-interfaces/src/server/get-triple-client-metadata.ts
@@ -2,9 +2,11 @@
 
 import { headers } from 'next/headers'
 
-import { parseTripleClientUserAgent } from './triple-client-user-agent'
+import { parseTripleClientUserAgent } from '../triple-client-user-agent'
 
-/** (구)TripleClientMetadataProvider.getInitialProps의 next v13 app router 버전입니다. */
+/**
+  (구)TripleClientMetadataProvider.getInitialProps의 next v13 app router 버전입니다. 
+*/
 export function getTripleClientMetadata() {
   const headerInstance = headers()
   const serverSideUserAgent = headerInstance.get('user-agent')
