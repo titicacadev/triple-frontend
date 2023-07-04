4d9ca32abcab957f2c3ebc8905dd83c35e0f20d2
JeongHyeon <37819666+sohnjunior@users.noreply.github.com>
2023-07-04 16:55:35 +0900

    feat(triple-document): coupon 스토리 추가하고 CSF3 적용

--- packages/triple-document/src/triple-document.stories.tsx
+++ packages/triple-document/src/triple-document.stories.tsx
@@ -1,8 +1,10 @@
+import { rest } from 'msw'
 import { useEffect } from 'react'
-import { Meta } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 import { useScrollToAnchor } from '@titicaca/react-hooks'
 import { Container } from '@titicaca/core-elements'
 
+import { DeepLinkProvider } from './prop-context/deep-link'
 import MOCK_REGIONS from './mocks/triple-document.regions.json'
 import MOCK_EMBEDDED from './mocks/triple-document.embedded.json'
 import MOCK_ITINERARY from './mocks/triple-document.itinerary.json'
@@ -20,25 +22,27 @@ const {
   embedded: Embedded,
   anchor: Anchor,
   itinerary: Itinerary,
+  coupon: Coupon,
 } = ELEMENTS
 
 export default {
   title: 'triple-document / TripleDocument',
 } as Meta
 
-export function Sample() {
-  return (
+export const Sample: StoryObj<typeof TripleDocument> = {
+  name: '샘플',
+  render: () => (
     <Container centered css={{ maxWidth: 768 }}>
       <TripleDocument>
         {SAMPLE as TripleElementData<string, unknown>[]}
       </TripleDocument>
     </Container>
-  )
+  ),
 }
-Sample.storyName = '샘플'
 
-export function TextExample() {
-  return (
+export const TextExample: StoryObj<typeof Text> = {
+  name: '텍스트',
+  render: () => (
     <>
       <Text value={{ text: '텍스트: medium 16 80%' }} />
       <Text
@@ -48,89 +52,138 @@ export function TextExample() {
       />
       <Text bold value={{ text: '강조 텍스트: bold 16 100%' }} alpha={1} />
     </>
-  )
-}
-TextExample.storyName = '텍스트'
-
-export function NoteExample() {
-  return (
-    <Note
-      value={{
-        body: '목적지로 바로 가지 않고, 중간 지점에서 잠시 머무는 단기 체류를 뜻한다. 보통 경유 시간인 3-4시간 정도가 아니라 24시간 이상을 뜻하기 때문에 관광과 숙박이 가능한 것이 특징. 일부 항공사는 스탑오버시 무료 관광을 제공하니 참고할것!',
-        title: '잠깐! 스탑오버(Stopover)란?',
-      }}
-    />
-  )
-}
-NoteExample.storyName = '노트'
-
-export function VideoExample() {
-  return (
-    <Video
-      value={{
-        provider: 'youtube',
-        identifier: 'hYIe4VrfHoA',
-      }}
-    />
-  )
-}
-VideoExample.storyName = '비디오'
-
-export function TableExample() {
-  return (
-    <Table
-      value={{
-        table: {
-          type: 'horizontal',
-          head: [{ text: '취소 시점' }, { text: '취소 수수료' }],
-          body: [
-            [{ text: '구매 당일 (No-show 제외)' }, { text: '0원' }],
-            [{ text: '구매 익일 ~ 출발 61일 전' }, { text: '1,000원' }],
-            [{ text: '출발 60일 전 ~ 출발 31일 전' }, { text: '3,000원' }],
-            [{ text: '출발 30일 전 ~ 출발 8일 전' }, { text: '4,000원' }],
-            [{ text: '출발 7일 전 ~ 출발 2일 전' }, { text: '6,000원' }],
-            [{ text: '출발 1일 전 ~ 출발시간 전' }, { text: '12,000원' }],
-            [{ text: '출발시간 이후 (No-show)' }, { text: '15,000원' }],
-          ],
-        },
-      }}
-    />
-  )
-}
-TableExample.storyName = '표'
-
-export function Region() {
-  return <Regions value={{ regions: MOCK_REGIONS }} />
-}
-Region.storyName = '리전'
-
-export function EmbeddedExample() {
-  return <Embedded value={MOCK_EMBEDDED} />
-}
-EmbeddedExample.storyName = '임베딩'
-
-export function AnchorExample() {
-  useEffect(() => {
-    window.history.pushState(null, '', '#android')
-  }, [])
-
-  useScrollToAnchor({ delayTime: 0 })
-
-  return (
-    <div>
-      <div style={{ height: '200vh' }} />
-      <div>Android</div>
-      <Anchor value={{ href: 'android' }} />
-      <div style={{ height: '200vh' }} />
-      <div>ios</div>
-      <Anchor value={{ href: 'ios' }} />
-    </div>
-  )
-}
-AnchorExample.storyName = 'Anchor'
-
-export function DocumentItinerary() {
-  return <Itinerary value={MOCK_ITINERARY.article.source.body[1].value} />
+  ),
 }
 
-DocumentItinerary.storyName = '추천코스 기본'
+export const NoteExample: StoryObj<typeof Note> = {
+  name: '노트',
+  render: (args) => <Note value={args.value} />,
+  args: {
+    value: {
+      body: '목적지로 바로 가지 않고, 중간 지점에서 잠시 머무는 단기 체류를 뜻한다. 보통 경유 시간인 3-4시간 정도가 아니라 24시간 이상을 뜻하기 때문에 관광과 숙박이 가능한 것이 특징. 일부 항공사는 스탑오버시 무료 관광을 제공하니 참고할것!',
+      title: '잠깐! 스탑오버(Stopover)란?',
+    },
+  },
+}
+
+export const VideoExample: StoryObj<typeof Video> = {
+  name: '비디오',
+  render: (args) => <Video value={args.value} />,
+  args: {
+    value: {
+      provider: 'youtube',
+      identifier: 'hYIe4VrfHoA',
+    },
+  },
+}
+
+export const TableExample: StoryObj<typeof Table> = {
+  name: '표',
+  render: (args) => <Table value={args.value} />,
+  args: {
+    value: {
+      table: {
+        type: 'horizontal',
+        head: [{ text: '취소 시점' }, { text: '취소 수수료' }],
+        body: [
+          [{ text: '구매 당일 (No-show 제외)' }, { text: '0원' }],
+          [{ text: '구매 익일 ~ 출발 61일 전' }, { text: '1,000원' }],
+          [{ text: '출발 60일 전 ~ 출발 31일 전' }, { text: '3,000원' }],
+          [{ text: '출발 30일 전 ~ 출발 8일 전' }, { text: '4,000원' }],
+          [{ text: '출발 7일 전 ~ 출발 2일 전' }, { text: '6,000원' }],
+          [{ text: '출발 1일 전 ~ 출발시간 전' }, { text: '12,000원' }],
+          [{ text: '출발시간 이후 (No-show)' }, { text: '15,000원' }],
+        ],
+      },
+    },
+  },
+}
+
+export const RegionExample: StoryObj<typeof Regions> = {
+  name: '리전',
+  render: (args) => <Regions value={args.value} />,
+  args: {
+    value: MOCK_REGIONS,
+  },
+}
+
+export const EmbeddedExample: StoryObj<typeof Embedded> = {
+  name: '임베딩',
+  render: (args) => <Embedded value={args.value} />,
+  args: {
+    value: MOCK_EMBEDDED,
+  },
+}
+
+export const AnchorExample: StoryObj<typeof Anchor> = {
+  name: '앵커',
+  render: function Render() {
+    useEffect(() => {
+      window.history.pushState(null, '', '#android')
+    }, [])
+
+    useScrollToAnchor({ delayTime: 0 })
+
+    return (
+      <div>
+        <div style={{ height: '200vh' }} />
+        <div>Android</div>
+        <Anchor value={{ href: 'android' }} />
+        <div style={{ height: '200vh' }} />
+        <div>ios</div>
+        <Anchor value={{ href: 'ios' }} />
+      </div>
+    )
+  },
+}
+
+export const DocumentItinerary: StoryObj<typeof Itinerary> = {
+  name: '추천코스 기본',
+  render: (args) => <Itinerary value={args.value} />,
+  args: {
+    value: MOCK_ITINERARY.article.source.body[1].value,
+  },
+}
+
+export const CouponExample: StoryObj<typeof Coupon> = {
+  name: '쿠폰',
+  render: (args) => (
+    <DeepLinkProvider value="https://triple-dev.onelink.me">
+      <Coupon value={args.value} />
+    </DeepLinkProvider>
+  ),
+  args: {
+    value: {
+      identifier: 'TEST_IDENTIFIER',
+      description:
+        '쿠폰은 최초 1회만 지급되며, 이미 쿠폰을 받았다면  ‘쿠폰함’에서 확인 할 수 있습니다.',
+      enabledAt: '2023-07-03',
+      color: {
+        background: undefined,
+        buttonBackground: '#368fff',
+        buttonText: '#ffffff',
+        description: '#3a3a3a80',
+      },
+    },
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        rest.get('/api/benefit/coupons/:id', (req, res, ctx) => {
+          return res(
+            ctx.json({
+              downloaded: true,
+            }),
+          )
+        }),
+        rest.get('/api/users/smscert', (req, res, ctx) => {
+          return res(
+            ctx.json({
+              verified: true,
+            }),
+          )
+        }),
+      ],
+    },
+  },
+}
