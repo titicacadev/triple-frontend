9d0f98dad50891deb531529dcf9691cba338747a
Heeryong Kang <drakang4@gmail.com>
2023-07-07 17:35:16 +0900

    chore: use pnpm

--- packages/intersection-observer/src/intersection-observer.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'intersection-observer' {
-  interface Window {
-    IntersectionObserver: unknown
-    IntersectionObserverEntry: unknown
-    intersectionRatio: unknown
-  }
-  const content: unknown
-  export default content
-}
