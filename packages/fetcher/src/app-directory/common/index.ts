f6eee2206dc1c64da71419b9793e920e9c682020
Hajung Song <dongoc21hj@gmail.com>
2023-07-12 09:37:05 +0900

    fetcher에 common export 경로 추가

---
+++ packages/fetcher/src/app-directory/common/index.ts
@@ -0,0 +1,8 @@
+export * from '../../types'
+export { fetcher } from '../../fetcher'
+export { get, put, post, del } from '../../methods'
+export {
+  authFetcherize,
+  ssrFetcherize,
+  NEED_LOGIN_IDENTIFIER,
+} from '../../factories'
