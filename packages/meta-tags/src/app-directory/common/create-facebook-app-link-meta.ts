1d79d58620f6de28acf4d15a48061921c1c4f5fc
Hajung Song <dongoc21hj@gmail.com>
2023-07-12 14:35:03 +0900

    임시로 국제화 코드 제거

--- packages/meta-tags/src/app-directory/common/create-facebook-app-link-meta.ts
+++ packages/meta-tags/src/app-directory/common/create-facebook-app-link-meta.ts
@@ -1,25 +1,26 @@
-import { getTranslation } from '@titicaca/i18n/lib/app-directory/server'
-import { Language } from '@titicaca/i18n/lib/app-directory/types'
 import { Metadata } from 'next'
+// import { getTranslation } from '@titicaca/i18n/lib/app-directory/server'
+// import { Language } from '@titicaca/i18n/lib/app-directory/types'
 
 /**
  * FacebookAppLinkMeta 컴포넌트의 Next13 app router 버전 유틸 함수입니다.
  */
 export function createFacebookAppLinkMeta({
-  lang = 'ko',
+  // lang = 'ko',
   appName: appNameFromProps,
   iosAppStoreId = '1225499481',
   appPath = '/',
   appPackageName = 'com.titicacacorp.triple',
 }: {
-  lang?: Language
+  // lang?: Language
   appName?: string
   iosAppStoreId?: string
   appPath?: string
   appPackageName?: string
 } = {}): Metadata {
-  const t = getTranslation({ lang, namespace: 'common-web' })
-  const appName = appNameFromProps ?? t(['teuripeul', '트리플']) ?? undefined
+  // const t = getTranslation({ lang, namespace: 'common-web' })
+  // const appName = appNameFromProps ?? t(['teuripeul', '트리플']) ?? undefined
+  const appName = appNameFromProps ?? '트리플'
   const appUrlScheme = process.env.NEXT_PUBLIC_APP_URL_SCHEME || ''
   const url = `${appUrlScheme}://${appPath}`
 
