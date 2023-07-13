c33f834c27ff5e9d13374b76cb18f0a6254d105f
jhyj0521 <jeremy@triple-corp.com>
2023-07-13 14:08:59 +0900

    feat: slide layer 동작 방식 변경
    프레임이 여러 개인 경우, opacity를 조절하여 현재 노출되는 프레임을 정하는 방식으로 수정합니다.

--- packages/triple-header/src/layer/transitions/slide.tsx
+++ packages/triple-header/src/layer/transitions/slide.tsx
@@ -1,5 +1,14 @@
 import { useEffect, useState, ReactNode } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
+import styled from 'styled-components'
+import { Container } from '@titicaca/core-elements'
+
+const SlideContainer = styled(Container)<{ isVisible: boolean }>`
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+`
 
 export function Slide({ children }: { children: ReactNode[] }) {
   const [visibleFrameIndex, setVisibleFrameIndex] = useState(0)
@@ -19,10 +28,14 @@ export function Slide({ children }: { children: ReactNode[] }) {
   }, [children, visibleFrameIndex])
 
   return (
-    <AnimatePresence>
-      <motion.div key={visibleFrameIndex} animate={{}}>
-        {children[visibleFrameIndex]}
-      </motion.div>
-    </AnimatePresence>
+    <>
+      {children.map((slide, index) => {
+        return (
+          <SlideContainer key={index} isVisible={index === visibleFrameIndex}>
+            {slide}
+          </SlideContainer>
+        )
+      })}
+    </>
   )
 }
