a1bf1a07327dc2907e39bf0e4b30fc2a95b4da2b
Hajung Song <dongoc21hj@gmail.com>
2023-07-13 15:03:03 +0900

    주석 수정

--- packages/meta-tags/src/common-meta.tsx
+++ packages/meta-tags/src/common-meta.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 /**
- * next13 app router를 사용할 경우 @titicaca/meta-tags/common의 commonMeta를 사용해주세요
+ * next13 app router를 사용할 경우 '@titicaca/meta-tags/common'의 commonMeta를 사용해주세요
  */
 export function CommonMeta() {
   return (
