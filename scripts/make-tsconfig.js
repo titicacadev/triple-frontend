7c9ea97d03f612044d933b4375fab0c95f1e3b95
Hajung Song <dongoc21hj@gmail.com>
2023-07-17 14:15:29 +0900

    make-tsconfig의 path 수정

--- scripts/make-tsconfig.js
+++ scripts/make-tsconfig.js
@@ -1,4 +1,4 @@
-const fs = require('fs/promises')
+const fs = require('fs')
 const path = require('path')
 const { exec } = require('child_process')
 
@@ -41,6 +41,21 @@ function packagesToPaths(packages) {
     (paths, packageName) => ({
       ...paths,
       [`@titicaca/${packageName}`]: [`./packages/${packageName}/src`],
+      ...createExportPath({
+        packageName,
+        filename: 'common',
+        filePath: 'src/app-directory/common',
+      }),
+      ...createExportPath({
+        packageName,
+        filename: 'client',
+        filePath: 'src/app-directory/client',
+      }),
+      ...createExportPath({
+        packageName,
+        filename: 'server',
+        filePath: 'src/app-directory/server',
+      }),
     }),
     {},
   )
@@ -49,11 +64,28 @@ function packagesToPaths(packages) {
 async function writeTsconfig(transformer) {
   const tsconfigPath = path.resolve(process.cwd(), './tsconfig.test.json')
 
-  const tsconfig = JSON.parse((await fs.readFile(tsconfigPath)).toString())
+  const tsconfig = JSON.parse((await fs.readFileSync(tsconfigPath)).toString())
 
   const newTsconfig = transformer(tsconfig)
   const fileContent = `${JSON.stringify(newTsconfig, null, '  ')}\n`
   const prettified = prettier.format(fileContent, { parser: 'json' })
 
-  await fs.writeFile(tsconfigPath, prettified)
+  await fs.writeFileSync(tsconfigPath, prettified)
+}
+
+function createExportPath({ packageName, filename, filePath }) {
+  const hasPath = fs.existsSync(
+    path.relative(
+      process.cwd(),
+      path.join('./packages', packageName, filePath),
+    ),
+  )
+
+  return hasPath
+    ? {
+        [`@titicaca/${packageName}/${filename}`]: [
+          `./packages//${packageName}/${filePath}`,
+        ],
+      }
+    : {}
 }
