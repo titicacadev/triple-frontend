90911d4a7303dac9283d8ac9547d0682d26da09f
jhyj0521 <jeremy@triple-corp.com>
2023-07-19 11:25:52 +0900

    feat: fade-in-out layer 동작 방식 변경
    현재 보여져야 하는 프레임에 따라, opacity를 조절하는 애니메이션을 사용하는 방식으로 변경합니다.

--- packages/triple-header/src/layer/transitions/fade-in-out.tsx
+++ packages/triple-header/src/layer/transitions/fade-in-out.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState, ReactNode } from 'react'
-import { AnimatePresence } from 'framer-motion'
 
 import { MotionContainer } from '../../motion-container'
 
 const variants = {
-  active: () => ({
+  fadeIn: {
     opacity: [0, 1],
     transition: { duration: 1.5 },
-  }),
-  exit: () => ({ opacity: 0, transition: { duration: 1 } }),
+  },
+  fadeOut: { opacity: 0, transition: { duration: 1 } },
 }
 
 export function FadeInOut({ children }: { children: ReactNode[] }) {
@@ -29,15 +28,17 @@ export function FadeInOut({ children }: { children: ReactNode[] }) {
   }, [children, visibleFrameIndex])
 
   return (
-    <AnimatePresence>
-      <MotionContainer
-        key={visibleFrameIndex}
-        variants={variants}
-        animate="active"
-        exit="exit"
-      >
-        {children[visibleFrameIndex]}
-      </MotionContainer>
-    </AnimatePresence>
+    <>
+      {children.map((slide, index) => (
+        <MotionContainer
+          key={index}
+          initial="fadeOut"
+          variants={variants}
+          animate={index === visibleFrameIndex ? 'fadeIn' : 'fadeOut'}
+        >
+          {slide}
+        </MotionContainer>
+      ))}
+    </>
   )
 }
