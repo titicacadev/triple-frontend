29bec054bcfd67fe02b66acc12fe7940ba118531
Heeryong Kang <drakang4@gmail.com>
2023-07-20 11:44:27 +0900

    chore: assert 모듈 대신 expect 사용

--- packages/router/src/navigate/canonization.spec.ts
+++ packages/router/src/navigate/canonization.spec.ts
@@ -1,5 +1,3 @@
-import * as assert from 'assert'
-
 import canonizeTargetAddress from './canonization'
 
 describe('canonizeTargetAddress', () => {
@@ -7,92 +5,79 @@ describe('canonizeTargetAddress', () => {
   const webUrlBase = 'https://triple.guide'
 
   it('should canonize triple web url to path', () => {
-    assert.strictEqual(
+    expect(
       canonizeTargetAddress({
         href: `https://triple.guide/articles/${resourceId}?_triple_no_navbar`,
         webUrlBase,
         expandInlinkStrictly: true,
       }),
-      `/articles/${resourceId}?_triple_no_navbar`,
-    )
+    ).toBe(`/articles/${resourceId}?_triple_no_navbar`)
   })
 
   it('should canonize external url as is', () => {
     const url = 'https://google.com'
-
-    assert.strictEqual(
+    expect(
       canonizeTargetAddress({
         href: url,
         webUrlBase,
         expandInlinkStrictly: true,
       }),
-      url,
-    )
+    ).toBe(url)
   })
 
   it('should canonize inlink url in not-strict mode', () => {
     const path = `/articles/${resourceId}?_triple_no_navbar`
-
-    assert.strictEqual(
+    expect(
       canonizeTargetAddress({
         href: `/inlink?path=${encodeURIComponent(path)}`,
         webUrlBase,
         expandInlinkStrictly: false,
       }),
-      path,
-    )
+    ).toBe(path)
   })
 
   it('should not canonize inlink url in strict mode', () => {
     const path = `/articles/${resourceId}?_triple_no_navbar`
-
-    assert.strictEqual(
+    expect(
       canonizeTargetAddress({
         href: `/inlink?path=${encodeURIComponent(path)}`,
         webUrlBase,
         expandInlinkStrictly: true,
       }),
-      `/inlink?path=${encodeURIComponent(path)}`,
-    )
+    ).toBe(`/inlink?path=${encodeURIComponent(path)}`)
   })
 
   it('should canonize inlink url in strict mode conditionally', () => {
     const path = `/articles/${resourceId}?_triple_no_navbar`
-
-    assert.strictEqual(
+    expect(
       canonizeTargetAddress({
         href: `/inlink?path=${encodeURIComponent(path)}&_web_expand`,
         webUrlBase,
         expandInlinkStrictly: true,
       }),
-      path,
-    )
+    ).toBe(path)
   })
 
   it('should canonize outlink external url', () => {
     const url = 'https://google.com'
-
-    assert.strictEqual(
+    expect(
       canonizeTargetAddress({
         href: `/outlink?url=${encodeURIComponent(url)}`,
         webUrlBase,
         expandInlinkStrictly: true,
       }),
-      url,
-    )
+    ).toBe(url)
   })
 
   it('should canonize outlink internal url', () => {
     const path = `/articles/${resourceId}?_triple_no_navbar`
     const url = `${webUrlBase}${path}`
-
-    assert.strictEqual(
+    expect(
       canonizeTargetAddress({
         href: `/outlink?url=${encodeURIComponent(url)}`,
         webUrlBase,
         expandInlinkStrictly: true,
       }),
-      path,
-    )
+    ).toBe(path)
   })
 })
