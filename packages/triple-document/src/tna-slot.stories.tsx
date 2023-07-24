67e1257d3b57e0a51185b000ed579d59411c7370
Heeryong Kang <drakang4@gmail.com>
2023-07-24 16:44:05 +0900

    chore: fix storybook build

--- packages/triple-document/src/tna-slot.stories.tsx
+++ packages/triple-document/src/tna-slot.stories.tsx
@@ -1,15 +1,47 @@
 import type { Meta } from '@storybook/react'
 import { ScrapsProvider } from '@titicaca/react-contexts'
+import { appWithTranslation } from '@titicaca/next-i18next'
 import { rest } from 'msw'
+
+import { koCommonWeb } from '../../i18n/src/assets/ko/common-web'
+import { jaCommonWeb } from '../../i18n/src/assets/ja/common-web'
+import { zhTwCommonWeb } from '../../i18n/src/assets/zh-TW/common-web'
 
 import ELEMENTS from './elements'
 import SLOTS from './mocks/slots.sample.json'
+
+const locales = ['ko', 'ja', 'zh-TW']
+const resources = {
+  ko: {
+    'common-web': koCommonWeb,
+  },
+  ja: {
+    'common-web': jaCommonWeb,
+  },
+  'zh-TW': {
+    'common-web': zhTwCommonWeb,
+  },
+}
 
 const { tnaProducts: TnaProducts } = ELEMENTS
 
 export default {
   title: 'triple-document / T&A Slot',
   component: TnaProducts,
+  decorators: [
+    (Story, context) => {
+      const App = appWithTranslation(Story, {
+        i18n: { locales, defaultLocale: locales[0] },
+        lng: context.globals.locale,
+        fallbackLng: 'ko',
+        resources,
+        defaultNS: 'common-web',
+        serializeConfig: false,
+      })
+
+      return <App pageProps={{}} />
+    },
+  ],
 } as Meta
 
 export function InTripleDocument() {
