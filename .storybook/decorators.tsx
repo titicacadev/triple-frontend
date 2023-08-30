394d204f7f8192af9b88839dcf0e88905bc2b9c5
Heeryong Kang <drakang4@gmail.com>
2023-08-30 10:23:47 +0900

    chore: storybook decorator 추출

---
+++ .storybook/decorators.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import { GlobalStyle } from '../packages/core-elements/src'
+import {
+  EnvProvider,
+  HistoryProvider,
+  SessionContextProvider,
+  UserAgentProvider,
+} from '../packages/react-contexts/src'
+import { TripleClientMetadataProvider } from '../packages/react-triple-client-interfaces/src'
+
+export function globalStyleDecorator(Story) {
+  return (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  )
+}
+
+export function envProviderDecorator(Story) {
+  return (
+    <EnvProvider
+      appUrlScheme="dev-soto"
+      webUrlBase="https://triple-dev.titicaca-corp.com"
+      authBasePath="MOCK_AUTH_BASE_PATH"
+      facebookAppId=""
+      defaultPageTitle=""
+      defaultPageDescription=""
+      googleMapsApiKey="AIzaSyDuSWU_yBwuQzeyRFcTqhyifqNX_8oaXI4"
+      afOnelinkId=""
+      afOnelinkPid=""
+      afOnelinkSubdomain=""
+    >
+      <Story />
+    </EnvProvider>
+  )
+}
+
+export function historyProviderDecorator(Story) {
+  return (
+    <HistoryProvider
+      isPublic={false}
+      isAndroid={false}
+      transitionModalHash="transition.general"
+    >
+      <Story />
+    </HistoryProvider>
+  )
+}
+
+export function sessionContextProviderDecorator(Story) {
+  return (
+    <SessionContextProvider
+      type="browser"
+      props={{
+        initialUser: undefined,
+        initialSessionAvailability: false,
+      }}
+    >
+      <Story />
+    </SessionContextProvider>
+  )
+}
+
+export function userAgentProviderDecorator(Story) {
+  return (
+    <UserAgentProvider
+      value={{
+        isPublic: true,
+        isMobile: false,
+        os: {},
+        app: null,
+      }}
+    >
+      <Story />
+    </UserAgentProvider>
+  )
+}
+
+export function tripleClientMetadataDecorator(Story) {
+  return (
+    <TripleClientMetadataProvider>
+      <Story />
+    </TripleClientMetadataProvider>
+  )
+}
