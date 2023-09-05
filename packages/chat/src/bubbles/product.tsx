6a089f5496a11df8d103b1d7adcc52300dbddfcf
choisohyun <sohyun4283@naver.com>
2023-09-05 17:07:29 +0900

    (chat) feat: ProductBubble UI 스타일 적용

--- packages/chat/src/bubbles/product.tsx
+++ packages/chat/src/bubbles/product.tsx
@@ -1,3 +1,86 @@
-export const ProductBubble = () => {
-  return <div />
+import { Color } from '@titicaca/color-palette'
+import { FlexBox, Text } from '@titicaca/core-elements'
+
+import { useChat } from '../chat'
+import { CustomerBookingStatus, ProductItem } from '../types'
+
+import { TextBubble } from './text'
+import {
+  Badge,
+  ProductHr,
+  ProductImage,
+  ProductInfo,
+  ProductName,
+} from './elements'
+
+interface ProductBubbleProps {
+  my: boolean
+  product: ProductItem
+}
+
+const PRODUCT_BADGE_COLOR: Record<CustomerBookingStatus, Color> = {
+  BOOKED: 'blue',
+  ONGOING: 'blue',
+  COMPLETED: 'mint',
+  CANCEL_REQUESTED: 'gray300',
+  CANCELED: 'gray300',
+}
+
+const PRODUCT_BADGE_LABEL: Record<CustomerBookingStatus, string> = {
+  BOOKED: '예약접수',
+  ONGOING: '예약진행중',
+  COMPLETED: '예약확정',
+  CANCEL_REQUESTED: '예약취소중',
+  CANCELED: '예약취소',
+}
+
+export const ProductBubble = ({ my, product }: ProductBubbleProps) => {
+  const { textBubbleFontSize, textBubbleMaxWidthOffset } = useChat()
+  const {
+    customerBookingStatus,
+    productName,
+    productThumbnail,
+    itemName,
+    optionName,
+    dateOfUse,
+    bookingId,
+  } = product
+
+  return (
+    <TextBubble
+      my={my}
+      size={textBubbleFontSize}
+      maxWidthOffset={textBubbleMaxWidthOffset}
+      margin={my ? { left: 8 } : undefined}
+    >
+      {customerBookingStatus && (
+        <Badge backgroundColor={PRODUCT_BADGE_COLOR[customerBookingStatus]}>
+          {PRODUCT_BADGE_LABEL[customerBookingStatus]}
+        </Badge>
+      )}
+
+      <FlexBox
+        flex
+        gap="12px"
+        justifyContent="space-between"
+        alignItems="center"
+        css={{ margin: '6px 0' }}
+      >
+        <ProductName color="gray" size={15}>
+          {productName}
+        </ProductName>
+        <ProductImage src={productThumbnail} alt="상품 사진" />
+      </FlexBox>
+
+      <Text color="gray700" size={13}>
+        {itemName}
+      </Text>
+
+      <ProductHr compact color="var(--color-gray50)" />
+
+      <ProductInfo title="선택옵션" label={optionName} />
+      <ProductInfo title="이용예정" label={dateOfUse} />
+      <ProductInfo title="예약번호" label={bookingId.toString()} />
+    </TextBubble>
+  )
 }
