97313db74cca5c52b234f9e1e364bb2fcc7d49da
zhsks528 <hsm950825@naver.com>
2023-09-07 01:40:22 +0900

    Export reviews service funcs

--- packages/review/src/index.ts
+++ packages/review/src/index.ts
@@ -1,4 +1,17 @@
 export { ReviewsShorten } from './components/reviews-shorten'
 export { Reviews } from './components/reviews'
 export { writeReview } from './utils'
+export {
+  useClientActions,
+  usePopularReviews,
+  useLatestReviews,
+  useInfinitePopularReviews,
+  useInfiniteLatestReviews,
+  useReviewCount,
+  useDescriptions,
+  useMyReview,
+  useLikeReviewMutation,
+  useUnlikeReviewMutation,
+  useDeleteReviewMutation,
+} from './services'
 export type { SortingOption } from './components/sorting-context'
