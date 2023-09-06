c7a69968da0d707636304b73588bc6f2b09cb82b
zhsks528 <hsm950825@naver.com>
2023-09-06 19:18:49 +0900

    Export sorting option types

--- packages/review/src/services/index.ts
+++ packages/review/src/services/index.ts
@@ -1,2 +1,13 @@
-export * from './use-client-actions'
-export * from './use-reviews'
+export { useClientActions } from './use-client-actions'
+export {
+  usePopularReviews,
+  useLatestReviews,
+  useInfinitePopularReviews,
+  useInfiniteLatestReviews,
+  useReviewCount,
+  useDescriptions,
+  useMyReview,
+  useLikeReviewMutation,
+  useUnlikeReviewMutation,
+  useDeleteReviewMutation,
+} from './use-reviews'
