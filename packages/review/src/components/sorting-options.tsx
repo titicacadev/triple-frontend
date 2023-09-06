e370e0076ed2c7b8393a61165e6abcab97a55d55
zhsks528 <hsm950825@naver.com>
2023-09-06 15:22:43 +0900

    Add cursor design

--- packages/review/src/components/sorting-options.tsx
+++ packages/review/src/components/sorting-options.tsx
@@ -38,6 +38,7 @@ export function SortingOptions() {
         flex
         css={{
           alignItems: 'center',
+          cursor: 'pointer',
         }}
         onClick={handleActionSheetOpen}
       >
