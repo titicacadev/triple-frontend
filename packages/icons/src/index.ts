9fc426765961b0a4fab722e0743b0f03c630c5d1
Heeryong Kang <drakang4@gmail.com>
2023-09-11 15:39:05 +0900

    chore!: icons 패키지 제거

--- packages/icons/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Back } from './back'
-export { default as Search } from './search'
-export { default as Close } from './close'
-export { default as SearchPlace } from './search-place'
-export { default as ArrowIcon } from './arrow-icon'
