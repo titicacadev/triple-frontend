00e9e2a9d0631d6818232864634f71462b643b13
zhsks528 <hsm950825@naver.com>
2023-09-14 14:10:14 +0900

    Rollback exported service function

--- packages/review/src/services/index.ts
+++ packages/review/src/services/index.ts
@@ -1,15 +1,2 @@
-export { useClientActions } from './use-client-actions'
-export {
-  usePopularReviews,
-  useLatestReviews,
-  useRatingReviews,
-  useInfinitePopularReviews,
-  useInfiniteLatestReviews,
-  useInfiniteRatingReviews,
-  useReviewCount,
-  useDescriptions,
-  useMyReview,
-  useLikeReviewMutation,
-  useUnlikeReviewMutation,
-  useDeleteReviewMutation,
-} from './use-reviews'
+export * from './use-client-actions'
+export * from './use-reviews'
