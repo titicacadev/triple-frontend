686585f5e19321c19cf44b88a65cc80059a463fc
zhsks528 <hsm950825@naver.com>
2023-09-14 14:12:37 +0900

    Rollback exported source component

--- packages/review/src/index.ts
+++ packages/review/src/index.ts
@@ -1,17 +1,5 @@
-export { ReviewsShorten } from './components/reviews-shorten'
-export { Reviews } from './components/reviews'
-export { writeReview } from './utils'
-export {
-  useClientActions,
-  usePopularReviews,
-  useLatestReviews,
-  useInfinitePopularReviews,
-  useInfiniteLatestReviews,
-  useReviewCount,
-  useDescriptions,
-  useMyReview,
-  useLikeReviewMutation,
-  useUnlikeReviewMutation,
-  useDeleteReviewMutation,
-} from './services'
-export type { SortingOption } from './components/sorting-context'
+export * from './components/reviews-shorten'
+export * from './components/reviews'
+export * from './utils'
+export * from './services'
+export type { SortingOption, SortingType } from './components/sorting-context'
