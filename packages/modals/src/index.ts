71bd9bc3a9887c8acf913990a13f86301bcf8444
Heeryong Kang <drakang4@gmail.com>
2023-09-18 15:24:08 +0900

    refactor: modals 패키지를 tds-ui로 이동

--- packages/modals/src/index.ts
+++ packages/modals/src/index.ts
@@ -1,5 +1,6 @@
-export * from './alert'
-export * from './confirm'
-export * from './modal'
+export * from '@titicaca/tds-ui/src/components/alert'
+export * from '@titicaca/tds-ui/src/components/confirm'
+export * from '@titicaca/tds-ui/src/components/modal'
+
 export * from './login-cta-modal'
 export * from './transition-modal'
