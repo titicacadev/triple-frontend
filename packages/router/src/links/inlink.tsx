9c30cdc2f3409d563c67e20ea0a238b4a6556b0f
Heeryong Kang <drakang4@gmail.com>
2023-09-22 13:38:51 +0900

    feat(router): inlink, outlink 추가

---
+++ packages/router/src/links/inlink.tsx
@@ -0,0 +1,51 @@
+import { AnchorHTMLAttributes, PropsWithChildren } from 'react'
+import { useEnv } from '@titicaca/react-contexts'
+import {
+  useTripleClientMetadata,
+  AppSpecificLinkProps,
+  appSpecificLinkOptions,
+} from '@titicaca/react-triple-client-interfaces'
+import { generateUrl } from '@titicaca/view-utilities'
+import qs from 'qs'
+
+export interface InlinkProps
+  extends PropsWithChildren,
+    AppSpecificLinkProps,
+    Exclude<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> {
+  path: string
+}
+
+export const Inlink = ({
+  children,
+  path,
+  lnbTarget,
+  noNavbar,
+  shouldPresent,
+  swipeToClose,
+  ...props
+}: InlinkProps) => {
+  const { appUrlScheme } = useEnv()
+  const app = useTripleClientMetadata()
+
+  const href = app
+    ? generateUrl({
+        scheme: appUrlScheme,
+        path: '/inlink',
+        query: qs.stringify({
+          path: appSpecificLinkOptions({
+            href: path,
+            lnbTarget,
+            noNavbar,
+            shouldPresent,
+            swipeToClose,
+          }),
+        }),
+      })
+    : path
+
+  return (
+    <a {...props} href={href}>
+      {children}
+    </a>
+  )
+}
