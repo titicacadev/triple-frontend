8b6407f7c0aebdbe91b07c9dcca162dd856df406
Hajung Song <dongoc21hj@gmail.com>
2023-09-25 14:42:52 +0900

    chat container에 css props 적용할 수 있도록 추가

--- packages/chat/src/chat/chat-container.tsx
+++ packages/chat/src/chat/chat-container.tsx
@@ -1,5 +1,6 @@
-import React, { ElementType, PropsWithChildren, useState } from 'react'
+import { ElementType, PropsWithChildren, useState } from 'react'
 import dynamic from 'next/dynamic'
+import { CSSProp } from 'styled-components'
 
 import {
   ImagePayload,
@@ -20,6 +21,9 @@ export interface ChatContainerProps extends ChatContextValue {
    * Chat list와 보내기 Input 창을 감싸는 컨테이너로, 커스텀 스타일 등 적용 가능
    */
   container: ElementType
+  containerProps: {
+    css: CSSProp
+  }
   /**
    * input 창, 보내기 버튼 등을 포함하는 컴포넌트
    */
@@ -43,6 +47,7 @@ const defaultOnImageBubbleClick = (imageInfos: MetaDataInterface[]) => {
  */
 export const ChatContainer = ({
   container: Container,
+  containerProps,
   inputElement: Input,
   children,
 
@@ -72,7 +77,7 @@ export const ChatContainer = ({
       }}
     >
       <ScrollProvider>
-        <Container>{children}</Container>
+        <Container {...containerProps}>{children}</Container>
         {Input && postMessage ? <Input postMessage={postMessage} /> : null}
       </ScrollProvider>
     </ChatContext.Provider>
