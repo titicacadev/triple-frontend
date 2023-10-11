ba876d2ff99fddb9d21e6ffe5e23bd7d99a60f5c
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-10-11 18:08:52 +0900

    kint5 document 노트 컴포넌트 스타일 수정

--- packages/kint5-document/src/elements/note.tsx
+++ packages/kint5-document/src/elements/note.tsx
@@ -6,13 +6,15 @@ export default function Note({
   value: { title: string; body: string }
 }) {
   return (
-    <Segment css={{ margin: 30 }}>
-      <Text bold size="small" color="gray" lineHeight={1.57}>
-        {title}
-      </Text>
-      <Text size="small" color="gray" alpha={0.8} lineHeight={1.57}>
-        {body}
-      </Text>
+    <Segment
+      css={{
+        margin: '30px 0',
+        borderRadius: 16,
+        backgroundColor: 'var(--color-kint5-gray20)',
+      }}
+    >
+      <Text css={{ fontSize: 14, fontWeight: 700 }}>{title}</Text>
+      <Text css={{ fontSize: 14, fontWeight: 400, marginTop: 12 }}>{body}</Text>
     </Segment>
   )
 }
