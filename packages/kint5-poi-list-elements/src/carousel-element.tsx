478fb38ac79c37230f7c35b5576cf443f32e339f
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-10-11 12:36:16 +0900

    kint5-poi-list-elements 패키지 추가

---
+++ packages/kint5-poi-list-elements/src/carousel-element.tsx
@@ -0,0 +1,111 @@
+import { ReactNode } from 'react'
+import {
+  CarouselSizes,
+  Carousel,
+  Text,
+  Container,
+  Image,
+} from '@titicaca/kint5-core-elements'
+import { OverlayScrapButton } from '@titicaca/scrap-button'
+import { FrameRatioAndSizes } from '@titicaca/type-definitions'
+
+import { POI_IMAGE_PLACEHOLDERS } from './constants'
+import { getTypeNames } from './get-type-names'
+import {
+  PoiListElementBaseProps,
+  ActionButtonElement,
+  PoiListElementType,
+} from './types'
+
+function PoiCarouselElement<T extends PoiListElementType>({
+  poi,
+  poi: {
+    type,
+    region,
+    nameOverride,
+    source: { image, names, areas, vicinity },
+  },
+  onClick,
+  actionButtonElement,
+  description,
+  additionalInfo = null,
+  carouselSize,
+  titleTopSpacing = 10,
+  imageFrame,
+  onImpress,
+  optimized,
+}: PoiListElementBaseProps<T> & {
+  actionButtonElement?: ActionButtonElement
+  description?: ReactNode
+  additionalInfo?: ReactNode
+  carouselSize?: CarouselSizes
+  titleTopSpacing?: number
+  imageFrame?: FrameRatioAndSizes
+  onImpress?: () => void
+  optimized?: boolean
+}) {
+  if (!poi) {
+    return null
+  }
+
+  const { names: regionNames } = region?.source || {}
+
+  const name = nameOverride || names.ko || names.en || names.local
+  const regionName = regionNames?.ko || regionNames?.en || regionNames?.local
+
+  return (
+    <Carousel.Item
+      size={carouselSize || 'small'}
+      onClick={onClick}
+      onImpress={onImpress}
+    >
+      <Image>
+        <Image.FixedRatioFrame frame={imageFrame || 'large'}>
+          {image ? (
+            optimized ? (
+              <Image.OptimizedImg
+                cloudinaryId={image.cloudinaryId as string}
+                cloudinaryBucket={image.cloudinaryBucket}
+                alt={name || ''}
+              />
+            ) : (
+              <Image.Img src={image.sizes.large.url} alt={name || ''} />
+            )
+          ) : (
+            <Image.Placeholder src={POI_IMAGE_PLACEHOLDERS[type]} />
+          )}
+        </Image.FixedRatioFrame>
+      </Image>
+
+      <Text bold ellipsis alpha={1} margin={{ top: titleTopSpacing }}>
+        {name}
+      </Text>
+      <Text size="tiny" alpha={0.7} margin={{ top: 2 }}>
+        {description || getTypeNames(type)}
+      </Text>
+      <Text size="tiny" alpha={0.7} margin={{ top: 2 }}>
+        {regionName
+          ? areas?.[0]?.name
+            ? `${regionName}(${areas?.[0]?.name})`
+            : regionName
+          : vicinity}
+      </Text>
+
+      {actionButtonElement || (
+        <Container
+          position="absolute"
+          css={{
+            top: '3px',
+            right: '3px',
+          }}
+        >
+          <OverlayScrapButton resource={poi} size={36} />
+        </Container>
+      )}
+
+      {additionalInfo}
+    </Carousel.Item>
+  )
+}
+
+export default PoiCarouselElement
---
+++ packages/kint5-poi-list-elements/src/index.ts
@@ -0,0 +1,6 @@
+export * from './compact-poi-list-element'
+export * from './extended-poi-list-element'
+export * from './poi-list-element'
+export { default as PoiCardElement } from './poi-card-element'
+export { default as PoiCarouselElement } from './carousel-element'
+export * from './types'
