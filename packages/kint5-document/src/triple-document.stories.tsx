67bedcd67677a0fb9a9985dd41ea73e56d6933e6
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-10-12 10:49:04 +0900

    kint5 document 쿠폰 컴포넌트 스토리북을 별도의 파일로 분리

--- packages/kint5-document/src/triple-document.stories.tsx
+++ packages/kint5-document/src/triple-document.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Container } from '@titicaca/kint5-core-elements'
 import { useScrollToAnchor } from '@titicaca/react-hooks'
-import { rest } from 'msw'
 import { useEffect } from 'react'
 import { appWithTranslation } from '@titicaca/next-i18next'
 import { koCommonWeb } from '@titicaca/i18n/src/assets/ko/common-web'
@@ -12,7 +11,6 @@ import ELEMENTS from './elements'
 import MOCK_EMBEDDED from './mocks/triple-document.embedded.json'
 import MOCK_ITINERARY from './mocks/triple-document.itinerary.json'
 import SAMPLE from './mocks/triple-document.sample.json'
-import { DeepLinkProvider } from './prop-context/deep-link'
 import { TripleDocument } from './triple-document'
 import { TripleElementData } from './types'
 
@@ -34,7 +32,6 @@ const {
   embedded: Embedded,
   anchor: Anchor,
   itinerary: Itinerary,
-  coupon: Coupon,
 } = ELEMENTS
 
 export default {
@@ -123,48 +120,5 @@ export const DocumentItinerary: StoryObj<typeof Itinerary> = {
   render: (args) => <Itinerary value={args.value} />,
   args: {
     value: MOCK_ITINERARY.article.source.body[1].value,
-  },
-}
-
-export const CouponExample: StoryObj<typeof Coupon> = {
-  name: '쿠폰',
-  render: (args) => (
-    <DeepLinkProvider value="https://triple-dev.onelink.me">
-      <Coupon value={args.value} />
-    </DeepLinkProvider>
-  ),
-  args: {
-    value: {
-      identifier: 'TEST_IDENTIFIER',
-      description:
-        '쿠폰은 최초 1회만 지급되며, 이미 쿠폰을 받았다면  ‘쿠폰함’에서 확인 할 수 있습니다.',
-      enabledAt: '2023-07-03',
-      color: {
-        background: undefined,
-        buttonBackground: '#368fff',
-        buttonText: '#ffffff',
-        description: '#3a3a3a80',
-      },
-    },
-  },
-  parameters: {
-    msw: {
-      handlers: [
-        rest.get('/api/benefit/coupons/:id', (req, res, ctx) => {
-          return res(
-            ctx.json({
-              downloaded: true,
-            }),
-          )
-        }),
-        rest.get('/api/users/smscert', (req, res, ctx) => {
-          return res(
-            ctx.json({
-              verified: true,
-            }),
-          )
-        }),
-      ],
-    },
   },
 }
