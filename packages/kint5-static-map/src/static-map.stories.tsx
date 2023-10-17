38598c43717d1b5fbe350f4ef3491838b781c9c1
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-10-17 16:43:48 +0900

    kint5-static-map 패키지 추가

---
+++ packages/kint5-static-map/src/static-map.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import StaticMap from './static-map'
+
+export default {
+  title: 'kint5-static-map / StaticMap',
+  component: StaticMap,
+} as Meta<typeof StaticMap>
+
+export const Basic: StoryObj<typeof StaticMap> = {
+  args: {
+    type: 'attraction',
+    lat: 35.6328964,
+    lon: 139.8803943,
+  },
+}
