dc8038c918c6560cc1057470a39e13a3be87adcb
zhsks528 <hsm950825@naver.com>
2023-10-17 16:25:00 +0900

    Add converse handler

---
+++ packages/standard-action-handler/src/converse.tsx
@@ -0,0 +1,44 @@
+import { createRoot } from 'react-dom/client'
+import { Modal } from '@titicaca/modals'
+
+import { WebActionParams } from './types'
+
+const title = 'TEST'
+const description = '성공입니다.'
+
+export default function converse({ url: { path } = {} }: WebActionParams) {
+  if (path === '/web-action/converse') {
+    if (title && description) {
+      const container = document.createElement('div')
+      const root = createRoot(container)
+
+      root.render(
+        OpenModal({ title, description, onClose: () => root.unmount() }),
+      )
+
+      return true
+    }
+  }
+}
+
+function OpenModal({
+  title,
+  description,
+  onClose,
+}: {
+  title: string
+  description: string
+  onClose: () => void
+}) {
+  return (
+    <Modal open onClose={onClose}>
+      <Modal.Title>{title}</Modal.Title>
+      <Modal.Description>{description}</Modal.Description>
+      <Modal.Actions>
+        <Modal.Action color="blue" onClick={onClose}>
+          확인
+        </Modal.Action>
+      </Modal.Actions>
+    </Modal>
+  )
+}
