2de68e6375ae6250c0607f322938adec6a05ea33
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-10-18 14:46:24 +0900

    kint5-diretions-finder 패키지 추가

---
+++ packages/kint5-directions-finter/src/direction-buttons.tsx
@@ -0,0 +1,81 @@
+import { useCallback } from 'react'
+import { useTranslation } from '@titicaca/next-i18next'
+import {
+  useEventTrackingContext,
+  useHistoryFunctions,
+  useUriHash,
+} from '@titicaca/react-contexts'
+import { useTripleClientMetadata } from '@titicaca/react-triple-client-interfaces'
+import { Button, ButtonGroup } from '@titicaca/kint5-core-elements'
+
+import AskToTheLocal from './ask-to-the-local'
+import { HASH_ASK_TO_LOCALS_POPUP } from './constants'
+
+function DirectionButtons({
+  onDirectionsClick,
+  primaryName,
+  localName,
+  localAddress,
+  phoneNumber,
+  isDomestic = false,
+}: {
+  onDirectionsClick: () => void
+  primaryName: string
+  localName?: string
+  localAddress?: string
+  phoneNumber?: string
+  isDomestic?: boolean
+}) {
+  const { t } = useTranslation('common-web')
+
+  const app = useTripleClientMetadata()
+  const uriHash = useUriHash()
+  const { push, back, showTransitionModal } = useHistoryFunctions()
+  const { trackSimpleEvent } = useEventTrackingContext()
+
+  const handleAskToLocalsClick = useCallback(() => {
+    trackSimpleEvent({ action: '기본정보_현지에서길묻기' })
+
+    app ? push(HASH_ASK_TO_LOCALS_POPUP) : showTransitionModal()
+  }, [trackSimpleEvent, push, showTransitionModal, app])
+
+  return (
+    <>
+      <ButtonGroup horizontalGap={10}>
+        {localName && localAddress ? (
+          <Button
+            basic
+            color="gray"
+            size="small"
+            onClick={handleAskToLocalsClick}
+          >
+            {t(['hyeonjieseo-gilmudgi', '현지에서 길묻기'])}
+          </Button>
+        ) : null}
+        <Button
+          basic
+          inverted
+          color="blue"
+          size="small"
+          onClick={onDirectionsClick}
+        >
+          {t(['gilcajgi', '길찾기'])}
+        </Button>
+      </ButtonGroup>
+
+      {localName && localAddress ? (
+        <AskToTheLocal
+          open={uriHash === HASH_ASK_TO_LOCALS_POPUP}
+          onClose={back}
+          localName={localName}
+          localAddress={localAddress}
+          primaryName={primaryName}
+          phoneNumber={phoneNumber}
+          isDomestic={isDomestic}
+        />
+      ) : null}
+    </>
+  )
+}
+
+export default DirectionButtons
---
+++ packages/kint5-directions-finter/src/directions-finder.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import DirectionsFinder from '.'
+
+export default {
+  title: 'kint5-directions-finder / DirectionsFinder',
+  component: DirectionsFinder,
+} as Meta<typeof DirectionsFinder>
+
+export const Basic: StoryObj<typeof DirectionsFinder> = {
+  args: {
+    primaryName: '도쿄 디즈니 랜드',
+    localName: '東京ディズニーランド',
+    localAddress: '〒279-0031 東京都千葉県浦安市舞浜11',
+    phoneNumber: '+81453305211',
+  },
+}
