8266fa40fa8444a86cc92e2d545accd2271c3ef7
Heeryong Kang <drakang4@gmail.com>
2023-10-26 18:46:41 +0900

    feat: client-app context 추가

---
+++ packages/triple-web/src/hooks/use-client-app.ts
@@ -0,0 +1,13 @@
+import { useContext } from 'react'
+
+import { ClientAppContext } from '../contexts/client-app'
+
+export function useClientApp() {
+  const context = useContext(ClientAppContext)
+
+  if (context === undefined) {
+    throw new Error('ClientAppContext가 없습니다.')
+  }
+
+  return context
+}
