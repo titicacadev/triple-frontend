e811a048d5625eed60498dd9e2646218dabb327c
Heeryong Kang <drakang4@gmail.com>
2023-10-26 18:47:41 +0900

    fix: 클라이언트 사이드에서 user-agent context는 window.navigator.userAgent 사용

--- packages/triple-web-binding-nextjs-pages/src/user-agent.ts
+++ packages/triple-web-binding-nextjs-pages/src/user-agent.ts
@@ -3,7 +3,9 @@ import { UserAgent } from '@titicaca/triple-web'
 import { UAParser } from 'ua-parser-js'
 
 export function getUserAgent(ctx: NextPageContext): UserAgent {
-  const userAgent = ctx.req?.headers['user-agent']
+  const userAgent = ctx.req
+    ? ctx.req.headers['user-agent'] ?? ''
+    : window.navigator.userAgent
   const parser = new UAParser(userAgent)
 
   return {
