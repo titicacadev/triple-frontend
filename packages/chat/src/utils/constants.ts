a13175ca59a4c19649623ff66bbfdf2c2fadcecb
Hajung Song <dongoc21hj@gmail.com>
2023-10-31 12:27:33 +0900

    예시 코드 추가

--- packages/chat/src/utils/constants.ts
+++ packages/chat/src/utils/constants.ts
@@ -37,33 +37,17 @@ export const MEDIA_ARGS: ChatContextValue = {
 
 export const CHAT_ARGS: ChatProps = {
   displayTarget: UserType.TNA_PARTNER,
-  userInfo: {
-    me: {
-      id: '61ea67f0de3e37001997a80f',
-      type: UserType.TNA_PARTNER,
-      identifier: '130',
-      code: 'TNA_BPM',
-      profile: {
-        name: 'TNA_BPM',
-        thumbnail:
-          'https://s3.ap-northeast-2.amazonaws.com/triple-tna-dev/partner/logo/1e2496ab-8725-4df4-84c7-db9804d3c71d.jpeg',
-        message: '',
-      },
+  me: {
+    id: '61ea67f0de3e37001997a80f',
+    type: UserType.TNA_PARTNER,
+    identifier: '130',
+    code: 'TNA_BPM',
+    profile: {
+      name: 'TNA_BPM',
+      thumbnail:
+        'https://s3.ap-northeast-2.amazonaws.com/triple-tna-dev/partner/logo/1e2496ab-8725-4df4-84c7-db9804d3c71d.jpeg',
+      message: '',
     },
-    others: [
-      {
-        id: '6344be9953749900140bca42',
-        type: UserType.TRIPLE_USER,
-        identifier: '4043',
-        code: '_KA2408084137-1661761357899',
-        profile: {
-          name: '후라이',
-          thumbnail:
-            'https://media.triple.guide/triple-dev/c_limit,f_auto,h_2048,w_2048/52557846-363d-430a-9afd-1cd7fd4fe0b4.jpeg',
-          message: '',
-        },
-      },
-    ],
   },
   getMessages: async () => {
     return [
@@ -96,6 +80,18 @@ export const CHAT_ARGS: ChatProps = {
           message:
             '안녕하세요.\nTNA_BPM입니다. 예약해주셔서 감사합니다!\n\n궁금한 점이 있으시면 TNA_BPM 문의를 편하게 이용해주세요.',
         },
+        sender: {
+          id: '61ea67f0de3e37001997a80f',
+          type: UserType.TNA_PARTNER,
+          identifier: '130',
+          code: 'TNA_BPM',
+          profile: {
+            name: 'TNA_BPM',
+            thumbnail:
+              'https://s3.ap-northeast-2.amazonaws.com/triple-tna-dev/partner/logo/1e2496ab-8725-4df4-84c7-db9804d3c71d.jpeg',
+            message: '',
+          },
+        },
       },
       {
         id: 5749,
@@ -106,6 +102,18 @@ export const CHAT_ARGS: ChatProps = {
         payload: {
           type: MessageType.TEXT,
           message: '테스트 메시지',
+        },
+        sender: {
+          id: '6344be9953749900140bca42',
+          type: UserType.TRIPLE_USER,
+          identifier: '4043',
+          code: '1',
+          profile: {
+            name: '후라이',
+            thumbnail:
+              'https://media.triple.guide/triple-dev/c_limit,f_auto,h_2048,w_2048/52557846-363d-430a-9afd-1cd7fd4fe0b4.jpeg',
+            message: '',
+          },
         },
       },
     ]
@@ -153,6 +161,18 @@ export const CHAT_ARGS: ChatProps = {
         type: MessageType.TEXT,
         message: '테스트 메시지',
       },
+      sender: {
+        id: '6344be9953749900140bca42',
+        type: UserType.TRIPLE_USER,
+        identifier: '4043',
+        code: '1',
+        profile: {
+          name: '후라이',
+          thumbnail:
+            'https://media.triple.guide/triple-dev/c_limit,f_auto,h_2048,w_2048/52557846-363d-430a-9afd-1cd7fd4fe0b4.jpeg',
+          message: '',
+        },
+      },
     },
     unreadCount: 0,
     metadata: {
@@ -171,6 +191,18 @@ export const CHAT_ARGS: ChatProps = {
           createdAt: '2022-11-04T06:44:57.017Z',
           displayTarget: 'all',
           payload,
+          sender: {
+            id: '61ea67f0de3e37001997a80f',
+            type: UserType.TNA_PARTNER,
+            identifier: '130',
+            code: 'TNA_BPM',
+            profile: {
+              name: 'TNA_BPM',
+              thumbnail:
+                'https://s3.ap-northeast-2.amazonaws.com/triple-tna-dev/partner/logo/1e2496ab-8725-4df4-84c7-db9804d3c71d.jpeg',
+              message: '',
+            },
+          },
         },
       ],
     }
