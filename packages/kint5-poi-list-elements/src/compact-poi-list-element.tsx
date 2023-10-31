a9533555f453251e95a4a4fd7300c141ff6923d9
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-10-31 10:56:47 +0900

    kint5-poi-list-elements의 CompactPoiListElement에서 POI 이름을 표시할 때 primary, en, local 순으로 고려하도록 함

--- packages/kint5-poi-list-elements/src/compact-poi-list-element.tsx
+++ packages/kint5-poi-list-elements/src/compact-poi-list-element.tsx
@@ -54,8 +54,9 @@ export function CompactPoiListElement<T extends PoiListElementType>({
 
   const { names: regionNames } = region?.source || {}
 
-  const name = nameOverride || names.ko || names.en || names.local
-  const regionName = regionNames?.ko || regionNames?.en || regionNames?.local
+  const name = nameOverride || names.primary || names.en || names.local
+  const regionName =
+    regionNames?.primary || regionNames?.en || regionNames?.local
 
   return (
     <List.Item
