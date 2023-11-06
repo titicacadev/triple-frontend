82d68ec6df3a6285017e9194533a430f8f3463ae
Hajung Song <dongoc21hj@gmail.com>
2023-11-06 11:44:01 +0900

    chat-scroll-container 추가

--- packages/chat/src/chat/index.ts
+++ packages/chat/src/chat/index.ts
@@ -1,6 +1,10 @@
+export * from './reducer'
+export * from './scroll-context'
+export * from './constants'
+export * from './chat-scroll-container'
+
+// TODO 아래 파일들은 bubble 작업 이후 삭제될 예정입니다.
 export * from './chat'
 export * from './chat-container'
 export * from './chat-context'
-export * from './reducer'
 export * from './use-chat-message'
-export * from './scroll-context'
