832c2ba40fca11760bb1135075674e1f2cb552d9
Hajung Song <dongoc21hj@gmail.com>
2023-11-06 14:37:30 +0900

    messages-reducer 추가

--- packages/chat/src/chat/index.ts
+++ packages/chat/src/chat/index.ts
@@ -1,10 +1,13 @@
-export * from './reducer'
 export * from './scroll-context'
 export * from './constants'
 export * from './chat-scroll-container'
+export * from './messages-reducer'
 
 // TODO 아래 파일들은 bubble 작업 이후 삭제될 예정입니다.
 export * from './chat'
 export * from './chat-container'
 export * from './chat-context'
 export * from './use-chat-message'
+export * from './use-chat-message'
+export * from './scroll-context'
+// export * from './reducer'
