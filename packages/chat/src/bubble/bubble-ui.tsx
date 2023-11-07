414cc3f7dc7051fc2bf237c13b77c46362a132f2
guswl98 <hjee0901@gmail.com>
2023-11-07 10:58:06 +0900

    Deleted 컴포넌트 추가 필요
    BubbleUI에서 type 수정
    
    Deleted 컴포넌트 추가 필요

--- packages/chat/src/bubble/bubble-ui.tsx
+++ packages/chat/src/bubble/bubble-ui.tsx
@@ -1,6 +1,6 @@
 import {
   ImagePayload,
-  MessageType,
+  MessageType as OriginMessageType,
   ProductPayload,
   RichPayload,
   TextPayload,
@@ -10,21 +10,33 @@ import { TextBubble } from './text'
 import { ImageBubble } from './image'
 import { RichBubble } from './rich'
 import {
+  BlindedBubbleProp,
   ImageBubbleProp,
   ProductBubbleProps,
   RichBubbleProp,
   TextBubbleProp,
 } from './type'
 import { ProductBubble } from './product'
+import BlindedBubble from './blinded'
+
+enum MessageUIType {
+  BLINDED = 'blinded',
+  DELETED = 'deleted',
+}
+
+const MessageType = { ...MessageUIType, ...OriginMessageType }
 
 type BubbleUIProps =
   | (TextPayload & TextBubbleProp)
   | (ImagePayload & ImageBubbleProp)
   | (RichPayload & RichBubbleProp)
   | (ProductPayload & ProductBubbleProps)
+  | ({ type: MessageUIType.BLINDED } & BlindedBubbleProp)
 
 export default function BubbleUI({ ...bubble }: BubbleUIProps) {
   switch (bubble.type) {
+    case MessageType.BLINDED:
+      return <BlindedBubble {...bubble} />
     case MessageType.TEXT:
       return <TextBubble {...bubble} />
     case MessageType.IMAGES:
@@ -33,5 +45,9 @@ export default function BubbleUI({ ...bubble }: BubbleUIProps) {
       return <RichBubble {...bubble} />
     case MessageType.PRODUCT:
       return <ProductBubble {...bubble} />
+    //   case MessageType.DELETED:
+    //     return <DeletedBubble />
+    default:
+      throw new Error('지원하지 않는 메시지 타입입니다.')
   }
 }
