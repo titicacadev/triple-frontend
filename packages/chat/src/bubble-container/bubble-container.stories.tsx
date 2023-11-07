8da402177ab0f119b6387f29ec45ad0959cb6486
guswl98 <hjee0901@gmail.com>
2023-11-07 11:00:00 +0900

    BubbleContainer 스토리북 생성

---
+++ packages/chat/src/bubble-container/bubble-container.stories.tsx
@@ -0,0 +1,86 @@
+import { Meta, StoryFn } from '@storybook/react'
+
+import { TextBubble } from '../bubble/text'
+
+import BubbleContainer, { BubbleContainerProp } from './bubble-container'
+
+export default {
+  title: 'chat / BubbleContainer',
+  component: BubbleContainer,
+} as Meta<typeof BubbleContainer>
+
+const Template: StoryFn<BubbleContainerProp> = (args) => (
+  <BubbleContainer {...args}>
+    <TextBubble
+      text={'안녕하세요\nhttps://www.google.com'}
+      my={args.my}
+      id="text-bubble"
+    />
+  </BubbleContainer>
+)
+
+export const SentBubbleContainer = {
+  render: Template,
+  argTypes: {
+    my: {
+      type: 'boolean',
+      required: true,
+    },
+    createdAt: {
+      type: 'date',
+    },
+    unreadCount: {
+      type: 'number',
+    },
+    showInfo: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+  args: {
+    my: true,
+    createdAt: new Date(2022, 10, 1).toISOString(),
+    unreadCount: null,
+    showInfo: true,
+  },
+}
+
+export const ReceivedBubbleContainer = {
+  render: Template,
+  argTypes: {
+    my: {
+      type: 'boolean',
+      required: true,
+    },
+    createdAt: {
+      type: 'date',
+    },
+    unreadCount: {
+      type: 'number',
+    },
+    showInfo: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    profile: {
+      control: {
+        type: 'object',
+      },
+    },
+  },
+  args: {
+    my: false,
+    createdAt: new Date(2022, 10, 1).toISOString(),
+    unreadCount: null,
+    showInfo: true,
+    profile: {
+      imageUrl:
+        'https://assets.triple-dev.titicaca-corp.com/images/app-download@2x.png',
+      name: '테스트계정',
+      userId: 'test',
+      unregister: false,
+    },
+  },
+}
