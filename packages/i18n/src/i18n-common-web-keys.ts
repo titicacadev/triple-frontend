dd51414da56058cfe767ff4e0d48e878529807be
Heeryong Kang <drakang4@gmail.com>
2023-11-20 13:30:39 +0900

    feat(i18n): 불필요한 코드 제거

--- packages/i18n/src/i18n-common-web-keys.ts
+++
@@ -1,5 +0,0 @@
-import { koCommonWeb } from './assets/ko/common-web'
-
-type I18nKeys = typeof koCommonWeb
-type I18nTokens = Record<I18nKeys[keyof I18nKeys], string>
-export type I18nCommonWebKeys = I18nKeys & I18nTokens
