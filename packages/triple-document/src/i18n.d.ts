6cef15cdde5b65c14a94f0da18f11e03a431b127
Heeryong Kang <drakang4@gmail.com>
2023-11-20 13:42:13 +0900

    feat: i18n.d.ts 업데이트

--- packages/triple-document/src/i18n.d.ts
+++ packages/triple-document/src/i18n.d.ts
@@ -1,11 +1,10 @@
-import '@titicaca/next-i18next'
-import { I18nCommonWebKeys } from '@titicaca/i18n'
+import 'i18next'
+import { I18nKeys } from '@titicaca/i18n'
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    defaultNS: 'common-web'
     resources: {
-      'common-web': I18nCommonWebKeys
+      'triple-frontend': I18nKeys
     }
   }
 }
