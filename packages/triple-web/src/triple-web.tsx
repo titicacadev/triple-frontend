799a97e0876d00ee3c9aa24b038fe5f78f1dcf05
Heeryong Kang <drakang4@gmail.com>
2023-11-20 11:06:24 +0900

    feat(triple-web): I18nProvider 추가

--- packages/triple-web/src/triple-web.tsx
+++ packages/triple-web/src/triple-web.tsx
@@ -6,6 +6,8 @@ import { LoginCtaModal, TransitionModal } from './components'
 import {
   ClientAppContext,
   EnvContext,
+  I18nProvider,
+  I18nValue,
   ModalProvider,
   SessionProvider,
   UserAgentContext,
@@ -16,6 +18,7 @@ import { ClientAppValue, EnvValue, SessionValue, UserAgentValue } from './types'
 export interface TripleWebProps extends PropsWithChildren {
   clientAppProvider?: ClientAppValue
   envProvider?: EnvValue
+  i18nProvider?: I18nValue
   sessionProvider?: SessionValue
   userAgentProvider?: UserAgentValue
 }
@@ -24,23 +27,26 @@ export function TripleWeb({
   children,
   clientAppProvider,
   envProvider,
+  i18nProvider,
   sessionProvider,
   userAgentProvider,
 }: TripleWebProps) {
   return (
     <ClientAppContext.Provider value={clientAppProvider}>
       <EnvContext.Provider value={envProvider}>
-        <SessionProvider initialSession={sessionProvider}>
-          <UserAgentContext.Provider value={userAgentProvider}>
-            <HashRouterProvider>
-              <ModalProvider>
-                {children}
-                <LoginCtaModal />
-                <TransitionModal />
-              </ModalProvider>
-            </HashRouterProvider>
-          </UserAgentContext.Provider>
-        </SessionProvider>
+        <I18nProvider {...i18nProvider}>
+          <SessionProvider initialSession={sessionProvider}>
+            <UserAgentContext.Provider value={userAgentProvider}>
+              <HashRouterProvider>
+                <ModalProvider>
+                  {children}
+                  <LoginCtaModal />
+                  <TransitionModal />
+                </ModalProvider>
+              </HashRouterProvider>
+            </UserAgentContext.Provider>
+          </SessionProvider>
+        </I18nProvider>
       </EnvContext.Provider>
     </ClientAppContext.Provider>
   )
