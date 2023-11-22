ec8bf138674865de056412a2c7bb690081019b58
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-11-22 18:26:27 +0900

    [kint5-review] 유저의 리뷰 개수를 표시하지 않음

--- packages/kint5-review/src/components/review-element/user.tsx
+++ packages/kint5-review/src/components/review-element/user.tsx
@@ -1,12 +1,11 @@
-import { PropsWithChildren, MouseEventHandler } from 'react'
-import { useTranslation } from '@titicaca/next-i18next'
+import { MouseEventHandler } from 'react'
 import styled from 'styled-components'
-import { Container, Text } from '@titicaca/kint5-core-elements'
+import { FlexBox, Text } from '@titicaca/kint5-core-elements'
 
 import { BaseUserFragment } from '../../data/graphql'
 
 const UserPhoto = styled.img`
-  margin-right: 9px;
+  margin-right: 8px;
   width: 40px;
   height: 40px;
   background-color: #efefef;
@@ -15,58 +14,24 @@ const UserPhoto = styled.img`
 `
 
 export default function User({
-  user: { photo, name, userBoard, unregister },
+  user: { photo, name },
   onClick,
 }: {
   onClick?: MouseEventHandler
   user: BaseUserFragment
 }) {
-  const { t } = useTranslation('common-web')
-
-  const { reviewsV2: reviewsCount } = userBoard || {}
-
   return (
-    <Container
-      display="flex"
+    <FlexBox
+      flex
       css={{
         padding: '0 0 2px',
+        alignItems: 'center',
       }}
     >
       {photo ? <UserPhoto src={photo} onClick={onClick} /> : null}
-      <Container css={{ paddingTop: 2 }}>
-        <Name onClick={onClick}>{name}</Name>
-        {!unregister ? (
-          <Text
-            onClick={onClick}
-            css={{
-              fontSize: 13,
-              fontWeight: 400,
-              color: 'var(--color-kint5-gray60)',
-              marginTop: 4,
-            }}
-          >
-            {reviewsCount
-              ? t(['reviewscount-gaeyi-ribyu', '{{reviewsCount}}개의 리뷰'], {
-                  reviewsCount,
-                })
-              : null}
-          </Text>
-        ) : null}
-      </Container>
-    </Container>
-  )
-}
-
-function Name({
-  onClick,
-  children,
-}: PropsWithChildren<{ onClick?: MouseEventHandler<HTMLDivElement> }>) {
-  return (
-    <Text
-      onClick={onClick}
-      css={{ fontSize: 14, fontWeight: 700, wordBreak: 'break-word' }}
-    >
-      {children}
-    </Text>
+      <Text css={{ fontSize: 14, fontWeight: 700, wordBreak: 'break-word' }}>
+        {name}
+      </Text>
+    </FlexBox>
   )
 }
