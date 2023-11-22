5eb1599a466e64168166392fdbc5e630220f73a0
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-11-22 18:30:25 +0900

    [kint5-review] 유저 이미지에 ThumbnailBorder 컴포넌트 적용

--- packages/kint5-review/src/components/review-element/user.tsx
+++ packages/kint5-review/src/components/review-element/user.tsx
@@ -1,15 +1,18 @@
 import { MouseEventHandler } from 'react'
 import styled from 'styled-components'
-import { FlexBox, Text } from '@titicaca/kint5-core-elements'
+import {
+  Container,
+  FlexBox,
+  Text,
+  ThumbnailBorder,
+} from '@titicaca/kint5-core-elements'
 
 import { BaseUserFragment } from '../../data/graphql'
 
 const UserPhoto = styled.img`
-  margin-right: 8px;
   width: 40px;
   height: 40px;
-  background-color: #efefef;
-  border-radius: 19px;
+  border-radius: 50%;
   object-fit: cover;
 `
 
@@ -26,9 +29,20 @@ export default function User({
       css={{
         padding: '0 0 2px',
         alignItems: 'center',
+        gap: 8,
       }}
     >
-      {photo ? <UserPhoto src={photo} onClick={onClick} /> : null}
+      <Container
+        css={{
+          position: 'relative',
+          borderRadius: '50%',
+          width: 40,
+          height: 40,
+        }}
+      >
+        {photo ? <UserPhoto src={photo} onClick={onClick} /> : null}
+        <ThumbnailBorder css={{ borderRadius: '50%' }} />
+      </Container>
       <Text css={{ fontSize: 14, fontWeight: 700, wordBreak: 'break-word' }}>
         {name}
       </Text>
