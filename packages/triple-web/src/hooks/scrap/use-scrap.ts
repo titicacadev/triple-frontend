54227f5625ec7e04be87ec91cc11171ca4b7dc7e
zhsks528 <hsm950825@naver.com>
2023-11-23 15:49:22 +0900

    Refine scrap logic Change ContextAPI to custom hook

---
+++ packages/triple-web/src/hooks/scrap/use-scrap.ts
@@ -0,0 +1,140 @@
+import { useCallback, useEffect } from 'react'
+import {
+  notifyScraped,
+  notifyUnscraped,
+  subscribeScrapedChangeEvent,
+  unsubscribeScrapedChangeEvent,
+} from '@titicaca/triple-web-to-native-interfaces'
+
+import type { ScrapProps, Target } from '../../types'
+
+import { useScrapsReducer } from './use-reducer'
+import { fetchScrape, fetchUnscrape } from './services'
+import {
+  START_SCRAPE,
+  SCRAPE,
+  SCRAPE_FAILED,
+  START_UNSCRAPE,
+  UNSCRAPE,
+  UNSCRAPE_FAILED,
+} from './constants'
+
+export function useScrap({
+  scraps: initialScraps = {},
+  enableTrackEvent,
+  beforeScrapedChange,
+  afterScrapedChange,
+}: ScrapProps) {
+  const { scraps, updating, dispatch } = useScrapsReducer({ initialScraps })
+
+  const deriveCurrentStateAndCount = useCallback(
+    ({
+      id,
+      scraped,
+      scrapsCount: originalScrapsCount,
+    }: {
+      id: string
+      scraped?: boolean
+      scrapsCount?: number
+    }) => {
+      const currentState =
+        typeof updating[id] !== 'undefined' ? updating[id] : scraps[id]
+      const scrapsCount = Number(originalScrapsCount || 0)
+
+      if (typeof scraped !== 'boolean' || typeof currentState !== 'boolean') {
+        /* At least one of the status are unknown: Reduces to a bitwise OR operation */
+        return {
+          scraped: !!scraped || !!currentState,
+          scrapsCount,
+        }
+      }
+
+      return {
+        scraped: currentState,
+        scrapsCount:
+          scraped === currentState
+            ? scrapsCount
+            : currentState
+            ? scrapsCount + 1
+            : scrapsCount - 1,
+      }
+    },
+    [scraps, updating],
+  )
+
+  const onScrape = useCallback(
+    async ({ id, type }: Target) => {
+      if (typeof updating[id] !== 'undefined') {
+        return
+      }
+
+      if (
+        beforeScrapedChange &&
+        beforeScrapedChange({ id, type }, true) === false
+      ) {
+        return
+      }
+
+      dispatch({ type: START_SCRAPE, id })
+
+      const response = await fetchScrape({ id, type })
+
+      if (response.ok) {
+        notifyScraped(id)
+
+        afterScrapedChange && afterScrapedChange({ id, type }, true)
+
+        dispatch({ type: SCRAPE, id })
+      } else {
+        dispatch({ type: SCRAPE_FAILED, id })
+      }
+    },
+    [updating, beforeScrapedChange, dispatch, afterScrapedChange],
+  )
+
+  const onUnscrape = useCallback(
+    async ({ id, type }: Target) => {
+      if (typeof updating[id] !== 'undefined') {
+        return
+      }
+
+      if (
+        beforeScrapedChange &&
+        beforeScrapedChange({ id, type }, false) === false
+      ) {
+        return
+      }
+
+      dispatch({ type: START_UNSCRAPE, id })
+
+      const response = await fetchUnscrape({ id, type })
+
+      if (response.ok) {
+        notifyUnscraped(id)
+
+        afterScrapedChange && afterScrapedChange({ id, type }, false)
+
+        dispatch({ type: UNSCRAPE, id })
+      } else {
+        dispatch({ type: UNSCRAPE_FAILED, id })
+      }
+    },
+    [updating, beforeScrapedChange, dispatch, afterScrapedChange],
+  )
+
+  useEffect(() => {
+    const handleSubscribeEvent = ({
+      id,
+      scraped,
+    }: {
+      id: string
+      scraped: boolean
+    }) => dispatch({ type: scraped ? SCRAPE : UNSCRAPE, id })
+
+    subscribeScrapedChangeEvent(handleSubscribeEvent)
+
+    return () => unsubscribeScrapedChangeEvent(handleSubscribeEvent)
+  }, [dispatch])
+
+  return { deriveCurrentStateAndCount, onScrape, onUnscrape, enableTrackEvent }
+}
