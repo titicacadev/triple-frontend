c17c4363ae38e5d3c246a13d9ebf8d23892d0cbb
Heeryong Kang <drakang4@gmail.com>
2023-11-26 20:01:26 +0900

    feat(triple-web): event-tracking context에 utm 추가

--- packages/triple-web/src/types/event-tracking.ts
+++ packages/triple-web/src/types/event-tracking.ts
@@ -1,7 +1,16 @@
+export interface EventTrackingUtmValue {
+  source?: string
+  medium?: string
+  campaign?: string
+  term?: string
+  content?: string
+}
+
 export interface EventTrackingValue {
   page: {
     label: string
     path: string
   }
+  utm: EventTrackingUtmValue
   onError?: (error: Error) => void
 }
