fcdfbdc6621866c9fe9d7517d1c2e29fc9cc2b78
Heeryong Kang <drakang4@gmail.com>
2023-11-27 22:52:41 +0900

    chore: modals 패키지 제거

--- packages/modals/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from '@titicaca/tds-ui/src/components/alert'
-export * from '@titicaca/tds-ui/src/components/confirm'
-export * from '@titicaca/tds-ui/src/components/modal'
-
-export * from './login-cta-modal'
-export * from './transition-modal'
