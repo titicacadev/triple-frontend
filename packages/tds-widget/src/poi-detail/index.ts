63858926de6cf530abbdc5628f6db96cc6d1fbfa
Heeryong Kang <drakang4@gmail.com>
2023-11-27 17:13:09 +0900

    feat(tds-widget): react-contexts/images-context를 ImageCarouselProvider으로 이동

--- packages/tds-widget/src/poi-detail/index.ts
+++ packages/tds-widget/src/poi-detail/index.ts
@@ -1,5 +1,10 @@
 export { default as Actions } from './actions'
 export { default as DetailHeader } from './detail-header'
 export { default as ImageCarousel } from './image-carousel'
+export {
+  ImageCarouselProvider,
+  ImageCarouselProviderProps,
+  useImageCarousel,
+} from './image-carousel/provider'
 export { default as RecommendedArticles } from './recommended-articles'
 export { default as DetailHeaderV2 } from './detail-header-v2'
