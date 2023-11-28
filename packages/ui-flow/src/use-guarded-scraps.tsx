da820245a33ca2b67bd97b15172d0390799cc465
zhsks528 <hsm950825@naver.com>
2023-11-28 11:29:06 +0900

    Change ContextAPI to custom hook

---
+++ packages/ui-flow/src/use-guarded-scraps.tsx
@@ -0,0 +1,47 @@
+import { useSessionAvailability } from '@titicaca/react-contexts'
+import { useTripleClientMetadata } from '@titicaca/react-triple-client-interfaces'
+import {
+  useTransitionModal,
+  TransitionType,
+  useLoginCtaModal,
+} from '@titicaca/modals'
+import { useScrap } from '@titicaca/tds-widget'
+
+/**
+ * 모웹이거나 세션ID가 없을 때 스크랩이 작동하면 안된다.
+ */
+export function useGuardedScraps({
+  beforeScrapedChange,
+  ...props
+}: Parameters<typeof useScrap>[0]) {
+  const app = useTripleClientMetadata()
+  const sessionAvailable = useSessionAvailability()
+  const { show: showTransitionModal } = useTransitionModal()
+  const { show: showLoginCta } = useLoginCtaModal()
+
+  const { deriveCurrentStateAndCount, onScrape, onUnscrape } = useScrap({
+    beforeScrapedChange: (target, scraped) => {
+      if (!app) {
+        showTransitionModal(TransitionType.Scrap)
+        return false
+      }
+
+      if (sessionAvailable === false) {
+        showLoginCta(undefined, 'POI저장')
+        return false
+      }
+
+      if (beforeScrapedChange) {
+        return beforeScrapedChange(target, scraped)
+      }
+      return true
+    },
+    ...props,
+  })
+
+  return {
+    onGuardedScrape: onScrape,
+    onGuardedUnscrape: onUnscrape,
+    deriveCurrentStateAndCount,
+  }
+}
