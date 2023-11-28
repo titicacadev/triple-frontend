0113c94153b95555d7fc030f9aaecab097095c1d
Hajung Song <dongoc21hj@gmail.com>
2023-11-28 16:49:06 +0900

    LinkBreak 컴포넌트 추가

--- packages/directions-finder/src/directions-finder.stories.tsx
+++ packages/directions-finder/src/directions-finder.stories.tsx
@@ -15,3 +15,13 @@ export const Basic: StoryObj<typeof DirectionsFinder> = {
     phoneNumber: '+81453305211',
   },
 }
+
+export const Grab: StoryObj<typeof DirectionsFinder> = {
+  args: {
+    primaryName: '도쿄 디즈니 랜드',
+    localName: '東京ディズニーランド',
+    localAddress: '〒279-0031 東京都千葉県浦安市舞浜11',
+    phoneNumber: '+81453305211',
+    isGrabSupported: true,
+  },
+}
