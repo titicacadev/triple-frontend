800ef3a29be66fd12b4b1ad23dce6ec059ac9aa3
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-11-28 16:02:27 +0900

    [kint5-poi-detail] AreaNames에 FilledLocationIcon 적용하고 텍스트 색상을 kint5-gray60로 변경

--- packages/kint5-poi-detail/src/area-names.tsx
+++ packages/kint5-poi-detail/src/area-names.tsx
@@ -1,14 +1,9 @@
 import { ReactNode } from 'react'
-import styled from 'styled-components'
-import { Container, Text } from '@titicaca/kint5-core-elements'
-
-const AreaContainer = styled(Container)`
-  padding-left: 16px;
-  background-image: url('https://assets.triple-dev.titicaca-corp.com/images/kint5-location-1.svg');
-  background-size: 12px 12px;
-  background-repeat: no-repeat;
-  background-position: left center;
-`
+import {
+  FilledLocationIcon,
+  FlexBox,
+  Text,
+} from '@titicaca/kint5-core-elements'
 
 interface Area {
   id: number | string
@@ -29,11 +24,18 @@ export default function AreaNames({
   const names = areaName || areas[0]?.name || vicinity
 
   return names ? (
-    <AreaContainer>
-      <Text css={{ fontSize: 13, fontWeight: 400, marginTop: 12 }}>
+    <FlexBox flex css={{ alignItems: 'center', marginTop: 12, gap: 4 }}>
+      <FilledLocationIcon />
+      <Text
+        css={{
+          fontSize: 13,
+          fontWeight: 400,
+          color: 'var(--color-kint5-gray60)',
+        }}
+      >
         {names}
         {arrowAction}
       </Text>
-    </AreaContainer>
+    </FlexBox>
   ) : null
 }
