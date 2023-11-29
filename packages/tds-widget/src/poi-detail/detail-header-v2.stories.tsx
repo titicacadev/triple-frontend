ab11f80280018f3d6e469d101b2eaf2763f9116b
zhsks528 <hsm950825@naver.com>
2023-11-29 15:26:52 +0900

    Setup global userAgentDecorator

--- packages/tds-widget/src/poi-detail/detail-header-v2.stories.tsx
+++ packages/tds-widget/src/poi-detail/detail-header-v2.stories.tsx
@@ -1,27 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { TripleWeb } from '@titicaca/triple-web'
 
 import DetailHeaderV2 from './detail-header-v2'
 
 export default {
   title: 'poi-detail / DetailHeader V2',
   component: DetailHeaderV2,
-  decorators: [
-    (Story) => (
-      <TripleWeb
-        userAgentProvider={{
-          ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
-          browser: { name: 'WebKit', version: '605.1.15', major: '605' },
-          engine: { name: 'WebKit', version: '605.1.15' },
-          os: { name: 'iOS', version: '13.3.1' },
-          device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
-          cpu: { architecture: undefined },
-        }}
-      >
-        <Story />
-      </TripleWeb>
-    ),
-  ],
 } as Meta<typeof DetailHeaderV2>
 
 export const Basic: StoryObj<typeof DetailHeaderV2> = {
