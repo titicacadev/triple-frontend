312c1ef78e1d71bd1c713fa4e5a83c47427b7dd1
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-11-30 18:23:40 +0900

    [kint5-review] FoldableComment에 unfoldTextCss prop 추가

--- packages/kint5-review/src/components/review-element/foldable-comment.tsx
+++ packages/kint5-review/src/components/review-element/foldable-comment.tsx
@@ -1,6 +1,6 @@
 import { MouseEventHandler } from 'react'
 import { useTranslation } from '@titicaca/next-i18next'
-import styled from 'styled-components'
+import { CSSObject } from 'styled-components'
 import { findFoldedPosition } from '@titicaca/view-utilities'
 
 import Comment from './comment'
@@ -8,22 +8,18 @@ import Comment from './comment'
 const MAX_COMMENT_WITH_IMAGE_LINES = 3
 const MAX_COMMENT_LINES = 6
 
-const Unfold = styled.button`
-  display: inline-block;
-  color: #2987f0;
-  outline: 0;
-`
-
 export default function FoldableComment({
   comment,
   hasImage,
   onUnfoldButtonClick,
   maxCommentLines: maxCommentLinesProp,
+  unfoldTextCss,
 }: {
   comment: string
   hasImage: boolean
   onUnfoldButtonClick: MouseEventHandler<HTMLButtonElement>
   maxCommentLines?: number
+  unfoldTextCss?: CSSObject
 }) {
   const maxCommentLines =
     maxCommentLinesProp || hasImage
@@ -35,6 +31,7 @@ export default function FoldableComment({
   return foldedPosition ? (
     <FoldedComment
       comment={comment.slice(0, foldedPosition)}
+      unfoldTextCss={unfoldTextCss}
       onUnfoldButtonClick={onUnfoldButtonClick}
     />
   ) : (
@@ -45,16 +42,28 @@ export default function FoldableComment({
 function FoldedComment({
   comment,
   onUnfoldButtonClick,
+  unfoldTextCss,
 }: {
   comment: string
   onUnfoldButtonClick: MouseEventHandler<HTMLButtonElement>
+  unfoldTextCss?: CSSObject
 }) {
   const { t } = useTranslation('common-web')
 
   return (
     <Comment>
       {`${comment} …`}
-      <Unfold onClick={onUnfoldButtonClick}>{t(['deobogi', '더보기'])}</Unfold>
+      <button
+        onClick={onUnfoldButtonClick}
+        css={{
+          display: 'inline-block',
+          color: '#2987f0',
+          outline: '',
+          ...unfoldTextCss,
+        }}
+      >
+        {t(['deobogi', '더보기'])}
+      </button>
     </Comment>
   )
 }
