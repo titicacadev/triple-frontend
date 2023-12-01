60145ab174a3d7ae9ff5cfe55a9d43e101a18d67
jhyj0521 <jeremy@triple-corp.com>
2023-12-01 14:12:50 +0900

    feat: slide, fadeInOut setInterval에서 framermotion 기능 사용하는 방식으로 변경

--- packages/triple-header/src/layer/transitions/slide.tsx
+++ packages/triple-header/src/layer/transitions/slide.tsx
@@ -1,41 +1,33 @@
-import { useEffect, useState, ReactNode } from 'react'
-import styled from 'styled-components'
-import { Container } from '@titicaca/core-elements'
+import { ReactNode } from 'react'
 
-const SlideContainer = styled(Container)<{ isVisible: boolean }>`
-  position: absolute;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
-`
+import { MotionContainer } from '../../motion-container'
+
+const variants = {
+  slide: ({ index, length }: { index: number; length: number }) => ({
+    opacity: [0, 1, 1, 0],
+    transition: {
+      repeat: Infinity,
+      duration: 3,
+      times: [0, 0, 1, 1],
+      delay: 3 * index,
+      repeatDelay: 3 * length - 3,
+    },
+  }),
+}
 
 export function Slide({ children }: { children: ReactNode[] }) {
-  const [visibleFrameIndex, setVisibleFrameIndex] = useState(0)
-
-  useEffect(() => {
-    const timer = setInterval(
-      () =>
-        setVisibleFrameIndex((prevVisibleFrameIndex) => {
-          return prevVisibleFrameIndex === children.length - 1
-            ? 0
-            : prevVisibleFrameIndex + 1
-        }),
-      3000,
-    )
-
-    return () => clearInterval(timer)
-  }, [children, visibleFrameIndex])
-
   return (
     <>
-      {children.map((slide, index) => {
-        return (
-          <SlideContainer key={index} isVisible={index === visibleFrameIndex}>
-            {slide}
-          </SlideContainer>
-        )
-      })}
+      {children.map((slide, index) => (
+        <MotionContainer
+          key={index}
+          animate="slide"
+          custom={{ index, length: children.length }}
+          variants={variants}
+        >
+          {slide}
+        </MotionContainer>
+      ))}
     </>
   )
 }
