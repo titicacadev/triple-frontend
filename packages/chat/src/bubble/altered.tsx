f0faa226e6e22ccb1dbf7496cfbb2c61668c11ff
guswl98 <hjee0901@gmail.com>
2023-12-04 16:48:14 +0900

    대체 텍스트 컬러 prop 추가

--- packages/chat/src/bubble/altered.tsx
+++ packages/chat/src/bubble/altered.tsx
@@ -1,29 +1,27 @@
 import { FlexBox } from '@titicaca/core-elements'
-import styled from 'styled-components'
+
+import ExclamationMarkIcon from '../icons/ExclamationMarkIcon'
 
 import { Bubble } from './bubble'
 import { BlindedBubbleProp } from './type'
 
-const ExclamationMarkIcon = styled.span<{ color?: 'gray' | 'white' }>`
-  ${({ color = 'gray' }) =>
-    `background-image: url('https://assets.triple.guide/images/ico_exclamation_mark_${color}.svg');`}
-
-  width: 16px;
-  height: 16px;
-`
-
 export default function AlteredBubble({
   my,
   alternativeText,
+  textColor,
   ...props
 }: BlindedBubbleProp) {
   return (
-    <Bubble my={my} css={{ margin: my ? '0 0 0 8px' : undefined }} {...props}>
+    <Bubble
+      my={my}
+      css={{
+        margin: my ? '0 0 0 8px' : undefined,
+        ...(textColor && { color: textColor }),
+      }}
+      {...props}
+    >
       <FlexBox flex alignItems="center" gap="4px">
-        <ExclamationMarkIcon
-          color={my ? 'white' : 'gray'}
-          style={{ flex: '0 0 16px' }}
-        />
+        <ExclamationMarkIcon color={textColor} />
         <span>{alternativeText ?? '관리자에 의해 삭제되었습니다'}</span>
       </FlexBox>
     </Bubble>
