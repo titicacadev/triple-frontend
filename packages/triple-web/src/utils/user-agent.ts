08ec9c797b65e5d179cb5302030e16583c92ec09
zhsks528 <hsm950825@naver.com>
2023-12-04 09:57:06 +0900

    Refine user agent context

--- packages/triple-web/src/utils/user-agent.ts
+++
@@ -1,8 +0,0 @@
-import { UserAgentValue } from '../types'
-
-const MOBILE_REGEX =
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
-
-export function validateMobile(userAgent: UserAgentValue['ua']): boolean {
-  return MOBILE_REGEX.test(userAgent)
-}
