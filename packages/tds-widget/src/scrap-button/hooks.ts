8d8d422de45898badb62d6a7c088c0001e425a3d
zhsks528 <hsm950825@naver.com>
2023-12-05 18:37:37 +0900

    Refactoring scrap-button

--- packages/tds-widget/src/scrap-button/hooks.ts
+++ packages/tds-widget/src/scrap-button/hooks.ts
@@ -3,35 +3,37 @@ import { useTrackEventWithMetadata } from '@titicaca/triple-web'
 import { useScrap } from '../scrap/use-scrap'
 
 import type { ScrapButtonProps } from './types'
+import { useScrapControl } from './scrap-button-controls'
 
 export function useScraped({
   resource: { id, type, scraped },
-  onScrape: initialOnScrape,
-  onUnscrape: initialOnUnscrape,
 }: ScrapButtonProps) {
-  const { onScrape, onUnscrape, deriveCurrentStateAndCount, enableTrackEvent } =
-    useScrap()
+  const { onScrape, onUnscrape, deriveCurrentStateAndCount } = useScrap()
+  const scrapControlActions = useScrapControl()
   const { scraped: actualScraped } = deriveCurrentStateAndCount({ id, scraped })
   const trackEventWithMetadata = useTrackEventWithMetadata()
 
-  const scrape = initialOnScrape ?? onScrape
-  const unscrape = initialOnUnscrape ?? onUnscrape
+  const scrape = scrapControlActions?.onScrape ?? onScrape
+  const unscrape = scrapControlActions?.onUnscrape ?? onUnscrape
 
   const handleToggleScrape = () => {
     const toggleScrape = actualScraped ? unscrape : scrape
     toggleScrape({ id, type })
 
-    if (enableTrackEvent) {
-      const action = actualScraped ? 'POI저장취소' : 'POI저장'
-      trackEventWithMetadata({
-        ga: [action, `${id}`],
-        fa: {
-          action,
-          item_id: id,
-          content_type: type,
-        },
-      })
+    if (scrapControlActions !== undefined) {
+      return
     }
+
+    const action = actualScraped ? 'POI저장취소' : 'POI저장'
+
+    trackEventWithMetadata({
+      ga: [action, `${id}`],
+      fa: {
+        action,
+        item_id: id,
+        content_type: type,
+      },
+    })
   }
 
   return [actualScraped, handleToggleScrape] as const
