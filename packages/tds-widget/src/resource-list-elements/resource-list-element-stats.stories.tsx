c0f43fda4a0f78bdcf1a50ff7321f5a2651f4941
zhsks528 <hsm950825@naver.com>
2023-12-05 14:29:52 +0900

    Update resource-list-elements storybook

--- packages/tds-widget/src/resource-list-elements/resource-list-element-stats.stories.tsx
+++ packages/tds-widget/src/resource-list-elements/resource-list-element-stats.stories.tsx
@@ -2,12 +2,20 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import ResourceListElementStats from './resource-list-element-stats'
 
-export default {
+const meta: Meta<typeof ResourceListElementStats> = {
   title: 'resource-list-element / resource-list-element-stats',
   component: ResourceListElementStats,
-} as Meta<typeof ResourceListElementStats>
+}
 
-export const Basic: StoryObj<typeof ResourceListElementStats> = {
+export default meta
+
+export const Word: StoryObj<typeof ResourceListElementStats> = {
+  args: {
+    stats: ['볼거리'],
+  },
+}
+
+export const Phrase: StoryObj<typeof ResourceListElementStats> = {
   args: {
     stats: ['볼거리', '판교'],
   },
--- packages/tds-widget/src/resource-list-elements/review-scrap-stat.stories.tsx
+++ packages/tds-widget/src/resource-list-elements/review-scrap-stat.stories.tsx
@@ -2,12 +2,27 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import ReviewScrapStat from './review-scrap-stat'
 
-export default {
+const meta: Meta<typeof ReviewScrapStat> = {
   title: 'resource-list-element / review-scrap-stat',
   component: ReviewScrapStat,
-} as Meta<typeof ReviewScrapStat>
+}
 
-export const Basic: StoryObj<typeof ReviewScrapStat> = {
+export default meta
+
+export const Review: StoryObj<typeof ReviewScrapStat> = {
+  args: {
+    reviewsCount: 23,
+    reviewsRating: 3.7,
+  },
+}
+
+export const Scrap: StoryObj<typeof ReviewScrapStat> = {
+  args: {
+    scrapsCount: 1027,
+  },
+}
+
+export const Full: StoryObj<typeof ReviewScrapStat> = {
   args: {
     reviewsCount: 23,
     scrapsCount: 7,
