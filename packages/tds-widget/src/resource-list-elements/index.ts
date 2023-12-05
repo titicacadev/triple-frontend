efb534af1d6514c87124dd3ae224c4a798ef11a4
zhsks528 <hsm950825@naver.com>
2023-12-05 14:34:21 +0900

    Fix export expression ExtendedResourceListElement component

--- packages/tds-widget/src/resource-list-elements/index.ts
+++ packages/tds-widget/src/resource-list-elements/index.ts
@@ -3,6 +3,6 @@ export { default as ReviewScrapStat } from './review-scrap-stat'
 export { default as ResourceListElementStats } from './resource-list-element-stats'
 
 export {
-  default,
+  default as ExtendedResourceListElement,
   ResourceListElementProps,
 } from './extended-resource-list-element'
