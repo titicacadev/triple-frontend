e2c65a8c558615ffca5c45fab05aa4d5e9619f8d
zhsks528 <hsm950825@naver.com>
2023-12-06 15:13:30 +0900

    ImagesProvider -> ImageCarouselProvider 수정

--- packages/tds-widget/src/poi-detail/image-carousel.stories.tsx
+++ packages/tds-widget/src/poi-detail/image-carousel.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { ImagesProvider } from '@titicaca/react-contexts'
 
 import ImageCarousel from './image-carousel'
+import { ImageCarouselProvider } from './image-carousel/provider'
 
 export default {
   title: 'poi-detail / ImageCarousel',
@@ -11,14 +11,14 @@ export default {
 export const AttractionOrRestaurant: StoryObj<typeof ImageCarousel> = {
   decorators: [
     (Story) => (
-      <ImagesProvider
+      <ImageCarouselProvider
         source={{
           id: 'e889ae22-0336-4cf9-8fbb-742b95fd09d0',
           type: 'attraction',
         }}
       >
         <Story />
-      </ImagesProvider>
+      </ImageCarouselProvider>
     ),
   ],
 }
@@ -26,14 +26,14 @@ export const AttractionOrRestaurant: StoryObj<typeof ImageCarousel> = {
 export const Hotel: StoryObj<typeof ImageCarousel> = {
   decorators: [
     (Story) => (
-      <ImagesProvider
+      <ImageCarouselProvider
         source={{
           id: '1ff98b6f-ca34-4961-ae29-fa52c8ca2e21',
           type: 'hotel',
         }}
       >
         <Story />
-      </ImagesProvider>
+      </ImageCarouselProvider>
     ),
   ],
 }
