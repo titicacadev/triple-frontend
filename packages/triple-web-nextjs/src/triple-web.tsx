5f5a919d5a35f34a3fae976a9e51edef585cea4f
Heeryong Kang <drakang4@gmail.com>
2023-12-08 19:29:47 +0900

    feat(triple-web-nextjs): app router용 TripleWeb 추가

---
+++ packages/triple-web-nextjs/src/triple-web.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import {
+  TripleWeb as TripleWebBase,
+  TripleWebProps as TripleWebBaseProps,
+  I18nValue,
+} from '@titicaca/triple-web'
+
+import { useLang } from './hooks'
+
+export interface TripleWebProps
+  extends Omit<TripleWebBaseProps, 'i18nProvider'> {
+  i18nProvider: Omit<I18nValue, 'lang'> & { key?: string; fallback: string }
+}
+
+export async function TripleWeb({
+  children,
+  clientAppProvider,
+  envProvider,
+  i18nProvider,
+  sessionProvider,
+  userAgentProvider,
+}: TripleWebProps) {
+  const lang = useLang(i18nProvider.fallback, i18nProvider.key)
+
+  return (
+    <TripleWebBase
+      clientAppProvider={clientAppProvider}
+      envProvider={envProvider}
+      i18nProvider={{ i18n: i18nProvider.i18n, lang }}
+      sessionProvider={sessionProvider}
+      userAgentProvider={userAgentProvider}
+    >
+      {children}
+    </TripleWebBase>
+  )
+}
