b9b952cdb0e49f9d73b418284ad6949659756bfb
jaehyeon48 <kjhyeon48@interparktriple.com>
2023-12-08 10:19:34 +0900

    [kint5-core-elements] CaretDownIcon 추가

--- packages/kint5-core-elements/src/elements/icon/index.ts
+++ packages/kint5-core-elements/src/elements/icon/index.ts
@@ -1,5 +1,6 @@
 export * from './bubble-icon'
 export * from './calendar-icon'
+export * from './caret-down-icon'
 export * from './caret-up-icon'
 export * from './close-icon'
 export * from './empty-checkbox-icon'
