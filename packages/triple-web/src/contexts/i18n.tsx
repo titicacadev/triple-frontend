87a3537ddfc0874d072858dc45f4c488c3054cf2
Heeryong Kang <drakang4@gmail.com>
2023-12-08 19:24:56 +0900

    feat(triple-web): SSR에서 i18n 초기 언어 설정

--- packages/triple-web/src/contexts/i18n.tsx
+++ packages/triple-web/src/contexts/i18n.tsx
@@ -1,21 +1,21 @@
 'use client'
 
 import { PropsWithChildren, createContext, useEffect } from 'react'
-import { i18n as I18nInstance } from 'i18next'
 
-export interface I18nValue {
-  i18n?: I18nInstance
-  lang?: string
-}
+import { I18nValue } from '../types'
 
 export const I18nContext = createContext<I18nValue | undefined>(undefined)
 
-type I18nProviderProps = PropsWithChildren & I18nValue
+export type I18nProviderProps = PropsWithChildren & I18nValue
 
 export function I18nProvider({ children, i18n, lang }: I18nProviderProps) {
+  if (typeof window === 'undefined' && i18n.resolvedLanguage !== lang) {
+    i18n.changeLanguage(lang)
+  }
+
   useEffect(() => {
-    if (i18n?.resolvedLanguage !== lang) {
-      i18n?.changeLanguage(lang)
+    if (i18n.resolvedLanguage !== lang) {
+      i18n.changeLanguage(lang)
     }
   }, [i18n, lang])
 
