d9fb9b4773976d62b21bc813a00fc742b39a2a76
zhsks528 <hsm950825@naver.com>
2023-12-12 21:17:10 +0900

    FlickingCarousel 재작성

--- packages/tds-ui/src/components/carousel/flicking-carousel.tsx
+++ packages/tds-ui/src/components/carousel/flicking-carousel.tsx
@@ -1,103 +1,66 @@
-import { PropsWithChildren, useRef } from 'react'
-import styled, { css } from 'styled-components'
-import Flicking from '@egjs/react-flicking'
+import { PropsWithChildren } from 'react'
+import type { FlickingProps } from '@egjs/react-flicking'
 import type { FlickingOptions } from '@egjs/flicking'
+import styled from 'styled-components'
 
 import { Container } from '../container'
-import { formatMarginPadding } from '../../mixins'
 import type { MarginPadding } from '../../commons'
 
-import ArrowIcon from './arrow-icon'
-import { CarouselItem } from './carousel-item'
+import { FlickingCarouselProvider } from './flicking-carousel-context'
+import { FlickingPageLabel } from './flicking-page-label'
+import FlickingCarouselContent from './flicking-carousel-content'
+import { FlickingCarouselControls } from './flicking-carousel-controls'
 
-interface FlickingCarouselProps {
+interface CarouselBaseProps {
   margin?: MarginPadding
   containerPadding?: { left: number; right: number }
 }
 
-const FlickingContainer = styled.div`
-  .eg-flick-panel {
-    margin-left: 0 !important;
+interface FlickingEvents {
+  onMoveStart?: FlickingProps['onMoveStart']
+  onMove?: FlickingProps['onMove']
+  onMoveEnd?: FlickingProps['onMoveEnd']
+  options?: Partial<FlickingOptions>
+}
+
+type FlickingCarouselProps = CarouselBaseProps & FlickingEvents
+
+const CarouselContainer = styled(Container)`
+  overflow: visible;
+
+  img {
+    pointer-events: none;
   }
 `
 
-const FlickingScrollButton = styled.button<{
-  direction: 'left' | 'right'
-  containerPadding?: { left: number; right: number }
-}>`
-  position: absolute;
-  width: 60px;
-  height: 60px;
-  top: calc(50% - 30px);
-  ${({ direction, containerPadding }) => css`
-    ${direction}: ${(containerPadding?.[direction] || 0) - 30}px;
-  `}
-  z-index: 60;
-  outline: none;
-`
-
-const FLICK_ATTRIBUTES: Partial<FlickingOptions> = {
-  deceleration: 0.0075,
-  horizontal: true,
-  circular: true,
-  infinite: false,
-  infiniteThreshold: 0,
-  lastIndex: Infinity,
-  threshold: 40,
-  duration: 100,
-  panelEffect: (x: number) => 1 - Math.pow(1 - x, 3),
-  defaultIndex: 0,
-  thresholdAngle: 45,
-  bounce: 10,
-  autoResize: false,
-  adaptive: false,
-  bound: false,
-  overflow: false,
-  hanger: '50%',
-  anchor: '50%',
-  gap: 10,
-  moveType: { type: 'snap', count: 1 },
-  collectStatistics: false,
-  zIndex: 50,
-  classPrefix: 'eg-flick',
-}
-
+/**
+ * 정의: @egjs/flicking를 적용한 Carousel입니다.
+ * 기능: 가로 스크롤 지원, 좌/우 화살표 지원
+ */
 export function FlickingCarousel({
   margin,
   containerPadding,
+  onMoveStart,
+  onMove,
+  onMoveEnd,
+  options,
   children,
+  ...cssProps
 }: PropsWithChildren<FlickingCarouselProps>) {
-  const flickingRef = useRef<Flicking>(null)
-
   return (
-    <Container
-      position="relative"
-      css={css`
-        ${formatMarginPadding(margin, 'margin')}
-        ${formatMarginPadding(containerPadding, 'padding')}
-      `}
+    <FlickingCarouselProvider
+      onMoveStart={onMoveStart}
+      onMove={onMove}
+      onMoveEnd={onMoveEnd}
+      options={options}
     >
-      <FlickingScrollButton
-        containerPadding={containerPadding}
-        direction="left"
-        onClick={() => flickingRef.current?.prev()}
-      >
-        <ArrowIcon direction="left" />
-      </FlickingScrollButton>
-      <FlickingContainer>
-        <Flicking ref={flickingRef} {...FLICK_ATTRIBUTES}>
-          {children}
-        </Flicking>
-      </FlickingContainer>
-      <FlickingScrollButton
-        containerPadding={containerPadding}
-        direction="right"
-        onClick={() => flickingRef.current?.next()}
-      >
-        <ArrowIcon direction="right" />
-      </FlickingScrollButton>
-    </Container>
+      <CarouselContainer position="relative" {...cssProps}>
+        {children}
+      </CarouselContainer>
+    </FlickingCarouselProvider>
   )
 }
 
-FlickingCarousel.Item = CarouselItem
+FlickingCarousel.PageLabel = FlickingPageLabel
+FlickingCarousel.Content = FlickingCarouselContent
+FlickingCarousel.Controls = FlickingCarouselControls
