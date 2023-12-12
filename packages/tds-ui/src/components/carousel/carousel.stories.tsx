cfbf1180e2e2b0ad4f3efe4f5a11f42d3f335419
zhsks528 <hsm950825@naver.com>
2023-12-12 21:17:54 +0900

    tds-ui/carousel storybook 작성

---
+++ packages/tds-ui/src/components/carousel/carousel.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Carousel } from './carousel'
+import IMAGES from './mocks/carousel.sample.json'
+
+const meta: Meta<typeof Carousel> = {
+  title: 'core-elements / Carousel / default',
+  component: Carousel,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Carousel>
+
+export const Basic: Story = {
+  render: () => (
+    <Carousel>
+      {IMAGES.map((image, key) => (
+        <img
+          key={key}
+          src={image.sizes.large.url}
+          alt="test"
+          width={400}
+          height={400}
+        />
+      ))}
+    </Carousel>
+  ),
+}
