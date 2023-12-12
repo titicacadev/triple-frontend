17c8dfa1d49d147558da1a897ebbed2b32f23907
zhsks528 <hsm950825@naver.com>
2023-12-13 02:57:24 +0900

    flicking ref 추가

--- packages/tds-ui/src/components/carousel/flicking-carousel.tsx
+++ packages/tds-ui/src/components/carousel/flicking-carousel.tsx
@@ -1,14 +1,15 @@
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useRef } from 'react'
 import type { FlickingProps } from '@egjs/react-flicking'
 import type { FlickingOptions } from '@egjs/flicking'
 import styled from 'styled-components'
+import Flicking from '@egjs/react-flicking'
 
 import { Container } from '../container'
 import type { MarginPadding } from '../../commons'
 
-import { FlickingCarouselProvider } from './flicking-carousel-context'
+import FlickingCarouselProvider from './flicking-carousel-context'
 import { FlickingPageLabel } from './flicking-page-label'
-import FlickingCarouselContent from './flicking-carousel-content'
+import { FlickingCarouselContent } from './flicking-carousel-content'
 import { FlickingCarouselControls } from './flicking-carousel-controls'
 
 interface CarouselBaseProps {
@@ -47,8 +48,11 @@ export function FlickingCarousel({
   children,
   ...cssProps
 }: PropsWithChildren<FlickingCarouselProps>) {
+  const flickingRef = useRef<Flicking>(null)
+
   return (
     <FlickingCarouselProvider
+      ref={flickingRef}
       onMoveStart={onMoveStart}
       onMove={onMove}
       onMoveEnd={onMoveEnd}
