59d36caaf54b116cae6cd5566a1a7f38378823c5
zhsks528 <hsm950825@naver.com>
2023-12-13 16:02:33 +0900

    Hook import 수정

--- packages/triple-web/src/contexts/client-app.ts
+++ packages/triple-web/src/contexts/client-app.ts
@@ -1,17 +1,7 @@
-import { createContext, useContext } from 'react'
+import { createContext } from 'react'
 
 import { ClientAppValue } from '../types'
 
 export const ClientAppContext = createContext<ClientAppValue | undefined>(
   undefined,
 )
-
-export function useClientApp() {
-  const context = useContext(ClientAppContext)
-
-  if (context === undefined) {
-    throw new Error('ClientAppContext가 없습니다.')
-  }
-
-  return context
-}
