a4e762ff3ea51947cd0b46823dd513e7417bf7ae
zhsks528 <hsm950825@naver.com>
2023-12-18 15:53:26 +0900

    Carousel storybook 보완

--- packages/tds-ui/src/components/carousel/carousel.stories.tsx
+++ packages/tds-ui/src/components/carousel/carousel.stories.tsx
@@ -4,8 +4,15 @@ import { Carousel } from './carousel'
 import IMAGES from './mocks/carousel.sample.json'
 
 const meta: Meta<typeof Carousel> = {
-  title: 'tds-ui / Carousel / default',
+  title: 'tds-ui / Carousel / Carousel ',
   component: Carousel,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Only CSS로 작성된 Carousel 컴포넌트 입니다.',
+      },
+    },
+  },
 }
 
 export default meta
@@ -13,6 +20,7 @@ export default meta
 type Story = StoryObj<typeof Carousel>
 
 export const Basic: Story = {
+  name: '기본 Carousel',
   render: () => (
     <Carousel>
       {IMAGES.map((image, key) => (
