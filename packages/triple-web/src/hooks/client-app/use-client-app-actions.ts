416b9f91f09b168a5e2228eb1dd4b3a97312ac35
Heeryong Kang <drakang4@gmail.com>
2023-12-18 13:11:36 +0900

    feat(triple-web): useClientAppActions 이동

--- packages/triple-web/src/hooks/client-app/use-client-app-actions.ts
+++ packages/triple-web/src/hooks/client-app/use-client-app-actions.ts
@@ -1,7 +1,7 @@
 import * as WebToNativeInterfaces from '@titicaca/triple-web-to-native-interfaces'
 import * as semver from 'semver'
 
-import { useTripleClientMetadata } from './triple-client-metadata-context'
+import { useClientApp } from './use-client-app'
 
 const KNOWN_INITIAL_VERSIONS: Partial<
   Record<keyof typeof WebToNativeInterfaces, string>
@@ -10,15 +10,15 @@ const KNOWN_INITIAL_VERSIONS: Partial<
   unsubscribeTripUpdateEvent: '5.11.0',
 }
 
-export function useTripleClientActions() {
-  const clientContextValue = useTripleClientMetadata()
+export function useClientAppActions() {
+  const clientApp = useClientApp()
 
-  if (!clientContextValue) {
+  if (!clientApp) {
     return {}
   }
 
-  const { appVersion } = clientContextValue
-  const appVersionSemver = semver.coerce(appVersion)
+  const { version } = clientApp.metadata
+  const versionSemver = semver.coerce(version)
 
   const filteredAccessibleWebToNativeInterfaces = Object.keys(
     WebToNativeInterfaces,
@@ -36,7 +36,7 @@ export function useTripleClientActions() {
       if (
         KNOWN_INITIAL_VERSIONS[interfaceNameKey] &&
         !semver.gte(
-          appVersionSemver as semver.SemVer,
+          versionSemver as semver.SemVer,
           KNOWN_INITIAL_VERSIONS[interfaceNameKey] as string,
         )
       ) {
