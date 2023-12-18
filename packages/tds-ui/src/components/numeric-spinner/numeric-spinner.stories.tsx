7d88754776a5f1330c3667a7e93398e3819145eb
zhsks528 <hsm950825@naver.com>
2023-12-19 00:19:47 +0900

    NumericSpinner storybook 보완

--- packages/tds-ui/src/components/numeric-spinner/numeric-spinner.stories.tsx
+++ packages/tds-ui/src/components/numeric-spinner/numeric-spinner.stories.tsx
@@ -1,53 +1,112 @@
-import type { StoryFn, Meta, StoryObj } from '@storybook/react'
-import { useState } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { useArgs } from '@storybook/preview-api'
 
 import { NumericSpinner } from './numeric-spinner'
 
-export default {
+const meta: Meta<typeof NumericSpinner> = {
   title: 'tds-ui / NumericSpinner',
   component: NumericSpinner,
-} as Meta<typeof NumericSpinner>
+  parameters: {
+    docs: {
+      description: {
+        component:
+          '사용자가 버튼을 통해 숫자를 조절할 수 있는 뷰 컴포넌트입니다.',
+      },
+    },
+  },
+}
 
-export const Default: StoryObj<typeof NumericSpinner> = {
+export default meta
+
+type Story = StoryObj<typeof NumericSpinner>
+
+export const Default: Story = {
+  name: '기본',
   args: {
     label: '성인',
+    value: 0,
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: number) => {
+      updateArgs({ value })
+    }
+
+    return <NumericSpinner {...args} value={value} onChange={handleChange} />
   },
 }
 
-export const Disabled: StoryObj<typeof NumericSpinner> = {
+export const Disabled: Story = {
+  name: '비활성화',
   args: {
-    ...Default.args,
+    label: '성인',
     disabled: true,
   },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: number) => {
+      updateArgs({ value })
+    }
+
+    return <NumericSpinner {...args} value={value} onChange={handleChange} />
+  },
 }
 
-export const Label: StoryFn<typeof NumericSpinner> = () => {
-  return (
-    <>
-      <NumericSpinner {...Default.args} />
-      <NumericSpinner {...Default.args} sublabel="10,000원" />
-      <NumericSpinner {...Default.args} strikeLabel="20,000원" />
-      <NumericSpinner
-        {...Default.args}
-        sublabel="10,000원"
-        strikeLabel="20,000원"
-      />
-    </>
-  )
+export const Label: Story = {
+  name: '스티커',
+  args: {
+    label: '성인',
+    value: 0,
+    sublabel: '10,000원',
+    strikeLabel: '20,000원',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: number) => {
+      updateArgs({ value })
+    }
+
+    return <NumericSpinner {...args} value={value} onChange={handleChange} />
+  },
 }
 
-export const Size: StoryFn<typeof NumericSpinner> = () => {
-  return (
-    <>
-      <NumericSpinner {...Default.args} size="tiny" />
-      <NumericSpinner {...Default.args} size="medium" />
-      <NumericSpinner {...Default.args} size="big" />
-    </>
-  )
-}
+export const Size: Story = {
+  name: '글자 크기',
+  args: {
+    label: '성인',
+    value: 0,
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
 
-export const Controlled: StoryFn<typeof NumericSpinner> = () => {
-  const [value, setValue] = useState(1)
+    const handleChange = (value: number) => {
+      updateArgs({ value })
+    }
 
-  return <NumericSpinner {...Default.args} value={value} onChange={setValue} />
+    return (
+      <>
+        <NumericSpinner
+          {...args}
+          value={value}
+          size="tiny"
+          onChange={handleChange}
+        />
+        <NumericSpinner
+          {...args}
+          value={value}
+          size="medium"
+          onChange={handleChange}
+        />
+        <NumericSpinner
+          {...args}
+          value={value}
+          size="big"
+          onChange={handleChange}
+        />
+      </>
+    )
+  },
 }
