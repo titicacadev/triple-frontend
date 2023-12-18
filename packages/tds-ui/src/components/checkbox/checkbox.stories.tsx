1dd1cb7c6ca7bc7a4679f48e6410f9d5c1a50775
zhsks528 <hsm950825@naver.com>
2023-12-18 16:26:08 +0900

    Checkbox storybook 보완

--- packages/tds-ui/src/components/checkbox/checkbox.stories.tsx
+++ packages/tds-ui/src/components/checkbox/checkbox.stories.tsx
@@ -1,28 +1,68 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/react'
-import { useState } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { useArgs } from '@storybook/preview-api'
 
 import { Checkbox } from './checkbox'
 
-export default {
-  title: 'tds-ui / Checkbox',
+const meta: Meta<typeof Checkbox> = {
+  title: 'tds-ui / Checkbox ',
   component: Checkbox,
-} as Meta<typeof Checkbox>
-
-export const Default: StoryObj<typeof Checkbox> = {
-  render: (args) => {
-    return <Checkbox {...args}>Option</Checkbox>
+  parameters: {
+    docs: {
+      description: {
+        component:
+          '사용자가 수많은 선택 사항에서 여러 개를 선택할 수 있도록 제공하는 뷰 컴포넌트입니다.',
+      },
+    },
   },
 }
 
-export const Controlled: StoryFn<typeof Checkbox> = () => {
-  const [checked, setChecekd] = useState(false)
+export default meta
 
-  return (
-    <Checkbox
-      checked={checked}
-      onChange={(event) => setChecekd(event.target.checked)}
-    >
-      Option
-    </Checkbox>
-  )
+type Story = StoryObj<typeof Checkbox>
+
+export const Square: Story = {
+  name: '사각형 모양',
+  render: (args) => {
+    return (
+      <div css={{ display: 'inline-block' }}>
+        <Checkbox {...args} variant="square" />
+        <Checkbox {...args} variant="square" checked />
+      </div>
+    )
+  },
+}
+
+export const Round: Story = {
+  name: '둥근 모양',
+  render: (args) => {
+    return (
+      <div css={{ display: 'inline-block' }}>
+        <Checkbox {...args} variant="round" />
+        <Checkbox {...args} variant="round" checked />
+      </div>
+    )
+  },
+}
+
+export const WithText: Story = {
+  name: '제목',
+  args: {
+    variant: 'square',
+    checked: true,
+  },
+  render: function Render(args) {
+    const [{ checked }, updateArgs] = useArgs()
+
+    const onChange = () => {
+      updateArgs({ checked: !checked })
+    }
+
+    return (
+      <div css={{ display: 'inline-block' }}>
+        <Checkbox {...args} onChange={onChange}>
+          개인정보 동의
+        </Checkbox>
+      </div>
+    )
+  },
 }
