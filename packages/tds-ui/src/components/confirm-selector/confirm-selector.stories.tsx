11d3bb16b9c753eb5eceb82270bc348581ae9965
zhsks528 <hsm950825@naver.com>
2023-12-18 17:55:06 +0900

    ConfirmSelector storybook 수정

--- packages/tds-ui/src/components/confirm-selector/confirm-selector.stories.tsx
+++ packages/tds-ui/src/components/confirm-selector/confirm-selector.stories.tsx
@@ -1,12 +1,17 @@
-import type { StoryFn, Meta } from '@storybook/react'
+import type { StoryObj, Meta } from '@storybook/react'
 
 import { ConfirmSelector } from './confirm-selector'
 
-export default {
+const meta: Meta<typeof ConfirmSelector> = {
   title: 'tds-ui / ConfirmSelector',
   component: ConfirmSelector,
-} as Meta<typeof ConfirmSelector>
+}
 
-export const Default: StoryFn<typeof ConfirmSelector> = () => {
-  return <ConfirmSelector>이용약관 동의</ConfirmSelector>
+export default meta
+
+export const Default: StoryObj<typeof ConfirmSelector> = {
+  name: '기본',
+  render: () => {
+    return <ConfirmSelector>이용약관 동의</ConfirmSelector>
+  },
 }
