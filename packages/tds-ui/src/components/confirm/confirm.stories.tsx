dca3aab06af47c59b713a4977f3e5949cb445767
zhsks528 <hsm950825@naver.com>
2023-12-18 17:47:50 +0900

    Confirm storybook 보완

--- packages/tds-ui/src/components/confirm/confirm.stories.tsx
+++ packages/tds-ui/src/components/confirm/confirm.stories.tsx
@@ -1,19 +1,29 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { useArgs } from '@storybook/preview-api'
 
 import { Confirm } from './confirm'
 
-export default {
-  title: 'modals / Confirm',
+const meta: Meta<typeof Confirm> = {
+  title: 'tds-ui / Confirm',
   component: Confirm,
   parameters: {
-    story: {
-      inline: false,
-      iframeHeight: 500,
+    docs: {
+      description: {
+        component:
+          '사용자에게 다음 행동을 결정하도록 도와주는 뷰 컴포넌트입니다.',
+      },
+      story: {
+        inline: false,
+        iframeHeight: 500,
+      },
     },
   },
-} as Meta<typeof Confirm>
+}
+
+export default meta
 
 export const Default: StoryObj<typeof Confirm> = {
+  name: '기본',
   args: {
     open: true,
     title: '요금 변동을 확인해주세요',
@@ -21,5 +31,20 @@ export const Default: StoryObj<typeof Confirm> = {
       '유류할증료 및 기타세금에 변동이 있어 항공권의 총 요금이 변경되었습니다. 이 요금으로 결제를 진행하시겠습니까?',
     cancelText: '예약 취소',
     confirmText: '네, 진행합니다.',
+  },
+  render: function Render(args) {
+    const [{ open }, updateArgs] = useArgs()
+
+    const onClose = () => {
+      updateArgs({ open: !open })
+    }
+
+    if (!open) {
+      return (
+        <button onClick={() => updateArgs({ open: true })}>모달 열기</button>
+      )
+    }
+
+    return <Confirm {...args} onClose={onClose} />
   },
 }
