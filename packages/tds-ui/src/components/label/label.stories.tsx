1e450bdadf5c0e0050cd42aca5dbb13b3d412474
zhsks528 <hsm950825@naver.com>
2023-12-18 19:48:28 +0900

    Label storybook 보완

--- packages/tds-ui/src/components/label/label.stories.tsx
+++ packages/tds-ui/src/components/label/label.stories.tsx
@@ -1,27 +1,100 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import styled from 'styled-components'
 
-import { Label } from './label'
+import { Label, LabelColor } from './label'
 
-export default {
+const meta: Meta<typeof Label> = {
   title: 'tds-ui / Label',
   component: Label,
-} as Meta
-
-export const Radio: StoryObj<typeof Label> = {
-  name: '라디오',
-  args: {
-    radio: true,
-    selected: false,
-    children: '최신순',
+  parameters: {
+    docs: {
+      description: {
+        component:
+          '사용자에게 다음 행동을 결정하도록 도와주는 뷰 컴포넌트입니다.',
+      },
+    },
   },
 }
 
-export const Promo: StoryObj<typeof Label> = {
-  name: '최대 24%',
+export default meta
+
+type Story = StoryObj<typeof Label>
+
+export const Default: Story = {
+  name: '기본',
+  render: (args) => {
+    return <Label {...args}>최신순</Label>
+  },
+}
+
+export const Radio: Story = {
+  name: '라디오',
+  args: {
+    radio: true,
+  },
+  render: (args) => {
+    return (
+      <>
+        <Label {...args} selected margin={{ right: 10 }}>
+          최신순
+        </Label>
+        <Label {...args}>인기순</Label>
+      </>
+    )
+  },
+}
+
+const LabelContainer = styled.div`
+  display: flex;
+  gap: 5px;
+`
+
+const COLORS: LabelColor[] = [
+  'blue',
+  'red',
+  'purple',
+  'orange',
+  'gray',
+  'green',
+  'white',
+  'skyblue',
+  'lightpurple',
+  'black',
+]
+
+const SIZES: ['tiny', 'small', 'medium', 'large'] = [
+  'tiny',
+  'small',
+  'medium',
+  'large',
+]
+
+export const Promo: Story = {
+  name: '프로모션',
   args: {
     promo: true,
-    size: 'medium',
-    color: 'purple',
-    children: '최대 24%',
+  },
+  render: (args) => {
+    return (
+      <>
+        <div>색상</div>
+        <LabelContainer>
+          {COLORS.map((color, idx) => (
+            <Label {...args} key={idx} color={color} size="large">
+              {color}
+            </Label>
+          ))}
+        </LabelContainer>
+
+        <div>글자 크기</div>
+        <LabelContainer>
+          {SIZES.map((size, idx) => (
+            <Label {...args} key={idx} size={size}>
+              {size}
+            </Label>
+          ))}
+        </LabelContainer>
+      </>
+    )
   },
 }
