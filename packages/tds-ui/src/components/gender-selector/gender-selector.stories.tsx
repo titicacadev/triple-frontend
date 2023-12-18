2ad1e79543fddf48600f17a0eec956b15a172447
zhsks528 <hsm950825@naver.com>
2023-12-18 19:24:37 +0900

    GenderSelector storybook 보완

--- packages/tds-ui/src/components/gender-selector/gender-selector.stories.tsx
+++ packages/tds-ui/src/components/gender-selector/gender-selector.stories.tsx
@@ -1,19 +1,127 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/react'
-import { useState } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { useArgs } from '@storybook/preview-api'
 
 import { GenderSelector } from './gender-selector'
 
-export default {
+const meta: Meta<typeof GenderSelector> = {
   title: 'tds-ui / GenderSelector',
   component: GenderSelector,
-} as Meta<typeof GenderSelector>
-
-export const Default: StoryObj<typeof GenderSelector> = {
-  args: {},
+  parameters: {
+    docs: {
+      description: {
+        component: '성별을 선택할 때 사용되는 뷰 컴포넌트입니다.',
+      },
+    },
+  },
 }
 
-export const Controlled: StoryFn<typeof GenderSelector> = () => {
-  const [value, setValue] = useState('MALE')
+export default meta
 
-  return <GenderSelector name="gender" value={value} onChange={setValue} />
+type Story = StoryObj<typeof GenderSelector>
+
+export const Default: Story = {
+  name: '기본',
+  args: {
+    name: 'gender',
+    value: 'MALE',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return <GenderSelector {...args} value={value} onChange={handleChange} />
+  },
+}
+
+export const Disabled: Story = {
+  name: '비활성화',
+  args: {
+    name: 'gender',
+    value: 'MALE',
+    disabled: true,
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return <GenderSelector {...args} value={value} onChange={handleChange} />
+  },
+}
+
+export const Required: Story = {
+  name: '필수',
+  args: {
+    name: 'gender',
+    value: 'MALE',
+    required: true,
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return <GenderSelector {...args} value={value} onChange={handleChange} />
+  },
+}
+
+export const WithLabel: Story = {
+  name: '스티커',
+  args: {
+    name: 'gender',
+    value: 'MALE',
+    label: '성별 선택',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return <GenderSelector {...args} value={value} onChange={handleChange} />
+  },
+}
+
+export const WithHelpMessage: Story = {
+  name: '가이드 메시지',
+  args: {
+    name: 'gender',
+    value: 'MALE',
+    help: '가이드 메시지',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return <GenderSelector {...args} value={value} onChange={handleChange} />
+  },
+}
+
+export const WithErrorMessage: Story = {
+  name: '에러 메시지',
+  args: {
+    name: 'gender',
+    value: 'MALE',
+    error: '에러 메시지',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return <GenderSelector {...args} value={value} onChange={handleChange} />
+  },
 }
