0b46dac88a6fa42803cfd8b06d5a19dc499594d6
zhsks528 <hsm950825@naver.com>
2023-12-18 19:09:58 +0900

    FormField storybook 보완

--- packages/tds-ui/src/components/form-field/form-field.stories.tsx
+++ packages/tds-ui/src/components/form-field/form-field.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
 import { FormField } from './form-field'
 import { useFormField } from './form-field-context'
@@ -6,10 +6,19 @@ import { FormFieldError } from './form-field-error'
 import { FormFieldHelp } from './form-field-help'
 import { FormFieldLabel } from './form-field-label'
 
-export default {
+const meta: Meta<typeof FormField> = {
   title: 'tds-ui / FormField',
   component: FormField,
-} as Meta<typeof FormField>
+  parameters: {
+    docs: {
+      description: {
+        component: 'Form을 구성할 때 사용되는 뷰 컴포넌트입니다.',
+      },
+    },
+  },
+}
+
+export default meta
 
 const CustomInput = () => {
   const {
@@ -26,6 +35,7 @@ const CustomInput = () => {
   return (
     <input
       id={inputId}
+      value="FormField 테스트"
       disabled={isDisabled}
       required={isRequired}
       aria-invalid={isError}
@@ -37,32 +47,60 @@ const CustomInput = () => {
   )
 }
 
-export const Default = () => {
-  return (
-    <FormField>
-      <FormFieldLabel>Label</FormFieldLabel>
-      <CustomInput />
-      <FormFieldHelp>Helper text.</FormFieldHelp>
-    </FormField>
-  )
+type Story = StoryObj<typeof FormField>
+
+export const Default: Story = {
+  name: '기본',
+  render: (args) => {
+    return (
+      <FormField {...args}>
+        <FormFieldLabel>Label</FormFieldLabel>
+        <CustomInput />
+      </FormField>
+    )
+  },
 }
 
-export const Required = () => {
-  return (
-    <FormField isRequired>
-      <FormFieldLabel>Label</FormFieldLabel>
-      <CustomInput />
-      <FormFieldHelp>Helper text.</FormFieldHelp>
-    </FormField>
-  )
+export const Required: Story = {
+  name: '필수',
+  args: {
+    isRequired: true,
+  },
+  render: (args) => {
+    return (
+      <FormField {...args}>
+        <FormFieldLabel>Label</FormFieldLabel>
+        <CustomInput />
+      </FormField>
+    )
+  },
 }
 
-export const Error = () => {
-  return (
-    <FormField isError>
-      <FormFieldLabel>Label</FormFieldLabel>
-      <CustomInput />
-      <FormFieldError>Error message.</FormFieldError>
-    </FormField>
-  )
+export const WithHelpMessage: Story = {
+  name: '가이드 메시지',
+  render: (args) => {
+    return (
+      <FormField {...args}>
+        <FormFieldLabel>Label</FormFieldLabel>
+        <CustomInput />
+        <FormFieldHelp>Helper text.</FormFieldHelp>
+      </FormField>
+    )
+  },
+}
+
+export const WithErrorMessage: Story = {
+  name: '에러 메시지',
+  args: {
+    isError: true,
+  },
+  render: (args) => {
+    return (
+      <FormField {...args}>
+        <FormFieldLabel>Label</FormFieldLabel>
+        <CustomInput />
+        <FormFieldError>Helper text.</FormFieldError>
+      </FormField>
+    )
+  },
 }
