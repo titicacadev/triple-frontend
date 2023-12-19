f5181d52a62bacb54770bd0e62800629ada8a3be
zhsks528 <hsm950825@naver.com>
2023-12-19 10:30:02 +0900

    RadioGroup storybook 보완

--- packages/tds-ui/src/components/radio-group/radio-group.stories.tsx
+++ packages/tds-ui/src/components/radio-group/radio-group.stories.tsx
@@ -1,74 +1,167 @@
-import type { Meta, StoryFn } from '@storybook/react'
-import { useState } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { useArgs } from '@storybook/preview-api'
 
 import { Radio } from '../radio/radio'
 
 import { RadioGroup } from './radio-group'
 
-export default {
+const meta: Meta<typeof RadioGroup> = {
   title: 'tds-ui / RadioGroup',
   component: RadioGroup,
-} as Meta<typeof RadioGroup>
-
-export const Default: StoryFn<typeof RadioGroup> = () => {
-  return (
-    <RadioGroup name="option" label="Label" help="Help message">
-      <Radio value="a">Option A</Radio>
-      <Radio value="b">Option B</Radio>
-      <Radio value="c">Option C</Radio>
-    </RadioGroup>
-  )
+  parameters: {
+    docs: {
+      description: {
+        component: '개별 라디오를 묶어줄 때 사용되는 뷰 컴포넌트입니다.',
+      },
+    },
+  },
 }
 
-export const Disabled: StoryFn<typeof RadioGroup> = () => {
-  return (
-    <RadioGroup name="option" label="Label" help="Help message" disabled>
-      <Radio value="a">Option A</Radio>
-      <Radio value="b">Option B</Radio>
-      <Radio value="c">Option C</Radio>
-    </RadioGroup>
-  )
+export default meta
+
+type Story = StoryObj<typeof RadioGroup>
+
+export const Default: Story = {
+  name: '기본',
+  args: {
+    name: 'option',
+    value: 'a',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return (
+      <RadioGroup {...args} value={value} onChange={handleChange}>
+        <Radio value="a">Option A</Radio>
+        <Radio value="b">Option B</Radio>
+        <Radio value="c">Option C</Radio>
+      </RadioGroup>
+    )
+  },
 }
 
-export const Required: StoryFn<typeof RadioGroup> = () => {
-  return (
-    <RadioGroup name="option" label="Label" help="Help message" required>
-      <Radio value="a">Option A</Radio>
-      <Radio value="b">Option B</Radio>
-      <Radio value="c">Option C</Radio>
-    </RadioGroup>
-  )
+export const Disabled: Story = {
+  name: '비활성화',
+  args: {
+    name: 'option',
+    value: 'a',
+    disabled: true,
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return (
+      <RadioGroup {...args} value={value} onChange={handleChange}>
+        <Radio value="a">Option A</Radio>
+        <Radio value="b">Option B</Radio>
+        <Radio value="c">Option C</Radio>
+      </RadioGroup>
+    )
+  },
 }
 
-export const Error: StoryFn<typeof RadioGroup> = () => {
-  return (
-    <RadioGroup
-      name="option"
-      label="Label"
-      help="Help message"
-      error="Error message"
-    >
-      <Radio value="a">Option A</Radio>
-      <Radio value="b">Option B</Radio>
-      <Radio value="c">Option C</Radio>
-    </RadioGroup>
-  )
+export const Required: Story = {
+  name: '필수',
+  args: {
+    name: 'option',
+    value: 'a',
+    required: true,
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return (
+      <RadioGroup {...args} value={value} onChange={handleChange}>
+        <Radio value="a">Option A</Radio>
+        <Radio value="b">Option B</Radio>
+        <Radio value="c">Option C</Radio>
+      </RadioGroup>
+    )
+  },
 }
 
-export const Controlled: StoryFn<typeof RadioGroup> = () => {
-  const [value, setValue] = useState<string>()
+export const WithLabel: Story = {
+  name: '스티커',
+  args: {
+    name: 'option',
+    value: 'a',
+    label: '라디오',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
 
-  return (
-    <RadioGroup
-      name="option"
-      label="Label"
-      help="Help message"
-      value={value}
-      onChange={(selectedValue) => setValue(selectedValue)}
-    >
-      <Radio value="a">Option A</Radio>
-      <Radio value="b">Option B</Radio>
-      <Radio value="c">Option C</Radio>
-    </RadioGroup>
-  )
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return (
+      <RadioGroup {...args} value={value} onChange={handleChange}>
+        <Radio value="a">Option A</Radio>
+        <Radio value="b">Option B</Radio>
+        <Radio value="c">Option C</Radio>
+      </RadioGroup>
+    )
+  },
+}
+
+export const WithHelpMessage: Story = {
+  name: '가이드 메시지',
+  args: {
+    name: 'option',
+    value: 'a',
+    label: '라디오',
+    help: 'Help text',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return (
+      <RadioGroup {...args} value={value} onChange={handleChange}>
+        <Radio value="a">Option A</Radio>
+        <Radio value="b">Option B</Radio>
+        <Radio value="c">Option C</Radio>
+      </RadioGroup>
+    )
+  },
+}
+
+export const WithErrorMessage: Story = {
+  name: '에러 메시지',
+  args: {
+    name: 'option',
+    value: 'a',
+    label: '라디오',
+    error: 'Error text',
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs()
+
+    const handleChange = (value: string) => {
+      updateArgs({ value })
+    }
+
+    return (
+      <RadioGroup {...args} value={value} onChange={handleChange}>
+        <Radio value="a">Option A</Radio>
+        <Radio value="b">Option B</Radio>
+        <Radio value="c">Option C</Radio>
+      </RadioGroup>
+    )
+  },
 }
