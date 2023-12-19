26d40906a1fb925b09f1f8a54dd4f67586cc116c
zhsks528 <hsm950825@naver.com>
2023-12-19 11:32:17 +0900

    StickyHeader storybook 보완

--- packages/tds-ui/src/components/sticky-header/sticky-header.stories.tsx
+++ packages/tds-ui/src/components/sticky-header/sticky-header.stories.tsx
@@ -2,12 +2,22 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { StickyHeader } from './sticky-header'
 
-export default {
+const meta: Meta<typeof StickyHeader> = {
   title: 'tds-ui / StickyHeader',
   component: StickyHeader,
-} as Meta
+  parameters: {
+    docs: {
+      description: {
+        component: '최상단에 `sticky`하게 고정시켜주는 뷰 컴포넌트입니다.',
+      },
+    },
+  },
+}
 
-export const Basic: StoryObj = {
+export default meta
+
+export const Default: StoryObj<typeof StickyHeader> = {
+  name: '기본',
   render: (args) => {
     return <StickyHeader {...args}>Basic StickyHeader</StickyHeader>
   },
