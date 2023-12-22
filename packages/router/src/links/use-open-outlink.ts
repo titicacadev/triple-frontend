1c82649a73d9094b847a56cbcd66805546bb0428
Heeryong Kang <drakang4@gmail.com>
2023-12-22 14:58:15 +0900

    refactor: useNavigate 마이그레이션

--- packages/router/src/links/use-open-outlink.ts
+++ packages/router/src/links/use-open-outlink.ts
@@ -2,7 +2,7 @@ import { useClientApp, useEnv } from '@titicaca/triple-web'
 
 import { OutlinkParams, makeOutlink } from './make-outlink'
 
-export type UseOpenOutlinkOptions = Omit<OutlinkParams, 'url'>
+export type OpenOutlinkOptions = Omit<OutlinkParams, 'url'>
 
 export function useOpenOutlink() {
   const clientApp = useClientApp()
@@ -13,7 +13,7 @@ export function useOpenOutlink() {
      * Outlink로 만들 absolute URL.
      */
     url: string,
-    options?: UseOpenOutlinkOptions,
+    options?: OpenOutlinkOptions,
   ) => {
     const href = clientApp
       ? makeOutlink(appUrlScheme, { url, ...options })
