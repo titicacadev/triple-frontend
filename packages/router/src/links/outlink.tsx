f76117eb6c0303f53bd60564c4af159b6bd7c8ff
Heeryong Kang <drakang4@gmail.com>
2023-12-22 14:39:05 +0900

    feat(router): 새로운 open link 훅 추가

--- packages/router/src/links/outlink.tsx
+++ packages/router/src/links/outlink.tsx
@@ -1,31 +1,43 @@
 import { AnchorHTMLAttributes, PropsWithChildren } from 'react'
 import { useClientApp, useEnv } from '@titicaca/triple-web'
-import { generateUrl } from '@titicaca/view-utilities'
-import qs from 'qs'
+
+import { OutlinkParams, makeOutlink } from './make-outlink'
 
 export interface OutlinkProps
   extends PropsWithChildren,
+    Omit<OutlinkParams, 'target'>,
     Exclude<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> {
   url: string
-  title?: string
+  /**
+   * 인 앱 웹뷰일 때, 외부 브라우저로 열립니다.
+   */
+  external?: boolean
 }
 
-export const Outlink = ({ children, url, title, ...props }: OutlinkProps) => {
+/**
+ * 인 앱 웹뷰에서 누르면 `/outlink`를 사용해서 외부 브라우저로 열립니다.
+ * 인 앱 웹뷰가 아니라면 평범한 `<a>` 태그로 작동합니다.
+ */
+export const Outlink = ({
+  children,
+  url,
+  external,
+  title,
+  ...props
+}: OutlinkProps) => {
+  const clientApp = useClientApp()
   const { appUrlScheme } = useEnv()
-  const app = useClientApp()
 
-  const href = app
-    ? generateUrl({
-        scheme: appUrlScheme,
-        path: '/outlink',
-        query: qs.stringify({
-          url,
-          title,
-        }),
+  const href = clientApp
+    ? makeOutlink(appUrlScheme, {
+        url,
+        target: external ? 'browser' : undefined,
+        title,
       })
     : url
 
   return (
+    // url prop이 항상 absolute이므로 local URL일 수가 없기 때문에 a 태그를 사용합니다.
     <a {...props} href={href}>
       {children}
     </a>
