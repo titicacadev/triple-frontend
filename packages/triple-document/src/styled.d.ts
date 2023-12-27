270c993bc8e451390f5446a4867cb852f0545756
Heeryong Kang <drakang4@gmail.com>
2023-12-27 11:26:59 +0900

    chore: fix styled.d.ts

--- packages/triple-document/src/styled.d.ts
+++ packages/triple-document/src/styled.d.ts
@@ -1,1 +1,7 @@
+import 'styled-components'
 import {} from 'styled-components/cssprop'
+import type { Theme } from '@titicaca/tds-theme'
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {}
+}
