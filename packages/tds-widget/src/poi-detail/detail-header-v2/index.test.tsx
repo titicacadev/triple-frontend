2df9662ced5b2f85062a829925c9a48b92074fc9
zhsks528 <hsm950825@naver.com>
2024-01-02 15:52:46 +0900

    DetailHeader test 코드 수정

--- packages/tds-widget/src/poi-detail/detail-header-v2/index.test.tsx
+++ packages/tds-widget/src/poi-detail/detail-header-v2/index.test.tsx
@@ -1,12 +1,23 @@
+import { defaultTheme } from '@titicaca/tds-theme'
+import { ThemeProvider } from 'styled-components'
 import { FC, MouseEventHandler, PropsWithChildren, ReactNode } from 'react'
 import '@titicaca/tds-ui'
 import { render, screen } from '@testing-library/react'
-import {
-  useTrackEvent,
-  useHashRouter,
-  TestWrapper,
-  ClientAppName,
-} from '@titicaca/triple-web'
+import { TestWrapper, ClientAppName } from '@titicaca/triple-web'
+
+import DetailHeaderV2 from './index'
+
+const addUriHashMockFn = jest.fn()
+const removeUriHashMockFn = jest.fn()
+
+jest.mock('@titicaca/triple-web', () => ({
+  ...jest.requireActual('@titicaca/triple-web'),
+  useTrackEvent: jest.fn().mockImplementation(() => jest.fn()),
+  useHashRouter: jest.fn().mockImplementation(() => ({
+    addUriHash: addUriHashMockFn,
+    removeUriHash: removeUriHashMockFn,
+  })),
+}))
 
 jest.mock('@titicaca/triple-web')
 jest.mock('@titicaca/tds-ui', () => ({
@@ -29,46 +40,38 @@ jest.mock('@titicaca/tds-ui', () => ({
     },
 }))
 
-/* eslint-disable-next-line */
-import DetailHeaderV2 from '.'
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 describe('when user is on app', () => {
   it('attaches long-click handler to the outermost section', () => {
-    const mockTrackEvent = jest.fn()
-    const mockPush = jest.fn()
-
-    ;(
-      useTrackEvent as unknown as jest.MockedFunction<
-        () => ReturnType<typeof useTrackEvent>
-      >
-    ).mockImplementation(() => mockTrackEvent)
-    ;(
-      useHashRouter as unknown as jest.MockedFunction<
-        () => Pick<
-          ReturnType<typeof useHashRouter>,
-          'addUriHash' | 'removeUriHash'
-        >
-      >
-    ).mockImplementation(() => ({
-      addUriHash: mockPush,
-      removeUriHash: jest.fn(),
-    }))
-
     render(
-      <DetailHeaderV2
-        names={{ ko: 'test', en: 'test', local: 'test' }}
-        areaName="테스트 지역"
-        scrapsCount={1}
-        reviewsCount={0}
-        reviewsRating={0}
-        onReviewsRatingClick={jest.fn()}
-        onCopy={jest.fn()}
-      />,
+      <ThemeProvider theme={defaultTheme}>
+        <DetailHeaderV2
+          names={{ ko: 'test', en: 'test', local: 'test' }}
+          areaName="테스트 지역"
+          scrapsCount={1}
+          reviewsCount={0}
+          reviewsRating={0}
+          onReviewsRatingClick={jest.fn()}
+          onCopy={jest.fn()}
+        />
+      </ThemeProvider>,
       {
         wrapper: TestWrapper({
           clientAppProvider: {
             device: { autoplay: 'always', networkType: 'unknown' },
-            metadata: { name: ClientAppName.Android, version: '1.0.0' },
+            metadata: { name: ClientAppName.iOS, version: '6.5.0' },
+          },
+          userAgentProvider: {
+            ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+            browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+            engine: { name: 'WebKit', version: '605.1.15' },
+            os: { name: 'iOS', version: '13.3.1' },
+            device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+            cpu: { architecture: undefined },
+            isMobile: true,
           },
         }),
       },
@@ -76,51 +79,58 @@ describe('when user is on app', () => {
 
     screen.getByTestId('mock-clickable-section').click()
 
-    expect(mockPush).toHaveBeenCalled()
+    expect(addUriHashMockFn).toHaveBeenCalled()
   })
 })
 
 describe('when user is on web', () => {
   it('attaches long-click handler to the outermost section', () => {
-    const mockTrackEvent = jest.fn()
-    const mockPush = jest.fn()
-
-    ;(
-      useTrackEvent as unknown as jest.MockedFunction<
-        () => ReturnType<typeof useTrackEvent>
-      >
-    ).mockImplementation(() => mockTrackEvent)
-    ;(
-      useHashRouter as unknown as jest.MockedFunction<
-        () => Pick<
-          ReturnType<typeof useHashRouter>,
-          'addUriHash' | 'removeUriHash'
-        >
-      >
-    ).mockImplementation(() => ({
-      addUriHash: mockPush,
-      removeUriHash: jest.fn(),
-    }))
-
     render(
-      <DetailHeaderV2
-        names={{ ko: 'test', en: 'test', local: 'test' }}
-        areaName="테스트 지역"
-        scrapsCount={1}
-        reviewsCount={0}
-        reviewsRating={0}
-        onReviewsRatingClick={jest.fn()}
-        onCopy={jest.fn()}
-      />,
+      <ThemeProvider theme={defaultTheme}>
+        <DetailHeaderV2
+          names={{ ko: 'test', en: 'test', local: 'test' }}
+          areaName="테스트 지역"
+          scrapsCount={1}
+          reviewsCount={0}
+          reviewsRating={0}
+          onReviewsRatingClick={jest.fn()}
+          onCopy={jest.fn()}
+        />
+      </ThemeProvider>,
       {
         wrapper: TestWrapper({
           clientAppProvider: null,
+          userAgentProvider: {
+            ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            browser: {
+              name: 'Chrome',
+              version: '120.0.0.0',
+              major: '120',
+            },
+            cpu: {
+              architecture: 'arm64',
+            },
+            device: {
+              type: undefined,
+              model: 'Macintosh',
+              vendor: 'Apple',
+            },
+            engine: {
+              name: 'Blink',
+              version: '120.0.0.0',
+            },
+            os: {
+              name: 'macOS',
+              version: '13.4.0',
+            },
+            isMobile: true,
+          },
         }),
       },
     )
 
     screen.getByTestId('mock-clickable-section').click()
 
-    expect(mockPush).not.toHaveBeenCalled()
+    expect(addUriHashMockFn).not.toHaveBeenCalled()
   })
 })
--- packages/tds-widget/src/poi-detail/detail-header/index.test.tsx
+++ packages/tds-widget/src/poi-detail/detail-header/index.test.tsx
@@ -1,14 +1,23 @@
 import { FC, MouseEventHandler, PropsWithChildren, ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
 import '@titicaca/tds-ui'
 import { render, screen } from '@testing-library/react'
-import {
-  useTrackEvent,
-  useHashRouter,
-  TestWrapper,
-  ClientAppName,
-} from '@titicaca/triple-web'
+import { ClientAppName, TestWrapper } from '@titicaca/triple-web'
+import { defaultTheme } from '@titicaca/tds-theme'
 
-jest.mock('@titicaca/triple-web')
+import DetailHeader from './index'
+
+const addUriHashMockFn = jest.fn()
+const removeUriHashMockFn = jest.fn()
+
+jest.mock('@titicaca/triple-web', () => ({
+  ...jest.requireActual('@titicaca/triple-web'),
+  useTrackEvent: jest.fn().mockImplementation(() => jest.fn()),
+  useHashRouter: jest.fn().mockImplementation(() => ({
+    addUriHash: addUriHashMockFn,
+    removeUriHash: removeUriHashMockFn,
+  })),
+}))
 jest.mock('@titicaca/tds-ui', () => ({
   ...jest.requireActual('@titicaca/tds-ui'),
   longClickable: (Component: FC<{ children?: ReactNode; onClick: unknown }>) =>
@@ -29,46 +38,39 @@ jest.mock('@titicaca/tds-ui', () => ({
     },
 }))
 
-/* eslint-disable-next-line import/first */
-import DetailHeader from '.'
+beforeEach(() => {
+  jest.clearAllMocks()
+})
 
 describe('when user is on app', () => {
-  it('attaches long-click handler to the outermost section', () => {
-    const mockTrackEvent = jest.fn()
-    const mockPush = jest.fn()
-
-    ;(
-      useTrackEvent as unknown as jest.MockedFunction<
-        () => ReturnType<typeof useTrackEvent>
-      >
-    ).mockImplementation(() => mockTrackEvent)
-    ;(
-      useHashRouter as unknown as jest.MockedFunction<
-        () => Pick<
-          ReturnType<typeof useHashRouter>,
-          'addUriHash' | 'removeUriHash'
-        >
-      >
-    ).mockImplementation(() => ({
-      addUriHash: mockPush,
-      removeUriHash: jest.fn(),
-    }))
-
+  test('attaches long-click handler to the outermost section', () => {
     render(
-      <DetailHeader
-        names={{ ko: 'test', en: 'test', local: 'test' }}
-        areaName="테스트 지역"
-        scrapsCount={1}
-        reviewsCount={0}
-        reviewsRating={0}
-        onReviewsRatingClick={jest.fn()}
-        onCopy={jest.fn()}
-      />,
+      <ThemeProvider theme={defaultTheme}>
+        <DetailHeader
+          names={{ ko: 'test', en: 'test', local: 'test' }}
+          areaName="테스트 지역"
+          scrapsCount={1}
+          reviewsCount={0}
+          reviewsRating={0}
+          onReviewsRatingClick={jest.fn()}
+          onCopy={jest.fn()}
+        />
+        ,
+      </ThemeProvider>,
       {
         wrapper: TestWrapper({
           clientAppProvider: {
             device: { autoplay: 'always', networkType: 'unknown' },
-            metadata: { name: ClientAppName.Android, version: '1.0.0' },
+            metadata: { name: ClientAppName.iOS, version: '6.5.0' },
+          },
+          userAgentProvider: {
+            ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+            browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+            engine: { name: 'WebKit', version: '605.1.15' },
+            os: { name: 'iOS', version: '13.3.1' },
+            device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+            cpu: { architecture: undefined },
+            isMobile: true,
           },
         }),
       },
@@ -76,51 +78,58 @@ describe('when user is on app', () => {
 
     screen.getByTestId('mock-clickable-section').click()
 
-    expect(mockPush).toHaveBeenCalled()
+    expect(addUriHashMockFn).toHaveBeenCalled()
   })
 })
 
 describe('when user is on web', () => {
-  it('attaches long-click handler to the outermost section', () => {
-    const mockTrackEvent = jest.fn()
-    const mockPush = jest.fn()
-
-    ;(
-      useTrackEvent as unknown as jest.MockedFunction<
-        () => ReturnType<typeof useTrackEvent>
-      >
-    ).mockImplementation(() => mockTrackEvent)
-    ;(
-      useHashRouter as unknown as jest.MockedFunction<
-        () => Pick<
-          ReturnType<typeof useHashRouter>,
-          'addUriHash' | 'removeUriHash'
-        >
-      >
-    ).mockImplementation(() => ({
-      addUriHash: mockPush,
-      removeUriHash: jest.fn(),
-    }))
-
+  test('attaches long-click handler to the outermost section', () => {
     render(
-      <DetailHeader
-        names={{ ko: 'test', en: 'test', local: 'test' }}
-        areaName="테스트 지역"
-        scrapsCount={1}
-        reviewsCount={0}
-        reviewsRating={0}
-        onReviewsRatingClick={jest.fn()}
-        onCopy={jest.fn()}
-      />,
+      <ThemeProvider theme={defaultTheme}>
+        <DetailHeader
+          names={{ ko: 'test', en: 'test', local: 'test' }}
+          areaName="테스트 지역"
+          scrapsCount={1}
+          reviewsCount={0}
+          reviewsRating={0}
+          onReviewsRatingClick={jest.fn()}
+          onCopy={jest.fn()}
+        />
+      </ThemeProvider>,
       {
         wrapper: TestWrapper({
           clientAppProvider: null,
+          userAgentProvider: {
+            ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            browser: {
+              name: 'Chrome',
+              version: '120.0.0.0',
+              major: '120',
+            },
+            cpu: {
+              architecture: 'arm64',
+            },
+            device: {
+              type: undefined,
+              model: 'Macintosh',
+              vendor: 'Apple',
+            },
+            engine: {
+              name: 'Blink',
+              version: '120.0.0.0',
+            },
+            os: {
+              name: 'macOS',
+              version: '13.4.0',
+            },
+            isMobile: true,
+          },
         }),
       },
     )
 
     screen.getByTestId('mock-clickable-section').click()
 
-    expect(mockPush).not.toHaveBeenCalled()
+    expect(addUriHashMockFn).not.toHaveBeenCalled()
   })
 })
