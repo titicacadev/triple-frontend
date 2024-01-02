d281f7b907233298257c86a7de9cc4c60c17069a
zhsks528 <hsm950825@naver.com>
2024-01-02 10:33:49 +0900

    register test 코드 수정

--- packages/tds-widget/src/replies/register.test.tsx
+++ packages/tds-widget/src/replies/register.test.tsx
@@ -1,15 +1,20 @@
-import { fireEvent, render, screen } from '@testing-library/react'
-import { TripleWeb } from '@titicaca/triple-web'
 import { PropsWithChildren } from 'react'
+import i18n from 'i18next'
+import { fireEvent, render, screen } from '@testing-library/react'
+import {
+  ClientAppName,
+  TripleWeb,
+  EventTrackingProvider,
+} from '@titicaca/triple-web'
 
 import { RepliesProvider } from './context'
 import Register from './register'
 
+const mockedOnReplyAdd = jest.fn()
+const mockedOnReplyEdit = jest.fn()
+
 describe('Reply 등록 버튼을 테스트합니다.', () => {
   test('입력 창에 입력된 값이 없으면 버튼의 글자 색상은 blue500 입니다.', () => {
-    const mockedOnReplyAdd = jest.fn()
-    const mockedOnReplyEdit = jest.fn()
-
     render(
       <Register
         resourceId=""
@@ -29,9 +34,6 @@ describe('Reply 등록 버튼을 테스트합니다.', () => {
   })
 
   test('입력 창에 입력된 값이 있으면 버튼의 글자 색상은 blue 입니다.', async () => {
-    const mockedOnReplyAdd = jest.fn()
-    const mockedOnReplyEdit = jest.fn()
-
     render(
       <Register
         resourceId=""
@@ -55,13 +57,23 @@ describe('Reply 등록 버튼을 테스트합니다.', () => {
 function ReplyWithLoginWrapper({ children }: PropsWithChildren<unknown>) {
   return (
     <TripleWeb
+      clientAppProvider={{
+        metadata: {
+          name: ClientAppName.iOS,
+          version: '6.5.0',
+        },
+        device: {
+          autoplay: 'always',
+          networkType: 'wifi',
+        },
+      }}
       envProvider={{
-        appUrlScheme: '',
-        webUrlBase: '',
+        appUrlScheme: 'dev-soto',
+        webUrlBase: 'https://triple-dev.titicaca-corp.com',
         facebookAppId: '',
         defaultPageTitle: '',
         defaultPageDescription: '',
-        googleMapsApiKey: '',
+        googleMapsApiKey: 'AIzaSyDuSWU_yBwuQzeyRFcTqhyifqNX_8oaXI4',
         afOnelinkId: '',
         afOnelinkPid: '',
         afOnelinkSubdomain: '',
@@ -69,8 +81,23 @@ function ReplyWithLoginWrapper({ children }: PropsWithChildren<unknown>) {
       sessionProvider={{
         user: null,
       }}
+      i18nProvider={{
+        i18n,
+        lang: 'ko',
+      }}
+      userAgentProvider={{
+        ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+        browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+        engine: { name: 'WebKit', version: '605.1.15' },
+        os: { name: 'iOS', version: '13.3.1' },
+        device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+        cpu: { architecture: undefined },
+        isMobile: true,
+      }}
     >
-      <RepliesProvider>{children}</RepliesProvider>
+      <EventTrackingProvider page={{ label: 'test', path: '/test' }} utm={{}}>
+        <RepliesProvider>{children}</RepliesProvider>
+      </EventTrackingProvider>
     </TripleWeb>
   )
 }
