6f76130424c440d2947d29036d860cf76174e686
zhsks528 <hsm950825@naver.com>
2024-01-02 14:55:07 +0900

    tds-ui test 코드 수정

--- packages/tds-ui/src/components/popup/popup.test.tsx
+++ packages/tds-ui/src/components/popup/popup.test.tsx
@@ -1,7 +1,14 @@
+import { PropsWithChildren } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+import { defaultTheme } from '@titicaca/tds-theme'
 
 import { Popup } from './popup'
+
+function ThemeWrapper({ children }: PropsWithChildren<unknown>) {
+  return <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+}
 
 test('올바른 aria attributes를 가집니다.', () => {
   const onClose = jest.fn()
@@ -10,6 +17,7 @@ test('올바른 aria attributes를 가집니다.', () => {
     <Popup open onClose={onClose}>
       contents
     </Popup>,
+    { wrapper: ThemeWrapper },
   )
 
   const modal = screen.getByRole('dialog')
@@ -27,6 +35,7 @@ test('ESC 키를 누르면 닫습니다.', async () => {
     <Popup open onClose={onClose}>
       contents
     </Popup>,
+    { wrapper: ThemeWrapper },
   )
 
   await user.keyboard('{Escape}')
@@ -44,6 +53,7 @@ test('focus trap을 사용합니다.', async () => {
       <button>Button 1</button>
       <button>Button 2</button>
     </Popup>,
+    { wrapper: ThemeWrapper },
   )
 
   await waitFor(() => expect(screen.getByRole('dialog')).toHaveFocus())
