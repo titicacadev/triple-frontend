dd2cca957b653065a484fcca4d759c0fbb3a30a7
zhsks528 <hsm950825@naver.com>
2024-01-02 10:38:55 +0900

    reply test code 수정

--- packages/tds-widget/src/replies/reply.test.tsx
+++ packages/tds-widget/src/replies/reply.test.tsx
@@ -1,6 +1,9 @@
 import { PropsWithChildren } from 'react'
+import i18n from 'i18next'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import {
+  ClientAppName,
+  EventTrackingProvider,
   TripleWeb,
   useClientAppCallback,
   useSessionCallback,
@@ -233,19 +236,47 @@ function generateMockReply(reactions: Pick<ReplyType, 'reactions'>) {
 function ReplyWithLoginWrapper({ children }: PropsWithChildren<unknown>) {
   return (
     <TripleWeb
+      clientAppProvider={{
+        metadata: {
+          name: ClientAppName.iOS,
+          version: '6.5.0',
+        },
+        device: {
+          autoplay: 'always',
+          networkType: 'wifi',
+        },
+      }}
       envProvider={{
-        appUrlScheme: '',
-        webUrlBase: '',
+        appUrlScheme: 'dev-soto',
+        webUrlBase: 'https://triple-dev.titicaca-corp.com',
         facebookAppId: '',
         defaultPageTitle: '',
         defaultPageDescription: '',
-        googleMapsApiKey: '',
+        googleMapsApiKey: 'AIzaSyDuSWU_yBwuQzeyRFcTqhyifqNX_8oaXI4',
         afOnelinkId: '',
         afOnelinkPid: '',
         afOnelinkSubdomain: '',
       }}
+      sessionProvider={{
+        user: null,
+      }}
+      i18nProvider={{
+        i18n,
+        lang: 'ko',
+      }}
+      userAgentProvider={{
+        ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+        browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+        engine: { name: 'WebKit', version: '605.1.15' },
+        os: { name: 'iOS', version: '13.3.1' },
+        device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+        cpu: { architecture: undefined },
+        isMobile: true,
+      }}
     >
-      <RepliesProvider>{children}</RepliesProvider>
+      <EventTrackingProvider page={{ label: 'test', path: '/test' }} utm={{}}>
+        <RepliesProvider>{children}</RepliesProvider>
+      </EventTrackingProvider>
     </TripleWeb>
   )
 }
