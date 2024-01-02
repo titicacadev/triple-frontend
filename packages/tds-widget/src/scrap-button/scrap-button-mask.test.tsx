32cc8e74a0169390657e91d7107672deee48051d
zhsks528 <hsm950825@naver.com>
2024-01-02 10:54:25 +0900

    scarp button mask test 코드 수정

--- packages/tds-widget/src/scrap-button/scrap-button-mask.test.tsx
+++ packages/tds-widget/src/scrap-button/scrap-button-mask.test.tsx
@@ -1,8 +1,15 @@
+import { PropsWithChildren } from 'react'
 import { render, screen } from '@testing-library/react'
+import i18n from 'i18next'
+import {
+  ClientAppName,
+  EventTrackingProvider,
+  TripleWeb,
+} from '@titicaca/triple-web'
 
 import { ScrapButtonMask } from './scrap-button-mask'
-import { ComposedOutlineScrapButton as OutlineScrapButton } from './outline-scrap-button'
 import { ComposedOverlayScrapButton as OverlayScrapButton } from './overlay-scrap-button'
+import { ComposedOutlineScrapButton as OutlineScrapButton } from './outline-scrap-button'
 
 describe('ScrapButtonMask 컴포넌트', () => {
   it('should not render child scrap button.', () => {
@@ -25,6 +32,9 @@ describe('ScrapButtonMask 컴포넌트', () => {
           size={36}
         />
       </ScrapButtonMask>,
+      {
+        wrapper: TestWrapper,
+      },
     )
 
     expect(container).toBeEmptyDOMElement()
@@ -52,8 +62,59 @@ describe('ScrapButtonMask 컴포넌트', () => {
           data-testid="scrap-button-2"
         />
       </ScrapButtonMask>,
+      {
+        wrapper: TestWrapper,
+      },
     )
 
     expect(screen.getAllByRole('button')).toHaveLength(2)
   })
 })
+
+function TestWrapper({ children }: PropsWithChildren<unknown>) {
+  return (
+    <TripleWeb
+      clientAppProvider={{
+        metadata: {
+          name: ClientAppName.iOS,
+          version: '6.5.0',
+        },
+        device: {
+          autoplay: 'always',
+          networkType: 'wifi',
+        },
+      }}
+      envProvider={{
+        appUrlScheme: 'dev-soto',
+        webUrlBase: 'https://triple-dev.titicaca-corp.com',
+        facebookAppId: '',
+        defaultPageTitle: '',
+        defaultPageDescription: '',
+        googleMapsApiKey: 'AIzaSyDuSWU_yBwuQzeyRFcTqhyifqNX_8oaXI4',
+        afOnelinkId: '',
+        afOnelinkPid: '',
+        afOnelinkSubdomain: '',
+      }}
+      sessionProvider={{
+        user: null,
+      }}
+      i18nProvider={{
+        i18n,
+        lang: 'ko',
+      }}
+      userAgentProvider={{
+        ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+        browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+        engine: { name: 'WebKit', version: '605.1.15' },
+        os: { name: 'iOS', version: '13.3.1' },
+        device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+        cpu: { architecture: undefined },
+        isMobile: true,
+      }}
+    >
+      <EventTrackingProvider page={{ label: 'test', path: '/test' }} utm={{}}>
+        {children}
+      </EventTrackingProvider>
+    </TripleWeb>
+  )
+}
