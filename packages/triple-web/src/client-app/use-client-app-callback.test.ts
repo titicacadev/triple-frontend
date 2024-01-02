67048bb5a39317a568f8366ce6c433ee0a169350
zhsks528 <hsm950825@naver.com>
2024-01-02 17:21:43 +0900

    triple-web hook test 코드 수정

--- packages/triple-web/src/client-app/use-client-app-callback.test.ts
+++ packages/triple-web/src/client-app/use-client-app-callback.test.ts
@@ -1,43 +1,59 @@
 import { renderHook } from '@testing-library/react'
 
-import * as useTransitionModalModule from '../modal/use-transition-modal'
 import { TransitionType } from '../modal/constants'
+import { TestWrapper } from '../test-utils'
 
-import * as useClientAppModule from './use-client-app'
 import { useClientAppCallback } from './use-client-app-callback'
 import { ClientAppName } from './types'
+
+const mockFn = jest.fn()
+const mockShow = jest.fn()
 
 jest.mock('../modal/use-transition-modal', () => ({
   __esModule: true,
   ...jest.requireActual('../modal/use-transition-modal'),
+  useTransitionModal: jest.fn().mockImplementation(() => ({
+    show: mockShow,
+  })),
 }))
-jest.mock('./use-client-app', () => ({
-  __esModule: true,
-  ...jest.requireActual('./use-client-app'),
-}))
-
-const mockFn = jest.fn()
-const mockShow = jest.fn()
-const mockClose = jest.fn()
-const mockUseTransitionModal = jest.spyOn(
-  useTransitionModalModule,
-  'useTransitionModal',
-)
-const mockUseClientApp = jest.spyOn(useClientAppModule, 'useClientApp')
-
-beforeEach(() => {
-  mockUseTransitionModal.mockReturnValue({ show: mockShow, close: mockClose })
-})
 
 afterEach(() => {
   jest.clearAllMocks()
 })
 
 test('일반 브라우저에서 앱 전환 모달 표시 함수를 호출합니다.', () => {
-  mockUseClientApp.mockReturnValue(null)
-
-  const { result } = renderHook(() =>
-    useClientAppCallback(TransitionType.General, mockFn),
+  const { result } = renderHook(
+    () => useClientAppCallback(TransitionType.General, mockFn),
+    {
+      wrapper: TestWrapper({
+        clientAppProvider: null,
+        userAgentProvider: {
+          ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+          browser: {
+            name: 'Chrome',
+            version: '120.0.0.0',
+            major: '120',
+          },
+          cpu: {
+            architecture: 'arm64',
+          },
+          device: {
+            type: undefined,
+            model: 'Macintosh',
+            vendor: 'Apple',
+          },
+          engine: {
+            name: 'Blink',
+            version: '120.0.0.0',
+          },
+          os: {
+            name: 'macOS',
+            version: '13.4.0',
+          },
+          isMobile: true,
+        },
+      }),
+    },
   )
 
   result.current()
@@ -46,19 +62,25 @@ test('일반 브라우저에서 앱 전환 모달 표시 함수를 호출합니�
 })
 
 test('앱에서 앱 전환 모달 표시 함수를 호출하지 않습니다.', () => {
-  mockUseClientApp.mockReturnValue({
-    device: {
-      autoplay: 'always',
-      networkType: 'unknown',
+  const { result } = renderHook(
+    () => useClientAppCallback(TransitionType.General, mockFn),
+    {
+      wrapper: TestWrapper({
+        clientAppProvider: {
+          device: { autoplay: 'always', networkType: 'unknown' },
+          metadata: { name: ClientAppName.iOS, version: '6.5.0' },
+        },
+        userAgentProvider: {
+          ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+          browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+          engine: { name: 'WebKit', version: '605.1.15' },
+          os: { name: 'iOS', version: '13.3.1' },
+          device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+          cpu: { architecture: undefined },
+          isMobile: true,
+        },
+      }),
     },
-    metadata: {
-      name: ClientAppName.Android,
-      version: '1.0.0',
-    },
-  })
-
-  const { result } = renderHook(() =>
-    useClientAppCallback(TransitionType.General, mockFn),
   )
 
   result.current()
