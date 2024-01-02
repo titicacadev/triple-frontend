24934f8fc1b0f9e4c38a01e64135b8a9eda00eb7
zhsks528 <hsm950825@naver.com>
2024-01-02 15:54:25 +0900

    PublicHeader test 코드 수정

--- packages/tds-widget/src/public-header/public-header.spec.tsx
+++ packages/tds-widget/src/public-header/public-header.spec.tsx
@@ -3,12 +3,21 @@ import { ClientAppName, TestWrapper } from '@titicaca/triple-web'
 
 import { PublicHeader } from './public-header'
 
-it('renders nothing inside triple client', () => {
+test('renders nothing inside triple client', () => {
   const { container } = render(<PublicHeader />, {
     wrapper: TestWrapper({
       clientAppProvider: {
         device: { autoplay: 'always', networkType: 'unknown' },
-        metadata: { name: ClientAppName.Android, version: '1.0.0' },
+        metadata: { name: ClientAppName.iOS, version: '6.5.0' },
+      },
+      userAgentProvider: {
+        ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+        browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+        engine: { name: 'WebKit', version: '605.1.15' },
+        os: { name: 'iOS', version: '13.3.1' },
+        device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+        cpu: { architecture: undefined },
+        isMobile: true,
       },
     }),
   })
@@ -16,10 +25,35 @@ it('renders nothing inside triple client', () => {
   expect(container.childNodes).toHaveLength(0)
 })
 
-it('renders header outside triple client', () => {
+test('renders header outside triple client', () => {
   const { container } = render(<PublicHeader />, {
     wrapper: TestWrapper({
       clientAppProvider: null,
+      userAgentProvider: {
+        ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        browser: {
+          name: 'Chrome',
+          version: '120.0.0.0',
+          major: '120',
+        },
+        cpu: {
+          architecture: 'arm64',
+        },
+        device: {
+          type: undefined,
+          model: 'Macintosh',
+          vendor: 'Apple',
+        },
+        engine: {
+          name: 'Blink',
+          version: '120.0.0.0',
+        },
+        os: {
+          name: 'macOS',
+          version: '13.4.0',
+        },
+        isMobile: true,
+      },
     }),
   })
 
