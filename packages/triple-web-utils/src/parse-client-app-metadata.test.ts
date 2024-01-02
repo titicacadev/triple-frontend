d46590d1936d1a2a10c7448fe79b614245f6472a
zhsks528 <hsm950825@naver.com>
2024-01-02 17:44:44 +0900

    triple-web-utils test 코드 수정

--- packages/triple-web-utils/src/parse-client-app-metadata.test.ts
+++ packages/triple-web-utils/src/parse-client-app-metadata.test.ts
@@ -1,6 +1,6 @@
 import { parseClientAppMetadata } from './parse-client-app-metadata'
 
-it('should parse Chrome on Windows as a non-app', () => {
+test('should parse Chrome on Windows as a non-app', () => {
   expect(
     parseClientAppMetadata(
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
@@ -8,35 +8,35 @@ it('should parse Chrome on Windows as a non-app', () => {
   ).toBeNull()
 })
 
-it('should parse Triple Android debug client as an app', () => {
+test('should parse Triple Android debug client as an app', () => {
   expect(
     parseClientAppMetadata(
       'Mozilla/5.0 (Linux; Android 11; SM-G975N Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.86 Mobile Safari/537.36 Triple-Android/5.3.0-debug',
     ),
   ).toEqual({
-    appName: 'Triple-Android',
-    appVersion: '5.3.0-debug',
+    name: 'Triple-Android',
+    version: '5.3.0-debug',
   })
 })
 
-it('should parse Triple Android client as an app', () => {
+test('should parse Triple Android client as an app', () => {
   expect(
     parseClientAppMetadata(
       'Mozilla/5.0 (Linux; Android 10; SM-N960N Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/96.0.4664.92 Mobile Safari/537.36 Triple-Android/5.10.0',
     ),
   ).toEqual({
-    appName: 'Triple-Android',
-    appVersion: '5.10.0',
+    name: 'Triple-Android',
+    version: '5.10.0',
   })
 })
 
-it('should parse Triple iOS client as an app', () => {
+test('should parse Triple iOS client as an app', () => {
   expect(
     parseClientAppMetadata(
       'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/5.10.0',
     ),
   ).toEqual({
-    appName: 'Triple-iOS',
-    appVersion: '5.10.0',
+    name: 'Triple-iOS',
+    version: '5.10.0',
   })
 })
