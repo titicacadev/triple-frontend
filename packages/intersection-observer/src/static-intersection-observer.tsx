6335d40046046bf62a53ef6d84a6ae1a22ebd434
Heeryong Kang <drakang4@gmail.com>
2024-01-03 11:15:57 +0900

    feat(intersection-observer): polyfill, lazy load 제거한 static intersection observer 추가

--- packages/intersection-observer/src/static-intersection-observer.tsx
+++ packages/intersection-observer/src/static-intersection-observer.tsx
@@ -1,4 +1,40 @@
-import 'intersection-observer'
-import ReactIntersectionObserver from '@titicaca/react-intersection-observer'
+import { PropsWithChildren, useEffect, useRef } from 'react'
 
-export default ReactIntersectionObserver
+export interface StaticIntersectionObserverProps
+  extends PropsWithChildren,
+    IntersectionObserverInit {
+  onChange?: (entry: IntersectionObserverEntry, unobserve: () => void) => void
+}
+
+export function StaticIntersectionObserver({
+  children,
+  root,
+  rootMargin,
+  threshold,
+  onChange,
+}: StaticIntersectionObserverProps) {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const target = ref.current
+    if (target) {
+      const handleIntersection: IntersectionObserverCallback = (
+        entries,
+        observer,
+      ) => {
+        onChange?.(entries[0], () => observer.unobserve(target))
+      }
+
+      const observer = new IntersectionObserver(handleIntersection, {
+        root,
+        rootMargin,
+        threshold,
+      })
+
+      observer.observe(target)
+      return () => observer.disconnect()
+    }
+  }, [onChange, root, rootMargin, threshold])
+
+  return <div ref={ref}> {children}</div>
+}
