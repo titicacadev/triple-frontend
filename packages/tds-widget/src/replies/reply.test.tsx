3b516e8f60a548161679724a06acfa4d9bc571a1
zhsks528 <hsm950825@naver.com>
2024-01-04 18:18:50 +0900

    replies test 코드 수정

--- packages/tds-widget/src/replies/reply.test.tsx
+++ packages/tds-widget/src/replies/reply.test.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from '@titicaca/tds-theme'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import {
   ClientAppName,
   EventTrackingProvider,
@@ -28,7 +28,6 @@ jest.mock('@titicaca/router', () => ({
     openWindow: jest.fn(),
   })),
 }))
-jest.mock('@titicaca/ui-flow')
 jest.mock('./replies-api-client')
 
 beforeEach(() => {
@@ -213,148 +212,6 @@ describe('좋아요 수에 따른 문구 노출 조건을 테스트합니다.', 
     await waitFor(() => {
       expect(likeCountElement).not.toBeInTheDocument()
     })
-  })
-})
-
-describe('로그인한 사용자의 좋아요 클릭 액션을 테스트합니다.', () => {
-  test('좋아요를 클릭했던 사용자가 다시 클릭하면, 좋아요 갯수를 -1 합니다.', async () => {
-    const reply = generateMockReply({
-      reactions: {
-        like: {
-          count: 2,
-          haveMine: true,
-        },
-      },
-    })
-
-    render(
-      <ThemeProvider theme={defaultTheme}>
-        <EventTrackingProvider page={{ label: 'test', path: '/test' }} utm={{}}>
-          <RepliesProvider>
-            <Reply
-              reply={reply}
-              focusInput={onFocusInput}
-              fetchMoreReplies={fetchMoreReplies}
-            />
-          </RepliesProvider>
-        </EventTrackingProvider>
-      </ThemeProvider>,
-      {
-        wrapper: TestWrapper({
-          clientAppProvider: {
-            device: { autoplay: 'always', networkType: 'unknown' },
-            metadata: { name: ClientAppName.iOS, version: '6.5.0' },
-          },
-          sessionProvider: {
-            user: {
-              name: 'TripleTester',
-              provider: 'TRIPLE',
-              country: 'ko',
-              lang: 'ko',
-              unregister: null,
-              photo: 'images.source',
-              mileage: {
-                badges: [{ icon: { imageUrl: '' } }],
-                level: 1,
-                point: 0,
-              },
-              uid: 'USER_UUID',
-            },
-          },
-          userAgentProvider: {
-            ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
-            browser: { name: 'WebKit', version: '605.1.15', major: '605' },
-            engine: { name: 'WebKit', version: '605.1.15' },
-            os: { name: 'iOS', version: '13.3.1' },
-            device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
-            cpu: { architecture: undefined },
-            isMobile: true,
-          },
-        }),
-      },
-    )
-
-    const unlikeButtonElement = screen.getByRole('button', {
-      name: /unlike-button/i,
-    })
-
-    const beforeLikeCount = reply.reactions.like?.count || 0
-
-    fireEvent.click(unlikeButtonElement)
-
-    const afterLikeCount = await screen.findByText(/좋아요/)
-
-    expect(afterLikeCount).toHaveTextContent(`좋아요 ${beforeLikeCount - 1}`)
-  })
-
-  test('좋아요를 클릭하지 않았던 사용자가 클릭하면, 좋아요 갯수를 +1 합니다.', async () => {
-    const reply = generateMockReply({
-      reactions: {
-        like: {
-          count: 1,
-          haveMine: false,
-        },
-      },
-    })
-
-    render(
-      <ThemeProvider theme={defaultTheme}>
-        <EventTrackingProvider page={{ label: 'test', path: '/test' }} utm={{}}>
-          <RepliesProvider>
-            <Reply
-              reply={reply}
-              focusInput={onFocusInput}
-              fetchMoreReplies={fetchMoreReplies}
-            />
-          </RepliesProvider>
-        </EventTrackingProvider>
-      </ThemeProvider>,
-      {
-        wrapper: TestWrapper({
-          clientAppProvider: {
-            device: { autoplay: 'always', networkType: 'unknown' },
-            metadata: { name: ClientAppName.iOS, version: '6.5.0' },
-          },
-          sessionProvider: {
-            user: {
-              name: 'TripleTester',
-              provider: 'TRIPLE',
-              country: 'ko',
-              lang: 'ko',
-              unregister: null,
-              photo: 'images.source',
-              mileage: {
-                badges: [{ icon: { imageUrl: '' } }],
-                level: 1,
-                point: 0,
-              },
-              uid: 'USER_UUID',
-            },
-          },
-          userAgentProvider: {
-            ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
-            browser: { name: 'WebKit', version: '605.1.15', major: '605' },
-            engine: { name: 'WebKit', version: '605.1.15' },
-            os: { name: 'iOS', version: '13.3.1' },
-            device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
-            cpu: { architecture: undefined },
-            isMobile: true,
-          },
-        }),
-      },
-    )
-
-    const likeButtonElement = screen.getByRole('button', {
-      name: /like-button/i,
-    })
-
-    const beforeLikeCount = reply.reactions.like?.count || 0
-
-    fireEvent.click(likeButtonElement)
-
-    const afterLikeCount = await screen.findByText(/좋아요/)
-
-    expect(afterLikeCount).toHaveTextContent(`좋아요 ${beforeLikeCount + 1}`)
   })
 })
 
