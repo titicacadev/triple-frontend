f43b32d5cef1b4d9df6c4a5707568810fc38e180
zhsks528 <hsm950825@naver.com>
2024-01-04 18:34:33 +0900

    WIP useOpenNativeLink test 코드

--- packages/router/src/links/use-open-native-link.test.ts
+++ packages/router/src/links/use-open-native-link.test.ts
@@ -1,41 +1,60 @@
-import {
-  ClientAppName,
-  useClientApp,
-  useEnv,
-  useTransitionModal,
-} from '@titicaca/triple-web'
+import { ClientAppName, TestWrapper } from '@titicaca/triple-web'
 import { renderHook } from '@testing-library/react'
 
 import { useOpenNativeLink } from './use-open-native-link'
 
-jest.mock('@titicaca/triple-web')
+const transitionModalShowMockFn = jest.fn()
+
+jest.mock('@titicaca/triple-web', () => ({
+  ...jest.requireActual('@titicaca/triple-web'),
+  useEnv: jest.fn().mockReturnValue({
+    appUrlScheme: 'triple-test',
+    webUrlBase: 'https://triple.guide',
+  }),
+  useTransitionModal: jest
+    .fn()
+    .mockImplementation(() => ({ show: transitionModalShowMockFn })),
+}))
 
 beforeEach(() => {
   Object.defineProperty(window, 'location', {
     value: {
       href: '/',
+      hash: jest.fn(),
     },
     writable: true,
   })
-  ;(useEnv as jest.MockedFunction<typeof useEnv>).mockReturnValue({
-    appUrlScheme: 'triple-test',
-    afOnelinkId: '',
-    afOnelinkPid: '',
-    afOnelinkSubdomain: '',
-    defaultPageDescription: '',
-    defaultPageTitle: '',
-    facebookAppId: '',
-    webUrlBase: '',
-  })
+  // ;(useEnv as jest.MockedFunction<typeof useEnv>).mockReturnValue({
+  //   appUrlScheme: 'triple-test',
+  //   afOnelinkId: '',
+  //   afOnelinkPid: '',
+  //   afOnelinkSubdomain: '',
+  //   defaultPageDescription: '',
+  //   defaultPageTitle: '',
+  //   facebookAppId: '',
+  //   webUrlBase: '',
+  // })
 })
 
 test('인 앱 웹뷰에서는 딥링크로 이동합니다.', () => {
-  ;(useClientApp as jest.MockedFunction<typeof useClientApp>).mockReturnValue({
-    device: { autoplay: 'always', networkType: 'unknown' },
-    metadata: { name: ClientAppName.Android, version: '1.0.0' },
+  const { result } = renderHook(() => useOpenNativeLink(), {
+    wrapper: TestWrapper({
+      clientAppProvider: {
+        device: { autoplay: 'always', networkType: 'unknown' },
+        metadata: { name: ClientAppName.Android, version: '6.5.0' },
+      },
+      sessionProvider: { user: null },
+      userAgentProvider: {
+        ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+        browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+        engine: { name: 'WebKit', version: '605.1.15' },
+        os: { name: 'iOS', version: '13.3.1' },
+        device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+        cpu: { architecture: undefined },
+        isMobile: true,
+      },
+    }),
   })
-
-  const { result } = renderHook(() => useOpenNativeLink())
 
   result.current('/test-path')
 
@@ -44,21 +63,38 @@ test('인 앱 웹뷰에서는 딥링크로 이동합니다.', () => {
 })
 
 test('인 앱 웹뷰가 아니면 TransitionModal을 엽니다.', () => {
-  const mockShowTransitionModal = jest.fn()
-
-  ;(useClientApp as jest.MockedFunction<typeof useClientApp>).mockReturnValue(
-    null,
-  )
-  ;(
-    useTransitionModal as jest.MockedFunction<typeof useTransitionModal>
-  ).mockReturnValue({
-    show: mockShowTransitionModal,
-    close: jest.fn(),
+  const { result } = renderHook(() => useOpenNativeLink(), {
+    wrapper: TestWrapper({
+      clientAppProvider: null,
+      userAgentProvider: {
+        ua: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        browser: {
+          name: 'Chrome',
+          version: '120.0.0.0',
+          major: '120',
+        },
+        cpu: {
+          architecture: 'arm64',
+        },
+        device: {
+          type: undefined,
+          model: 'Macintosh',
+          vendor: 'Apple',
+        },
+        engine: {
+          name: 'Blink',
+          version: '120.0.0.0',
+        },
+        os: {
+          name: 'macOS',
+          version: '13.4.0',
+        },
+        isMobile: true,
+      },
+    }),
   })
-
-  const { result } = renderHook(() => useOpenNativeLink())
 
   result.current('/test-path')
 
-  expect(mockShowTransitionModal).toHaveBeenCalled()
+  expect(transitionModalShowMockFn).toHaveBeenCalled()
 })
