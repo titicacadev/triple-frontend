92e0b292915003f02cfdc1320d7708da3c4c9335
Heeryong Kang <drakang4@gmail.com>
2024-01-05 15:44:17 +0900

    fix: 무한 리렌더 현상 수정

--- packages/triple-web/src/i18n/context.tsx
+++ packages/triple-web/src/i18n/context.tsx
@@ -9,12 +9,12 @@ export const I18nContext = createContext<I18nValue | undefined>(undefined)
 export type I18nProviderProps = PropsWithChildren & I18nValue
 
 export function I18nProvider({ children, i18n, lang }: I18nProviderProps) {
-  if (typeof window === 'undefined' && i18n.resolvedLanguage !== lang) {
+  if (typeof window === 'undefined' && i18n.language !== lang) {
     i18n.changeLanguage(lang)
   }
 
   useEffect(() => {
-    if (i18n.resolvedLanguage !== lang) {
+    if (i18n.language !== lang) {
       i18n.changeLanguage(lang)
     }
   }, [i18n, lang])
