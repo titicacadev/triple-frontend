383d7f9f73c33c08c94f074a9e55f17d74f50763
guswl98 <hjee0901@gmail.com>
2024-01-12 11:42:52 +0900

    addReply API mocking

--- packages/replies/src/replies.stories.tsx
+++ packages/replies/src/replies.stories.tsx
@@ -1,10 +1,50 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { SessionContextProvider } from '@titicaca/react-contexts'
+import { rest } from 'msw'
 
 import Replies from './replies'
 
 export default {
   title: 'replies / Replies',
   component: Replies,
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post(
+          '/api/reply/messages?contentFormat=plaintext',
+          async (req, res, ctx) => {
+            const newReply = await req.json()
+            return res(
+              ctx.json({
+                id: 'new reply',
+                parentId: undefined,
+                blinded: false,
+                deleted: false,
+                isMine: true,
+                childrenCount: 0,
+                createdAt: new Date(2024, 1, 1).toString(),
+                updatedAt: new Date(2024, 1, 1).toString(),
+                reactions: {},
+                content: { text: newReply.content },
+                children: [],
+                writer: {
+                  href: '',
+                  name: '트리플',
+                  profileImage: '',
+                  badges: [],
+                },
+                actionSpecifications: {
+                  delete: false,
+                  reaction: false,
+                  report: false,
+                },
+              }),
+            )
+          },
+        ),
+      ],
+    },
+  },
   argTypes: {
     resourceId: {
       type: 'string',
@@ -37,6 +77,19 @@ export default {
       required: false,
     },
   },
+  decorators: [
+    (Story) => (
+      <SessionContextProvider
+        type="browser"
+        props={{
+          initialUser: undefined,
+          initialSessionAvailability: true,
+        }}
+      >
+        <Story />
+      </SessionContextProvider>
+    ),
+  ],
 } as Meta<typeof Replies>
 
 export const Basic: StoryObj<typeof Replies> = {
