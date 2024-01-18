6c2ff2d01ec5a64e78e22dbfefa1c55c6ec58c63
guswl98 <hjee0901@gmail.com>
2024-01-18 11:27:07 +0900

    text bubble 관련 style 주석 제거

--- packages/chat/src/bubble/text.tsx
+++ packages/chat/src/bubble/text.tsx
@@ -6,43 +6,6 @@ import { TextItem } from './item'
 import { Bubble } from './bubble'
 import { TextBubbleProp } from './type'
 
-/**
- * 
-const BACKGROUND_COLORS: {
-  [key in BackgroundColor]: string
-} = {
-  blue: '#d7e9ff',
-  gray: '#f5f5f5',
-  darkGray: '#F6F6F6',
-  mint: '#24CABD',
-}
-
-const TAIL_POSITION_STYLE_MAP: { [key: string]: ReturnType<typeof css> } = {
-  left: css`
-    &::before {
-      left: -10px;
-    }
-  `,
-  right: css`
-    right: 10px;
-
-    &::before {
-      right: -10px;
-    }
-  `,
-}
-
-const getBackgroundImage = (color: BackgroundColor) => {
-  return `https://assets.triple-dev.titicaca-corp.com/images/img-speechbubble-${color}@3x.png`
-}
-
-function getDefaultBackgroundColor(my: boolean) {
-  return my ? 'blue' : 'gray'
-}
-
- * 
- */
-
 export function TextBubble({ message, my, ...props }: TextBubbleProp) {
   const aTagNavigator = useATagNavigator()
 
