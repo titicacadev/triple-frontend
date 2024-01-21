dd919ffdf4aee015047a2eda03adb4c245016b47
Heeryong Kang <drakang4@gmail.com>
2024-01-21 21:57:39 +0900

    feat: 쓰기 편한 EventMetadataProvider, EventTrackingProvider export

--- packages/triple-web-nextjs-pages/src/index.ts
+++ packages/triple-web-nextjs-pages/src/index.ts
@@ -1,4 +1,6 @@
 export * from './auth-guard'
 export * from './build-triple-web-props'
+export * from './event-metadata-provider'
+export * from './event-tracking-provider'
 export * from './put-invalid-session-id-remover'
 export * from './triple-web'
