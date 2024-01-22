20dae9ffacc9666d2b8439ae9e65b91a1247550b
guswl98 <hjee0901@gmail.com>
2024-01-22 13:46:04 +0900

    ParentMessage 스토리북 추가

---
+++ packages/chat/src/bubble/parent/parent.stories.tsx
@@ -0,0 +1,79 @@
+import { StoryFn } from '@storybook/react'
+
+import { Bubble } from '../bubble'
+import { TextItem } from '../item'
+import { ScrollProvider } from '../../chat'
+
+import ParentMessage from './parent-message'
+import ParentMessageUI, { ParentMessageUIProp } from './parent-ui'
+
+const Template: StoryFn<ParentMessageUIProp> = (args) => (
+  <ScrollProvider>
+    <Bubble id="bubble" my={false}>
+      <ParentMessageUI {...args} />
+      <TextItem text="안녕하세요" />
+    </Bubble>
+  </ScrollProvider>
+)
+export default {
+  title: 'chat / BubbleWithParent',
+  component: ParentMessage,
+  render: Template,
+  argTypes: {
+    id: {
+      type: 'text',
+      required: true,
+    },
+    value: {
+      type: 'object',
+      required: true,
+    },
+    blinded: {
+      type: 'boolean',
+      required: true,
+    },
+  },
+}
+
+export const Image = {
+  args: {
+    id: 'parent-message',
+    type: 'image',
+    value: {
+      sender: { name: '트리플', unregistered: false },
+      images: [
+        {
+          cloudinaryBucket: 'triple-dev',
+          cloudinaryId: 'cloudinary',
+          id: 'image',
+          type: 'image',
+          width: 1125,
+          height: 2436,
+          sizes: {
+            full: {
+              url: 'https://res.cloudinary.com/triple-entry/image/upload/w_1024,h_1024,c_limit,f_auto/07f5ed9c-1102-4ec0-b07c-7b1b098311b2.jpg',
+            },
+            large: {
+              url: 'https://res.cloudinary.com/triple-entry/image/upload/w_1024,h_1024,c_limit,f_auto/07f5ed9c-1102-4ec0-b07c-7b1b098311b2.jpg',
+            },
+            smallSquare: {
+              url: 'https://res.cloudinary.com/triple-entry/image/upload/w_1024,h_1024,c_limit,f_auto/07f5ed9c-1102-4ec0-b07c-7b1b098311b2.jpg',
+            },
+          },
+        },
+      ],
+    },
+  },
+}
+
+export const Text = {
+  args: {
+    id: 'parent-message',
+    type: 'text',
+    value: {
+      sender: { name: '트리플', unregistered: false },
+      message: '반가워요',
+    },
+    blinded: false,
+  },
+}
