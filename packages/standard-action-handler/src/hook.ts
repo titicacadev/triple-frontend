269f5f0c6605e1b5b85601ca31b917b4c9fa0aa3
guswl98 <hjee0901@gmail.com>
2024-01-30 11:25:09 +0900

    hook에 showTransitionModal option 적용

--- packages/standard-action-handler/src/hook.ts
+++ packages/standard-action-handler/src/hook.ts
@@ -1,10 +1,12 @@
 import { useNavigate, useExternalRouter } from '@titicaca/router'
+import { useTransitionModal } from '@titicaca/modals'
 
 import { initialize } from './index'
 
 export function useStandardActionHandler() {
   const navigate = useNavigate()
   const routeExternally = useExternalRouter()
+  const { show } = useTransitionModal()
 
-  return initialize({ navigate, routeExternally })
+  return initialize({ navigate, routeExternally, showTransitionModal: show })
 }
