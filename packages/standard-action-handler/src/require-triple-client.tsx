a294e88f7e3f52c65f3c4820c8e7980a1a7bf62d
guswl98 <hjee0901@gmail.com>
2024-01-30 18:15:31 +0900

    requireTripleClient url에서 web-action이 실행 가능하도록 수정

--- packages/standard-action-handler/src/require-triple-client.tsx
+++ packages/standard-action-handler/src/require-triple-client.tsx
@@ -1,26 +1,31 @@
 import qs from 'qs'
 import { TransitionType } from '@titicaca/modals'
+import { parseUrl } from '@titicaca/view-utilities'
 
 import { WebActionParams } from './types'
 
 export default async function requireTripleClient({
   url: { path, query } = {},
   options: { navigate, app, showTransitionModal } = {},
+  handler,
 }: WebActionParams) {
-  if (path === '/web-action/require-triple-client') {
+  if (path === '/web-action/require-triple-client' && query) {
     if (!app) {
       if (showTransitionModal) {
         showTransitionModal(TransitionType.General)
       }
     } else {
-      if (!query) {
-        return true
-      }
       const { url } = qs.parse(query)
-      if (navigate && typeof url === 'string') {
-        navigate(url)
-        return true
+      if (typeof url !== 'string') {
+        return false
       }
+      const parsedUrl = parseUrl(url)
+      if (parsedUrl.path?.match(/^\/web-action\//) && handler) {
+        await handler.execute(url)
+      } else if (navigate) {
+        navigate(url)
+      }
+      return true
     }
   }
   return false
