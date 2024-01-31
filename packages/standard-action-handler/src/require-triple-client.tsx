c3c1fc6d23c7af7c79d05255a344e97dac90b3d7
guswl98 <hjee0901@gmail.com>
2024-01-31 13:23:11 +0900

    불필요한 if-else 제거

--- packages/standard-action-handler/src/require-triple-client.tsx
+++ packages/standard-action-handler/src/require-triple-client.tsx
@@ -1,12 +1,11 @@
 import qs from 'qs'
 import { TransitionType } from '@titicaca/modals'
-import { parseUrl } from '@titicaca/view-utilities'
 
 import { WebActionParams } from './types'
 
 export default async function requireTripleClient({
   url: { path, query } = {},
-  options: { navigate, app, showTransitionModal } = {},
+  options: { app, showTransitionModal } = {},
   handler,
 }: WebActionParams) {
   if (path === '/web-action/require-triple-client' && query) {
@@ -19,13 +18,10 @@ export default async function requireTripleClient({
       if (typeof url !== 'string') {
         return false
       }
-      const parsedUrl = parseUrl(url)
-      if (parsedUrl.path?.match(/^\/web-action\//) && handler) {
+      if (handler) {
         await handler.execute(url)
-      } else if (navigate) {
-        navigate(url)
+        return true
       }
-      return true
     }
   }
   return false
