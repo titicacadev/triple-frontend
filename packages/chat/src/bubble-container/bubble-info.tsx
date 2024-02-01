66669ecb6e312317b4012200c3072ebaa7ea6c48
Hajung Song <dongoc21hj@gmail.com>
2024-02-01 16:58:15 +0900

    bubbleInfo 영역에 답장하기 아이콘 추가

--- packages/chat/src/bubble-container/bubble-info.tsx
+++ packages/chat/src/bubble-container/bubble-info.tsx
@@ -3,6 +3,8 @@ import { Container, Text } from '@titicaca/core-elements'
 import { format, setDefaultOptions } from 'date-fns'
 import { ko } from 'date-fns/locale'
 
+import { ReplyMessageIcon } from '../icons/reply-meesage-icon'
+
 const BubbleInfoContainer = styled(Container)`
   vertical-align: bottom;
 `
@@ -12,22 +14,44 @@ const UnreadMessageCountText = styled.div`
   font-size: 10px;
 `
 
+const ReplyActionButton = styled.button<{
+  align: 'left' | 'right'
+}>`
+  display: flex;
+  align-items: flex-end;
+  justify-content: ${({ align }) => (align === 'right' ? 'flex-end' : 'auto')};
+  width: 100%;
+  height: 22px;
+  padding-bottom: 3px;
+  cursor: pointer;
+`
+
 setDefaultOptions({ locale: ko })
 
 export function BubbleInfo({
+  align,
   unreadCount,
   date,
   showTimeInfo = true,
   showDateInfo = false,
+  onReplyClick,
   ...props
 }: {
+  align: 'left' | 'right'
   unreadCount: number | null
   date: string
   showTimeInfo?: boolean
   showDateInfo?: boolean
+  onReplyClick?: () => void
 }) {
   return (
     <BubbleInfoContainer position="relative" display="inline-block" {...props}>
+      {onReplyClick ? (
+        <ReplyActionButton align={align} onClick={onReplyClick}>
+          <ReplyMessageIcon />
+        </ReplyActionButton>
+      ) : null}
+
       {unreadCount ? (
         <UnreadMessageCountText>{unreadCount}</UnreadMessageCountText>
       ) : null}
