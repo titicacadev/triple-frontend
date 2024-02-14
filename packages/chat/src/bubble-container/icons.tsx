08ae55e97fc651e36010a5b719145d3a273fd3c0
guswl98 <hjee0901@gmail.com>
2024-02-14 11:03:13 +0900

    Retry, Delete 아이콘 추가

---
+++ packages/chat/src/bubble-container/icons.tsx
@@ -0,0 +1,52 @@
+export function RetryIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.1765 9.09523C10.4332 10.6599 8.83833 11.7417 6.99083 11.7417C4.43302 11.7417 2.3595 9.66818 2.3595 7.11037C2.3595 4.55256 4.43302 2.47905 6.99083 2.47905C8.96589 2.47905 10.6522 3.71537 11.3181 5.45633"
+        stroke="white"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+      />
+      <path
+        d="M9.73587 5.33219L11.7016 5.60706L11.9765 3.64133"
+        stroke="white"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export function DeleteIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.047 11L7 7.024L3 3"
+        stroke="white"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.953 11L7 7.024L11 3"
+        stroke="white"
+        strokeWidth="1.4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
