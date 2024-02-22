123a51f3d3e08d24e189ac2f2ac3c93d7271dbd0
dia-triple <133629020+dia-triple@users.noreply.github.com>
2024-02-22 18:17:27 +0900

    fix: middleware 타입 오류 수정

--- packages/triple-web-nextjs-pages/src/middlewares/old-ios-cookie.tsx
+++ packages/triple-web-nextjs-pages/src/middlewares/old-ios-cookie.tsx
@@ -1,39 +1,41 @@
 import { clientAppRegex } from '@titicaca/triple-web-utils'
-import { NextMiddleware, NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse, NextFetchEvent } from 'next/server'
 import satisfies from 'semver/functions/satisfies'
 
-export const oldIosCookiesMiddleware: NextMiddleware = (
-  request: NextRequest,
-) => {
-  const response = NextResponse.next()
+import { CustomMiddleware } from './chain'
 
-  const userAgent = request.headers.get('User-Agent')
+export function oldIosCookiesMiddleware(_: CustomMiddleware) {
+  return async function middleware(request: NextRequest, _: NextFetchEvent) {
+    const response = NextResponse.next()
 
-  if (!userAgent) {
+    const userAgent = request.headers.get('User-Agent')
+
+    if (!userAgent) {
+      return response
+    }
+
+    const metadata = clientAppRegex.exec(userAgent)
+
+    try {
+      const oldVersionRange = '< 6.5.5'
+      const isOldIosApp =
+        metadata &&
+        metadata[1] === 'Triple-iOS' &&
+        satisfies(metadata[2], oldVersionRange, {
+          includePrerelease: true,
+        })
+
+      if (isOldIosApp) {
+        const cookies = request.cookies.getAll()
+
+        cookies.forEach((cookie) => {
+          response.cookies.set(cookie.name, cookie.value)
+        })
+      }
+    } catch {
+      // semver 파싱 에러가 발생하면 ignore 합니다.
+    }
+
     return response
   }
-
-  const metadata = clientAppRegex.exec(userAgent)
-
-  try {
-    const oldVersionRange = '< 6.5.5'
-    const isOldIosApp =
-      metadata &&
-      metadata[1] === 'Triple-iOS' &&
-      satisfies(metadata[2], oldVersionRange, {
-        includePrerelease: true,
-      })
-
-    if (isOldIosApp) {
-      const cookies = request.cookies.getAll()
-
-      cookies.forEach((cookie) => {
-        response.cookies.set(cookie.name, cookie.value)
-      })
-    }
-  } catch {
-    // semver 파싱 에러가 발생하면 ignore 합니다.
-  }
-
-  return response
 }
