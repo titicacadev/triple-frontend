108483bb5d6ad136bb8592cb8cd7d3ccf882e322
hailey <hailey@interparktriple.com>
2024-03-06 10:36:55 +0900

    alteredBubble props 복구

--- packages/chat/src/bubble/altered.tsx
+++ packages/chat/src/bubble/altered.tsx
@@ -13,30 +13,11 @@ export default function AlteredBubble({
   ...props
 }: BlindedBubbleProp) {
   return (
-    <Bubble
-      my={my}
-      css={{ ...(textColor && { color: textColor }) }}
-      {...omitColorCssProp(props)}
-    >
+    <Bubble my={my} css={{ ...(textColor && { color: textColor }) }} {...props}>
       <FlexBox flex alignItems="center" gap="4px">
         <ExclamationMarkIcon color={textColor} />
         <span>{alternativeText ?? '관리자에 의해 삭제되었습니다'}</span>
       </FlexBox>
     </Bubble>
   )
-}
-
-function omitColorCssProp(
-  props: Omit<
-    BlindedBubbleProp,
-    'my' | 'parentMessage' | 'alternativeText' | 'textColor'
-  >,
-) {
-  const copiedProps = { ...props }
-  if (copiedProps.css) {
-    if (typeof copiedProps.css === 'object' && 'color' in copiedProps.css) {
-      delete copiedProps.css.color
-    }
-  }
-  return copiedProps
 }
