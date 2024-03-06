4e099c3c6a3cb95cce8d6157202177ad8808bfae
hailey <hailey@interparktriple.com>
2024-03-06 09:23:19 +0900

    note에도 markdownText를 지원하도록 수정

--- packages/triple-document/src/elements/note.tsx
+++ packages/triple-document/src/elements/note.tsx
@@ -1,18 +1,33 @@
 import { Segment, Text } from '@titicaca/core-elements'
+import { marked } from 'marked'
+import styled from 'styled-components'
+
+const Markdown = styled(Text)`
+  a {
+    text-decoration: underline;
+    color: var(--color-blue);
+  }
+`
 
 export default function Note({
-  value: { title, body },
+  value: { title, body, markdownText },
 }: {
-  value: { title: string; body: string }
+  value: { title: string; body?: string; markdownText?: string }
 }) {
+  const html = body || marked.parse(markdownText || '')
+
   return (
     <Segment css={{ margin: 30 }}>
       <Text bold size="small" color="gray" lineHeight={1.57}>
         {title}
       </Text>
-      <Text size="small" color="gray" alpha={0.8} lineHeight={1.57}>
-        {body}
-      </Text>
+      <Markdown
+        size="small"
+        color="gray"
+        alpha={0.8}
+        lineHeight={1.57}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
     </Segment>
   )
 }
