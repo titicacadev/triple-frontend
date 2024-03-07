7a2d86e2a9f55088330042f1bc670393ab9dc370
guswl98 <hjee0901@gmail.com>
2024-03-07 09:19:41 +0900

    styled span 적용

--- packages/chat/src/bubble/altered.tsx
+++ packages/chat/src/bubble/altered.tsx
@@ -1,9 +1,18 @@
 import { FlexBox } from '@titicaca/core-elements'
+import styled, { css } from 'styled-components'
 
 import ExclamationMarkIcon from '../icons/ExclamationMarkIcon'
 
 import Bubble from './bubble'
 import { BlindedBubbleProp } from './type'
+
+const AlteredText = styled.span<{ color?: string }>`
+  ${({ color }) =>
+    color &&
+    css`
+      color: ${color};
+    `}
+`
 
 export default function AlteredBubble({
   my,
@@ -14,13 +23,14 @@ export default function AlteredBubble({
 }: BlindedBubbleProp) {
   const alteredCSS = {
     margin: my ? '0 0 0 8px' : undefined,
-    ...(textColor && { color: textColor }),
   }
   return (
     <Bubble my={my} {...props} css={alteredCSS}>
       <FlexBox flex alignItems="center" gap="4px">
         <ExclamationMarkIcon color={textColor} />
-        <span>{alternativeText ?? '관리자에 의해 삭제되었습니다'}</span>
+        <AlteredText color={textColor}>
+          {alternativeText ?? '관리자에 의해 삭제되었습니다'}
+        </AlteredText>
       </FlexBox>
     </Bubble>
   )
