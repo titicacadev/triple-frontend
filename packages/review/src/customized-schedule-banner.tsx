de846022cda687ae18818d56e1442cd293af2d12
guswl98 <hjee0901@gmail.com>
2024-03-14 16:56:34 +0900

    맞춤 일정 배너가 노출될 때마다 이벤트 로깅

--- packages/review/src/customized-schedule-banner.tsx
+++ packages/review/src/customized-schedule-banner.tsx
@@ -1,20 +1,11 @@
 import { Container, FlexBox, Text } from '@titicaca/core-elements'
 import { useEventTrackingContext } from '@titicaca/react-contexts'
 import { useExternalRouter } from '@titicaca/router'
-import { useEffect } from 'react'
+import { StaticIntersectionObserver as IntersectionObserver } from '@titicaca/intersection-observer'
 
 export default function CustomizedScheduleBanner() {
   const routeExternally = useExternalRouter()
   const { trackEvent } = useEventTrackingContext()
-
-  useEffect(() => {
-    trackEvent({
-      ga: ['맞춤일정배너_노출'],
-      fa: {
-        action: '맞춤일정배너_노출',
-      },
-    })
-  }, [])
 
   function handleClick() {
     trackEvent({
@@ -33,30 +24,48 @@ export default function CustomizedScheduleBanner() {
   }
 
   return (
-    <Container
-      css={{
-        padding: '12.5px 16px 12.5px 20px',
-        backgroundColor: '#DBE9FF',
-        borderRadius: 6,
-        marginTop: 20,
-        marginBottom: 28,
-        cursor: 'pointer',
+    <IntersectionObserver
+      onChange={({ isIntersecting }) => {
+        if (isIntersecting) {
+          trackEvent({
+            ga: ['맞춤일정배너_노출'],
+            fa: {
+              action: '맞춤일정배너_노출',
+            },
+          })
+        }
       }}
-      onClick={() => handleClick()}
     >
-      <FlexBox flex justifyContent="space-between">
-        <FlexBox flex flexDirection="column" justifyContent="center" gap="2px">
-          <Text size={16} css={{ fontWeight: 700 }}>
-            일정 알아서 다 짜드려요
-          </Text>
-          <Text size={12}>트리플 맞춤 일정 추천 받으러 가기</Text>
+      <Container
+        css={{
+          padding: '12.5px 16px 12.5px 20px',
+          backgroundColor: '#DBE9FF',
+          borderRadius: 6,
+          marginTop: 20,
+          marginBottom: 28,
+          cursor: 'pointer',
+        }}
+        onClick={() => handleClick()}
+      >
+        <FlexBox flex justifyContent="space-between">
+          <FlexBox
+            flex
+            flexDirection="column"
+            justifyContent="center"
+            gap="2px"
+          >
+            <Text size={16} css={{ fontWeight: 700 }}>
+              일정 알아서 다 짜드려요
+            </Text>
+            <Text size={12}>트리플 맞춤 일정 추천 받으러 가기</Text>
+          </FlexBox>
+          <img
+            src="http://assets.triple-dev.titicaca-corp.com/images/img-map-with-pin.svg"
+            alt=""
+            width={52}
+          />
         </FlexBox>
-        <img
-          src="http://assets.triple-dev.titicaca-corp.com/images/img-map-with-pin.svg"
-          alt=""
-          width={52}
-        />
-      </FlexBox>
-    </Container>
+      </Container>
+    </IntersectionObserver>
   )
 }
