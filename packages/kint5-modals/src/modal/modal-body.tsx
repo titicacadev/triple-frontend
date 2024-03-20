71de716572bca05b497fa42778cabcba82e62971
jaehyeon48 <kjhyeon48@interparktriple.com>
2024-03-20 13:52:09 +0900

    ModalBody의 기본 패딩값을 '26px 16px'로 변경

--- packages/kint5-modals/src/modal/modal-body.tsx
+++ packages/kint5-modals/src/modal/modal-body.tsx
@@ -1,15 +1,9 @@
 import { PropsWithChildren } from 'react'
 import { Container } from '@titicaca/kint5-core-elements'
-import { css } from 'styled-components'
 
 export const ModalBody = ({ children, ...props }: PropsWithChildren) => {
   return (
-    <Container
-      css={css`
-        padding: 40px 30px;
-      `}
-      {...props}
-    >
+    <Container css={{ padding: '26px 16px' }} {...props}>
       {children}
     </Container>
   )
