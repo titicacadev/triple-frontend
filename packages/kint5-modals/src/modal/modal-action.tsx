fcf189fd5e2d3975d31a1cdeef9b3db4b3934f56
jaehyeon48 <kjhyeon48@interparktriple.com>
2024-03-20 14:01:39 +0900

    ModalAction 색상 수정

--- packages/kint5-modals/src/modal/modal-action.tsx
+++ packages/kint5-modals/src/modal/modal-action.tsx
@@ -1,12 +1,12 @@
-import { GlobalColors } from '@titicaca/kint5-core-elements'
 import styled from 'styled-components'
 
-const ACTION_COLORS: Partial<Record<GlobalColors, string>> = {
-  gray: 'rgba(58, 58, 58, 0.5)',
-  blue: '#368fff',
-}
+const ACTION_COLORS = {
+  black: '#000',
+  blue: '#1769FF',
+  red: '#FF322E',
+} as const
 
-export const ModalAction = styled.a<{ color?: GlobalColors }>`
+export const ModalAction = styled.a<{ color?: keyof typeof ACTION_COLORS }>`
   display: inline-block;
   white-space: nowrap;
   height: 50px;
@@ -14,6 +14,6 @@ export const ModalAction = styled.a<{ color?: GlobalColors }>`
   font-size: 14px;
   font-weight: bold;
   text-align: center;
-  color: ${({ color }) => ACTION_COLORS[color || 'gray']};
+  color: ${({ color }) => ACTION_COLORS[color || 'black']};
   cursor: pointer;
 `
