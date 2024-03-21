9ff2af4e30f7e64d0f9e008dc9a7fcb97be0e0d1
hailey <hailey@interparktriple.com>
2024-03-21 14:30:59 +0900

    [WATF-64] cta 오버레이 문구 수정

--- packages/poi-detail/src/image-carousel/cta-overlay.tsx
+++ packages/poi-detail/src/image-carousel/cta-overlay.tsx
@@ -1,12 +1,10 @@
 import styled from 'styled-components'
-import { useTranslation } from '@titicaca/next-i18next'
 
 const MoreImageOverlayLink = styled.a`
   display: block;
   width: 100%;
   text-align: center;
   color: white;
-  vertical-align: middle;
   top: 50%;
   position: absolute;
   transform: translateY(-50%);
@@ -21,12 +19,10 @@ const MoreImageOverlayLinkIcon = styled.img`
 `
 
 export default function CtaOverlay() {
-  const { t } = useTranslation('common-web')
-
   return (
     <MoreImageOverlayLink>
       <MoreImageOverlayLinkIcon src="https://assets.triple.guide/images/ico-arrow@4x.png" />
-      {t(['teuripeul-aebeseo-deobogi', '트리플 앱에서 더보기'])}
+      더 많은 이미지 보기
     </MoreImageOverlayLink>
   )
 }
