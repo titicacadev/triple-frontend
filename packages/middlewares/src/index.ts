7ac6d55e847cc5444d5244c955f0218c0fc55f3b
dia-triple <133629020+dia-triple@users.noreply.github.com>
2024-03-21 17:07:43 +0900

    test: oldIosCookiesMiddleware export 하지 않도록 주석 처리

--- packages/middlewares/src/index.ts
+++ packages/middlewares/src/index.ts
@@ -1,3 +1,3 @@
 export { chain } from './chain'
-export { oldIosCookiesMiddleware } from './old-ios-cookie'
+// export { oldIosCookiesMiddleware } from './old-ios-cookie'
 export { sessionCookieMiddleware } from './session-cookie'
