c536adb9b0d365f79f783fde42569a22b765bb24
guswl98 <hjee0901@gmail.com>
2024-03-21 14:24:12 +0900

    [WATF-67] 확대뷰 팝업 생성

--- packages/image-viewer/src/detail-viewer/index.tsx
+++ packages/image-viewer/src/detail-viewer/index.tsx
@@ -1,3 +1,26 @@
-export default function DetailViewer() {
-  return <div>확대뷰</div>
+import styled from 'styled-components'
+import { useState } from 'react'
+
+import { Container } from '../../../core-elements/src'
+import { useImagesContext } from '../../../react-contexts/src'
+
+const Image = styled.img``
+
+export interface DetailViewerProp {
+  imageIndex: number
+}
+
+export default function DetailViewer({
+  imageIndex: initialImageIndex,
+}: DetailViewerProp) {
+  const [imageIndex] = useState(initialImageIndex)
+  const { images } = useImagesContext()
+
+  const image = images[imageIndex]
+
+  return (
+    <Container css={{ width: '100%', height: '100%' }}>
+      <Image src={image.sizes.large.url} alt={image.id} />
+    </Container>
+  )
 }
