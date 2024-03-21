a085e74a4168566b43d573182455933cf8e71574
torres-triple <torres@interparktriple.com>
2024-03-21 13:21:11 +0900

    fix: meta-tags 불필요 주석 제거

--- packages/meta-tags/src/product-script.tsx
+++ packages/meta-tags/src/product-script.tsx
@@ -3,9 +3,6 @@ import Head from 'next/head'
 import { createScript } from './utils'
 import { ProductScriptProps } from './types'
 
-/**
- * Next13 app router 버전일 경우 '@titicaca/meta-tags/common'의 ProductScript를 사용하세요.
- */
 export function ProductScript(props: ProductScriptProps) {
   const productScript = createScript(props, 'Product')
 
