c0a6fd2cb2d601b7dc1c4d9d26b3e2a8d5f4e380
guswl98 <hjee0901@gmail.com>
2024-03-22 14:56:48 +0900

    [WATF-68] 터치로 이미지 좌우 이동

--- packages/image-viewer/src/detail-viewer/index.tsx
+++ packages/image-viewer/src/detail-viewer/index.tsx
@@ -1,6 +1,8 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Container } from '@titicaca/core-elements'
-import { useImagesContext } from '@titicaca/react-contexts'
+import { useImagesContext, useUserAgentContext } from '@titicaca/react-contexts'
+import Flicking from '@egjs/react-flicking'
+import { useRef } from 'react'
 
 const Image = styled.img`
   max-width: 100%;
@@ -20,29 +22,95 @@ const SourceUrl = styled.p`
   color: var(--color-gray700);
 `
 
+const Button = styled.button<{ direction: 'next' | 'prev' }>`
+  z-index: 9999;
+  position: absolute;
+  background-image: url('https://assets.triple.guide/images/ico-arrow-right-black@3x.png');
+  background-size: 30px;
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 44px;
+  height: 44px;
+  top: 50%;
+  border: 1px solid var(--color-gray100);
+  ${({ direction }) =>
+    direction === 'next'
+      ? css`
+          transform: translateY(-50%);
+          right: 20px;
+        `
+      : css`
+          transform: rotate(180deg) translateY(50%);
+          left: 20px;
+        `}
+  background-color: white;
+  border-radius: 50%;
+`
+
 const SOURCE_HEIGHT = 54
 
 export interface DetailViewerProp {
   imageIndex: number
+  changeImageIndex: (idx: number) => void
 }
 
-export default function DetailViewer({ imageIndex }: DetailViewerProp) {
-  const { images } = useImagesContext()
+export default function DetailViewer({
+  imageIndex,
+  changeImageIndex,
+}: DetailViewerProp) {
+  const { images, total } = useImagesContext()
+  const { isMobile } = useUserAgentContext()
+  const flickingRef = useRef<Flicking>(null)
 
-  const image = images[imageIndex]
+  function onNextImageShow() {
+    if (flickingRef.current) {
+      flickingRef.current.next()
+    }
+    changeImageIndex((imageIndex + 1) % total)
+  }
+
+  function onPrevImageShow() {
+    if (flickingRef.current) {
+      flickingRef.current.prev()
+    }
+    changeImageIndex(imageIndex === 0 ? total - 1 : imageIndex - 1)
+  }
 
   return (
     <Container css={{ position: 'relative', width: '100%', height: '100%' }}>
-      <Container
-        css={{
-          width: '100%',
-          height: `calc(100% - ${image.sourceUrl ? SOURCE_HEIGHT : 0}px)`,
-          display: 'flex',
+      <Flicking
+        ref={flickingRef}
+        css={{ position: 'relative', width: '100%', height: '100%' }}
+        defaultIndex={imageIndex}
+        onChange={(e) => {
+          changeImageIndex(e.index)
         }}
       >
-        <Image src={image.sizes.large.url} alt={image.id} />
-      </Container>
-      {image.sourceUrl ? <SourceUrl>{image.sourceUrl}</SourceUrl> : null}
+        {images.map((image) => (
+          <Container key={image.id} css={{ width: '100%', height: '100%' }}>
+            <Container
+              css={{
+                width: '100%',
+                height: `calc(100% - ${image.sourceUrl ? SOURCE_HEIGHT : 0}px)`,
+                display: 'flex',
+              }}
+            >
+              <Image src={image.sizes.large.url} alt={image.id} />
+            </Container>
+            {image.sourceUrl ? <SourceUrl>{image.sourceUrl}</SourceUrl> : null}
+          </Container>
+        ))}
+      </Flicking>
+      {!isMobile ? (
+        <>
+          {imageIndex > 0 ? (
+            <Button direction="prev" onClick={onPrevImageShow} />
+          ) : null}
+          {imageIndex < total - 1 ? (
+            <Button direction="next" onClick={onNextImageShow} />
+          ) : null}
+        </>
+      ) : null}
     </Container>
   )
 }
--- packages/image-viewer/src/image-viewer.tsx
+++ packages/image-viewer/src/image-viewer.tsx
@@ -1,8 +1,6 @@
 import { PropsWithChildren, useState } from 'react'
 import Popup from '@titicaca/popup'
 import { Container, Navbar } from '@titicaca/core-elements'
-import styled, { css } from 'styled-components'
-import { useImagesContext, useUserAgentContext } from '@titicaca/react-contexts'
 
 import DetailViewer from './detail-viewer'
 
@@ -26,46 +24,15 @@ export interface DetailViewerPopupProp {
 
 const NAVBAR_HEIGHT = 52
 
-const Button = styled.button<{ direction: 'next' | 'prev' }>`
-  position: absolute;
-  background-image: url('https://assets.triple.guide/images/ico-arrow-right-black@3x.png');
-  background-size: 30px;
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 44px;
-  height: 44px;
-  top: 50%;
-  border: 1px solid var(--color-gray100);
-  ${({ direction }) =>
-    direction === 'next'
-      ? css`
-          transform: translateY(-50%);
-          right: 20px;
-        `
-      : css`
-          transform: rotate(180deg) translateY(50%);
-          left: 20px;
-        `}
-  background-color: white;
-  border-radius: 50%;
-`
-
 export function DetailViewerPopup({
   open,
   onClose,
   imageIndex: initialImageIndex,
 }: DetailViewerPopupProp) {
-  const { isMobile } = useUserAgentContext()
-
-  const { total } = useImagesContext()
   const [imageIndex, setImageIndex] = useState(initialImageIndex)
 
-  function showNextImage() {
-    setImageIndex((imageIndex + 1) % total)
-  }
-
-  function showPrevImage() {
-    setImageIndex(imageIndex === 0 ? total - 1 : imageIndex - 1)
+  function changeImageIndex(idx: number) {
+    setImageIndex(idx)
   }
 
   return (
@@ -76,13 +43,10 @@ export function DetailViewerPopup({
       <Container
         css={{ height: `calc(100vh - ${NAVBAR_HEIGHT}px)`, width: '100vw' }}
       >
-        <DetailViewer imageIndex={imageIndex} />
-        {!isMobile ? (
-          <>
-            <Button direction="prev" onClick={showPrevImage} />
-            <Button direction="next" onClick={showNextImage} />
-          </>
-        ) : null}
+        <DetailViewer
+          imageIndex={imageIndex}
+          changeImageIndex={changeImageIndex}
+        />
       </Container>
     </ImageViewerPopup>
   )
