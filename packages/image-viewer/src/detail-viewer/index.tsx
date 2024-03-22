681fc6635f8f13223c7747755456fe1037aa5f07
guswl98 <hjee0901@gmail.com>
2024-03-22 11:52:46 +0900

    [WATF-67] 이미지 출처 추가

--- packages/image-viewer/src/detail-viewer/index.tsx
+++ packages/image-viewer/src/detail-viewer/index.tsx
@@ -9,6 +9,20 @@ const Image = styled.img`
   margin: auto;
 `
 
+const SourceUrl = styled.p`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  background-color: var(--color-white900);
+  padding: 20px;
+  font-size: 12px;
+  line-height: 12px;
+  height: 54px;
+  color: var(--color-gray700);
+`
+
+const SOURCE_HEIGHT = 54
+
 export interface DetailViewerProp {
   imageIndex: number
 }
@@ -22,8 +36,17 @@ export default function DetailViewer({
   const image = images[imageIndex]
 
   return (
-    <Container css={{ width: '100%', height: '100%', display: 'flex' }}>
-      <Image src={image.sizes.large.url} alt={image.id} />
+    <Container css={{ width: '100%', height: '100%' }}>
+      <Container
+        css={{
+          width: '100%',
+          height: `calc(100% - ${image.sourceUrl ? SOURCE_HEIGHT : 0}px)`,
+          display: 'flex',
+        }}
+      >
+        <Image src={image.sizes.large.url} alt={image.id} />
+      </Container>
+      {image.sourceUrl ? <SourceUrl>{image.sourceUrl}</SourceUrl> : null}
     </Container>
   )
 }
