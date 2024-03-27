95fbd94e56b178f84be58acf5c971c8205f6a274
hailey <hailey@interparktriple.com>
2024-03-27 15:24:13 +0900

    [WATF-89] 이미지 뷰어 내 ImagesContext 제거

--- packages/image-viewer/src/image-viewer.tsx
+++ packages/image-viewer/src/image-viewer.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react'
 import Popup from '@titicaca/popup'
 import { Container, Navbar } from '@titicaca/core-elements'
-import { useImagesContext } from '@titicaca/react-contexts'
 import styled from 'styled-components'
 
-import DetailViewer from './detail-viewer'
+import DetailViewer, { DetailViewerProp } from './detail-viewer'
 
 const NAVBAR_HEIGHT = 52
 
@@ -14,7 +13,8 @@ const Text = styled.span`
   line-height: 34px;
 `
 
-export interface ImageViewerPopupProps {
+export interface ImageViewerPopupProps
+  extends Pick<DetailViewerProp, 'images' | 'totalCount' | 'fetchNext'> {
   open: boolean
   onClose?: () => void
   defaultImageIndex: number | null
@@ -26,6 +26,9 @@ export interface ImageViewerPopupProps {
 export function ImageViewerPopup({
   open,
   onClose,
+  images,
+  totalCount,
+  fetchNext,
   defaultImageIndex,
 }: ImageViewerPopupProps) {
   const [imageIndex, setImageIndex] = useState<null | number>(defaultImageIndex)
@@ -41,22 +44,33 @@ export function ImageViewerPopup({
   return (
     <Popup open={open} onClose={handleClose} noNavbar>
       {imageIndex != null ? (
-        <DetailViewerContainer onClose={handleClose} imageIndex={imageIndex} />
+        <DetailViewerContainer
+          images={images}
+          totalCount={totalCount}
+          fetchNext={fetchNext}
+          onClose={handleClose}
+          imageIndex={imageIndex}
+        />
       ) : null}
     </Popup>
   )
 }
 
-export interface DetailViewerContainerProp {
+export interface DetailViewerContainerProp
+  extends Pick<
+    DetailViewerProp,
+    'images' | 'totalCount' | 'fetchNext' | 'imageIndex'
+  > {
   onClose?: () => void
-  imageIndex: number
 }
 
 export function DetailViewerContainer({
   onClose,
   imageIndex: initialImageIndex,
+  images,
+  totalCount,
+  fetchNext,
 }: DetailViewerContainerProp) {
-  const { total } = useImagesContext()
   const [imageIndex, setImageIndex] = useState(initialImageIndex)
 
   function changeImageIndex(idx: number) {
@@ -89,13 +103,18 @@ export function DetailViewerContainer({
           }}
         >
           <Text>{imageIndex + 1}</Text>
-          <Text css={{ color: 'var(--color-gray300)' }}>&nbsp;/ {total}</Text>
+          <Text css={{ color: 'var(--color-gray300)' }}>
+            &nbsp;/ {totalCount}
+          </Text>
         </Navbar.Item>
       </Navbar>
       <Container
         css={{ height: `calc(100vh - ${NAVBAR_HEIGHT}px)`, width: '100vw' }}
       >
         <DetailViewer
+          images={images}
+          totalCount={totalCount}
+          fetchNext={fetchNext}
           imageIndex={imageIndex}
           changeImageIndex={changeImageIndex}
         />
