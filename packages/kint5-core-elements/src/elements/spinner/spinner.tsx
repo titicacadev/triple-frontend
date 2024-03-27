cd634b0b41295777f4391fdcad31cbe1261e2411
jaehyeon48 <kjhyeon48@interparktriple.com>
2024-03-27 16:32:45 +0900

    스피너 기본 아이콘 변경

--- packages/kint5-core-elements/src/elements/spinner/spinner.tsx
+++ packages/kint5-core-elements/src/elements/spinner/spinner.tsx
@@ -1,14 +1,10 @@
 import { ReactNode } from 'react'
 import { TRIPLE_FALLBACK_ACTION_CLASS_NAME } from '@titicaca/triple-fallback-action'
-import styled, { css, keyframes } from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { layeringMixin, LayeringMixinProps } from '../../mixins'
 
-const loadingAnimation = keyframes`
-  100% {
-    background-position: -1740px;
-  }
-`
+import { Loading } from './loading'
 
 const Container = styled.div<{ full?: boolean } & LayeringMixinProps>`
   width: 100%;
@@ -33,15 +29,6 @@ const Wrapper = styled.div`
   text-align: center;
 `
 
-const Icon = styled.div`
-  margin: 0 auto;
-  width: 58px;
-  height: 58px;
-  background-image: url('https://assets.triple.guide/images/ico-spinner.png');
-  background-size: 1740px 58px;
-  animation: ${loadingAnimation} 1s steps(30) infinite;
-`
-
 export function Spinner({
   full,
   children,
@@ -56,7 +43,7 @@ export function Spinner({
   return (
     <Container full={full} zTier={zTier} zIndex={zIndex}>
       <Wrapper className={TRIPLE_FALLBACK_ACTION_CLASS_NAME}>
-        {disableDefaultIcon ? null : <Icon />}
+        {disableDefaultIcon ? null : <Loading />}
         {children}
       </Wrapper>
     </Container>
