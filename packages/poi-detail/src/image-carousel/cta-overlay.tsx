74c18b366f6acfc7ad06197712c6bdfc5d8dfc73
hailey <hailey@interparktriple.com>
2024-03-29 15:05:56 +0900

    [WATF-87] cta overlay 노출 이벤트 수정

--- packages/poi-detail/src/image-carousel/cta-overlay.tsx
+++ packages/poi-detail/src/image-carousel/cta-overlay.tsx
@@ -1,5 +1,3 @@
-import { StaticIntersectionObserver } from '@titicaca/intersection-observer'
-import { useEventTrackingContext } from '@titicaca/react-contexts'
 import styled from 'styled-components'
 
 const MoreImageOverlayLink = styled.a`
@@ -21,19 +19,10 @@ const MoreImageOverlayLinkIcon = styled.img`
 `
 
 export default function CtaOverlay() {
-  const { trackEvent } = useEventTrackingContext()
   return (
-    <StaticIntersectionObserver
-      onChange={({ isIntersecting }) => {
-        if (isIntersecting) {
-          trackEvent({ fa: { action: '대표사진_더보기_노출' } })
-        }
-      }}
-    >
-      <MoreImageOverlayLink>
-        더 많은 이미지 보기
-        <MoreImageOverlayLinkIcon src="https://assets.triple.guide/images/ico-arrow@4x.png" />
-      </MoreImageOverlayLink>
-    </StaticIntersectionObserver>
+    <MoreImageOverlayLink>
+      더 많은 이미지 보기
+      <MoreImageOverlayLinkIcon src="https://assets.triple.guide/images/ico-arrow@4x.png" />
+    </MoreImageOverlayLink>
   )
 }
