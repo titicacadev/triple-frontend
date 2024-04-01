92e327539487f7b788497e863f925be5f88585d4
Heeryong Kang <drakang4@gmail.com>
2024-04-01 15:58:17 +0900

    refactor(meta-tags): v14 구조로 마이그레이션

--- packages/meta-tags/src/essential-content-meta.tsx
+++ packages/meta-tags/src/essential-content-meta.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useEnv } from '@titicaca/react-contexts'
+import { useEnv } from '@titicaca/triple-web'
 
 export function EssentialContentMeta({
   title: titleFromProps,
