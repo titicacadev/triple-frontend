4679328023a7bb40284a43794ac9f803dd21f631
jaehyeon48 <kjhyeon48@interparktriple.com>
2024-04-08 22:00:47 +0900

    kint5 media popup 스토리북 수정

--- packages/kint5-media-popup/src/media-popup.stories.tsx
+++ packages/kint5-media-popup/src/media-popup.stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import type { Meta } from '@storybook/react'
 
 import {
@@ -16,36 +17,92 @@ export default {
 } as Meta
 
 export function OneImageExample() {
-  return <MediaPopup open media={ONE_IMAGE} />
+  const [index, setIndex] = useState(0)
+  return (
+    <MediaPopup
+      open
+      media={ONE_IMAGE}
+      currentMediumIndex={index}
+      onMediumChange={setIndex}
+    />
+  )
 }
 OneImageExample.storyName = '이미지 한 개'
 
 export function ThreeImagesExample() {
-  return <MediaPopup open media={THREE_IMAGES} />
+  const [index, setIndex] = useState(0)
+  return (
+    <MediaPopup
+      open
+      media={THREE_IMAGES}
+      currentMediumIndex={index}
+      onMediumChange={setIndex}
+    />
+  )
 }
 ThreeImagesExample.storyName = '이미지 세 개'
 
 export function FiveImagesExample() {
-  return <MediaPopup open media={FIVE_IMAGES} />
+  const [index, setIndex] = useState(0)
+  return (
+    <MediaPopup
+      open
+      media={FIVE_IMAGES}
+      currentMediumIndex={index}
+      onMediumChange={setIndex}
+    />
+  )
 }
 FiveImagesExample.storyName = '이미지 다섯 개'
 
 export function SevenImagesExample() {
-  return <MediaPopup open media={SEVEN_IMAGES} initialMediaIndex={3} />
+  const [index, setIndex] = useState(3)
+  return (
+    <MediaPopup
+      open
+      media={SEVEN_IMAGES}
+      currentMediumIndex={index}
+      onMediumChange={setIndex}
+    />
+  )
 }
 SevenImagesExample.storyName = '이미지 일곱 개 / 초기 인덱스 3'
 
 export function OneVideoExample() {
-  return <MediaPopup open media={ONE_VIDEO} />
+  const [index, setIndex] = useState(0)
+  return (
+    <MediaPopup
+      open
+      media={ONE_VIDEO}
+      currentMediumIndex={index}
+      onMediumChange={setIndex}
+    />
+  )
 }
 OneVideoExample.storyName = '동영상 한 개'
 
 export function OneVideoAndOneImageExample() {
-  return <MediaPopup open media={ONE_VIDEO_ONE_IMAGE} />
+  const [index, setIndex] = useState(0)
+  return (
+    <MediaPopup
+      open
+      media={ONE_VIDEO_ONE_IMAGE}
+      currentMediumIndex={index}
+      onMediumChange={setIndex}
+    />
+  )
 }
 OneVideoAndOneImageExample.storyName = '이미지 한 개, 동영상 한 개'
 
 export function OneVideoAndFourImagesExample() {
-  return <MediaPopup open media={ONE_VIDEO_FOUR_IMAGES} />
+  const [index, setIndex] = useState(0)
+  return (
+    <MediaPopup
+      open
+      media={ONE_VIDEO_FOUR_IMAGES}
+      currentMediumIndex={index}
+      onMediumChange={setIndex}
+    />
+  )
 }
 OneVideoAndFourImagesExample.storyName = '이미지 네 개, 동영상 한 개'
