51fcf501f1e47ba7b7cb13b6fd645b853cac3ce3
jaehyeon48 <kjhyeon48@interparktriple.com>
2024-04-08 22:01:59 +0900

    kint5-review 패키지에 kint5-media-popup 의존성 추가

--- packages/kint5-review/src/components/review-element/media/index.tsx
+++ packages/kint5-review/src/components/review-element/media/index.tsx
@@ -2,12 +2,11 @@ import { FlexBox } from '@titicaca/kint5-core-elements'
 import {
   useEventTrackingContext,
   useHistoryFunctions,
+  useUriHash,
 } from '@titicaca/react-contexts'
 import { ImageMeta } from '@titicaca/type-definitions'
-import { useMemo } from 'react'
-
-import { HASH_EXTRA_INFO_SPLIT_STRING } from '../../constants'
-import { REVIEW_MEDIA_POPUP_HASH } from '../../review-media-popup'
+import { useMemo, useState } from 'react'
+import { MEDIA_POPUP_HASH, MediaPopup } from '@titicaca/kint5-media-popup'
 
 import { compareMedia } from './compare-media'
 import { MediumWrapper } from './elements'
@@ -29,6 +28,8 @@ function Media({
 }: Props) {
   const { push } = useHistoryFunctions()
   const { trackEvent } = useEventTrackingContext()
+  const uriHash = useUriHash()
+  const [currentMediaPopupIndex, setCurrentMediaPopupIndex] = useState(0)
 
   const hasVideo = media.some((medium) => medium.type === 'video')
 
@@ -46,57 +47,64 @@ function Media({
   }
 
   return (
-    <MediaWrapper length={length}>
-      {sortedMedia.slice(0, limit).map((medium, index) => {
-        const thumbnailType = medium.type === 'video' ? '비디오' : '사진'
+    <>
+      <MediaWrapper length={length}>
+        {sortedMedia.slice(0, limit).map((medium, index) => {
+          const thumbnailType = medium.type === 'video' ? '비디오' : '사진'
 
-        return (
-          <MediumWrapper
-            key={medium.id}
-            onClick={() => {
-              if (!allowNavigateImages) {
-                return
-              }
+          return (
+            <MediumWrapper
+              key={medium.id}
+              onClick={() => {
+                if (!allowNavigateImages) {
+                  return
+                }
 
-              trackEvent({
-                ga: ['리뷰_리뷰썸네일_클릭', thumbnailType],
-                fa: {
-                  action: '리뷰_리뷰썸네일_클릭',
-                  media_id: medium.id,
-                  type: thumbnailType,
-                  review_id: reviewId,
-                },
-              })
+                trackEvent({
+                  ga: ['리뷰_리뷰썸네일_클릭', thumbnailType],
+                  fa: {
+                    action: '리뷰_리뷰썸네일_클릭',
+                    media_id: medium.id,
+                    type: thumbnailType,
+                    review_id: reviewId,
+                  },
+                })
 
-              push(
-                `${REVIEW_MEDIA_POPUP_HASH}${HASH_EXTRA_INFO_SPLIT_STRING}${reviewId}${HASH_EXTRA_INFO_SPLIT_STRING}${index}`,
-              )
-            }}
-          >
-            <Medium medium={medium} />
-            {restLength > 0 && index === limit - 1 ? (
-              <FlexBox
-                flex
-                css={{
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  position: 'absolute',
-                  top: 0,
-                  width: '100%',
-                  height: '100%',
-                  color: 'var(--color-kint5-gray0)',
-                  backgroundColor: 'rgba(0, 0, 0, 0.3)',
-                  fontSize: 20,
-                  fontWeight: 700,
-                }}
-              >
-                +{restLength}
-              </FlexBox>
-            ) : null}
-          </MediumWrapper>
-        )
-      })}
-    </MediaWrapper>
+                setCurrentMediaPopupIndex(index)
+                push(`${MEDIA_POPUP_HASH}.${reviewId}`)
+              }}
+            >
+              <Medium medium={medium} />
+              {restLength > 0 && index === limit - 1 ? (
+                <FlexBox
+                  flex
+                  css={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    position: 'absolute',
+                    top: 0,
+                    width: '100%',
+                    height: '100%',
+                    color: 'var(--color-kint5-gray0)',
+                    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+                    fontSize: 20,
+                    fontWeight: 700,
+                  }}
+                >
+                  +{restLength}
+                </FlexBox>
+              ) : null}
+            </MediumWrapper>
+          )
+        })}
+      </MediaWrapper>
+      <MediaPopup
+        open={uriHash === `${MEDIA_POPUP_HASH}.${reviewId}`}
+        media={sortedMedia}
+        currentMediumIndex={currentMediaPopupIndex}
+        onMediumChange={setCurrentMediaPopupIndex}
+      />
+    </>
   )
 }
 
