171c9c8afc35898d03b828b763fc11ea47ff6fda
jaehyeon48 <kjhyeon48@interparktriple.com>
2024-04-12 14:15:07 +0900

    추천일정 담기 페스타 타입 테스트

--- packages/kint5-document/src/elements/itinerary/use-handle-add-pois-to-trip.ts
+++ packages/kint5-document/src/elements/itinerary/use-handle-add-pois-to-trip.ts
@@ -13,15 +13,21 @@ export function useHandleAddPoiToTrip() {
   const handleFn = useCallback(
     (poiId: string | string[]) => {
       const pois = Array.isArray(poiId)
-        ? poiId.map((poiId) => `poi:${poiId}`).join(',')
-        : `poi:${poiId}`
+        ? poiId.map((poiId) => `poi:${poiId}`)
+        : [`poi:${poiId}`]
+
+      const festas = [
+        process.env.NEXT_PUBLIC_ITINERARY_FESTA_ID1,
+        process.env.NEXT_PUBLIC_ITINERARY_FESTA_ID2,
+        process.env.NEXT_PUBLIC_ITINERARY_FESTA_ID3,
+      ].map((festaId) => `festa:${festaId}`)
 
       navigate(
         generateUrl({
           scheme: appUrlScheme,
           path: '/action/add_trip_schedule',
           query: qs.stringify({
-            items: pois,
+            items: [...pois, ...festas].join(','),
           }),
         }),
       )
