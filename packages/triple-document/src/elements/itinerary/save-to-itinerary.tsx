503e479094b3ac20565de94e7ba99024d46d239a
Heeryong Kang <drakang4@gmail.com>
2024-04-15 21:07:36 +0900

    fix i18next

--- packages/triple-document/src/elements/itinerary/save-to-itinerary.tsx
+++ packages/triple-document/src/elements/itinerary/save-to-itinerary.tsx
@@ -1,5 +1,5 @@
 import { useCallback } from 'react'
-import { useTranslation } from '@titicaca/next-i18next'
+import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 import { Button, Text } from '@titicaca/tds-ui'
 import { useTrackEvent } from '@titicaca/triple-web'
