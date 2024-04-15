0f4f30abd865bf05c3e87d7387bebef0dd83fa8f
Heeryong Kang <drakang4@gmail.com>
2024-04-15 21:03:47 +0900

    fix storybook build

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,86 +1,75 @@
 import React from 'react'
-import { GlobalStyle } from '../packages/core-elements/src'
-import {
-  EnvProvider,
-  HistoryProvider,
-  SessionContextProvider,
-  UserAgentProvider,
-} from '../packages/react-contexts/src'
-import { TripleClientMetadataProvider } from '../packages/react-triple-client-interfaces/src'
+import { GlobalStyle, defaultTheme } from '../packages/tds-theme/src'
+import { TripleWeb, ClientAppName } from '../packages/triple-web/src'
+import { ThemeProvider } from 'styled-components'
 
-export function globalStyleDecorator(Story) {
+import i18n from './i18next'
+
+export function themeDecorator(Story) {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <Story />
-    </>
+    </ThemeProvider>
   )
 }
 
-export function envProviderDecorator(Story) {
+export function tripleWebProviderDecorator(Story) {
   return (
-    <EnvProvider
-      appUrlScheme="dev-soto"
-      webUrlBase="https://triple-dev.titicaca-corp.com"
-      authBasePath="MOCK_AUTH_BASE_PATH"
-      facebookAppId=""
-      defaultPageTitle=""
-      defaultPageDescription=""
-      googleMapsApiKey="AIzaSyDuSWU_yBwuQzeyRFcTqhyifqNX_8oaXI4"
-      afOnelinkId=""
-      afOnelinkPid=""
-      afOnelinkSubdomain=""
-    >
-      <Story />
-    </EnvProvider>
-  )
-}
-
-export function historyProviderDecorator(Story) {
-  return (
-    <HistoryProvider
-      isPublic={false}
-      isAndroid={false}
-      transitionModalHash="transition.general"
-    >
-      <Story />
-    </HistoryProvider>
-  )
-}
-
-export function sessionContextProviderDecorator(Story) {
-  return (
-    <SessionContextProvider
-      type="browser"
-      props={{
-        initialUser: undefined,
-        initialSessionAvailability: false,
+    <TripleWeb
+      clientAppProvider={{
+        metadata: {
+          name: ClientAppName.iOS,
+          version: '6.5.0',
+        },
+        device: {
+          autoplay: 'always',
+          networkType: 'wifi',
+        },
+      }}
+      envProvider={{
+        appUrlScheme: 'dev-soto',
+        basePath: '/',
+        webUrlBase: 'https://triple-dev.titicaca-corp.com',
+        facebookAppId: '',
+        defaultPageTitle: '',
+        defaultPageDescription: '',
+        googleMapsApiKey: 'AIzaSyDuSWU_yBwuQzeyRFcTqhyifqNX_8oaXI4',
+        afOnelinkId: '',
+        afOnelinkPid: '',
+        afOnelinkSubdomain: '',
+      }}
+      i18nProvider={{
+        i18n,
+        lang: 'ko',
+      }}
+      sessionProvider={{
+        user: {
+          name: 'TripleTester',
+          provider: 'TRIPLE',
+          country: 'ko',
+          lang: 'ko',
+          unregister: null,
+          photo: 'images.source',
+          mileage: {
+            badges: [{ icon: { imageUrl: '' } }],
+            level: 1,
+            point: 30,
+          },
+          uid: 'test',
+        },
+      }}
+      userAgentProvider={{
+        ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;Triple-iOS/6.5.0',
+        browser: { name: 'WebKit', version: '605.1.15', major: '605' },
+        engine: { name: 'WebKit', version: '605.1.15' },
+        os: { name: 'iOS', version: '13.3.1' },
+        device: { vendor: 'Apple', model: 'iPhone', type: 'mobile' },
+        cpu: { architecture: undefined },
+        isMobile: true,
       }}
     >
       <Story />
-    </SessionContextProvider>
-  )
-}
-
-export function userAgentProviderDecorator(Story) {
-  return (
-    <UserAgentProvider
-      value={{
-        isPublic: true,
-        isMobile: false,
-        os: {},
-        app: null,
-      }}
-    >
-      <Story />
-    </UserAgentProvider>
-  )
-}
-
-export function tripleClientMetadataDecorator(Story) {
-  return (
-    <TripleClientMetadataProvider>
-      <Story />
-    </TripleClientMetadataProvider>
+    </TripleWeb>
   )
 }
