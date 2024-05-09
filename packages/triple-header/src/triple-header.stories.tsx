68277d3701f7c26b424adb2cb1f851e7bc0da0a2
steve <steve@interparktriple.com>
2024-05-09 18:32:15 +0900

    feat: storybook 수정

--- packages/triple-header/src/triple-header.stories.tsx
+++ packages/triple-header/src/triple-header.stories.tsx
@@ -1,7 +1,8 @@
 import type { Meta } from '@storybook/react'
 import { EventTrackingProvider } from '@titicaca/triple-web'
 
-import SAMPLE from './mocks/triple-header.sample.json'
+import IMATE_TYPE_SAMPLE from './mocks/image-type.sample.json'
+import JSON_TYPE_SAMPLE from './mocks/json-type.sample.json'
 import { TripleHeader } from './triple-header'
 import { TripleHeaderProps } from './types'
 
@@ -16,6 +17,10 @@ export default {
   ],
 } as Meta<typeof TripleHeader>
 
-export function Sample() {
-  return <TripleHeader>{SAMPLE as TripleHeaderProps}</TripleHeader>
+export function ImageType() {
+  return <TripleHeader>{IMATE_TYPE_SAMPLE as TripleHeaderProps}</TripleHeader>
+}
+
+export function JsonType() {
+  return <TripleHeader>{JSON_TYPE_SAMPLE as TripleHeaderProps}</TripleHeader>
 }
