7880d2bffb2a4e9ac9a3e01a488ea682ba7d41d7
jaehyeon48 <kjhyeon48@interparktriple.com>
2024-05-09 10:59:51 +0900

    StaticMap 마커를 위로 18px만큼 올림

--- packages/kint5-static-map/src/static-map.tsx
+++ packages/kint5-static-map/src/static-map.tsx
@@ -114,7 +114,7 @@ function StaticMap({
           position: 'absolute',
           top: '50%',
           left: '50%',
-          transform: 'translate(-50%, -50%)',
+          transform: 'translate(-50%, calc(-50% - 18px))',
         }}
       >
         <MapMarker />
