09d02de046af4d6abc29239617bd46ecd62d25d0
steve <steve@interparktriple.com>
2024-05-11 11:59:06 +0900

    feat: use-lottie useEffect deps 수정

--- packages/react-hooks/src/use-lottie.tsx
+++ packages/react-hooks/src/use-lottie.tsx
@@ -17,29 +17,25 @@ export function useLottie<T extends HTMLElement>({
   const [animation, setAnimation] = useState<AnimationItem>()
   const animationRef = useRef<T>(null)
 
-  useEffect(
-    () => {
-      if (animationRef.current) {
-        const animation = Lottie.loadAnimation({
-          container: animationRef.current,
-          renderer: 'svg',
-          loop,
-          autoplay,
-          path,
-          animationData: data,
-          rendererSettings,
-        })
+  useEffect(() => {
+    if (animationRef.current) {
+      const animation = Lottie.loadAnimation({
+        container: animationRef.current,
+        renderer: 'svg',
+        loop,
+        autoplay,
+        path,
+        animationData: data,
+        rendererSettings,
+      })
 
-        setAnimation(animation)
+      setAnimation(animation)
 
-        return () => {
-          animation.destroy()
-        }
+      return () => {
+        animation.destroy()
       }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
-  )
+    }
+  }, [autoplay, data, loop, path, rendererSettings])
 
   return { animation, animationRef }
 }
