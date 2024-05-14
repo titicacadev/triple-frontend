6d066667ee1079291cfa4b0780652f0f86d07c9f
steve <steve@interparktriple.com>
2024-05-14 11:51:05 +0900

    feat: useEffect한번만 실행되도록 수정

--- packages/triple-header/src/triple-header.stories.tsx
+++ packages/triple-header/src/triple-header.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta } from '@storybook/react'
 import { EventTrackingProvider } from '@titicaca/triple-web'
 
 import FRAMER_TYPE_SAMPLE from './mocks/framer-type.sample.json'
-// import LOTTIE_TYPE_SAMPLE from './mocks/lottie-type.sample.json'
+import LOTTIE_TYPE_SAMPLE from './mocks/lottie-type.sample.json'
 import { TripleHeader } from './triple-header'
 import { TripleHeaderProps } from './types'
 
@@ -22,7 +22,6 @@ export function FramerType() {
   return <TripleHeader>{FRAMER_TYPE_SAMPLE as TripleHeaderProps}</TripleHeader>
 }
 
-// TODO: Lottie 에러 해결후 주석 해제 필요
-// export function LottieType() {
-//   return <TripleHeader>{LOTTIE_TYPE_SAMPLE as TripleHeaderProps}</TripleHeader>
-// }
+export function LottieType() {
+  return <TripleHeader>{LOTTIE_TYPE_SAMPLE as TripleHeaderProps}</TripleHeader>
+}
