d4122edb60edad7c6cedcff3c366b197749557e1
guswl98 <hjee0901@gmail.com>
2024-05-17 10:39:08 +0900

    ko property를 복구

--- packages/type-definitions/src/translated-property.ts
+++ packages/type-definitions/src/translated-property.ts
@@ -1,13 +1,6 @@
 export interface TranslatedProperty {
   primary?: string | null
-  /** @deprecated */
   ko?: string | null
   en: string | null
   local: string | null
 }
-// TODO: content 마이그레이션이 완료되면 사용할 수 있음
-// export interface TranslatedProperty {
-//   primary: string | null
-//   en: string | null
-//   local: string | null
-// }
