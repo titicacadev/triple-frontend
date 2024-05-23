fe82193dd67a5c3d71400b8dcac50721bdbb95a8
jaehyeon48 <kjhyeon48@interparktriple.com>
2024-05-23 14:51:54 +0900

    i18nizeHref 추가

---
+++ packages/kint5-router/src/i18nize-href.ts
@@ -0,0 +1,18 @@
+interface I18nizeHrefArgs {
+  lang: string
+  path: string
+  webUrlBase?: string
+  basePath?: string
+}
+
+export function i18nizeHref({
+  lang,
+  path,
+  webUrlBase: webUrlBaseArg,
+  basePath: basePathArg,
+}: I18nizeHrefArgs) {
+  const webUrlBase = webUrlBaseArg || process.env.NEXT_PUBLIC_WEB_URL_BASE
+  const basePath = basePathArg || process.env.NEXT_PUBLIC_BASE_PATH
+
+  return `${webUrlBase}${basePath}/${lang}${path}`
+}
