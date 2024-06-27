0468fee2079e2579d8e4a80ae6f48642f1fb92b6
guswl98 <hjee0901@gmail.com>
2024-06-27 15:53:01 +0900

    액션 시트 pointerEvents 추가

--- packages/tds-ui/src/components/action-sheet/action-sheet.tsx
+++ packages/tds-ui/src/components/action-sheet/action-sheet.tsx
@@ -114,6 +114,7 @@ export const ActionSheet = ({
                 labelId={labelId}
                 transitionStatus={status}
                 aria-modal
+                css={{ pointerEvents: 'auto' }}
                 {...getFloatingProps(props)}
               >
                 {children}
