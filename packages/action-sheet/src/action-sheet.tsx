23037b0f5d8c18c59e324bd51c018ec18777f0ac
guswl98 <hjee0901@gmail.com>
2024-07-12 15:01:03 +0900

    ActionSheet에 ref 추가

--- packages/action-sheet/src/action-sheet.tsx
+++ packages/action-sheet/src/action-sheet.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useId } from 'react'
+import { forwardRef, useEffect, useId } from 'react'
 import { FlexBox } from '@titicaca/core-elements'
 import {
   FloatingFocusManager,
@@ -37,96 +37,102 @@ export interface ActionSheetProps
 /**
  * ActionSheet
  */
-export const ActionSheet = ({
-  children,
-  open = false,
-  lockScroll = true,
-  title,
-  borderRadius = 12,
-  bottomSpacing = 13,
-  from = 'bottom',
-  maxContentHeight = 'calc(100vh - 256px)',
-  onClose,
-  onEnter,
-  onEntered,
-  onExit,
-  onExited,
-  ...props
-}: ActionSheetProps) => {
-  const labelId = useId()
+export const ActionSheet = forwardRef<HTMLDivElement, ActionSheetProps>(
+  function ActionSheet(
+    {
+      children,
+      open = false,
+      lockScroll = true,
+      title,
+      borderRadius = 12,
+      bottomSpacing = 13,
+      from = 'bottom',
+      maxContentHeight = 'calc(100vh - 256px)',
+      onClose,
+      onEnter,
+      onEntered,
+      onExit,
+      onExited,
+      ...props
+    },
+    ref,
+  ) {
+    const labelId = useId()
 
-  const { context, refs } = useFloating({
-    open,
-    onOpenChange: (open) => (open ? undefined : onClose?.()),
-  })
+    const { context, refs } = useFloating({
+      open,
+      onOpenChange: (open) => (open ? undefined : onClose?.()),
+    })
 
-  const dismiss = useDismiss(context)
-  const role = useRole(context, { role: 'dialog' })
+    const dismiss = useDismiss(context)
+    const role = useRole(context, { role: 'dialog' })
 
-  const { getFloatingProps } = useInteractions([dismiss, role])
+    const { getFloatingProps } = useInteractions([dismiss, role])
 
-  const { isMounted, status } = useTransitionStatus(context, {
-    duration: TRANSITION_DURATION,
-  })
+    const { isMounted, status } = useTransitionStatus(context, {
+      duration: TRANSITION_DURATION,
+    })
 
-  useEffect(() => {
-    if (status === 'open') {
-      onEnter?.()
-      const timeout = setTimeout(() => onEntered?.(), TRANSITION_DURATION)
-      return () => clearTimeout(timeout)
-    } else if (status === 'close') {
-      onExit?.()
-      const timeout = setTimeout(() => onExited?.(), TRANSITION_DURATION)
-      return () => clearTimeout(timeout)
-    }
-  }, [onEnter, onEntered, onExit, onExited, status])
+    useEffect(() => {
+      if (status === 'open') {
+        onEnter?.()
+        const timeout = setTimeout(() => onEntered?.(), TRANSITION_DURATION)
+        return () => clearTimeout(timeout)
+      } else if (status === 'close') {
+        onExit?.()
+        const timeout = setTimeout(() => onExited?.(), TRANSITION_DURATION)
+        return () => clearTimeout(timeout)
+      }
+    }, [onEnter, onEntered, onExit, onExited, status])
 
-  return (
-    <ActionSheetContext.Provider value={{ open, onClose }}>
-      {isMounted ? (
-        <FloatingPortal>
-          {lockScroll && (
-            <ActionSheetOverlay
-              transitionStatus={status}
-              lockScroll={lockScroll}
-            />
-          )}
-          <FlexBox
-            flex
-            justifyContent="center"
-            css={{
-              position: 'fixed',
-              left: 0,
-              right: 0,
-              top: 0,
-              bottom: 0,
-              zIndex: 9999,
-              ...(!lockScroll && { pointerEvents: 'none' }),
-            }}
-          >
-            <FloatingFocusManager
-              context={context}
-              initialFocus={refs.floating}
-            >
-              <ActionSheetBody
-                ref={refs.setFloating}
-                borderRadius={borderRadius}
-                bottomSpacing={bottomSpacing}
-                maxContentHeight={maxContentHeight}
-                from={from}
-                title={title}
-                labelId={labelId}
+    return (
+      <ActionSheetContext.Provider value={{ open, onClose }}>
+        {isMounted ? (
+          <FloatingPortal>
+            {lockScroll && (
+              <ActionSheetOverlay
                 transitionStatus={status}
-                aria-modal
-                css={{ pointerEvents: 'auto' }}
-                {...getFloatingProps(props)}
+                lockScroll={lockScroll}
+              />
+            )}
+            <FlexBox
+              flex
+              justifyContent="center"
+              css={{
+                position: 'fixed',
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0,
+                zIndex: 9999,
+                ...(!lockScroll && { pointerEvents: 'none' }),
+              }}
+              ref={ref}
+            >
+              <FloatingFocusManager
+                context={context}
+                initialFocus={refs.floating}
               >
-                {children}
-              </ActionSheetBody>
-            </FloatingFocusManager>
-          </FlexBox>
-        </FloatingPortal>
-      ) : null}
-    </ActionSheetContext.Provider>
-  )
-}
+                <ActionSheetBody
+                  ref={refs.setFloating}
+                  borderRadius={borderRadius}
+                  bottomSpacing={bottomSpacing}
+                  maxContentHeight={maxContentHeight}
+                  from={from}
+                  title={title}
+                  labelId={labelId}
+                  transitionStatus={status}
+                  aria-modal
+                  css={{ pointerEvents: 'auto' }}
+                  {...getFloatingProps(props)}
+                >
+                  {children}
+                </ActionSheetBody>
+              </FloatingFocusManager>
+            </FlexBox>
+          </FloatingPortal>
+        ) : null}
+      </ActionSheetContext.Provider>
+    )
+  },
+)
