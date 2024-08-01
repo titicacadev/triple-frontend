d3ba4147557fa7895f3b47839c46da8a077e0858
hailey <hailey@interparktriple.com>
2024-08-01 13:25:57 +0900

    MenuType export 추가

--- packages/tds-widget/src/public-header/index.ts
+++ packages/tds-widget/src/public-header/index.ts
@@ -4,5 +4,6 @@ export * from './extra-action-separator'
 export * from './constants'
 export * from './side-menu'
 export * from './header-menu-button'
+export * from './side-menu/type'
 
 export { PublicHeader as default } from './public-header'
