3fdfb0fe3e88e05b3934f074072a58df073a67e6
hailey <hailey@interparktriple.com>
2024-08-01 17:12:23 +0900

    부모 메세지 클릭 이벤트 추가

--- packages/tds-widget/src/chat/bubble/text.tsx
+++ packages/tds-widget/src/chat/bubble/text.tsx
@@ -33,6 +33,7 @@ export function TextBubble({
   openFullTextView,
   closeFullTextView,
   isFullTextViewOpen,
+  onParentMessageClick,
   ...props
 }: TextBubbleProp) {
   const aTagNavigator = useATagNavigator()
@@ -50,6 +51,7 @@ export function TextBubble({
           }
         `}
         my={my}
+        onParentMessageClick={onParentMessageClick}
         {...props}
       >
         <TextItem
--- packages/tds-widget/src/chat/bubble/type.ts
+++ packages/tds-widget/src/chat/bubble/type.ts
@@ -78,6 +78,7 @@ export type TextBubbleProp = {
   isFullTextViewOpen?: boolean
   openFullTextView?: () => void
   closeFullTextView?: () => void
+  onParentMessageClick?: (id: string) => void
 } & BubbleProp
 
 export type RichBubbleProp = {
