8f10c3691fa9e15e175ca7d03fa2b22bbc3a3421
guswl98 <hjee0901@gmail.com>
2024-08-05 16:31:37 +0900

    useFetchImages hook 분리

---
+++ packages/react-contexts/src/images-context/types.ts
@@ -0,0 +1,7 @@
+export type ImageCategoryOrder =
+  | 'image' // 대표 이미지
+  | 'recommendation'
+  | 'menuItem' // 대표 메뉴 이미지
+  | 'menuBoard' // 메뉴판 이미지
+  | 'featuredContent'
+  | 'images' // 어드민에 등록된 이미지들 가운데 대표 이미지를 제외한 나머지 이미지
