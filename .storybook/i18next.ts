021537ea2dd37bd1faa9787c30d1b3c1d35378ec
Heeryong Kang <drakang4@gmail.com>
2024-08-08 14:49:01 +0900

    chore: storybook locale 수정

--- .storybook/i18next.ts
+++ .storybook/i18next.ts
@@ -1,24 +1,27 @@
 import i18n from 'i18next'
-import resourcesToBackend from 'i18next-resources-to-backend'
 import { initReactI18next } from 'react-i18next/initReactI18next'
 
 const supportedLngs = ['ko', 'ja', 'zh-TW']
-
-i18n
-  .use(
-    resourcesToBackend(
-      (language: string, namespace: string) =>
-        import(`../packages/i18n/src/locales/${language}`),
-    ),
-  )
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'ko',
-    supportedLngs,
-
-    interpolation: {
-      escapeValue: false,
-    },
+const ns = ['triple-frontend']
+const resources = ns.reduce((acc, n) => {
+  supportedLngs.forEach((lng) => {
+    if (!acc[lng]) acc[lng] = {}
+    acc[lng] = {
+      ...acc[lng],
+      [n]: require(`../packages/i18n/src/locales/${lng}`).default,
+    }
   })
+  return acc
+}, {})
+
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: 'ko',
+  supportedLngs,
+
+  interpolation: {
+    escapeValue: false,
+  },
+})
 
 export default i18n
