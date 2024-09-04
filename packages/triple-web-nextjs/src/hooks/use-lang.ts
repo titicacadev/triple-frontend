6c86a0d0aca7de1b0f5bc5ac44d6ab95101a9f78
Heeryong Kang <drakang4@gmail.com>
2024-09-04 15:02:04 +0900

    chore(triple-web-nextjs): i18n provider 제거

--- packages/triple-web-nextjs/src/hooks/use-lang.ts
+++
@@ -1,8 +0,0 @@
-import { useSearchParams } from 'next/navigation'
-
-export function useLang(fallback: string, key = 'lang') {
-  const searchParams = useSearchParams()
-  const lang = searchParams.get(key) || fallback
-
-  return lang
-}
