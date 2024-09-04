e22f8a080bcf70c182120d509bc3c1a52881de31
Heeryong Kang <drakang4@gmail.com>
2024-09-04 15:01:47 +0900

    chore(triple-web): i18n provider 제거

--- packages/triple-web/src/providers/triple-web.tsx
+++ packages/triple-web/src/providers/triple-web.tsx
@@ -4,14 +4,12 @@ import { PropsWithChildren } from 'react'
 
 import { ClientAppContext } from '../client-app/context'
 import { EnvContext } from '../env/context'
-import { I18nProvider } from '../i18n/context'
 import { SessionProvider } from '../session/context'
 import { UserAgentContext } from '../user-agent/context'
 import { HashRouterProvider } from '../hash-router/context'
 import { ModalProvider } from '../modal/context'
 import type { ClientAppValue } from '../client-app/types'
 import type { EnvValue } from '../env/types'
-import type { I18nValue } from '../i18n/types'
 import type { SessionValue } from '../session/types'
 import type { UserAgentValue } from '../user-agent/types'
 import { LoginCtaModal } from '../modal/components/login-cta-modal'
@@ -20,7 +18,6 @@ import { AppInstallCtaModal } from '../modal/components/app-install-cta-modal'
 export interface TripleWebProps extends PropsWithChildren {
   clientAppProvider: ClientAppValue
   envProvider: EnvValue
-  i18nProvider: I18nValue
   sessionProvider: SessionValue
   userAgentProvider: UserAgentValue
 }
@@ -29,26 +26,24 @@ export function TripleWeb({
   children,
   clientAppProvider,
   envProvider,
-  i18nProvider,
+
   sessionProvider,
   userAgentProvider,
 }: TripleWebProps) {
   return (
     <ClientAppContext.Provider value={clientAppProvider}>
       <EnvContext.Provider value={envProvider}>
-        <I18nProvider i18n={i18nProvider.i18n} lang={i18nProvider.lang}>
-          <SessionProvider initialSession={sessionProvider}>
-            <UserAgentContext.Provider value={userAgentProvider}>
-              <HashRouterProvider>
-                <ModalProvider>
-                  {children}
-                  <LoginCtaModal />
-                  <AppInstallCtaModal />
-                </ModalProvider>
-              </HashRouterProvider>
-            </UserAgentContext.Provider>
-          </SessionProvider>
-        </I18nProvider>
+        <SessionProvider initialSession={sessionProvider}>
+          <UserAgentContext.Provider value={userAgentProvider}>
+            <HashRouterProvider>
+              <ModalProvider>
+                {children}
+                <LoginCtaModal />
+                <AppInstallCtaModal />
+              </ModalProvider>
+            </HashRouterProvider>
+          </UserAgentContext.Provider>
+        </SessionProvider>
       </EnvContext.Provider>
     </ClientAppContext.Provider>
   )
