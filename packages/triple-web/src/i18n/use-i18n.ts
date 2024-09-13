f0cb8e08f5f4a3308bd9969fa6146c3848714b80
Heeryong Kang <drakang4@gmail.com>
2024-09-13 14:29:06 +0900

    docs(triple-web): i18n hooks 문서 추가

--- packages/triple-web/src/i18n/use-i18n.ts
+++ packages/triple-web/src/i18n/use-i18n.ts
@@ -2,6 +2,9 @@ import { useContext } from 'react'
 
 import { I18nContext } from './context'
 
+/**
+ * I18nContext 값을 가져옵니다.
+ */
 export function useI18n() {
   const context = useContext(I18nContext)
 
