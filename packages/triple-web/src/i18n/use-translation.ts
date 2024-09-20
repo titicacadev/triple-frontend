707ff576cd8c87ff43413d75d38035a9510351e3
Heeryong Kang <drakang4@gmail.com>
2024-09-20 10:51:08 +0900

    fix(triple-web): locale import path 수정

--- packages/triple-web/src/i18n/use-translation.ts
+++ packages/triple-web/src/i18n/use-translation.ts
@@ -1,16 +1,12 @@
-import { interpolate } from '@titicaca/i18n'
-import en from '@titicaca/i18n/lib/locales/en'
-import ko from '@titicaca/i18n/lib/locales/ko'
-import ja from '@titicaca/i18n/lib/locales/ja'
-import zhTw from '@titicaca/i18n/lib/locales/zh-TW'
+import { interpolate, locales } from '@titicaca/i18n'
 
 import { useI18n } from './use-i18n'
 
 const translations: Record<string, Record<string, string>> = {
-  en,
-  ko,
-  ja,
-  'zh-TW': zhTw,
+  en: locales.en,
+  ja: locales.ja,
+  ko: locales.ko,
+  'zh-TW': locales.zhTw,
 }
 
 /**
