9cbc58211d870d96d8770c1c31697aa308970ffe
guswl98 <hjee0901@gmail.com>
2024-10-22 17:57:34 +0900

    로그인 필요 시 로그인 유도 모달 노출

--- packages/standard-action-handler/src/converse.tsx
+++ packages/standard-action-handler/src/converse.tsx
@@ -6,8 +6,14 @@ import { WebActionParams } from './types'
 
 const HASH_CONVERSE_MODAL = 'hash.converse-modal'
 
+const NEED_LOGIN_CONTENT = {
+  title: '로그인이 필요합니다.',
+  description: '로그인하고 트리플을\n더 편하게 이용하세요🙂',
+}
+
 export default async function converse({
   url: { path, query } = {},
+  options: { navigate } = {},
 }: WebActionParams) {
   if (path === '/web-action/converse' && query) {
     const { path: pathFromQuery } = qs.parse(query) as { path: string }
@@ -15,6 +21,8 @@ export default async function converse({
     const { title, description } = await fetchApi(pathFromQuery)
 
     if (title && description) {
+      const isLoginModal = title === NEED_LOGIN_CONTENT.title
+
       window.history.pushState(null, '', `#${HASH_CONVERSE_MODAL}`)
 
       const container = document.createElement('div')
@@ -22,6 +30,17 @@ export default async function converse({
 
       const closeModal = () => {
         window.history.back()
+      }
+
+      const onClickLogin = () => {
+        const loginUrl = window.location.href.replace(
+          `#${HASH_CONVERSE_MODAL}`,
+          '',
+        )
+        closeModal()
+        if (navigate) {
+          navigate(`/login?returnUrl=${encodeURIComponent(loginUrl)}`)
+        }
       }
 
       const handlePopstate = () => {
@@ -37,7 +56,8 @@ export default async function converse({
         OpenModal({
           title,
           description,
-          onClose: closeModal,
+          onCancel: isLoginModal ? closeModal : undefined,
+          onConfirm: isLoginModal ? onClickLogin : closeModal,
         }),
       )
 
@@ -53,20 +73,23 @@ export default async function converse({
 export function OpenModal({
   title,
   description,
-  onClose,
+  onCancel,
+  onConfirm,
 }: {
   title: string
   description: string
-  onClose: () => void
+  onCancel?: () => void
+  onConfirm: () => void
 }) {
   return (
-    <Modal open onClose={onClose}>
+    <Modal open onClose={onCancel}>
       <Modal.Body>
         <Modal.Title>{title}</Modal.Title>
         <Modal.Description>{description}</Modal.Description>
       </Modal.Body>
       <Modal.Actions>
-        <Modal.Action color="blue" onClick={onClose}>
+        {onCancel && <Modal.Action onClick={onCancel}>취소</Modal.Action>}
+        <Modal.Action color="blue" onClick={onConfirm}>
           확인
         </Modal.Action>
       </Modal.Actions>
@@ -85,6 +108,9 @@ async function fetchApi(
   })
 
   if (!response.ok) {
+    if (response.status === 400 || response.status === 401) {
+      return NEED_LOGIN_CONTENT
+    }
     return {
       title: '안내',
       description:
