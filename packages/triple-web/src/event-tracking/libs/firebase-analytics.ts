83631dd8171dce29a514a102bd75581255a516ca
hailey <hailey@interparktriple.com>
2024-12-05 17:32:22 +0900

    initializeAnalytics 삭제

--- packages/triple-web/src/event-tracking/libs/firebase-analytics.ts
+++ packages/triple-web/src/event-tracking/libs/firebase-analytics.ts
@@ -1,14 +1,10 @@
 import { getApp } from 'firebase/app'
-import { getAnalytics, initializeAnalytics } from 'firebase/analytics'
+import { getAnalytics } from 'firebase/analytics'
 
 export function getFirebaseAnalytics() {
   try {
     const app = getApp()
-    const analytics =
-      getAnalytics(app) ??
-      initializeAnalytics(app, {
-        config: { send_page_view: false },
-      })
+    const analytics = getAnalytics(app)
 
     return analytics
   } catch (error) {}
