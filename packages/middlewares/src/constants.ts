61890a1f0e7253193507f545df530e8eda8e14f2
guswl98 <hjee0901@gmail.com>
2024-12-11 14:19:53 +0900

    세션 관련 쿠키가 있는 경우에만 sessionMiddleware 실행하도록 수정

--- packages/middlewares/src/constants.ts
+++ packages/middlewares/src/constants.ts
@@ -1,3 +1,7 @@
 export const NEED_LOGIN_IDENTIFIER = 'NEED_LOGIN'
 
 export const X_AUTH_STATUS = 'x-auth-status'
+
+export const TP_TK = 'TP_TK'
+export const TP_SE = 'TP_SE'
+export const X_SOTO_SESSION = 'x-soto-session'
