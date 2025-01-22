5704c87936ca8f6ff5164a468a5f57ec7b024945
guswl98 <hjee0901@gmail.com>
2025-01-22 09:59:04 +0900

    파일명 오타 수정

---
+++ packages/tds-widget/src/chat/icons/reply-message-icon.tsx
@@ -0,0 +1,24 @@
+export function ReplyMessageIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="14" height="14" rx="3" fill="#EAF2F2" />
+      <path
+        d="M8 6L10 8.05594L8 10"
+        stroke="#A8ADAD"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9 8L5 8C4.44772 8 4 7.55228 4 7L4 4"
+        stroke="#A8ADAD"
+        strokeLinecap="round"
+      />
+    </svg>
+  )
+}
