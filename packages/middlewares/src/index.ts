4ca460c30240dcc83b661084cec6a66706946d2a
guswl98 <hjee0901@gmail.com>
2025-02-24 14:31:33 +0900

    session cookie를 session refresh로 수정

--- packages/middlewares/src/index.ts
+++ packages/middlewares/src/index.ts
@@ -1,4 +1,10 @@
+import { chain, MiddlewareFactory } from './chain'
+import { refreshSessionMiddleware } from './refresh-session'
+
 export { chain } from './chain'
 export { oldIosCookiesMiddleware } from './old-ios-cookie'
-export { sessionCookieMiddleware } from './session-cookie'
+export { refreshSessionMiddleware } from './refresh-session'
 export { NEED_LOGIN_IDENTIFIER, X_AUTH_STATUS } from './constants'
+
+export const middlewareWithSessionRefresh = (functions: MiddlewareFactory[]) =>
+  chain([refreshSessionMiddleware, ...functions])
