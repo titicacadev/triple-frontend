0d358dd7292264bd0783c5f72898d28b42ce7625
hailey <hailey@interparktriple.com>
2025-03-04 13:09:02 +0900

    triple korea 링크를 범용 extra footer link로 변경

--- packages/footer/src/index.ts
+++ packages/footer/src/index.ts
@@ -3,4 +3,6 @@ import DefaultFooter from './default-footer'
 export { default as LogoFooter } from './logo-footer'
 export { default as AwardFooter } from './award-footer'
 
+export { ExtraFooterLink } from './extra-footer-link'
+
 export default DefaultFooter
