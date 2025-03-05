f51304a75125b7d243edffc671a1ff51b806c5b8
hailey <hailey@interparktriple.com>
2025-03-05 10:54:52 +0900

    useEnv에 webAssetUrl 환경변수 추가

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -30,6 +30,7 @@ export function tripleWebProviderDecorator(Story, context) {
         afOnelinkId: '',
         afOnelinkPid: '',
         afOnelinkSubdomain: '',
+        webAssetUrl: 'https://triple-dev.titicaca-corp.com',
       }}
       i18nProvider={{
         defaultLocale: 'ko',
