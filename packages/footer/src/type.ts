641e9d83a02f0e7c12d67d181ef84ccf6a5ac69d
hailey <hailey@interparktriple.com>
2025-03-05 09:20:43 +0900

    mock footer.json 및 footerInfo type 생성

---
+++ packages/footer/src/type.ts
@@ -0,0 +1,30 @@
+export interface CompanyInfo {
+  company: Company
+  disclaimer: string
+  links: Link[]
+  extraLinks: Link[]
+  awards: Award[]
+}
+
+export interface Company {
+  name: string
+  ceo: WithLabel<{ names: string[] }>
+  businessRegistrationNumber: WithLabel<{ value: string }>
+  salesReportNumber: WithLabel<{ value: string }>
+  address: WithLabel<{ value: string }>
+  contact: WithLabel<{ phone: string; email: string }>
+}
+
+export interface Link {
+  label: string
+  url: string
+  faEventAction?: string
+}
+
+export interface Award {
+  text: string
+  alt: string
+  imageUrl: string
+}
+
+type WithLabel<T> = T & { label: string }
