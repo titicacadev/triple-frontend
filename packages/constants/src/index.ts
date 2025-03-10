44dbd6d5706757e03bedc5f3a90b7d3211dd7ea0
guswl98 <hjee0901@gmail.com>
2025-03-10 10:45:18 +0900

    constants 패키지에 세션 관련 변수 정의

--- packages/constants/src/index.ts
+++ packages/constants/src/index.ts
@@ -2,3 +2,8 @@ export * from './regex'
 
 /** API 호출 시 필요한 사용자 세션 HTTP 헤더 키  */
 export const SESSION_KEY = 'x-soto-session'
+
+export const TP_SE = 'TP_SE'
+export const TP_TK = 'TP_TK'
+
+export const X_TRIPLE_WEB_DEVICE_ID = 'x-triple-web-device-id'
--- packages/react-contexts/src/middlewares/index.ts
+++ packages/react-contexts/src/middlewares/index.ts
@@ -3,6 +3,13 @@ import type { MiddlewareFactory } from './types'
 import { refreshSessionMiddleware } from './refresh-session'
 import { setWebDeviceIdMiddleware } from './set-web-device-id'
 
+export {
+  TP_SE,
+  TP_TK,
+  X_TRIPLE_WEB_DEVICE_ID,
+  SESSION_KEY as X_SOTO_SESSION,
+} from '@titicaca/constants'
+
 export const constructMiddleware = (functions: MiddlewareFactory[]) =>
   chain([...functions, refreshSessionMiddleware, setWebDeviceIdMiddleware])
 
