2a1c9916b65aa3f4e26feaf9356eb94f6873f301
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-03-10 10:14:03 +0900

    feat: 채팅 버블 부가 정보 커스텀 스타일 프롭스 추가

--- packages/tds-widget/src/chat/messages/date-divider.tsx
+++ packages/tds-widget/src/chat/messages/date-divider.tsx
@@ -1,9 +1,15 @@
 import { Text } from '@titicaca/tds-ui'
 import { format } from 'date-fns'
 
-export function DateDivider({ date }: { date: Date }) {
+export function DateDivider({ date, ...props }: { date: Date }) {
   return (
-    <Text textAlign="center" size={11} color="gray700" margin={{ top: 30 }}>
+    <Text
+      textAlign="center"
+      size={11}
+      color="gray700"
+      margin={{ top: 30 }}
+      {...props}
+    >
       {format(date, 'yyyy년 MM월 dd일')}
     </Text>
   )
