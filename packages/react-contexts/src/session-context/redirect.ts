b7874751a121ee441a8b6c9e10fd42567e17efda
guswl98 <hjee0901@gmail.com>
2025-03-11 14:20:28 +0900

    device id 제거

--- packages/react-contexts/src/session-context/redirect.ts
+++ packages/react-contexts/src/session-context/redirect.ts
@@ -1,20 +1,17 @@
 import { generateUrl } from '@titicaca/view-utilities'
 import qs from 'qs'
-import Cookies from 'universal-cookie'
 
 export function getRedirectUrl(href: string) {
   const currentUrl = decodeURI(window.location.href)
-  const deviceId = new Cookies(document.cookie).get<string>(
-    'x-triple-web-device-id',
-  )
 
-  const redirectUrl = generateUrl({
+  const redirectUrl = generateUrl(
+    {
+      query: qs.stringify({
+        redirect: currentUrl,
+      }),
+    },
     href,
-    query: qs.stringify({
-      returnUrl: currentUrl,
-      deviceId,
-    }),
-  })
+  )
 
   return redirectUrl
 }
