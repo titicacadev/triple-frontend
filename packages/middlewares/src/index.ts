cacda8d872eec77e7c94220e98213bc2aa849729
guswl98 <hjee0901@gmail.com>
2025-03-14 13:22:50 +0900

    필요없는 미들웨어 코드 삭제

--- packages/middlewares/src/index.ts
+++ packages/middlewares/src/index.ts
@@ -3,8 +3,6 @@ import type { MiddlewareFactory } from './types'
 import { refreshSessionMiddleware } from './refresh-session'
 import { setWebDeviceIdMiddleware } from './set-web-device-id'
 
-export { oldIosCookiesMiddleware } from './old-ios-cookie'
-
 export const constructMiddleware = (functions: MiddlewareFactory[]) =>
   chain([...functions, refreshSessionMiddleware, setWebDeviceIdMiddleware])
 
