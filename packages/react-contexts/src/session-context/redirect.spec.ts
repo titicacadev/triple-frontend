81446037bf3aa9738573db1381760bf896b823c2
hailey <hailey@interparktriple.com>
2025-03-19 14:54:39 +0900

    redirect시 baseUrl에 쿼리스트링이 추가될 경우의 테스트 코드 추가

--- packages/react-contexts/src/session-context/redirect.spec.ts
+++ packages/react-contexts/src/session-context/redirect.spec.ts
@@ -15,4 +15,13 @@ describe('getRedirectUrl', () => {
       )}`,
     )
   })
+
+  it('should return redirect url with redirect query string and original href query string', () => {
+    const result = getRedirectUrl('https://example.com?code=123')
+    expect(result).toBe(
+      `https://example.com?code=123&redirect=${encodeURIComponent(
+        window.location.href,
+      )}`,
+    )
+  })
 })
