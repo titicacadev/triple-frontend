b3e031f63dd1bc9c1eac89851413b43ddb424261
hailey <hailey@interparktriple.com>
2025-03-20 13:44:14 +0900

    getSessionAvailability에서 checkSession 사용

--- packages/triple-web-nextjs-pages/src/ssr-utils/get-session-availability.ts
+++ packages/triple-web-nextjs-pages/src/ssr-utils/get-session-availability.ts
@@ -1,12 +1,9 @@
-import { checkClientApp } from '@titicaca/triple-web-utils'
 import type { GetServerSidePropsContext } from 'next'
 
-import { checkClientAppSession, checkWebSession } from '../helpers/session'
+import { checkSession } from '../helpers/session'
 
 export function getSessionAvailability(
   ctx: GetServerSidePropsContext,
 ): boolean {
-  const isClientApp = checkClientApp(ctx.req.headers['user-agent'] ?? '')
-
-  return isClientApp ? checkClientAppSession(ctx.req) : checkWebSession(ctx.req)
+  return checkSession(ctx.req)
 }
