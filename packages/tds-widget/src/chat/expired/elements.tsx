2145fb5d48202a013b65f02e03dd92cc1b7d5df8
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-03-24 07:34:47 +0900

    feat: expired 컴포넌트 추가

---
+++ packages/tds-widget/src/chat/expired/elements.tsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+import { Container as BaseContainer } from '@titicaca/tds-ui'
+
+import { expiredTheme } from './theme-provider'
+
+export const ButtonIcon = styled.div<{ src: string }>`
+  width: 16px;
+  height: 16px;
+  background-image: url(${({ src }) => src});
+  background-size: 16px 16px;
+  display: inline-block;
+  vertical-align: text-top;
+`
+
+export const Button = styled.button.attrs({ type: 'button' })`
+  padding: 8px 16px;
+  color: ${({ theme }) => (theme.expired || expiredTheme).button.color};
+  font-weight: 400;
+  border: 1px solid
+    ${({ theme }) => (theme.expired || expiredTheme).button.borderColor};
+  border-radius: 8px;
+  font-size: 14px;
+  line-height: 19px;
+  background-color: ${({ theme }) =>
+    (theme.expired || expiredTheme).button.backgroundColor};
+
+  ${ButtonIcon} {
+    margin-right: 6px;
+  }
+`
+
+export const Container = styled(BaseContainer)`
+  background-color: ${({ theme }) =>
+    (theme.expired || expiredTheme).backgroundColor};
+  padding: 50px 26px 60px;
+  font-size: 14px;
+  line-height: 19px;
+  align-items: center;
+  text-align: center;
+  color: #666;
+  font-weight: 400;
+  white-space: pre-line;
+
+  h2 {
+    font-size: 16px;
+    line-height: 22px;
+    font-weight: 700;
+    color: ${({ theme }) => (theme.expired || expiredTheme).titleColor};
+    margin-bottom: 6px;
+  }
+
+  ${Button} {
+    margin-top: 16px;
+
+    & + ${Button} {
+      margin-top: 8px;
+    }
+  }
+`
