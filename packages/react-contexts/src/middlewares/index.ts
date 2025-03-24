f5ab1ec3c9ee21f4c655f11cd6a002d53cabd1b7
hailey <hailey@interparktriple.com>
2025-03-24 11:40:54 +0900

    middleware에 chain, 기타 미들웨어 export 추가

--- packages/react-contexts/src/middlewares/index.ts
+++ packages/react-contexts/src/middlewares/index.ts
@@ -7,3 +7,6 @@ export const constructMiddleware = (functions: MiddlewareFactory[]) =>
   chain([...functions, refreshSessionMiddleware, setWebDeviceIdMiddleware])
 
 export * from './types'
+export { chain } from './chain'
+export { refreshSessionMiddleware } from './refresh-session'
+export { setWebDeviceIdMiddleware } from './set-web-device-id'
