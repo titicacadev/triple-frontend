7abedb63a4d7a45684f3d0371c5d38a2f6f9a2e9
hailey <hailey@interparktriple.com>
2025-03-25 08:43:09 +0900

    setWebDeviceId 미들웨어에 domain 적용

--- packages/middlewares/src/set-web-device-id.ts
+++ packages/middlewares/src/set-web-device-id.ts
@@ -6,6 +6,7 @@ import {
 } from 'next/server'
 import { v4 as uuidV4 } from 'uuid'
 import { X_TRIPLE_WEB_DEVICE_ID } from '@titicaca/constants'
+import { parseUrl } from '@titicaca/view-utilities'
 
 import { getIsTripleApp } from './utils/get-triple-app'
 import { applySetCookie } from './utils/apply-set-cookie'
@@ -31,10 +32,19 @@ export function setWebDeviceIdMiddleware(next: NextMiddleware) {
       const randomWebDeviceId = uuidV4()
       response.cookies.set(X_TRIPLE_WEB_DEVICE_ID, randomWebDeviceId, {
         secure: true,
+        domain: getDomain(request),
       })
       applySetCookie(request, response)
     }
 
     return response
   }
+}
+
+function getDomain(request: NextRequest) {
+  const hostFromRequest = request.headers.get('host')
+  const isLocalhost = hostFromRequest?.split(':')[0] === 'localhost'
+  const { host } = parseUrl(process.env.NEXT_PUBLIC_WEB_URL_BASE)
+
+  return isLocalhost ? 'localhost' : `.${host}`
 }
