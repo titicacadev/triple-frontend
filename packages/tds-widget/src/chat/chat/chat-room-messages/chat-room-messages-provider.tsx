60bf4a083221e86d169efef16c2e318fc306e170
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-03-25 10:07:01 +0900

    refactor: chat-room-messages provider 추가

---
+++ packages/tds-widget/src/chat/chat/chat-room-messages/chat-room-messages-provider.tsx
@@ -0,0 +1,36 @@
+import { PropsWithChildren } from 'react'
+
+import { ChatRoomInterface, ChatUserInterface, UserType } from '../../types'
+import { ScrollProvider } from '../scroll-context'
+import { RoomProvider, type RoomProviderProps } from '../room-context'
+
+import {
+  ChatMessagesProvider,
+  type ChatMessagesProviderProps,
+} from './chat-message-context'
+
+interface ChatRoomMessagesProviderProps<
+  T = ChatRoomInterface,
+  U = ChatUserInterface,
+  V = UserType,
+> extends ChatMessagesProviderProps<V>,
+    RoomProviderProps<T, U> {}
+
+export function ChatRoomMessagesProvider<
+  T = ChatRoomInterface,
+  U = ChatUserInterface,
+  V = UserType,
+>({
+  room,
+  me,
+  children,
+  ...props
+}: PropsWithChildren<ChatRoomMessagesProviderProps<T, U, V>>) {
+  return (
+    <RoomProvider room={room} me={me}>
+      <ScrollProvider>
+        <ChatMessagesProvider {...props}>{children}</ChatMessagesProvider>
+      </ScrollProvider>
+    </RoomProvider>
+  )
+}
