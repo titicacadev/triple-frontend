eadb49fd7317ae8c5bc164b6e9b3916e627379eb
guswl98 <hjee0901@gmail.com>
2025-03-26 16:35:47 +0900

    앱/웹 구분 없이 refresh 시도

--- packages/triple-web-nextjs-pages/src/initializers/session.ts
+++ packages/triple-web-nextjs-pages/src/initializers/session.ts
@@ -7,35 +7,22 @@ import {
   post,
   get,
 } from '@titicaca/fetcher'
-import {
-  GET_USER_REQUEST_URL,
-  checkClientApp,
-} from '@titicaca/triple-web-utils'
+import { GET_USER_REQUEST_URL } from '@titicaca/triple-web-utils'
 
 import { checkSession } from '../helpers/session'
 
 /**
- * - app (server-side): refresh X
- * - app (client-side): refresh X
- * - browser (server-side) refresh O
- * - browser (client-side) refresh O
  * @returns
  */
 export async function getSession(ctx: NextPageContext): Promise<SessionValue> {
-  const userAgent = ctx.req
-    ? (ctx.req.headers['user-agent'] ?? '')
-    : window.navigator.userAgent
-
-  const isClientApp = checkClientApp(userAgent)
-
-  const user = await fetchUser(ctx, isClientApp)
+  const user = await fetchUser(ctx)
 
   return {
     user,
   }
 }
 
-async function fetchUser(ctx: NextPageContext, isClientApp: boolean) {
+async function fetchUser(ctx: NextPageContext) {
   if (ctx.req) {
     // Server-side
 
@@ -52,80 +39,49 @@ async function fetchUser(ctx: NextPageContext, isClientApp: boolean) {
       cookie: ctx.req.headers.cookie,
     }
 
-    if (isClientApp) {
-      const finalFetcher = ssrFetcherize(get, ssrFetcherizeOptions)
+    const finalFetcher = authFetcherize(
+      ssrFetcherize(get, ssrFetcherizeOptions),
+      {
+        refresh: () =>
+          ssrFetcherize(
+            post,
+            ssrFetcherizeOptions,
+          )('/api/users/web-session/token'),
+      },
+    )
 
-      const response = await finalFetcher<SessionUser>(GET_USER_REQUEST_URL)
+    const response = await finalFetcher<SessionUser>(GET_USER_REQUEST_URL)
 
-      if (response.status !== 401) {
-        captureHttpError(response)
-      }
-
-      if (response.ok === false) {
-        return null
-      }
-
-      return response.parsedBody
-    } else {
-      const finalFetcher = authFetcherize(
-        ssrFetcherize(get, ssrFetcherizeOptions),
-        {
-          refresh: () =>
-            ssrFetcherize(
-              post,
-              ssrFetcherizeOptions,
-            )('/api/users/web-session/token'),
-        },
-      )
-
-      const response = await finalFetcher<SessionUser>(GET_USER_REQUEST_URL)
-
-      if (response === 'NEED_LOGIN') {
-        return null
-      }
-
-      captureHttpError(response)
-
-      if (response.ok === false) {
-        return null
-      }
-
-      return response.parsedBody
+    if (response === 'NEED_LOGIN') {
+      return null
     }
+
+    captureHttpError(response)
+
+    if (response.ok === false) {
+      return null
+    }
+
+    return response.parsedBody
   } else {
     // Client-side
-    if (isClientApp) {
-      const finalFetcher = get
 
-      const response = await finalFetcher<SessionUser>(GET_USER_REQUEST_URL)
+    const finalFetcher = authFetcherize(get, {
+      refresh: () => post('/api/users/web-session/token'),
+    })
 
-      if (response.status !== 401) {
-        captureHttpError(response)
-      }
+    const response = await finalFetcher<SessionUser>(GET_USER_REQUEST_URL)
 
-      if (response.ok === false) {
-        return null
-      }
-
-      return response.parsedBody
-    } else {
-      const finalFetcher = authFetcherize(get, {
-        refresh: () => post('/api/users/web-session/token'),
-      })
-
-      const response = await finalFetcher<SessionUser>(GET_USER_REQUEST_URL)
-
-      if (response === 'NEED_LOGIN') {
-        return null
-      }
-
-      captureHttpError(response)
-
-      if (response.ok === false) {
-        return null
-      }
-
-      return response.parsedBody
+    if (response === 'NEED_LOGIN') {
+      return null
     }
+
+    captureHttpError(response)
+
+    if (response.ok === false) {
+      return null
+    }
+
+    return response.parsedBody
   }
 }
