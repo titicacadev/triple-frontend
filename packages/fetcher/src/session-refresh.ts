1d5f29a4c2cce229c7d921ff255aa72e2eae4904
guswl98 <hjee0901@gmail.com>
2025-03-28 15:38:41 +0900

    getSetCookie 메소드로 변경

--- packages/fetcher/src/session-refresh.ts
+++ packages/fetcher/src/session-refresh.ts
@@ -1,5 +1,3 @@
-import { parseString, splitCookiesString } from 'set-cookie-parser'
-
 import { ssrFetcherize } from './factories'
 import { post } from './methods'
 import { RequestOptions } from './types'
@@ -9,8 +7,8 @@ export type SetCookie = Record<string, string>
  *
  * @param fetcher post 함수
  * @param options fetcher에 전달할 옵션
- * @returns session 갱신이 성공한 경우에는 새로운 쿠키를 key: set-cookie 값의 형태로 반환하고, 실패한 경우에는 undefined를 반환합니다.
- * ex) TP_SE : 'TP_SE=some session token; Path=/; HttpOnly; Secure'
+ * @returns session 갱신이 성공한 경우에는 새로운 쿠키를 string[] 값의 형태로 반환하고, 실패한 경우에는 undefined를 반환합니다.
+ * ex)  ['TP_SE=some session token; Path=/; HttpOnly; Secure']
  */
 export async function sessionRefresh({
   fetcher = post,
@@ -18,22 +16,14 @@ export async function sessionRefresh({
 }: {
   fetcher?: typeof post
   options?: RequestOptions
-}): Promise<SetCookie | undefined> {
+}): Promise<string[] | undefined> {
   const response = await fetcher<Record<never, never> | { message: string }>(
     '/api/users/web-session/token',
     options,
   )
   if (response.ok) {
-    const setCookie = response.headers.get('set-cookie')
-    if (setCookie) {
-      const setCookies = splitCookiesString(setCookie)
-      const setCookiesMap: SetCookie = {}
-      setCookies.forEach((cookie) => {
-        const { name } = parseString(cookie)
-        setCookiesMap[name] = cookie
-      })
-      return setCookiesMap
-    }
+    const setCookie = response.headers.getSetCookie()
+    return setCookie
   }
   return undefined
 }
