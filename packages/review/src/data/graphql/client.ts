1d1dc38e3b45aa6b56071fbf7a43da138e1eb1d7
guswl98 <hjee0901@gmail.com>
2025-03-28 11:46:20 +0900

    review session refresh 로직 추가

--- packages/review/src/data/graphql/client.ts
+++ packages/review/src/data/graphql/client.ts
@@ -1,4 +1,5 @@
-import { request } from 'graphql-request'
+import { ClientError, request } from 'graphql-request'
+import { sessionRefresh } from '@titicaca/fetcher'
 
 import { Requester, getSdk } from './generated'
 
@@ -6,3 +7,16 @@ const requester: Requester = (doc, vars) =>
   request({ document: doc, url: '/api/graphql', variables: vars ?? undefined })
 
 export const client = getSdk(requester)
+
+export async function reviewClient<T>(query: Promise<T>) {
+  try {
+    const response = await query
+    return response
+  } catch (e) {
+    if (e instanceof ClientError && e.response.status === 401) {
+      await sessionRefresh({})
+      return query
+    }
+    throw e
+  }
+}
