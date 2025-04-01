5e849c14efa1fc8629e0df5618327044d1cabc02
guswl98 <hjee0901@gmail.com>
2025-04-01 11:36:53 +0900

    handle401Error로 401 처리 변경

--- packages/tds-widget/src/review/data/graphql/client.ts
+++ packages/tds-widget/src/review/data/graphql/client.ts
@@ -1,5 +1,6 @@
 import { ClientError, request } from 'graphql-request'
 import { sessionRefresh } from '@titicaca/fetcher'
+import { ACCESS_TOKEN_EXPIRED_EXCEPTION } from '@titicaca/fetcher/src/response-handler'
 
 import { Requester, getSdk } from './generated'
 
@@ -18,10 +19,12 @@ export async function reviewClient<T>(query: () => Promise<T>) {
     return response
   } catch (e) {
     if (e instanceof ClientError && e.response.status === 401) {
-      const refreshResponse = await sessionRefresh({})
-      if (refreshResponse) {
-        const newResponse = await query()
-        return newResponse
+      if (e.response.exception === ACCESS_TOKEN_EXPIRED_EXCEPTION) {
+        const refreshResponse = await sessionRefresh({})
+        if (refreshResponse) {
+          const newResponse = await query()
+          return newResponse
+        }
       }
     }
     throw e
