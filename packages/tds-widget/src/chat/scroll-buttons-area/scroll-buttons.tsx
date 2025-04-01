4a6f2635bf3b354485057418685c78175189dc5a
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-04-01 10:15:52 +0900

    feat: 스타일 커스텀 허용 및 export 누락 추가

--- packages/tds-widget/src/chat/scroll-buttons-area/scroll-buttons.tsx
+++ packages/tds-widget/src/chat/scroll-buttons-area/scroll-buttons.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useRef, useState } from 'react'
+import { CSSProp } from 'styled-components'
 
 import { ChatMessageInterface, UserType } from '../types'
 
 import { NewMessage, ScrollToBottomButton } from './elements'
 
-interface ScrollButtonsProps<T = UserType> {
+export interface ScrollButtonsProps<T = UserType> {
   onClick: (behavior: ScrollBehavior) => void
   message?: ChatMessageInterface<T>
   newMessageActive: boolean
   isBottomIntersecting: boolean
+  scrollButtonsStyle?: {
+    scrollToButton?: {
+      css?: CSSProp
+    }
+    newMessage?: {
+      css?: CSSProp
+    }
+  }
 }
 
 export function ScrollButtons<T = UserType>({
@@ -16,6 +25,7 @@ export function ScrollButtons<T = UserType>({
   onClick,
   isBottomIntersecting,
   newMessageActive,
+  scrollButtonsStyle = {},
 }: ScrollButtonsProps<T>) {
   const [message, setMessage] = useState<ChatMessageInterface<T> | null>(null)
   const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null)
@@ -45,9 +55,16 @@ export function ScrollButtons<T = UserType>({
   return (
     <>
       {!(isBottomIntersecting || message) && (
-        <ScrollToBottomButton onClick={onClick} />
+        <ScrollToBottomButton
+          onClick={onClick}
+          css={scrollButtonsStyle?.scrollToButton?.css}
+        />
       )}
-      <NewMessage message={message} onClick={handleClick} />
+      <NewMessage
+        message={message}
+        onClick={handleClick}
+        css={scrollButtonsStyle?.newMessage?.css}
+      />
     </>
   )
 }
