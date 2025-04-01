435f579f469440209a4e341ab64ad319e0d53d6e
guswl98 <hjee0901@gmail.com>
2025-04-01 10:25:22 +0900

    refresh가 필요한 경우를 구분

--- packages/fetcher/src/response-handler.ts
+++ packages/fetcher/src/response-handler.ts
@@ -1,6 +1,7 @@
 import { withScope, captureException } from '@sentry/nextjs'
 
 import { HttpResponse } from './types'
+import { NEED_LOGIN_IDENTIFIER } from './factories'
 
 export function captureHttpError<
   Response extends HttpResponse<unknown, unknown>,
@@ -12,4 +13,28 @@ export function captureHttpError<
       captureException(new Error(`${response.status} - ${response.url}`))
     })
   }
+}
+
+export const ACCESS_TOKEN_EXPIRED_EXCEPTION = 'AccessTokenExpiredException'
+export const NEED_REFRESH_IDENTIFIER = 'NEED_REFRESH'
+
+type ResponseWithError = Pick<Response, 'headers' | 'ok' | 'status' | 'url'> & {
+  ok: false
+  parsedBody: { exception: string; message: string; status: string }
+}
+
+export function handle401Error<SuccessBody, FailureBody>(
+  response: HttpResponse<SuccessBody, FailureBody>,
+) {
+  if (!response.ok) {
+    const errorResponse = response as ResponseWithError
+    if (errorResponse.status === 401) {
+      const { exception } = errorResponse.parsedBody
+      if (exception === ACCESS_TOKEN_EXPIRED_EXCEPTION) {
+        return NEED_REFRESH_IDENTIFIER
+      }
+      return NEED_LOGIN_IDENTIFIER
+    }
+  }
+  return response
 }
