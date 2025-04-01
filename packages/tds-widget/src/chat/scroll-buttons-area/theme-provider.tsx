2f8f50fb4f5b685836ed670e8ae82f326189214e
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-04-01 10:08:09 +0900

    chore: 컬러 주석 추가

--- packages/tds-widget/src/chat/scroll-buttons-area/theme-provider.tsx
+++ packages/tds-widget/src/chat/scroll-buttons-area/theme-provider.tsx
@@ -3,16 +3,16 @@ import { DefaultTheme, ThemeProvider } from 'styled-components'
 
 export const scrollButtonsTheme = {
   scrollToBottomButton: {
-    backgroundColor: 'rgba(255, 255, 255, 1)',
-    borderColor: 'rgba(234, 234, 234, 1)',
+    backgroundColor: 'rgba(255, 255, 255, 1)', // color-neutral-w-100
+    borderColor: 'rgba(234, 234, 234, 1)', // color-neutral-g-10
   },
   newMessageButton: {
-    backgroundColor: 'rgba(255, 255, 255, 1)',
-    borderColor: 'rgba(41, 41, 45, 0.1)',
+    backgroundColor: 'rgba(255, 255, 255, 1)', // color-neutral-w-100
+    borderColor: 'rgba(41, 41, 45, 0.1)', // color-neutral-b-10
     boxShadowColor: 'rgba(0, 0, 0, 0.12)',
-    messageColor: 'rgba(41, 41, 45, 1)',
-    nameColor: 'rgba(126, 126, 129, 1)',
-    thumbnailBorderColor: 'rgba(223, 223, 224, 1)',
+    messageColor: 'rgba(41, 41, 45, 1)', // color-neutral-b-100
+    nameColor: 'rgba(126, 126, 129, 1)', // color-neutral-g-60
+    thumbnailBorderColor: 'rgba(223, 223, 224, 1)', // color-neutral-g-15
   },
 }
 
