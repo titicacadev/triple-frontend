8601ee0d0b7afeab50dcfd6b15f513c4594f73fc
hailey <hailey@interparktriple.com>
2025-04-10 18:08:02 +0900

    SessionAvailability 여부에 SESSION_KEY도 확인

--- packages/react-contexts/src/session-context/session-availability.ts
+++ packages/react-contexts/src/session-context/session-availability.ts
@@ -1,10 +1,13 @@
 import { IncomingMessage } from 'http'
 
 import Cookies from 'universal-cookie'
-import { TP_TK } from '@titicaca/constants'
+import { SESSION_KEY, TP_TK } from '@titicaca/constants'
 
 export default function getSessionAvailabilityFromRequest(
   req: IncomingMessage | undefined,
 ) {
-  return !!new Cookies(req?.headers.cookie).get(TP_TK)
+  return (
+    !!new Cookies(req?.headers.cookie).get(TP_TK) ||
+    !!new Cookies(req?.headers.cookie).get(SESSION_KEY)
+  )
 }
