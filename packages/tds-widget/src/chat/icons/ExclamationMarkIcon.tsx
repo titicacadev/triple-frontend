7466146eac9398b1d7c8d82ff4d56018dcad10fb
hoolie-triple <hoolie96@interparktriple.com>
2025-04-11 10:24:59 +0900

    fix: type error

--- packages/tds-widget/src/chat/icons/ExclamationMarkIcon.tsx
+++ packages/tds-widget/src/chat/icons/ExclamationMarkIcon.tsx
@@ -1,4 +1,10 @@
-export default function ExclamationMarkIcon({ color }: { color?: string }) {
+import { styled, CSSProp } from 'styled-components'
+
+const Path = styled.path<{ color?: CSSProp }>`
+  stroke: ${({ color }) => color || '#3A3A3A'};
+`
+
+export default function ExclamationMarkIcon({ color }: { color?: CSSProp }) {
   return (
     <svg
       width="16"
@@ -8,14 +14,14 @@ export default function ExclamationMarkIcon({ color }: { color?: string }) {
       xmlns="http://www.w3.org/2000/svg"
       style={{ flex: '0 0 16px' }}
     >
-      <path
+      <Path
         d="M7.99995 14.3C11.4793 14.3 14.3 11.4794 14.3 8.00001C14.3 4.52062 11.4793 1.70001 7.99995 1.70001C4.52056 1.70001 1.69995 4.52062 1.69995 8.00001C1.69995 11.4794 4.52056 14.3 7.99995 14.3Z"
-        stroke={color || '#3A3A3A'}
+        color={color}
         strokeWidth="1.2"
       />
-      <path
+      <Path
         d="M7.38477 9.03192L7.26758 4.35419H8.73242L8.61523 9.03192H7.38477ZM8 11.4587C7.55566 11.4587 7.2041 11.1218 7.2041 10.6921C7.2041 10.2624 7.55566 9.92548 8 9.92548C8.44434 9.92548 8.7959 10.2624 8.7959 10.6921C8.7959 11.1218 8.44434 11.4587 8 11.4587Z"
-        fill={color || '#3A3A3A'}
+        color={color}
       />
     </svg>
   )
