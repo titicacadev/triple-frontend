10417c0607a5e6c81ceaf488cbaf1e7f19f360c1
guswl98 <hjee0901@gmail.com>
2025-04-14 15:48:56 +0900

    next13 용 middleware 추가

--- packages/middlewares/src/index.ts
+++ packages/middlewares/src/index.ts
@@ -1,12 +1,20 @@
 import { chain } from './chain'
 import type { MiddlewareFactory } from './types'
-import { refreshSessionMiddleware } from './refresh-session'
+import {
+  refreshSessionMiddleware,
+  refreshSessionMiddlewareNext13,
+} from './refresh-session'
 import { setWebDeviceIdMiddleware } from './set-web-device-id'
 
 export const constructMiddleware = (functions: MiddlewareFactory[]) =>
   chain([...functions, refreshSessionMiddleware, setWebDeviceIdMiddleware])
+export const constructMiddlewareNext13 = (functions: MiddlewareFactory[]) =>
+  chain([...functions, refreshSessionMiddlewareNext13])
 
 export * from './types'
 export { chain } from './chain'
-export { refreshSessionMiddleware } from './refresh-session'
+export {
+  refreshSessionMiddleware,
+  refreshSessionMiddlewareNext13,
+} from './refresh-session'
 export { setWebDeviceIdMiddleware } from './set-web-device-id'
