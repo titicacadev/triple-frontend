386fb788cb591d6ad76c322e550688c41c6c3a0c
guswl98 <hjee0901@gmail.com>
2025-04-15 16:55:02 +0900

    context가 null일 때는 값을 그대로 사용하도록 변경

--- packages/triple-web/src/client-app/use-client-app.ts
+++ packages/triple-web/src/client-app/use-client-app.ts
@@ -7,15 +7,17 @@ import { getClientApp } from './get-client-app'
  * ClientAppContext 값을 가져옵니다.
  */
 export function useClientApp() {
+  const context = useContext(ClientAppContext)
   const clientApp =
-    useContext(ClientAppContext) ??
-    (typeof window !== 'undefined'
-      ? getClientApp({
-          userAgent: undefined,
-          autoplay: undefined,
-          networkType: undefined,
-        })
-      : undefined)
+    context === undefined
+      ? typeof window !== 'undefined'
+        ? getClientApp({
+            userAgent: undefined,
+            autoplay: undefined,
+            networkType: undefined,
+          })
+        : undefined
+      : context
 
   if (clientApp === undefined) {
     throw new Error('ClientAppContext가 없거나 클라이언트 환경이 아닙니다.')
