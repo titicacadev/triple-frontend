a02f29262f03d168e744bf244195fd0bc8744feb
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-04-16 14:25:57 +0900

    [DMTALK-176] fix: 스토리북용 놀컬러 추가

--- packages/tds-widget/src/chat/reservation-info/reservation-info.stories.tsx
+++ packages/tds-widget/src/chat/reservation-info/reservation-info.stories.tsx
@@ -1,39 +1,9 @@
 import type { Meta, StoryFn } from '@storybook/react'
 
 import { NolThemeProvider } from '../nol-theme-provider'
+import { NOL_COLOR } from '../nol-theme-provider/constants'
 
 import { ReservationInfo, type ReservationInfoProps } from './reservation-info'
-
-const NOL_COLOR = {
-  // black
-  'color-neutral-b-100': 'rgba(41, 41, 45, 1)',
-  'color-neutral-b-80': 'rgba(41, 41, 45, 0.8)',
-  'color-neutral-b-60': 'rgba(41, 41, 45, 0.6)',
-  'color-neutral-b-50': 'rgba(41, 41, 45, 0.5)',
-  'color-neutral-b-15': 'rgba(41, 41, 45, 0.15)',
-  'color-neutral-b-10': 'rgba(41, 41, 45, 0.1)',
-  'color-neutral-b-5': 'rgba(41, 41, 45, 0.05)',
-
-  // white
-  'color-neutral-w-100': 'rgba(255, 255, 255, 1)',
-  'color-neutral-w-20': 'rgba(255, 255, 255, 0.2)',
-
-  // gray
-  'color-neutral-g-80': 'rgba(84, 84, 87, 1)',
-  'color-neutral-g-60': 'rgba(126, 126, 129, 1)',
-  'color-neutral-g-50': 'rgba(148, 148, 150, 1)',
-  'color-neutral-g-30': 'rgba(191, 191, 192, 1)',
-  'color-neutral-g-15': 'rgba(223, 223, 224, 1)',
-  'color-neutral-g-10': 'rgba(234, 234, 234, 1)',
-  'color-neutral-g-5': 'rgba(244, 244, 245, 1)',
-
-  // primary
-  'color-primary-nol': 'rgba(65, 84, 255, 1)',
-
-  // red
-  'color-primary-red': 'rgba(255, 50, 46, 1)',
-  'color-brand-shopping-red-400': 'rgba(255, 114, 98, 1)',
-}
 
 export default {
   title: 'tds-widget / chat / ReservationInfo',
