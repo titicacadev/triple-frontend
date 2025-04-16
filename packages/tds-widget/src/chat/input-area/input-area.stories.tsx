bd801c8776c42692d2716150d3c12999ed27ece9
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-04-16 14:18:43 +0900

    [DMTALK-176] fix: 스토리북 오류 수정

--- packages/tds-widget/src/chat/input-area/input-area.stories.tsx
+++ packages/tds-widget/src/chat/input-area/input-area.stories.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import type { Meta, StoryFn } from '@storybook/react'
 
+import { NolThemeProvider } from '../nol-theme-provider'
+
 import { InputAreaUI, InputAreaUIProps, NolInputAreaUI } from './index'
 
 export default {
@@ -36,7 +38,17 @@ export const Default = {
 }
 
 const NolTemplate: StoryFn<InputAreaUIProps> = (args) => (
-  <NolInputAreaUI {...args} />
+  <NolThemeProvider
+    theme={{
+      'color-neutral-b-100': 'rgba(41, 41, 45, 1)',
+      'color-neutral-w-100': 'rgba(255, 255, 255, 1)',
+      'color-neutral-g-50': 'rgba(148, 148, 150, 1)',
+      'color-neutral-g-15': 'rgba(223, 223, 224, 1)',
+      'color-primary-nol': 'rgba(65, 84, 255, 1)',
+    }}
+  >
+    <NolInputAreaUI {...args} />
+  </NolThemeProvider>
 )
 
 export const Nol = {
