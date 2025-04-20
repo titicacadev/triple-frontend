cb32008126af39840c13598c7fb4b8f116e3845d
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-04-21 08:24:45 +0900

    [DMTALK-184] fix: 아이콘 오류 수정

--- packages/tds-widget/src/chat/icons/ExclamationMarkIcon.tsx
+++ packages/tds-widget/src/chat/icons/ExclamationMarkIcon.tsx
@@ -1,10 +1,15 @@
 import { styled, CSSProp } from 'styled-components'
 
-const Path = styled.path<{ color?: CSSProp }>`
-  stroke: ${({ color }) => color || '#3A3A3A'};
+const Path = styled.path<{ stroke?: CSSProp; fill?: CSSProp }>`
+  stroke: ${({ stroke }) => stroke};
+  fill: ${({ fill }) => fill};
 `
 
-export default function ExclamationMarkIcon({ color }: { color?: CSSProp }) {
+export default function ExclamationMarkIcon({
+  color = '#3A3A3A',
+}: {
+  color?: CSSProp
+}) {
   return (
     <svg
       width="16"
@@ -16,12 +21,12 @@ export default function ExclamationMarkIcon({ color }: { color?: CSSProp }) {
     >
       <Path
         d="M7.99995 14.3C11.4793 14.3 14.3 11.4794 14.3 8.00001C14.3 4.52062 11.4793 1.70001 7.99995 1.70001C4.52056 1.70001 1.69995 4.52062 1.69995 8.00001C1.69995 11.4794 4.52056 14.3 7.99995 14.3Z"
-        color={color}
+        stroke={color}
         strokeWidth="1.2"
       />
       <Path
         d="M7.38477 9.03192L7.26758 4.35419H8.73242L8.61523 9.03192H7.38477ZM8 11.4587C7.55566 11.4587 7.2041 11.1218 7.2041 10.6921C7.2041 10.2624 7.55566 9.92548 8 9.92548C8.44434 9.92548 8.7959 10.2624 8.7959 10.6921C8.7959 11.1218 8.44434 11.4587 8 11.4587Z"
-        color={color}
+        fill={color}
       />
     </svg>
   )
