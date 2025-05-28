888d5e964476bff9cf355455fb042b48fad9bf7c
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-05-28 17:45:59 +0900

    [DMTALK-374] feat: rich bubble splitter 추가

---
+++ packages/tds-widget/src/chat/bubble/button.tsx
@@ -0,0 +1,38 @@
+import { css } from 'styled-components'
+
+import { useATagNavigator } from '../utils'
+
+import Bubble from './bubble'
+import { ButtonBubbleProp } from './type'
+import { TextItem } from './item'
+
+export function ButtonBubble({
+  id,
+  label,
+  action,
+  onLinkClick,
+  my,
+  ...props
+}: ButtonBubbleProp) {
+  const aTagNavigator = useATagNavigator(onLinkClick)
+
+  return (
+    <Bubble
+      id={id}
+      css={css`
+        a {
+          color: ${my ? '#B5FFFB' : 'var(--color-blue)'};
+          text-decoration: underline;
+        }
+      `}
+      my={my}
+      {...props}
+    >
+      <TextItem
+        href={action.param}
+        text={label}
+        onClick={(e) => aTagNavigator(e)}
+      />
+    </Bubble>
+  )
+}
