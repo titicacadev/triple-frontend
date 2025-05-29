501104585fa068b5b9a7fddd7915abe41f385419
guswl98 <hjee0901@gmail.com>
2025-05-29 09:07:32 +0900

    ContextValue 설명 추가

--- packages/triple-web/src/hash-router/context.tsx
+++ packages/triple-web/src/hash-router/context.tsx
@@ -11,10 +11,35 @@ import {
 import { useUserAgent } from '../user-agent/use-user-agent'
 
 export interface HashRouterContextValue {
+  /**
+   * @deprecated 'uriHash는 Multiple hash 값을 지원하지 않습니다. 대신 hasUriHash 메소드를 사용하세요.'
+   */
   uriHash: string
+  /**
+   *
+   * @param hash 추가할 hash 값입니다.
+   * @param type 기기 환경에 의존하지 않고 강제로 hash를 추가하고 싶을 때 지정합니다. removeUriHash와 동일한 값을 설정해야합니다.
+   */
   addUriHash: (hash: string, type?: 'push' | 'replace') => void
+  /**
+   * 현재 hash 값에서 마지막 hash를 제거합니다.
+   * @param type 기기 환경에 의존하지 않고 강제로 hash를 제거하고 싶을 때 지정합니다. addUriHash와 동일한 값을 설정해야합니다.
+   */
   removeUriHash: (type?: 'pop' | 'replace') => void
+  /**
+   *
+   * @param hash 확인할 hash 값입니다.
+   * @returns hash값이 현재 uriHash에 포함되어 있는지 여부를 반환합니다.
+   */
   hasUriHash: (hash: string) => boolean
+  /**
+   *
+   * @param sourceHash 대체될 hash 값입니다.
+   * @param targetHash 대체될 hash 값입니다.
+   *
+   * 하나의 hash가 제거되는 동시에 다른 hash를 추가해야할 때 유용합니다.
+   */
+  replaceUriHash: (sourceHash: string, targetHash: string) => void
 }
 
 export const HashRouterContext = createContext<HashRouterContextValue | null>(
@@ -98,6 +123,27 @@ export function HashRouterProvider({ children }: { children: ReactNode }) {
     [isAndroid],
   )
 
+  const replaceUriHash = useCallback(
+    (sourceHash: string, targetHash: string) => {
+      if (!window.location.hash) {
+        return
+      }
+
+      const url = new URL(window.location.href)
+      const currentHash = window.location.hash.replace('#', '')
+
+      if (currentHash.includes(sourceHash)) {
+        url.hash = currentHash.replace(sourceHash, targetHash)
+        window.history.replaceState(null, '', url)
+        window.dispatchEvent(new Event('hashchange'))
+      } else if (process.env.NODE_ENV === 'development') {
+        // eslint-disable-next-line no-console
+        console.warn(`❗️${sourceHash} does not exist in the hash.`)
+      }
+    },
+    [],
+  )
+
   const hasUriHash = useCallback(
     (hash: string) => {
       return uriHash.split('&').includes(hash)
@@ -110,6 +156,7 @@ export function HashRouterProvider({ children }: { children: ReactNode }) {
     addUriHash,
     removeUriHash,
     hasUriHash,
+    replaceUriHash,
   }
 
   return (
