822e168e7ce7e33a3a49de8607f16aadb4420ed5
dia-triple <133629020+dia-triple@users.noreply.github.com>
2025-06-24 09:10:18 +0900

    [DMTALK-403] refactor: pusher data 타입 분리

---
+++ packages/tds-widget/src/chat/types/pusher.ts
@@ -0,0 +1,47 @@
+import { ChatMessageInterface } from './message'
+import { RoomType } from './room'
+import { HasUnreadOfRoomInterface } from './unread'
+import { ChatUserInterface, UserType } from './user'
+
+interface ChannelUser<T = UserType>
+  extends Omit<ChatUserInterface<T>, 'channel'> {
+  roomMemberId: string
+}
+
+interface ChannelRoom<T = RoomType> {
+  id: string
+  name?: string
+  isDirect: boolean
+  lastMessageId: number
+  memberIds: string[]
+  type: T
+  privateChannel: boolean
+}
+
+interface ChannelRoomMetadata {
+  memberCounts: number
+}
+
+/**
+ * sendUnreadMessage 이벤트로 전달되는 데이터 타입
+ */
+export interface UnreadChatMessageData<T = UserType> {
+  message?: ChatMessageInterface<T>
+  otherUnreadInfo?: HasUnreadOfRoomInterface
+  roomId: string
+}
+
+/**
+ * sendMessage 이벤트로 전달되는 데이터 타입
+ */
+export interface ChatMessageData<T = UserType> {
+  message?: ChatMessageInterface<T>
+  metadata: ChannelRoomMetadata
+}
+
+export interface JoinedChatData<T = RoomType, U = UserType> {
+  room: ChannelRoom<T>
+  members: ChannelUser<U>[]
+  memberCounts: number
+  metadata: ChannelRoomMetadata
+}
