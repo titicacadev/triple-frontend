7fc306399efbf1448d009a19bde2f593c79a33dd
dia-triple <dia99@nol-universe.com>
2025-09-08 09:42:01 +0900

    [DMTALK-555] refactor: handleSendClick 공통화

--- packages/tds-widget/src/chat/input-area/utils.ts
+++ packages/tds-widget/src/chat/input-area/utils.ts
@@ -1,8 +1,37 @@
-import { SyntheticEvent } from 'react'
+import { RefObject, SyntheticEvent } from 'react'
 
 export function textAreaAutoResize(e: SyntheticEvent, maxHeight: number) {
   const textareaElement = e.target as HTMLTextAreaElement
   textareaElement.style.height = ''
   textareaElement.style.height =
     Math.min(textareaElement.scrollHeight, maxHeight) + 'px'
+}
+
+export function handleSendClick(
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  options: {
+    dismissKeyboardOnSend: boolean
+    textareaRef: RefObject<HTMLTextAreaElement>
+    inputValue: string
+    onSendMessage: () => void
+    minHeight: number
+  },
+) {
+  e.preventDefault()
+  const {
+    dismissKeyboardOnSend,
+    textareaRef,
+    inputValue,
+    onSendMessage,
+    minHeight,
+  } = options
+  if (!dismissKeyboardOnSend) {
+    textareaRef.current?.focus()
+  }
+  if (inputValue.trim().length > 0) {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = `${minHeight}px`
+    }
+    onSendMessage()
+  }
 }
