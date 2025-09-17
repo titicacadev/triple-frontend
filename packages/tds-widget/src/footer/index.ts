9465c8a049f98019b1a9b32ab5cc220d657aaaf8
hailey_yanolja <hailey@nol-universe.com>
2025-09-17 14:09:54 +0900

    award 푸터 삭제

--- packages/tds-widget/src/footer/index.ts
+++ packages/tds-widget/src/footer/index.ts
@@ -1,6 +1,5 @@
 import { DefaultFooter } from './footers/default-footer'
 
 export { LogoFooter } from './footers/logo-footer'
-export { AwardFooter } from './footers/award-footer'
 
 export default DefaultFooter
