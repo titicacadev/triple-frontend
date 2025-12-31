13f03fca31c2914466847fc74004ee04686e8264
hailey_yanolja <hailey@nol-universe.com>
2025-12-31 10:36:46 +0900

    누락된 스토리북 props 추가

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,10 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+import {} from 'react'
+import type { CSSProp } from 'styled-components'
+
+declare module 'react' {
+  interface Attributes {
+    css?: CSSProp | undefined
+  }
+}
--- packages/directions-finder/src/directions-finder.stories.tsx
+++ packages/directions-finder/src/directions-finder.stories.tsx
@@ -13,6 +13,7 @@ export const Basic: StoryObj<typeof DirectionsFinder> = {
     localName: '東京ディズニーランド',
     localAddress: '〒279-0031 東京都千葉県浦安市舞浜11',
     phoneNumber: '+81453305211',
+    onCallGrabButtonClick: () => {},
   },
 }
 
