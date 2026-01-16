cb097d8bb024f6cbf78b823d96a3d134f38f32b8
hailey_yanolja <hailey@nol-universe.com>
2026-01-16 16:12:58 +0900

    intersection-observer 패키지 제거

--- packages/intersection-observer/src/index.ts
+++
@@ -1,5 +0,0 @@
-import IntersectionObserver from './lazy-loaded-intersection-observer'
-
-export { default as StaticIntersectionObserver } from './static-intersection-observer'
-export { default as useIntersection } from './use-intersection'
-export default IntersectionObserver
--- packages/intersection-observer/src/static-intersection-observer.tsx
+++
@@ -1,4 +0,0 @@
-import 'intersection-observer'
-import ReactIntersectionObserver from '@titicaca/react-intersection-observer'
-
-export default ReactIntersectionObserver
